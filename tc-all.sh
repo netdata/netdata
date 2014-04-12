99b7c855552849c58f6a0159f3b594681b0fb892
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2014-04-12 03:03:25 +0300

    reduced the time needed for each loop to 1/4; in my machine went from 22ms to 5ms per loop

--- tc-all.sh
+++ tc-all.sh
@@ -1,33 +1,66 @@
 #!/bin/sh
 
-t=$1
-x=$(( t + 1 - 1 ))
-if [ "$x" -eq 0 ]
-	then
-	x=1
-fi
-
 echo "MYPID $$"
 
+# check if we have a valid number for interval
+t=$1
+sleep_time=$(( t + 1 - 1 ))
+if [ "$sleep_time" -eq 0 ]
+	then
+	sleep_time=1
+fi
+
+devices=
+fix_names=
+show_tc() {
+	local x="$1"
+
+	echo "BEGIN $x"
+	
+	/sbin/tc -s class show dev $x
+	
+	# check FireQOS names for classes
+	if [ ! -z "$fix_names" -a -f /var/run/fireqos/ifaces/$x ]
+	then
+		name="`cat /var/run/fireqos/ifaces/$x`"
+		echo "SETDEVICENAME $name"
+
+		interface_classes=
+		. /var/run/fireqos/$name.conf
+		for x in $interface_classes_monitor
+		do
+				echo "SETCLASSNAME $x"
+		done
+	fi
+	echo "END $x"
+}
+
+all_devices() {
+	cat /proc/net/dev | grep ":" | cut -d ':' -f 1 | while read dev
+	do
+		l=`/sbin/tc class show dev $dev | wc -l`
+		[ $l -ne 0 ] && echo $dev
+	done
+}
+
+names_every=$((120 / sleep_time))
+c=0
 while [ 1 ]
 do
-	/sbin/tc qdisc show | cut -d ' ' -f 5 | sort -u | while read x
+	fix_names=
+	c=$((c + 1))
+
+	if [ $c -le 1 -o $c -ge $names_every ]
+	then
+		c=1
+		fix_names="YES"
+		devices="`all_devices`"
+	fi
+
+	for d in $devices
 	do
-		echo "BEGIN $x"
-		/sbin/tc -s class show dev $x
-		if [ -f /var/run/fireqos/ifaces/$x ]
-		then
-			name="`cat /var/run/fireqos/ifaces/$x`"
-			echo "SETDEVICENAME $name"
-			interface_classes=
-			. /var/run/fireqos/$name.conf
-			for x in $interface_classes_monitor
-			do
-					echo "SETCLASSNAME $x"
-			done
-		fi
-		echo "END $x"
+		show_tc $d
 	done
 
-	sleep $x
+	sleep $sleep_time
 done
