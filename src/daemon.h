d40c693e32638e189bcae3562427a4ae64309e13
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2016-04-03 23:55:53 +0300

    Squashed commit of the following:
    commit 950f04df117bf246cfa9a2fa99bf12a64d6bd0a4
    Author: Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
    Date:   Sun Apr 3 23:47:06 2016 +0300
    
        dont keep the pidfd open
    
    commit bd45505d0177703940b2393a5e36b3e80b912ff1
    Author: Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
    Date:   Sun Apr 3 23:33:18 2016 +0300
    
        dont unlink pidfile at start
    
    commit 4afd30e81d3cd70400162c7603b659fd35160c8b
    Author: Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
    Date:   Sun Apr 3 23:30:47 2016 +0300
    
        close pidfile then unlink
    
    commit faf66f4f5f39bda8d0b5e947afbdb8cd1ac505be
    Author: Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
    Date:   Sun Apr 3 19:38:49 2016 +0300
    
        fix to keep close the file if it has been successfully chwoned
    
    commit 0af1e73e6db3b681e8303ee394fc2ac003db6e67
    Author: Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
    Date:   Sun Apr 3 19:35:31 2016 +0300
    
        chown pidfile only when dropping privileges; do not keep the pidfile open when not dropping privileges
    
    commit 550c9f8e9757f5a5767d11764a80f7736c197b37
    Author: Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
    Date:   Sun Apr 3 19:15:32 2016 +0300
    
        do not chown pidfile
    
    commit ca808d2231ed175397408ea436a50642e10cd281
    Author: Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
    Date:   Sun Apr 3 18:57:30 2016 +0300
    
        added command line parameter -pidfile; the pidfile is only generated if this parameter is passed; #151 #156

--- src/daemon.h
+++ src/daemon.h
@@ -3,12 +3,13 @@
 
 extern void sig_handler(int signo);
 
-extern void prepare_rundir();
-
 extern int become_user(const char *username);
 
 extern int become_daemon(int dont_fork, int close_all_files, const char *user, const char *input, const char *output, const char *error, const char *access, int *access_fd, FILE **access_fp);
 
 extern void netdata_cleanup_and_exit(int i);
+
+extern char pidfile[];
+extern int pidfd;
 
 #endif /* NETDATA_DAEMON_H */
