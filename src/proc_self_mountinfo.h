2a9809c20a64c57ca55eba7553a4614bfa818144
Costa Tsaousis <costa@tsaousis.gr>
2016-04-30 14:36:38 +0300

    support for newer kernels that show cgroups mountinfo at super_options

--- src/proc_self_mountinfo.h
+++ src/proc_self_mountinfo.h
@@ -34,6 +34,7 @@ struct mountinfo {
 
 extern struct mountinfo *mountinfo_find(struct mountinfo *root, unsigned long major, unsigned long minor);
 extern struct mountinfo *mountinfo_find_by_filesystem_mount_source(struct mountinfo *root, const char *filesystem, const char *mount_source);
+extern struct mountinfo *mountinfo_find_by_filesystem_super_option(struct mountinfo *root, const char *filesystem, const char *super_options);
 
 extern void mountinfo_free(struct mountinfo *mi);
 extern struct mountinfo *mountinfo_read();
