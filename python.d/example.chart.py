bd546d32209e4cc5ccc8d91a3e70a8808ffe73e0
paulfantom <paulfantom@gmail.com>
2016-06-10 14:27:44 +0200

    Initial support for python plugins

---
+++ python.d/example.chart.py
@@ -0,0 +1,15 @@
+import random
+
+def check():
+    return True
+
+def create():
+    print("CHART python_example.random '' 'A random number' 'random number' random random line 90000 1")
+    print("DIMENSION random1 '' absolute 1 1")
+    return True
+
+def update(interval):
+    print("BEGIN python_example.random "+str(interval))
+    print("SET random1 = "+str(random.randint(0,100)))
+    print("END")
+    return True
