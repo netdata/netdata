5605c196c55df738bdc53536ea42924a2e29b34b
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2016-06-16 21:06:41 +0300

    log error if cgroup mount points cannot be found

--- src/sys_fs_cgroup.c
+++ src/sys_fs_cgroup.c
@@ -48,21 +48,30 @@ void read_cgroup_plugin_configuration() {
 
 	mi = mountinfo_find_by_filesystem_mount_source(root, "cgroup", "cpuacct");
 	if(!mi) mi = mountinfo_find_by_filesystem_super_option(root, "cgroup", "cpuacct");
-	if(!mi) s = "/sys/fs/cgroup/cpuacct";
+	if(!mi) {
+		error("Cannot find cgroup cpuacct mountinfo. Assuming default: /sys/fs/cgroup/cpuacct");
+		s = "/sys/fs/cgroup/cpuacct";
+	}
 	else s = mi->mount_point;
 	snprintfz(filename, FILENAME_MAX, "%s%s", global_host_prefix, s);
 	cgroup_cpuacct_base = config_get("plugin:cgroups", "path to /sys/fs/cgroup/cpuacct", filename);
 
 	mi = mountinfo_find_by_filesystem_mount_source(root, "cgroup", "blkio");
 	if(!mi) mi = mountinfo_find_by_filesystem_super_option(root, "cgroup", "blkio");
-	if(!mi) s = "/sys/fs/cgroup/blkio";
+	if(!mi) {
+		error("Cannot find cgroup blkio mountinfo. Assuming default: /sys/fs/cgroup/blkio");
+		s = "/sys/fs/cgroup/blkio";
+	}
 	else s = mi->mount_point;
 	snprintfz(filename, FILENAME_MAX, "%s%s", global_host_prefix, s);
 	cgroup_blkio_base = config_get("plugin:cgroups", "path to /sys/fs/cgroup/blkio", filename);
 
 	mi = mountinfo_find_by_filesystem_mount_source(root, "cgroup", "memory");
 	if(!mi) mi = mountinfo_find_by_filesystem_super_option(root, "cgroup", "memory");
-	if(!mi) s = "/sys/fs/cgroup/memory";
+	if(!mi) {
+		error("Cannot find cgroup memory mountinfo. Assuming default: /sys/fs/cgroup/memory");
+		s = "/sys/fs/cgroup/memory";
+	}
 	else s = mi->mount_point;
 	snprintfz(filename, FILENAME_MAX, "%s%s", global_host_prefix, s);
 	cgroup_memory_base = config_get("plugin:cgroups", "path to /sys/fs/cgroup/memory", filename);
