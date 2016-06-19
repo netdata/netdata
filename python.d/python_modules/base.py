49b71e8140b89731bdbc2ca2bfe4886d667d2681
paulfantom <paulfantom@gmail.com>
2016-06-19 14:35:27 +0200

    Code cleanup

--- python.d/python_modules/base.py
+++ python.d/python_modules/base.py
@@ -6,47 +6,48 @@ import sys
 
 
 class BaseService(object):
-    def __init__(self,name=None,configuration=None):
+    def __init__(self, configuration=None):
         if configuration is None:
             self.error("BaseService: no configuration parameters supplied. Cannot create Service.")
             raise RuntimeError
         else:
             self._extract_base_config(configuration)
-            self._create_timetable()
+            self.timetable = {}
+            self.create_timetable()
             self.execution_name = ""
 
-    def _extract_base_config(self,config):
+    def _extract_base_config(self, config):
         self.update_every = int(config['update_every'])
         self.priority = int(config['priority'])
         self.retries = int(config['retries'])
         self.retries_left = self.retries
 
-    def _create_timetable(self,freq=None):
+    def create_timetable(self, freq=None):
         if freq is None:
             freq = self.update_every
         now = time()
-        self.timetable = {'last' : now,
-                          'next' : now - (now % freq) + freq,
-                          'freq' : freq}
+        self.timetable = {'last': now,
+                          'next': now - (now % freq) + freq,
+                          'freq': freq}
 
-
-    def error(self, msg, exception=""):
+    @staticmethod
+    def error(msg, exception=""):
         if exception != "":
             exception = " " + str(exception).replace("\n"," ")
         sys.stderr.write(str(msg)+exception+"\n")
         sys.stderr.flush()
 
     def check(self):
-        # TODO notify about not overriden function
+        # TODO notify about not overridden function
         self.error("Where is your check()?")
         return False
 
     def create(self):
-        # TODO notify about not overriden function
+        # TODO notify about not overridden function
         self.error("Where is your create()?")
         return False
 
     def update(self):
-        # TODO notify about not overriden function
+        # TODO notify about not overridden function
         self.error("Where is your update()?")
         return False
