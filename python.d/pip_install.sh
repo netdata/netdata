6504ccfe3cd8387a8812a62c86bcc48e37ac2654
paulfantom <paulfantom@gmail.com>
2016-06-20 13:50:24 +0200

    simple pip wrapper script

---
+++ python.d/pip_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PIP=`which pip`
+
+$PIP install --target="python_modules" yaml
+
+$PIP install --target="python_modules" MySQL-python || echo "You need to install libmysqlclient-dev and python-dev"
