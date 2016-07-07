f862e71a06e593fed7a3fcaffde7d5f0f5d50f36
paulfantom <paulfantom@gmail.com>
2016-07-07 12:23:15 +0200

    sort makefile + minor change in comments

--- python.d/mysql.chart.py
+++ python.d/mysql.chart.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Description: MySQL netdata python.d plugin
+# Description: MySQL netdata python.d module
 # Author: Pawel Krupa (paulfantom)
 
 from base import SimpleService
--- python.d/phpfpm.chart.py
+++ python.d/phpfpm.chart.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Description: PHP-FPM netdata python.d plugin
+# Description: PHP-FPM netdata python.d module
 # Author: Pawel Krupa (paulfantom)
 
 from base import UrlService
