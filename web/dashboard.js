d584533f12ef1c8c0420d9e4b975cf6597a80300
LucaDev <l.kroeger01@gmail.com>
2016-08-27 18:50:47 +0200

    added notifications on alarm

--- web/dashboard.js
+++ web/dashboard.js
@@ -5601,6 +5601,15 @@
         update_forever: function() {
             NETDATA.alarms.get('active', function(data) {
                 if(data !== null) {
+                    if("Notification" in window && NETDATA.alarms.current != null) {
+                        if(Object.keys(NETDATA.alarms.current.alarms).length < Object.keys(data.alarms).length) {
+                            if (Notification.permission === 'granted') {
+                                new Notification('Netdata Alarm!', {body: 'Your Server needs attention!',
+                                    icon: 'images/seo-performance-128.png'});
+                            }
+                        }
+                    }
+
                     NETDATA.alarms.current = data;
 
                     if (typeof NETDATA.alarms.callback === 'function') {
@@ -5633,6 +5642,9 @@
 
         init: function() {
             NETDATA.alarms.update_forever();
+            if ("Notification" in window && Notification.permission !== 'granted') {
+                Notification.requestPermission();
+            }
         }
     };
 
