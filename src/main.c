02c265ad473f989d56a0b19016a14c7768a83fb3
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2016-08-28 00:37:58 +0300

    update version number to 1.3.1_master

--- src/main.c
+++ src/main.c
@@ -378,7 +378,7 @@ int main(int argc, char **argv)
                     break;
                 case 'v':
                     // TODO: Outsource version to makefile which can compute version from git.
-                    printf("netdata 1.3.0\n");
+                    printf("netdata 1.3.1_master\n");
                     return 0;
                 case 'W':
                     {
