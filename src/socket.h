51d62cee057cba90a8a75c01a4692e073eaf5ad4
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2016-12-24 17:25:43 +0200

    re-worked connect_to() to properly support IPv6; #1432

---
+++ src/socket.h
@@ -0,0 +1,10 @@
+//
+// Created by costa on 24/12/2016.
+//
+
+#ifndef NETDATA_SOCKET_H
+#define NETDATA_SOCKET_H
+
+extern int connect_to(const char *definition, int default_port, struct timeval *timeout);
+
+#endif //NETDATA_SOCKET_H
