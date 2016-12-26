dc485c8642329efab93446bad9eaffb145c93c78
Vladimir Kobal <vlad@prokk.net>
2016-12-26 19:34:23 +0200

    Separate macOS plugin

---
+++ src/macos_sysctl.c
@@ -0,0 +1,74 @@
+#include "common.h"
+#include <sys/sysctl.h>
+
+#define GETSYSCTL(name, var) getsysctl(name, &(var), sizeof(var))
+
+// MacOS calculates load averages once every 5 seconds
+#define MIN_LOADAVG_UPDATE_EVERY 5
+
+int getsysctl(const char *name, void *ptr, size_t len);
+
+int do_macos_sysctl(int update_every, usec_t dt) {
+    (void)dt;
+
+    static int do_loadavg = -1;
+
+    if (unlikely(do_loadavg == -1)) {
+        do_loadavg              = config_get_boolean("plugin:macos:sysctl", "enable load average", 1);
+    }
+
+    RRDSET *st;
+
+    int system_pagesize = getpagesize(); // wouldn't it be better to get value directly from hw.pagesize?
+    int i, n;
+    int common_error = 0;
+    size_t size;
+
+    // NEEDED BY: do_loadavg
+    static usec_t last_loadavg_usec = 0;
+    struct loadavg sysload;
+
+    if (last_loadavg_usec <= dt) {
+        if (likely(do_loadavg)) {
+            if (unlikely(GETSYSCTL("vm.loadavg", sysload))) {
+                do_loadavg = 0;
+                error("DISABLED: system.load");
+            } else {
+
+                st = rrdset_find_bytype("system", "load");
+                if (unlikely(!st)) {
+                    st = rrdset_create("system", "load", NULL, "load", NULL, "System Load Average", "load", 100, (update_every < MIN_LOADAVG_UPDATE_EVERY) ? MIN_LOADAVG_UPDATE_EVERY : update_every, RRDSET_TYPE_LINE);
+                    rrddim_add(st, "load1", NULL, 1, 1000, RRDDIM_ABSOLUTE);
+                    rrddim_add(st, "load5", NULL, 1, 1000, RRDDIM_ABSOLUTE);
+                    rrddim_add(st, "load15", NULL, 1, 1000, RRDDIM_ABSOLUTE);
+                }
+                else rrdset_next(st);
+
+                rrddim_set(st, "load1", (collected_number) ((double)sysload.ldavg[0] / sysload.fscale * 1000));
+                rrddim_set(st, "load5", (collected_number) ((double)sysload.ldavg[1] / sysload.fscale * 1000));
+                rrddim_set(st, "load15", (collected_number) ((double)sysload.ldavg[2] / sysload.fscale * 1000));
+                rrdset_done(st);
+            }
+        }
+
+        last_loadavg_usec = st->update_every * USEC_PER_SEC;
+    }
+    else last_loadavg_usec -= dt;
+
+    return 0;
+}
+
+int getsysctl(const char *name, void *ptr, size_t len)
+{
+    size_t nlen = len;
+
+    if (unlikely(sysctlbyname(name, ptr, &nlen, NULL, 0) == -1)) {
+        error("MACOS: sysctl(%s...) failed: %s", name, strerror(errno));
+        return 1;
+    }
+    if (unlikely(nlen != len)) {
+        error("MACOS: sysctl(%s...) expected %lu, got %lu", name, (unsigned long)len, (unsigned long)nlen);
+        return 1;
+    }
+    return 0;
+}
---
+++ src/plugin_macos.c
@@ -0,0 +1,74 @@
+#include "common.h"
+
+void *macos_main(void *ptr)
+{
+    (void)ptr;
+
+    info("MACOS Plugin thread created with task id %d", gettid());
+
+    if(pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, NULL) != 0)
+        error("Cannot set pthread cancel type to DEFERRED.");
+
+    if(pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL) != 0)
+        error("Cannot set pthread cancel state to ENABLE.");
+
+    // disable (by default) various interface that are not needed
+    /*
+    config_get_boolean("plugin:proc:/proc/net/dev:lo", "enabled", 0);
+    config_get_boolean("plugin:proc:/proc/net/dev:fireqos_monitor", "enabled", 0);
+    */
+
+    // when ZERO, attempt to do it
+    int vdo_cpu_netdata             = !config_get_boolean("plugin:macos", "netdata server resources", 1);
+    int vdo_macos_sysctl            = !config_get_boolean("plugin:macos", "sysctl", 1);
+    int vdo_macos_mach_smi          = !config_get_boolean("plugin:macos", "mach system management interface", 1);
+
+    // keep track of the time each module was called
+    unsigned long long sutime_macos_sysctl = 0ULL;
+    unsigned long long sutime_macos_mach_smi = 0ULL;
+
+    usec_t step = rrd_update_every * USEC_PER_SEC;
+    for(;;) {
+        usec_t now = now_realtime_usec();
+        usec_t next = now - (now % step) + step;
+
+        while(now < next) {
+            sleep_usec(next - now);
+            now = now_realtime_usec();
+        }
+
+        if(unlikely(netdata_exit)) break;
+
+        // BEGIN -- the job to be done
+
+        if(!vdo_macos_sysctl) {
+            debug(D_PROCNETDEV_LOOP, "MACOS: calling do_macos_sysctl().");
+            now = now_realtime_usec();
+            vdo_macos_sysctl = do_macos_sysctl(rrd_update_every, (sutime_macos_sysctl > 0)?now - sutime_macos_sysctl:0ULL);
+            sutime_macos_sysctl = now;
+        }
+        if(unlikely(netdata_exit)) break;
+
+        if(!vdo_macos_mach_smi) {
+            debug(D_PROCNETDEV_LOOP, "MACOS: calling do_macos_mach_smi().");
+            now = now_realtime_usec();
+            vdo_macos_mach_smi = do_macos_mach_smi(rrd_update_every, (sutime_macos_mach_smi > 0)?now - sutime_macos_mach_smi:0ULL);
+            sutime_macos_mach_smi = now;
+        }
+        if(unlikely(netdata_exit)) break;
+
+        // END -- the job is done
+
+        // --------------------------------------------------------------------
+
+        if(!vdo_cpu_netdata) {
+            global_statistics_charts();
+            registry_statistics();
+        }
+    }
+
+    info("MACOS thread exiting");
+
+    pthread_exit(NULL);
+    return NULL;
+}
