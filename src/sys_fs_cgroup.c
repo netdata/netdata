09c03c39be4bdd2069571ec80dd43847d9f887d5
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2016-12-27 22:02:24 +0200

    add more debug info for cgroups default status

--- src/sys_fs_cgroup.c
+++ src/sys_fs_cgroup.c
@@ -673,7 +673,6 @@ struct cgroup *cgroup_add(const char *id) {
 
         // disable by default the root cgroup
         def = 0;
-        debug(D_CGROUP, "cgroup '%s' is the root container (by default %s)", id, (def)?"enabled":"disabled");
     }
     else {
         if(*chart_id == '/') chart_id++;
@@ -708,9 +707,9 @@ struct cgroup *cgroup_add(const char *id) {
                 (len > 7 && !strncmp(chart_id, "lxc/", 4) && !strncmp(&chart_id[len - 3], "/ns", 3)) // #1397
                 ) {
             def = 0;
-            debug(D_CGROUP, "cgroup '%s' is %s (by default)", id, (def)?"enabled":"disabled");
         }
     }
+    debug(D_CGROUP, "cgroup '%s' (chart_id '%s') is (by default) %s", id, chart_id, (def)?"enabled":"disabled");
 
     struct cgroup *cg = callocz(1, sizeof(struct cgroup));
 
