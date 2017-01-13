83153c78fe19679d707c87291b3e89f8674782fe
Simon Nagl <simonnagl@aim.com>
2017-01-13 11:03:12 +0100

    Document backends.h

--- src/backends.h
+++ src/backends.h
@@ -1,6 +1,17 @@
 #ifndef NETDATA_BACKENDS_H
 #define NETDATA_BACKENDS_H 1
 
+/**
+ * @file backends.h
+ * @brief This file holds the API to start the netdata backend thread.
+ */
+ 
+/** 
+ * Thread for netdata backends.
+ *
+ * @param ptr to struct netdata_static_thread
+ * @return NULL
+ */
 void *backends_main(void *ptr);
 
 #endif /* NETDATA_BACKENDS_H */
