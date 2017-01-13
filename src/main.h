edc455d96702166eced4cd7789e87f1798f56f6c
Simon Nagl <simonnagl@aim.com>
2017-01-13 13:34:03 +0100

    Document main.h

--- src/main.h
+++ src/main.h
@@ -1,6 +1,29 @@
 #ifndef NETDATA_MAIN_H
 #define NETDATA_MAIN_H 1
 
+/*! 
+ * \mainpage
+ *
+ * \tableofcontents
+ *
+ * \section sec_outline Outline
+ *
+ * \todo 
+ * - Tell the reader where he is here.
+ * - Give an overview about how the code is structured.
+ * 
+ * \section sec_rules Coding Conventions
+ *
+ * [doxygen_syntax]: http://www.stack.nl/~dimitri/doxygen/manual/docblocks.html "Doxygen Syntax"
+ * 
+ * - Header files must be documentented with [doxygen_syntax].
+ */
+
+ /**
+ * @file main.h
+ * @brief This file holds the API of global netdata elements.
+ */
+
 /**
  * This struct contains information about command line options.
  */
@@ -22,22 +45,50 @@ struct option_def {
  */
 extern struct option_def options[];
 
+/**
+ * Static threads of netdata.
+ */
 struct netdata_static_thread {
+    /**
+     * Name of the thrad.
+     *
+     * i.e. used for logging
+     */
     char *name;
 
-    char *config_section;
-    char *config_name;
+    char *config_section; ///< config section to enable thread
+    char *config_name;    ///< config name to enable thread
 
-    volatile int enabled;
+    volatile int enabled; ///< boolean
 
-    pthread_t *thread;
+    pthread_t *thread; ///<
 
-    void (*init_routine) (void);
-    void *(*start_routine) (void *);
+    void (*init_routine) (void);     ///< function called before thread starts
+    void *(*start_routine) (void *); ///< function the thread executes
 };
 
+/**
+ * Kill all child processes.
+ */
 extern void kill_childs(void);
+/**
+ * Send `signal` to process `pid`.
+ *
+ * Send the signal `signal` to process with pid `pid`.
+ * Signals are specified in signal.h.
+ * 
+ * @param signal 
+ * @param pid
+ * @return 0 on success.
+ */
 extern int killpid(pid_t pid, int signal);
+/**
+ * Stop netdata.
+ *
+ * Write database to disk. Signal running threads and processes to stop.
+ *
+ * @param ret Return code to exit with.
+ */
 extern void netdata_cleanup_and_exit(int ret) NORETURN;
 
 #endif /* NETDATA_MAIN_H */
