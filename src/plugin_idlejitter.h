c5819039c80a7f6735bfa8791ee1220b4e3589eb
Simon Nagl <sin6332@thi.de>
2017-01-21 10:07:44 +0100

    Document plugin_idlejitter.h

--- src/plugin_idlejitter.h
+++ src/plugin_idlejitter.h
@@ -1,6 +1,17 @@
 #ifndef NETDATA_PLUGIN_IDLEJITTER_H
 #define NETDATA_PLUGIN_IDLEJITTER_H 1
 
+/**
+ * @file plugin_idlejitter.h
+ * @brief Thread to collect IDLEJITTER data.
+ */
+
+/**
+ * Main method of IDLEJITTER thread.
+ *
+ * @param ptr to struct netdata_static_thread
+ * @return NULL
+ */
 extern void *cpuidlejitter_main(void *ptr);
 
 #endif /* NETDATA_PLUGIN_IDLEJITTER_H */
