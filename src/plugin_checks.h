0717d17ebfb5ae9bc304d64d249781e6e69b78b0
Simon Nagl <sin6332@thi.de>
2017-01-21 10:32:34 +0100

    Document plugin_checks.h

--- src/plugin_checks.h
+++ src/plugin_checks.h
@@ -1,6 +1,17 @@
 #ifndef NETDATA_PLUGIN_CHECKS_H
 #define NETDATA_PLUGIN_CHECKS_H 1
 
+/**
+ * @file plugin_checks.h
+ * @brief Thread CHECK with some example charts.
+ */
+
+/**
+ * Main method of checks thread.
+ *
+ * @param ptr to struct netdata_static_thread
+ * @return NULL
+ */
 void *checks_main(void *ptr);
 
 #endif /* NETDATA_PLUGIN_PROC_H */
