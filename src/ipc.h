204f89252103748f08d8b5da9e78c91089dcd152
Simon Nagl <sin6332@thi.de>
2017-01-21 15:59:06 +0100

    Document ipc.h

--- src/ipc.h
+++ src/ipc.h
@@ -1,6 +1,18 @@
 #ifndef NETDATA_PLUGIN_IPC_H
 #define NETDATA_PLUGIN_IPC_H 1
 
+/**
+ * @file ipc.h
+ * @brief Inter process communication statistics.
+ */
+
+/**
+ * Data collector of ipc statistics
+ *
+ * @param update_every intervall in seconds this is called.
+ * @param dt microseconds passed since the last call.
+ * @return 0 on success. 1 on error.
+ */
 extern int do_ipc(int update_every, usec_t dt);
 
 #endif /* NETDATA_PLUGIN_IPC_H */
