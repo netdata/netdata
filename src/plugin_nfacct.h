e42832b804c3dd374d58647cb46867115cf732d9
Simon Nagl <sin6332@thi.de>
2017-01-21 10:01:09 +0100

    Document plugin_nfacct.h

--- src/plugin_nfacct.h
+++ src/plugin_nfacct.h
@@ -1,6 +1,19 @@
 #ifndef NETDATA_NFACCT_H
 #define NETDATA_NFACCT_H 1
 
+/**
+ * @file plugin_nfacct.h
+ * @brief Thread to collect nfacct data.
+ */
+
+/**
+ * Main method of the nfacct thread.
+ *
+ * This thread collects nfacct data.
+ *
+ * @param ptr to struct netdata_static_thread
+ * @return NULL
+ */
 extern void *nfacct_main(void *ptr);
 
 #endif /* NETDATA_NFACCT_H */
