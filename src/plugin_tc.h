a0a851d619f2728b89b165e6771a6b8905e98d12
Simon Nagl <sin6332@thi.de>
2017-01-21 10:05:55 +0100

    Document plugin_tc.h

--- src/plugin_tc.h
+++ src/plugin_tc.h
@@ -1,7 +1,19 @@
 #ifndef NETDATA_PLUGIN_TC_H
 #define NETDATA_PLUGIN_TC_H 1
 
+/**
+ * @file plugin_tc.h
+ * @brief Thread to collect network tc classes data.
+ */
+
+/// PID of tc child
 extern volatile pid_t tc_child_pid;
+/**
+ * Main method of TC thread.
+ *
+ * @param ptr to struct netdata_static_thread
+ * @return NULL
+ */
 extern void *tc_main(void *ptr);
 
 #endif /* NETDATA_PLUGIN_TC_H */
