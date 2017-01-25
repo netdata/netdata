261a94ecd2676b886883d3b5532f84c5f5129072
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2017-01-25 02:06:53 +0200

    enable cgroup renames for docker under systemd; fixes #1636

--- src/sys_fs_cgroup.c
+++ src/sys_fs_cgroup.c
@@ -181,6 +181,7 @@ void read_cgroup_plugin_configuration() {
 
     enabled_cgroup_renames = simple_pattern_create(
             config_get("plugin:cgroups", "run script to rename cgroups matching",
+                    " /system.slice/docker-*.scope "
                     " !/ "
                     " !*.mount "
                     " !*.partition "
