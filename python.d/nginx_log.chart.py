2c68e455c1e553e06f67e7fdcfbf74b8d230fd2a
Ilya <ilyamaschenko@gmail.com>
2017-02-04 14:57:47 +0900

    nginx_log plugin: code optimization, bandwidth and unique visitors charts added

--- python.d/nginx_log.chart.py
+++ python.d/nginx_log.chart.py
@@ -1,25 +1,34 @@
 # -*- coding: utf-8 -*-
 # Description: nginx log netdata python.d module
-# Author: Pawel Krupa (paulfantom)
+# Author: Pawel Krupa (paulfantom), l2isbad
 
 from base import LogService
 import re
+import bisect
 
 priority = 60000
 retries = 60
-# update_every = 3
 
-ORDER = ['codes']
+ORDER = ['codes', 'bandwidth', 'unique']
 CHARTS = {
     'codes': {
-        'options': [None, 'nginx status codes', 'requests/s', 'requests', 'nginx_log.codes', 'stacked'],
+        'options': [None, 'Status codes', 'requests/s', 'requests', 'nginx_log.codes', 'stacked'],
         'lines': [
-            ["2xx", None, "incremental"],
-            ["5xx", None, "incremental"],
-            ["3xx", None, "incremental"],
-            ["4xx", None, "incremental"],
-            ["1xx", None, "incremental"],
-            ["other", None, "incremental"]
+            ['2', '2xx', 'incremental'],
+            ['5', '5xx', 'incremental'],
+            ['3', '3xx', 'incremental'],
+            ['4', '4xx', 'incremental'],
+            ['1', '1xx', 'incremental']
+        ]},
+    'bandwidth': {
+        'options': [None, 'Bandwidth', 'KB/s', 'bandwidth', 'nginx_log.bw', 'area'],
+        'lines': [
+            ['bandwidth', 'sent', 'incremental', 1, 1024]
+        ]},
+    'unique': {
+        'options': [None, 'Unique visitors', 'number', 'unique visitors', 'nginx_log.uv', 'line'],
+        'lines': [
+            ['unique', 'visitors', 'absolute', 1, 1]
         ]}
 }
 
@@ -27,56 +36,39 @@ CHARTS = {
 class Service(LogService):
     def __init__(self, configuration=None, name=None):
         LogService.__init__(self, configuration=configuration, name=name)
-        if len(self.log_path) == 0:
-            self.log_path = "/var/log/nginx/access.log"
         self.order = ORDER
         self.definitions = CHARTS
-        pattern = r'" ([0-9]{3}) ?'
-        #pattern = r'(?:" )([0-9][0-9][0-9]) ?'
-        self.regex = re.compile(pattern)
-
-        self.data = {
-            '1xx': 0,
-            '2xx': 0,
-            '3xx': 0,
-            '4xx': 0,
-            '5xx': 0,
-            'other': 0
-        }
+        self.regex = re.compile(r'(\d{1,3}(?:\.\d{1,3}){3}).*?((?<=\s)[1-5])\d\d (\d+)?')
+        self.data = {str(k): 0 for k in range(1, 6)}
+        self.data.update({'bandwidth': 0, 'unique': 0})
+        self.unique = list()
 
     def _get_data(self):
-        """
+        '''
         Parse new log lines
         :return: dict
-        """
-        try:
-            raw = self._get_raw_data()
-            if raw is None:
-                return None
-            elif not raw:
-                return self.data
-        except (ValueError, AttributeError):
-            return None
+        '''
+        raw = self._get_raw_data()
+        if raw is None: return None
 
-        regex = self.regex
         for line in raw:
-            code = regex.search(line)
-            try:
-                beginning = code.group(1)[0]
-            except AttributeError:
-                continue
-
-            if beginning == '2':
-                self.data["2xx"] += 1
-            elif beginning == '3':
-                self.data["3xx"] += 1
-            elif beginning == '4':
-                self.data["4xx"] += 1
-            elif beginning == '5':
-                self.data["5xx"] += 1
-            elif beginning == '1':
-                self.data["1xx"] += 1
-            else:
-                self.data["other"] += 1
-
+            match = self.regex.findall(line)
+            if match:
+                address, code, sent = match[0][0], match[0][1], match[0][2] or 0
+                self.data[code] += 1
+                self.data['bandwidth'] += int(sent)
+                if self.contains(address):
+                    self.data['unique'] += 1
         return self.data
+
+    def contains(self, address):
+        index = bisect.bisect_left(self.unique, address)
+        if index < len(self.unique):
+            if self.unique[index] == address:
+                return False
+            else:
+                bisect.insort_left(self.unique, address)
+                return True
+        else:
+            bisect.insort_left(self.unique, address)
+            return True
