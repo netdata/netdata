7849da02ebaa158a71781d0acaa644d1b4339321
Ilya <ilyamaschenko@gmail.com>
2017-02-04 18:57:38 +0900

    nginx_log plugin: charts title renamed, "other" dim for non standart resp codes added

--- python.d/nginx_log.chart.py
+++ python.d/nginx_log.chart.py
@@ -9,27 +9,28 @@ import bisect
 priority = 60000
 retries = 60
 
-ORDER = ['codes', 'bandwidth', 'visitors']
+ORDER = ['codes', 'bandwidth', 'clients']
 CHARTS = {
     'codes': {
-        'options': [None, 'Status codes', 'requests/s', 'requests', 'nginx_log.codes', 'stacked'],
+        'options': [None, 'Response Codes', 'requests/s', 'Responses', 'nginx_log.codes', 'stacked'],
         'lines': [
             ['2', '2xx', 'incremental'],
             ['5', '5xx', 'incremental'],
             ['3', '3xx', 'incremental'],
             ['4', '4xx', 'incremental'],
-            ['1', '1xx', 'incremental']
+            ['1', '1xx', 'incremental'],
+            ['0', 'other', 'incremental']
         ]},
     'bandwidth': {
-        'options': [None, 'Bandwidth', 'KB/s', 'bandwidth', 'nginx_log.bw', 'area'],
+        'options': [None, 'Bandwidth', 'KB/s', 'Bandwidth', 'nginx_log.bw', 'area'],
         'lines': [
             ['bandwidth', 'sent', 'incremental', 1, 1024]
         ]},
-    'visitors': {
-        'options': [None, 'Unique visitors', 'number', 'visitors', 'nginx_log.visitors', 'stacked'],
+    'clients': {
+        'options': [None, 'Unique Client IPs', 'number', 'Client IPs', 'nginx_log.clients', 'stacked'],
         'lines': [
-            ['unique_cur', 'current', 'incremental', 1, 1],
-            ['unique_tot', 'total', 'absolute', 1, 1]
+            ['unique_cur', 'current_poll', 'incremental', 1, 1],
+            ['unique_tot', 'all_time', 'absolute', 1, 1]
         ]}
 }
 
@@ -40,7 +41,7 @@ class Service(LogService):
         self.order = ORDER
         self.definitions = CHARTS
         self.regex = re.compile(r'(\d{1,3}(?:\.\d{1,3}){3}).*?((?<=\s)[1-5])\d\d (\d+)?')
-        self.data = {str(k): 0 for k in range(1, 6)}
+        self.data = {str(k): 0 for k in range(6)}
         self.data.update({'bandwidth': 0, 'unique_cur': 0, 'unique_tot': 0})
         self.unique = list()
 
@@ -56,7 +57,10 @@ class Service(LogService):
             match = self.regex.findall(line)
             if match:
                 address, code, sent = match[0][0], match[0][1], match[0][2] or 0
-                self.data[code] += 1
+                try:
+                    self.data[code] += 1
+                except KeyError:
+                    self.data['0'] += 1
                 self.data['bandwidth'] += int(sent)
                 if self.contains(address):
                     self.data['unique_cur'] += 1
