af1f064afbb7c5fc3b50b34caefb1823f2f4618c
Vladimir Kobal <vlad@prokk.net>
2017-03-09 18:29:33 +0200

    Separate kern.ipc.sem module

--- src/plugin_freebsd.c
+++ src/plugin_freebsd.c
@@ -27,6 +27,7 @@ static struct freebsd_module {
         { .name = "vm.swap_info", .dim = "swap", .enabled = 1, .func = do_vm_swap_info },
         { .name = "system.ram", .dim = "system_ram", .enabled = 1, .func = do_system_ram },
         { .name = "vm.stats.vm.v_swappgs", .dim = "swap_io", .enabled = 1, .func = do_vm_stats_sys_v_swappgs },
+        { .name = "kern.ipc.sem", .dim = "semaphores", .enabled = 1, .func = do_kern_ipc_sem },
 
         // CPU metrics
         { .name = "kern.cp_times", .dim = "cp_times", .enabled = 1, .func = do_kern_cp_times },
