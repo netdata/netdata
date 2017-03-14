de8ce5a5c2efca032c02374391f48e0a9a7fe8d0
Vladimir Kobal <vlad@prokk.net>
2017-03-14 18:51:11 +0200

    Separate net.inet6.icmp6.stats module

--- src/plugin_freebsd.h
+++ src/plugin_freebsd.h
@@ -31,6 +31,7 @@ extern int do_net_inet_udp_stats(int update_every, usec_t dt);
 extern int do_net_inet_icmp_stats(int update_every, usec_t dt);
 extern int do_net_inet_ip_stats(int update_every, usec_t dt);
 extern int do_net_inet6_ip6_stats(int update_every, usec_t dt);
+extern int do_net_inet6_icmp6_stats(int update_every, usec_t dt);
 
 extern int do_freebsd_sysctl_old(int update_every, usec_t dt);
 
