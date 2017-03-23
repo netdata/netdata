df784630704b481901c5f476cdbbde4c5e2e4a69
Ilya <ilyamaschenko@gmail.com>
2017-03-23 15:24:47 +0900

    haproxy_plugin: get data via UrlService bugfix

--- python.d/haproxy.chart.py
+++ python.d/haproxy.chart.py
@@ -13,43 +13,44 @@ retries = 60
 ORDER = ['fbin', 'fbout', 'fscur', 'fqcur', 'bbin', 'bbout', 'bscur', 'bqcur', 'health_sdown', 'health_bdown']
 CHARTS = {
     'fbin': {
-        'options': [None, "Kilobytes in", "kilobytes in/s", 'Frontend', 'haproxy_f.bin', 'line'],
+        'options': [None, "Kilobytes In", "KB/s", 'frontend', 'haproxy_f.bin', 'line'],
         'lines': [
         ]},
     'fbout': {
-        'options': [None, "Kilobytes out", "kilobytes out/s", 'Frontend', 'haproxy_f.bout', 'line'],
+        'options': [None, "Kilobytes Out", "KB/s", 'frontend', 'haproxy_f.bout', 'line'],
         'lines': [
         ]},
     'fscur': {
-        'options': [None, "Sessions active", "sessions", 'Frontend', 'haproxy_f.scur', 'line'],
+        'options': [None, "Sessions Active", "sessions", 'frontend', 'haproxy_f.scur', 'line'],
         'lines': [
         ]},
     'fqcur': {
-        'options': [None, "Session in queue", "sessions", 'Frontend', 'haproxy_f.qcur', 'line'],
+        'options': [None, "Session In Queue", "sessions", 'frontend', 'haproxy_f.qcur', 'line'],
         'lines': [
         ]},
     'bbin': {
-        'options': [None, "Kilobytes in", "kilobytes in/s", 'Backend', 'haproxy_b.bin', 'line'],
+        'options': [None, "Kilobytes In", "KB/s", 'backend', 'haproxy_b.bin', 'line'],
         'lines': [
         ]},
     'bbout': {
-        'options': [None, "Kilobytes out", "kilobytes out/s", 'Backend', 'haproxy_b.bout', 'line'],
+        'options': [None, "Kilobytes Out", "KB/s", 'backend', 'haproxy_b.bout', 'line'],
         'lines': [
         ]},
     'bscur': {
-        'options': [None, "Sessions active", "sessions", 'Backend', 'haproxy_b.scur', 'line'],
+        'options': [None, "Sessions Active", "sessions", 'backend', 'haproxy_b.scur', 'line'],
         'lines': [
         ]},
     'bqcur': {
-        'options': [None, "Sessions in queue", "sessions", 'Backend', 'haproxy_b.qcur', 'line'],
+        'options': [None, "Sessions In Queue", "sessions", 'backend', 'haproxy_b.qcur', 'line'],
         'lines': [
         ]},
     'health_sdown': {
-        'options': [None, "Number of servers in backend in DOWN state", "failed servers", 'Health', 'haproxy_hs.down', 'line'],
+        'options': [None, "Backend Servers In DOWN State", "failed servers", 'health',
+                    'haproxy_hs.down', 'line'],
         'lines': [
         ]},
     'health_bdown': {
-        'options': [None, "Is backend alive? 1 = DOWN", "failed backend", 'Health', 'haproxy_hb.down', 'line'],
+        'options': [None, "Is backend alive? 1 = DOWN", "failed backend", 'health', 'haproxy_hb.down', 'line'],
         'lines': [
         ]}
 }
@@ -57,61 +58,68 @@ CHARTS = {
 
 class Service(UrlService, SocketService):
     def __init__(self, configuration=None, name=None):
-        SocketService.__init__(self, configuration=configuration, name=name)
-        self.user = self.configuration.get('user')
-        self.password = self.configuration.get('pass')
-        self.request = 'show stat\n'
-        self.poll_method = (UrlService, SocketService)
+        if 'socket' in configuration:
+            SocketService.__init__(self, configuration=configuration, name=name)
+            self.poll_method = SocketService
+            self.request = 'show stat\n'
+        else:
+            UrlService.__init__(self, configuration=configuration, name=name)
+            self.poll_method = UrlService
         self.order = ORDER
+        self.definitions = CHARTS
         self.order_front = [_ for _ in ORDER if _.startswith('f')]
         self.order_back = [_ for _ in ORDER if _.startswith('b')]
-        self.definitions = CHARTS
         self.charts = True
 
     def check(self):
-        if self.configuration.get('url'):
-            self.poll_method = self.poll_method[0]
-            url = self.configuration.get('url')
-            if not url.endswith(';csv;norefresh'):
-                self.error('Bad url(%s). Must be http://<ip.address>:<port>/<url>;csv;norefresh' % url)
-                return False
-        elif self.configuration.get('socket'):
-            self.poll_method = self.poll_method[1]
-        else:
-            self.error('No configuration is specified')
-            return False
 
         if self.poll_method.check(self):
-            self.info('Plugin was started succesfully. We are using %s.' % self.poll_method.__name__)
+            self.info('Plugin was started successfully. We are using %s.' % self.poll_method.__name__)
             return True
+        else:
+            return False
 
     def create_charts(self, front_ends, back_ends):
-        for _ in range(len(front_ends)):
-            self.definitions['fbin']['lines'].append(['_'.join(['fbin', front_ends[_]['# pxname']]), front_ends[_]['# pxname'], 'incremental', 1, 1024])
-            self.definitions['fbout']['lines'].append(['_'.join(['fbout', front_ends[_]['# pxname']]), front_ends[_]['# pxname'], 'incremental', 1, 1024])
-            self.definitions['fscur']['lines'].append(['_'.join(['fscur', front_ends[_]['# pxname']]), front_ends[_]['# pxname'], 'absolute'])
-            self.definitions['fqcur']['lines'].append(['_'.join(['fqcur', front_ends[_]['# pxname']]), front_ends[_]['# pxname'], 'absolute'])
-        
-        for _ in range(len(back_ends)):
-            self.definitions['bbin']['lines'].append(['_'.join(['bbin', back_ends[_]['# pxname']]), back_ends[_]['# pxname'], 'incremental', 1, 1024])
-            self.definitions['bbout']['lines'].append(['_'.join(['bbout', back_ends[_]['# pxname']]), back_ends[_]['# pxname'], 'incremental', 1, 1024])
-            self.definitions['bscur']['lines'].append(['_'.join(['bscur', back_ends[_]['# pxname']]), back_ends[_]['# pxname'], 'absolute'])
-            self.definitions['bqcur']['lines'].append(['_'.join(['bqcur', back_ends[_]['# pxname']]), back_ends[_]['# pxname'], 'absolute'])
-            self.definitions['health_sdown']['lines'].append(['_'.join(['hsdown', back_ends[_]['# pxname']]), back_ends[_]['# pxname'], 'absolute'])
-            self.definitions['health_bdown']['lines'].append(['_'.join(['hbdown', back_ends[_]['# pxname']]), back_ends[_]['# pxname'], 'absolute'])
-                
+        for _ in enumerate(front_ends):
+            idx = _[0]
+            self.definitions['fbin']['lines'].append(['_'.join(['fbin', front_ends[idx]['# pxname']]),
+                                                      front_ends[idx]['# pxname'], 'incremental', 1, 1024])
+            self.definitions['fbout']['lines'].append(['_'.join(['fbout', front_ends[idx]['# pxname']]),
+                                                       front_ends[idx]['# pxname'], 'incremental', 1, 1024])
+            self.definitions['fscur']['lines'].append(['_'.join(['fscur', front_ends[idx]['# pxname']]),
+                                                       front_ends[idx]['# pxname'], 'absolute'])
+            self.definitions['fqcur']['lines'].append(['_'.join(['fqcur', front_ends[idx]['# pxname']]),
+                                                       front_ends[idx]['# pxname'], 'absolute'])
+
+        for _ in enumerate(back_ends):
+            idx = _[0]
+            self.definitions['bbin']['lines'].append(['_'.join(['bbin', back_ends[idx]['# pxname']]),
+                                                      back_ends[idx]['# pxname'], 'incremental', 1, 1024])
+            self.definitions['bbout']['lines'].append(['_'.join(['bbout', back_ends[idx]['# pxname']]),
+                                                       back_ends[idx]['# pxname'], 'incremental', 1, 1024])
+            self.definitions['bscur']['lines'].append(['_'.join(['bscur', back_ends[idx]['# pxname']]),
+                                                       back_ends[idx]['# pxname'], 'absolute'])
+            self.definitions['bqcur']['lines'].append(['_'.join(['bqcur', back_ends[idx]['# pxname']]),
+                                                       back_ends[idx]['# pxname'], 'absolute'])
+            self.definitions['health_sdown']['lines'].append(['_'.join(['hsdown', back_ends[idx]['# pxname']]),
+                                                              back_ends[idx]['# pxname'], 'absolute'])
+            self.definitions['health_bdown']['lines'].append(['_'.join(['hbdown', back_ends[idx]['# pxname']]),
+                                                              back_ends[idx]['# pxname'], 'absolute'])
+
     def _get_data(self):
         """
         Format data received from http request
         :return: dict
         """
-        try:
-            raw_data = self.poll_method._get_raw_data(self).splitlines()
-        except Exception as e:
-            self.error(str(e))
-            return None
+        raw_data = self.poll_method._get_raw_data(self)
 
-        all_instances = [dict(zip(raw_data[0].split(','), raw_data[_].split(','))) for _ in range(1, len(raw_data))]
+        if not raw_data:
+            return None
+        else:
+            raw_data = raw_data.splitlines()
+
+        all_instances = [dict(zip(raw_data[0].split(','),
+                                  raw_data[_].split(','))) for _ in range(1, len(raw_data))]
 
         back_ends = list(filter(is_backend, all_instances))
         front_ends = list(filter(is_frontend, all_instances))
@@ -124,21 +132,26 @@ class Service(UrlService, SocketService):
         to_netdata = dict()
 
         for frontend in front_ends:
-            for _ in self.order_front:
-                to_netdata.update({'_'.join([_, frontend['# pxname']]): int(frontend[_[1:]]) if frontend.get(_[1:]) else 0})
+            for idx in self.order_front:
+                to_netdata.update({'_'.join([idx, frontend['# pxname']]):
+                                   int(frontend[idx[1:]]) if frontend.get(idx[1:]) else 0})
 
         for backend in back_ends:
-            for _ in self.order_back:
-                to_netdata.update({'_'.join([_, backend['# pxname']]): int(backend[_[1:]]) if backend.get(_[1:]) else 0})
+            for idx in self.order_back:
+                to_netdata.update({'_'.join([idx, backend['# pxname']]):
+                                   int(backend[idx[1:]]) if backend.get(idx[1:]) else 0})
 
-        for _ in range(len(back_ends)):
-            to_netdata.update({'_'.join(['hsdown', back_ends[_]['# pxname']]):
-                           len([server for server in servers if is_server_down(server, back_ends, _)])})
-            to_netdata.update({'_'.join(['hbdown', back_ends[_]['# pxname']]): 1 if is_backend_down(back_ends, _) else 0})
+        for _ in enumerate(back_ends):
+            idx = _[0]
+            to_netdata.update({'_'.join(['hsdown', back_ends[idx]['# pxname']]):
+                               len([server for server in servers if is_server_down(server, back_ends, idx)])})
+            to_netdata.update({'_'.join(['hbdown', back_ends[idx]['# pxname']]):
+                               1 if is_backend_down(back_ends, idx) else 0})
 
         return to_netdata
 
-    def _check_raw_data(self, data):
+    @staticmethod
+    def _check_raw_data(data):
         """
         Check if all data has been gathered from socket
         :param data: str
@@ -146,32 +159,22 @@ class Service(UrlService, SocketService):
         """
         return not bool(data)
 
+
 def is_backend(backend):
-    try:
-        return backend['svname'] == 'BACKEND' and backend['# pxname'] != 'stats'
-    except Exception:
-        return False
+        return backend.get('svname') == 'BACKEND' and backend.get('# pxname') != 'stats'
+
 
 def is_frontend(frontend):
-    try:
-        return frontend['svname'] == 'FRONTEND' and frontend['# pxname'] != 'stats'
-    except Exception:
-        return False
+        return frontend.get('svname') == 'FRONTEND' and frontend.get('# pxname') != 'stats'
+
 
 def is_server(server):
-    try:
-        return not server['svname'].startswith(('FRONTEND', 'BACKEND'))
-    except Exception:
-        return False
+        return not server.get('svname', '').startswith(('FRONTEND', 'BACKEND'))
 
-def is_server_down(server, back_ends, _):
-    try:
-        return server['# pxname'] == back_ends[_]['# pxname'] and server['status'] == 'DOWN'
-    except Exception:
-        return False
 
-def is_backend_down(back_ends, _):
-    try:
-        return back_ends[_]['status'] == 'DOWN'
-    except Exception:
-        return False
+def is_server_down(server, back_ends, idx):
+    return server.get('# pxname') == back_ends[idx].get('# pxname') and server.get('status') == 'DOWN'
+
+
+def is_backend_down(back_ends, idx):
+    return back_ends[idx].get('status') == 'DOWN'
