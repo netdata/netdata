547fd9f2b85a2591322cd93d641dd3b2948a4a1f
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2017-03-27 01:15:56 +0300

    convert relative paths to absolute

--- makeself/run-all-jobs.sh
+++ makeself/run-all-jobs.sh
@@ -6,17 +6,31 @@ umask 002
 # be nice
 renice 19 $$ >/dev/null 2>/dev/null
 
+# -----------------------------------------------------------------------------
 # prepare the environment for the jobs
+
+# installation directory
 export NETDATA_INSTALL_PATH="${1-/opt/netdata}"
+
+# our source directory
 export NETDATA_MAKESELF_PATH="$(dirname "${0}")"
+if [ "${NETDATA_MAKESELF_PATH:0:1}" != "/" ]
+	then
+	export NETDATA_MAKESELF_PATH="$(pwd)/${NETDATA_MAKESELF_PATH}"
+fi
+
+# netdata source directory
 export NETDATA_SOURCE_PATH="${NETDATA_MAKESELF_PATH}/.."
 
+# number of processors this system has
 PROCESSORS=$(cat /proc/cpuinfo 2>/dev/null | grep ^processor | wc -l)
 [ -z "${PROCESSORS}" -o $(( PROCESSORS )) -lt 1 ] && PROCESSORS=1
 export PROCESSORS
 
 # make sure ${NULL} is empty
 export NULL=
+
+# -----------------------------------------------------------------------------
 
 cd "${NETDATA_MAKESELF_PATH}" || exit 1
 
