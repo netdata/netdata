4add904179400312845a6d15186fb00e951a0fcc
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2017-04-23 04:18:04 +0300

    moved all socket related code to socket.c/h

--- src/socket.c
+++ src/socket.c
@@ -1,5 +1,328 @@
 #include "common.h"
 
+// --------------------------------------------------------------------------------------------------------------------
+// listening sockets
+
+int create_listen_socket4(int socktype, const char *ip, int port, int listen_backlog) {
+    int sock;
+    int sockopt = 1;
+
+    debug(D_LISTENER, "IPv4 creating new listening socket on ip '%s' port %d", ip, port);
+
+    sock = socket(AF_INET, socktype, 0);
+    if(sock < 0) {
+        error("IPv4 socket() on ip '%s' port %d failed.", ip, port);
+        return -1;
+    }
+
+    /* avoid "address already in use" */
+    if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (void*)&sockopt, sizeof(sockopt)) != 0)
+        error("Cannot set SO_REUSEADDR on ip '%s' port's %d.", ip, port);
+
+    struct sockaddr_in name;
+    memset(&name, 0, sizeof(struct sockaddr_in));
+    name.sin_family = AF_INET;
+    name.sin_port = htons (port);
+
+    int ret = inet_pton(AF_INET, ip, (void *)&name.sin_addr.s_addr);
+    if(ret != 1) {
+        error("Failed to convert IP '%s' to a valid IPv4 address.", ip);
+        close(sock);
+        return -1;
+    }
+
+    if(bind (sock, (struct sockaddr *) &name, sizeof (name)) < 0) {
+        close(sock);
+        error("IPv4 bind() on ip '%s' port %d failed.", ip, port);
+        return -1;
+    }
+
+    if(listen(sock, listen_backlog) < 0) {
+        close(sock);
+        error("IPv4 listen() on ip '%s' port %d failed.", ip, port);
+        return -1;
+    }
+
+    debug(D_LISTENER, "Listening on IPv4 ip '%s' port %d", ip, port);
+    return sock;
+}
+
+int create_listen_socket6(int socktype, uint32_t scope_id, const char *ip, int port, int listen_backlog) {
+    int sock = -1;
+    int sockopt = 1;
+    int ipv6only = 1;
+
+    debug(D_LISTENER, "IPv6 creating new listening socket on ip '%s' port %d", ip, port);
+
+    sock = socket(AF_INET6, socktype, 0);
+    if (sock < 0) {
+        error("IPv6 socket() on ip '%s' port %d failed.", ip, port);
+        return -1;
+    }
+
+    /* avoid "address already in use" */
+    if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (void*)&sockopt, sizeof(sockopt)) != 0)
+        error("Cannot set SO_REUSEADDR on ip '%s' port's %d.", ip, port);
+
+    /* IPv6 only */
+    if(setsockopt(sock, IPPROTO_IPV6, IPV6_V6ONLY, (void*)&ipv6only, sizeof(ipv6only)) != 0)
+        error("Cannot set IPV6_V6ONLY on ip '%s' port's %d.", ip, port);
+
+    struct sockaddr_in6 name;
+    memset(&name, 0, sizeof(struct sockaddr_in6));
+    name.sin6_family = AF_INET6;
+    name.sin6_port = htons ((uint16_t) port);
+    name.sin6_scope_id = scope_id;
+
+    int ret = inet_pton(AF_INET6, ip, (void *)&name.sin6_addr.s6_addr);
+    if(ret != 1) {
+        error("Failed to convert IP '%s' to a valid IPv6 address.", ip);
+        close(sock);
+        return -1;
+    }
+
+    name.sin6_scope_id = scope_id;
+
+    if (bind (sock, (struct sockaddr *) &name, sizeof (name)) < 0) {
+        close(sock);
+        error("IPv6 bind() on ip '%s' port %d failed.", ip, port);
+        return -1;
+    }
+
+    if (listen(sock, listen_backlog) < 0) {
+        close(sock);
+        error("IPv6 listen() on ip '%s' port %d failed.", ip, port);
+        return -1;
+    }
+
+    debug(D_LISTENER, "Listening on IPv6 ip '%s' port %d", ip, port);
+    return sock;
+}
+
+static inline int listen_sockets_add(LISTEN_SOCKETS *sockets, int fd, const char *protocol, const char *ip, int port) {
+    if(sockets->opened >= MAX_LISTEN_FDS) {
+        error("Too many listening sockets. Failed to add listening %s socket at ip '%s' port %d", protocol, ip, port);
+        close(fd);
+        return -1;
+    }
+
+    sockets->fds[sockets->opened] = fd;
+
+    char buffer[100 + 1];
+    snprintfz(buffer, 100, "%s:[%s]:%d", protocol, ip, port);
+    sockets->fds_names[sockets->opened] = strdupz(buffer);
+
+    sockets->opened++;
+    return 0;
+}
+
+int listen_sockets_check_is_member(LISTEN_SOCKETS *sockets, int fd) {
+    size_t i;
+    for(i = 0; i < sockets->opened ;i++)
+        if(sockets->fds[i] == fd) return 1;
+
+    return 0;
+}
+
+static inline void listen_sockets_init(LISTEN_SOCKETS *sockets) {
+    size_t i;
+    for(i = 0; i < MAX_LISTEN_FDS ;i++) {
+        sockets->fds[i] = -1;
+        sockets->fds_names[i] = NULL;
+    }
+
+    sockets->opened = 0;
+    sockets->failed = 0;
+}
+
+void listen_sockets_close(LISTEN_SOCKETS *sockets) {
+    size_t i;
+    for(i = 0; i < sockets->opened ;i++) {
+        close(sockets->fds[i]);
+        sockets->fds[i] = -1;
+
+        freez(sockets->fds_names[i]);
+        sockets->fds_names[i] = NULL;
+    }
+
+    sockets->opened = 0;
+    sockets->failed = 0;
+}
+
+static inline int bind_to_one(LISTEN_SOCKETS *sockets, const char *definition, int default_port, int listen_backlog) {
+    int added = 0;
+    struct addrinfo hints;
+    struct addrinfo *result = NULL, *rp = NULL;
+
+    char buffer[strlen(definition) + 1];
+    strcpy(buffer, definition);
+
+    char buffer2[10 + 1];
+    snprintfz(buffer2, 10, "%d", default_port);
+
+    char *ip = buffer, *port = buffer2, *interface = "";;
+
+    int protocol = IPPROTO_TCP, socktype = SOCK_STREAM;
+    const char *protocol_str = "tcp";
+
+    if(strncmp(ip, "tcp:", 4) == 0) {
+        ip += 4;
+        protocol = IPPROTO_TCP;
+        socktype = SOCK_STREAM;
+        protocol_str = "tcp";
+    }
+    else if(strncmp(ip, "udp:", 4) == 0) {
+        ip += 4;
+        protocol = IPPROTO_UDP;
+        socktype = SOCK_DGRAM;
+        protocol_str = "udp";
+    }
+
+    char *e = ip;
+    if(*e == '[') {
+        e = ++ip;
+        while(*e && *e != ']') e++;
+        if(*e == ']') {
+            *e = '\0';
+            e++;
+        }
+    }
+    else {
+        while(*e && *e != ':' && *e != '%') e++;
+    }
+
+    if(*e == '%') {
+        *e = '\0';
+        e++;
+        interface = e;
+        while(*e && *e != ':') e++;
+    }
+
+    if(*e == ':') {
+        port = e + 1;
+        *e = '\0';
+    }
+
+    uint32_t scope_id = 0;
+    if(*interface) {
+        scope_id = if_nametoindex(interface);
+        if(!scope_id)
+            error("Cannot find a network interface named '%s'. Continuing with limiting the network interface", interface);
+    }
+
+    if(!*ip || *ip == '*' || !strcmp(ip, "any") || !strcmp(ip, "all"))
+        ip = NULL;
+
+    if(!*port)
+        port = buffer2;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;  /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = socktype;
+    hints.ai_flags = AI_PASSIVE;  /* For wildcard IP address */
+    hints.ai_protocol = protocol;
+    hints.ai_canonname = NULL;
+    hints.ai_addr = NULL;
+    hints.ai_next = NULL;
+
+    int r = getaddrinfo(ip, port, &hints, &result);
+    if (r != 0) {
+        error("getaddrinfo('%s', '%s'): %s\n", ip, port, gai_strerror(r));
+        return -1;
+    }
+
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        int fd = -1;
+
+        char rip[INET_ADDRSTRLEN + INET6_ADDRSTRLEN] = "INVALID";
+        int rport = default_port;
+
+        switch (rp->ai_addr->sa_family) {
+            case AF_INET: {
+                struct sockaddr_in *sin = (struct sockaddr_in *) rp->ai_addr;
+                inet_ntop(AF_INET, &sin->sin_addr, rip, INET_ADDRSTRLEN);
+                rport = ntohs(sin->sin_port);
+                fd = create_listen_socket4(socktype, rip, rport, listen_backlog);
+                break;
+            }
+
+            case AF_INET6: {
+                struct sockaddr_in6 *sin6 = (struct sockaddr_in6 *) rp->ai_addr;
+                inet_ntop(AF_INET6, &sin6->sin6_addr, rip, INET6_ADDRSTRLEN);
+                rport = ntohs(sin6->sin6_port);
+                fd = create_listen_socket6(socktype, scope_id, rip, rport, listen_backlog);
+                break;
+            }
+
+            default:
+                debug(D_LISTENER, "Unknown socket family %d", rp->ai_addr->sa_family);
+                break;
+        }
+
+        if (fd == -1) {
+            error("Cannot bind to ip '%s', port %d", rip, rport);
+            sockets->failed++;
+        }
+        else {
+            listen_sockets_add(sockets, fd, protocol_str, rip, rport);
+            added++;
+        }
+    }
+
+    freeaddrinfo(result);
+
+    return added;
+}
+
+int listen_sockets_setup(LISTEN_SOCKETS *sockets) {
+    listen_sockets_init(sockets);
+
+    sockets->backlog = (int) config_get_number(sockets->config_section, "listen backlog", sockets->backlog);
+
+    int old_port = sockets->default_port;
+    sockets->default_port = (int) config_get_number(sockets->config_section, "default port", sockets->default_port);
+    if(sockets->default_port < 1 || sockets->default_port > 65535) {
+        error("Invalid listen port %d given. Defaulting to %d.", sockets->default_port, old_port);
+        sockets->default_port = (int) config_set_number(sockets->config_section, "default port", old_port);
+    }
+    debug(D_OPTIONS, "Default listen port set to %d.", sockets->default_port);
+
+    char *s = config_get(sockets->config_section, "bind to", "*");
+    while(*s) {
+        char *e = s;
+
+        // skip separators, moving both s(tart) and e(nd)
+        while(isspace(*e) || *e == ',') s = ++e;
+
+        // move e(nd) to the first separator
+        while(*e && !isspace(*e) && *e != ',') e++;
+
+        // is there anything?
+        if(!*s || s == e) break;
+
+        char buf[e - s + 1];
+        strncpyz(buf, s, e - s);
+        bind_to_one(sockets, buf, sockets->default_port, sockets->backlog);
+
+        s = e;
+    }
+
+    if(!sockets->opened)
+        fatal("Cannot listen on any socket. Exiting...");
+
+    else if(sockets->failed) {
+        size_t i;
+        for(i = 0; i < sockets->opened ;i++)
+            info("Listen socket %s opened successfully.", sockets->fds_names[i]);
+    }
+
+    return (int)sockets->opened;
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// connect to another host/port
+
 // _connect_to()
 // protocol    IPPROTO_TCP, IPPROTO_UDP
 // socktype    SOCK_STREAM, SOCK_DGRAM
@@ -228,6 +551,10 @@ int connect_to_one_of(const char *destination, int default_port, struct timeval 
     return sock;
 }
 
+
+// --------------------------------------------------------------------------------------------------------------------
+// helpers to send/receive data in one call, in blocking mode, with a timeout
+
 ssize_t recv_timeout(int sockfd, void *buf, size_t len, int flags, int timeout) {
     for(;;) {
         struct pollfd fd = {
@@ -289,3 +616,45 @@ ssize_t send_timeout(int sockfd, void *buf, size_t len, int flags, int timeout) 
 
     return send(sockfd, buf, len, flags);
 }
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// accept4() replacement for systems that do not have one
+
+#ifndef HAVE_ACCEPT4
+int accept4(int sock, struct sockaddr *addr, socklen_t *addrlen, int flags) {
+    int fd = accept(sock, addr, addrlen);
+    int newflags = 0;
+
+    if (fd < 0) return fd;
+
+    if (flags & SOCK_NONBLOCK) {
+        newflags |= O_NONBLOCK;
+        flags &= ~SOCK_NONBLOCK;
+    }
+
+#ifdef SOCK_CLOEXEC
+#ifdef O_CLOEXEC
+    if (flags & SOCK_CLOEXEC) {
+        newflags |= O_CLOEXEC;
+        flags &= ~SOCK_CLOEXEC;
+    }
+#endif
+#endif
+
+    if (flags) {
+        errno = -EINVAL;
+        return -1;
+    }
+
+    if (fcntl(fd, F_SETFL, newflags) < 0) {
+        int saved_errno = errno;
+        close(fd);
+        errno = saved_errno;
+        return -1;
+    }
+
+    return fd;
+}
+#endif
+
--- src/socket.h
+++ src/socket.h
@@ -1,14 +1,41 @@
-//
-// Created by costa on 24/12/2016.
-//
-
 #ifndef NETDATA_SOCKET_H
 #define NETDATA_SOCKET_H
+
+#ifndef MAX_LISTEN_FDS
+#define MAX_LISTEN_FDS 50
+#endif
+
+typedef struct listen_sockets {
+    const char *config_section;         // the netdata configuration section to read settings from
+    int default_port;                   // the default port to use
+    int backlog;                        // the default listen backlog to use
+
+    size_t opened;                      // the number of sockets opened
+    size_t failed;                      // the number of sockets attempted to open, but failed
+    int fds[MAX_LISTEN_FDS];            // the open sockets
+    char *fds_names[MAX_LISTEN_FDS];    // descriptions for the open sockets
+} LISTEN_SOCKETS;
+
+extern int listen_sockets_setup(LISTEN_SOCKETS *sockets);
+extern void listen_sockets_close(LISTEN_SOCKETS *sockets);
 
 extern int connect_to(const char *definition, int default_port, struct timeval *timeout);
 extern int connect_to_one_of(const char *destination, int default_port, struct timeval *timeout, size_t *reconnects_counter, char *connected_to, size_t connected_to_size);
 
 extern ssize_t recv_timeout(int sockfd, void *buf, size_t len, int flags, int timeout);
 extern ssize_t send_timeout(int sockfd, void *buf, size_t len, int flags, int timeout);
+
+#ifndef HAVE_ACCEPT4
+extern int accept4(int sock, struct sockaddr *addr, socklen_t *addrlen, int flags);
+
+#ifndef SOCK_NONBLOCK
+#define SOCK_NONBLOCK 00004000
+#endif  /* #ifndef SOCK_NONBLOCK */
+
+#ifndef SOCK_CLOEXEC
+#define SOCK_CLOEXEC 02000000
+#endif /* #ifndef SOCK_CLOEXEC */
+
+#endif /* #ifndef HAVE_ACCEPT4 */
 
 #endif //NETDATA_SOCKET_H
