bc9bb117e6fbdab23df4724b627cd2dfc53e133c
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2017-04-26 16:32:20 +0300

    sync the y-range of charts under the same family, with the same context and the same units; fixes #2132

--- src/proc_net_softnet_stat.c
+++ src/proc_net_softnet_stat.c
@@ -79,7 +79,7 @@ int do_proc_net_softnet_stat(int update_every, usec_t dt) {
 
     st = rrdset_find_bytype_localhost("system", "softnet_stat");
     if(unlikely(!st)) {
-        st = rrdset_create_localhost("system", "softnet_stat", NULL, "softnet_stat", NULL, "System softnet_stat"
+        st = rrdset_create_localhost("system", "softnet_stat", NULL, "softnet_stat", "system.softnet_stat", "System softnet_stat"
                                      , "events/s", 955, update_every, RRDSET_TYPE_LINE);
         for(w = 0; w < allocated_columns ;w++)
             if(unlikely(softnet_column_name(w)))
@@ -103,7 +103,7 @@ int do_proc_net_softnet_stat(int update_every, usec_t dt) {
                 char title[100+1];
                 snprintfz(title, 100, "CPU%zu softnet_stat", l);
 
-                st = rrdset_create_localhost("cpu", id, NULL, "softnet_stat", NULL, title, "events/s", 4101 + l
+                st = rrdset_create_localhost("cpu", id, NULL, "softnet_stat", "cpu.softnet_stat", title, "events/s", 4101 + l
                                              , update_every, RRDSET_TYPE_LINE);
                 for(w = 0; w < allocated_columns ;w++)
                     if(unlikely(softnet_column_name(w)))
