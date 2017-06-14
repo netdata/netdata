30a4d477a9eea6d4b9d3d996fda74b94b0b0b3dc
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2017-06-15 02:35:49 +0300

    remove static references to /etc/netdata from plugins and use /../../../../etc/netdata relative to plugin file; fixes #2264

--- plugins.d/alarm-notify.sh
+++ plugins.d/alarm-notify.sh
@@ -122,8 +122,8 @@ custom_sender() {
 # -----------------------------------------------------------------------------
 # defaults to allow running this script by hand
 
-NETDATA_CONFIG_DIR="${NETDATA_CONFIG_DIR-/etc/netdata}"
-NETDATA_CACHE_DIR="${NETDATA_CACHE_DIR-/var/cache/netdata}"
+[ -z "${NETDATA_CONFIG_DIR}" ] && NETDATA_CONFIG_DIR="$(dirname "${0}")/../../../../etc/netdata"
+[ -z "${NETDATA_CACHE_DIR}" ] && NETDATA_CACHE_DIR="$(dirname "${0}")/../../../../var/cache/netdata"
 [ -z "${NETDATA_REGISTRY_URL}" ] && NETDATA_REGISTRY_URL="https://registry.my-netdata.io"
 
 # -----------------------------------------------------------------------------
