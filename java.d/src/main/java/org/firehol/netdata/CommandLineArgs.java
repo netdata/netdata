9e3b24b0676505091e1f9c3c146ed583c356e107
Simon Nagl <simonnagl@aim.com>
2017-09-02 08:26:42 +0200

    java-plugin: fix license headers

--- java.d/src/main/java/org/firehol/netdata/CommandLineArgs.java
+++ java.d/src/main/java/org/firehol/netdata/CommandLineArgs.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2017 Simon Nagl
  *
- * netadata-plugin-java-daemon is free software: you can redistribute it and/or modify
+ * netdata is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
