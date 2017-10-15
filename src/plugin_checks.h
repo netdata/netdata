b08f339d2f346f0af9283b01a2172d92bc2191a0
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2017-10-15 23:51:52 +0300

    do not compile checks plugin if netdata is compiled with debugging

--- src/plugin_checks.h
+++ src/plugin_checks.h
@@ -1,6 +1,8 @@
 #ifndef NETDATA_PLUGIN_CHECKS_H
 #define NETDATA_PLUGIN_CHECKS_H 1
 
+#ifdef NETDATA_INTERNAL_CHECKS
 void *checks_main(void *ptr);
+#endif // NETDATA_INTERNAL_CHECKS
 
 #endif /* NETDATA_PLUGIN_PROC_H */
