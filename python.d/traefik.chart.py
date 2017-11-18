b3a82ef29cab749ff6f0ea583879d07bad7643fb
amenezes <alexandre.fmenezes@gmail.com>
2017-11-17 23:07:28 -0200

    refactored code

--- python.d/traefik.chart.py
+++ python.d/traefik.chart.py
@@ -3,6 +3,7 @@
 # Author: Alexandre Menezes (@ale_menezes)
 
 from json import loads
+
 from bases.FrameworkServices.UrlService import UrlService
 
 # default module values (can be overridden per job in `config`)
@@ -10,18 +11,8 @@ update_every = 1
 priority = 60000
 retries = 10
 
-HEALTH_STATS = [
-    'average_response_time_sec',
-    'total_response_time_sec',
-    'total_count',
-    'total_status_code_count',
-    'uptime_sec'
-]
-
 # charts order (can be overridden if you want less charts, or different order)
-ORDER = [
-    'uptime', 'average_response_time', 'total_response_time', 'requests', 'response_statuses', 'responses_summary'
-    ]
+ORDER = ['uptime', 'average_response_time', 'total_response_time', 'requests', 'response_statuses', 'response_class', 'detailed_response_codes']
 
 CHARTS = {
     'uptime': {
@@ -45,7 +36,7 @@ CHARTS = {
             ['total_count', 'requests', 'incremental']
         ]},
     'response_statuses': {
-        'options': [None, 'Response Statuses', 'requests/s', 'responses statuses', 'traefik.response_statuses', 'stacked'],
+        'options': [None, 'Response Statuses', 'requests/s', 'responses', 'traefik.response_statuses', 'stacked'],
         'lines': [
             ['successful_requests', 'success', 'incremental'],
             ['redirects', 'redirect', 'incremental'],
@@ -53,8 +44,8 @@ CHARTS = {
             ['bad_requests', 'bad', 'incremental'],
             ['other_requests', 'other', 'incremental']
         ]},
-    'responses_summary': {
-        'options': [None, 'Responses Summary', 'requests/s', 'responses summary', 'traefik.response_summary', 'stacked'],
+    'response_class': {
+        'options': [None, 'Responses by class', 'requests/s', 'responses', 'traefik.response_class', 'stacked'],
         'lines': [
             ['1xx', None, 'incremental'],
             ['2xx', None, 'incremental'],
@@ -62,9 +53,21 @@ CHARTS = {
             ['4xx', None, 'incremental'],
             ['5xx', None, 'incremental'],
             ['unmatched', None, 'incremental']
+        ]},
+    'detailed_response_codes': {
+        'options': [None, 'Detailed Response Codes', 'requests/s', 'responses', 'traefik.detailed_response_codes', 'stacked'],
+        'lines': [
+            ['None', None, 'incremental']
         ]}
     }
 
+HEALTH_STATS = [
+    'uptime_sec',
+    'average_response_time_sec',
+    'total_response_time_sec',
+    'total_count',
+    'total_status_code_count'
+]
 
 class Service(UrlService):
     def __init__(self, configuration=None, name=None):
@@ -72,10 +75,24 @@ class Service(UrlService):
         self.url = self.configuration.get('url', 'http://localhost:8080/health')
         self.order = ORDER
         self.definitions = CHARTS
-        self.data = {'1xx': 0, '2xx': 0, '3xx': 0, '4xx': 0,
-                '5xx': 0, 'unmatched': 0, 'successful_requests': 0,
-                'redirects': 0, 'bad_requests': 0, 'server_errors': 0,
-                'other_requests': 0}
+        self.data = {'None': 0}
+
+    def check(self):
+        if not (self.url):
+            self.error('url is not defined in the module configuration file')
+            return False
+
+        self._manager = self._build_manager()
+        data = self._get_raw_data(self.url)
+        data = loads(data)
+
+        for code in data['total_status_code_count']:
+            self.definitions['detailed_response_codes']['lines'].append(
+                    [code, code, 'incremental']
+            )
+            if code not in self.data:
+                self.data[code] = data['total_status_code_count'][code]
+        return UrlService.check(self)
 
     def _get_data(self):
         data = self._get_raw_data()
@@ -84,41 +101,68 @@ class Service(UrlService):
             return None
 
         data = loads(data)
+
+        self.get_data_per_code_status(raw_data=data)
+
+        self.get_data_per_code_family(raw_data=data)
+
         self.get_data_per_code(raw_data=data)
+
         to_netdata = fetch_data_(raw_data=data, metrics=HEALTH_STATS)
         to_netdata.update(self.data)
-        self.data = {'1xx': 0, '2xx': 0, '3xx': 0, '4xx': 0,
-                '5xx': 0, 'unmatched': 0, 'successful_requests': 0,
-                'redirects': 0, 'bad_requests': 0, 'server_errors': 0,
-                'other_requests': 0}
+
         return to_netdata or None
+
+    def get_data_per_code_status(self, raw_data):
+        code_status = {'successful_requests': 0, 'redirects': 0,
+                       'bad_requests': 0, 'server_errors': 0,
+                       'other_requests': 0}
+        for code in raw_data['total_status_code_count']:
+            code_prefix = code[0]
+            if code_prefix == '1' or code_prefix == '2' or code == '304':
+                code_status['successful_requests'] += raw_data['total_status_code_count'][code]
+            elif code_prefix == '3':
+                code_status['redirects'] += raw_data['total_status_code_count'][code]
+            elif code_prefix == '4':
+                code_status['bad_requests'] += raw_data['total_status_code_count'][code]
+            elif code_prefix == '5':
+                code_status['server_errors'] += raw_data['total_status_code_count'][code]
+            else:
+                code_status['other_requests'] += raw_data['total_status_code_count'][code]
+        self.data.update(code_status)
+
+    def get_data_per_code_family(self, raw_data):
+        code_family = {'1xx': 0, '2xx': 0, '3xx': 0, '4xx': 0,
+                '5xx': 0, 'unmatched': 0}
+        for code in raw_data['total_status_code_count']:
+            code_prefix = code[0]
+            if code_prefix == '1':
+                code_family['1xx'] += raw_data['total_status_code_count'][code]
+            elif code_prefix == '2':
+                code_family['2xx'] += raw_data['total_status_code_count'][code]
+            elif code_prefix == '3':
+                code_family['3xx'] += raw_data['total_status_code_count'][code]
+            elif code_prefix == '4':
+                code_family['4xx'] += raw_data['total_status_code_count'][code]
+            elif code_prefix == '5':
+                code_family['5xx'] += raw_data['total_status_code_count'][code]
+            else:
+                code_family['unmatched'] += raw_data['total_status_code_count'][code]
+        self.data.update(code_family)
 
     def get_data_per_code(self, raw_data):
         for code in raw_data['total_status_code_count']:
-            code_prefix = list(code)[0]
-            if code_prefix == '1':
-                self.data['1xx'] = raw_data['total_status_code_count'][code]
-                self.data['successful_requests'] += raw_data['total_status_code_count'][code]
-            elif code_prefix == '2':
-                self.data['2xx'] = raw_data['total_status_code_count'][code]
-                self.data['successful_requests'] += raw_data['total_status_code_count'][code]
-            elif code_prefix == '304':
-                self.data['successful_requests'] += raw_data['total_status_code_count'][code]
-            elif code_prefix == '3':
-                self.data['3xx'] = raw_data['total_status_code_count'][code]
-                self.data['redirects'] += raw_data['total_status_code_count'][code]
-            elif code_prefix == '4':
-                self.data['4xx'] = raw_data['total_status_code_count'][code]
-                self.data['bad_requests'] += raw_data['total_status_code_count'][code]
-            elif code_prefix == '5':
-                self.data['5xx'] = raw_data['total_status_code_count'][code]
-                self.data['server_errors'] += raw_data['total_status_code_count'][code]
-            else:
-                self.data['unmatched'] = raw_data['total_status_code_count'][code]
-                self.data['other_requests'] += raw_data['total_status_code_count'][code]
+            self.add_chart_dimension(code)
+            self.data[code] = raw_data['total_status_code_count'][code]
+
+    def add_chart_dimension(self, code):
+        if code not in self.data:
+            self.charts['detailed_response_codes'].add_dimension([code, code, 'incremental'])
+            self.data[code] = 0
 
 def fetch_data_(raw_data, metrics):
     data = dict()
+
     for metric in metrics:
         value = raw_data
         metrics_list = metric.split('.')
@@ -128,4 +172,5 @@ def fetch_data_(raw_data, metrics):
         except KeyError:
             continue
         data['_'.join(metrics_list)] = value
+
     return data
