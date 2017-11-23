033046ca170794ca96bf6fac1b78cd760b6770f8
blub <>
2017-11-23 15:42:13 +0100

    Modified dnsdist values

--- python.d/dnsdist.chart.py
+++ python.d/dnsdist.chart.py
@@ -2,54 +2,83 @@
 from json import loads
 from bases.FrameworkServices.UrlService import UrlService
 
-ORDER = ['latency', 'cache', 'acl', 'noncompliant', 'queries', 'health']
+ORDER = ['queries', 'queries_dropped', 'answers', 'backend_responses', 'backend_commerrors', 'backend_errors', 'cache', 'servercpu', 'servermem', 'query_latency', 'query_latency_avg']
 CHARTS = {
-	'latency': {
-		'options': [None, 'response latency', 'value', 'latency', 'dnsdist.latency', 'area'],
-		'lines': [
-			['latency-slow', '> 1sec', 'incremental'],
-			['latency100-1000', '100-1000ms', 'incremental'],
-			['latency50-100', '50-100ms', 'incremental'],
-			['latency10-50', '10-50ms', 'incremental'],
-			['latency1-10', '1-10ms', 'incremental'],
-			['latency0-1', '< 1ms', 'incremental']
-	]},
-	'cache': {
-		'options': [None, 'cache performance', 'value', 'cache', 'dnsdist.cache', 'area'],
-		'lines': [
-			['cache-hits', 'hits', 'incremental'],
-			['cache-misses', 'misses', 'incremental']
-	]},
-	'acl': {
-		'options': [None, 'access-control-list events', 'value', 'acl', 'dnsdist.acl', 'area'],
-		'lines': [
-			['acl-drops', 'drop by acl', 'incremental'],
-			['rule-drop', 'drop by rule', 'incremental'],
-			['rule-nxdomain', 'nxdomain by rule', 'incremental'],
-			['rule-refused', 'refused by rule', 'incremental']
-	]},
-	'noncompliant': {
-		'options': [None, 'noncompliant data', 'value', 'noncompliant', 'dnsdist.noncompliant', 'area'],
-		'lines': [
-			['empty-queries', 'empty queries', 'incremental'],
-		    ['no-policy', 'no policy', 'incremental'],
-			['noncompliant-queries', 'noncompliant queries', 'incremental'],
-			['noncompliant-responses', 'noncompliant responses', 'incremental']
-	]},
 	'queries': {
-		'options': [None, 'queries', 'value', 'queries', 'dnsdist.queries', 'area'],
+		'options': [None, 'Client queries received', 'queries/s', 'queries', 'dnsdist.queries', 'line'],
 		'lines': [
-			['queries', 'queries', 'incremental'],
-			['rdqueries', 'recursive queries', 'incremental'],
+			['queries', 'all', 'incremental'],
+			['rdqueries', 'recursive', 'incremental'],
+			['empty-queries', 'empty', 'incremental']
+	]},
+	'queries_dropped': {
+		'options': [None, 'Client queries dropped', 'queries/s', 'queries', 'dnsdist.queries_dropped', 'line'],
+		'lines': [
+			['rule-drop', 'rule drop', 'incremental'],
+			['dyn-blocked', 'dynamic block', 'incremental'],
+			['no-policy', 'no policy', 'incremental'],
+			['noncompliant-queries', 'non compliant', 'incremental'],
+			['acl-drops', 'acl', 'incremental'],
+	]},
+	'answers': {
+		'options': [None, 'Answers statistics', 'answers/s', 'answers', 'dnsdist.answers', 'line'],
+		'lines': [
+			['self-answered', 'self answered', 'incremental'],
+			['rule-nxdomain', 'nxdomain', 'incremental', -1],
+			['rule-refused', 'refused', 'incremental', -1],
+			['trunc-failures', 'trunc failures', 'incremental', -1]
+	]},
+	'backend_responses': {
+		'options': [None, 'Backend responses', 'responses/s', 'backends', 'dnsdist.backend_responses', 'line'],
+		'lines': [
 			['responses', 'responses', 'incremental']
 	]},
-	'health': {
-		'options': [None, 'health', 'value', 'health', 'dnsdist.health', 'area'],
+	'backend_commerrors': {
+		'options': [None, 'Backend Communication Errors', 'errors/s', 'backends', 'dnsdist.backend_commerrors', 'line'],
 		'lines': [
-			['downstream-send-errors', 'ds send errors', 'incremental'],
-			['downstream-timeouts', 'ds timeouts', 'incremental'],
-			['servfail-responses', 'servfail responses', 'incremental'],
-			['trunc-failures', 'trunc failures', 'incremental']
+			['downstream-send-errors', 'send errors', 'incremental']
+	]},
+	'backend_errors': {
+		'options': [None, 'Backend error responses', 'responses/s', 'backends', 'dnsdist.backend_errors', 'line'],
+		'lines': [
+			['downstream-timeouts', 'timeout', 'incremental'],
+			['servfail-responses', 'servfail', 'incremental'],
+			['noncompliant-responses', 'non compliant', 'incremental']
+	]},
+	'cache': {
+		'options': [None, 'Cache performance', 'answers/s', 'cache', 'dnsdist.cache', 'area'],
+		'lines': [
+			['cache-hits', 'hits', 'incremental'],
+			['cache-misses', 'misses', 'incremental', -1]
+	]},
+	'servercpu': {
+		'options': [None, 'DNSDIST server CPU utilization', 'ms/s', 'server', 'dnsdist.servercpu', 'stacked'],
+		'lines': [
+			['cpu-sys-msec', 'system state', 'incremental'],
+			['cpu-user-msec', 'user state', 'incremental']
+	]},
+	'servermem': {
+		'options': [None, 'DNSDIST server memory utilization', 'MiB', 'server', 'dnsdist.servermem', 'area'],
+		'lines': [
+			['real-memory-usage', 'memory usage', 'absolute', 1, 1048576]
+	]},
+	'query_latency': {
+		'options': [None, 'Query latency', 'queries/s', 'latency', 'dnsdist.query_latency', 'stacked'],
+		'lines': [
+			['latency0-1', '1ms', 'incremental'],
+			['latency1-10', '10ms', 'incremental'],
+			['latency10-50', '50ms', 'incremental'],
+			['latency50-100', '100ms', 'incremental'],
+			['latency100-1000', '1sec', 'incremental'],
+			['latency-slow', 'slow', 'incremental']
+	]},
+	'query_latency_avg': {
+		'options': [None, 'Average latency for the last N queries', 'ms/query', 'latency', 'dnsdist.query_latency_avg', 'line'],
+		'lines': [
+			['latency-avg100', '100', 'absolute'],
+			['latency-avg1000', '1k', 'absolute'],
+			['latency-avg10000', '10k', 'absolute'],
+			['latency-avg1000000', '1000k', 'absolute']
 	]}
 }
 
