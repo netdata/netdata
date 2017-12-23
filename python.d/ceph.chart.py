37ec3474b943fb7e1c3bcca2a034188e7f05e722
Luis Eduardo <leduardo@lsd.ufcg.edu.br>
2017-12-23 11:05:38 -0300

    Add @l2isbad suggestions and improve pool method

--- python.d/ceph.chart.py
+++ python.d/ceph.chart.py
@@ -2,36 +2,42 @@
 # Description: ceph netdata python.d module
 # Author: Luis Eduardo (lets00)
 
-import rados
+try:
+    import rados
+    CEPH = True
+except ImportError:
+    CEPH = False
+
 import json
 from bases.FrameworkServices.SimpleService import SimpleService
+
+# default module values (can be overridden per job in `config`)
+update_every = 10
+priority = 60000
+retries = 60
 
 ORDER = ['general', 'pool_usage', 'pool_objects', 'osd_usage']
 
 CHARTS = {
     'general': {
-        'options': [None, 'Ceph General Options', 'usage', 'general', 'ceph.general', 'line'],
-        'lines': []
+        'options': [None, 'Ceph General Options', 'KB', 'general', 'ceph.general', 'stacked'],
+        'lines': [
+            ['general_usage', 'Usage', 'absolute', 1, 1024],
+            ['general_available', 'Available', 'absolute', 1, 1024]
+        ]
     },
     'pool_usage': {
-        'options': [None, 'Ceph Pools', 'usage', 'pool usage', 'ceph.pool_usage', 'line'],
+        'options': [None, 'Ceph Pools', 'KB', 'pool', 'ceph.pool_usage', 'line'],
         'lines': []
     },
     'pool_objects': {
-        'options': [None, 'Ceph Pools', 'objects', 'number of objects', 'ceph.pool_objects', 'line'],
+        'options': [None, 'Ceph Pools', 'objects', 'pool', 'ceph.pool_objects', 'line'],
         'lines': []
     },
     'osd_usage': {
-        'options': [None, 'Ceph OSDs', 'usage', 'osd usage', 'ceph.osd_usage', 'line'],
+        'options': [None, 'Ceph OSDs', 'KB', 'osd', 'ceph.osd_usage', 'line'],
         'lines': []
     }
-}
-
-PRESENTATION_MAP = {
-    'KB': 1,
-    'MB': 1024,
-    'GB': 1024**2,
-    'TB': 1024**3
 }
 
 
@@ -42,8 +48,6 @@ class Service(SimpleService):
         self.definitions = CHARTS
         self.config_file = self.configuration.get('config_file')
         self.keyring_file = self.configuration.get('keyring_file')
-        self.presentation_data = self.configuration.get('presentation_data')
-        self.presentation_value = None
         self.cluster = rados.Rados(conffile=self.config_file,
                                    conf=dict(keyring=self.keyring_file))
 
@@ -52,15 +56,12 @@ class Service(SimpleService):
         Checks module
         :return:
         """
+        if not CEPH:
+            self.error('rados module is needed to use ceph.chart.py')
+            return False
         if not (self.config_file and self.keyring_file):
             self.error('config_file and/or keyring_file is not defined')
             return False
-
-        # Set default presentation_data to GB if it does not defined and declare
-        # presentation_value with PRESENTATION_MAP value.
-        if not self.presentation_data:
-            self.presentation_data = 'GB'
-        self.presentation_value = PRESENTATION_MAP[self.presentation_data]
         try:
             self.cluster.connect()
         except Exception as error:
@@ -71,38 +72,22 @@ class Service(SimpleService):
 
     def create_definitions(self):
         """
-        Create dynamically charts options, add presentation data/value
+        Create dynamically charts options
         set on YAML file
         :return: None
         """
-        # Options
-        self.definitions['general']['options'] = [None, 'Ceph General Options',
-                                                  'usage ({})'.format(self.presentation_data),
-                                                  'general', 'ceph.general', 'areastack']
-        self.definitions['pool_usage']['options'] = [None, 'Ceph Pools',
-                                                     'usage ({})'.format(self.presentation_data),
-                                                     'pool usage', 'ceph.pool_usage', 'line']
-        self.definitions['osd_usage']['options'] = [None, 'Ceph OSDs',
-                                                     'usage ({})'.format(self.presentation_data),
-                                                     'osd usage', 'ceph.osd_usage', 'line']
-
         # Lines
-        self.definitions['general']['lines'].append(['general_total', 'Total',
-                                                'absolute', 1, self.presentation_value])
-        self.definitions['general']['lines'].append(['general_usage', 'Usage',
-                                                'absolute', 1, self.presentation_value])
-        self.definitions['general']['lines'].append(['general_available', 'Available',
-                                                'absolute', 1, self.presentation_value])
-
-        for pool in self.cluster.list_pools():
-            self.definitions['pool_usage']['lines'].append([pool, pool,
-                                                            'absolute', 1, self.presentation_value])
-            self.definitions['pool_objects']['lines'].append(["obj_{}".format(pool), pool,
+        for pool in self._get_df()['pools']:
+            self.definitions['pool_usage']['lines'].append([pool['name'],
+                                                            pool['name'],
+                                                            'absolute'])
+            self.definitions['pool_objects']['lines'].append(["obj_{}".format(pool['name']),
+                                                              pool['name'],
                                                               'absolute'])
-
-        for osd in self._get_osds_df()['nodes']:
-            self.definitions['osd_usage']['lines'].append([osd['name'], osd['name'],
-                                                           'absolute', 1, self.presentation_value])
+        for osd in self._get_osd_df()['nodes']:
+            self.definitions['osd_usage']['lines'].append([osd['name'],
+                                                           osd['name'],
+                                                           'absolute'])
 
     def get_data(self):
         """
@@ -124,11 +109,9 @@ class Service(SimpleService):
         Get ceph's general usage
         :return: dict
         """
-        info = self.cluster.get_cluster_stats()
-        # Values are get in bytes, convert it
-        return {'general_total': float(info['kb']),
-                'general_usage': float(info['kb_used']),
-                'general_available': float(info['kb_avail'])
+        info = self._get_df()['stats']
+        return {'general_usage': info['total_used_bytes'],
+                'general_available': info['total_avail_bytes']
                 }
 
     def _get_pool_usage(self):
@@ -138,7 +121,7 @@ class Service(SimpleService):
         """
         pool_usage = {}
         for df in self._get_df()['pools']:
-            pool_usage[df['name']] = df['stats']['bytes_used']
+            pool_usage[df['name']] = df['stats']['kb_used']
         return pool_usage
 
     def _get_pool_objects(self):
@@ -152,13 +135,12 @@ class Service(SimpleService):
         return pool_objects
 
     def _get_osd_usage(self):
-        # TODO: FIX ME
         """
         Process raw data into osd dict information to get osd usage
         :return: A osd dict with osd name's key and usage bytes' value
         """
         osd_usage = {}
-        for osd in self._get_osds_df()['nodes']:
+        for osd in self._get_osd_df()['nodes']:
             osd_usage[osd['name']] = float(osd['kb_used'])
         return osd_usage
 
@@ -172,7 +154,7 @@ class Service(SimpleService):
             'format': 'json'
         }), '')[1])
 
-    def _get_osds_df(self):
+    def _get_osd_df(self):
         """
         Get ceph osd df output
         :return: ceph osd df --format json
