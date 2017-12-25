45e0bc526ed896f236b208994edcacdfc5806c7e
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2017-12-25 20:25:15 +0200

    support option --dont-start-it on static 64 bit installer; fixes https://github.com/firehol/binary-packages/issues/3

--- kickstart-static64.sh
+++ kickstart-static64.sh
@@ -213,16 +213,30 @@ fi
 # ---------------------------------------------------------------------------------------------------------------------
 
 opts=
-if [ "${1}" = "--dont-wait" -o "${1}" = "--non-interactive" ]
-then
-    opts="--accept"
-fi
+inner_opts=
+while [ ! -z "${1}" ]
+do
+    if [ "${1}" = "--dont-wait" -o "${1}" = "--non-interactive" ]
+    then
+        opts="${opts} --accept"
+    elif [ "${1}" = "--dont-start-it" ]
+    then
+        inner_opts="${inner_opts} ${1}"
+    else
+        echo >&2 "Unknown option '${1}'"
+        exit 1
+    fi
+    shift
+done
+[ ! -z "${inner_opts}" ] && inner_opts="-- ${inner_opts}"
+
+# ---------------------------------------------------------------------------------------------------------------------
 
 progress "Installing netdata"
 
 sudo=
 [ "${UID}" != "0" ] && sudo="sudo"
-run ${sudo} sh "/tmp/${LATEST}" ${opts}
+run ${sudo} sh "/tmp/${LATEST}" ${opts} ${inner_opts}
 
 if [ $? -eq 0 ]
 	then
