917b674f818690c7702d64d080c8b7ef4bf38278
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2017-12-27 20:35:35 +0200

    remove obsolete compilation flag

--- makeself/jobs/70-netdata-git.install.sh
+++ makeself/jobs/70-netdata-git.install.sh
@@ -6,7 +6,7 @@ cd "${NETDATA_SOURCE_PATH}" || exit 1
 
 if [ ${NETDATA_BUILD_WITH_DEBUG} -eq 0 ]
 then
-    export CFLAGS="-static -O3 -DNETDATA_WAIT_BEFORE_EXIT=2"
+    export CFLAGS="-static -O3"
 else
     export CFLAGS="-static -O1 -ggdb -Wall -Wextra -Wformat-signedness -fstack-protector-all -D_FORTIFY_SOURCE=2 -DNETDATA_INTERNAL_CHECKS=1"
 #    export CFLAGS="-static -O1 -ggdb -Wall -Wextra -Wformat-signedness"
