7be05a9f07f7589e6df58747d6a8084cd44c10ae
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2018-01-03 17:14:56 +0200

    added script to build RPM

---
+++ contrib/rhel/build-netdata-rpm.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# docker run -it --rm centos:6.9 /bin/sh
+# yum -y install rpm-build redhat-rpm-config yum-utils autoconf automake curl gcc git libmnl-devel libuuid-devel make pkgconfig zlib-devel
+
+set -e
+
+cd $(dirname $0)/../../
+source "installer/functions.sh"
+
+run ./autogen.sh
+run ./configure --enable-maintainer-mode
+run make dist
+
+version=$(grep PACKAGE_VERSION < config.h | cut -d '"' -f 2)
+if [ -z "${version}" ]
+then
+    echo >&2 "Cannot find netdata version."
+    exit 1
+fi
+
+tgz="netdata-${version}.tar.gz"
+if [ ! -f "${tgz}" ]
+then
+	echo >&2 "Cannot find the generated tar.gz file '${tgz}'"
+	exit 1
+fi
+
+srpm=$(run rpmbuild -ts "${tgz}" | cut -d ' ' -f 2)
+if [ -z "${srpm}" -o ! -f "${srpm}" ]
+then
+	echo >&2 "Cannot find the generated SRPM file '${srpm}'"
+	exit 1
+fi
+
+run yum-builddep "${srpm}"
+
+run rpmbuild --rebuild "${srpm}"
+
+echo >&2 "All done!"
