7efa360861325cdfa9845834cde2f938d485956b
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2018-01-06 15:32:53 +0200

    fixed typo

--- web/dashboard_info.js
+++ web/dashboard_info.js
@@ -713,7 +713,7 @@ netdataDashboard.context = {
 
     'ipv4.tcplistenissues': {
         info: '<b>overflows</b> (or <code>ListenOverflows</code>) is the number of incoming connections that could not be handled because the receive queue of the application was full (for both IPv4 and IPv6).'
-            + ' <b>drops</b> (or <code>ListenDrops</code>) is the number of incoming connections that could not be handled, including SYN floods, overflows, out of memory, security issues, no route to destination, recemption of related ICMP messages, socket is broadcast or multicast (for both IPv4 and IPv6).'
+            + ' <b>drops</b> (or <code>ListenDrops</code>) is the number of incoming connections that could not be handled, including SYN floods, overflows, out of memory, security issues, no route to destination, reception of related ICMP messages, socket is broadcast or multicast (for both IPv4 and IPv6).'
     },
 
     // ------------------------------------------------------------------------
