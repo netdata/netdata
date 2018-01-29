10c7e663cb418a47a3b3df8201a86d127860f0df
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2018-01-29 22:55:13 +0200

    escape alarm expressions to avoid them being detected as xss

--- src/health_json.c
+++ src/health_json.c
@@ -2,8 +2,12 @@
 #include "common.h"
 
 static inline void health_string2json(BUFFER *wb, const char *prefix, const char *label, const char *value, const char *suffix) {
-    if(value && *value)
-        buffer_sprintf(wb, "%s\"%s\":\"%s\"%s", prefix, label, value, suffix);
+    if(value && *value) {
+        buffer_sprintf(wb, "%s\"%s\":\"", prefix, label);
+        buffer_strcat_htmlescape(wb, value);
+        buffer_strcat(wb, "\"");
+        buffer_strcat(wb, suffix);
+    }
     else
         buffer_sprintf(wb, "%s\"%s\":null%s", prefix, label, suffix);
 }
