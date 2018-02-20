164d13c681931f84394534e2a8c19b5ab4f2a20f
Chris <github.account@chrigel.net>
2018-02-20 11:52:56 +0100

    Add multi-port support per host

--- python.d/portcheck.chart.py
+++ python.d/portcheck.chart.py
@@ -15,19 +15,21 @@ ORDER = ['latency', 'error']
 CHARTS = {
     'latency': {
         'options': [None, 'TCP connect latency', 'ms', 'latency', 'portcheck.latency', 'line'],
-        'lines': [
-            ['tcp_connect_time', 'connect', 'absolute', 100, 1000]
-        ]},
+        'lines': []
+    },
     'error': {
         'options': [None, 'Portcheck error code', 'code', 'error', 'portcheck.error', 'line'],
         'lines': [
-            ['tcp_error', 'error', 'absolute']
+
         ]}
 }
 
 SOCKET_FAILED = -1
 CONNECTION_FAILED = -2
 CONNECTION_TIMED_OUT = -3
+
+TCP_DIMENSION_CONNECT_PREFIX = 'tcp_connect_'
+TCP_DIMENSION_ERROR_PREFIX = 'tcp_error_'
 
 
 class Service(SimpleService):
@@ -36,28 +38,37 @@ class Service(SimpleService):
         self.order = ORDER
         self.definitions = CHARTS
         self.chart_name = ""
-        self._sock = None
-        self.__socket_config = None
         self.host = self.configuration.get('host', None)
-        self.port = self.configuration.get('port', None)
-        self.update_every = self.configuration.get('update every', 3)
-        self.timeout = self.configuration.get('timeout', 5)
+        self.ports = self.configuration.get('ports', None)
+        self.update_every = self.configuration.get('update every', 1)
+        self.timeout = self.configuration.get('timeout', 3)
 
     def check(self):
         """
         Parse configuration, check if configuration is available, and dynamically create chart lines data
         :return: boolean
         """
-        if self.host is None or self.port is None:
-            self.error("Host and/or port missing")
+        if self.host is None or self.ports is None:
+            self.error("Host and/or ports missing")
             return False
-        else:
+        if not isinstance(self.ports, list):
+            self.error('"ports" is not defined as a list. Specify a PyYaml compatible list.')
+            return False
+
+        for port in self.ports:
+            if not isinstance(port, int):
+                self.error("{port} is not an integer. Disabling plugin.".format(port=port))
+                return False
             self.debug("Enabled portcheck: {host}:{port}, update every {update}s, timeout: {timeout}s".format(
-                host=self.host, port=self.port, update=self.update_every, timeout=self.timeout
+                host=self.host, port=port, update=self.update_every, timeout=self.timeout
             ))
-            # We will accept any configuration, even if initial connection fails (a service might be down from
-            # the beginning)
-            return True
+            self.definitions['latency']['lines'].append(
+                [TCP_DIMENSION_CONNECT_PREFIX + str(port), port, 'absolute', 100, 1000])
+            self.definitions['error']['lines'].append(
+                [TCP_DIMENSION_ERROR_PREFIX + str(port), port, 'absolute'])
+        # We will accept any (valid-ish) configuration, even if initial connection fails (a service might be down from
+        # the beginning)
+        return True
 
     def _get_data(self):
         """
@@ -66,73 +77,79 @@ class Service(SimpleService):
         """
         data = dict()
 
-        for res in socket.getaddrinfo(self.host, self.port, socket.AF_UNSPEC, socket.SOCK_STREAM):
-            # use first working socket
-            if self._create_socket(res):
-                self._connect2socket(data)
-                self._disconnect()
-                return data
+        for port in self.ports:
+            success = False
+            for socket_config in socket.getaddrinfo(self.host, port, socket.AF_UNSPEC, socket.SOCK_STREAM):
+                # use first working socket
+                sock = self._create_socket(socket_config)
+                if sock is not None:
+                    self._connect2socket(data, socket_config, sock)
+                    self._disconnect(sock)
+                    success = True
+                    break
 
-        # We could not connect
-        data['tcp_connect_time'] = 0
-        data['tcp_error'] = SOCKET_FAILED
+            # We could not connect
+            if not success:
+                data[TCP_DIMENSION_CONNECT_PREFIX + str(port)] = 0
+                data[TCP_DIMENSION_ERROR_PREFIX + str(port)] = SOCKET_FAILED
+
         return data
 
-    def _create_socket(self, res=None):
-        af, sock_type, proto, canon_name, sa = res
+    def _create_socket(self, socket_config):
+        af, sock_type, proto, canon_name, sa = socket_config
         try:
             self.debug('Creating socket to "{address}", port {port}'.format(address=sa[0], port=sa[1]))
-            self._sock = socket.socket(af, sock_type, proto)
-            self.__socket_config = res
-            self._sock.settimeout(self.timeout)
-            return True
+            sock = socket.socket(af, sock_type, proto)
+            sock.settimeout(self.timeout)
+            return sock
         except socket.error as error:
-            self.error('Failed to create socket "{address}", port {port}, error: {error}'.format(
+            self.debug('Failed to create socket "{address}", port {port}, error: {error}'.format(
                 address=sa[0], port=sa[1], error=error
             ))
-            return False
+            return None
 
-    def _connect2socket(self, data):
+    def _connect2socket(self, data, socket_config, sock):
         """
         Connect to a socket, passing the result of getaddrinfo()
         :return: dict
         """
-        data['tcp_connect_time'] = 0
-        data['tcp_error'] = 0
 
-        af, sock_type, proto, canon_name, sa = self.__socket_config
+        af, sock_type, proto, canon_name, sa = socket_config
+        port = str(sa[1])
+        data[TCP_DIMENSION_CONNECT_PREFIX + port] = 0
+        data[TCP_DIMENSION_ERROR_PREFIX + port] = 0
         try:
-            self.debug('connecting socket to "{address}", port {port}'.format(address=sa[0], port=sa[1]))
+            self.debug('Connecting socket to "{address}", port {port}'.format(address=sa[0], port=port))
             start = time.time()
-            self._sock.connect(sa)
+            sock.connect(sa)
             diff = time.time() - start
-            self.debug('connected to "{address}", port {port}, latency {latency}'.format(
-                address=sa[0], port=sa[1], latency=diff
+            self.debug('Connected to "{address}", port {port}, latency {latency}'.format(
+                address=sa[0], port=port, latency=diff
             ))
             # we will set it at least 0.1 ms. 0.0 would mean failed connection (handy for 3rd-party-APIs)
-            data['tcp_connect_time'] = max(round(diff * 10000), 1)
+            data[TCP_DIMENSION_CONNECT_PREFIX + port] = max(round(diff * 10000), 1)
 
         except socket.timeout as error:
-            self.error('Socket timed out on "{address}", port {port}, error: {error}'.format(
-                address=sa[0], port=sa[1], error=error
+            self.debug('Socket timed out on "{address}", port {port}, error: {error}'.format(
+                address=sa[0], port=port, error=error
             ))
-            data['tcp_error'] = CONNECTION_TIMED_OUT
+            data[TCP_DIMENSION_ERROR_PREFIX + port] = CONNECTION_TIMED_OUT
 
         except socket.error as error:
-            self.error('Failed to connect to "{address}", port {port}, error: {error}'.format(
-                address=sa[0], port=sa[1], error=error
+            self.debug('Failed to connect to "{address}", port {port}, error: {error}'.format(
+                address=sa[0], port=port, error=error
             ))
-            data['tcp_error'] = CONNECTION_FAILED
+            data[TCP_DIMENSION_ERROR_PREFIX + port] = CONNECTION_FAILED
 
-    def _disconnect(self):
+    def _disconnect(self, sock):
         """
         Close socket connection
         :return:
         """
-        if self._sock is not None:
+        if sock is not None:
             try:
-                self.debug('closing socket')
-                self._sock.shutdown(2)  # 0 - read, 1 - write, 2 - all
-                self._sock.close()
+                self.debug('Closing socket')
+                sock.shutdown(2)  # 0 - read, 1 - write, 2 - all
+                sock.close()
             except Exception:
                 pass
