65afeeaaa89148ec050d59ef366db0a07a5cbc22
Chris <github.account@chrigel.net>
2018-02-28 18:03:21 +0100

    Refactored code

--- python.d/httpcheck.chart.py
+++ python.d/httpcheck.chart.py
@@ -3,18 +3,17 @@
 # Original Author: ccremer (github.com/ccremer)
 
 import urllib3
-import time
 import re
+
+try:
+    from time import monotonic as time
+except ImportError:
+    from time import time
 
 from bases.FrameworkServices.UrlService import UrlService
 
-try:
-    urllib3.disable_warnings()
-except AttributeError:
-    pass
-
 # default module values (can be overridden per job in `config`)
-# update_every = 2
+update_every = 3
 priority = 60000
 retries = 60
 
@@ -57,32 +56,15 @@ CHARTS = {
 class Service(UrlService):
     def __init__(self, configuration=None, name=None):
         UrlService.__init__(self, configuration=configuration, name=name)
-        self.url = self.configuration.get('url', None)
-        self.regex = re.compile(self.configuration.get('regex', '.*'))
+        self.regex = self._compile(self.configuration.get('regex'))
         self.status_codes_accepted = self.configuration.get('status_accepted', [200])
         self.follow_redirect = self.configuration.get('redirect', True)
         self.order = ORDER
         self.definitions = CHARTS
-        self._manager = None
 
-    def check(self):
-        """
-        Format configuration data and try to connect to server
-        :return: boolean
-        """
-        if not (self.url and isinstance(self.url, str)):
-            self.error('URL is not defined or type is not <str>')
-            return False
-
-        self._manager = self._build_manager()
-        if not self._manager:
-            return False
-
-        self.info('Enabled {url} with (redirect={redirect}, status={accepted}, interval={update}, timeout={timeout}, '
-                  'regex={regex})'
-                  .format(url=self.url, redirect=self.follow_redirect, accepted=self.status_codes_accepted,
-                          update=self.update_every, timeout=self.request_timeout, regex=self.regex.pattern))
-        return True
+    @staticmethod
+    def _compile(pattern=None):
+        return None if pattern is None else re.compile(pattern)
 
     def _get_data(self):
         """
@@ -97,28 +79,15 @@ class Service(UrlService):
         data[HTTP_NO_CONNECTION] = 0
         url = self.url
         try:
-            retr = 1 if self.follow_redirect else False
-            start = time.time()
-            response = self._manager.request(
-                method='GET', url=url, timeout=self.request_timeout, retries=retr, headers=self._manager.headers,
-                redirect=self.follow_redirect
-            )
-            content = response.data
-            diff = time.time() - start
+            start = time()
+            status, content = self._get_raw_data_with_status(retries=1 if self.follow_redirect else False,
+                                                             redirect=self.follow_redirect)
+            diff = time() - start
             data[HTTP_RESPONSE_TIME] = max(round(diff * 10000), 0)
-            data[HTTP_RESPONSE_LENGTH] = len(content)
             self.debug('Url: {url}. Host responded with status code {code} in {diff} s'.format(
-                url=url, code=response.status, diff=diff
+                url=url, code=status, diff=diff
             ))
-            self.debug('Content: \n\n{content}\n'.format(content=content))
-            if response.status in self.status_codes_accepted:
-                if self.regex.search(content) is None:
-                    self.debug('No match for regex \'{regex}\' found'.format(regex=self.regex.pattern))
-                    data[HTTP_BAD_CONTENT] = 1
-                else:
-                    data[HTTP_SUCCESS] = 1
-            else:
-                data[HTTP_BAD_STATUS] = 1
+            self.process_response(content, data, status)
 
         except urllib3.exceptions.NewConnectionError as error:
             self.debug("Connection failed: {url}. Error: {error}".format(url=url, error=error))
@@ -137,3 +106,15 @@ class Service(UrlService):
             return None
 
         return data
+
+    def process_response(self, content, data, status):
+        data[HTTP_RESPONSE_LENGTH] = len(content)
+        self.debug('Content: \n\n{content}\n'.format(content=content))
+        if status in self.status_codes_accepted:
+            if self.regex is not None and self.regex.search(content) is None:
+                self.debug("No match for regex '{regex}' found".format(regex=self.regex.pattern))
+                data[HTTP_BAD_CONTENT] = 1
+            else:
+                data[HTTP_SUCCESS] = 1
+        else:
+            data[HTTP_BAD_STATUS] = 1
