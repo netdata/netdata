cb869b29a6824a6183f16f7c6c9f9ceb28313e4d
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2018-05-26 19:57:56 +0300

    the missing variable definition of the last commit

--- makeself/jobs/99-makeself.install.sh
+++ makeself/jobs/99-makeself.install.sh
@@ -64,7 +64,7 @@ run mv "${NETDATA_INSTALL_PATH}/bin/netdata" \
 
 cat >"${NETDATA_INSTALL_PATH}/bin/netdata" <<EOF
 #!${NETDATA_INSTALL_PATH}/bin/bash
-export BASH_LOADABLES_PATH="${NETDATA_INSTALL_PATH}/lib/bash"
+export NETDATA_BASH_LOADABLES="DISABLE"
 export PATH="${NETDATA_INSTALL_PATH}/bin:\${PATH}"
 exec "${NETDATA_INSTALL_PATH}/bin/srv/netdata" "\${@}"
 EOF
