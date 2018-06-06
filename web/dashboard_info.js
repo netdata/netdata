949c7cb982529ef5c4666dd0abdb56af727216f6
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2018-06-06 10:46:44 -0400

    Add per-thread stats reporting for the unbound plugin.
    Each of the three regular metric sets queries, request list, and recursion
    timings) also have per-thread versions reported by Unbound if threading
    is enabled.  This commit adds an option (`per_thread`) to enable
    reporting this data in the unbound module.  It produces one chart
    per-thread per-set of metrics, all of which are grouped below the main
    charts by their metric set, and displayed at reduced height (because
    it's _a lot_ of charts on most systems, since having at least one thread
    per core is pretty normal for Unbound configurations).  Detection is
    done only at startup, during the check to see if the plugin will run or
    not, which greatly simplifies the data collection code.
    
    It also fixes a few typos in the Unbound documentation and config file,
    and removes some bogus family overrides in the plugin itself.

--- web/dashboard_info.js
+++ web/dashboard_info.js
@@ -2107,5 +2107,17 @@ netdataDashboard.context = {
         info: 'The number of items in each of the various caches.'
     }
 
+    'unbound.threads.queries': {
+        height: 0.2
+    }
+
+    'unbound.threads.reqlist': {
+        height: 0.2
+    }
+
+    'unbound.threads.recursion': {
+        height: 0.2
+    }
+
     // ------------------------------------------------------------------------
 };
