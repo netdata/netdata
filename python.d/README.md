a08ac5aacbfb516290adb8d2107ef2428009e3f9
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2018-06-08 14:25:12 -0400

    Add a python plugin for monitoring power supplies on Linux.
    This adds a python-based module for tracking statistics relating to
    Linux kernel power_supply class devices.  This allows tracking battery
    statistics on Linux systems, as well as (in theory) other energy storage
    devices that utilize the kernel's power_supply class.
    
    The primary purpose of this module is twofold:
    
    * To provide a way for battery powered IoT devices to easily alert about a
    low battery.
    * To provide a way for all battery powered devices to alert on some easy
    to monitor battery health conditions.
    
    It provides up to four charts, one which provides the remaining capacity
    as a percentage, and three others which report info about charge (in
    amp-hours), energy (in watt-hours), and volrage, each providing info
    about the current values, and possibly minimal and maximal values that
    can be used for computing battery life.
    
    Exact support provided by each individual device varies.  Almost all
    provide the percentage capacity, but beyond that they may or may not
    support any or all of the attributes needed for the other three charts
    (ACPI compliant systems for example support most of the charge related
    ones, and two of the voltage related values, but none of the energy
    related ones).
    
    Data collection is done by scanning entries in /sys/class/power_supply.
    One job must be created for each power supply to be monitored, and there
    is no autodetection (though the config includes an example that should
    work to monitor the main battery on most laptops).

--- python.d/README.md
+++ python.d/README.md
@@ -1039,6 +1039,74 @@ The module will not work If no configuration is given.
 
 ---
 
+# linux\_power\_supply
+
+This module monitors variosu metrics reported by power supply drivers
+on Linux.  This allows tracking and alerting on things like remaining
+battery capacity.
+
+Depending on the uderlying driver, it may provide the following charts
+and metrics:
+
+1. Capacity: The power supply capacity expressed as a percentage.
+  * capacity\_now
+
+2. Charge: The charge for the power supply, expressed as microamphours.
+  * charge\_full\_design
+  * charge\_full
+  * charge\_now
+  * charge\_empty
+  * charge\_empty\_design
+
+3. Energy: The energy for the power supply, expressed as microwatthours.
+  * energy\_full\_design
+  * energy\_full
+  * energy\_now
+  * energy\_empty
+  * energy\_empty\_design
+
+2. Voltage: The voltage for the power supply, expressed as microvolts.
+  * voltage\_max\_design
+  * voltage\_max
+  * voltage\_now
+  * voltage\_min
+  * voltage\_min\_design
+
+### configuration
+
+Sample:
+
+```yaml
+battery:
+  supply: 'BAT0'
+  charts: 'capacity charge energy voltage'
+```
+
+The `supply` key specifies the name of the power supply device to monitor.
+You can use `ls /sys/class/power_supply` to get a list of such devices
+on your system.
+
+The `charts` key is a space separated list of which charts to try
+to display.  It defaults to trying to display everything.
+
+### notes
+
+* Most drivers provide at least the first chart.  Battery powered ACPI
+compliant systems (like most laptops) provide all but the third, but do
+not provide all of the metrics for each chart.
+
+* Current, energy, and voltages are reported with a _very_ high precision
+by the power\_supply framework.  Usually, this is far higher than the
+actual hardware supports reporting, so expect to see changes in these
+charts jump instead of scaling smoothly.
+
+* If `max` or `full` attribute is defined by the driver, but not a
+corresponding `min or `empty` attribute, then netdata will still provide
+the corresponding `min` or `empty`, which will then always read as zero.
+This way, alerts which match on these will still work.
+
+---
+
 # litespeed
 
 Module monitor litespeed web server performance metrics.
---
+++ python.d/linux_power_supply.chart.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+# Description: Linux power_supply netdata python.d module
+# Author: Austin S. Hemmelgarn (Ferroin)
+
+import os
+
+from bases.FrameworkServices.SocketService import SimpleService
+
+# Everything except percentages is reported as µ units.
+PRECISION = 10 ** 6
+
+# A priority of 90000 places us next to the other PSU related stuff.
+PRIORITY=90000
+
+# We add our charts dynamically when we probe for the device attributes,
+# so these are empty by default.
+ORDER = []
+
+CHARTS = {}
+
+
+def _get_capacity_chart(syspath):
+    # Capacity is measured in percent.  We track one value.
+    options = [None, 'Capacity', '%', 'power_supply', 'power_supply.capacity', 'line']
+    lines = list()
+    attr_now = 'capacity'
+    if _get_sysfs_value(os.path.join(syspath, attr_now)) is not None:
+        lines.append([attr_now, attr_now, 'absolute', 1, 1])
+        return {'capacity': {'options': options, 'lines': lines}}, [attr_now]
+    else:
+        return None, None
+
+def _get_generic_chart(syspath, name, unit, maxname, minname):
+    # Used to generate charts for energy, charge, and voltage.
+    options = [None, name.title(), unit, 'power_supply', 'power_supply.{0}'.format(name), 'line']
+    lines = list()
+    attrlist = list()
+    attr_max_design = '{0}_{1}_design'.format(name, maxname)
+    attr_max = '{0}_{1}'.format(name, maxname)
+    attr_now = '{0}_now'.format(name)
+    attr_min = '{0}_{1}'.format(name, minname)
+    attr_min_design = '{0}_{1}_design'.format(name, minname)
+    if _get_sysfs_value(os.path.join(syspath, attr_now)) is not None:
+        lines.append([attr_now, attr_now, 'absolute', 1, PRECISION])
+        attrlist.append(attr_now)
+    else:
+        return None, None
+    if _get_sysfs_value(os.path.join(syspath, attr_max)) is not None:
+        lines.insert(0, [attr_max, attr_max, 'absolute', 1, PRECISION])
+        lines.append([attr_min, attr_min, 'absolute', 1, PRECISION])
+        attrlist.append(attr_max)
+        attrlist.append(attr_min)
+    elif _get_sysfs_value(os.path.join(syspath, attr_min)) is not None:
+        lines.append([attr_min, attr_min, 'absolute', 1, PRECISION])
+        attrlist.append(attr_min)
+    if _get_sysfs_value(os.path.join(syspath, attr_max_design)) is not None:
+        lines.insert(0, [attr_max_design, attr_max_design, 'absolute', 1, PRECISION])
+        lines.append([attr_min_design, attr_min_design, 'absolute', 1, PRECISION])
+        attrlist.append(attr_max_design)
+        attrlist.append(attr_min_design)
+    elif _get_sysfs_value(os.path.join(syspath, attr_min_design)) is not None:
+        lines.append([attr_min_design, attr_min_design, 'absolute', 1, PRECISION])
+        attrlist.append(attr_min_design)
+    return {name: {'options': options, 'lines': lines}}, attrlist
+
+def _get_charge_chart(syspath):
+    # Charge is measured in microamphours.  We track up to five
+    # attributes.
+    return _get_generic_chart(syspath, 'charge', 'µAh', 'full', 'empty')
+
+def _get_energy_chart(syspath):
+    # Energy is measured in microwatthours.  We track up to five
+    # attributes.
+    return _get_generic_chart(syspath, 'energy', 'µWh', 'full', 'empty')
+
+def _get_voltage_chart(syspath):
+    # Voltage is measured in microvolts. We track up to five attributes.
+    return _get_generic_chart(syspath, 'voltage', 'µV', 'min', 'max')
+
+# This is a list of functions for generating charts.  Used below to save
+# a bit of code (and to make it a bit easier to add new charts).
+GET_CHART = {
+    'capacity': _get_capacity_chart,
+    'charge': _get_charge_chart,
+    'energy': _get_energy_chart,
+    'voltage': _get_voltage_chart
+}
+
+# This opens the specified file and returns the value in it or None if
+# the file doesn't exist.
+def _get_sysfs_value(filepath):
+    try:
+        with open(filepath, 'r') as datasource:
+            return int(datasource.read())
+    except (OSError, IOError):
+        return None
+
+# Certain attributes (*_empty and *_empty_design) are implicitly zero
+# if not provided by the driver.  This function is used to fetch those
+# attributes.
+def _get_sysfs_value_or_zero(filepath):
+    result = _get_sysfs_value(filepath)
+    if result is None:
+        return 0
+    return result
+
+
+class Service(SimpleService):
+    def __init__(self, configuration=None, name=None):
+        SimpleService.__init__(self, configuration=configuration, name=name)
+        self.definitions = dict()
+        self.order = list()
+        self.attrlist = list()
+        self.supply = self.configuration.get('supply', None)
+        if self.supply is not None:
+            self.syspath = '/sys/class/power_supply/{0}'.format(self.supply)
+        self.types = self.configuration.get('charts', 'energy charge capacity voltage').split()
+
+    def check(self):
+        if self.supply is None:
+            self.error('No power supply specified for monitoring.')
+            return False
+        if not len(self.types) > 0:
+            self.error('No attributes requested for monitoring.')
+            return False
+        if os.access(self.syspath, os.R_OK):
+            chartset = set(GET_CHART.keys()).intersection(set(self.types))
+            if len(chartset) > 0:
+                charts = dict()
+                attrlist = list()
+                for item in chartset:
+                    chart, attrs = GET_CHART[item](self.syspath)
+                    if chart is not None:
+                        charts.update(chart)
+                        attrlist.extend(attrs)
+                if len(charts) == 0:
+                    self.error('No charts can be created.')
+                    return False
+                self.definitions.update(charts)
+                self.order.extend(sorted(charts.keys()))
+                self.attrlist.extend(attrlist)
+            else:
+                self.error('No valid attributes requested for monitoring.')
+                return False
+        else:
+            self.error('Unable to access {0}'.format(self.syspath))
+            return False
+        return True
+
+    def _get_data(self):
+        data = dict()
+        for attr in self.attrlist:
+            attrpath = os.path.join(self.syspath, attr)
+            if attr.endswith('_min') or attr.endswith('_min_design') or \
+               attr.endswith('_empty') or attr.endswith('_empty_design'):
+                data[attr] = _get_sysfs_value_or_zero(attrpath)
+            else:
+                data[attr] = _get_sysfs_value(attrpath)
+        return data
