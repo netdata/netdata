ae40e91ae38e9619bfecee8688f0a46a6bc2a14a
Julien Grossiord <julien.grossiord@islay.fr>
2018-08-24 18:47:30 +0200

    Added graphs in web_log to track the virtual hosts and the port number (http vs https)

--- python.d/web_log.chart.py
+++ python.d/web_log.chart.py
@@ -29,7 +29,7 @@ ORDER_APACHE_CACHE = ['apache_cache']
 
 ORDER_WEB = ['response_statuses', 'response_codes', 'bandwidth',
              'response_time', 'response_time_hist', 'response_time_upstream', 'response_time_upstream_hist',
-             'requests_per_url', 'requests_per_user_defined', 'http_method', 'http_version',
+             'requests_per_url', 'requests_per_user_defined', 'http_method', 'vhost', 'port', 'http_version',
              'requests_per_ipproto', 'clients', 'clients_all']
 
 ORDER_SQUID = ['squid_response_statuses', 'squid_response_codes', 'squid_detailed_response_codes',
@@ -128,6 +128,18 @@ CHARTS_WEB = {
                     'web_log.requests_per_user_defined', 'stacked'],
         'lines': [
             ['user_pattern_other', 'other', 'incremental', 1, 1]
+        ]},
+    'port': {
+        'options': [None, 'Requests Per Port', 'requests/s', 'port',
+                    'web_log.port', 'stacked'],
+        'lines': [
+            ['80', 'http', 'incremental', 1, 1],
+            ['443', 'https', 'incremental', 1, 1]
+        ]},
+    'vhost': {
+        'options': [None, 'Requests Per Vhost', 'requests/s', 'vhost',
+                    'web_log.vhost', 'stacked'],
+        'lines': [
         ]}
 }
 
@@ -328,7 +340,8 @@ class Web:
                      'resp_time_upstream_avg': 0, 'unique_cur_ipv4': 0, 'unique_cur_ipv6': 0, '2xx': 0,
                      '5xx': 0, '3xx': 0, '4xx': 0, '1xx': 0, '0xx': 0, 'unmatched': 0, 'req_ipv4': 0,
                      'req_ipv6': 0, 'unique_tot_ipv4': 0, 'unique_tot_ipv6': 0, 'successful_requests': 0,
-                     'redirects': 0, 'bad_requests': 0, 'server_errors': 0, 'other_requests': 0, 'GET': 0}
+                     'redirects': 0, 'bad_requests': 0, 'server_errors': 0, 'other_requests': 0, 'GET': 0,
+                     '80': 0, '443': 0}
 
     def __getattr__(self, item):
         return getattr(self.service, item)
@@ -704,6 +717,23 @@ class Web:
             if dim_id not in self.data:
                 self.charts['http_version'].add_dimension([dim_id,
                                                            match_dict['http_version'],
+                                                           'incremental'])
+                self.data[dim_id] = 0
+            self.data[dim_id] += 1
+        # requests per port number
+        if match_dict.get('port'):
+            if match_dict['port'] not in self.data:
+                self.charts['port'].add_dimension([match_dict['port'],
+                                                           match_dict['port'],
+                                                           'incremental'])
+                self.data[match_dict['port']] = 0
+            self.data[match_dict['port']] += 1
+        # requests per vhost
+        if match_dict.get('vhost'):
+            dim_id = match_dict['vhost'].replace('.', '_')
+            if dim_id not in self.data:
+                self.charts['vhost'].add_dimension([dim_id,
+                                                           match_dict['vhost'],
                                                            'incremental'])
                 self.data[dim_id] = 0
             self.data[dim_id] += 1
