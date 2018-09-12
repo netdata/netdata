9be34c604e0b03a1d3f91df3866d48b08fa60163
Paweł Krupa <pawel@krupa.net.pl>
2018-09-12 19:09:26 +0200

    CI builds in containers (#4174)
    * builds in containers
    
    * fix build problems

---
+++ .travis/containerized_build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+docker build -t dev-image -f .travis/images/Dockerfile.$1 .
+
+docker run -it -w /code dev-image ./netdata-installer.sh --dont-wait --dont-start-it --install /tmp
