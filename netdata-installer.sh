43138b02d95663b50cf48ae3174cdc594c1bea29
paulfantom <pawel@krupa.net.pl>
2018-09-12 20:21:52 +0200

    disable linters on files which should be soon deprecated

--- netdata-installer.sh
+++ netdata-installer.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-3.0+
+# shellcheck disable=SC1090,SC1091,SC1117,SC2002,SC2034,SC2044,SC2046,SC2086,SC2129,SC2162,SC2166,SC2181
 
 export PATH="${PATH}:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
 uniquepath() {
