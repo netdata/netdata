6a84af230ed7dd6086f39fb7601a54c460925ed6
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2018-09-26 14:28:45 -0400

    python.d/powerdns.chart.py PEP 8 cleanup
    Fixed formatting of container literals.

--- python.d/powerdns.chart.py
+++ python.d/powerdns.chart.py
@@ -21,7 +21,8 @@ CHARTS = {
             ['udp-answers', None, 'incremental'],
             ['tcp-queries', None, 'incremental'],
             ['tcp-answers', None, 'incremental']
-        ]},
+        ]
+    },
     'cache_usage': {
         'options': [None, 'PowerDNS Cache Usage', 'count', 'cache', 'powerdns.cache_usage', 'line'],
         'lines': [
@@ -29,7 +30,8 @@ CHARTS = {
             ['query-cache-miss', None, 'incremental'],
             ['packetcache-hit', 'packet-cache-hit', 'incremental'],
             ['packetcache-miss', 'packet-cache-miss', 'incremental']
-        ]},
+        ]
+    },
     'cache_size': {
         'options': [None, 'PowerDNS Cache Size', 'count', 'cache', 'powerdns.cache_size', 'line'],
         'lines': [
@@ -37,58 +39,64 @@ CHARTS = {
             ['packetcache-size', 'packet-cache-size', 'absolute'],
             ['key-cache-size', None, 'absolute'],
             ['meta-cache-size', None, 'absolute']
-        ]},
+        ]
+    },
     'latency': {
         'options': [None, 'PowerDNS Latency', 'microseconds', 'latency', 'powerdns.latency', 'line'],
         'lines': [
             ['latency', None, 'absolute']
-        ]}
-
+        ]
+    }
 }
 
 RECURSOR_ORDER = ['questions-in', 'questions-out', 'answer-times', 'timeouts', 'drops', 'cache_usage', 'cache_size']
 
 RECURSOR_CHARTS = {
     'questions-in': {
-        'options': [
-            None, 'PowerDNS Recursor Questions In', 'count', 'questions', 'powerdns_recursor.questions-in', 'line'],
+        'options': [None, 'PowerDNS Recursor Questions In', 'count', 'questions', 'powerdns_recursor.questions-in',
+                    'line'],
         'lines': [
             ['questions', None, 'incremental'],
             ['ipv6-questions', None, 'incremental'],
             ['tcp-questions', None, 'incremental']
-        ]},
+        ]
+    },
     'questions-out': {
-        'options': [
-            None, 'PowerDNS Recursor Questions Out', 'count', 'questions', 'powerdns_recursor.questions-out', 'line'],
+        'options': [None, 'PowerDNS Recursor Questions Out', 'count', 'questions', 'powerdns_recursor.questions-out',
+                    'line'],
         'lines': [
             ['all-outqueries', None, 'incremental'],
             ['ipv6-outqueries', None, 'incremental'],
             ['tcp-outqueries', None, 'incremental'],
             ['throttled-outqueries', None, 'incremental']
-        ]},
+        ]
+    },
     'answer-times': {
-        'options': [
-            None, 'PowerDNS Recursor Answer Times', 'count', 'performance', 'powerdns_recursor.answer-times', 'line'],
+        'options': [None, 'PowerDNS Recursor Answer Times', 'count', 'performance', 'powerdns_recursor.answer-times',
+                    'line'],
         'lines': [
             ['answers-slow', None, 'incremental'],
             ['answers0-1', None, 'incremental'],
             ['answers1-10', None, 'incremental'],
             ['answers10-100', None, 'incremental'],
             ['answers100-1000', None, 'incremental']
-        ]},
+        ]
+    },
     'timeouts': {
-        'options': [
-            None, 'PowerDNS Recursor Questions Time', 'count', 'performance', 'powerdns_recursor.timeouts', 'line'],
+        'options': [None, 'PowerDNS Recursor Questions Time', 'count', 'performance', 'powerdns_recursor.timeouts',
+                    'line'],
         'lines': [
             ['outgoing-timeouts', None, 'incremental'],
             ['outgoing4-timeouts', None, 'incremental'],
             ['outgoing6-timeouts', None, 'incremental']
-        ]},
+        ]
+    },
     'drops': {
         'options': [None, 'PowerDNS Recursor Drops', 'count', 'performance', 'powerdns_recursor.drops', 'line'],
         'lines': [
             ['over-capacity-drops', None, 'incremental']
-        ]},
+        ]
+    },
     'cache_usage': {
         'options': [None, 'PowerDNS Recursor Cache Usage', 'count', 'cache', 'powerdns_recursor.cache_usage', 'line'],
         'lines': [
@@ -96,14 +104,16 @@ RECURSOR_CHARTS = {
             ['cache-misses', None, 'incremental'],
             ['packetcache-hits', 'packet-cache-hit', 'incremental'],
             ['packetcache-misses', 'packet-cache-miss', 'incremental']
-        ]},
+        ]
+    },
     'cache_size': {
         'options': [None, 'PowerDNS Recursor Cache Size', 'count', 'cache', 'powerdns_recursor.cache_size', 'line'],
         'lines': [
             ['cache-entries', None, 'absolute'],
             ['packetcache-entries', None, 'absolute'],
             ['negcache-entries', None, 'absolute']
-        ]}
+        ]
+    }
 }
 
 
