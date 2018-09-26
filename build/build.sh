345e3902924b1a846ac02362ae137b76983c2c44
paulfantom <pawel@krupa.net.pl>
2018-09-26 21:05:03 +0200

    showcase package building

--- build/build.sh
+++ build/build.sh
@@ -1,22 +1,67 @@
 #!/bin/bash
 
+BUILDER_IMAGE="netdata-builder:v0.1"
+DIR=$(pwd)/pack
+VERSION="1.10_git"
+ITERATION="$(git rev-parse HEAD | cut -c1-8)"
+
 if [ -f build.sh ]; then
-    cd ../ || exit 1
+    exit 1
 fi
 
 if [ "$IS_CONTAINER" != "" ]; then
-  autoreconf -ivf
-  ./configure --enable-maintainer-mode
-  make dist
-  rm -rf autom4te.cache
+  case "$1" in
+    "prepare")
+      autoreconf -ivf
+      #./configure --enable-maintainer-mode
+      #./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --with-zlib --with-math --with-user=netdata CFLAGS=-O2
+      ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --with-zlib --with-math --with-user=root CFLAGS=-O2
+      ;;
+    "fpm")
+      make
+      make install DESTDIR="${DIR}"
+      fpm --output-type rpm \
+          --input-type dir \
+          --name netdata \
+          --license "GPL-3.0" \
+          --url "https://my-netdata.io" \
+          --maintainer "Netdata Team" \
+          --version "${VERSION}" \
+          --iteration "${ITERATION}" \
+          --package ./ \
+          --depends libcap \
+          --config-files /etc/netdata \
+          --chdir "${DIR}"
+      fpm --output-type deb \
+          --input-type dir \
+          --name netdata \
+          --license "GPL-3.0" \
+          --url "https://my-netdata.io" \
+          --maintainer "Netdata Team" \
+          --version "${VERSION}" \
+          --iteration "${ITERATION}" \
+          --package ./ \
+          --depends libcap \
+          --config-files /etc/netdata \
+          --chdir "${DIR}"
+      ;;
+    "tarball")
+      make dist
+      ;;
+    "clean") 
+      rm -rf autom4te.cache
+      git clean -X -f
+      ;;
+  esac
+
 else
-  if [[ "$(docker images -q netdata-gcc-builder:latest 2> /dev/null)" == "" ]]; then
-      docker build -t netdata-gcc-builder:latest -f build/Dockerfile .
+  if [[ "$(docker images -q $BUILDER_IMAGE 2> /dev/null)" == "" ]]; then
+      docker build -t $BUILDER_IMAGE -f build/Dockerfile .
   fi
   docker run --rm -it \
     --env IS_CONTAINER=TRUE \
     --volume "${PWD}:/project:Z" \
     --workdir "/project" \
-    netdata:gcc \
-    ./.travis/build.sh
+    "$BUILDER_IMAGE" \
+    ./build/build.sh "${@}"
 fi
