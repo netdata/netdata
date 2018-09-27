bcddc95306b43ab5a136391ae43503e07d11d838
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2018-09-27 08:56:26 -0400

    Python.d PEP 8 cleanup, modules S-Z (#4302)
    * python.d/samba.chart.py PEP 8 cleanup
    
    Fix formatting of container literals.
    
    * python.d/sensors.chart.py PEP 8 cleanup
    
    Fix formatting of container literals and quoting of string literals.
    
    * python.d/smartd_log.chart.py PEP 8 cleanup
    
    Fix formatting of container literals.
    
    * python.d/spigotmc.chart.py PEP 8 cleanup
    
    Fix spacing of code blocks.
    
    * python.d/springboot.chart.py PEP 8 cleanup
    
    Bunches of formatting fixes, plus some code correctness fixes.
    
    * python.d/squid.chart.py PEP 8 cleanup
    
    Fix formatting of container literals and quoting of string literals.
    
    * python.d/tomcat.chart.py PEP 8 cleanup
    
    Multiple formatting fixes.
    
    * python.d/traefik.chart.py PEP 8 cleanup
    
    Fixed long lines and formatting of container literals.
    
    * python.d/unbound.chart.py PEP 8 cleanup
    
    Fixed overly long lines.
    
    * python.d/varnish.chart.py PEP 8 cleanup
    
    Fixed formatting of container literals.
    
    * python.d/w1sensor.chart.py PEP 8 cleanup
    
    Fixed container literal formatting.
    
    * python.d/web_log.chart.py PEP 8 cleanup
    
    Multiple formatting fixes.
    
    * Fixed trailing whitespace.

--- python.d/springboot.chart.py
+++ python.d/springboot.chart.py
@@ -18,53 +18,60 @@ DEFAULT_CHARTS = {
     'response_code': {
         'options': [None, "Response Codes", "requests/s", "response", "springboot.response_code", "stacked"],
         'lines': [
-            ["resp_other",  'Other', 'incremental'],
-            ["resp_1xx",    '1xx',   'incremental'],
-            ["resp_2xx",    '2xx',   'incremental'],
-            ["resp_3xx",    '3xx',   'incremental'],
-            ["resp_4xx",    '4xx',   'incremental'],
-            ["resp_5xx",    '5xx',   'incremental'],
-        ]},
+            ["resp_other", 'Other', 'incremental'],
+            ["resp_1xx", '1xx', 'incremental'],
+            ["resp_2xx", '2xx', 'incremental'],
+            ["resp_3xx", '3xx', 'incremental'],
+            ["resp_4xx", '4xx', 'incremental'],
+            ["resp_5xx", '5xx', 'incremental'],
+        ]
+    },
     'threads': {
         'options': [None, "Threads", "current threads", "threads", "springboot.threads", "area"],
         'lines': [
             ["threads_daemon", 'daemon', 'absolute'],
             ["threads", 'total', 'absolute'],
-        ]},
+        ]
+    },
     'gc_time': {
         'options': [None, "GC Time", "milliseconds", "garbage collection", "springboot.gc_time", "stacked"],
         'lines': [
-            ["gc_copy_time",                'Copy',                 'incremental'],
-            ["gc_marksweepcompact_time",    'MarkSweepCompact',     'incremental'],
-            ["gc_parnew_time",              'ParNew',               'incremental'],
-            ["gc_concurrentmarksweep_time", 'ConcurrentMarkSweep',  'incremental'],
-            ["gc_ps_scavenge_time",         'PS Scavenge',          'incremental'],
-            ["gc_ps_marksweep_time",        'PS MarkSweep',         'incremental'],
-            ["gc_g1_young_generation_time", 'G1 Young Generation',  'incremental'],
-            ["gc_g1_old_generation_time",   'G1 Old Generation',    'incremental'],
-        ]},
+            ["gc_copy_time", 'Copy', 'incremental'],
+            ["gc_marksweepcompact_time", 'MarkSweepCompact', 'incremental'],
+            ["gc_parnew_time", 'ParNew', 'incremental'],
+            ["gc_concurrentmarksweep_time", 'ConcurrentMarkSweep', 'incremental'],
+            ["gc_ps_scavenge_time", 'PS Scavenge', 'incremental'],
+            ["gc_ps_marksweep_time", 'PS MarkSweep', 'incremental'],
+            ["gc_g1_young_generation_time", 'G1 Young Generation', 'incremental'],
+            ["gc_g1_old_generation_time", 'G1 Old Generation', 'incremental'],
+        ]
+    },
     'gc_ope': {
         'options': [None, "GC Operations", "operations/s", "garbage collection", "springboot.gc_ope", "stacked"],
         'lines': [
-            ["gc_copy_count",                'Copy',                 'incremental'],
-            ["gc_marksweepcompact_count",    'MarkSweepCompact',     'incremental'],
-            ["gc_parnew_count",              'ParNew',               'incremental'],
-            ["gc_concurrentmarksweep_count", 'ConcurrentMarkSweep',  'incremental'],
-            ["gc_ps_scavenge_count",         'PS Scavenge',          'incremental'],
-            ["gc_ps_marksweep_count",        'PS MarkSweep',         'incremental'],
-            ["gc_g1_young_generation_count", 'G1 Young Generation',  'incremental'],
-            ["gc_g1_old_generation_count",   'G1 Old Generation',    'incremental'],
-        ]},
+            ["gc_copy_count", 'Copy', 'incremental'],
+            ["gc_marksweepcompact_count", 'MarkSweepCompact', 'incremental'],
+            ["gc_parnew_count", 'ParNew', 'incremental'],
+            ["gc_concurrentmarksweep_count", 'ConcurrentMarkSweep', 'incremental'],
+            ["gc_ps_scavenge_count", 'PS Scavenge', 'incremental'],
+            ["gc_ps_marksweep_count", 'PS MarkSweep', 'incremental'],
+            ["gc_g1_young_generation_count", 'G1 Young Generation', 'incremental'],
+            ["gc_g1_old_generation_count", 'G1 Old Generation', 'incremental'],
+        ]
+    },
     'heap': {
         'options': [None, "Heap Memory Usage", "KB", "heap memory", "springboot.heap", "area"],
         'lines': [
             ["heap_committed", 'committed', "absolute"],
             ["heap_used", 'used', "absolute"],
-        ]},
+        ]
+    }
 }
+
 
 class ExtraChartError(ValueError):
     pass
+
 
 class Service(UrlService):
     def __init__(self, configuration=None, name=None):
@@ -88,11 +95,11 @@ class Service(UrlService):
             return None
 
         result = {
-            'resp_1xx':   0,
-            'resp_2xx':   0,
-            'resp_3xx':   0,
-            'resp_4xx':   0,
-            'resp_5xx':   0,
+            'resp_1xx': 0,
+            'resp_2xx': 0,
+            'resp_3xx': 0,
+            'resp_4xx': 0,
+            'resp_5xx': 0,
             'resp_other': 0,
         }
 
@@ -122,14 +129,14 @@ class Service(UrlService):
             self.order.append(extra['id'])
 
     def _add_extra_chart(self, chart):
-        chart_id  = chart.get('id',      None) or die('id is not defined in extra chart')
-        options   = chart.get('options', None) or die('option is not defined in extra chart: %s' % chart_id)
-        lines     = chart.get('lines',   None) or die('lines is not defined in extra chart: %s' % chart_id)
+        chart_id = chart.get('id', None) or self.die('id is not defined in extra chart')
+        options = chart.get('options', None) or self.die('option is not defined in extra chart: %s' % chart_id)
+        lines = chart.get('lines', None) or self.die('lines is not defined in extra chart: %s' % chart_id)
 
-        title     = options.get('title', None) or die('title is missing: %s' % chart_id)
-        units     = options.get('units', None) or die('units is missing: %s' % chart_id)
-        family    = options.get('family',    title)
-        context   = options.get('context',   'springboot.' + title)
+        title = options.get('title', None) or self.die('title is missing: %s' % chart_id)
+        units = options.get('units', None) or self.die('units is missing: %s' % chart_id)
+        family = options.get('family', title)
+        context = options.get('context', 'springboot.' + title)
         charttype = options.get('charttype', 'line')
 
         result = {
@@ -138,11 +145,11 @@ class Service(UrlService):
         }
 
         for line in lines:
-            dimension  = line.get('dimension',  None) or die('dimension is missing: %s' % chart_id)
-            name       = line.get('name',       dimension)
-            algorithm  = line.get('algorithm',  'absolute')
+            dimension = line.get('dimension',  None) or self.die('dimension is missing: %s' % chart_id)
+            name = line.get('name', dimension)
+            algorithm = line.get('algorithm', 'absolute')
             multiplier = line.get('multiplier', 1)
-            divisor    = line.get('divisor',    1)
+            divisor = line.get('divisor', 1)
             result['lines'].append([dimension, name, algorithm, multiplier, divisor])
 
         self.definitions[chart_id] = result
