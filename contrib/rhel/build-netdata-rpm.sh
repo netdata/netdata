a2bbb635e1a1f803e55278242783314b0a5f11b1
paulfantom <pawel@krupa.net.pl>
2018-10-17 00:58:14 +0200

    scripted rpm build; move spec file to contrib

--- contrib/rhel/build-netdata-rpm.sh
+++ contrib/rhel/build-netdata-rpm.sh
@@ -1,47 +1,62 @@
 #!/usr/bin/env bash
 
-# docker run -it --rm centos:6.9 /bin/sh
-# yum -y install rpm-build redhat-rpm-config yum-utils autoconf automake curl gcc git libmnl-devel libuuid-devel make pkgconfig zlib-devel
-
-cd "$(dirname "$0")/../../" || exit 1
-# shellcheck disable=SC1091
-source "installer/functions.sh" || exit 1
-
-set -e
-
-run autoreconf -ivf
-run ./configure --enable-maintainer-mode
-run make dist
-
-version=$(grep PACKAGE_VERSION < config.h | cut -d '"' -f 2)
-if [ -z "${version}" ]
-then
+build() {
+  yum -y install rpm-build redhat-rpm-config yum-utils autoconf automake curl gcc git libmnl-devel libuuid-devel make pkgconfig zlib-devel
+   
+  # shellcheck source=/dev/null
+  source "installer/functions.sh"
+  
+  set -e
+  autoreconf -ivf
+  ./configure --enable-maintainer-mode
+  make dist
+  
+  version=$(grep PACKAGE_VERSION < config.h | cut -d '"' -f 2)
+  if [ -z "${version}" ]; then
     echo >&2 "Cannot find netdata version."
     exit 1
+  fi
+  
+  tgz="netdata-${version}.tar.gz"
+  if [ ! -f "${tgz}" ]; then
+    echo >&2 "Cannot find the generated tar.gz file '${tgz}'"
+    exit 1
+  fi
+  
+  srpm=$(rpmbuild -ts "${tgz}" | cut -d ' ' -f 2)
+  if [ -z "${srpm}" ] || [ ! -f "${srpm}" ]; then
+    echo >&2 "Cannot find the generated SRPM file '${srpm}'"
+    exit 1
+  fi
+  
+  #if which yum-builddep 2>/dev/null
+  #then
+  #    run yum-builddep "${srpm}"
+  #elif which dnf 2>/dev/null
+  #then
+  #    [ "${UID}" = 0 ] && run dnf builddep "${srpm}"
+  #fi
+  
+  rpmbuild --rebuild "${srpm}"
+  
+  cp /root/rpmbuild/RPMS/x86_64/netdata-* ./
+  
+  echo >&2 "All done!"
+}
+
+VERSION="6.9"
+case "$1" in
+  "centos7") VERSION="7";;
+  "centos6") VERSION="6.9";;
+esac
+
+if [ "$IS_CONTAINER" != "" ]; then
+  build
+else
+  docker run --rm \
+    --env IS_CONTAINER=TRUE \
+    --volume "${PWD}:/code:Z" \
+    --workdir /code \
+    "centos:$VERSION" \
+    ./contrib/rhel/build-netdata-rpm.sh
 fi
-
-tgz="netdata-${version}.tar.gz"
-if [ ! -f "${tgz}" ]
-then
-	echo >&2 "Cannot find the generated tar.gz file '${tgz}'"
-	exit 1
-fi
-
-srpm=$(run rpmbuild -ts "${tgz}" | cut -d ' ' -f 2)
-if [ -z "${srpm}" ] || [ ! -f "${srpm}" ]
-then
-	echo >&2 "Cannot find the generated SRPM file '${srpm}'"
-	exit 1
-fi
-
-#if which yum-builddep 2>/dev/null
-#then
-#    run yum-builddep "${srpm}"
-#elif which dnf 2>/dev/null
-#then
-#    [ "${UID}" = 0 ] && run dnf builddep "${srpm}"
-#fi
-
-run rpmbuild --rebuild "${srpm}"
-
-echo >&2 "All done!"
