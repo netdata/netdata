b5a150f6728ee26d464215f431c58b11300301a9
George Moschovitis <george@netdata.cloud>
2018-10-25 19:12:09 +0300

    Extract units-conversion.js and sparkline.js

--- web/gui/build-dashboard-js.sh
+++ web/gui/build-dashboard-js.sh
@@ -5,6 +5,8 @@ cat src/prologue.js.inc \
     src/compatibility.js \
     src/xss.js \
     src/colors.js \
+    src/units-conversion.js \
+    src/sparkline.js \
     src/main.js \
     src/epilogue.js.inc \
     > dashboard.js
