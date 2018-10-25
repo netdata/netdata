6ceffa1d698aa3dbf2bc77599ec6ddfd00577985
George Moschovitis <george@netdata.cloud>
2018-10-25 20:33:56 +0300

    Extracted more charting libraries, cleanup

--- web/gui/build-dashboard-js.sh
+++ web/gui/build-dashboard-js.sh
@@ -6,8 +6,12 @@ cat src/prologue.js.inc \
     src/xss.js \
     src/colors.js \
     src/units-conversion.js \
+    src/dygraph.js \
     src/sparkline.js \
     src/google-charts.js \
+    src/gauge.js \
+    src/easy-pie-chart.js \
+    src/d3.js \
     src/main.js \
     src/epilogue.js.inc \
     > dashboard.js
