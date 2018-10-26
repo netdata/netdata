9940d67236a0e9b67acfd9f1262c15c6338a4d74
George Moschovitis <george@netdata.cloud>
2018-10-26 18:17:17 +0300

    Extracted server-detection.js

--- web/gui/src/dashboard.js/utils.js
+++ web/gui/src/dashboard.js/utils.js
@@ -418,30 +418,3 @@ NETDATA._scriptSource = function () {
 
     return script;
 };
-
-if (typeof netdataServer !== 'undefined') {
-    NETDATA.serverDefault = netdataServer;
-} else {
-    let s = NETDATA._scriptSource();
-    if (s) {
-        NETDATA.serverDefault = s.replace(/\/dashboard.js(\?.*)?$/g, "");
-    } else {
-        console.log('WARNING: Cannot detect the URL of the netdata server.');
-        NETDATA.serverDefault = null;
-    }
-}
-
-if (NETDATA.serverDefault === null) {
-    NETDATA.serverDefault = '';
-} else if (NETDATA.serverDefault.slice(-1) !== '/') {
-    NETDATA.serverDefault += '/';
-}
-
-if (typeof netdataServerStatic !== 'undefined' && netdataServerStatic !== null && netdataServerStatic !== '') {
-    NETDATA.serverStatic = netdataServerStatic;
-    if (NETDATA.serverStatic.slice(-1) !== '/') {
-        NETDATA.serverStatic += '/';
-    }
-} else {
-    NETDATA.serverStatic = NETDATA.serverDefault;
-}
