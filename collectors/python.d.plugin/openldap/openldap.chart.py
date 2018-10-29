8660896456c10ef8a3baab91cfe161823a0c36a5
Manos Kartsonakis <e.kartsonakis@telekom.com>
2018-10-29 09:42:20 +0200

    style fixes

--- collectors/python.d.plugin/openldap/openldap.chart.py
+++ collectors/python.d.plugin/openldap/openldap.chart.py
@@ -11,9 +11,8 @@ update_every = 10
 priority = 60000
 retries = 60
 
-
-
-ORDER = ['total_connections', 'bytes_sent', 'operations', 'referrals_sent', 'entries_sent', 'ldap_operations', 'waiters']
+ORDER = ['total_connections', 'bytes_sent', 'operations',
+         'referrals_sent', 'entries_sent', 'ldap_operations', 'waiters']
 
 CHARTS = {
     'total_connections': {
@@ -80,27 +79,27 @@ class Service(SimpleService):
 
         # Stuff to gather - make tuples of DN dn and attrib to get
         searchlist = {
-        'total_connections':('cn=Total,cn=Connections,cn=Monitor','monitorCounter'),
-        'bytes_sent': ('cn=Bytes,cn=Statistics,cn=Monitor','monitorCounter'),
-        'completed_operations': ('cn=Operations,cn=Monitor','monitorOpCompleted'),
-        'initiated_operations': ('cn=Operations,cn=Monitor','monitorOpInitiated'),
-        'referrals_sent': ('cn=Referrals,cn=Statistics,cn=Monitor','monitorCounter'),
-        'entries_sent': ('cn=Entries,cn=Statistics,cn=Monitor','monitorCounter'),
-        'bind_operations': ('cn=Bind,cn=Operations,cn=Monitor','monitorOpCompleted'),
-        'unbind_operations': ('cn=Unbind,cn=Operations,cn=Monitor','monitorOpCompleted'),
-        'add_operations': ('cn=Add,cn=Operations,cn=Monitor','monitorOpInitiated'),
-        'delete_operations':  ('cn=Delete,cn=Operations,cn=Monitor','monitorOpCompleted'),
-        'modify_operations': ('cn=Modify,cn=Operations,cn=Monitor','monitorOpCompleted'),
-        'compare_operations': ('cn=Compare,cn=Operations,cn=Monitor','monitorOpCompleted'),
-        'search_operations': ('cn=Search,cn=Operations,cn=Monitor','monitorOpCompleted'),
-        'write_waiters': ('cn=Write,cn=Waiters,cn=Monitor','monitorCounter'),
-        'read_waiters': ('cn=Read,cn=Waiters,cn=Monitor','monitorCounter'),
+        'total_connections':('cn=Total,cn=Connections,cn=Monitor', 'monitorCounter'),
+        'bytes_sent': ('cn=Bytes,cn=Statistics,cn=Monitor', 'monitorCounter'),
+        'completed_operations': ('cn=Operations,cn=Monitor', 'monitorOpCompleted'),
+        'initiated_operations': ('cn=Operations,cn=Monitor', 'monitorOpInitiated'),
+        'referrals_sent': ('cn=Referrals,cn=Statistics,cn=Monitor', 'monitorCounter'),
+        'entries_sent': ('cn=Entries,cn=Statistics,cn=Monitor', 'monitorCounter'),
+        'bind_operations': ('cn=Bind,cn=Operations,cn=Monitor', 'monitorOpCompleted'),
+        'unbind_operations': ('cn=Unbind,cn=Operations,cn=Monitor', 'monitorOpCompleted'),
+        'add_operations': ('cn=Add,cn=Operations,cn=Monitor', 'monitorOpInitiated'),
+        'delete_operations':  ('cn=Delete,cn=Operations,cn=Monitor', 'monitorOpCompleted'),
+        'modify_operations': ('cn=Modify,cn=Operations,cn=Monitor', 'monitorOpCompleted'),
+        'compare_operations': ('cn=Compare,cn=Operations,cn=Monitor', 'monitorOpCompleted'),
+        'search_operations': ('cn=Search,cn=Operations,cn=Monitor', 'monitorOpCompleted'),
+        'write_waiters': ('cn=Write,cn=Waiters,cn=Monitor', 'monitorCounter'),
+        'read_waiters': ('cn=Read,cn=Waiters,cn=Monitor', 'monitorCounter'),
         }
 
         for key in searchlist.keys():
             bind = searchlist[key][0]
             attr = searchlist[key][1]
-            num = self.conn.search(bind,ldap.SCOPE_BASE,'objectClass=*',[attr,])
+            num = self.conn.search(bind, ldap.SCOPE_BASE, 'objectClass=*', [attr,])
         
             try:
                 result_type, result_data = self.conn.result(num, 1)
