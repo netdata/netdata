cb72d25ebb28709442849f5a26c089da759089ef
cakrit <chris@netdata.cloud>
2018-10-31 20:50:19 +0100

    Updated comments

--- htmldoc/buildhtml.sh
+++ htmldoc/buildhtml.sh
@@ -6,7 +6,6 @@
 # Assumes that the script is executed from the root netdata folder, by calling htmldoc/buildhtml.sh
 
 # Copy all netdata .md files to htmldoc/src 
-
 rm -rf htmldoc/src
 find . -path htmldoc -prune -o -name "*.md" | cpio -pd htmldoc/src
 
