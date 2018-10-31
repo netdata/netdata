f3493bdf85ab9f86ddb5d533f7ad857883b658ca
cakrit <chris@netdata.cloud>
2018-10-31 20:50:19 +0100

    Updated comments

--- htmldoc/buildhtml.sh
+++ htmldoc/buildhtml.sh
@@ -5,7 +5,7 @@
 # Builds the html static site, using mkdocs
 # Assumes that the script is executed from the root netdata folder, by calling htmldoc/buildhtml.sh
 
-# Copy all # netdata .md files to source/# netdata 
+# Copy all netdata .md files to htmldoc/src 
 rm -rf htmldoc/src
 find . -path htmldoc -prune -o -name "*.md" | cpio -pd htmldoc/src
 
