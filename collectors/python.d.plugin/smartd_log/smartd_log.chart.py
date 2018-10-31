39b9ec8d33f10136e4e24b4935c7d6261eb77d5a
lgz <ilyamaschenko@gmail.com>
2018-10-31 10:02:53 +0900

    smartd_log: add __hash__ to BaseDisk

--- collectors/python.d.plugin/smartd_log/smartd_log.chart.py
+++ collectors/python.d.plugin/smartd_log/smartd_log.chart.py
@@ -433,7 +433,10 @@ class BaseDisk:
         return self.name == other
 
     def __ne__(self, other):
-        return not self.__eq__(other)
+        return not self == other
+
+    def __hash__(self):
+        return hash(repr(self))
 
     def parser(self, data):
         raise NotImplementedError
