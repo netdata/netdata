406e625ac41f9bf866f721f3c32ec5994b1e70fb
cakrit <chris@netdata.cloud>
2018-10-31 20:41:59 +0100

    Test 2

--- doc/src/example.chart.sh.md
+++
@@ -1,5 +0,0 @@
-# Sample charts.d collector
-
-This is just an example charts.d data collector.
-
-Its shell script code is [here](https://github.com/netdata/netdata/blob/master/charts.d/example.chart.sh).
--- htmldoc/buildhtml.sh
+++ htmldoc/buildhtml.sh
@@ -6,6 +6,7 @@
 # Assumes that the script is executed from the root netdata folder, by calling htmldoc/buildhtml.sh
 
 # Copy all netdata .md files to htmldoc/src 
+
 rm -rf htmldoc/src
 find . -path htmldoc -prune -o -name "*.md" | cpio -pd htmldoc/src
 
