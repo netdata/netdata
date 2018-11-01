9ba9c8c9bcf6c8e4e4b1413d1a1a6c93f44477de
Paweł Krupa <pawel@krupa.net.pl>
2018-11-01 18:03:47 +0100

    cleaner releaser.sh script (#4542)
    [netdata minor release] and [minor] (just to be sure)

---
+++ .travis/generate_changelog.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -f .gitignore ]
+then
+  echo "Run as ./travis/$(basename "$0") from top level directory of git repository"
+  exit 1
+fi
+
+ORGANIZATION=$(echo "$TRAVIS_REPO_SLUG" | awk -F '/' '{print $1}')
+PROJECT=$(echo "$TRAVIS_REPO_SLUG" | awk -F '/' '{print $2}')
+
+echo "--- Creating changelog ---"
+git checkout master
+git pull
+#docker run -it --rm -v "$(pwd)":/usr/local/src/your-app ferrarimarco/github-changelog-generator:1.14.3 \
+docker run -it -v "$(pwd)":/project markmandel/github-changelog-generator:latest \
+                                                        --user "${ORGANIZATION}" \
+                                                        --project "${PROJECT}" \
+                                                        --token "${GITHUB_TOKEN}" \
+                                                        --since-tag "v1.10.0" \
+                                                        --unreleased-label "**Next release**" \
+                                                        --no-compare-link \
+                                                        --exclude-labels duplicate,question,invalid,wontfix,discussion,documentation
+
+echo "--- Uploading changelog ---"
+git add CHANGELOG.md
+git commit -m '[ci skip] Automatic changelog update'
+git push "https://${GITHUB_TOKEN}:@${GIT_URL}"
