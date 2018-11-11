0d929c3959be603e84d8dcc637aa551e72e6c289
Manos Kartsonakis <e.kartsonakis@telekom.com>
2018-11-11 20:06:44 +0200

    format suggestions, added connect-reconnect methods and alive flag

--- collectors/python.d.plugin/openldap/openldap.chart.py
+++ collectors/python.d.plugin/openldap/openldap.chart.py
@@ -12,14 +12,18 @@ except ImportError:
 from bases.FrameworkServices.SimpleService import SimpleService
 
 # default module values (can be overridden per job in `config`)
-update_every = 10
+update_every = 1
 priority = 60000
 retries = 60
 
 ORDER = [
-    'total_connections', 'bytes_sent',
-    'operations', 'referrals_sent',
-    'entries_sent', 'ldap_operations', 'waiters'
+    'total_connections',
+    'bytes_sent',
+    'operations',
+    'referrals_sent',
+    'entries_sent',
+    'ldap_operations',
+    'waiters'
 ]
 
 CHARTS = {
@@ -27,73 +31,103 @@ CHARTS = {
      'total_connections': {
         'options': [None, 'Total Connections', 'connections', 'ldap', 'monitorCounter', 'line'],
         'lines': [
-            ['total_connections', None, 'incremental']
+            ['total_connections', 'connections', 'incremental']
         ]
     },
   
     'bytes_sent': {
         'options': [None, 'Bytes Statistics', 'Bytes', 'ldap', 'monitorCounter', 'line'],
         'lines': [
-            ['bytes_sent', None, 'incremental']
+            ['bytes_sent', 'sent', 'incremental', 1, 1024]
         ]
     },
     'operations': {
         'options': [None, 'Operations', 'ops', 'ldap', 'monitorCounter', 'line'],
         'lines': [
-            ['completed_operations', None, 'incremental'],
-            ['initiated_operations', None, 'incremental']
+            ['completed_operations', 'completed', 'incremental'],
+            ['initiated_operations', 'initiated', 'incremental']
         ]
     },
     'referrals_sent': {
         'options': [None, 'Referrals Statistics', 'referalls_sent', 'ldap', 'monitorCounter', 'line'],
         'lines': [
-            ['referrals_sent', None, 'incremental']
+            ['referrals_sent', 'sent', 'incremental']
         ]
     },
     'entries_sent': {
         'options': [None, 'Entries Statistics', 'entries_sent', 'ldap', 'monitorCounter', 'line'],
         'lines': [
-            ['entries_sent', None, 'incremental']
+            ['entries_sent', 'sent', 'incremental']
         ]
     },
     'ldap_operations': {
         'options': [None, 'Operations', 'bind_operations', 'ldap', 'monitorCounter', 'line'],
         'lines': [
-            ['bind_operations', None, 'incremental'],
-            ['search_operations', None, 'incremental'],
-            ['unbind_operations', None, 'incremental'],
-            ['add_operations', None, 'incremental'],
-            ['delete_operations', None, 'incremental'],
-            ['modify_operations', None, 'incremental'],
-            ['compare_operations', None, 'incremental']
+            ['bind_operations', 'bind', 'incremental'],
+            ['search_operations', 'search', 'incremental'],
+            ['unbind_operations', 'unbind', 'incremental'],
+            ['add_operations', 'add', 'incremental'],
+            ['delete_operations', 'delete', 'incremental'],
+            ['modify_operations', 'modify', 'incremental'],
+            ['compare_operations', 'compare', 'incremental']
         ]
     },
     'waiters': {
         'options': [None, 'Waiters', 'amount', 'ldap', 'monitorCounter', 'line'],
         'lines': [
-            ['write_waiters', None, 'incremental'],
-            ['read_waiters', None, 'incremental']
+            ['write_waiters', 'write', 'incremental'],
+            ['read_waiters', 'read', 'incremental']
         ]
     },
 }
 
 # Stuff to gather - make tuples of DN dn and attrib to get
-searchlist = {
-    'total_connections':('cn=Total,cn=Connections,cn=Monitor', 'monitorCounter'),
-    'bytes_sent': ('cn=Bytes,cn=Statistics,cn=Monitor', 'monitorCounter'),
-    'completed_operations': ('cn=Operations,cn=Monitor', 'monitorOpCompleted'),
-    'initiated_operations': ('cn=Operations,cn=Monitor', 'monitorOpInitiated'),
-    'referrals_sent': ('cn=Referrals,cn=Statistics,cn=Monitor', 'monitorCounter'),
-    'entries_sent': ('cn=Entries,cn=Statistics,cn=Monitor', 'monitorCounter'),
-    'bind_operations': ('cn=Bind,cn=Operations,cn=Monitor', 'monitorOpCompleted'),
-    'unbind_operations': ('cn=Unbind,cn=Operations,cn=Monitor', 'monitorOpCompleted'),
-    'add_operations': ('cn=Add,cn=Operations,cn=Monitor', 'monitorOpInitiated'),
-    'delete_operations':  ('cn=Delete,cn=Operations,cn=Monitor', 'monitorOpCompleted'),
-    'modify_operations': ('cn=Modify,cn=Operations,cn=Monitor', 'monitorOpCompleted'),
-    'compare_operations': ('cn=Compare,cn=Operations,cn=Monitor', 'monitorOpCompleted'),
-    'search_operations': ('cn=Search,cn=Operations,cn=Monitor', 'monitorOpCompleted'),
-    'write_waiters': ('cn=Write,cn=Waiters,cn=Monitor', 'monitorCounter'),
-    'read_waiters': ('cn=Read,cn=Waiters,cn=Monitor', 'monitorCounter'),
+SEARCH_LIST = {
+    'total_connections': (
+        'cn=Total,cn=Connections,cn=Monitor', 'monitorCounter',
+    ),
+    'bytes_sent': (
+        'cn=Bytes,cn=Statistics,cn=Monitor', 'monitorCounter',
+    ),
+    'completed_operations': (
+        'cn=Operations,cn=Monitor', 'monitorOpCompleted',
+    ),
+    'initiated_operations': (
+        'cn=Operations,cn=Monitor', 'monitorOpInitiated',
+    ),
+    'referrals_sent': (
+        'cn=Referrals,cn=Statistics,cn=Monitor', 'monitorCounter',
+    ),
+    'entries_sent': (
+        'cn=Entries,cn=Statistics,cn=Monitor', 'monitorCounter',
+    ),
+    'bind_operations': (
+        'cn=Bind,cn=Operations,cn=Monitor', 'monitorOpCompleted',
+    ),
+    'unbind_operations': (
+        'cn=Unbind,cn=Operations,cn=Monitor', 'monitorOpCompleted',
+    ),
+    'add_operations': (
+        'cn=Add,cn=Operations,cn=Monitor', 'monitorOpInitiated',
+    ),
+    'delete_operations':  (
+        'cn=Delete,cn=Operations,cn=Monitor', 'monitorOpCompleted',
+    ),
+    'modify_operations': (
+        'cn=Modify,cn=Operations,cn=Monitor', 'monitorOpCompleted',
+    ),
+    'compare_operations': (
+        'cn=Compare,cn=Operations,cn=Monitor', 'monitorOpCompleted',
+    ),
+    'search_operations': (
+        'cn=Search,cn=Operations,cn=Monitor', 'monitorOpCompleted',
+    ),
+    'write_waiters': (
+        'cn=Write,cn=Waiters,cn=Monitor', 'monitorCounter',
+    ),
+    'read_waiters': (
+        'cn=Read,cn=Waiters,cn=Monitor', 'monitorCounter',
+    ),
 }
 
 class Service(SimpleService):
@@ -106,40 +140,64 @@ class Service(SimpleService):
         self.port = configuration.get('port')
         self.username = configuration.get('username')
         self.password = configuration.get('password')
+
+        self.alive = False
+        self.conn = None
+
+
+    def disconnect(self):
+        if self.conn:
+            self.conn.unbind()
+            self.conn = None
+            self.alive = False
         
+
     def connect(self):
-        try: 
-            print self.server
+        try:
             self.conn = ldap.initialize('ldap://%s:%s' % ( self.server, self.port))
             self.conn.simple_bind(self.username, self.password)
-            return True
-        except:
+        except ldap.LDAPError as error:
+            self.error(error)
             return False
+
+        self.alive = True
+        return True
+
+
+    def reconnect(self):
+        self.disconnect()
+        return self.connect()
+
 
     def check(self):
         if not HAS_LDAP: 
-            self.error('\'ldap\' python module is needed to use openldap.chart.py') 
-            return False
+            self.error("'python-ldap' package is needed") 
+            return None
 
-        self.connect()
-        if self.conn:
-            return True
-        else:
-            self.error('Could not establish ldap connection. Check your openldap.conf')
-            return False
+        return self.connect() and self.get_data()
+
 
     def get_data(self):
+        if not self.alive and not self.reconnect():
+            return None
+
         data = dict()
-        for key in searchlist.keys():
-            bind = searchlist[key][0]
-            attr = searchlist[key][1]
-            num = self.conn.search(bind, ldap.SCOPE_BASE, 'objectClass=*', [attr,])
+        for key in SEARCH_LIST.keys():
+            bind = SEARCH_LIST[key][0]
+            attr = SEARCH_LIST[key][1]
             try:
+                num = self.conn.search(bind, ldap.SCOPE_BASE, 'objectClass=*', [attr,])
                 result_type, result_data = self.conn.result(num, 1)
+            except ldap.LDAPError as error:
+                self.error(error)
+
+            try:
                 if result_type == 101:
                     val = int(result_data[0][1].values()[0][0])
-                    data[key] = val
-            except Exception as e:
-                return repr(e)
+            except ValueError:
+                val = 'NaN'
+
+            data[key] = val
+        
         return data
 
