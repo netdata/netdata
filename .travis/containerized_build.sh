b9b61d0e1a6f44b235af81f4540f398f0309fed9
paulfantom <pawel@krupa.net.pl>
2018-11-25 00:27:15 +0100

    move build Dockerfiles to external repo

--- .travis/containerized_build.sh
+++ .travis/containerized_build.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-docker build -t dev-image -f ".travis/images/Dockerfile.$1" .
-
-docker run -it -w /code dev-image ./netdata-installer.sh --dont-wait --dont-start-it --install /tmp
+docker run -it -w /code "netdata/os-test:$1" ./netdata-installer.sh --dont-wait --dont-start-it --install /tmp
