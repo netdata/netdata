8d500b92ec547bbfcf4c5f7b5e78ad8d84884a7c
paulfantom <pawel@krupa.net.pl>
2018-11-26 12:40:21 +0100

    mount volume for containerized build tests

--- .travis/containerized_build.sh
+++ .travis/containerized_build.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-docker run -it -w /code "netdata/os-test:$1" ./netdata-installer.sh --dont-wait --dont-start-it --install /tmp
+if [ ! -f .gitignore ]
+then
+  echo "Run as ./travis/$(basename "$0") from top level directory of git repository"
+  exit 1
+fi
+
+docker run -it -v "${PWD}:/code:rw" -w /code "netdata/os-test:$1" ./netdata-installer.sh --dont-wait --dont-start-it --install /tmp
