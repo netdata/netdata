075b5ae09fa5d124e077519896479f0321c74b6c
Vladimir Kobal <vlad@prokk.net>
2018-12-02 20:09:08 +0200

    Disable python module, minor cleanup

--- collectors/proc.plugin/proc_mdstat.c
+++ collectors/proc.plugin/proc_mdstat.c
@@ -42,7 +42,7 @@ struct raid {
 
 static inline char *remove_trailing_chars(char *s, char c) {
     while(*s) {
-        if(*s == c) {
+        if(unlikely(*s == c)) {
             *s = '\0';
         }
         s++;
@@ -128,11 +128,11 @@ int do_proc_mdstat(int update_every, usec_t dt) {
             }
             str_inuse = ++s;
             while(*s) {
-                if(*s == '/') {
+                if(unlikely(*s == '/')) {
                     *s = '\0';
                     str_total = s + 1;
                 }
-                else if(*s == ']') {
+                else if(unlikely(*s == ']')) {
                     *s = '\0';
                     break;
                 }
@@ -153,11 +153,11 @@ int do_proc_mdstat(int update_every, usec_t dt) {
             // check if any operation is performed on the raid
             words = procfile_linewords(ff, l + 2);
             if(likely(words < 2)) continue;
-            if (unlikely(words < 7)) {
+            if(unlikely(words < 7)) {
                 error("Cannot read /proc/mdstat line. Expected 7 params, read %zu.", words);
                 continue;
             }
-            if(procfile_lineword(ff, l + 2, 0)[0] != '[') continue;
+            if(unlikely(procfile_lineword(ff, l + 2, 0)[0] != '[')) continue;
             char *word;
 
             word = procfile_lineword(ff, l + 2, 3);
@@ -186,7 +186,7 @@ int do_proc_mdstat(int update_every, usec_t dt) {
 
             word += 7; // skip leading "finish="
 
-            if(s > word)
+            if(likely(s > word))
                 raid->finish_in = (unsigned long long)(str2ld(word, NULL) * 60);
 
             word = procfile_lineword(ff, l + 2, 6);
@@ -194,7 +194,7 @@ int do_proc_mdstat(int update_every, usec_t dt) {
 
             word += 6; // skip leading "speed="
 
-            if(s > word)
+            if(likely(s > word))
                 raid->speed = str2ull(word);
         }
 
@@ -204,7 +204,7 @@ int do_proc_mdstat(int update_every, usec_t dt) {
 
     // read mismatch_cnt files
 
-    if(!mismatch_cnt_filename) {
+    if(unlikely(!mismatch_cnt_filename)) {
         char filename[FILENAME_MAX + 1];
 
         snprintfz(filename, FILENAME_MAX, "%s%s", netdata_configured_host_prefix, "/sys/block/%s/md/mismatch_cnt");
@@ -216,11 +216,11 @@ int do_proc_mdstat(int update_every, usec_t dt) {
         char filename[FILENAME_MAX + 1];
         struct raid *raid = &raids[raid_idx];
 
-        if(raid->used) {
+        if(likely(raid->used)) {
             snprintfz(filename, FILENAME_MAX, mismatch_cnt_filename, raid->name);
             stat(mdstat_filename, &stbuf);
             if(unlikely(stbuf.st_mtim.tv_sec != raid->mismatch_modification_time.tv_sec || stbuf.st_mtim.tv_nsec != raid->mismatch_modification_time.tv_nsec)) {
-                if(read_single_number_file(filename, &raid->mismatch_cnt) == -1) {
+                if(unlikely(read_single_number_file(filename, &raid->mismatch_cnt) == -1)) {
                     error("Cannot read file '%s'", filename);
                     return 1;
                 }
@@ -236,7 +236,7 @@ int do_proc_mdstat(int update_every, usec_t dt) {
     if(unlikely(!st_mdstat_health))
         st_mdstat_health = rrdset_create_localhost(
                 "mdstat"
-                , "new_mdstat_health" // TODO: rename chart
+                , "mdstat_health"
                 , NULL
                 , "health"
                 , "md.health"
@@ -244,7 +244,7 @@ int do_proc_mdstat(int update_every, usec_t dt) {
                 , "failed disks"
                 , PLUGIN_PROC_NAME
                 , PLUGIN_PROC_MODULE_MDSTAT_NAME
-                , 6500 // TODO: define NETDATA_CHART_PRIO_MDSTAT_HEALTH
+                , NETDATA_CHART_PRIO_MDSTAT_HEALTH
                 , update_every
                 , RRDSET_TYPE_LINE
         );
@@ -254,7 +254,7 @@ int do_proc_mdstat(int update_every, usec_t dt) {
     for(raid_idx = 0; raid_idx < raids_num; raid_idx++) {
         struct raid *raid = &raids[raid_idx];
 
-        if(raid->used) {
+        if(likely(raid->used)) {
             if(unlikely(!raid->rd_health && !(raid->rd_health = rrddim_find(st_mdstat_health, raid->name))))
                     raid->rd_health = rrddim_add(st_mdstat_health, raid->name, NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
 
@@ -271,8 +271,8 @@ int do_proc_mdstat(int update_every, usec_t dt) {
         char id[50 + 1];
         char family[50 + 1];
 
-        if(raid->used) {
-            snprintfz(id, 50, "new_%s_disks", raid->name); // TODO: rename chart
+        if(likely(raid->used)) {
+            snprintfz(id, 50, "%s_disks", raid->name);
 
             if(unlikely(!raid->st_disks && !(raid->st_disks = rrdset_find_byname_localhost(id)))) {
                 snprintfz(family, 50, "%s", raid->name);
@@ -282,11 +282,11 @@ int do_proc_mdstat(int update_every, usec_t dt) {
                         , NULL
                         , family
                         , "md.disks"
-                        , "Disk Stats"
+                        , "Disks Stats"
                         , "disks"
                         , PLUGIN_PROC_NAME
                         , PLUGIN_PROC_MODULE_MDSTAT_NAME
-                        , 6500 + raid_idx // TODO: define NETDATA_CHART_PRIO_MDSTAT_RAID
+                        , NETDATA_CHART_PRIO_MDSTAT_DISKS + raid_idx * 10
                         , update_every
                         , RRDSET_TYPE_STACKED
                 );
@@ -306,7 +306,7 @@ int do_proc_mdstat(int update_every, usec_t dt) {
 
             // --------------------------------------------------------------------
 
-            snprintfz(id, 50, "new_%s_mismatch", raid->name); // TODO: rename chart
+            snprintfz(id, 50, "%s_mismatch", raid->name);
 
             if(unlikely(!raid->st_mismatch_cnt && !(raid->st_mismatch_cnt = rrdset_find_byname_localhost(id)))) {
                 snprintfz(family, 50, "%s", raid->name);
@@ -321,7 +321,7 @@ int do_proc_mdstat(int update_every, usec_t dt) {
                         , "unsynchronized blocks"
                         , PLUGIN_PROC_NAME
                         , PLUGIN_PROC_MODULE_MDSTAT_NAME
-                        , 6500 + raid_idx + 1 // TODO: define NETDATA_CHART_PRIO_MDSTAT_RAID
+                        , NETDATA_CHART_PRIO_MDSTAT_MISMATCH + raid_idx * 10
                         , update_every
                         , RRDSET_TYPE_LINE
                 );
@@ -338,7 +338,7 @@ int do_proc_mdstat(int update_every, usec_t dt) {
 
             // --------------------------------------------------------------------
 
-            snprintfz(id, 50, "new_%s_operation", raid->name); // TODO: rename chart
+            snprintfz(id, 50, "%s_operation", raid->name);
 
             if(unlikely(!raid->st_operation && !(raid->st_operation = rrdset_find_byname_localhost(id)))) {
                 snprintfz(family, 50, "%s", raid->name);
@@ -348,12 +348,12 @@ int do_proc_mdstat(int update_every, usec_t dt) {
                         , id
                         , NULL
                         , family
-                        , "ms.status"
+                        , "md.status"
                         , "Current Status"
                         , "percent"
                         , PLUGIN_PROC_NAME
                         , PLUGIN_PROC_MODULE_MDSTAT_NAME
-                        , 6500 + raid_idx + 2 // TODO: define NETDATA_CHART_PRIO_MDSTAT_RAID
+                        , NETDATA_CHART_PRIO_MDSTAT_OPERATION + raid_idx * 10
                         , update_every
                         , RRDSET_TYPE_LINE
                 );
@@ -379,7 +379,7 @@ int do_proc_mdstat(int update_every, usec_t dt) {
 
             // --------------------------------------------------------------------
 
-            snprintfz(id, 50, "new_%s_finish", raid->name); // TODO: rename chart
+            snprintfz(id, 50, "%s_finish", raid->name);
 
             if(unlikely(!raid->st_finish && !(raid->st_finish = rrdset_find_byname_localhost(id)))) {
                 snprintfz(family, 50, "%s", raid->name);
@@ -394,7 +394,7 @@ int do_proc_mdstat(int update_every, usec_t dt) {
                         , "seconds"
                         , PLUGIN_PROC_NAME
                         , PLUGIN_PROC_MODULE_MDSTAT_NAME
-                        , 6500 + raid_idx + 3 // TODO: define NETDATA_CHART_PRIO_MDSTAT_RAID
+                        , NETDATA_CHART_PRIO_MDSTAT_FINISH + raid_idx * 10
                         , update_every
                         , RRDSET_TYPE_LINE
                 );
@@ -411,7 +411,7 @@ int do_proc_mdstat(int update_every, usec_t dt) {
 
             // --------------------------------------------------------------------
 
-            snprintfz(id, 50, "new_%s_speed", raid->name); // TODO: rename chart
+            snprintfz(id, 50, "%s_speed", raid->name);
 
             if(unlikely(!raid->st_speed && !(raid->st_speed = rrdset_find_byname_localhost(id)))) {
                 snprintfz(family, 50, "%s", raid->name);
@@ -426,7 +426,7 @@ int do_proc_mdstat(int update_every, usec_t dt) {
                         , "KB/s"
                         , PLUGIN_PROC_NAME
                         , PLUGIN_PROC_MODULE_MDSTAT_NAME
-                        , 6500 + raid_idx + 4 // TODO: define NETDATA_CHART_PRIO_MDSTAT_RAID
+                        , NETDATA_CHART_PRIO_MDSTAT_SPEED + raid_idx * 10
                         , update_every
                         , RRDSET_TYPE_LINE
                 );
