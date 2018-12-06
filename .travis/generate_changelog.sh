14688c0b9045513a491c4333f5bd93ae973da2f6
Paweł Krupa <pawel@krupa.net.pl>
2018-12-06 10:22:37 +0100

    run shfmt on CI scripts (#4928)
    [netdata release candidate]

--- .travis/generate_changelog.sh
+++ .travis/generate_changelog.sh
@@ -2,10 +2,9 @@
 
 set -e
 
-if [ ! -f .gitignore ]
-then
-  echo "Run as ./travis/$(basename "$0") from top level directory of git repository"
-  exit 1
+if [ ! -f .gitignore ]; then
+	echo "Run as ./travis/$(basename "$0") from top level directory of git repository"
+	exit 1
 fi
 
 COMMIT_AND_PUSH=${COMMIT_AND_PUSH:-0}
@@ -29,13 +28,13 @@ git checkout master
 git pull
 #docker run -it --rm -v "$(pwd)":/usr/local/src/your-app ferrarimarco/github-changelog-generator:1.14.3 \
 docker run -it -v "$(pwd)":/project markmandel/github-changelog-generator:latest \
-                                                        --user "${ORGANIZATION}" \
-                                                        --project "${PROJECT}" \
-                                                        --token "${GITHUB_TOKEN}" \
-                                                        --since-tag "v1.10.0" \
-                                                        --unreleased-label "**Next release**" \
-                                                        --exclude-labels "stale,duplicate,question,invalid,wontfix,discussion,no changelog" \
-                                                        --no-compare-link ${OPTS}
+	--user "${ORGANIZATION}" \
+	--project "${PROJECT}" \
+	--token "${GITHUB_TOKEN}" \
+	--since-tag "v1.10.0" \
+	--unreleased-label "**Next release**" \
+	--exclude-labels "stale,duplicate,question,invalid,wontfix,discussion,no changelog" \
+	--no-compare-link ${OPTS}
 
 echo "--- Uploading changelog ---"
 git add CHANGELOG.md
