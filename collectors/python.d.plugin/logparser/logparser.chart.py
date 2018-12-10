8d384506e81ac029518326ddaae37d61860acce1
Hamed Beiranvand <hamedbrd@gmail.com>
2018-12-10 15:38:54 +0330

    parse value and use appropriate matcher for incoming data

--- collectors/python.d.plugin/logparser/logparser.chart.py
+++ collectors/python.d.plugin/logparser/logparser.chart.py
@@ -3,7 +3,7 @@
 # Author: Hamed Beiranvand (hamedbrd)
 
 import re
-
+import sys
 from bases.FrameworkServices.LogService import LogService
 
 
@@ -16,6 +16,9 @@ CHARTS = {
         ]
     }
 }
+
+METHOD_REGEX = "regex"
+METHOD_STRING = "string"
 
 
 class Service(LogService):
@@ -39,14 +42,44 @@ class Service(LogService):
             return False
         return True
 
-    def get_data(self):
+    def get_data(self):      
         raw = self._get_raw_data()
         if not raw:
             return None if raw is None else self.data
-        for item in self.dimensions:
 
-            for line in raw:
-                regex = re.compile(self.dimensions[item])
-                self.data[item] += bool(regex.search(line))
+        for item in self.dimensions:
+            item_val = self.dimensions[item]
+            method, value = item_val.split("=")
+
+            if method == METHOD_REGEX:
+                 self.data[item] = self.regex_matcher_factory(value,item,raw)
+
+            if method == METHOD_STRING:
+                 self.data[item] = self.string_matcher_factory(value,item,raw)
 
         return self.data
+            
+        
+    def regex_matcher_factory(self,value,item,data):
+        count = 0
+        for line in data:
+            if value.startswith('^'):
+                    regex = re.compile(value)
+                    count += bool(regex.match(line))
+            else:
+                    regex = re.compile(value)
+                    count += bool(regex.search(line))
+        return count
+    
+    def string_matcher_factory(self,value,item,data):
+         count = 0
+         for line in data:
+            if value.startswith("^"):
+                count += regex.startswith(line)
+            elif value.endswith('$'):
+                count += regex.endswith(line)
+            else:
+                regex = re.compile(value)
+                count += bool(regex.search(line))
+
+         return count
