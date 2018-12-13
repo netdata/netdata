2e6d26bafd5ce00b47d098a045a1d9c4f2d84936
Vladimir Kobal <vlad@prokk.net>
2018-12-13 18:48:26 +0200

    Move power supply python module to proc plugin (#4960)
    * Add power supply module
    
    * Use linked list
    
    * Add charts for all properties
    
    * Organize charts menu
    
    * Fix line endings
    
    * Keep files open
    
    * Add options for chart disabling
    
    * Final cleanup
    
    * Add documentation, disable python module
    
    * Fix file attributes
    
    * Mark python module as obsolete
    
    * Allow symbolic links to power source directories

--- collectors/proc.plugin/plugin_proc.h
+++ collectors/proc.plugin/plugin_proc.h
@@ -53,6 +53,7 @@ extern int do_proc_net_sockstat(int update_every, usec_t dt);
 extern int do_proc_net_sockstat6(int update_every, usec_t dt);
 extern int do_proc_net_sctp_snmp(int update_every, usec_t dt);
 extern int do_ipc(int update_every, usec_t dt);
+extern int do_sys_class_power_supply(int update_every, usec_t dt);
 extern int get_numa_node_count(void);
 
 // metrics that need to be shared among data collectors
