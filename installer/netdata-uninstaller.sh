889734018bc70cc81c18102742eb33e4f16f6ea3
Paweł Krupa <pawel@krupa.net.pl>
2018-12-18 14:02:34 +0100

    Non-interactive uninstaller (#5021)

--- installer/netdata-uninstaller.sh
+++ installer/netdata-uninstaller.sh
@@ -7,9 +7,42 @@
 #  - NETDATA_PREFIX
 #  - NETDATA_ADDED_TO_GROUPS
 
-if [ "$1" != "--force" ]; then
+usage="$(basename "$0") [-h] [-f ] -- program to calculate the answer to life, the universe and everything
+
+where:
+    -h           show this help text
+    -f, --force  force uninstallation and do not ask any questions
+    -y, --yes    flag needs to be set to proceed with uninstallation"
+
+RM_FLAGS="-i"
+INTERACTIVE=1
+YES=0
+while :; do
+	case "$1" in
+	-h | --help)
+		echo "$usage" >&2
+		exit 1
+		;;
+	-f | --force)
+		RM_FLAGS="-f"
+		INTERACTIVE=0
+		shift
+		;;
+	-y | --yes)
+		YES=1
+		shift
+		;;
+	-*)
+		echo "$usage" >&2
+		exit 1
+		;;
+	*) break ;;
+	esac
+done
+
+if [ "$YES" != "1" ]; then
 	echo >&2 "This script will REMOVE netdata from your system."
-	echo >&2 "Run it again with --force to do it."
+	echo >&2 "Run it again with --yes to do it."
 	exit 1
 fi
 
@@ -24,53 +57,62 @@ sleep 2
 
 if [ ! -z "${NETDATA_PREFIX}" ] && [ -d "${NETDATA_PREFIX}" ]; then
 	# installation prefix was given
-
-	portable_deletedir_recursively_interactively "${NETDATA_PREFIX}"
-
+	if [ $INTERACTIVE -eq 1 ]; then
+		portable_deletedir_recursively_interactively "${NETDATA_PREFIX}"
+	else
+		run rm -f -R "${NETDATA_PREFIX}"
+	fi
 else
 	# installation prefix was NOT given
 
 	if [ -f "${NETDATA_PREFIX}/usr/sbin/netdata" ]; then
 		echo "Deleting ${NETDATA_PREFIX}/usr/sbin/netdata ..."
-		run rm -i "${NETDATA_PREFIX}/usr/sbin/netdata"
+		run rm ${RM_FLAGS} "${NETDATA_PREFIX}/usr/sbin/netdata"
 	fi
 
-	portable_deletedir_recursively_interactively "${NETDATA_PREFIX}/etc/netdata"
-	portable_deletedir_recursively_interactively "${NETDATA_PREFIX}/usr/share/netdata"
-	portable_deletedir_recursively_interactively "${NETDATA_PREFIX}/usr/libexec/netdata"
-	portable_deletedir_recursively_interactively "${NETDATA_PREFIX}/var/lib/netdata"
-	portable_deletedir_recursively_interactively "${NETDATA_PREFIX}/var/cache/netdata"
-	portable_deletedir_recursively_interactively "${NETDATA_PREFIX}/var/log/netdata"
+	for dir in "${NETDATA_PREFIX}/etc/netdata" \
+			"${NETDATA_PREFIX}/usr/share/netdata" \
+			"${NETDATA_PREFIX}/usr/libexec/netdata" \
+			"${NETDATA_PREFIX}/var/lib/netdata" \
+			"${NETDATA_PREFIX}/var/cache/netdata" \
+			"${NETDATA_PREFIX}/var/log/netdata"
+	do
+		if [ $INTERACTIVE -eq 1 ]; then
+			portable_deletedir_recursively_interactively "${dir}"
+		else
+			run rm -f -R "${dir}"
+		fi
+	done
 fi
 
 if [ -f /etc/logrotate.d/netdata ]; then
 	echo "Deleting /etc/logrotate.d/netdata ..."
-	run rm -i /etc/logrotate.d/netdata
+	run rm ${RM_FLAGS} /etc/logrotate.d/netdata
 fi
 
 if [ -f /etc/systemd/system/netdata.service ]; then
 	echo "Deleting /etc/systemd/system/netdata.service ..."
-	run rm -i /etc/systemd/system/netdata.service
+	run rm ${RM_FLAGS} /etc/systemd/system/netdata.service
 fi
 
 if [ -f /lib/systemd/system/netdata.service ]; then
 	echo "Deleting /lib/systemd/system/netdata.service ..."
-	run rm -i /lib/systemd/system/netdata.service
+	run rm ${RM_FLAGS} /lib/systemd/system/netdata.service
 fi
 
 if [ -f /etc/init.d/netdata ]; then
 	echo "Deleting /etc/init.d/netdata ..."
-	run rm -i /etc/init.d/netdata
+	run rm ${RM_FLAGS} /etc/init.d/netdata
 fi
 
 if [ -f /etc/periodic/daily/netdata-updater ]; then
 	echo "Deleting /etc/periodic/daily/netdata-updater ..."
-	run rm -i /etc/periodic/daily/netdata-updater
+	run rm ${RM_FLAGS} /etc/periodic/daily/netdata-updater
 fi
 
 if [ -f /etc/cron.daily/netdata-updater ]; then
 	echo "Deleting /etc/cron.daily/netdata-updater ..."
-	run rm -i /etc/cron.daily/netdata-updater
+	run rm ${RM_FLAGS} /etc/cron.daily/netdata-updater
 fi
 
 portable_check_user_exists netdata
--- tests/lifecycle.sh
+++ tests/lifecycle.sh
@@ -8,13 +8,16 @@ then
   exit 1
 fi
 
-if ! git config user.name; then
-  git config user.email "test@example.com"
-  git config user.name "test"
-fi
+WORKSPACE=$(mktemp -d)
+cp -r ./ "${WORKSPACE}/"
+cd "${WORKSPACE}"
+git config user.email "test@example.com"
+git config user.name "test"
 
 echo "========= INSTALL ========="
 ./netdata-installer.sh  --dont-wait --dont-start-it --install /tmp &>/dev/null
+# Copy uninstaller as upgrader will overwrite it with a version from master branch
+cp netdata-uninstaller.sh /tmp/netdata-uninstaller.sh
 
 echo "========= ADD GARBAGE ========="
 touch test
@@ -26,5 +29,6 @@ git status
 echo "========= UPDATE ========="
 ./netdata-updater.sh
 
-#echo "========= UNINSTALL ========="
-#./netdata-uninstaller.sh
+echo "========= UNINSTALL ========="
+mv /tmp/netdata-uninstaller.sh ./netdata-uninstaller.sh
+./netdata-uninstaller.sh --yes --force
