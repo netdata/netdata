bf867a0c9eb5536fe81875ce67268675d491641f
Matthew Wolffsohn <matthew@wolfie.ovh>
2018-12-31 12:12:22 +0000

    Dashboard grammar change (#5080)
    * Dashboard grammar change
    
    Changes the message for Total Swap I/O to something that reads a little better to me, and also adds a missing parentheses.
    
    * Remove double parenthesis
    
    * Change back to "either"
    
    * Swap back to 'is', as 'either' is the focus
    
    * Verbose explanation
    
    * Switch to using code statements instead

--- web/gui/dashboard_info.js
+++ web/gui/dashboard_info.js
@@ -647,7 +647,7 @@ netdataDashboard.context = {
     },
 
     'system.swapio': {
-        info: 'Total Swap I/O. (netdata measures both <code>in</code> and <code>out</code>. If either of them is not shown in the chart, it is because it is zero - you can change the page settings to always render all the available dimensions on all charts).'
+        info: 'Total Swap I/O. (netdata measures both <code>in</code> and <code>out</code>. If either of the metrics <code>in</code> or <code>out</code> is not shown in the chart, the reason is that the metric is zero. - you can change the page settings to always render all the available dimensions on all charts).'
     },
 
     'system.pgfaults': {
