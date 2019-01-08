b3a6ef8092f3d68708a8201f226187be28610696
paulfantom <pawel@krupa.net.pl>
2019-01-08 19:05:47 +0100

    fix version in rpm building script

--- contrib/rhel/build-netdata-rpm.sh
+++ contrib/rhel/build-netdata-rpm.sh
@@ -4,21 +4,13 @@ build() {
   yum -y install rpm-build redhat-rpm-config yum-utils autoconf automake curl gcc git libmnl-devel libuuid-devel make pkgconfig zlib-devel
 
   cd "$(dirname "$0")/../../" || exit 1
-  # shellcheck disable=SC1091
-  source "packaging/installer/functions.sh" || exit 1
 
   set -e
   autoreconf -ivf
   ./configure --enable-maintainer-mode
   make dist
 
-  version=$(grep PACKAGE_VERSION < config.h | cut -d '"' -f 2)
-  if [ -z "${version}" ]; then
-    echo >&2 "Cannot find netdata version."
-    exit 1
-  fi
-
-  tgz="netdata-${version}.tar.gz"
+  tgz="$(ls netdata-v*.tar.gz)"
   if [ ! -f "${tgz}" ]; then
     echo >&2 "Cannot find the generated tar.gz file '${tgz}'"
     exit 1
