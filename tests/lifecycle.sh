b046d248e26acb1d2dd8a30a5e8c8fccb15fad1a
paulfantom <pawel@krupa.net.pl>
2019-01-15 01:06:19 +0100

    new lifecycle test

--- tests/lifecycle.sh
+++ tests/lifecycle.sh
@@ -16,19 +16,11 @@ git config user.name "test"
 
 echo "========= INSTALL ========="
 ./netdata-installer.sh  --dont-wait --dont-start-it --auto-update --install /tmp &>/dev/null
-# Copy uninstaller as upgrader will overwrite it with a version from master branch
-cp netdata-uninstaller.sh /tmp/netdata-uninstaller.sh
-
-echo "========= ADD GARBAGE ========="
-touch garbagefile
-git add garbagefile
-git commit -m 'test commit'
-touch new_file
-git status
+ls /tmp
 
 echo "========= UPDATE ========="
-/etc/cron.daily/netdata-updater
+ENVIRONMENT_FILE=/tmp/netdata/etc/.environment /etc/cron.daily/netdata-updater
 
-echo "========= UNINSTALL ========="
-mv /tmp/netdata-uninstaller.sh ./netdata-uninstaller.sh
-./netdata-uninstaller.sh --yes --force
+#echo "========= UNINSTALL ========="
+#mv /tmp/netdata-uninstaller.sh ./netdata-uninstaller.sh
+#./netdata-uninstaller.sh --yes --force
