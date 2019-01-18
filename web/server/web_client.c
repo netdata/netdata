a626d7db1d9dd21474c4b26fe8ca4c27b7db1d52
cakrit <chris@netdata.cloud>
2019-01-18 16:56:30 +0100

    Use X-Auth-Token instead of Authorization header, to allow the management API to work authenticated behind proxies as well

--- web/server/web_client.c
+++ web/server/web_client.c
@@ -732,7 +732,7 @@ static inline char *http_header_parse(struct web_client *w, char *s, int parse_u
         hash_accept_encoding = simple_uhash("Accept-Encoding");
         hash_donottrack = simple_uhash("DNT");
         hash_useragent = simple_uhash("User-Agent");
-        hash_authorization = simple_uhash("Authorization");
+        hash_authorization = simple_uhash("X-Auth-Token");
     }
 
     char *e = s;
@@ -777,12 +777,8 @@ static inline char *http_header_parse(struct web_client *w, char *s, int parse_u
     }
     else if(parse_useragent && hash == hash_useragent && !strcasecmp(s, "User-Agent")) {
         w->user_agent = strdupz(v);
-    } else if(hash == hash_authorization&& !strcasecmp(s, "Authorization")) {
-        char *auth_type = strstr(v, "Bearer");
-        if(auth_type) {
-            auth_type += 7;
-            w->auth_bearer_token = strdupz(auth_type);
-        }
+    } else if(hash == hash_authorization&& !strcasecmp(s, "X-Auth-Token")) {
+        w->auth_bearer_token = strdupz(v);
     }
 #ifdef NETDATA_WITH_ZLIB
     else if(hash == hash_accept_encoding && !strcasecmp(s, "Accept-Encoding")) {
