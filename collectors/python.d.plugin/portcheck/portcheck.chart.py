11fd381cf6151405daac4e63756d29e7a85dfa59
Ilya Mashchenko <ilyamaschenko@gmail.com>
2019-02-05 15:36:43 +0300

    fix some python codacy errors (#5331)
    * debug error instead of continue
    
    * portcheck: remove unused variable
    
    * postgres: remove unused variable
    
    * SocketService: log error on disconnect instead of pass
    
    * SocketService: add ssl_version opt to ssl.wrap_socket call
    
    * minor

--- collectors/python.d.plugin/portcheck/portcheck.chart.py
+++ collectors/python.d.plugin/portcheck/portcheck.chart.py
@@ -118,7 +118,7 @@ class Service(SimpleService):
         :return: dict
         """
 
-        af, _, proto, _, sa = socket_config
+        _, _, proto, _, sa = socket_config
         port = str(sa[1])
         try:
             self.debug('Connecting socket to "{address}", port {port}'.format(address=sa[0], port=port))
