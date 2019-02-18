0259640be1899063832429deed3536bc875e9800
Ilya Mashchenko <ilyamaschenko@gmail.com>
2019-02-18 16:12:22 +0300

    python module sensors fix (#5406)
    * get_data indentation fix
    
    * fix divisor of energy and power charts

--- collectors/python.d.plugin/sensors/sensors.chart.py
+++ collectors/python.d.plugin/sensors/sensors.chart.py
@@ -41,7 +41,7 @@ CHARTS = {
     'power': {
         'options': [None, ' power', 'Watt', 'power', 'sensors.power', 'line'],
         'lines': [
-            [None, None, 'absolute', 1, 1000000]
+            [None, None, 'absolute', 1, 1000]
         ]
     },
     'fan': {
@@ -51,9 +51,9 @@ CHARTS = {
         ]
     },
     'energy': {
-        'options': [None, ' energy', 'Joule', 'energy', 'sensors.energy', 'areastack'],
+        'options': [None, ' energy', 'Joule', 'energy', 'sensors.energy', 'line'],
         'lines': [
-            [None, None, 'incremental', 1, 1000000]
+            [None, None, 'incremental', 1, 1000]
         ]
     },
     'humidity': {
@@ -115,7 +115,7 @@ class Service(SimpleService):
                         limit = LIMITS[type_name]
                         if val < limit[0] or val > limit[1]:
                             continue
-                        data[prefix + '_' + str(feature.name.decode())] = int(val * 1000)
+                    data[prefix + '_' + str(feature.name.decode())] = int(val * 1000)
         except sensors.SensorsError as error:
             self.error(error)
             return None
