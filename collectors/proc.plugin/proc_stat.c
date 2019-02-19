4908689c79f0c32c396c744f873d8ab6e47446fc
cakrit <chris@netdata.cloud>
2019-02-19 13:51:46 +0100

    Use trim

--- collectors/proc.plugin/proc_stat.c
+++ collectors/proc.plugin/proc_stat.c
@@ -425,10 +425,9 @@ static int read_cpuidle_states(char *cpuidle_name_filename , char *cpuidle_time_
                 cc->rescan_cpu_states = 1;
                 return 1;
             }
-            int i=0;
-            while(i < (int)r && !isspace(name_buf[i])) i++; //Ignore white space after the name
-            if (i < (int)r) name_buf[i] = '\0';
-            cc->cpuidle_state[state].name = strdupz(name_buf);
+
+            name_buf[r-1] = '\0';
+            cc->cpuidle_state[state].name = strdupz(trim(name_buf));
             close(fd);
 
             snprintfz(filename, FILENAME_MAX, cpuidle_time_filename, core, state);
