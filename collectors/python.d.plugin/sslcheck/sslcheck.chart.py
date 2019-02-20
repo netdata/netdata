cd4e1a45eea420007b5e787950f1341e4548cefb
Peter Thurner <p.thurner@blunix.org>
2019-02-20 02:02:36 +0100

    cleaned up

--- collectors/python.d.plugin/sslcheck/sslcheck.chart.py
+++ collectors/python.d.plugin/sslcheck/sslcheck.chart.py
@@ -5,13 +5,7 @@
 
 import ssl
 import socket
-
 import datetime
-
-try:
-    from time import monotonic as time
-except ImportError:
-    from time import time
 
 from bases.FrameworkServices.SimpleService import SimpleService
 
@@ -22,7 +16,7 @@ ORDER = ['daysuntilexpiration']
 
 CHARTS = {
     'daysuntilexpiration': {
-        'options': [None, 'Days until expiration', 'days', 'daysuntilexpiration', 'sslcheck.daysuntilexpiration', 'line'],
+        'options': [None, 'days until certificate expiration', 'days', 'days until expiration', 'sslcheck.daysuntilexpiration', 'line'],
         'lines': [
             [DAYS_UNTIL_EXPIRATION, 'days until expiration', 'absolute', 100, 1000]
         ]
@@ -89,22 +83,3 @@ class Service(SimpleService):
         """Get the number of days left in a cert's lifetime."""
         expires = self.ssl_expiry_datetime(hostname, port)
         return (expires - datetime.datetime.utcnow()).days
-
-    # def ssl_expires_in(self, hostname, buffer_days=14):
-    #    """
-    #    Check if `hostname` SSL cert expires within `buffer_days`.
-    #    :return: Bool
-    #    """
-    #    remaining = self.ssl_valid_time_remaining(hostname)
-
-    #    # if the cert expires in less than two weeks or is already expired, return True
-    #    if remaining < datetime.timedelta(days=0):
-    #        # cert has already expired
-    #        return True
-    #    elif remaining < datetime.timedelta(days=buffer_days):
-    #        # expires sooner than the buffer
-    #        return True
-    #    else:
-    #        # everything is fine
-    #        return False
-
