894fe21dc9123fdd6221243c41491073f2219763
Pavlos Emm. Katsoulakis <paul@netdata.rocks>
2019-03-08 18:28:19 +0000

    Cleanup build process, first round

--- packaging/docker/build.sh
+++ packaging/docker/build.sh
@@ -1,51 +1,57 @@
 #!/bin/bash
-
 # Cross-arch docker build helper script
 # Needs docker in version >18.02 due to usage of manifests
 #
 # Copyright: SPDX-License-Identifier: GPL-3.0-or-later
 #
 # Author  : Pawel Krupa (paulfantom)
+# Author  : Pavlos Emm. Katsoulakis (paul@netdata.cloud)
 
 set -e
 
-if [ ! -f .gitignore ]; then
-	echo "Run as ./packaging/docker/$(basename "$0") from top level directory of git repository"
-	exit 1
-fi
-
-echo "Docker build process starting.."
-
-if [ "$1" == "" ]; then
-    VERSION=$(git tag --points-at)
-else
-    VERSION="$1"
-fi
-if [ "${VERSION}" == "" ]; then
-    VERSION="latest"
-fi
-
+VERSION="$1"
+REPOSITORY="${REPOSITORY:-netdata}"
 declare -A ARCH_MAP
 ARCH_MAP=( ["i386"]="386" ["amd64"]="amd64" ["armhf"]="arm" ["aarch64"]="arm64")
-if [ -z ${DEVEL+x} ]; then
-    declare -a ARCHITECTURES=(i386 armhf aarch64 amd64)
-else
-    declare -a ARCHITECTURES=(amd64)
-    unset DOCKER_PASSWORD
-    unset DOCKER_USERNAME
+DEVEL_ARCHS=(amd64)
+ARCHS="${!ARCH_MAP[@]}"
+
+# When development mode is set, build on DEVEL_ARCHS
+if [ ! -z ${DEVEL+x} ]; then
+    declare -a ARCHS=(${DEVEL_ARCHS[@]})
 fi
 
-REPOSITORY="${REPOSITORY:-netdata}"
-echo "Building ${VERSION} of ${REPOSITORY} container"
+# Ensure there is a version, the most appropriate one
+if [ "${VERSION}" == "" ]; then
+    VERSION=$(git tag --points-at)
+    if [ "${VERSION}" == "" ]; then
+        VERSION="latest"
+    fi
+fi
+
+# TODO: Need a more stable way to find where to run from.
+if [ ! -f .gitignore ]; then
+    echo "Run as ./packaging/docker/$(basename "$0") from top level directory of git repository"
+    echo "Docker build process aborted"
+    exit 1
+fi
+
+echo "Docker image build in progress.."
+echo "Version       : ${VERSION}"
+echo "Repository    : ${REPOSITORY}"
+echo "Architectures : ${ARCHS}"
 
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
 
 # Build images using multi-arch Dockerfile.
-for ARCH in "${ARCHITECTURES[@]}"; do
+for ARCH in "${ARCHS[@]}"; do
+     TAG="${REPOSITORY}:${VERSION}-${ARCH}"
+     echo "Building tag ${TAG}.."
      eval docker build \
-     		--build-arg ARCH="${ARCH}" \
-     		--tag "${REPOSITORY}:${VERSION}-${ARCH}" \
-     		--file packaging/docker/Dockerfile ./
+          --build-arg ARCH="${ARCH}" \
+          --tag "${TAG}" \
+          --file packaging/docker/Dockerfile ./
+     echo "..Done!"
 done
 
 echo "Docker build process completed!"
