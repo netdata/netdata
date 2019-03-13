fb6def2fff1e1d333b94ee7ada25d610aee1708b
Vladimir Kobal <vlad@prokk.net>
2019-03-13 18:18:55 +0200

    Fix memory leaks (#5604)

--- collectors/plugins.d/plugins_d.c
+++ collectors/plugins.d/plugins_d.c
@@ -101,11 +101,13 @@ static inline int quoted_strings_splitter(char *str, char **words, int max_words
 
 inline int pluginsd_initialize_plugin_directories() {
     char plugins_dirs[(FILENAME_MAX * 2) + 1];
-    char *plugins_dir_list = NULL;
+    static char *plugins_dir_list = NULL;
 
     // Get the configuration entry
-    snprintfz(plugins_dirs, FILENAME_MAX * 2, "\"%s\" \"%s/custom-plugins.d\"", PLUGINS_DIR, CONFIG_DIR);
-    plugins_dir_list = strdupz(config_get(CONFIG_SECTION_GLOBAL, "plugins directory",  plugins_dirs));
+    if(likely(!plugins_dir_list)) {
+        snprintfz(plugins_dirs, FILENAME_MAX * 2, "\"%s\" \"%s/custom-plugins.d\"", PLUGINS_DIR, CONFIG_DIR);
+        plugins_dir_list = strdupz(config_get(CONFIG_SECTION_GLOBAL, "plugins directory",  plugins_dirs));
+    }
 
     // Parse it and store it to plugin directories
     return quoted_strings_splitter(plugins_dir_list, plugin_directories, PLUGINSD_MAX_DIRECTORIES, config_isspace);
