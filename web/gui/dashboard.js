c5c4ac7bb0e98b365888dd8f1fb57552f81bb493
cakrit <chris@netdata.cloud>
2019-03-15 20:36:19 +0100

    load gtm.js synchronously

--- web/gui/dashboard.js
+++ web/gui/dashboard.js
@@ -9776,7 +9776,7 @@ NETDATA.registry = {
                     if (data.anonymous_statistics) {
                         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=false;j.src=
                             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                         })(window,document,'script','dataLayer','GTM-N6CBMJD');
                         dataLayer.push({"anonymous_statistics" : "true", "machine_guid" : data.machine_guid});
