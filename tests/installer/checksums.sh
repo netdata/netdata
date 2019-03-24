3ff3105700887b2bebb8a4260ae7f3068f026e8d
Pavlos Emm. Katsoulakis <paul@netdata.rocks>
2019-03-24 16:05:55 +0000

    netdata/packaging/ci: First rounf of PR feedback adjustments
    1) changes wordings as recommended
    2) pass file info in parameter and use it in the wordings
    3) as shellcheck suggests, use -n instead of ! -z. Makes sense actually, ! -z is kind of reverse logic that confuses
    
    More adjustments on a follow up commit

--- tests/installer/checksums.sh
+++ tests/installer/checksums.sh
@@ -12,33 +12,34 @@ README_DOC="packaging/installer/README.md"
 # If we are not in netdata git repo, at the top level directory, fail
 TOP_LEVEL=$(basename "$(git rev-parse --show-toplevel 2> /dev/null || echo "")")
 CWD="$(git rev-parse --show-cdup 2> /dev/null || echo "")"
-if [ ! -z $CWD ] || [ ! "${TOP_LEVEL}" == "netdata" ]; then
+if [ -n "$CWD" ] || [ ! "${TOP_LEVEL}" == "netdata" ]; then
     echo "Run as .travis/$(basename "$0") from top level directory of netdata git repository"
     echo "Kickstart validation process aborted"
     exit 1
 fi
 
 for file in kickstart.sh kickstart-static64.sh; do
-	CHECKSUM_IN_README=$(grep "$file" $README_DOC | grep md5sum | cut -d '"' -f2)
+	README_MD5=$(grep "$file" $README_DOC | grep md5sum | cut -d '"' -f2)
 	KICKSTART_URL="https://my-netdata.io/$file"
-	KICKSTART_MD5="$(md5sum "packaging/installer/$file" | cut -d' ' -f1)"
+	KICKSTART="packaging/installer/$file"
+	KICKSTART_MD5="$(md5sum "${KICKSTART}" | cut -d' ' -f1)"
 	CALCULATED_MD5="$(curl -Ss ${KICKSTART_URL} | md5sum | cut -d ' ' -f 1)"
 
 	# Conditionally run the website validation
 	if [ -z "${LOCAL_ONLY}" ]; then
-		echo "Validating ${KICKSTART_URL} against ${KICKSTART_MD5}.."
+		echo "Validating ${KICKSTART_URL} against local file ${KICKSTART} with MD5 ${KICKSTART_MD5}.."
 		if [ "$KICKSTART_MD5" == "$CALCULATED_MD5" ]; then
 			echo "${KICKSTART_URL} looks fine"
 		else
-			echo "${KICKSTART_URL} has changed, please update it"
+			echo "${KICKSTART_URL} md5sum does not match local file. It needs to be updated"
 			exit 1
 		fi
 	fi
 
 	echo "Validating documentation for $file"
-	if [ "$KICKSTART_MD5" != "$CHECKSUM_IN_README" ]; then
+	if [ "$KICKSTART_MD5" != "$README_MD5" ]; then
 		echo "Invalid checksum for $file in $README_DOC."
-		echo "checksum in docs: $CHECKSUM_IN_README"
+		echo "checksum in docs: $README_MD5"
 		echo "current checksum: $KICKSTART_MD5"
 		exit 2
 	else
