4b7ebd378b55632a046644d9adb9a7286bcb2b24
Pavlos Emm. Katsoulakis <paul@netdata.rocks>
2019-03-25 18:47:56 +0000

    netdata/packaging/nightlies: Clean up nightlies, round one of many
    1) Move ALL changelog generation related code, to generate changelog script
    2) Remove that || logic from everywherem, except the part we execute changelog generation.
       We have said its optional, so do not fail nightlies if this script fails, just report the problem
    3) change the gitignore change and use the more stable logic that monitors folder path utilizing git
    4) Add a bit more verbosity everywhere so we can tell what is going on
    5) Adjust generate changelog script, based on the newly migrated code.
       a) Variable declarations to the top, so that if something wrong with the generated ones we break early and avoid the mess
       b) put all that github handling, right after markmandel's changelog generation
       c) Gently handle github commands, add enough verbosity to identify where we broke and also check in the end what happened and clean-up github before you bail out
    6) A couple of renames for variables here and there, for clarity and readability

--- .travis/generate_changelog.sh
+++ .travis/generate_changelog.sh
@@ -1,29 +1,51 @@
 #!/bin/bash
-
+#
+# Changelog generation scriptlet.
+#
+# Copyright: SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Author  : Pawel Krupa (paulfantom)
+# Author  : Pavlos Emm. Katsoulakis (paul@netdata.cloud)
 set -e
 
-if [ ! -f .gitignore ]; then
-	echo "Run as ./travis/$(basename "$0") from top level directory of git repository"
-	exit 1
+# If we are not in netdata git repo, at the top level directory, fail
+TOP_LEVEL=$(basename "$(git rev-parse --show-toplevel)")
+CWD=$(git rev-parse --show-cdup)
+if [ ! -z $CWD ] || [ ! "${TOP_LEVEL}" == "netdata" ]; then
+    echo "Run as .travis/$(basename "$0") from top level directory of netdata git repository"
+    echo "Changelog generation process aborted"
+    exit 1
 fi
 
-ORGANIZATION=$(echo "$TRAVIS_REPO_SLUG" | awk -F '/' '{print $1}')
-PROJECT=$(echo "$TRAVIS_REPO_SLUG" | awk -F '/' '{print $2}')
+ORG=$(echo "$TRAVIS_REPO_SLUG" | cut -d '/' -f1)
+PROJECT=$(echo "$TRAVIS_REPO_SLUG" | cut -d '/' -f 2)
 GIT_MAIL=${GIT_MAIL:-"bot@netdata.cloud"}
 GIT_USER=${GIT_USER:-"netdatabot"}
-
+LAST_TAG=$(git describe --abbrev=0 --tags)
+COMMITS_SINCE_RELEASE=$(git rev-list "$LAST_TAG"..HEAD --count)
+PUSH_URL=$(git config --get remote.origin.url | sed -e 's/^https:\/\///')
+FAIL=0
 if [ -z ${GIT_TAG+x} ]; then
 	OPTS=""
 else
 	OPTS="--future-release ${GIT_TAG}"
 fi
 
-echo "--- Creating changelog ---"
+# If no commits since release, just stop
+if [ "$COMMITS_SINCE_RELEASE" == "$(rev <packaging/version | cut -d- -f 2 | rev)" ]; then
+	echo "No changes since last release"
+	exit 0
+fi
+echo "We got $COMMITS_SINCE_RELEASE changes since $LAST_TAG, re-generating changelog"
+git config user.email "${GIT_MAIL}"
+git config user.name "${GIT_USER}"
 git checkout master
 git pull
+
+echo "Running project markmandel for github changelog generation"
 #docker run -it --rm -v "$(pwd)":/usr/local/src/your-app ferrarimarco/github-changelog-generator:1.14.3 \
 docker run -it -v "$(pwd)":/project markmandel/github-changelog-generator:latest \
-	--user "${ORGANIZATION}" \
+	--user "${ORG}" \
 	--project "${PROJECT}" \
 	--token "${GITHUB_TOKEN}" \
 	--since-tag "v1.10.0" \
@@ -31,3 +53,18 @@ docker run -it -v "$(pwd)":/project markmandel/github-changelog-generator:latest
 	--exclude-labels "stale,duplicate,question,invalid,wontfix,discussion,no changelog" \
 	--no-compare-link ${OPTS}
 
+echo "Changelog created! Adding packaging/version and CHANGELOG.md to the repository"
+echo "$LAST_TAG-$((COMMITS_SINCE_RELEASE + 1))-nightly" > packaging/version
+git add packaging/version && echo "1) Added packaging/version to repository" || FAIL=1
+git add CHANGELOG.md && echo "2) Added changelog file to repository" || FAIL=1
+git commit -m '[ci skip] create nightly packages and update changelog' && echo "3) Committed changes to repository" || FAIL=1
+git push "https://${GITHUB_TOKEN}:@${PUSH_URL}" && echo "4) Pushed changes to remote ${PUSH_URL}" || FAIL=1
+
+# In case of a failure, wrap it up and bail out cleanly
+if [ $FAIL -eq 1 ]; then
+	git clean -xfd
+	echo "Changelog generation failed during github UPDATE!"
+	exit 1
+fi
+
+echo "Changelog generation completed successfully!"
--- .travis/nightlies.sh
+++ .travis/nightlies.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#
 # This is the nightlies orchastration script
 # It runs the following activities in order:
 # 1) Generate changelog
@@ -10,49 +11,28 @@
 #
 # Author  : Pawel Krupa (paulfantom)
 # Author  : Pavlos Emm. Katsoulakis (paul@netdata.cloud)
-
 set -e
 
 FAIL=0
 
-if [ ! -f .gitignore ]; then
-	echo "Run as ./travis/$(basename "$0") from top level directory of git repository"
-	exit 1
+# If we are not in netdata git repo, at the top level directory, fail
+TOP_LEVEL=$(basename "$(git rev-parse --show-toplevel)")
+CWD=$(git rev-parse --show-cdup)
+if [ ! -z $CWD ] || [ ! "${TOP_LEVEL}" == "netdata" ]; then
+    echo "Run as .travis/$(basename "$0") from top level directory of netdata git repository"
+    echo "Changelog generation process aborted"
+    exit 1
 fi
 
-export GIT_MAIL="bot@netdata.cloud"
-export GIT_USER="netdatabot"
-echo "--- Initialize git configuration ---"
-git config user.email "${GIT_MAIL}"
-git config user.name "${GIT_USER}"
+echo "--- Running Changelog generation ---"
+.travis/generate_changelog.sh || echo "Changelog generation has failed, this is a soft error, process continues"
 
-echo "--- UPDATE VERSION FILE ---"
-LAST_TAG=$(git describe --abbrev=0 --tags)
-NO_COMMITS=$(git rev-list "$LAST_TAG"..HEAD --count)
-if [ "$NO_COMMITS" == "$(rev <packaging/version | cut -d- -f 2 | rev)" ]; then
-	echo "Nothing changed since last nightly build"
-	exit 0
-fi
-echo "$LAST_TAG-$((NO_COMMITS + 1))-nightly" >packaging/version
-git add packaging/version || exit 1
+echo "--- Build docker images ---"
+packaging/docker/build.sh
+echo "--- Publish docker images ---"
+packaging/docker/publish.sh
 
-echo "--- GENERATE CHANGELOG ---"
-if .travis/generate_changelog.sh; then
-	git add CHANGELOG.md
-
-	echo "--- UPLOAD FILE CHANGES ---"
-	git commit -m '[ci skip] create nightly packages and update changelog'
-	git push "https://${GITHUB_TOKEN}:@$(git config --get remote.origin.url | sed -e 's/^https:\/\///')"
-else
-	git clean -xfd
-	echo "Changelog generation has failed, will proceed anyway"
-fi
-
-echo "--- BUILD & PUBLISH DOCKER IMAGES ---"
-packaging/docker/build.sh || FAIL=1
-packaging/docker/publish.sh || FAIL=1
-
-echo "--- BUILD ARTIFACTS ---"
-.travis/create_artifacts.sh || FAIL=1
+echo "--- Build artifacts ---"
+.travis/create_artifacts.sh
 
 exit "${FAIL}"
