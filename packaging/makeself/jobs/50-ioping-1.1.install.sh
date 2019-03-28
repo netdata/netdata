7386df8c107088da1c037696760f5685595ec641
Vladimir Kobal <vlad@prokk.net>
2019-03-28 21:10:42 +0200

    Finalize installation of ioping

--- packaging/makeself/jobs/50-ioping-1.1.install.sh
+++ packaging/makeself/jobs/50-ioping-1.1.install.sh
@@ -3,19 +3,21 @@
 
 . $(dirname "${0}")/../functions.sh "${@}" || exit 1
 
-git clone "https://github.com/koct9i/ioping.git ioping-1.1"
+if [ -d ioping-netdata ]
+    then
+    run rm -rf ioping-netdata || exit 1
+fi
+
+run git clone https://github.com/netdata/ioping.git ioping-netdata
+
+run cd ioping-netdata || exit 1
 
 export CFLAGS="-static"
 
-cat >doc/Makefile <<EOF
-all:
-clean:
-install:
-EOF
-
 run make clean
 run make -j${SYSTEM_CPUS}
-run make install
+run mkdir -p ${NETDATA_INSTALL_PATH}/bin/
+run install -m 0755 ioping ${NETDATA_INSTALL_PATH}/bin/
 
 if [ ${NETDATA_BUILD_WITH_DEBUG} -eq 0 ]
 then
