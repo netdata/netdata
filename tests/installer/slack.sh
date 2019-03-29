88b6825c3089c519079efcc4cdce3e3be7f5e84b
Pavlos Emm. Katsoulakis <paul@netdata.rocks>
2019-03-29 05:35:51 +0000

    netdata/packaging/ci: Enrich slack lib, use it in travis. -- wip, still need how to format the message more correctly

--- tests/installer/slack.sh
+++ tests/installer/slack.sh
@@ -11,6 +11,33 @@
 # Author: Pavlos Emm. Katsoulakis <paul@netdata.cloud
 
 post_message() {
-	MESSAGE="$1"
-	curl -X POST --data-urlencode "payload={\"channel\": \"${SLACK_CHANNEL}\", \"username\": \"${SLACK_BOT_NAME}\", \"text\": \"${MESSAGE}\", \"icon_emoji\": \":space_invader:\"}" ${SLACK_INCOMING_WEBHOOK_URL}
+	TYPE="$1"
+	MESSAGE="$2"
+
+	case "$TYPE" in
+		"PLAIN_MESSAGE")
+			curl -X POST --data-urlencode "payload={\"channel\": \"${SLACK_CHANNEL}\", \"username\": \"${SLACK_BOT_NAME}\", \"text\": \"${MESSAGE}\", \"icon_emoji\": \":space_invader:\"}" ${SLACK_INCOMING_WEBHOOK_URL}
+			;;
+		"RICH_MESSAGE")
+			POST_MESSAGE="{
+				\"text\": \"${MESSAGE}\",
+				\"attachments\": [{
+				    \"text\": \"Current build\",
+				    \"fallback\": \"I could not determine the build\",
+				    \"callback_id\": \"\",
+				    \"color\": \"#3AA3E3\",
+				    \"attachment_type\": \"default\",
+				    \"actions\": [
+					{
+					    \"name\": \"${TRAVIS_BUILD_NUMBER}\",
+					    \"text\": \"View build status\",
+					    \"type\": \"button\",
+					    \"url\": \"${TRAVIS_BUILD_WEB_URL}\"
+					}]
+				}]
+			}"
+			echo "Sending ${POST_MESSAGE}"
+			curl -X POST --data-urlencode "payload=${POST_MESSAGE}" "${SLACK_INCOMING_WEBHOOK_URL}"
+			;;
+	esac
 }
