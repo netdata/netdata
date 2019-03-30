7bd1fe99f278291ffa8b5861f5e846d2fad7e2f2
Paul Katsoulakis <34388743+paulkatsoulakis@users.noreply.github.com>
2019-03-30 14:56:26 +0000

    netdata/packaging/ci: small improvements in logging (#5746)
    * netdata/packaging/ci: calculate the version string earlier, so that we can have it in our logs for troubleshooting. Also a small comment nit
    
    * netdata/packaging/ci: always use curlies

--- .travis/nightlies.sh
+++ .travis/nightlies.sh
@@ -18,24 +18,24 @@ FAIL=0
 # If we are not in netdata git repo, at the top level directory, fail
 TOP_LEVEL=$(basename "$(git rev-parse --show-toplevel)")
 CWD=$(git rev-parse --show-cdup || echo "")
-if [ -n "$CWD" ] || [ ! "${TOP_LEVEL}" == "netdata" ]; then
+if [ -n "${CWD}" ] || [ ! "${TOP_LEVEL}" == "netdata" ]; then
     echo "Run as .travis/$(basename "$0") from top level directory of netdata git repository"
     echo "Changelog generation process aborted"
     exit 1
 fi
 
 LAST_TAG=$(git describe --abbrev=0 --tags)
-COMMITS_SINCE_RELEASE=$(git rev-list "$LAST_TAG"..HEAD --count)
+COMMITS_SINCE_RELEASE=$(git rev-list "${LAST_TAG}"..HEAD --count)
 PREVIOUS_NIGHTLY_COUNT="$(rev <packaging/version | cut -d- -f 2 | rev)"
 
 # If no commits since release, just stop
-if [ "$COMMITS_SINCE_RELEASE" == "${PREVIOUS_NIGHTLY_COUNT}" ]; then
+if [ "${COMMITS_SINCE_RELEASE}" == "${PREVIOUS_NIGHTLY_COUNT}" ]; then
 	echo "No changes since last nighthly release"
 	exit 0
 fi
 
 echo "--- Running Changelog generation ---"
-.travis/generate_changelog.sh "$LAST_TAG" "$COMMITS_SINCE_RELEASE" || echo "Changelog generation has failed, this is a soft error, process continues"
+.travis/generate_changelog.sh "${LAST_TAG}" "${COMMITS_SINCE_RELEASE}" || echo "Changelog generation has failed, this is a soft error, process continues"
 
 echo "--- Build && publish docker images ---"
 # Do not fail artifacts creation if docker fails. We will be restructuring this on a follow up PR
