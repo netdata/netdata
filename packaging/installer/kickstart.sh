6bf1ae80b88217707f793cad4d69e2ac665934f1
Steven <steven.wobser+github@protonmail.com>
2019-04-03 14:25:07 +0200

    Fixed safe_sha256sum() function declaration
    Using the `function` keyword does not work for all shells.

--- packaging/installer/kickstart.sh
+++ packaging/installer/kickstart.sh
@@ -204,7 +204,7 @@ dependencies() {
 	fi
 }
 
-function safe_sha256sum() {
+safe_sha256sum() {
 	# Within the contexct of the installer, we only use -c option that is common between the two commands
 	# We will have to reconsider if we start non-common options
 	if command -v sha256sum >/dev/null 2>&1; then
