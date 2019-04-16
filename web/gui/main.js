7c05d225f932675332d9435f9d805cfbd4f5c544
George Moschovitis <george@netdata.cloud>
2019-04-16 15:07:37 +0300

    Fixed for better console embedding

--- web/gui/main.js
+++ web/gui/main.js
@@ -4792,11 +4792,12 @@ function renderAccountUI() {
         container.removeAttribute("title");
         container.removeAttribute("data-original-title");
         container.removeAttribute("data-placement");
+        // <a href="/console/index.html#/charts/${NETDATA.registry.machine_guid}" target="_blank" class="btn">
         container.innerHTML = (
             `<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span id="amc-account-name"></span> <strong class="caret"></strong></a>
             <ul id="cloud-menu" class="dropdown-menu scrollable-menu inpagemenu" role="menu">
                 <li>
-                    <a href="/console/index.html#/charts/${NETDATA.registry.machine_guid}" target="_blank" class="btn">
+                    <a href="/console/index.html#/" target="_blank" class="btn">
                     <i class="fas fa-tv"></i>&nbsp;&nbsp;<span class="hidden-sm hidden-md">Console</span>
                     </a>
                 </li>
