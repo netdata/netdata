94a6cd7dc3a583f2e0936eeb204e52a8147629dc
Andrey Galkin <andrey@futoin.org>
2019-04-18 22:29:58 +0300

    Post-review fixes & revised charts

--- collectors/python.d.plugin/energid/energid.chart.py
+++ collectors/python.d.plugin/energid/energid.chart.py
@@ -19,54 +19,93 @@ ORDER = [
     'mempool',
     'secmem',
     'network',
+    'timeoffset',
     'utxo',
+    'xfers',
 ]
 
 CHARTS = {
     'blockindex': {
         'options': [None, 'Blockchain Index', 'count', 'blockchain', 'energi.blockindex', 'area'],
         'lines': [
-            ['blockchain.blocks', 'blocks', 'absolute'],
-            ['blockchain.headers', 'headers', 'absolute'],
+            ['blockchain_blocks', 'blocks', 'absolute'],
+            ['blockchain_headers', 'headers', 'absolute'],
         ]
     },
     'difficulty': {
         'options': [None, 'Blockchain Difficulty', 'difficulty', 'blockchain', 'energi.difficulty', 'line'],
         'lines': [
-            ['blockchain.difficulty', 'Diff', 'absolute'],
+            ['blockchain_difficulty', 'Diff', 'absolute'],
         ],
     },
     'mempool': {
-        'options': [None, 'MemPool', 'MiB', 'mempool', 'energi.mempool', 'area'],
+        'options': [None, 'MemPool', 'MiB', 'memory', 'energid.mempool', 'area'],
         'lines': [
-            ['mempool.max', 'Max', 'absolute', None, 1024*1024],
-            ['mempool.current', 'Usage', 'absolute', None, 1024*1024],
-            ['mempool.txsize', 'TX Size', 'absolute', None, 1024*1024],
+            ['mempool_max', 'Max', 'absolute', None, 1024*1024],
+            ['mempool_current', 'Usage', 'absolute', None, 1024*1024],
+            ['mempool_txsize', 'TX Size', 'absolute', None, 1024*1024],
         ],
     },
     'secmem': {
-        'options': [None, 'Secure Memory', 'KiB', 'memory', 'energi.secmem', 'area'],
+        'options': [None, 'Secure Memory', 'KiB', 'memory', 'energid.secmem', 'area'],
         'lines': [
-            ['secmem.total', 'Total', 'absolute', None, 1024],
-            ['secmem.locked', 'Locked', 'absolute', None, 1024],
-            ['secmem.used', 'Used', 'absolute', None, 1024],
+            ['secmem_total', 'Total', 'absolute', None, 1024],
+            ['secmem_locked', 'Locked', 'absolute', None, 1024],
+            ['secmem_used', 'Used', 'absolute', None, 1024],
         ],
     },
     'network': {
-        'options': [None, 'Network', 'count', 'network', 'energi.network', 'line'],
+        'options': [None, 'Network', 'count', 'network', 'energid.network', 'line'],
         'lines': [
-            ['network.connections', 'Connections', 'absolute'],
+            ['network_connections', 'Connections', 'absolute'],
+        ],
+    },
+    'timeoffset': {
+        'options': [None, 'Network', 'seconds', 'network', 'energid.timeoffset', 'line'],
+        'lines': [
+            ['network_timeoffset', 'offseet', 'absolute'],
         ],
     },
     'utxo': {
-        'options': [None, 'UTXO', 'count', 'UTXO', 'energi.utxo', 'line'],
+        'options': [None, 'UTXO', 'count', 'UTXO', 'energid.utxo', 'line'],
         'lines': [
-            ['utxo.count', 'UTXO', 'absolute'],
-            ['utxo.xfers', 'Xfers', 'absolute'],
+            ['utxo_count', 'UTXO', 'absolute'],
+        ],
+    },
+    'xfers': {
+        'options': [None, 'UTXO', 'count', 'UTXO', 'energid.xfers', 'line'],
+        'lines': [
+            ['utxo_xfers', 'Xfers', 'absolute'],
         ],
     },
 }
 
+METHODS = {
+    'getblockchaininfo': lambda r: {
+        'blockchain_blocks': r['blocks'],
+        'blockchain_headers': r['headers'],
+        'blockchain_difficulty': r['difficulty'],
+    },
+    'getmempoolinfo': lambda r: {
+        'mempool_txcount': r['size'],
+        'mempool_txsize': r['bytes'],
+        'mempool_current': r['usage'],
+        'mempool_max': r['maxmempool'],
+    },
+    'getmemoryinfo': lambda r: dict([
+        ('secmem_' + k, v) for (k,v) in r['locked'].items()
+    ]),
+    'getnetworkinfo': lambda r: {
+        'network_timeoffset' : r['timeoffset'],
+        'network_connections': r['connections'],
+    },
+    'gettxoutsetinfo': lambda r: {
+        'utxo_count' : r['txouts'],
+        'utxo_xfers' : r['transactions'],
+        'utxo_size' : r['disk_size'],
+        'utxo_amount' : r['total_amount'],
+    },
+}
 
 class Service(UrlService):
     def __init__(self, configuration=None, name=None):
@@ -75,7 +114,8 @@ class Service(UrlService):
         self.definitions = CHARTS
         self.host = self.configuration.get('host', '127.0.0.1')
         self.port = self.configuration.get('port', 9796)
-        self.url = 'http://{host}:{port}'.format(
+        self.url = '{scheme}://{host}:{port}'.format(
+            scheme=self.configuration.get('scheme', 'http'),
             host=self.host,
             port=self.port,
         )
@@ -83,37 +123,11 @@ class Service(UrlService):
         self.header = {
             'Content-Type': 'application/json',
         }
-        self.methods = {
-            'getblockchaininfo': lambda r: {
-                'blockchain.blocks': r['blocks'],
-                'blockchain.headers': r['headers'],
-                'blockchain.difficulty': r['difficulty'],
-            },
-            'getmempoolinfo': lambda r: {
-                'mempool.txcount': r['size'],
-                'mempool.txsize': r['bytes'],
-                'mempool.current': r['usage'],
-                'mempool.max': r['maxmempool'],
-            },
-            'getmemoryinfo': lambda r: dict([
-                ('secmem.' + k, v) for (k,v) in r['locked'].items()
-            ]),
-            'getnetworkinfo': lambda r: {
-                'network.timeoffset' : r['timeoffset'],
-                'network.connections': r['connections'],
-            },
-            'gettxoutsetinfo': lambda r: {
-                'utxo.count' : r['txouts'],
-                'utxo.xfers' : r['transactions'],
-                'utxo.size' : r['disk_size'],
-                'utxo.amount' : r['total_amount'],
-            },
-        }
 
     def _get_data(self):
         batch = []
 
-        for i, method in enumerate(self.methods.keys()):
+        for i, method in enumerate(METHODS):
             batch.append({
                 'version': '1.1',
                 'id': i,
@@ -122,10 +136,14 @@ class Service(UrlService):
             })
 
         result = self._get_raw_data(body=json.dumps(batch))
+
+        if not result:
+            return None
+
         result = json.loads(result.decode('utf-8'))
         data = dict()
 
-        for i, (_, handler) in enumerate(self.methods.items()):
+        for i, (_, handler) in enumerate(METHODS.items()):
             r = result[i]
             data.update(handler(r['result']))
 
