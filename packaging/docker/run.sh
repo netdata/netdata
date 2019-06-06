79f1649de5ea52fe4b819b9bf062f725eee092fd
Paul Emm. Katsoulakis <paul@netdata.rocks>
2019-06-06 15:45:46 +0300

    netdata/packaging/docker: Fix docker socket utilization, first pass
    1) dont do anything if there is no socket available
    2) fix the socket ownership after adding the new group
    
    3) fix comments/copyrights

--- packaging/docker/run.sh
+++ packaging/docker/run.sh
@@ -1,16 +1,33 @@
-#!/bin/sh
+#!/usr/bin/env bash
+#
+# Entry point script for netdata
+#
+# Copyright: SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Author  : Pavlos Emm. Katsoulakis <paul@netdata.cloud>
+set -e
 
-#set -e
-
+echo "Netdata entrypoint script starting"
 if [ ${RESCRAMBLE+x} ]; then
     echo "Reinstalling all packages to get the latest Polymorphic Linux scramble"
     apk upgrade --update-cache --available
 fi
 
-if [ ${PGID+x} ]; then
-  echo "Adding user netdata to group with id ${PGID}"
-  addgroup -g "${PGID}" -S hostgroup 2>/dev/null
-  sed -i "s/${PGID}:$/${PGID}:netdata/g" /etc/group
+DOCKER_USR="netdata"
+DOCKER_SOCKET="/var/run/docker.sock"
+DOCKER_GROUP="docker"
+
+if [ -S "${DOCKER_SOCKET}" ] && [ -n "${PGID}" ]; then
+	echo "Adding group with ID ${PGID} and name '${DOCKER_GROUP}'"
+	addgroup -g "${PGID}" "${DOCKER_GROUP}"
+
+	echo "Adding user '${DOCKER_USR}' to group '${DOCKER_GROUP}'"
+	sed -i "s/${DOCKER_GID}:$/${DOCKER_GID}:${DOCKER_USR}/g" /etc/group
+
+	echo "Adjusting ownership of mapped docker socket '${DOCKER_SOCKET}'"
+	chown "root:${DOCKER_GROUP}" "${DOCKER_SOCKET}"
 fi
 
-exec /usr/sbin/netdata -u netdata -D -s /host -p "${NETDATA_PORT}" "$@"
+exec /usr/sbin/netdata -u "${DOCKER_USR}" -D -s /host -p "${NETDATA_PORT}" "$@"
+
+echo "Netdata entrypoint script, completed!"
