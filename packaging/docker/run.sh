7ee5dc37ae6bca9945db518b2b0cc7d69ce36ae0
Paul Emm. Katsoulakis <34388743+paulkatsoulakis@users.noreply.github.com>
2019-06-11 11:25:21 +0300

    netdata/packaging: Fix netdata/netdata docker image failure, when users passing PGID that already exists on the system (#6259)
    * netdata/packaging: Adjust group creation, make failure a soft error and allow failing over to alternative group name if given group id exist on the image
    
    * netdata/packaging: make exception handling a bit softer, dont run commands when required data are missing and provide enough visibility for the admin
    
    * netdata/packaging: Stronger pattern matching. 999 may match to 23999 if left as it was -- wow

--- packaging/docker/run.sh
+++ packaging/docker/run.sh
@@ -13,19 +13,37 @@ if [ ${RESCRAMBLE+x} ]; then
     apk upgrade --update-cache --available
 fi
 
+create_group_and_assign_to_user() {
+	local local_DOCKER_GROUP="$1"
+	local local_DOCKER_GID="$2"
+	local local_DOCKER_USR="$3"
+
+	echo >&2 "Adding group with ID ${local_DOCKER_GID} and name '${local_DOCKER_GROUP}'"
+	addgroup -g "${local_DOCKER_GID}" "${local_DOCKER_GROUP}" || echo >&2 "Could not add group ${local_DOCKER_GROUP} with ID ${local_DOCKER_GID}, its already there probably"
+
+	echo >&2 "Adding user '${local_DOCKER_USR}' to group '${local_DOCKER_GROUP}/${local_DOCKER_GID}'"
+	sed -i "s/:${local_DOCKER_GID}:$/:${local_DOCKER_GID}:${local_DOCKER_USR}/g" /etc/group
+
+	# Make sure we use the right docker group
+	GRP_TO_ASSIGN="$(grep ":x:${local_DOCKER_GID}:" /etc/group | cut -d':' -f1)"
+	if [ -z "${GRP_TO_ASSIGN}" ]; then
+		echo >&2 "Could not find group ID ${local_DOCKER_GID} in /etc/group. Check your logs and report it if this is an unrecovereable error"
+	else
+		echo >&2 "Group creation and assignment completed, netdata was assigned to group ${GRP_TO_ASSIGN}/${local_DOCKER_GID}"
+		echo "${GRP_TO_ASSIGN}"
+	fi
+}
+
 DOCKER_USR="netdata"
 DOCKER_SOCKET="/var/run/docker.sock"
 DOCKER_GROUP="docker"
 
 if [ -S "${DOCKER_SOCKET}" ] && [ -n "${PGID}" ]; then
-	echo "Adding group with ID ${PGID} and name '${DOCKER_GROUP}'"
-	addgroup -g "${PGID}" "${DOCKER_GROUP}"
-
-	echo "Adding user '${DOCKER_USR}' to group '${DOCKER_GROUP}'"
-	sed -i "s/${DOCKER_GID}:$/${DOCKER_GID}:${DOCKER_USR}/g" /etc/group
-
-	echo "Adjusting ownership of mapped docker socket '${DOCKER_SOCKET}'"
-	chown "root:${DOCKER_GROUP}" "${DOCKER_SOCKET}"
+	GRP=$(create_group_and_assign_to_user "${DOCKER_GROUP}" "${PGID}" "${DOCKER_USR}")
+	if [ -n "${GRP}" ]; then
+		echo "Adjusting ownership of mapped docker socket '${DOCKER_SOCKET}' to root:${GRP}"
+		chown "root:${GRP}" "${DOCKER_SOCKET}"
+	fi
 fi
 
 exec /usr/sbin/netdata -u "${DOCKER_USR}" -D -s /host -p "${NETDATA_PORT}" "$@"
