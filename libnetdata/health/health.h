9c0dc99ee67baa060cd64821dbcf198d26965967
Thiago Marques <thiagoftsm@gmail.com>
2019-06-26 15:51:14 +0000

    Alarms fill library!

--- libnetdata/health/health.h
+++ libnetdata/health/health.h
@@ -3,6 +3,16 @@
 
 # include "../libnetdata.h"
 
+#define HEALTH_ALARM_KEY "alarm"
+#define HEALTH_TEMPLATE_KEY "template"
+#define HEALTH_CONTEXT_KEY "context"
+#define HEALTH_CHART_KEY "chart"
+#define HEALTH_HOST_KEY "hosts"
+#define HEALTH_OS_KEY "os"
+#define HEALTH_FAMILIES_KEY "families"
+#define HEALTH_LOOKUP_KEY "lookup"
+#define HEALTH_CALC_KEY "calc"
+
 typedef struct silencer {
     char *alarms;
     SIMPLE_PATTERN *alarms_pattern;
@@ -21,5 +31,24 @@ typedef struct silencer {
 
     struct silencer *next;
 } SILENCER;
+
+typedef enum silence_type {
+    STYPE_NONE,
+    STYPE_DISABLE_ALARMS,
+    STYPE_SILENCE_NOTIFICATIONS
+} SILENCE_TYPE;
+
+typedef struct silencers {
+    int all_alarms;
+    SILENCE_TYPE stype;
+    SILENCER *silencers;
+} SILENCERS;
+
+SILENCERS *silencers;
+
+extern SILENCER *create_silencer(void);
+extern int health_silencers_json_read_callback(JSON_ENTRY *e);
+extern void health_silencers_add(SILENCER *silencer);
+extern SILENCER * health_silencers_addparam(SILENCER *silencer, char *key, char *value);
 
 #endif
