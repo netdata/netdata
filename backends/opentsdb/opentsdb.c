ca1799280d51e85f85ff436fca121e6d2a241bb3
thiagoftsm <49162938+thiagoftsm@users.noreply.github.com>
2019-06-27 11:20:28 -0300

    Backend and SSL! (#6220)
    * SSL_backend Begin of the encryptation of backend!
    
    * SSL_backend changing opentsdb!
    
    * SSL_backend fix HTTP message with JSON!
    
    * SSL_backend HTTP API done!
    
    * SSL_fix_format preparing to connect with proxy!
    
    * SSL_backend wip SSL send/receive !
    
    * SSL_backend working with proxy
    
    * SSL_backend removing comments!
    
    * SSL_backend docummentation!
    
    * SSL_backend review]!
    
    * SSL_backend organizing!
    
    * Alarm_backend remove comments!
    
    * SSL_backend!
    
    * SSL_backend typedef!
    
    * SSL_backend bring switch!
    
    * SSL_backend commiting format changes!
    
    * SSL_backend fix github parser!
    
    * SSL_Backend fix format!
    
    * SSL_backend switch everything!
    
    * SSL_backend reviewing!
    
    * SSL_backend comments!
    
    * SSL_backend indentation!
    
    * SSL_backend indentation 3!
    
    * SSL_backend documentation!
    
    * SSL_backend hidden pointer!
    
    * SSL_backend missing space
    
    * SSL_backend change documentation!
    
    * SSL_backend change documentation 2!

--- backends/opentsdb/opentsdb.c
+++ backends/opentsdb/opentsdb.c
@@ -80,6 +80,7 @@ int format_dimension_stored_opentsdb_telnet(
 
         return 1;
     }
+
     return 0;
 }
 
@@ -87,4 +88,118 @@ int process_opentsdb_response(BUFFER *b) {
     return discard_response(b, "opentsdb");
 }
 
+static inline void opentsdb_build_message(BUFFER *b, char *message, const char *hostname, int length) {
+    buffer_sprintf(
+            b
+            , "POST /api/put HTTP/1.1\r\n"
+              "Host: %s\r\n"
+              "Content-Type: application/json\r\n"
+              "Content-Length: %d\r\n"
+              "\r\n"
+              "%s"
+            , hostname
+            , length
+            , message
+    );
+}
 
+int format_dimension_collected_opentsdb_http(
+        BUFFER *b                 // the buffer to write data to
+        , const char *prefix        // the prefix to use
+        , RRDHOST *host             // the host this chart comes from
+        , const char *hostname      // the hostname (to override host->hostname)
+        , RRDSET *st                // the chart
+        , RRDDIM *rd                // the dimension
+        , time_t after              // the start timestamp
+        , time_t before             // the end timestamp
+        , BACKEND_OPTIONS backend_options // BACKEND_SOURCE_* bitmap
+) {
+    (void)host;
+    (void)after;
+    (void)before;
+
+    char message[1024];
+    char chart_name[RRD_ID_LENGTH_MAX + 1];
+    char dimension_name[RRD_ID_LENGTH_MAX + 1];
+    backend_name_copy(chart_name, (backend_options & BACKEND_OPTION_SEND_NAMES && st->name)?st->name:st->id, RRD_ID_LENGTH_MAX);
+    backend_name_copy(dimension_name, (backend_options & BACKEND_OPTION_SEND_NAMES && rd->name)?rd->name:rd->id, RRD_ID_LENGTH_MAX);
+
+    int length =  snprintfz(message
+                           , sizeof(message)
+                           , "{"
+                             "  \"metric\": \"%s.%s.%s\","
+                             "  \"timestamp\": %llu,"
+                             "  \"value\": "COLLECTED_NUMBER_FORMAT ","
+                             "  \"tags\": {"
+                             "    \"host\": \"%s%s%s\""
+                             "  }"
+                             "}"
+                           , prefix
+                           , chart_name
+                           , dimension_name
+                           , (unsigned long long)rd->last_collected_time.tv_sec
+                           , rd->last_collected_value
+                           , hostname
+                           , (host->tags)?" ":""
+                           , (host->tags)?host->tags:""
+                    );
+
+    if(length > 0) {
+        opentsdb_build_message(b, message, hostname, length);
+    }
+
+    return 1;
+}
+
+int format_dimension_stored_opentsdb_http(
+        BUFFER *b                 // the buffer to write data to
+        , const char *prefix        // the prefix to use
+        , RRDHOST *host             // the host this chart comes from
+        , const char *hostname      // the hostname (to override host->hostname)
+        , RRDSET *st                // the chart
+        , RRDDIM *rd                // the dimension
+        , time_t after              // the start timestamp
+        , time_t before             // the end timestamp
+        , BACKEND_OPTIONS backend_options // BACKEND_SOURCE_* bitmap
+) {
+    (void)host;
+
+    time_t first_t = after, last_t = before;
+    calculated_number value = backend_calculate_value_from_stored_data(st, rd, after, before, backend_options, &first_t, &last_t);
+
+    if(!isnan(value)) {
+        char chart_name[RRD_ID_LENGTH_MAX + 1];
+        char dimension_name[RRD_ID_LENGTH_MAX + 1];
+        backend_name_copy(chart_name, (backend_options & BACKEND_OPTION_SEND_NAMES && st->name)?st->name:st->id, RRD_ID_LENGTH_MAX);
+        backend_name_copy(dimension_name, (backend_options & BACKEND_OPTION_SEND_NAMES && rd->name)?rd->name:rd->id, RRD_ID_LENGTH_MAX);
+
+        char message[1024];
+        int length =  snprintfz(message
+                , sizeof(message)
+                , "{"
+                  "  \"metric\": \"%s.%s.%s\","
+                  "  \"timestamp\": %llu,"
+                  "  \"value\": "CALCULATED_NUMBER_FORMAT ","
+                  "  \"tags\": {"
+                  "    \"host\": \"%s%s%s\""
+                  "  }"
+                  "}"
+                , prefix
+                , chart_name
+                , dimension_name
+                , (unsigned long long)last_t
+                , value
+                , hostname
+                , (host->tags)?" ":""
+                , (host->tags)?host->tags:""
+        );
+
+        if(length > 0) {
+            opentsdb_build_message(b, message, hostname, length);
+        }
+
+        return 1;
+    }
+
+    return 0;
+}
