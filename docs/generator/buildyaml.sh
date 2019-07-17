986e468df600019ffcff5da0b206e09202ac7e40
Joel Hans <joel@netdata.cloud>
2019-07-17 11:42:16 -0700

    New 'homepage' for documentation site (#6428)
    * Created new docs homepage with attempt to "fix" build script
    
    * Created new docs homepage with attempt to "fix" build script
    
    * Trying more links
    
    * Trying more links
    
    * Move documentation homepage and got script mostly working
    
    * Move documentation homepage and got script mostly working
    
    * Final changes based on C&C feedback
    
    * Final changes based on C&C feedback
    
    * A few new custom styles
    
    * A few new custom styles
    
    * Removing unnecessary sed
    
    * Removing unnecessary sed
    
    * Final fixes to docs homepage
    
    * Final fixes to docs homepage
    
    * Cleanup on DOCUMENTATION page
    
    * Remove localization folder
    
    * Fixed typo
    
    * Created buttons for new documentation homepage.
    
    * Remove whitespace
    
    * Cleanup in the CSS file
    
    * Working on replacing html content
    
    * Finalized new build process
    
    * Updated the new what is Netdata page
    
    * Fixed all the broken links

--- docs/generator/buildyaml.sh
+++ docs/generator/buildyaml.sh
@@ -53,7 +53,7 @@ repo_url: https://github.com/netdata/netdata
 repo_name: GitHub
 edit_uri: blob/master
 site_description: Netdata Documentation
-copyright: Netdata, 2018
+copyright: Netdata, 2019
 docs_dir: '${docs_dir}'
 site_dir: '${site_dir}'
 #use_directory_urls: false
@@ -123,9 +123,12 @@ markdown_extensions:
  - pymdownx.extrarawhtml
 nav:'
 
-navpart 1 . README "About"
+navpart 1 . "README" ""
 
-echo -ne "    - 'docs/Demo-Sites.md'
+navpart 1 . . "About Netdata"
+
+echo -ne "    - 'docs/what-is-netdata.md'
+    - 'docs/Demo-Sites.md'
     - 'docs/netdata-security.md'
     - 'docs/anonymous-statistics.md'
     - 'docs/Donations-netdata-has-received.md'
