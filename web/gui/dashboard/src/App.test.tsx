cb638876a67f02bd80f7099bf9773655b8a09bfe
Jacek Kolasa <jacek.kolasa@gmail.com>
2019-08-13 13:33:46 +0300

    fix App.tsx test

--- web/gui/dashboard/src/App.test.tsx
+++ web/gui/dashboard/src/App.test.tsx
@@ -2,6 +2,10 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 
+jest.mock("react-redux", () => ({ useStore: () => {} }))
+
+jest.mock("./domains/chart/components/portals", () => {})
+
 it("renders without crashing", () => {
   const div = document.createElement("div")
   ReactDOM.render(<App />, div)
