cc479a0f7214c9d24d9edf9d4678aeb0fc195b1d
Kyle Agronick <kyle.agronick@cvscaremark.com>
2019-08-14 16:32:56 -0400

    Made changes requested in pull request #6567

--- collectors/python.d.plugin/gearman/gearman.chart.py
+++ collectors/python.d.plugin/gearman/gearman.chart.py
@@ -7,7 +7,6 @@
 from bases.FrameworkServices.SocketService import SocketService
 from copy import deepcopy
 
-update_every = 3
 priority = 60500
 
 
@@ -15,7 +14,7 @@ CHARTS = {
     'total_workers': {
         'options': ['total', 'Total Workers', 'Workers', 'Total Workers', 'gearman.total', 'line'],
         'lines': [
-            ['total_queued', 'Queued', 'absolute'],
+            ['total_pending', 'Pending', 'absolute'],
             ['total_running', 'Running', 'absolute'],
         ]
     },
@@ -26,12 +25,37 @@ def job_chart_template(job_name):
     return {
         'options': [job_name, job_name, 'Workers', 'Jobs', 'gearman.job', 'stacked'],
         'lines': [
-            ['{0}_queued'.format(job_name), 'Queued', 'absolute'],
+            ['{0}_pending'.format(job_name), 'Pending', 'absolute'],
             ['{0}_idle'.format(job_name), 'Idle', 'absolute'],
             ['{0}_running'.format(job_name), 'Running', 'absolute'],
         ]
     }
 
+def build_result_dict(job):
+    """
+    Get the status for each job
+    :return: dict
+    """
+
+    total, running, available = job['metrics']
+
+    idle = available - running
+    pending = total - running
+
+    return {
+        '{0}_pending'.format(job['job_name']): pending,
+        '{0}_idle'.format(job['job_name']): idle,
+        '{0}_running'.format(job['job_name']): running,
+    }
+
+def parse_worker_data(job):
+    job_name = job[0]
+    job_metrics = job[1:]
+
+    return {
+        'job_name': job_name,
+        'metrics': job_metrics,
+    }
 
 class Service(SocketService):
     def __init__(self, configuration=None, name=None):
@@ -56,44 +80,33 @@ class Service(SocketService):
         :return: dict
         """
 
-        # There is no way to get an accurate total idle count because
-        # a worker can be assigned to more than one job.
-        output = {
-            'total_queued': 0,
-            'total_idle': 0,
-            'total_running': 0,
-        }
+        active_jobs = self.get_active_jobs()
+        found_jobs, job_data = self.process_jobs(active_jobs)
+        self.remove_stale_jobs(found_jobs)
 
-        found_jobs = set()
+        if job_data:
+            return job_data
 
-        for job in self._get_worker_data():
-            job_name = job[0]
+    def get_active_jobs(self):
+        active_jobs = []
+
+        for job in self.get_worker_data():
+            parsed_job = parse_worker_data(job)
 
             # Gearman does not clean up old jobs
             # We only care about jobs that have
             # some relevant data
-            if any(job[1:]):
+            if not any(parsed_job['metrics']):
+                continue
 
-                if job_name not in self.active_jobs:
-                    self._add_chart(job_name)
+            active_jobs.append(parsed_job)
 
-                job_data = self._build_job(job)
-                output.update(job_data)
-                found_jobs.add(job_name)
+        return active_jobs
 
-                for sum_value in ('queued', 'running', 'idle'):
-                    output['total_{0}'.format(sum_value)] += job_data['{0}_{1}'.format(job_name, sum_value)]
-
-        for to_remove in self.active_jobs - found_jobs:
-            self._remove_chart(to_remove)
-
-        if found_jobs:
-            return output
-
-    def _get_worker_data(self):
+    def get_worker_data(self):
         """
         Split the data returned from Gearman into a list of lists
-        :return: generator
+        :return: list
         """
 
         try:
@@ -105,53 +118,101 @@ class Service(SocketService):
             self.debug("Gearman returned no data")
             return
 
-        for line in sorted([job.split() for job in raw.splitlines()][:-1], key=lambda x: x[0]):
+        job_lines = raw.splitlines()[:-1]
+        job_lines = [job.split() for job in job_lines]
+        job_lines.sort(key=lambda line: line[0])
+
+        for line in job_lines:
             line[1:] = map(int, line[1:])
-            yield line
 
-    def _build_job(self, job):
-        """
-        Get the status for each job
-        :return: dict
-        """
+        return job_lines
 
-        total, running, available = job[1:]
+    def process_jobs(self, active_jobs):
 
-        idle = available - running
-        pending = total - running
-
-        return {
-            '{0}_queued'.format(job[0]): pending,
-            '{0}_idle'.format(job[0]): idle,
-            '{0}_running'.format(job[0]): running,
+        # There is no way to get an accurate total idle count because
+        # a worker can be assigned to more than one job.
+        output = {
+            'total_pending': 0,
+            'total_idle': 0,
+            'total_running': 0,
         }
 
-    def _add_chart(self, job_name):
+        found_jobs = set()
+
+        for parsed_job in active_jobs:
+
+            job_name = self.add_job(parsed_job)
+            found_jobs.add(job_name)
+            job_data = build_result_dict(parsed_job)
+
+            for sum_value in ('pending', 'running', 'idle'):
+                output['total_{0}'.format(sum_value)] += job_data['{0}_{1}'.format(job_name, sum_value)]
+
+            output.update(job_data)
+
+        return found_jobs, output
+
+    def remove_stale_jobs(self, active_job_list):
         """
-        Adds a new job chart
-        :param job_name: The name of the job to add
-        :type job_name: string
+        Removes jobs that have no workers, pending jobs,
+        or running jobs
+        :param active_job_list: The latest list of active jobs
+        :type active_job_list: iterable
         :return: None
         """
 
-        job_key = 'job_{0}'.format(job_name)
-        template = job_chart_template(job_name)
-        new_chart = self.charts.add_chart([job_key] + template['options'])
-        for dimension in template['lines']:
-            new_chart.add_dimension(dimension)
+        for to_remove in self.active_jobs - active_job_list:
+            self.remove_job(to_remove)
 
-        self.active_jobs.add(job_name)
-
-    def _remove_chart(self, job_name):
+    def add_job(self, parsed_job):
         """
-        Removes a job chart
+        Adds a job to the list of active jobs
+        :param parsed_job: A parsed job dict
+        :type parsed_job: dict
+        :return: None
+        """
+
+        def add_chart(job_name):
+            """
+            Adds a new job chart
+            :param job_name: The name of the job to add
+            :type job_name: string
+            :return: None
+            """
+
+            job_key = 'job_{0}'.format(job_name)
+            template = job_chart_template(job_name)
+            new_chart = self.charts.add_chart([job_key] + template['options'])
+            for dimension in template['lines']:
+                new_chart.add_dimension(dimension)
+
+        if parsed_job['job_name'] not in self.active_jobs:
+            add_chart(parsed_job['job_name'])
+            self.active_jobs.add(parsed_job['job_name'])
+
+        return parsed_job['job_name']
+
+    def remove_job(self, job_name):
+        """
+        Removes a job to the list of active jobs
         :param job_name: The name of the job to remove
         :type job_name: string
         :return: None
         """
 
-        job_key = 'job_{0}'.format(job_name)
-        self.charts[job_key].obsolete()
+        def remove_chart(job_name):
+            """
+            Removes a job chart
+            :param job_name: The name of the job to remove
+            :type job_name: string
+            :return: None
+            """
+
+            job_key = 'job_{0}'.format(job_name)
+            self.charts[job_key].obsolete()
+            del self.charts[job_key]
+
+        remove_chart(job_name)
         self.active_jobs.remove(job_name)
 
 
