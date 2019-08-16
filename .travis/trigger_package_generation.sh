bdac2e9c8c4f43cbe072b171b942aa28516be880
Pavlos Emm. Katsoulakis <paul@netdata.rocks>
2019-08-16 09:52:27 +0300

    netdata/packaging: [ci skip] nit

--- .travis/trigger_package_generation.sh
+++ .travis/trigger_package_generation.sh
@@ -45,7 +45,7 @@ push_change
 echo "---- Waiting for ${WAIT_TIME} seconds before triggering next process ----"
 sleep "${WAIT_TIME}"
 
-commit_channge "amd64" "RPM"
+commit_change "amd64" "RPM"
 push_change
 
 echo "---- Done! ----"
