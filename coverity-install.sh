fd9eb73ccc705846eeaf816c590187077a9b7450
Costa Tsaousis <costa@tsaousis.gr>
2019-08-31 21:10:53 +0300

    make coverity-scan.sh usable by hand (#6747)
    * make coverity-scan.sh usable by hand
    
    * updated debug variable and docs
    
    * merge coverity-scan.sh and coverity-install.sh
    
    -- MERGING AND TAKING OVER FURTHER IMPROVEMENTS as agreed --

--- coverity-install.sh
+++ coverity-install.sh
@@ -5,38 +5,4 @@
 #
 # Author: Pavlos Emm. Katsoulakis (paul@netdata.cloud)
 
-token="${COVERITY_SCAN_TOKEN}"
-([ -z "${token}" ] && [ -f .coverity-token ]) && token="$(<.coverity-token)"
-if [ -z "${token}" ]; then
-	echo >&2 "Save the coverity token to .coverity-token or export it as COVERITY_SCAN_TOKEN."
-	exit 1
-fi
-
-covbuild="$(which cov-build 2>/dev/null || command -v cov-build 2>/dev/null)"
-([ -z "${covbuild}" ] && [ -f .coverity-build ]) && covbuild="$(<.coverity-build)"
-if [ ! -z "${covbuild}" ]; then
-	echo >&2 "Coverity already installed, nothing to do!"
-	exit 0
-fi
-
-echo >&2 "Installing coverity..."
-WORKDIR="/opt/coverity-source"
-mkdir -p "${WORKDIR}"
-
-curl -SL --data "token=${token}&project=${REPOSITORY}" https://scan.coverity.com/download/linux64 > "${WORKDIR}/coverity_tool.tar.gz"
-if [ -f "${WORKDIR}/coverity_tool.tar.gz" ]; then
-	tar -x -C "${WORKDIR}" -f "${WORKDIR}/coverity_tool.tar.gz"
-	sudo mv "${WORKDIR}/cov-analysis-linux64-2019.03" /opt/coverity
-	export PATH=${PATH}:/opt/coverity/bin/
-else
-	echo "Failed to download coverity tool tarball!"
-fi
-
-# Validate the installation
-covbuild="$(which cov-build 2>/dev/null || command -v cov-build 2>/dev/null)"
-if [ -z "$covbuild" ]; then
-	echo "Failed to install coverity!"
-	exit 1
-else
-	echo >&2 "Coverity scan installed!"
-fi
+exec ./coverity-scan.sh install "${@}"
