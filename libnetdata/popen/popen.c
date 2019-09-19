77d3bc7ec5c6a3321a90c008a03c13dad74eb4af
Thiago Marques <thiagoftsm@gmail.com>
2019-09-19 16:37:20 +0000

    coverity_20190905: Remove false error
    The condition added to fix Coverity was generating false positives, so we are changing to debug

--- libnetdata/popen/popen.c
+++ libnetdata/popen/popen.c
@@ -68,8 +68,7 @@ static inline FILE *custom_popene(const char *command, volatile pid_t *pidptr, c
     int i;
     for(i = (int) (sysconf(_SC_OPEN_MAX) - 1); i >= 0; i--)
         if(i != STDIN_FILENO && i != STDERR_FILENO)
-            if (fcntl(i, F_SETFD, FD_CLOEXEC) == -1)
-                debug("Error to call fcntl inside custom_peopen");
+            (void)fcntl(i, F_SETFD, FD_CLOEXEC);
 
     if (!posix_spawn_file_actions_init(&fa)) {
         // move the pipe to stdout in the child
