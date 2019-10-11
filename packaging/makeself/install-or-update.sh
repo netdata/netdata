f35c3ef947c2fe64999558e521385fe95b2ca269
Konstantinos Natsakis <5933427+knatsakis@users.noreply.github.com>
2019-10-11 11:19:09 +0300

    Partial fix for #7039
    - Moved updater installation code to packaging/installer/functions.sh
    - packaging/makeself/install-or-update.sh uses above code to install
    netdata updater

--- packaging/makeself/install-or-update.sh
+++ packaging/makeself/install-or-update.sh
@@ -9,6 +9,10 @@ umask 002
 # Be nice on production environments
 renice 19 $$ >/dev/null 2>/dev/null
 
+AUTOUPDATE=0
+NETDATA_PREFIX="/opt/netdata"
+NETDATA_USER_CONFIG_DIR="${NETDATA_PREFIX}/etc/netdata"
+
 # -----------------------------------------------------------------------------
 if [ -d /opt/netdata/etc/netdata.old ]; then
 	progress "Found old etc/netdata directory, reinstating this"
@@ -22,15 +26,13 @@ fi
 
 STARTIT=1
 
-while [ ! -z "${1}" ]
-do
-    if [ "${1}" = "--dont-start-it" ]
-    then
-        STARTIT=0
-    else
-        echo >&2 "Unknown option '${1}'. Ignoring it."
-    fi
-    shift
+while [ "${1}" ]; do
+	case "${1}" in
+		"--dont-start-it") STARTIT=0;;
+		"--auto-update"|"-u") AUTOUPDATE=1;;
+		*) echo >&2 "Unknown option '${1}'. Ignoring it.";;
+	esac
+	shift 1
 done
 
 deleted_stock_configs=0
@@ -135,6 +137,12 @@ install_netdata_logrotate || run_failed "Cannot install logrotate file for netda
 progress "Install netdata at system init"
 
 install_netdata_service || run_failed "Cannot install netdata init service."
+
+
+# -----------------------------------------------------------------------------
+progress "Install netdata updater tool"
+
+install_or_remove_netdata_updater || run_failed "Cannot install netdata updater tool."
 
 
 # -----------------------------------------------------------------------------
