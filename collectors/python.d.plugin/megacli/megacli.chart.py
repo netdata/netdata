2f8f3e68ccf93e4ffbdf4a4031014b353ec0eafe
ilyam8 <ilya@netdata.cloud>
2019-10-16 00:27:17 +0300

    use `sudo -n -l` instead of `sudo -n -v` for sudo check

--- collectors/python.d.plugin/megacli/megacli.chart.py
+++ collectors/python.d.plugin/megacli/megacli.chart.py
@@ -164,7 +164,7 @@ class Megacli:
     def __init__(self):
         self.s = find_binary('sudo')
         self.m = find_binary('megacli') or find_binary('MegaCli')  # Binary on FreeBSD is MegaCli
-        self.sudo_check = [self.s, '-n', '-v']
+        self.sudo_check = [self.s, '-n', '-l']
         self.disk_info = [self.s, '-n', self.m, '-LDPDInfo', '-aAll', '-NoLog']
         self.battery_info = [self.s, '-n', self.m, '-AdpBbuCmd', '-a0', '-NoLog']
 
