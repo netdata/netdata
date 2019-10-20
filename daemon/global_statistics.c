57c3a0e34fc378b8d9cbbbf4073777c8f4c995e5
Markos Fountoulakis <markos.fountoulakis.senior@gmail.com>
2019-10-21 00:38:31 +0300

    Added alarm for dbengine flushing errors

--- daemon/global_statistics.c
+++ daemon/global_statistics.c
@@ -775,6 +775,7 @@ void global_statistics_charts(void) {
             static RRDSET *st_errors = NULL;
             static RRDDIM *rd_fs_errors = NULL;
             static RRDDIM *rd_io_errors = NULL;
+            static RRDDIM *rd_flushing_errors = NULL;
 
             if (unlikely(!st_errors)) {
                 st_errors = rrdset_create_localhost(
@@ -794,12 +795,14 @@ void global_statistics_charts(void) {
 
                 rd_io_errors = rrddim_add(st_errors, "I/O errors", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
                 rd_fs_errors = rrddim_add(st_errors, "FS errors", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
+                rd_flushing_errors = rrddim_add(st_errors, "flushing errors", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
             }
             else
                 rrdset_next(st_errors);
 
             rrddim_set_by_pointer(st_errors, rd_io_errors, (collected_number)stats_array[30]);
             rrddim_set_by_pointer(st_errors, rd_fs_errors, (collected_number)stats_array[31]);
+            rrddim_set_by_pointer(st_errors, rd_flushing_errors, (collected_number)stats_array[34]);
             rrdset_done(st_errors);
         }
 
