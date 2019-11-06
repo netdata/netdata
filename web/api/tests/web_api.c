b554d816f348a1baffa8cc1232196ea30db8e938
Andrew Moss <1043609+amoss@users.noreply.github.com>
2019-11-06 23:00:54 +0100

    Parameterised buffer building function.
    Each endpoint in the API will be checked against a variety of http requests. This function builds the request on
    demand for each test-case and cleans up afterwards. Quick sketch of how this will be hooked into the testgenerator
    for CMocka.
    
    Current problems:
    * Produces incorrect status codes (unit-test or request validation?)
    * Strange sizes being reported during execution of web_client code.
    * Needs to step through partial buffer fills.

--- web/api/tests/web_api.c
+++ web/api/tests/web_api.c
@@ -6,6 +6,7 @@
 #include "../../../web/server/web_client.h"
 #include <setjmp.h>
 #include <cmocka.h>
+#include <stdbool.h>
 
 RRDHOST *__wrap_rrdhost_find_by_hostname(const char *hostname, uint32_t hash)
 {
@@ -94,12 +95,39 @@ struct config netdata_config = {.sections = NULL,
                                 .index = {.avl_tree = {.root = NULL, .compar = appconfig_section_compare},
                                           .rwlock = AVL_LOCK_INITIALIZER}};
 
-char req[] = "GET /api/v1/info HTTP/1.1\r\nHost: localhost:19999\r\nUser-Agent: python-requests/2.21.0\r\n"
-             "Accept-Encoding: gzip, deflate\r\nAccept: */*\r\nConnection: keep-alive\r\n\r\n";
+const char *http_headers[] = {"Host: 254.254.0.1",
+                              "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_" // No ,
+                              "0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36",
+                              "Connection: keep-alive",
+                              "X-Forwarded-For: 1.254.1.251",
+                              "Cookie: _ga=GA1.1.1227576758.1571113676; _gid=GA1.2.1222321739.1573628979",
+                              "X-Requested-With: XMLHttpRequest",
+                              "Accept-Encoding: gzip, deflate",
+                              "Cache-Control: no-cache, no-store"};
+#define MAX_HEADERS (sizeof(http_headers) / (sizeof(const char *)))
 
-static void test_api(void **state)
+static void build_request(struct web_buffer *wb, const char *url, bool use_cr, size_t num_headers)
 {
-    localhost = malloc(sizeof(RRDHOST));
+    buffer_reset(wb);
+    buffer_strcat(wb, "GET ");
+    buffer_strcat(wb, url);
+    buffer_strcat(wb, " HTTP/1.1");
+    if (use_cr)
+        buffer_strcat(wb, "\r");
+    buffer_strcat(wb, "\n");
+    for (size_t i = 0; i < num_headers && i < MAX_HEADERS; i++) {
+        buffer_strcat(wb, http_headers[i]);
+        if (use_cr)
+            buffer_strcat(wb, "\r");
+        buffer_strcat(wb, "\n");
+    }
+    if (use_cr)
+        buffer_strcat(wb, "\r");
+    buffer_strcat(wb, "\n");
+}
+
+static struct web_client *pre_test_setup()
+{
     struct web_client *w = (struct web_client *)malloc(sizeof(struct web_client));
     w->response.data = buffer_create(NETDATA_WEB_RESPONSE_INITIAL_SIZE);
     w->response.header = buffer_create(NETDATA_WEB_RESPONSE_HEADER_SIZE);
@@ -107,19 +135,34 @@ static void test_api(void **state)
     strcpy(w->origin, "*"); // Simulate web_client_create_on_fd()
     w->cookie1[0] = 0;      // Simulate web_client_create_on_fd()
     w->cookie2[0] = 0;      // Simulate web_client_create_on_fd()
+    return w;
+}
 
-    buffer_strcat(w->response.data, req);
-    web_client_process_request(w);
-    assert_int_equal(w->flags & WEB_CLIENT_FLAG_WAIT_RECEIVE, 0);
-
+static void post_test_cleanup(struct web_client *w)
+{
+    buffer_free(w->response.data);
+    buffer_free(w->response.header);
+    buffer_free(w->response.header_output);
     free(w);
-    free(localhost);
+}
+
+static void api_info(void **state)
+{
+    for (size_t i = 0; i < MAX_HEADERS; i++) {
+        struct web_client *w = pre_test_setup();
+        build_request(w->response.data, "/api/v1/info", true, 9001);
+        web_client_process_request(w);
+        assert_int_equal(w->flags & WEB_CLIENT_FLAG_WAIT_RECEIVE, 0);
+        post_test_cleanup(w);
+    }
 }
 
 int main(void)
 {
-    const struct CMUnitTest tests[] = {cmocka_unit_test(test_api)};
+    localhost = malloc(sizeof(RRDHOST));
+    const struct CMUnitTest tests[] = {cmocka_unit_test(api_info)};
     debug_flags = 0xffffffffffff;
 
+    free(localhost);
     return cmocka_run_group_tests_name("web_api", tests, NULL, NULL);
 }
