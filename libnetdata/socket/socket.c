6fed38f0e5830360eaa6d24ced7a1b292c03d4e6
Thiago Marques <thiagoftsm@gmail.com>
2019-11-10 22:17:07 +0000

    stream_tcp: Invalid format
    When the user adds two '/' to connect string Netdata parser
    cannot connect with the server and gives a message that does not carry the necessary information to fix.
    This commit fixes this problem

--- libnetdata/socket/socket.c
+++ libnetdata/socket/socket.c
@@ -846,6 +846,13 @@ int connect_to_one_of(const char *destination, int default_port, struct timeval 
         // is there anything?
         if(!*s || s == e) break;
 
+        if (e && e[0] == '/' && (e+1) && e[1] == '/') {
+            error("A destiny specified %s does not have the correct format [PROTOCOL:]IP[%INTERFACE][:PORT]"
+                  , s);
+            sock = -1;
+            break;
+        }
+
         char buf[e - s + 1];
         strncpyz(buf, s, e - s);
         if(reconnects_counter) *reconnects_counter += 1;
