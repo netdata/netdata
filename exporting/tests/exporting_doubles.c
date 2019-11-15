3958bd3871a03ae1ef64685895936ced6c8f11a1
Vladimir Kobal <vlad@prokk.net>
2019-11-15 13:35:01 +0200

    Split tests into multiple files

---
+++ exporting/tests/exporting_doubles.c
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "test_exporting_engine.h"
+
+struct engine *__real_read_exporting_config();
+struct engine *__wrap_read_exporting_config()
+{
+    function_called();
+    return mock_ptr_type(struct engine *);
+}
+
+struct engine *__mock_read_exporting_config()
+{
+    struct engine *engine = calloc(1, sizeof(struct engine));
+    engine->config.prefix = strdupz("netdata");
+    engine->config.hostname = strdupz("test-host");
+    engine->config.update_every = 3;
+    engine->config.options = BACKEND_SOURCE_DATA_AVERAGE | BACKEND_OPTION_SEND_NAMES;
+
+    engine->connector_root = calloc(1, sizeof(struct connector));
+    engine->connector_root->config.type = BACKEND_TYPE_GRAPHITE;
+    engine->connector_root->engine = engine;
+
+    engine->connector_root->instance_root = calloc(1, sizeof(struct instance));
+    struct instance *instance = engine->connector_root->instance_root;
+    instance->connector = engine->connector_root;
+    instance->config.destination = strdupz("localhost");
+    instance->config.update_every = 1;
+    instance->config.buffer_on_failures = 10;
+    instance->config.timeoutms = 10000;
+    instance->config.charts_pattern = strdupz("*");
+    instance->config.hosts_pattern = strdupz("localhost *");
+    instance->config.send_names_instead_of_ids = 1;
+
+    return engine;
+}
+
+int __real_init_connectors(struct engine *engine);
+int __wrap_init_connectors(struct engine *engine)
+{
+    function_called();
+    check_expected_ptr(engine);
+    return mock_type(int);
+}
+
+int __wrap_mark_scheduled_instances(struct engine *engine)
+{
+    function_called();
+    check_expected_ptr(engine);
+    return mock_type(int);
+}
+
+int __real_prepare_buffers(struct engine *engine);
+int __wrap_prepare_buffers(struct engine *engine)
+{
+    function_called();
+    check_expected_ptr(engine);
+    return mock_type(int);
+}
+
+int __wrap_notify_workers(struct engine *engine)
+{
+    function_called();
+    check_expected_ptr(engine);
+    return mock_type(int);
+}
+
+int __wrap_send_internal_metrics(struct engine *engine)
+{
+    function_called();
+    check_expected_ptr(engine);
+    return mock_type(int);
+}
+
+int __mock_start_batch_formatting(struct instance *instance)
+{
+    function_called();
+    check_expected_ptr(instance);
+    return mock_type(int);
+}
+
+int __mock_start_host_formatting(struct instance *instance)
+{
+    function_called();
+    check_expected_ptr(instance);
+    return mock_type(int);
+}
+
+int __mock_start_chart_formatting(struct instance *instance)
+{
+    function_called();
+    check_expected_ptr(instance);
+    return mock_type(int);
+}
+
+int __mock_metric_formatting(struct instance *instance, RRDDIM *rd)
+{
+    function_called();
+    check_expected_ptr(instance);
+    check_expected_ptr(rd);
+    return mock_type(int);
+}
+
+int __mock_end_chart_formatting(struct instance *instance)
+{
+    function_called();
+    check_expected_ptr(instance);
+    return mock_type(int);
+}
+
+int __mock_end_host_formatting(struct instance *instance)
+{
+    function_called();
+    check_expected_ptr(instance);
+    return mock_type(int);
+}
+
+int __mock_end_batch_formatting(struct instance *instance)
+{
+    function_called();
+    check_expected_ptr(instance);
+    return mock_type(int);
+}
---
+++ exporting/tests/netdata_doubles.c
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "test_exporting_engine.h"
+
+// Use memomy allocation functions guarded by CMocka in strdupz
+const char *__wrap_strdupz(const char *s)
+{
+    char *duplicate = malloc(sizeof(char) * (strlen(s) + 1));
+    strcpy(duplicate, s);
+
+    return duplicate;
+}
+
+time_t __wrap_now_realtime_sec(void)
+{
+    function_called();
+    return mock_type(time_t);
+}
+
+void __wrap_info_int(const char *file, const char *function, const unsigned long line, const char *fmt, ...)
+{
+    (void)file;
+    (void)function;
+    (void)line;
+
+    function_called();
+
+    va_list args;
+
+    va_start(args, fmt);
+    vsnprintf(log_line, MAX_LOG_LINE, fmt, args);
+    va_end(args);
+}
+
+int __wrap_connect_to_one_of(
+    const char *destination,
+    int default_port,
+    struct timeval *timeout,
+    size_t *reconnects_counter,
+    char *connected_to,
+    size_t connected_to_size)
+{
+    (void)timeout;
+
+    function_called();
+
+    check_expected(destination);
+    check_expected_ptr(default_port);
+    // TODO: check_expected_ptr(timeout);
+    check_expected(reconnects_counter);
+    check_expected(connected_to);
+    check_expected(connected_to_size);
+
+    return mock_type(int);
+}
+
+void __rrdhost_check_rdlock(RRDHOST *host, const char *file, const char *function, const unsigned long line)
+{
+    (void)host;
+    (void)file;
+    (void)function;
+    (void)line;
+}
+
+void __rrdset_check_rdlock(RRDSET *st, const char *file, const char *function, const unsigned long line)
+{
+    (void)st;
+    (void)file;
+    (void)function;
+    (void)line;
+}
+
+void __rrd_check_rdlock(const char *file, const char *function, const unsigned long line)
+{
+    (void)file;
+    (void)function;
+    (void)line;
+}
