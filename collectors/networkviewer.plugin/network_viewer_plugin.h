a073352ff6ac6ddd8c84667ea7fc7ef814ea0c27
Thiago Marques <thiagoftsm@gmail.com>
2019-11-18 02:11:49 +0000

    network_viewer: Network introduction
    I brought to the header the structures that will be used to work with information from the kernel

--- collectors/networkviewer.plugin/network_viewer_plugin.h
+++ collectors/networkviewer.plugin/network_viewer_plugin.h
@@ -1,7 +1,9 @@
 #ifndef _NETDATA_NETWORK_VIEWER_H_
 # define _NETDATA_NETWORK_VIEWER_H_ 1
 
-# include <linux/perf_event.h>
+# ifndef __FreeBSD__
+#   include <linux/perf_event.h>
+# endif
 # include <stdint.h>
 # include <errno.h>
 # include <signal.h>
@@ -12,11 +14,23 @@
 # include <unistd.h>
 # include <dlfcn.h>
 
+# include <arpa/inet.h>
+# include <sys/socket.h>
+# include <sys/types.h>
+
+# ifndef __FreeBSD__
+#   include <linux/netlink.h>
+#   include <linux/rtnetlink.h>
+# endif
+
+# include <netinet/in.h>
+# include <ifaddrs.h>
+
 # include <sys/sysinfo.h>
 
-typedef struct
-{
-    uint64_t pid;
+typedef struct {
+    uint64_t first;
+    uint64_t ct;
     uint32_t saddr;
     uint32_t daddr;
     uint16_t dport;
@@ -26,7 +40,16 @@ typedef struct
     uint8_t protocol;
 }netdata_conn_stats_t;
 
-# define NETDATA_MAX_PROCESSOR 12
-# define NETWORK_VIEWER_MAX_CONNECTIONS 8192
+typedef struct netdata_network
+{
+    struct netdata_network *next;
+    in_addr_t ipv4addr;
+    in_addr_t netmask;
+    in_addr_t router;
+    int isloopback;
+} netdata_network_t;
+
+
+# define NETDATA_MAX_PROCESSOR 128
 
 #endif
