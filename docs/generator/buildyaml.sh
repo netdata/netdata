1f6f2f364a60da6f041f1da4bae44417f9aa7470
Joel Hans <joel.g.hans@gmail.com>
2019-12-16 11:25:47 -0700

    Improve styling and add Netdata logo

--- docs/generator/buildyaml.sh
+++ docs/generator/buildyaml.sh
@@ -73,6 +73,7 @@ theme:
       accent: "light green"
     custom_dir: custom/themes/material
     favicon: custom/img/favicon.ico
+    logo: custom/img/netdata_logo.svg
     language: '${language}'
 extra_css:
   - "https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css"
