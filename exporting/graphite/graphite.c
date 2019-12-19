f988e1bbbfcc501f1fcddd6f330da591361a19ea
Vladimir Kobal <vlad@prokk.net>
2019-12-19 13:19:22 +0200

    Fix host labels in the Graphite exporting connector

--- exporting/graphite/graphite.c
+++ exporting/graphite/graphite.c
@@ -51,21 +51,9 @@ int init_graphite_instance(struct instance *instance)
     return 0;
 }
 
-static inline void sanitize_graphite_label_key(char *dst, char *src, size_t len) {
-    while (*src != '\0' && len) {
-        if (*src == ';' || *src == '!' || *src == '^' || *src == '=')
-            *dst++ = '_';
-        else
-            *dst++ = *src;
-        src++;
-        len--;
-    }
-    *dst = '\0';
-}
-
 static inline void sanitize_graphite_label_value(char *dst, char *src, size_t len) {
     while (*src != '\0' && len) {
-        if (*src == ';' || *src == '~')
+        if (isspace(*src) || *src == ';' || *src == '~')
             *dst++ = '_';
         else
             *dst++ = *src;
@@ -100,15 +88,12 @@ int format_host_labels_graphite_plaintext(struct instance *instance, RRDHOST *ho
                label->label_source != LABEL_SOURCE_NETDATA_CONF)))
             continue;
 
-        char key[CONFIG_MAX_NAME + 1];
-        sanitize_graphite_label_key(key, label->key, CONFIG_MAX_NAME);
-
         char value[CONFIG_MAX_VALUE + 1];
         sanitize_graphite_label_value(value, label->value, CONFIG_MAX_VALUE);
 
         if (*value) {
             buffer_strcat(instance->labels, ";");
-            buffer_sprintf(instance->labels, "%s=%s", key, value);
+            buffer_sprintf(instance->labels, "%s=%s", label->key, value);
         }
     }
     netdata_rwlock_unlock(&host->labels_rwlock);
