a8d90801c5f2aad966c2667615afcaf1dd0d9428
James Mills <1290234+prologic@users.noreply.github.com>
2020-01-14 12:46:18 +1000

    Fix minor typo(s)/grammar with safe_256sum() function

--- packaging/installer/kickstart-static64.sh
+++ packaging/installer/kickstart-static64.sh
@@ -155,8 +155,8 @@ set_tarball_urls() {
 }
 
 safe_sha256sum() {
-	# Within the contexct of the installer, we only use -c option that is common between the two commands
-	# We will have to reconsider if we start non-common options
+	# Within the context of the installer, we only use -c option that is common between the two commands
+	# We will have to reconsider if we start using non-common options
 	if command -v sha256sum >/dev/null 2>&1; then
 		sha256sum $@
 	elif command -v shasum >/dev/null 2>&1; then
