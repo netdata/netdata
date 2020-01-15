64ebcbdac3469cc773f0e15ecdcb486a8d498420
Andrew Moss <1043609+amoss@users.noreply.github.com>
2020-01-15 22:10:13 +0100

    Replaced the bin/clean_install to match new system.

--- build_external/bin/clean_install.sh
+++ build_external/bin/clean_install.sh
@@ -3,38 +3,20 @@
 # Basic setup, check we have a legal Distro / Version combo
 Distro=$1
 Version=$2
-OutBase=$(cd $(dirname $0) && cd .. && pwd)
-InBase=/opt/netdata
-ImageNameBefore="$Distro"_"$Version"_preinst
-ImageNameAfter="$Distro"_"$Version"_dev
-Dockerfile=$OutBase/"$Distro"_"$Version"_preinst.Dockerfile
-AbsSrc=$(readlink $OutBase/netdata || echo $OutBase/netdata)
+Repo=$(cd $(dirname $0) && cd ../.. && pwd)
+Base=$Repo/build_external
+Arch=$(uname)
 
-if [ ! -e "$Dockerfile" ]
-then
-  echo "Can't run installer on $Distro/$Version - $Dockerfile not found"
-  return -1
+TagName=clean_install_"$Distro"_"$Version":latest
+Dockerfile=$Base/clean_install_"$Distro"_"$Version".Dockerfile
+
+if [ ! -e "$Dockerfile" ]; then
+    echo "Can't run installer on $Distro/$Version - $Dockerfile not found"
+    exit -1
 fi
 
-# Create the build-state and store the current config.
-rm -rf $AbsSrc/build_external
-mkdir $AbsSrc/build_external
-cat <<EOF >$AbsSrc/build_external/clean_install.sh
-#!/usr/bin/env bash
-$InBase/source/netdata-installer.sh --install $InBase/install --dont-wait
-ln -sf /dev/stdout $InBase/install/netdata/var/log/netdata/access.log
-ln -sf /dev/stdout $InBase/install/netdata/var/log/netdata/debug.log
-ln -sf /dev/stderr $InBase/install/netdata/var/log/netdata/error.log
-EOF
-chmod +x $AbsSrc/build_external/clean_install.sh
-echo "$Distro"_"$Version" > $AbsSrc/build_external/current
-
-# Check the tag name is vacant.
-docker rm $ImageNameAfter 2>/dev/null
-
-# Run the install and wire up the logs as container output.
-docker run -it --mount type=bind,source="$AbsSrc",target=$InBase/source \
-           --name $ImageNameAfter \
-           -w $InBase/source $ImageNameBefore \
-           $InBase/source/build_external/clean_install.sh
-docker commit $ImageNameAfter $ImageNameAfter:latest
+if [ "$Arch" == "Linux" ]; then
+    sudo docker build -f "$Dockerfile" "$Repo" -t "$TagName"
+elif [ "$Arch" == "Darwin" ]; then
+    docker build -f "$Dockerfile" "$Repo" -t "$TagName"
+fi
