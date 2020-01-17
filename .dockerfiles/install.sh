f65197eb35d89bb64e4de5d2b83e0ed6c50f5fb6
James Mills <1290234+prologic@users.noreply.github.com>
2020-01-17 14:07:02 +1000

    Added support for openSUSE

--- .dockerfiles/install.sh
+++ .dockerfiles/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-install_deb() {
+install_debian_like() {
 	# This is needed to ensure package installs don't prompt for any user input.
 	ENV DEBIAN_FRONTEND=noninteractive
 
@@ -14,7 +14,7 @@ install_deb() {
 		curl netcat jq
 }
 
-install_rpm() {
+install_fedora_like() {
 	# Using a glob pattern here because I can't reliably determine what the
 	# resulting package name will be (TODO: There must be a better way!)
 
@@ -25,12 +25,29 @@ install_rpm() {
 	dnf install -y curl nc jq
 }
 
+install_suse_like() {
+	# Using a glob pattern here because I can't reliably determine what the
+	# resulting package name will be (TODO: There must be a better way!)
+
+	# Install NetData
+	# FIXME: Allow unsigned packages (for now) #7773
+	zypper install -y --allow-unsigned-rpm \
+		/artifacts/netdata-"${VERSION}"-*.rpm
+
+	# Install testing tools
+	zypper install -y --no-recommends \
+		curl netcat jq
+}
+
 case "${DISTRO}" in
 debian | ubuntu)
-	install_deb
+	install_debian_like
 	;;
 fedora | centos)
-	install_rpm
+	install_fedora_like
+	;;
+opensuse)
+	install_suse_like
 	;;
 *)
 	printf "ERROR: unspported distro: %s_%s\n" "${DISTRO}" "${DISTRO_VERSION}"
