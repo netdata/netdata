fdee75d1614af65a1f877796e75e53b653221bea
Thiago Marques <thiagoftsm@gmail.com>
2020-01-24 13:14:43 +0000

    stream_forward: Fix old slave support
    This commit fixes the communication with old versions of Netdata

--- streaming/rrdpush.c
+++ streaming/rrdpush.c
@@ -1174,13 +1174,12 @@ static int rrdpush_receive(int fd
 
     info("STREAM %s [receive from [%s]:%s]: initializing communication...", host->hostname, client_ip, client_port);
     char initial_response[HTTP_HEADER_SIZE];
-    if (stream_flags & LABEL_FLAG_UPDATE_STREAM) {
+    if (stream_flags & STREAM_HAS_INTEGER_VERSION) {
         info("STREAM %s [receive from [%s]:%s]: Netdata is using the newest stream protocol.", host->hostname, client_ip, client_port);
-        if (!host->stream_version)
-            sprintf(initial_response, "%s", START_STREAMING_PROMPT_V2);
-        else {
-            sprintf(initial_response, "%s%u", START_STREAMING_PROMPT_VN, STREAMING_PROTOCOL_CURRENT_VERSION);
-        }
+        sprintf(initial_response, "%s%u", START_STREAMING_PROMPT_VN, STREAMING_PROTOCOL_CURRENT_VERSION);
+    } else if (stream_flags & LABEL_FLAG_UPDATE_STREAM) {
+        info("STREAM %s [receive from [%s]:%s]: Netdata is using the second version of stream protocol.", host->hostname, client_ip, client_port);
+        sprintf(initial_response, "%s", START_STREAMING_PROMPT_V2);
     } else {
         info("STREAM %s [receive from [%s]:%s]: Netdata is using an old protocol.", host->hostname, client_ip, client_port);
         sprintf(initial_response, "%s", START_STREAMING_PROMPT);
@@ -1287,6 +1286,7 @@ struct rrdpush_thread {
     struct rrdhost_system_info *system_info;
     int update_every;
     int stream_flags;
+    uint32_t stream_version;
 #ifdef ENABLE_HTTPS
     struct netdata_ssl ssl;
 #endif
@@ -1391,6 +1391,7 @@ int rrdpush_receiver_thread_spawn(RRDHOST *host, struct web_client *w, char *url
 
     char *key = NULL, *hostname = NULL, *registry_hostname = NULL, *machine_guid = NULL, *os = "unknown", *timezone = "unknown", *tags = NULL;
     int update_every = default_rrd_update_every;
+    uint32_t stream_version = 0;
     char buf[GUID_LEN + 1];
     int stream_flags = LABEL_FLAG_STOP_STREAM;
 
@@ -1420,9 +1421,12 @@ int rrdpush_receiver_thread_spawn(RRDHOST *host, struct web_client *w, char *url
             timezone = value;
         else if(!strcmp(name, "tags"))
             tags = value;
-        else {
+        else if(!strcmp(name, "ver")) {
+            stream_flags = STREAM_HAS_INTEGER_VERSION;
+            stream_version = (uint32_t) strtoul(value, NULL, 0);
+        } else {
             if(!strcmp(name, "NETDATA_PROTOCOL_VERSION"))
-                stream_flags = LABEL_FLAG_UPDATE_STREAM;
+                stream_flags = (stream_version)?LABEL_FLAG_UPDATE_STREAM | STREAM_HAS_INTEGER_VERSION:LABEL_FLAG_UPDATE_STREAM;
             else {
                 // An old Netdata slave does not have a compatible streaming protocol, map to something sane.
                 if (!strcmp(name, "NETDATA_SYSTEM_OS_NAME"))
@@ -1557,6 +1561,7 @@ int rrdpush_receiver_thread_spawn(RRDHOST *host, struct web_client *w, char *url
     rpt->update_every      = update_every;
     rpt->system_info       = system_info;
     rpt->stream_flags      = stream_flags;
+    rpt->stream_version    = stream_version;
 #ifdef ENABLE_HTTPS
     rpt->ssl.conn          = w->ssl.conn;
     rpt->ssl.flags         = w->ssl.flags;
