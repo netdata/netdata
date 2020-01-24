3990182613ceac1a87b7624b1a883c8b66eba3f0
Austin S. Hemmelgarn <austin@netdata.cloud>
2020-01-24 09:09:47 -0500

    Fix parsing of `lscpu` output.
    This properly recognizes the CPU frequency value as MHz and truncates
    the value to an integer.

--- daemon/system-info.sh
+++ daemon/system-info.sh
@@ -186,12 +186,12 @@ dmidecode="$(command -v dmidecode)"
 dmidecode_output=""
 
 if [ -n "${lscpu}" ] && lscpu >/dev/null 2>&1 ; then
-        lscpu_output="$(${lscpu} 2>/dev/null)"
+        lscpu_output="$(LC_NUMERIC=C ${lscpu} 2>/dev/null)"
         CPU_INFO_SOURCE="lscpu"
         LCPU_COUNT="$(echo "${lscpu_output}" | grep "^CPU(s):" | cut -f 2 -d ':' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
         CPU_VENDOR="$(echo "${lscpu_output}" | grep "^Vendor ID:" | cut -f 2 -d ':' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
         CPU_MODEL="$(echo "${lscpu_output}" | grep "^Model name:" | cut -f 2 -d ':' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
-        possible_cpu_freq="$(echo "${lscpu_output}" | grep -F "CPU max MHz:" | cut -f 2 -d ':' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+        possible_cpu_freq="$(echo "${lscpu_output}" | grep -F "CPU max MHz:" | cut -f 2 -d ':' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | grep -o '^[0-9]*') MHz"
 elif [ -n "${dmidecode}" ] && dmidecode -t processor >/dev/null 2>&1 ; then
         dmidecode_output="$(${dmidecode} -t processor 2>/dev/null)"
         CPU_INFO_SOURCE="dmidecode"
