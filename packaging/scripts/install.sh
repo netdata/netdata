50e8f5215044505aa939757e7825d04818ae85ce
James Mills <1290234+prologic@users.noreply.github.com>
2020-01-24 11:17:26 +1000

    Re-update test/install shell scripts to be POSIX. We don't really want to depend on Bash here

--- packaging/scripts/install.sh
+++ packaging/scripts/install.sh
@@ -1,56 +1,56 @@
 #!/bin/sh
 
 install_debian_like() {
-	# This is needed to ensure package installs don't prompt for any user input.
-	export DEBIAN_FRONTEND=noninteractive
+  # This is needed to ensure package installs don't prompt for any user input.
+  export DEBIAN_FRONTEND=noninteractive
 
-	apt-get update
+  apt-get update
 
-	# Install NetData
-	apt-get install -y "/artifacts/netdata_${VERSION}_${ARCH}.deb"
+  # Install NetData
+  apt-get install -y "/artifacts/netdata_${VERSION}_${ARCH}.deb"
 
-	# Install testing tools
-	apt-get install -y --no-install-recommends \
-		curl netcat jq
+  # Install testing tools
+  apt-get install -y --no-install-recommends \
+    curl netcat jq
 }
 
 install_fedora_like() {
-	# Using a glob pattern here because I can't reliably determine what the
-	# resulting package name will be (TODO: There must be a better way!)
+  # Using a glob pattern here because I can't reliably determine what the
+  # resulting package name will be (TODO: There must be a better way!)
 
-	# Install NetData
-	dnf install -y /artifacts/netdata-"${VERSION}"-*.rpm
+  # Install NetData
+  dnf install -y /artifacts/netdata-"${VERSION}"-*.rpm
 
-	# Install testing tools
-	dnf install -y curl nc jq
+  # Install testing tools
+  dnf install -y curl nc jq
 }
 
 install_suse_like() {
-	# Using a glob pattern here because I can't reliably determine what the
-	# resulting package name will be (TODO: There must be a better way!)
+  # Using a glob pattern here because I can't reliably determine what the
+  # resulting package name will be (TODO: There must be a better way!)
 
-	# Install NetData
-	# FIXME: Allow unsigned packages (for now) #7773
-	zypper install -y --allow-unsigned-rpm \
-		/artifacts/netdata-"${VERSION}"-*.rpm
+  # Install NetData
+  # FIXME: Allow unsigned packages (for now) #7773
+  zypper install -y --allow-unsigned-rpm \
+    /artifacts/netdata-"${VERSION}"-*.rpm
 
-	# Install testing tools
-	zypper install -y --no-recommends \
-		curl netcat jq
+  # Install testing tools
+  zypper install -y --no-recommends \
+    curl netcat jq
 }
 
 case "${DISTRO}" in
-debian | ubuntu)
-	install_debian_like
-	;;
-fedora | centos)
-	install_fedora_like
-	;;
-opensuse)
-	install_suse_like
-	;;
-*)
-	printf "ERROR: unspported distro: %s_%s\n" "${DISTRO}" "${DISTRO_VERSION}"
-	exit 1
-	;;
+  debian | ubuntu)
+    install_debian_like
+    ;;
+  fedora | centos)
+    install_fedora_like
+    ;;
+  opensuse)
+    install_suse_like
+    ;;
+  *)
+    printf "ERROR: unspported distro: %s_%s\n" "${DISTRO}" "${DISTRO_VERSION}"
+    exit 1
+    ;;
 esac
--- packaging/scripts/test.sh
+++ packaging/scripts/test.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 
 wait_for() {
-	host="${1}"
-	port="${2}"
-	name="${3}"
-	timeout="${4:-30}"
+  host="${1}"
+  port="${2}"
+  name="${3}"
+  timeout="${4:-30}"
 
-	printf "Waiting for %s on %s:%s ... " "${name}" "${host}" "${port}"
+  printf "Waiting for %s on %s:%s ... " "${name}" "${host}" "${port}"
 
-	i=0
-	while ! nc -z "${host}" "${port}"; do
-		sleep 1
-		if [ "$((++i))" -gt "$timeout" ]; then
-			printf "Timed out!\n"
-			return 1
-		fi
-	done
-	printf "OK\n"
+  i=0
+  while ! nc -z "${host}" "${port}"; do
+    sleep 1
+    if [ "$i" -gt "$timeout" ]; then
+      printf "Timed out!\n"
+      return 1
+    fi
+    i="$((i + 1))"
+  done
+  printf "OK\n"
 }
 
-netdata -D >netdata.log 2>&1 &
+netdata -D > netdata.log 2>&1 &
 
 wait_for localhost 19999 netdata
 
