714f1d0e36bf239f633321968a55d0ba7e57d221
ilyam8 <ilya@netdata.cloud>
2020-02-01 20:45:19 +0300

    netdata-installer.sh: should_install_go simplify

--- netdata-installer.sh
+++ netdata-installer.sh
@@ -802,26 +802,36 @@ fi
 
 # -----------------------------------------------------------------------------
 
-# govercomp compares two versions. This function exit codes:
+# govercomp compares go.d.plugin versions. Exit codes:
 # 0 - version1 == version2
 # 1 - version1 > version2
 # 2 - version2 > version1
 # 3 - error
-# Expected version format: dot separated numbers.
 govercomp() {
-  if [[ "$1" == "$2" ]]; then
-    return 0
-  fi
+  # version in file:
+  # - v0.14.0
+  #
+  # 'go.d.plugin -v' output variants:
+  # - go.d.plugin, version: unknown
+  # - go.d.plugin, version: v0.14.1
+  # - go.d.plugin, version: v0.14.1-dirty
+  # - go.d.plugin, version: v0.14.1-1-g4c5f98c
+  # - go.d.plugin, version: v0.14.1-1-g4c5f98c-dirty
+  #
+  # we need to compare only MAJOR.MINOR.PATCH part
+  local ver1 ver2
+  ver1=$(echo "$1" | grep -E -o "[0-9]+\.[0-9]+\.[0-9]+")
+  ver2=$(echo "$2" | grep -E -o "[0-9]+\.[0-9]+\.[0-9]+")
 
   local IFS=.
-  local i ver1 ver2
-  read -ra ver1 <<<"$1"
-  read -ra ver2 <<<"$2"
+  read -ra ver1 <<<"$ver1"
+  read -ra ver2 <<<"$ver2"
 
   if [[ ${#ver1[@]} -eq 0 ]] || [[ ${#ver2[@]} -eq 0 ]]; then
     return 3
   fi
 
+  local i
   for ((i = ${#ver1[@]}; i < ${#ver2[@]}; i++)); do
     ver1[i]=0
   done
@@ -843,49 +853,13 @@ govercomp() {
 }
 
 should_install_go() {
-  # 'go.d.plugin -v' output variants:
-  # - go.d.plugin, version: unknown
-  # - go.d.plugin, version: v0.14.1
-  # - go.d.plugin, version: v0.14.1-dirty
-  # - go.d.plugin, version: v0.14.1-1-g4c5f98c
-  # - go.d.plugin, version: v0.14.1-1-g4c5f98c-dirty
-  #
-  # we download if:
-  # - error during determining latest/actual version and parsing
-  # - actual version < latest version
-  #
-  # we dont if:
-  # - actual version = latest version
-  # - actual version > latest version
-  #
-  # we need to compare only MAJOR.MINOR.PATCH part
+  local version_in_file
+  local binary_version
 
-  local latest_version
-  local binary
-  local binary_output
-  local actual_version
+  version_in_file="$(cat packaging/go.d.version 2>/dev/null)"
+  binary_version=$(${NETDATA_PREFIX}/usr/libexec/netdata/plugins.d/go.d.plugin -v 2>/dev/null)
 
-  latest_version="$(grep -E -o "[0-9]+\.[0-9]+\.[0-9]+" packaging/go.d.version 2>/dev/null)"
-  if [[ ! $latest_version ]]; then
-    return 0
-  fi
-
-  binary="${NETDATA_PREFIX}/usr/libexec/netdata/plugins.d/go.d.plugin"
-  if [[ ! -f $binary ]] || [[ ! -x $binary ]]; then
-    return 0
-  fi
-
-  binary_output=$($binary -v 2>/dev/null)
-  if [[ ! $binary_output ]]; then
-    return 0
-  fi
-
-  actual_version=$(echo "$binary_output" | grep -E -o "[0-9]+\.[0-9]+\.[0-9]+")
-  if [[ ! $actual_version ]]; then
-    return 0
-  fi
-
-  govercomp "$latest_version" "$actual_version"
+  govercomp "$version_in_file" "$binary_version"
   case $? in
   0) return 1 ;; # =
   2) return 1 ;; # <
