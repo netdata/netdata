d506a7c3e1a8a54e309f2ee0e6cbf806d1322242
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2020-02-03 10:06:03 +0200

    - Remove commented code

--- database/rrdcalc.c
+++ database/rrdcalc.c
@@ -106,8 +106,6 @@ static void rrdsetcalc_link(RRDSET *st, RRDCALC *rc) {
         );
         health_alarm_log(host, ae);
     }
-    //info("Adding CHART for alarm %s - %s", st->id, rc->name);
-    //aclk_queue_query("_chart", host->hostname, "chart", st->id, 2, 0, 0);
 }
 
 static inline int rrdcalc_is_matching_this_rrdset(RRDCALC *rc, RRDSET *st) {
