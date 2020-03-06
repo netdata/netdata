ae04e504ec66193517ff3ef207e5a4c7511465dc
James Mills <prologic@shortcircuit.net.au>
2020-03-06 15:00:50 +1000

    Use GHA path filters

--- tests/run-unit-tests.sh
+++ tests/run-unit-tests.sh
@@ -34,58 +34,6 @@ c_unit_tests() {
   "$HOME"/netdata/usr/sbin/netdata -W unittest
 }
 
-run_c_unit_tests=
+install_netdata || exit 1
 
-if [ -z ${TRAVIS_COMMIT_RANGE} ]; then
-  # Travis gave us no commit range, so just run all the unit tests.
-  # Per the docs, this is the case when a new branch is pushed for the first time.
-  echo "No commit range supplied, assuming the worst case and running all unit tests."
-  run_c_unit_tests=1
-else
-  changed_paths=
-
-  if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    # This is not a PR build.
-    COMMIT1="$(echo ${TRAVIS_COMMIT_RANGE} | cut -f 1 -d '.')"
-    COMMIT2="$(echo ${TRAVIS_COMMIT_RANGE} | cut -f 4 -d '.')"
-
-    if [ "$(git cat-file -t ${COMMIT1} 2> /dev/null)" = commit -a "$(git cat-file -t ${COMMIT2} 2> /dev/null)" = commit ]; then
-      # Examine the exact set of commits passed by Travis.
-      echo "Checking commits:"
-      git log --format=oneline --abbrev-commit ${COMMIT1}..${COMMIT2}
-      changed_paths="$(git diff --name-only ${COMMIT1}..${COMMIT2} --)"
-    else
-      # We couldn't find at least one of the changesets, so this build
-      # was probably triggered by a history rewrite. Since we can't
-      # figure out what chnaged, we need to just run all the tests anyway.
-      echo "Cannot determine which commits we are testing, running all unit tests."
-      run_c_unit_tests=1
-    fi
-  else
-    # This is a PR build, look at all commits from the target branch
-    # to HEAD.
-    echo "Checking commits:"
-    git log --format=oneline --abbrev-commit ${TRAVIS_BRANCH}..HEAD
-    changed_paths="$(git diff --name-only ${TRAVIS_BRANCH}..HEAD --)"
-  fi
-
-  if [ -n "${changed_paths}" ]; then
-    # Check for changes that would require the C code to be re-tested
-    if (echo ${changed_paths} | grep -qE "daemon/unit_test|database"); then
-      echo "Commits appear to change C code with unit tests, queueing C unit tests."
-      run_c_unit_tests=1
-    fi
-  fi
-fi
-
-if [ -z ${run_c_unit_tests} ]; then
-  # No tests to run, log this and exit with success
-  echo "Commit range ${TRAVIS_COMMIT_RANGE} appears to make no changes that require unit tests, skipping unit testing."
-  exit 0
-else
-  install_netdata || exit 1
-
-  if [ -n ${run_c_unit_tests} ]; then
-    c_unit_tests || exit 1
-  fi
-fi
+c_unit_tests || exit 1
