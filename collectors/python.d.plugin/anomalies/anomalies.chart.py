38929b4f8d989dfcda2b77629ddd549064beca2d
Andrew Maguire <andrewm4894@gmail.com>
2020-03-26 14:11:36 +0000

    try pull in some real data from the api

--- collectors/python.d.plugin/anomalies/anomalies.chart.py
+++ collectors/python.d.plugin/anomalies/anomalies.chart.py
@@ -28,51 +28,51 @@ CHARTS = {
     'cpu': {
         'options': [None, 'CPU Anomaly Scores', 'value', 'cpu', 'anomalies.cpu', 'line'],
         'lines': [
-            ['cpu_expected', 'cpu expected', 'absolute', 1, 100],
-            ['cpu_actual', 'cpu actual', 'absolute', 1, 100],
-            ['cpu_error', 'error', 'absolute', 1, 100]
+            ['cpu_expected', 'cpu expected', 'absolute', 1, 1000],
+            ['cpu_actual', 'cpu actual', 'absolute', 1, 1000],
+            ['cpu_error', 'error', 'absolute', 1, 1000]
         ]
     },
     'load': {
         'options': [None, 'Load Anomaly Scores', 'value', 'load', 'anomalies.load', 'line'],
         'lines': [
-            ['load_expected', 'load expected', 'absolute', 1, 100],
-            ['load_actual', 'load actual', 'absolute', 1, 100],
-            ['load_error', 'error', 'absolute', 1, 100]
+            ['load_expected', 'load expected', 'absolute', 1, 1000],
+            ['load_actual', 'load actual', 'absolute', 1, 1000],
+            ['load_error', 'error', 'absolute', 1, 1000]
         ]
     },
     'ram': {
         'options': [None, 'RAM Anomaly Scores', 'value', 'ram', 'anomalies.ram', 'line'],
         'lines': [
-            ['ram_expected', 'ram expected', 'absolute', 1, 100],
-            ['ram_actual', 'ram actual', 'absolute', 1, 100],
-            ['ram_error', 'error', 'absolute', 1, 100]
+            ['ram_expected', 'ram expected', 'absolute', 1, 1000],
+            ['ram_actual', 'ram actual', 'absolute', 1, 1000],
+            ['ram_error', 'error', 'absolute', 1, 1000]
         ]
     },
     'io': {
         'options': [None, 'IO Anomaly Scores', 'value', 'io', 'anomalies.io', 'line'],
         'lines': [
-            ['io_expected', 'io expected', 'absolute', 1, 100],
-            ['io_actual', 'io actual', 'absolute', 1, 100],
-            ['io_error', 'error', 'absolute', 1, 100]
+            ['io_expected', 'io expected', 'absolute', 1, 1000],
+            ['io_actual', 'io actual', 'absolute', 1, 1000],
+            ['io_error', 'error', 'absolute', 1, 1000]
         ]
     },
     'net': {
         'options': [None, 'Network Anomaly Scores', 'value', 'net', 'anomalies.net', 'line'],
         'lines': [
-            ['net_expected', 'net expected', 'absolute', 1, 100],
-            ['net_actual', 'net actual', 'absolute', 1, 100],
-            ['net_error', 'error', 'absolute', 1, 100]
+            ['net_expected', 'net expected', 'absolute', 1, 1000],
+            ['net_actual', 'net actual', 'absolute', 1, 1000],
+            ['net_error', 'error', 'absolute', 1, 1000]
         ]
     },
     'scores': {
         'options': [None, 'All Anomaly Scores', 'value', 'scores', 'anomalies.scores', 'line'],
         'lines': [
-            ['cpu_score', 'cpu score', 'absolute', 1, 100],
-            ['load_score', 'load score', 'absolute', 1, 100],
-            ['ram_score', 'ram score', 'absolute', 1, 100],
-            ['io_score', 'io score', 'absolute', 1, 100],
-            ['net_score', 'net score', 'absolute', 1, 100]
+            ['cpu_score', 'cpu score', 'absolute', 1, 1000],
+            ['load_score', 'load score', 'absolute', 1, 1000],
+            ['ram_score', 'ram score', 'absolute', 1, 1000],
+            ['io_score', 'io score', 'absolute', 1, 1000],
+            ['net_score', 'net score', 'absolute', 1, 1000]
         ]
     },
     'anomalies': {
@@ -117,11 +117,11 @@ def get_raw_data(host=None):
         anomalies = 1.0 if error_pct > 0.1 else 0.0
 
         # add data
-        data[f'{chart}_expected'] = (expected * 100)
-        data[f'{chart}_actual'] = (actual * 100)
-        data[f'{chart}_error'] = (error * 100)
-        data[f'{chart}_score'] = (error_pct * 100)
-        data[f'{chart}_anomaly'] = anomalies
+        data[f'{chart}_expected'] = int(expected * 1000)
+        data[f'{chart}_actual'] = int(actual * 1000)
+        data[f'{chart}_error'] = int(error * 1000)
+        data[f'{chart}_score'] = int(error_pct * 1000)
+        data[f'{chart}_anomaly'] = int(anomalies)
 
     return data
 
@@ -146,8 +146,8 @@ class Service(SimpleService):
 
 #%%
 
-#data = get_raw_data('london.my-netdata.io')
-#print(data)
+data = get_raw_data('london.my-netdata.io')
+print(data)
 
 #%%
 
