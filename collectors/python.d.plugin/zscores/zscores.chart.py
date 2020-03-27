fa3719656672a4816aab851eea2988146af35bda
Andrew Maguire <andrewm4894@gmail.com>
2020-03-27 13:49:44 +0000

    try pull in some real data from the api

--- collectors/python.d.plugin/zscores/zscores.chart.py
+++ collectors/python.d.plugin/zscores/zscores.chart.py
@@ -68,12 +68,12 @@ def get_raw_data(host=None):
         df = df.set_index('time').dropna().sort_index()
 
         # get mean
-        m = df.mean().shift(1)
+        m = df.mean()
         # get standard deviation
-        s = df.std(ddof=0).shift(1)
+        s = df.std(ddof=0)
         # get zscore
         df = (df - m) / s
-        df = df.dropna(axis=1, how='all').tail(1).clip(-10, 10)
+        df = df.tail(1).dropna(axis=1, how='all').clip(-10, 10)
 
         for col in df.columns:
             data[f'{chart}.{col}'] = df[col].values[0] * 1000
