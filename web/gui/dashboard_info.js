05ef74e55b48056e73f48c55f31737f8dd17727e
Andrew Maguire <andrewm4894@gmail.com>
2020-04-01 15:24:52 +0100

    dev

--- web/gui/dashboard_info.js
+++ web/gui/dashboard_info.js
@@ -216,7 +216,7 @@ netdataDashboard.menu = {
     
     'smoothing': {
         title: 'smoothing',
-        icon: '<i class="fas fa-exclamation"></i>',
+        icon: '<i class="fas fa-suitcase-rolling"></i>',
         info: 'smoothing'
     },
 
