e0eb2b2afcc8414aa34fe5561fe5fa4ab670a730
Andrew Maguire <andrewm4894@gmail.com>
2020-04-02 13:39:56 +0100

    dev

--- collectors/python.d.plugin/zscores/zscores.chart.py
+++ collectors/python.d.plugin/zscores/zscores.chart.py
@@ -92,8 +92,8 @@ class Service(SimpleService):
         data = dict()
 
         # get data from allmetrics and append to self
-        latest_observations = [self.get_allmetrics(host=HOST_PORT, charts=CHARTS_IN_SCOPE)]
-        df_latest = self.data_to_df(latest_observations)
+        latest_observations = self.get_allmetrics(host=HOST_PORT, charts=CHARTS_IN_SCOPE)
+        df_latest = self.data_to_df([latest_observations])
         self.debug('df_latest')
         self.debug(df_latest)
 
