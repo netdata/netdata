baf374bf4d783e2c96ae275ea267ea71b705fd7d
Andrew Maguire <andrewm4894@gmail.com>
2020-04-02 14:36:57 +0100

    dev

--- collectors/python.d.plugin/zscores/zscores.chart.py
+++ collectors/python.d.plugin/zscores/zscores.chart.py
@@ -123,8 +123,8 @@ class Service(SimpleService):
             z = np.clip(z, -10.0, 10.0)
             self.debug(f'z={z}')
             if metric not in self.charts['zscores']:
-                self.charts['zscores'].add_dimension([metric, metric, 'absolute', 1, 1000])
-            data[metric] = z * 1000
+                self.charts['zscores'].add_dimension([metric, metric, 'absolute', 1, 10000])
+            data[metric] = z * 10000
 
 
         ## save results to data
