d775f77d28c4e2cf6fdf79088b2a7c2fa015c85b
Andrew Maguire <andrewm4894@gmail.com>
2020-04-02 15:46:33 +0100

    dev

--- collectors/python.d.plugin/correlations/correlations.chart.py
+++ collectors/python.d.plugin/correlations/correlations.chart.py
@@ -26,7 +26,7 @@ ORDER = [
 
 CHARTS = {
     'metric_correlations': {
-        'options': [None, 'Metric Correlations', '(var1,var2)', 'correlations', 'correlations.correlations', 'lines'],
+        'options': [None, 'Metric Correlations', '(var1,var2)', 'Metric Correlations', 'correlations.correlations', 'lines'],
         'lines': []
     },
 }
