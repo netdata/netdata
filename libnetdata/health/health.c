4ce1b108afecf6e335e112136165e96539e77b92
thiagoftsm <thiagoftsm@gmail.com>
2020-04-03 12:18:44 +0000

    Fix Coverity Defect CID-349684 (#8586)
    This PR brought a test before to call the function `health_add_params` to avoid memory leak.

--- libnetdata/health/health.c
+++ libnetdata/health/health.c
@@ -138,8 +138,8 @@ int health_silencers_json_read_callback(JSON_ENTRY *e)
                 else if (!strcmp(e->data.string,"DISABLE")) silencers->stype = STYPE_DISABLE_ALARMS;
             } else {
                 debug(D_HEALTH, "JSON: Adding %s=%s", e->name, e->data.string);
-                SILENCER *test = health_silencers_addparam(e->callback_data, e->name, e->data.string);
-                (void)test;
+                if (e->callback_data)
+                    (void)health_silencers_addparam(e->callback_data, e->name, e->data.string);
             }
             break;
 
