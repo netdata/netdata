ddc092573b2247a15c6cd8b0ea4a792b8854968c
Austin S. Hemmelgarn <austin@netdata.cloud>
2020-04-27 13:06:01 -0400

    Use correct static library name.

--- netdata-installer.sh
+++ netdata-installer.sh
@@ -638,7 +638,7 @@ copy_jsonc() {
 
   run mkdir -p "${target_dir}" "${target_dir}/json-c" || return 1
 
-  run cp "${1}/json-c.a" "${target_dir}/json-c.a" || return 1
+  run cp "${1}/libjson-c.a" "${target_dir}/libjson-c.a" || return 1
   run cp -r "${1}/*.h" "${target_dir}/json-c" || return 1
 }
 
