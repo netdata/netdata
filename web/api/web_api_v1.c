7d02cc21aed3a60584b59eaf3b4795198f7e40f1
Timotej Šiškovič <timotej@netdata.cloud>
2020-05-12 19:28:38 +0200

    add slave guid and online status to /api/v1/info

--- web/api/web_api_v1.c
+++ web/api/web_api_v1.c
@@ -775,13 +775,44 @@ static inline void web_client_api_request_v1_info_summary_alarm_statuses(RRDHOST
 
 static inline void web_client_api_request_v1_info_mirrored_hosts(BUFFER *wb) {
     RRDHOST *rc;
+    BUFFER *guids = buffer_create(2048);
     int count = 0;
+
+    buffer_strcat(wb, "\t\"mirrored_hosts\": [\n");
+    buffer_strcat(guids, "\t\"mirrored_hosts_status\": [\n");
     rrd_rdlock();
     rrdhost_foreach_read(rc) {
         if (rrdhost_flag_check(rc, RRDHOST_FLAG_ARCHIVED))
             continue;
-        if(count > 0) buffer_strcat(wb, ",\n");
+        if(count > 0) {
+            buffer_strcat(wb, ",\n");
+            buffer_strcat(guids, ",\n");
+        }
         buffer_sprintf(wb, "\t\t\"%s\"", rc->hostname);
+        netdata_mutex_lock(&rc->receiver_lock);
+        netdata_mutex_lock(&rc->claimed_id_lock);
+        buffer_sprintf(guids, "\t\t{ \"guid\": \"%s\", \"reachable\": \"%s\", \"claim_id\": \"%s\" }"
+                       , rc->machine_guid
+                       , (rc->receiver || rc == localhost) ? "true" : "false"
+                       , rc->claimed_id ? rc->claimed_id : "null"
+        );
+        netdata_mutex_unlock(&rc->claimed_id_lock);
+        netdata_mutex_unlock(&rc->receiver_lock);
+        count++;
+    }
+    rrd_unlock();
+    buffer_strcat(wb, "\n\t],\n");
+    buffer_sprintf(wb, "%s\n\t],\n", buffer_tostring(guids));
+    buffer_free(guids);
+}
+
+static inline void web_client_api_request_v1_info_mirrored_hosts_uids(BUFFER *wb) {
+    RRDHOST *rc;
+    int count = 0;
+    rrd_rdlock();
+    rrdhost_foreach_read(rc) {
+        if(count > 0) buffer_strcat(wb, ",\n");
+        buffer_sprintf(wb, "\t\t\"%s\"", rc->machine_guid);
         count++;
     }
     buffer_strcat(wb, "\n");
@@ -825,9 +856,7 @@ inline int web_client_api_request_v1_info_fill_buffer(RRDHOST *host, BUFFER *wb)
     buffer_sprintf(wb, "\t\"version\": \"%s\",\n", host->program_version);
     buffer_sprintf(wb, "\t\"uid\": \"%s\",\n", host->machine_guid);
 
-    buffer_strcat(wb, "\t\"mirrored_hosts\": [\n");
     web_client_api_request_v1_info_mirrored_hosts(wb);
-    buffer_strcat(wb, "\t],\n");
 
     buffer_strcat(wb, "\t\"alarms\": {\n");
     web_client_api_request_v1_info_summary_alarm_statuses(host, wb);
