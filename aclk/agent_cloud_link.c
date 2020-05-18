2d2e11b9cb5b4f8354a209704b8ce535ad3bc70c
Andrew Moss <1043609+amoss@users.noreply.github.com>
2020-05-18 13:33:10 +0200

    Reset the username before making the MQTT connection.
    Co-authored-by: <hilari@hilarimoragrega.com>

--- aclk/agent_cloud_link.c
+++ aclk/agent_cloud_link.c
@@ -1307,9 +1307,10 @@ void aclk_get_challenge(char *aclk_hostname, char *aclk_port)
     }
     if (aclk_password != NULL )
         freez(aclk_password);
-    if (aclk_username == NULL)
-        aclk_username = strdupz(agent_id);
     aclk_password = password.result;
+    if (aclk_username != NULL)
+        freez(aclk_username);
+    aclk_username = strdupz(agent_id);
 
 CLEANUP:
     freez(data_buffer);
