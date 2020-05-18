fecd0b6d6f0198b0087e8498892b6464bd9cbe30
Thiago Marques <thiagoftsm@gmail.com>
2020-05-18 14:48:17 +0000

    small adjusts

--- exporting/clean_connectors.c
+++ exporting/clean_connectors.c
@@ -3,20 +3,34 @@
 #include "exporting_engine.h"
 
 /**
+ * Clean the instance config.
+ * @param ptr
+ */
+static void clean_instance_config(struct instance_config *ptr)
+{
+    if (ptr->name)
+        freez((void *)ptr->name);
+
+    if (ptr->destination)
+        freez((void *)ptr->destination);
+
+    if (ptr->charts_pattern)
+        simple_pattern_free(ptr->charts_pattern);
+
+    if (ptr->hosts_pattern)
+        simple_pattern_free(ptr->hosts_pattern);
+}
+
+/**
  * Clean the allocated variables
  *
  * @param ptr a pointer to the structure with variables to clean.
  */
 void clean_instance(struct instance *ptr)
 {
-    if (ptr->buffer)
-        freez(ptr->buffer);
-
-    if (ptr->connector_specific_data)
-        freez(ptr->connector_specific_data);
-
+    clean_instance_config(&ptr->config);
     if (ptr->labels)
-        freez(ptr->labels);
+        buffer_free(ptr->labels);
 
     uv_mutex_destroy(&ptr->mutex);
     uv_cond_destroy(&ptr->cond_var);
