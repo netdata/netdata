55007201685eec5051843fbbb93f290a7178948e
Austin S. Hemmelgarn <austin@netdata.cloud>
2020-06-01 07:57:48 -0400

    Really prevent overwriting netdata.conf on static installs. (#9174)
    The previous fix was incompletely tested and didn't actually workdue to
    using an incorrect variable name. This fixes that.

--- packaging/makeself/jobs/70-netdata-git.install.sh
+++ packaging/makeself/jobs/70-netdata-git.install.sh
@@ -25,7 +25,7 @@ run ./netdata-installer.sh \
   --dont-start-it
 
 # Remove the netdata.conf file from the tree. It has hard-coded sensible defaults builtin.
-rm -f "${NETDATA_INSTALL_PARENT}/etc/netdata/netdata.conf"
+run rm -f "${NETDATA_INSTALL_PATH}/etc/netdata/netdata.conf"
 
 # Ensure the netdata binary is in fact statically linked
 if run readelf -l "${NETDATA_INSTALL_PATH}"/bin/netdata | grep 'INTERP'; then
