4dee67cfc55f72d55e107506b1c4e01b8c4bf2e3
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2020-06-11 14:00:59 +0300

    Include file for `sched_yield()`

--- database/rrdset.c
+++ database/rrdset.c
@@ -2,6 +2,7 @@
 
 #define NETDATA_RRD_INTERNALS
 #include "rrd.h"
+#include <sched.h>
 
 void __rrdset_check_rdlock(RRDSET *st, const char *file, const char *function, const unsigned long line) {
     debug(D_RRD_CALLS, "Checking read lock on chart '%s'", st->id);
