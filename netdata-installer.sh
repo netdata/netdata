37deda9cb8c0e850508fe9efb10dae1cad70eaa1
meesaltena <Mees.altena@gmail.com>
2020-06-29 03:47:59 +0200

    Fix typo in netdata-installer.sh (#9433)
    * [ci skip] create nightly packages and update changelog
    
    * [Package amd64 DEB][Build latest] Package build process trigger
    
    * [Package i386 DEB][Build latest] Package build process trigger
    
    * [Package amd64 RPM][Build latest] Package build process trigger
    
    * Stop reading from /proc/sys/kernel/osrelease at trailing newline. (#9374)
    
    Remove new line that was creating wrong log information.
    
    * fixed typo
    
    Co-authored-by: netdatabot <bot@netdata.cloud>
    Co-authored-by: Jeff Cook <jeff@jeffcook.io>

--- netdata-installer.sh
+++ netdata-installer.sh
@@ -351,7 +351,7 @@ if [ -z "$NETDATA_DISABLE_TELEMETRY" ]; then
   ${TPUT_YELLOW}${TPUT_BOLD}NOTE${TPUT_RESET}:
   Anonymous usage stats will be collected and sent to Google Analytics.
   To opt-out, pass --disable-telemetry option to the installer or export
-  the enviornment variable DO_NOT_TRACK to a non-zero or non-empty value
+  the environment variable DO_NOT_TRACK to a non-zero or non-empty value
   (e.g: export DO_NOT_TRACK=1).
 
 BANNER4
