6149a7ddab1141955c2900c94c9c232f7cec7a79
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2020-07-10 20:32:37 +0300

    Make sure target_object is valid

--- database/engine/global_uuid_map/global_uuid_map.c
+++ database/engine/global_uuid_map/global_uuid_map.c
@@ -21,8 +21,8 @@ void add_to_guid_store(uuid_t *uuid)
     uuid_copy(guid_map_store->list[guid_map_store->index++], *uuid);
     char uuid_str[37];
     uuid_unparse_lower(guid_map_store->list[guid_map_store->index-1], uuid_str);
-    info("Adding UUID %d = %s", guid_map_store->index-1, uuid_str);
-    if (guid_map_store->index == 128) {
+    //info("Adding UUID %d = %s", guid_map_store->index-1, uuid_str);
+    if (guid_map_store->index == MAP_STORE_SIZE) {
         tmp_guid_map_store = callocz(1, sizeof(*guid_map_store));
         tmp_guid_map_store->next = guid_map_store;
         guid_map_store = tmp_guid_map_store;
