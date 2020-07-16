eb3395eade9a854dbac54429b2b225dd3da6e372
Austin S. Hemmelgarn <austin@netdata.cloud>
2020-07-16 07:35:33 -0400

    Add working support for bundling eBPF in binary packages.

--- packaging/bundle-dashboard.sh
+++ packaging/bundle-dashboard.sh
@@ -6,9 +6,9 @@ WEBDIR="${2}"
 DASHBOARD_TARBALL="dashboard.tar.gz"
 DASHBOARD_VERSION="$(cat "${SRCDIR}/packaging/dashboard.version")"
 
-mkdir -p "${SRCDIR}/tmp"
+mkdir -p "${SRCDIR}/tmp/dashboard"
 curl -sSL --connect-timeout 10 --retry 3 "https://github.com/netdata/dashboard/releases/download/${DASHBOARD_VERSION}/${DASHBOARD_TARBALL}" > "${DASHBOARD_TARBALL}" || exit 1
 sha256sum -c "${SRCDIR}/packaging/dashboard.checksums" || exit 1
-tar -xzf "${DASHBOARD_TARBALL}" -C "${SRCDIR}/tmp" || exit 1
+tar -xzf "${DASHBOARD_TARBALL}" -C "${SRCDIR}/tmp/dashboard" || exit 1
 # shellcheck disable=SC2046
-cp -a $(find "${SRCDIR}/tmp/build" -mindepth 1 -maxdepth 1) "${WEBDIR}"
+cp -a $(find "${SRCDIR}/tmp/dashboard/build" -mindepth 1 -maxdepth 1) "${WEBDIR}"
