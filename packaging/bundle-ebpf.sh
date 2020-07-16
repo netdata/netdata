7ff315810e1d2c5871c4e87049687852b2cee7ee
Austin S. Hemmelgarn <austin@netdata.cloud>
2020-07-15 21:38:52 -0400

    Fix SHA256 handling in eBPF bundling code. (#9546)
    This makes it compatible both with busybox and older GNU userspaces.

--- packaging/bundle-ebpf.sh
+++ packaging/bundle-ebpf.sh
@@ -8,7 +8,7 @@ EBPF_TARBALL="netdata-kernel-collector-glibc-${EBPF_VERSION}.tar.xz"
 
 mkdir -p "${SRCDIR}/tmp/ebpf"
 curl -sSL --connect-timeout 10 --retry 3 "https://github.com/netdata/kernel-collector/releases/download/${EBPF_VERSION}/${EBPF_TARBALL}" > "${EBPF_TARBALL}" || exit 1
-sha256sum -c --ignore-missing "${SRCDIR}/packaging/ebpf.checksums" || exit 1
+grep "${EBPF_TARBALL}" "${SRCDIR}/packaging/ebpf.checksums" | sha256sum -c - || exit 1
 tar -xaf "${EBPF_TARBALL}" -C "${SRCDIR}/tmp/ebpf" || exit 1
 # shellcheck disable=SC2046
 cp -a $(find "${SRCDIR}/tmp/ebpf" -mindepth 1 -maxdepth 1) "${PLUGINDIR}"
