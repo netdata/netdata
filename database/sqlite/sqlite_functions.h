743e6a09338c0ef5c89d03093fc7f359270b56db
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2020-08-10 10:57:29 +0300

    Explore adding metric page during commit

--- database/sqlite/sqlite_functions.h
+++ database/sqlite/sqlite_functions.h
@@ -24,6 +24,9 @@ extern int sql_dimension_archive(uuid_t *dim_uuid, int archive);
 extern int sql_dimension_options(uuid_t *dim_uuid, char *options);
 extern int sql_load_dimension(char *dim_str, RRDSET *st);
 extern int sql_load_chart_dimensions(RRDSET *st, char *dimensions);
+extern void sql_add_metric(uuid_t *dim_uuid, usec_t point_in_time, storage_number number);
+extern void sql_add_metric_page(uuid_t *dim_uuid, struct rrdeng_page_descr *descr);
+
 
 
 #endif //NETDATA_SQLITE_FUNCTIONS_H
