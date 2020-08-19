365cf149407f28bbc67b3d30629b11f52a93d979
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2020-08-19 15:39:59 +0300

    Optimizations

--- web/api/web_api_v1.c
+++ web/api/web_api_v1.c
@@ -355,7 +355,7 @@ inline int web_client_api_request_v1_charts(RRDHOST *host, struct web_client *w,
 
 inline int web_client_api_request_v1_archivedcharts(RRDHOST *host, struct web_client *w, char *url) {
     (void)url;
-
+    sql_sync_ram_db();
     buffer_flush(w->response.data);
     w->response.data->contenttype = CT_APPLICATION_JSON;
     charts2json(host, w->response.data, 0, 1);
