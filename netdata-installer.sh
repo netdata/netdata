ca6ec89f78c2358915b3761251ab83818298d3eb
Austin S. Hemmelgarn <austin@netdata.cloud>
2020-09-08 07:36:19 -0400

    Corrected configure option name for bundling libJudy in installer.

--- netdata-installer.sh
+++ netdata-installer.sh
@@ -736,7 +736,7 @@ bundle_judy() {
       copy_judy "${tmp}/libjudy-${JUDY_PACKAGE_VERSION}" &&
       rm -rf "${tmp}"; then
       run_ok "libJudy built and prepared."
-      NETDATA_CONFIGURE_OPTIONS="${NETDATA_CONFIGURE_OPTIONS} --with-libjudy=externaldeps/libJudy"
+      NETDATA_CONFIGURE_OPTIONS="${NETDATA_CONFIGURE_OPTIONS} --with-libJudy=externaldeps/libJudy"
     else
       run_failed "Failed to build libJudy."
       if [ -n "${NETDATA_BUILD_JUDY}" ]; then
