a54395a81dc506a626b7e618494c3493106660a2
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2020-09-15 17:12:07 +0300

    Restore the original rrddim get first/last times

--- database/rrd.h
+++ database/rrd.h
@@ -1055,46 +1055,15 @@ static inline time_t rrdset_first_entry_t(RRDSET *st) {
 
 // get the timestamp of the last entry in the round robin database
 static inline time_t rrddim_last_entry_t(RRDDIM *rd) {
-    RRDSET *st = rd->rrdset;
-    if (st->rrd_memory_mode == RRD_MEMORY_MODE_DBENGINE)  {
-        RRDDIM *temp_rd;
-        time_t last_entry_t  = 0;
-
-        int ret = netdata_rwlock_tryrdlock(&st->rrdset_rwlock);
-        //rrddim_foreach_read(rd, st) {
-        while (rd) {
-            temp_rd = rd->next;
-            last_entry_t = MAX(last_entry_t, rd->state->query_ops.latest_time(rd));
-            rd = temp_rd;
-        }
-        if(0 == ret) netdata_rwlock_unlock(&st->rrdset_rwlock);
-
-        return last_entry_t;
-    } else {
-        return (time_t)st->last_updated.tv_sec;
-    }
+    if (rd->rrdset->rrd_memory_mode == RRD_MEMORY_MODE_DBENGINE)
+        return rd->state->query_ops.latest_time(rd);
+    return (time_t)rd->rrdset->last_updated.tv_sec;
 }
 
-// get the timestamp of first entry in the round robin database
 static inline time_t rrddim_first_entry_t(RRDDIM *rd) {
-    RRDSET *st = rd->rrdset;
-    if (st->rrd_memory_mode == RRD_MEMORY_MODE_DBENGINE) {
-        RRDDIM *temp_rd;
-        time_t first_entry_t = LONG_MAX;
-
-        int ret = netdata_rwlock_tryrdlock(&st->rrdset_rwlock);
-        while (rd) {
-            temp_rd = rd->next;
-            first_entry_t = MIN(first_entry_t, rd->state->query_ops.oldest_time(rd));
-            rd = temp_rd;
-        }
-        if(0 == ret) netdata_rwlock_unlock(&st->rrdset_rwlock);
-
-        if (unlikely(LONG_MAX == first_entry_t)) return 0;
-        return first_entry_t;
-    } else {
-        return (time_t)(rrdset_last_entry_t(st) - rrdset_duration(st));
-    }
+    if (rd->rrdset->rrd_memory_mode == RRD_MEMORY_MODE_DBENGINE)
+        return rd->state->query_ops.oldest_time(rd);
+    return (time_t)(rd->rrdset->last_updated.tv_sec - rrdset_duration(rd->rrdset));
 }
 
 time_t rrdhost_last_entry_t(RRDHOST *h);
