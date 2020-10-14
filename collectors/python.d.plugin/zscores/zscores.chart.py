2354313e196c571441f1f8f934053481c6224df0
Andrew Maguire <andrewm4894@gmail.com>
2020-10-14 15:37:38 +0100

    refactor

--- collectors/python.d.plugin/zscores/zscores.chart.py
+++ collectors/python.d.plugin/zscores/zscores.chart.py
@@ -36,8 +36,8 @@ CHARTS = {
 class Service(SimpleService):
     def __init__(self, configuration=None, name=None):
         SimpleService.__init__(self, configuration=configuration, name=name)
-        self.host = self.configuration.get('host')
-        if self.configuration.get('charts_in_scope') == 'system.*':
+        self.host = self.configuration.get('host', '127.0.0.1:19999')
+        if self.configuration.get('charts_in_scope', 'system.*') == 'system.*':
             self.charts_in_scope = [c for c in requests.get(f'http://{self.host}/api/v1/charts').json()['charts'].keys() if c.startswith('system.')]
         else:
             self.charts_in_scope = self.configuration.get('charts_in_scope').split(',')
@@ -68,17 +68,11 @@ class Service(SimpleService):
     def train_model(self):
         """Calculate the mean and sigma for all relevant metrics and store them for use in calulcating zscore at each timestep.
         """
-
-        # calculate the mean and sigma between the timestamps after to before
         before = int(datetime.now().timestamp()) - self.offset_secs
         after = before - self.train_secs
-
-        # get means from rest api
         self.df_mean = get_data(
             self.host, self.charts_in_scope, after, before, points=10, group='average', col_sep='.'
             ).mean().to_frame().rename(columns={0: "mean"})
-
-        # get sigmas from rest api
         self.df_std = get_data(
             self.host, self.charts_in_scope, after, before, points=10, group='stddev', col_sep='.'
             ).mean().to_frame().rename(columns={0: "std"})
@@ -92,16 +86,16 @@ class Service(SimpleService):
         """
         # calculate clipped z score for each available metric
         df_z = pd.concat([self.df_mean, self.df_std, df_allmetrics], axis=1, join='inner')
-        df_z['z'] = ((df_z['value'] - df_z['mean']) / df_z['std']).clip(-self.z_clip, self.z_clip).fillna(0)
+        df_z['z'] = ((df_z['value'] - df_z['mean']) / df_z['std']).clip(-self.z_clip, self.z_clip).fillna(0) * 100
         if self.z_abs:
             df_z['z'] = df_z['z'].abs()
-        # append last z_smooth_n rows of zscores to history table
-        df_z_wide = df_z[['z']].reset_index().pivot_table(values='z', columns='index')
-        self.df_z_history = self.df_z_history.append(df_z_wide, sort=True).tail(self.z_smooth_n)
+        # append last z_smooth_n rows of zscores to history table in wide format
+        self.df_z_history = self.df_z_history.append(
+            df_z[['z']].reset_index().pivot_table(values='z', columns='index'), sort=True
+            ).tail(self.z_smooth_n)
         # get average zscore for last z_smooth_n for each metric
-        df_z_smooth = (self.df_z_history.melt(value_name='z').groupby('index')['z'].mean() * 100).to_frame()
+        df_z_smooth = self.df_z_history.melt(value_name='z').groupby('index')['z'].mean().to_frame()
         df_z_smooth['3sigma'] = np.where(abs(df_z_smooth['z']) > 300, 1, 0)
-        # create data dict for z scores
         data_z = df_z_smooth['z'].add_suffix('_z').to_dict()
         # aggregate to chart level if specified
         if self.mode == 'per_chart':
@@ -110,7 +104,6 @@ class Service(SimpleService):
                 data_z = list(df_z_smooth.groupby('chart').agg({'z': lambda x: max(x, key=abs)})['z'].to_dict().values())[0]
             else:
                 data_z = list(df_z_smooth.groupby('chart').agg({'z': [self.per_chart_agg]})['z'].to_dict().values())[0]
-        # create data dict for 3sigma flags
         data_3sigma = {}
         for k in data_z:
             data_3sigma[k.replace('_z','_3sigma')] = 1 if abs(data_z[k]) > 300 else 0
@@ -118,13 +111,9 @@ class Service(SimpleService):
         return data_z, data_3sigma
 
     def get_data(self):
-        # train model if needed
         if self.runs_counter <= self.burn_in or self.runs_counter % self.train_every_n == 0:
             self.train_model()
-        # get latest data
-        df_allmetrics = get_allmetrics(self.host, self.charts_in_scope, wide=True, col_sep='.').transpose()
-        # create data dicts
-        data_z, data_3sigma = self.create_data(df_allmetrics)
+        data_z, data_3sigma = self.create_data(get_allmetrics(self.host, self.charts_in_scope, wide=True, col_sep='.').transpose())
         data = {**data_z, **data_3sigma}
         self.validate_charts('z', data_z, divisor=100)
         self.validate_charts('3sigma', data_3sigma)
