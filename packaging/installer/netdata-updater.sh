6a507c951c5d249ad1007cd67bcf51c278378570
abrbon <abrbon@gmail.com>
2020-11-05 23:02:17 +0100

    fixed bug with autoupdater on FreeBSD/TrueNAS

--- packaging/installer/netdata-updater.sh
+++ packaging/installer/netdata-updater.sh
@@ -256,7 +256,11 @@ done
 # But only we're not a controlling terminal (tty)
 # Randomly sleep between 1s and 60m
 if [ ! -t 1 ] && [ -z "${NETDATA_NOT_RUNNING_FROM_CRON}" ]; then
-  sleep $(((RANDOM % 3600) + 1))s
+  if [ "$(uname -s)" = "FreeBSD" ]; then
+    sleep `jot -r 1 1 3600`
+  else
+    sleep $(((RANDOM % 3600) + 1))s
+  fi
 fi
 
 # Usually stored in /etc/netdata/.environment
