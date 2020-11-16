f05a5adc72655f0bffa2e88585c3398ab2c71e1f
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2020-11-16 10:33:46 +0200

    Remove more unused code

--- database/engine/metadata_log/metadatalog.h
+++ database/engine/metadata_log/metadatalog.h
@@ -19,25 +19,6 @@ struct parser_user_object;
 #define METALOG_FILE_NUMBER_SCAN_TMPL "%5u-%5u"
 #define METALOG_FILE_NUMBER_PRINT_TMPL "%5.5u-%5.5u"
 
-/*
- * Debug statistics not used by code logic.
- * They only describe operations since DB engine instance load time.
- */
-struct metalog_statistics {
-    rrdeng_stats_t io_write_bytes;
-    rrdeng_stats_t io_write_requests;
-    rrdeng_stats_t io_read_bytes;
-    rrdeng_stats_t io_read_requests;
-    rrdeng_stats_t io_write_record_bytes;
-    rrdeng_stats_t io_write_records;
-    rrdeng_stats_t io_read_record_bytes;
-    rrdeng_stats_t io_read_records;
-    rrdeng_stats_t metadata_logfile_creations;
-    rrdeng_stats_t metadata_logfile_deletions;
-    rrdeng_stats_t io_errors;
-    rrdeng_stats_t fs_errors;
-};
-
 struct metalog_instance {
     struct rrdengine_instance *rrdeng_ctx;
     struct parser_user_object *metalog_parser_object;
