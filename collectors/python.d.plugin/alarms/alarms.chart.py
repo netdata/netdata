5e1eac776f300e78a317135e38183e916c28c405
Andrew Maguire <andrewm4894@gmail.com>
2020-11-30 12:38:25 +0000

    enable alarms collector by default

--- collectors/python.d.plugin/alarms/alarms.chart.py
+++ collectors/python.d.plugin/alarms/alarms.chart.py
@@ -8,7 +8,7 @@ from json import loads
 from bases.FrameworkServices.UrlService import UrlService
 
 update_every = 10
-disabled_by_default = True
+disabled_by_default = False
 
 DEFAULT_STATUS_MAP = {'CLEAR': 0, 'WARNING': 1, 'CRITICAL': 2}
 
