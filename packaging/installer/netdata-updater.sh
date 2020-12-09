9d8974e756d896917cad24ec9808eb2653c67c30
Austin S. Hemmelgarn <austin@netdata.cloud>
2020-12-09 11:22:40 -0500

    Fix update logic to always download updater if we can’t find date for last commit.

--- packaging/installer/netdata-updater.sh
+++ packaging/installer/netdata-updater.sh
@@ -156,7 +156,7 @@ newer_commit_date() {
   fi
 
   if [ -z "${commit_date}" ] ; then
-    commit_date="1970-01-01T00:00:00Z"
+    commit_date="9999-12-31T23:59:59Z"
   fi
 
   if [ -e "${script_source}" ]; then
