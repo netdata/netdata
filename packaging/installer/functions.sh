a5bdced4ba8116402f77d967103c55ff76d3ec08
Josh Soref <jsoref@users.noreply.github.com>
2020-12-26 21:02:32 -0500

    spelling: context
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- packaging/installer/functions.sh
+++ packaging/installer/functions.sh
@@ -891,7 +891,7 @@ portable_add_user_to_group() {
 }
 
 safe_sha256sum() {
-  # Within the contexct of the installer, we only use -c option that is common between the two commands
+  # Within the context of the installer, we only use -c option that is common between the two commands
   # We will have to reconsider if we start non-common options
   if command -v sha256sum > /dev/null 2>&1; then
     sha256sum "$@"
