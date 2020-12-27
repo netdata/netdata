9cbe1d9c1acbd6fcb42bb725d6a8152eb19313bb
Josh Soref <jsoref@users.noreply.github.com>
2020-12-26 21:08:23 -0500

    spelling: dimensions
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- collectors/ebpf.plugin/ebpf_socket.c
+++ collectors/ebpf.plugin/ebpf_socket.c
@@ -259,7 +259,7 @@ static void ebpf_socket_send_nv_data(netdata_vector_plot_t *ptr)
 
 
 /**
- * Update the publish strctures to create the dimenssions
+ * Update the publish strctures to create the dimensions
  *
  * @param curr   Last values read from memory.
  * @param prev   Previous values read from memory.
