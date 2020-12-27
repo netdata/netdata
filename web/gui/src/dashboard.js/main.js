5befe325436100865662b37ed3069fc8cfd358de
Josh Soref <jsoref@users.noreply.github.com>
2020-12-26 21:09:06 -0500

    spelling: hiding
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- web/gui/src/dashboard.js/main.js
+++ web/gui/src/dashboard.js/main.js
@@ -1432,7 +1432,7 @@ let chartState = function (element) {
             if (NETDATA.options.current.show_help) {
                 if (this.element_legend_childs.toolbox !== null) {
                     if (this.debug) {
-                        this.log('hideChart(): hidding legend popovers');
+                        this.log('hideChart(): hiding legend popovers');
                     }
 
                     $(this.element_legend_childs.toolbox_left).popover('hide');
@@ -2979,7 +2979,7 @@ let chartState = function (element) {
             }
         }
 
-        // create a hidden div to be used for hidding
+        // create a hidden div to be used for hiding
         // the original legend of the chart library
         let el = document.createElement('div');
         if (this.element_legend !== null) {
