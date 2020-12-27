98709368f589edcea92d09764f6114a13d4c7bf3
Josh Soref <jsoref@users.noreply.github.com>
2020-12-26 21:11:47 -0500

    spelling: telemetry
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- packaging/installer/kickstart-static64.sh
+++ packaging/installer/kickstart-static64.sh
@@ -271,7 +271,7 @@ while [ -n "${1}" ]; do
 done
 
 if [ ! "${DO_NOT_TRACK:-0}" -eq 0 ] || [ -n "$DO_NOT_TRACK" ]; then
-  NETDATA_INSTALLER_OPTIONS="${NETDATA_INSTALLER_OPTIONS:+${NETDATA_INSTALLER_OPTIONS} }--disable-telemtry"
+  NETDATA_INSTALLER_OPTIONS="${NETDATA_INSTALLER_OPTIONS:+${NETDATA_INSTALLER_OPTIONS} }--disable-telemetry"
 fi
 
 # Netdata Tarball Base URL (defaults to our Google Storage Bucket)
