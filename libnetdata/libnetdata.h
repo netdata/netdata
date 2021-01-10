4be533102f14cfad98bb4e47bead5ed0e48cec8b
Vasilis Kalintiris <vasilis@netdata.cloud>
2021-01-10 15:40:55 +0200

    Merge libndinc.h into libnetdata.h

--- libnetdata/libnetdata.h
+++ libnetdata/libnetdata.h
@@ -3,13 +3,47 @@
 #ifndef NETDATA_LIB_H
 #define NETDATA_LIB_H 1
 
-# ifdef __cplusplus
+#ifdef __cplusplus
 extern "C" {
-# endif
+#endif
 
 #include "sysinc.h"
+
+#include "inlined.h"
 #include "utils.h"
-#include "libndinc.h"
+#include "os.h"
+
+#include "adaptive_resortable_list/adaptive_resortable_list.h"
+#include "avl/avl.h"
+#include "buffer/buffer.h"
+#include "circular_buffer/circular_buffer.h"
+#include "clocks/clocks.h"
+#include "config/appconfig.h"
+#include "dictionary/dictionary.h"
+
+#if defined(HAVE_LIBBPF) && !defined(__cplusplus)
+#include "ebpf/ebpf.h"
+#endif
+
+#include "eval/eval.h"
+#include "health/health.h"
+#include "json/json.h"
+#include "locks/locks.h"
+#include "log/log.h"
+#include "popen/popen.h"
+#include "procfile/procfile.h"
+#include "simple_pattern/simple_pattern.h"
+
+#ifdef ENABLE_HTTPS
+# include "socket/security.h"
+#endif
+
+#include "socket/socket.h"
+#include "statistical/statistical.h"
+#include "storage_number/storage_number.h"
+#include "string/utf8.h"
+#include "threads/threads.h"
+#include "url/url.h"
 
 # ifdef __cplusplus
 }
