cadbb5056d839e3bd72805e4f7ccdc76d2c4dbd3
kaskavel <31739664+kaskavel@users.noreply.github.com>
2021-03-03 21:36:53 +0200

    Add a new workflow to test that updater works as expected (#10599)
    * Add workflow for installing latest version of netdata, building from source of this branch & running the updater. Add script build-dist to create the artifacts used for the update.
    
    * Add more distributions, arguments in updater script & accomodate review comment
    
    * Run updater within docker for 6 distributions
    
    * Remove unecessary change in updater
    
    * Correct netdata_version --> updater_version in check-updater script
    
    * Review comments: remove unused vars & replace == with =

---
+++ .github/scripts/check-updater.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#
+set -e
+# shellcheck source=.github/scripts/functions.sh
+. "$(dirname "$0")/functions.sh"
+
+check_successfull_update() {
+  progress "Check netdata version after update"
+  (
+    netdata_version=$(netdata -v | awk '{print $2}')
+    updater_version=$(cat packaging/version)
+    if [ "$netdata_version" = "$updater_version" ]; then
+      echo "Update successfull!"
+    else
+      exit 1
+    fi
+  ) >&2
+}
+
+steps="check_successfull_update"
+
+_main() {
+  for step in $steps; do
+    if ! run "$step"; then
+      if [ -t 1 ]; then
+        debug
+      else
+        fail "Build failed"
+      fi
+    fi
+  done
+
+  echo "🎉 All Done!"
+}
+
+if [ -n "$0" ] && [ x"$0" != x"-bash" ]; then
+  _main "$@"
+fi
