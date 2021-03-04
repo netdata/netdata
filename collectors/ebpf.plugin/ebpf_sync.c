25b9eb3efaa218253c50e0bcc514ceb098235e20
Thiago Marques <thiagoftsm@gmail.com>
2021-03-04 21:13:51 +0000

    ebpf_sync: Load ebpf program

--- collectors/ebpf.plugin/ebpf_sync.c
+++ collectors/ebpf.plugin/ebpf_sync.c
@@ -3,6 +3,16 @@
 #include "ebpf.h"
 #include "ebpf_sync.h"
 
+static ebpf_data_t sync_data;
+
+static struct bpf_link **probe_links = NULL;
+static struct bpf_object *objects = NULL;
+
+
+static char *sync_counter_dimension_name[NETDATA_SYNC_END] = { "sync" };
+static netdata_syscall_stat_t sync_counter_aggregated_data;
+static netdata_publish_syscall_t sync_counter_publish_aggregated;
+
 /*****************************************************************
  *
  *  CLEANUP THREAD
@@ -19,6 +29,14 @@ static void ebpf_sync_cleanup(void *ptr)
     ebpf_module_t *em = (ebpf_module_t *)ptr;
     if (!em->enabled)
         return;
+
+    struct bpf_program *prog;
+    size_t i = 0 ;
+    bpf_object__for_each_program(prog, objects) {
+        bpf_link__destroy(probe_links[i]);
+        i++;
+    }
+    bpf_object__close(objects);
 }
 
 /*****************************************************************
@@ -30,7 +48,7 @@ static void ebpf_sync_cleanup(void *ptr)
 /**
  * Cachestat thread
  *
- * Thread used to make cachestat thread
+ * Thread used to make sync thread
  *
  * @param ptr a pointer to `struct ebpf_module`
  *
@@ -40,6 +58,29 @@ void *ebpf_sync_thread(void *ptr)
 {
     netdata_thread_cleanup_push(ebpf_sync_cleanup, ptr);
 
+    ebpf_module_t *em = (ebpf_module_t *)ptr;
+    fill_ebpf_data(&sync_data);
+
+    if (!em->enabled)
+        goto endsync;
+
+    if (ebpf_update_kernel(&sync_data)) {
+        pthread_mutex_unlock(&lock);
+        goto endsync;
+    }
+
+    probe_links = ebpf_load_program(ebpf_plugin_dir, em, kernel_string, &objects, sync_data.map_fd);
+    if (!probe_links) {
+        pthread_mutex_unlock(&lock);
+        goto endsync;
+    }
+
+    int algorithm = NETDATA_EBPF_INCREMENTAL_IDX;
+    ebpf_global_labels(&sync_counter_aggregated_data, &sync_counter_publish_aggregated,
+                       sync_counter_dimension_name, sync_counter_dimension_name,
+                       &algorithm, NETDATA_SYNC_END);
+
+endsync:
     netdata_thread_cleanup_pop(1);
     return NULL;
 }
--- collectors/ebpf.plugin/ebpf_sync.h
+++ collectors/ebpf.plugin/ebpf_sync.h
@@ -3,6 +3,12 @@
 #ifndef NETDATA_EBPF_SYNC_H
 #define NETDATA_EBPF_SYNC_H 1
 
+enum netdata_sync {
+    NETDATA_SYNC_GLOBLAL_TABLE,
+
+    NETDATA_SYNC_END
+};
+
 extern void *ebpf_sync_thread(void *ptr);
 
 #endif
