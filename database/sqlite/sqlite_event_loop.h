d63ca1b8433df704365b21b7e1ea1f5e8d834f46
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2021-03-07 20:48:11 +0200

    Start event loop for sqlite

---
+++ database/sqlite/sqlite_event_loop.h
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+
+#ifndef NETDATA_SQLITE_EVENT_LOOP_H
+#define NETDATA_SQLITE_EVENT_LOOP_H
+#include <uv.h>
+
+enum sqlite_opcode {
+    /* can be used to return empty status or flush the command queue */
+    SQLITEOP_NOOP = 0,
+    SQLITEOP_PAGE,
+    SQLITEOP_SHUTDOWN,
+    SQLITEOP_MAX_OPCODE
+};
+
+struct sqlite_cmd {
+    enum sqlite_opcode opcode;
+    union {
+//        struct rrdeng_read_page {
+//            struct rrdeng_page_descr *page_cache_descr;
+//        } read_page;
+//        struct rrdeng_read_extent {
+//            struct rrdeng_page_descr *page_cache_descr[MAX_PAGES_PER_EXTENT];
+//            int page_count;
+//        } read_extent;
+        struct completion *completion;
+    };
+};
+
+#define SQLITE_CMD_Q_MAX_SIZE (2048)
+
+struct sqlite_cmdqueue {
+    unsigned head, tail;
+    struct sqlite_cmd cmd_array[SQLITE_CMD_Q_MAX_SIZE];
+};
+
+struct sqlite_worker_config {
+    //struct rrdengine_instance *ctx;
+
+    uv_thread_t thread;
+    uv_loop_t* loop;
+    uv_async_t async;
+
+    /* file deletion thread */
+    //uv_thread_t *now_deleting_files;
+    //unsigned long cleanup_thread_deleting_files; /* set to 0 when now_deleting_files is still running */
+
+    /* dirty page deletion thread */
+    //uv_thread_t *now_invalidating_dirty_pages;
+    /* set to 0 when now_invalidating_dirty_pages is still running */
+    //unsigned long cleanup_thread_invalidating_dirty_pages;
+    //unsigned inflight_dirty_pages;
+
+    /* FIFO command queue */
+    uv_mutex_t cmd_mutex;
+    uv_cond_t cmd_cond;
+    volatile unsigned queue_size;
+    struct sqlite_cmdqueue cmd_queue;
+
+    //struct extent_cache xt_cache;
+
+    int error;
+};
+
+#endif //NETDATA_SQLITE_EVENT_LOOP_H
