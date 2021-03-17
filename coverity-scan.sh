432881952027ffede67c9bb2c8167cb559044415
Austin S. Hemmelgarn <austin@netdata.cloud>
2021-03-17 12:05:33 -0400

    Update Coverity version so that scanning works again.

--- coverity-scan.sh
+++ coverity-scan.sh
@@ -40,7 +40,7 @@ set -e
 INSTALL_DIR="/opt"
 
 # the version of coverity to use
-COVERITY_BUILD_VERSION="${COVERITY_BUILD_VERSION:-cov-analysis-linux64-2019.03}"
+COVERITY_BUILD_VERSION="${COVERITY_BUILD_VERSION:-cov-analysis-linux64-2020.09}"
 
 # TODO: For some reasons this does not fully load on Debian 10 (Haven't checked if it happens on other distros yet), it breaks
 source packaging/installer/functions.sh || echo "Failed to fully load the functions library"
