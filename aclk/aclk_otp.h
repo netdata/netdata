6ec7df9fef011fa389caa0bcf8febda8ad87fdcc
Timotej Šiškovič <timotej@netdata.cloud>
2021-03-19 15:21:50 +0100

    implement env parser

--- aclk/aclk_otp.h
+++ aclk/aclk_otp.h
@@ -6,5 +6,6 @@
 #include "../daemon/common.h"
 
 void aclk_get_mqtt_otp(RSA *p_key, char *aclk_hostname, int port, char **mqtt_usr, char **mqtt_pass);
+int aclk_get_env(const char *aclk_hostname, int aclk_port);
 
 #endif /* ACLK_OTP_H */
