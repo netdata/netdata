ad220b0acac4da7bd19e6ff3c3822125f9bf4860
Thiago Marques <thiagoftsm@gmail.com>
2021-03-22 22:17:49 +0000

    ebpf_directory_cache: Add function loader

--- collectors/ebpf.plugin/ebpf_dcstat.c
+++ collectors/ebpf.plugin/ebpf_dcstat.c
@@ -3,6 +3,21 @@
 #include "ebpf.h"
 #include "ebpf_dcstat.h"
 
+static char *dcstat_counter_dimension_name[NETDATA_DCSTAT_IDX_END] = { "ratio", "reference", "slow", "miss" };
+static netdata_syscall_stat_t dcstat_counter_aggregated_data[NETDATA_DCSTAT_IDX_END];
+static netdata_publish_syscall_t dcstat_counter_publish_aggregated[NETDATA_DCSTAT_IDX_END];
+
+static ebpf_data_t dcstat_data;
+
+static struct bpf_link **probe_links = NULL;
+static struct bpf_object *objects = NULL;
+
+struct config dcstat_config = { .first_section = NULL,
+    .last_section = NULL,
+    .mutex = NETDATA_MUTEX_INITIALIZER,
+    .index = { .avl_tree = { .root = NULL, .compar = appconfig_section_compare },
+        .rwlock = AVL_LOCK_INITIALIZER } };
+
 /*****************************************************************
  *
  *  FUNCTIONS TO CLOSE THE THREAD
@@ -19,6 +34,16 @@ static void ebpf_dcstat_cleanup(void *ptr)
     ebpf_module_t *em = (ebpf_module_t *)ptr;
     if (!em->enabled)
         return;
+
+    ebpf_cleanup_publish_syscall(dcstat_counter_publish_aggregated);
+
+    struct bpf_program *prog;
+    size_t i = 0 ;
+    bpf_object__for_each_program(prog, objects) {
+        bpf_link__destroy(probe_links[i]);
+        i++;
+    }
+    bpf_object__close(objects);
 }
 
 /*****************************************************************
@@ -59,6 +84,35 @@ void *ebpf_dcstat_thread(void *ptr)
 {
     netdata_thread_cleanup_push(ebpf_dcstat_cleanup, ptr);
 
+    ebpf_module_t *em = (ebpf_module_t *)ptr;
+    fill_ebpf_data(&dcstat_data);
+
+    ebpf_update_module(em, &dcstat_config, NETDATA_DIRECTORY_DCSTAT_CONFIG_FILE);
+
+    if (!em->enabled)
+        goto enddcstat;
+
+    pthread_mutex_lock(&lock);
+
+    probe_links = ebpf_load_program(ebpf_plugin_dir, em, kernel_string, &objects, dcstat_data.map_fd);
+    if (!probe_links) {
+        pthread_mutex_unlock(&lock);
+        goto enddcstat;
+    }
+
+    int algorithms[NETDATA_DCSTAT_IDX_END] = {
+        NETDATA_EBPF_ABSOLUTE_IDX, NETDATA_EBPF_INCREMENTAL_IDX, NETDATA_EBPF_INCREMENTAL_IDX,
+        NETDATA_EBPF_INCREMENTAL_IDX
+    };
+
+    ebpf_global_labels(dcstat_counter_aggregated_data, dcstat_counter_publish_aggregated,
+                       dcstat_counter_dimension_name, dcstat_counter_dimension_name,
+                       algorithms, NETDATA_DCSTAT_IDX_END);
+
+
+    pthread_mutex_unlock(&lock);
+
+enddcstat:
     netdata_thread_cleanup_pop(1);
     return NULL;
 }
--- collectors/ebpf.plugin/ebpf_dcstat.h
+++ collectors/ebpf.plugin/ebpf_dcstat.h
@@ -3,6 +3,19 @@
 #ifndef NETDATA_EBPF_DCSTAT_H
 #define NETDATA_EBPF_DCSTAT_H 1
 
+// configuration file
+#define NETDATA_DIRECTORY_DCSTAT_CONFIG_FILE "dcstat.conf"
+
+enum directory_cache_indexes {
+    NETDATA_DCSTAT_IDX_RATIO,
+    NETDATA_DCSTAT_IDX_REFERENCE,
+    NETDATA_DCSTAT_IDX_SLOW,
+    NETDATA_DCSTAT_IDX_MISS,
+
+    // Keep this as last and don't skip numbers as it is used as element counter
+    NETDATA_DCSTAT_IDX_END
+};
+
 extern void *ebpf_dcstat_thread(void *ptr);
 extern void ebpf_dcstat_create_apps_charts(struct ebpf_module *em, void *ptr);
 
