588363954922607aa2c1f2989b5cc536d0b11e13
Emmanuel Vasilakis <mrzammler@mm.st>
2021-04-05 16:43:31 +0300

    Get the local utc offset, and pass it to alarms (#10899)
    * get the utc_offset, and pass it to alarms
    
    * fix wrong variable set if fail. Also set to +0000
    
    * note
    
    * test tm_gmtoff
    
    * with tm_gmtoff
    
    * get the seconds off utc and the timezone from strftime

--- daemon/common.h
+++ daemon/common.h
@@ -96,6 +96,8 @@ extern char *netdata_configured_lock_dir;
 extern char *netdata_configured_home_dir;
 extern char *netdata_configured_host_prefix;
 extern char *netdata_configured_timezone;
+extern char *netdata_tz;
+extern int32_t netdata_utc_offset;
 extern int netdata_zero_metrics_enabled;
 extern int netdata_anonymous_statistics_enabled;
 
