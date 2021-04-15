5f0da92f8cb91369b6cc6bda5ebea2bdcb264981
Thiago Marques <thiagoftsm@gmail.com>
2021-04-15 16:01:02 +0000

    fix_ratio: Change dimension

--- collectors/ebpf.plugin/ebpf_cachestat.c
+++ collectors/ebpf.plugin/ebpf_cachestat.c
@@ -125,8 +125,6 @@ void cachestat_update_publish(netdata_publish_cachestat_t *out, uint64_t mpa, ui
         hits = 0;
     }
 
-    // Changed to avoid gaps
-    // calculated_number ratio = (total > 0) ? hits/total : 0;
     calculated_number ratio = (total > 0) ? hits/total : 1;
 
     out->ratio = (long long )(ratio*100);
@@ -538,7 +536,7 @@ static void ebpf_create_memory_charts()
 {
     ebpf_create_chart(NETDATA_EBPF_MEMORY_GROUP, NETDATA_CACHESTAT_HIT_RATIO_CHART,
                       "Hit is calculating using total cache added without dirties per total added because of red misses.",
-                      EBPF_CACHESTAT_DIMENSION_HITS, NETDATA_CACHESTAT_SUBMENU,
+                      EBPF_COMMON_DIMENSION_PERCENTAGE, NETDATA_CACHESTAT_SUBMENU,
                       NULL,
                       NETDATA_EBPF_CHART_TYPE_LINE,
                       21100,
