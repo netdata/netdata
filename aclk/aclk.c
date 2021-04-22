d7bf0efe766404a458ec4fcbcae5ec92a3705b36
Timotej Šiškovič <timotej@netdata.cloud>
2021-04-22 12:12:36 +0200

    minor - formatting

--- aclk/aclk.c
+++ aclk/aclk.c
@@ -428,7 +428,6 @@ static unsigned long aclk_reconnect_delay() {
         return aclk_tbeb_delay(1, 2, 0, 1024);
 
     return aclk_tbeb_delay(1, aclk_env->backoff.base, aclk_env->backoff.min_s, aclk_env->backoff.max_s);
-
 }
 
 /* Block till aclk_reconnect_delay is satisifed or netdata_exit is signalled
