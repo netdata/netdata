c0825ec66d11d0dcd71779382132a1fd3840d487
Andrew Maguire <andrewm4894@gmail.com>
2021-04-26 17:00:02 +0100

    Revert "apply code reformatting"
    This reverts commit 8e9eaf407865f093c2934e233ab044e740472c0c.

--- collectors/python.d.plugin/zscores/zscores.chart.py
+++ collectors/python.d.plugin/zscores/zscores.chart.py
@@ -13,6 +13,7 @@ import pandas as pd
 from bases.FrameworkServices.SimpleService import SimpleService
 from netdata_pandas.data import get_data, get_allmetrics
 
+
 priority = 60000
 update_every = 5
 
@@ -41,10 +42,9 @@ class Service(SimpleService):
         self.charts_to_exclude = self.configuration.get('charts_to_exclude', '').split(',')
         self.charts_in_scope = [
             c for c in
-            list(filter(self.charts_regex.match,
-                        requests.get(f'http://{self.host}/api/v1/charts').json()['charts'].keys()))
+            list(filter(self.charts_regex.match, requests.get(f'http://{self.host}/api/v1/charts').json()['charts'].keys()))
             if c not in self.charts_to_exclude
-        ]
+            ]
         self.train_secs = self.configuration.get('train_secs', 14400)
         self.offset_secs = self.configuration.get('offset_secs', 300)
         self.train_every_n = self.configuration.get('train_every_n', 900)
@@ -77,11 +77,11 @@ class Service(SimpleService):
 
         self.df_mean = get_data(
             self.host, self.charts_in_scope, after, before, points=10, group='average', col_sep='.'
-        ).mean().to_frame().rename(columns={0: "mean"})
+            ).mean().to_frame().rename(columns={0: "mean"})
 
         self.df_std = get_data(
             self.host, self.charts_in_scope, after, before, points=10, group='stddev', col_sep='.'
-        ).mean().to_frame().rename(columns={0: "std"})
+            ).mean().to_frame().rename(columns={0: "std"})
 
     def create_data(self, df_allmetrics):
         """Use x, mean, sigma to generate z scores and 3sigma flags via some pandas manipulation.
@@ -99,7 +99,7 @@ class Service(SimpleService):
         # append last z_smooth_n rows of zscores to history table in wide format
         self.df_z_history = self.df_z_history.append(
             df_z[['z']].reset_index().pivot_table(values='z', columns='index'), sort=True
-        ).tail(self.z_smooth_n)
+            ).tail(self.z_smooth_n)
 
         # get average zscore for last z_smooth_n for each metric
         df_z_smooth = self.df_z_history.melt(value_name='z').groupby('index')['z'].mean().to_frame()
@@ -110,14 +110,13 @@ class Service(SimpleService):
         if self.mode == 'per_chart':
             df_z_smooth['chart'] = ['.'.join(x[0:2]) + '_z' for x in df_z_smooth.index.str.split('.').to_list()]
             if self.per_chart_agg == 'absmax':
-                data_z = \
-                list(df_z_smooth.groupby('chart').agg({'z': lambda x: max(x, key=abs)})['z'].to_dict().values())[0]
+                data_z = list(df_z_smooth.groupby('chart').agg({'z': lambda x: max(x, key=abs)})['z'].to_dict().values())[0]
             else:
                 data_z = list(df_z_smooth.groupby('chart').agg({'z': [self.per_chart_agg]})['z'].to_dict().values())[0]
 
         data_3sigma = {}
         for k in data_z:
-            data_3sigma[k.replace('_z', '')] = 1 if abs(data_z[k]) > 300 else 0
+            data_3sigma[k.replace('_z','')] = 1 if abs(data_z[k]) > 300 else 0
 
         return data_z, data_3sigma
 
@@ -126,8 +125,7 @@ class Service(SimpleService):
         if self.runs_counter <= self.burn_in or self.runs_counter % self.train_every_n == 0:
             self.train_model()
 
-        data_z, data_3sigma = self.create_data(
-            get_allmetrics(self.host, self.charts_in_scope, wide=True, col_sep='.').transpose())
+        data_z, data_3sigma = self.create_data(get_allmetrics(self.host, self.charts_in_scope, wide=True, col_sep='.').transpose())
         data = {**data_z, **data_3sigma}
 
         self.validate_charts('z', data_z, divisor=100)
