0148358de306faa1789066ed54fd7acfac64c645
Timotej Šiškovič <timotej@netdata.cloud>
2021-05-10 17:52:55 +0200

    ChartsAndDimensionsUpdated

--- aclk/schema-wrappers/chart_stream.cc
+++ aclk/schema-wrappers/chart_stream.cc
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include "../aclk-schemas/proto/chart/v1/stream.pb.h"
+#include "proto/chart/v1/stream.pb.h"
 #include "chart_stream.h"
 
 #include <sys/time.h>
@@ -55,6 +55,101 @@ char *generate_reset_chart_messages(size_t *len, const chart_reset_reason_t reas
             break;
         default:
             return NULL;
+    }
+
+    *len = msg.ByteSizeLong();
+    char *bin = (char*)malloc(*len);
+    if (bin)
+        msg.SerializeToArray(bin, *len);
+
+    return bin;
+}
+
+char *generate_charts_and_dimensions_updated(size_t *len, const charts_and_dims_updated_t *updates)
+{
+    chart::v1::ChartsAndDimensionsUpdated msg;
+    aclk_lib::v1::ACLKMessagePosition *pos;
+
+    google::protobuf::Map<std::string, std::string> *map;
+    struct label *label;
+
+    if (!updates->chart_count && !updates->dim_count) {
+        return NULL;
+    }
+
+    msg.set_batch_id(updates->batch_id);
+
+    for (int i = 0; i < updates->chart_count; i++) {
+        chart::v1::ChartInstanceUpdated *chart = msg.add_charts();
+        struct chart_instance_updated *c_chart = &updates->charts[i];
+        chart->set_id(c_chart->id);
+        chart->set_claim_id(c_chart->claim_id);
+        chart->set_node_id(c_chart->node_id);
+        chart->set_name(c_chart->name);
+
+        map = chart->mutable_chart_labels();
+        label = c_chart->label_head;
+        while (label) {
+            map->insert({label->key, label->value});
+            label = label->next;
+        }
+
+        switch (c_chart->memory_mode) {
+        case RRD_MEMORY_MODE_NONE:
+            chart->set_memory_mode(chart::v1::NONE);
+            break;
+        case RRD_MEMORY_MODE_RAM:
+            chart->set_memory_mode(chart::v1::RAM);
+            break;
+        case RRD_MEMORY_MODE_MAP:
+            chart->set_memory_mode(chart::v1::MAP);
+            break;
+        case RRD_MEMORY_MODE_SAVE:
+            chart->set_memory_mode(chart::v1::SAVE);
+            break;
+        case RRD_MEMORY_MODE_ALLOC:
+            chart->set_memory_mode(chart::v1::ALLOC);
+            break;
+        case RRD_MEMORY_MODE_DBENGINE:
+            chart->set_memory_mode(chart::v1::DB_ENGINE);
+            break;
+        default:
+            return NULL;
+            break;
+        }
+
+        chart->set_update_every_interval(c_chart->update_every);
+        chart->set_config_hash(c_chart->config_hash);
+
+        pos = chart->mutable_position();
+        pos->set_sequence_id(c_chart->position.sequence_id);
+        pos->set_previous_sequence_id(c_chart->position.previous_sequence_id);
+        pos->mutable_seq_id_created_at()->set_seconds(c_chart->position.seq_id_creation_time.tv_sec);
+        pos->mutable_seq_id_created_at()->set_nanos(c_chart->position.seq_id_creation_time.tv_sec * 1000);
+    }
+
+    for (int i = 0; i < updates->dim_count; i++) {
+        chart::v1::ChartDimensionUpdated *dim = msg.add_dimensions();
+        struct chart_dimension_updated *c_dim = &updates->dims[i];
+        dim->set_id(c_dim->id);
+        dim->set_chart_id(c_dim->chart_id);
+        dim->set_node_id(c_dim->node_id);
+        dim->set_claim_id(c_dim->claim_id);
+        dim->set_name(c_dim->name);
+
+        google::protobuf::Timestamp *tv = dim->mutable_created_at();
+        tv->set_seconds(c_dim->created_at.tv_sec);
+        tv->set_nanos(c_dim->created_at.tv_usec * 1000);
+
+        tv = dim->mutable_last_timestamp();
+        tv->set_seconds(c_dim->last_timestamp.tv_sec);
+        tv->set_nanos(c_dim->last_timestamp.tv_usec * 1000);
+
+        pos = dim->mutable_position();
+        pos->set_sequence_id(c_dim->position.sequence_id);
+        pos->set_previous_sequence_id(c_dim->position.previous_sequence_id);
+        pos->mutable_seq_id_created_at()->set_seconds(c_dim->position.seq_id_creation_time.tv_sec);
+        pos->mutable_seq_id_created_at()->set_nanos(c_dim->position.seq_id_creation_time.tv_sec * 1000);
     }
 
     *len = msg.ByteSizeLong();
--- aclk/schema-wrappers/chart_stream.h
+++ aclk/schema-wrappers/chart_stream.h
@@ -7,6 +7,8 @@
 extern "C" {
 #endif
 
+#include "database/rrd.h"
+
 typedef struct {
     char* claim_id;
     char* node_id;
@@ -35,6 +37,51 @@ typedef enum {
 } chart_reset_reason_t;
 
 char *generate_reset_chart_messages(size_t *len, const chart_reset_reason_t reason);
+
+struct aclk_message_position {
+    uint64_t sequence_id;
+    struct timeval seq_id_creation_time;
+    uint64_t previous_sequence_id;
+};
+
+struct chart_instance_updated {
+    const char *id;
+    const char *claim_id;
+    const char *node_id;
+    const char *name;
+
+    struct label *label_head;
+
+    RRD_MEMORY_MODE memory_mode;
+
+    uint32_t update_every;
+    const char * config_hash;
+
+    struct aclk_message_position position;
+};
+
+struct chart_dimension_updated {
+    const char *id;
+    const char *chart_id;
+    const char *node_id;
+    const char *claim_id;
+    const char *name;
+    struct timeval created_at;
+    struct timeval last_timestamp;
+    struct aclk_message_position position;
+};
+
+typedef struct {
+    struct chart_instance_updated *charts;
+    uint16_t chart_count;
+
+    struct chart_dimension_updated *dims;
+    uint16_t dim_count;
+
+    uint64_t batch_id;
+} charts_and_dims_updated_t;
+
+char *generate_charts_and_dimensions_updated(size_t *len, const charts_and_dims_updated_t *updates);
 
 #ifdef __cplusplus
 }
