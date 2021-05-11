a8800109ee66947af0beba3ad60787dda18a621c
Emmanuel Vasilakis <mrzammler@mm.st>
2021-05-11 17:27:57 +0300

    change format

--- database/sqlite/sqlite_functions.h
+++ database/sqlite/sqlite_functions.h
@@ -81,33 +81,6 @@ extern void sql_load_node_id(RRDHOST *host);
 extern int execute_insert(sqlite3_stmt *res);
 extern void compute_chart_hash(RRDSET *st);
 extern void sql_chart_from_hash_id(char *hash_str, BUFFER *wb);
-extern int alert_hash_and_store_config(
-    uuid_t config_hash_id,
-    const char *alarm,
-    const char *template,
-    const char *os,
-    const char *host,
-    const char *on,
-    const char *families,
-    const char *plugin,
-    const char *module,
-    const char *lookup,
-    const char *calc,
-    const char *every,
-    const char *green,
-    const char *red,
-    const char *warn,
-    const char *crit,
-    const char *exec,
-    const char *to,
-    const char *units,
-    const char *info,
-    const char *classification,
-    const char *component,
-    const char *type,
-    const char *delay,
-    const char *options,
-    const char *repeat,
-    const char *host_labels);
+extern int alert_hash_and_store_config(uuid_t config_hash_id, const char *alarm, const char *template, const char *os, const char *host, const char *on, const char *families, const char *plugin, const char *module, const char *lookup, const char *calc, const char *every, const char *green, const char *red, const char *warn, const char *crit, const char *exec, const char *to, const char *units, const char *info, const char *classification, const char *component, const char *type, const char *delay, const char *options, const char *repeat, const char *host_labels);
 extern void sql_select_alert(char *hash_str, BUFFER *wb);
 #endif //NETDATA_SQLITE_FUNCTIONS_H
