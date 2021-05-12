2d88bcc831ba7909caf4f8f792deb55f43729d02
Emmanuel Vasilakis <mrzammler@mm.st>
2021-05-12 19:01:34 +0300

    write to aclk payload

--- health/health_log.c
+++ health/health_log.c
@@ -160,7 +160,8 @@ inline void health_alarm_log_save(RRDHOST *host, ALARM_ENTRY *ae) {
     if (netdata_cloud_setting) {
         if ((ae->new_status == RRDCALC_STATUS_WARNING || ae->new_status == RRDCALC_STATUS_CRITICAL) ||
             ((ae->old_status == RRDCALC_STATUS_WARNING || ae->old_status == RRDCALC_STATUS_CRITICAL))) {
-            aclk_update_alarm(host, ae);
+            //aclk_update_alarm(host, ae);
+            sql_queue_alarm_to_aclk(host, ae);
         }
     }
 #endif
