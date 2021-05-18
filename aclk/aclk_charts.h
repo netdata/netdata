a8c7637e2152b2d3f2ac96b3f092d6bff3852b92
Timotej Šiškovič <timotej@netdata.cloud>
2021-05-18 20:13:03 +0200

    add query

---
+++ aclk/aclk_charts.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+#ifndef ACLK_CHARTS_H
+#define ACLK_CHARTS_H
+
+#include "../daemon/common.h"
+
+void aclk_chart_dim_update(charts_and_dims_updated_t *update);
+
+#endif /* ACLK_CHARTS_H */
