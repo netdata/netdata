14349694a41863fb5295c5b66004053459d3c2e8
Timotej Šiškovič <timotej@netdata.cloud>
2021-05-21 12:22:42 +0200

    rename function to prevent confusion

--- aclk/aclk_charts.c
+++ aclk/aclk_charts.c
@@ -3,7 +3,7 @@
 
 #include "aclk_query_queue.h"
 
-void aclk_chart_dim_update(charts_and_dims_updated_t *update) {
+void aclk_charts_and_dims_update(charts_and_dims_updated_t *update) {
     aclk_query_t query = aclk_query_new(CHART_DIM_UPDATE);
     query->data.chart_dim_update = update;
     aclk_queue_query(query);
--- aclk/aclk_charts.h
+++ aclk/aclk_charts.h
@@ -4,7 +4,7 @@
 
 #include "../daemon/common.h"
 
-void aclk_chart_dim_update(charts_and_dims_updated_t *update);
+void aclk_charts_and_dims_update(charts_and_dims_updated_t *update);
 
 void aclk_chart_inst_update(char **payloads, size_t *payload_sizes, struct aclk_message_position *new_positions);
 
