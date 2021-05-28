19acfc90667e81a00b54909db594819441a18541
wangpei <81410079+wangpei-nice@users.noreply.github.com>
2021-05-29 00:08:47 +0800

    fix ebpf.plugin segfault when ebpf_load_program return null pointer (#11203)

--- collectors/ebpf.plugin/ebpf_dcstat.c
+++ collectors/ebpf.plugin/ebpf_dcstat.c
@@ -122,13 +122,15 @@ static void ebpf_dcstat_cleanup(void *ptr)
 
     ebpf_dcstat_clean_names();
 
-    struct bpf_program *prog;
-    size_t i = 0 ;
-    bpf_object__for_each_program(prog, objects) {
-        bpf_link__destroy(probe_links[i]);
-        i++;
+    if (probe_links) {
+        struct bpf_program *prog;
+        size_t i = 0 ;
+        bpf_object__for_each_program(prog, objects) {
+            bpf_link__destroy(probe_links[i]);
+            i++;
+        }
+        bpf_object__close(objects);
     }
-    bpf_object__close(objects);
 }
 
 /*****************************************************************
