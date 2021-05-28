689ebbc5e3141f72424fb74ed3e038318745db2e
Timotej Šiškovič <timotej@netdata.cloud>
2021-05-28 14:46:54 +0200

    AlarmLogHealth

--- aclk/schema-wrappers/alarm_stream.cc
+++ aclk/schema-wrappers/alarm_stream.cc
@@ -1,8 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "alarm_stream.h"
 
 #include "proto/alarm/v1/stream.pb.h"
 
 #include "libnetdata/libnetdata.h"
+
+#include "common.h"
 
 struct start_alarm_streaming parse_start_alarm_streaming(const char *data, size_t len)
 {
@@ -27,4 +31,43 @@ char *parse_send_alarm_log_health(const char *data, size_t len)
     if (!msg.ParseFromArray(data, len))
         return NULL;
     return strdupz(msg.node_id().c_str());
+}
+
+char *generate_alarm_log_health(size_t *len, struct alarm_log_health *data)
+{
+    alarmstream::v1::AlarmLogHealth msg;
+    alarmstream::v1::LogEntries *entries;
+
+    msg.set_claim_id(data->claim_id);
+    msg.set_node_id(data->node_id);
+    msg.set_enabled(data->enabled);
+
+    switch (data->status) {
+        case ALARM_LOG_STATUS_IDLE:
+            msg.set_status(alarmstream::v1::ALARM_LOG_STATUS_IDLE);
+            break;
+        case ALARM_LOG_STATUS_RUNNING:
+            msg.set_status(alarmstream::v1::ALARM_LOG_STATUS_RUNNING);
+            break;
+        case ALARM_LOG_STATUS_UNSPECIFIED:
+            msg.set_status(alarmstream::v1::ALARM_LOG_STATUS_UNSPECIFIED);
+            break;
+        default:
+            error("Unknown status of AlarmLogHealth LogEntry");
+            return NULL;
+    }
+
+    entries = msg.mutable_log_entries();
+    entries->set_first_sequence_id(data->first_seq_id);
+    entries->set_last_sequence_id(data->last_seq_id);
+
+    set_google_timestamp_from_timeval(data->first_when, entries->mutable_first_when());
+    set_google_timestamp_from_timeval(data->last_when, entries->mutable_last_when());
+
+    *len = msg.ByteSizeLong();
+    char *bin = (char*)mallocz(*len);
+    if (!msg.SerializeToArray(bin, *len))
+        return NULL;
+
+    return bin;
 }
--- aclk/schema-wrappers/alarm_stream.h
+++ aclk/schema-wrappers/alarm_stream.h
@@ -20,6 +20,24 @@ struct start_alarm_streaming {
 struct start_alarm_streaming parse_start_alarm_streaming(const char *data, size_t len);
 char *parse_send_alarm_log_health(const char *data, size_t len);
 
+enum alarm_log_status_aclk {
+    ALARM_LOG_STATUS_UNSPECIFIED = 0,
+    ALARM_LOG_STATUS_RUNNING = 1,
+    ALARM_LOG_STATUS_IDLE = 2
+};
+struct alarm_log_health {
+    char *claim_id;
+    char *node_id;
+    int enabled;
+    enum alarm_log_status_aclk status;
+    int64_t first_seq_id;
+    struct timeval first_when;
+    int64_t last_seq_id;
+    struct timeval last_when;
+};
+
+char *generate_alarm_log_health(size_t *len, struct alarm_log_health *data);
+
 #ifdef __cplusplus
 }
 #endif
