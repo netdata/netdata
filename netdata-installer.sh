0d96e0a187ee3419a9939b56970248356d05c1e7
vkalintiris <vasilis@netdata.cloud>
2021-06-01 13:22:36 +0300

    Compile/Link with absolute paths for bundled/vendored deps. (#11129)
    * Do not accept a path when using --with-bundled-lws.
    
    The bundled library is always placed under externaldeps/libwebsockets,
    when using the netdata-installer.sh script. When this option is missing,
    we look for the system-wide installed version.
    
    * Do not accept a path when using --with-bundled-libJudy.
    
    The bundled library is always placed under externaldeps/libJudy. When
    the option is not given, we look for the system-wide installed version.
    
    * Use absolute header paths for repo-internal deps.
    
    * Use absolute library paths for repo-internal deps.

--- netdata-installer.sh
+++ netdata-installer.sh
@@ -701,7 +701,7 @@ bundle_libwebsockets() {
       copy_libwebsockets "${tmp}/libwebsockets-${LIBWEBSOCKETS_PACKAGE_VERSION}" &&
       rm -rf "${tmp}"; then
       run_ok "libwebsockets built and prepared."
-      NETDATA_CONFIGURE_OPTIONS="${NETDATA_CONFIGURE_OPTIONS} --with-bundled-lws=externaldeps/libwebsockets"
+      NETDATA_CONFIGURE_OPTIONS="${NETDATA_CONFIGURE_OPTIONS} --with-bundled-lws"
     else
       run_failed "Failed to build libwebsockets."
       if [ -n "${NETDATA_REQUIRE_CLOUD}" ]; then
@@ -789,7 +789,7 @@ bundle_judy() {
       copy_judy "${tmp}/libjudy-${JUDY_PACKAGE_VERSION}" &&
       rm -rf "${tmp}"; then
       run_ok "libJudy built and prepared."
-      NETDATA_CONFIGURE_OPTIONS="${NETDATA_CONFIGURE_OPTIONS} --with-libJudy=externaldeps/libJudy"
+      NETDATA_CONFIGURE_OPTIONS="${NETDATA_CONFIGURE_OPTIONS} --with-bundled-libJudy"
     else
       run_failed "Failed to build libJudy."
       if [ -n "${NETDATA_BUILD_JUDY}" ]; then
