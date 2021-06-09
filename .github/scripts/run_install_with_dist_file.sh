9991529ec46302a15608d441f86e4b0dac78e16d
Austin S. Hemmelgarn <austin@netdata.cloud>
2021-06-09 08:00:55 -0400

    Disable telemetry in CI.
    It skews our data in (probably) strange ways.

--- .github/scripts/run_install_with_dist_file.sh
+++ .github/scripts/run_install_with_dist_file.sh
@@ -29,6 +29,7 @@ printf >&2 "Entering %s and starting docker run ..." "${distdir}"
 
 pushd "${distdir}" || exit 1
 docker run \
+  -e DO_NOT_TRACK=1 \
   -v "${PWD}:/netdata" \
   -w /netdata \
   "ubuntu:latest" \
