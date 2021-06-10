d5a4474682ab95a4b2cf6ce50fa5d5e579499d4a
Timotej Šiškovič <timotej@netdata.cloud>
2021-06-10 13:22:21 +0200

    add tx msg generators

--- aclk/aclk_tx_msgs.h
+++ aclk/aclk_tx_msgs.h
@@ -6,6 +6,7 @@
 #include "libnetdata/libnetdata.h"
 #include "daemon/common.h"
 #include "mqtt_wss_client.h"
+#include "schema-wrappers/schema_wrappers.h"
 
 void aclk_send_info_metadata(mqtt_wss_client client, int metadata_submitted, RRDHOST *host);
 void aclk_send_alarm_metadata(mqtt_wss_client client, int metadata_submitted);
@@ -18,5 +19,12 @@ void aclk_alarm_state_msg(mqtt_wss_client client, json_object *msg);
 
 json_object *aclk_generate_disconnect(const char *message);
 int aclk_send_app_layer_disconnect(mqtt_wss_client client, const char *message);
+
+// new protobuf msgs
+uint16_t aclk_send_agent_connection_update(mqtt_wss_client client, int reachable);
+char *aclk_generate_lwt(size_t *size);
+
+void aclk_generate_node_registration(mqtt_wss_client client, node_instance_creation_t *node_creation);
+void aclk_generate_node_state_update(mqtt_wss_client client, node_instance_connection_t *node_connection);
 
 #endif
