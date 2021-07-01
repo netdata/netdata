c97af0b45cf78af7fb155d98746e492572c49032
Timotej Šiškovič <timotej@netdata.cloud>
2021-07-01 14:57:52 +0200

    alarm config wrappers

--- aclk/schema-wrappers/alarm_config.cc
+++ aclk/schema-wrappers/alarm_config.cc
@@ -1,3 +1,72 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "alarm_config.h"
 
 #include "proto/alarm/v1/config.pb.h"
+
+#include "libnetdata/libnetdata.h"
+
+using namespace alarmconfig::v1;
+
+char *generate_provide_alarm_configuration(size_t *len, struct provide_alarm_configuration *data)
+{
+    ProvideAlarmConfiguration msg;
+    AlarmConfiguration *cfg = msg.mutable_config();
+
+    msg.set_config_hash(data->cfg_hash);
+
+    cfg->set_alarm(data->cfg.alarm);
+    cfg->set_template_(data->cfg.tmpl);
+    cfg->set_on_chart(data->cfg.on_chart);
+    
+    cfg->set_classification(data->cfg.classification);
+    cfg->set_type(data->cfg.type);
+    cfg->set_component(data->cfg.component);
+        
+    cfg->set_os(data->cfg.os);
+    cfg->set_hosts(data->cfg.hosts);
+    cfg->set_plugin(data->cfg.plugin);
+    cfg->set_module(data->cfg.module);
+    cfg->set_charts(data->cfg.charts);
+    cfg->set_families(data->cfg.families);
+    cfg->set_lookup(data->cfg.lookup);
+    cfg->set_every(data->cfg.every);
+    cfg->set_units(data->cfg.units);
+
+    cfg->set_green(data->cfg.green);
+    cfg->set_red(data->cfg.red);
+
+    cfg->set_calculation_expr(data->cfg.calculation_expr);
+    cfg->set_warning_expr(data->cfg.warning_expr);
+    cfg->set_critical_expr(data->cfg.critical_expr);
+    
+    cfg->set_recipient(data->cfg.recipient);
+    cfg->set_exec(data->cfg.exec);
+    cfg->set_delay(data->cfg.delay);
+    cfg->set_repeat(data->cfg.repeat);
+    cfg->set_info(data->cfg.info);
+    cfg->set_options(data->cfg.options);
+    cfg->set_host_labels(data->cfg.host_labels);
+
+    *len = msg.ByteSizeLong();
+    char *bin = (char*)mallocz(*len);
+    if (!msg.SerializeToArray(bin, *len))
+        return NULL;
+
+    return bin;
+}
+
+char *parse_send_alarm_configuration(const char *data, size_t len)
+{
+    SendAlarmConfiguration msg;
+    try {
+        if (!msg.ParseFromArray(data, len))
+            return NULL;
+        if (!msg.config_hash().c_str())
+            return NULL;
+        return strdupz(msg.config_hash().c_str());
+    } catch (...) {
+        return NULL;
+    }
+}
+
--- aclk/schema-wrappers/alarm_config.h
+++ aclk/schema-wrappers/alarm_config.h
@@ -9,6 +9,48 @@
 extern "C" {
 #endif
 
+struct aclk_alarm_configuration {
+    char *alarm;
+    char *tmpl;
+    char *on_chart;
+    
+    char *classification;
+    char *type;
+    char *component;
+        
+    char *os;
+    char *hosts;
+    char *plugin;
+    char *module;
+    char *charts;
+    char *families;
+    char *lookup;
+    char *every;
+    char *units;
+
+    char *green;
+    char *red;
+
+    char *calculation_expr;
+    char *warning_expr;
+    char *critical_expr;
+    
+    char *recipient;
+    char *exec;
+    char *delay;
+    char *repeat;
+    char *info;
+    char *options;
+    char *host_labels;
+};
+
+struct provide_alarm_configuration {
+    char *cfg_hash;
+    struct aclk_alarm_configuration cfg;
+};
+
+char *generate_provide_alarm_configuration(size_t *len, struct provide_alarm_configuration *data);
+char *parse_send_alarm_configuration(const char *data, size_t len);
 
 #ifdef __cplusplus
 }
