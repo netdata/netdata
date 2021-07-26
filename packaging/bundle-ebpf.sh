1bc3051f8e4736b1a71c3744a1c0d0fd59eed40c
Austin S. Hemmelgarn <austin@netdata.cloud>
2021-07-26 07:05:59 -0400

    Properly handle eBPF plugin in RPM packages. (#11362)

--- packaging/bundle-ebpf.sh
+++ packaging/bundle-ebpf.sh
@@ -6,12 +6,14 @@ PLUGINDIR="${2}"
 EBPF_VERSION="$(cat "${SRCDIR}/packaging/ebpf.version")"
 EBPF_TARBALL="netdata-kernel-collector-glibc-${EBPF_VERSION}.tar.xz"
 
-mkdir -p "${SRCDIR}/tmp/ebpf"
-curl -sSL --connect-timeout 10 --retry 3 "https://github.com/netdata/kernel-collector/releases/download/${EBPF_VERSION}/${EBPF_TARBALL}" > "${EBPF_TARBALL}" || exit 1
-grep "${EBPF_TARBALL}" "${SRCDIR}/packaging/ebpf.checksums" | sha256sum -c - || exit 1
-tar -xaf "${EBPF_TARBALL}" -C "${SRCDIR}/tmp/ebpf" || exit 1
-if [ ! -d "${PLUGINDIR}/ebpf.d" ];then
-    mkdir "${PLUGINDIR}/ebpf.d"
+if [ -x "${PLUGINDIR}/ebpf.plugin" ] ; then
+    mkdir -p "${SRCDIR}/tmp/ebpf"
+    curl -sSL --connect-timeout 10 --retry 3 "https://github.com/netdata/kernel-collector/releases/download/${EBPF_VERSION}/${EBPF_TARBALL}" > "${EBPF_TARBALL}" || exit 1
+    grep "${EBPF_TARBALL}" "${SRCDIR}/packaging/ebpf.checksums" | sha256sum -c - || exit 1
+    tar -xaf "${EBPF_TARBALL}" -C "${SRCDIR}/tmp/ebpf" || exit 1
+    if [ ! -d "${PLUGINDIR}/ebpf.d" ];then
+        mkdir "${PLUGINDIR}/ebpf.d"
+    fi
+    # shellcheck disable=SC2046
+    cp -a $(find "${SRCDIR}/tmp/ebpf" -mindepth 1 -maxdepth 1) "${PLUGINDIR}/ebpf.d"
 fi
-# shellcheck disable=SC2046
-cp -a $(find "${SRCDIR}/tmp/ebpf" -mindepth 1 -maxdepth 1) "${PLUGINDIR}/ebpf.d"
