3d2d3ee62d0a7c596a8f0a2627dfdde10c7a755d
Thiago Marques <thiagoftsm@gmail.com>
2021-08-04 13:44:39 +0000

    ipc_through_perf: fix typo dashboard_info.js

--- web/gui/dashboard_info.js
+++ web/gui/dashboard_info.js
@@ -3471,7 +3471,7 @@ netdataDashboard.context = {
     // Perf
 
     'perf.instructions_per_cycle': {
-        info: 'An IPC < 1.0 likely means memory bound, and an IPC > 1.0 likely means instruction bound. For more details about the metric take a lookt at this <a href="https://www.brendangregg.com/blog/2017-05-09/cpu-utilization-is-wrong.html" target="_blank">blog post</a>.'
+        info: 'An IPC < 1.0 likely means memory bound, and an IPC > 1.0 likely means instruction bound. For more details about the metric take a look at this <a href="https://www.brendangregg.com/blog/2017-05-09/cpu-utilization-is-wrong.html" target="_blank">blog post</a>.'
     },
 
     // ------------------------------------------------------------------------
