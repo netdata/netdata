ed9d96237773b933991fc26326c5c3202fb57779
Thiago Marques <thiagoftsm@gmail.com>
2021-08-09 19:22:34 +0000

    ebpf_split_programs: Fix selection mode

--- libnetdata/ebpf/ebpf.h
+++ libnetdata/ebpf/ebpf.h
@@ -178,7 +178,6 @@ extern struct bpf_link **ebpf_load_program(char *plugins_dir,
 
 extern void ebpf_mount_config_name(char *filename, size_t length, char *path, const char *config);
 extern int ebpf_load_config(struct config *config, char *filename);
-extern void ebpf_update_module_using_config(ebpf_module_t *modules);
 extern void ebpf_update_module(ebpf_module_t *em);
 extern void ebpf_update_names(ebpf_specify_name_t *opt, ebpf_module_t *em);
 extern void ebpf_load_addresses(ebpf_addresses_t *fa, int fd);
