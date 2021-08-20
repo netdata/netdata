b8e17824f399a5efded6c638b43fd20a911ddc2c
Timotej Šiškovič <timotej@netdata.cloud>
2021-08-20 17:39:59 +0200

    avoid exceptions in node_info

--- aclk/schema-wrappers/node_info.cc
+++ aclk/schema-wrappers/node_info.cc
@@ -9,54 +9,66 @@ static int generate_node_info(nodeinstance::info::v1::NodeInfo *info, struct acl
     struct label *label;
     google::protobuf::Map<std::string, std::string> *map;
 
-    try
-    {
+    if (data->name)
         info->set_name(data->name);
-    
+
+    if (data->os)
         info->set_os(data->os);
+    if (data->os_name)
         info->set_os_name(data->os_name);
+    if (data->os_version)
         info->set_os_version(data->os_version);
-    
+
+    if (data->kernel_name)
         info->set_kernel_name(data->kernel_name);
+    if (data->kernel_version)
         info->set_kernel_version(data->kernel_version);
-    
+
+    if (data->architecture)
         info->set_architecture(data->architecture);
-    
-        info->set_cpus(data->cpus);
-    
+
+    info->set_cpus(data->cpus);
+
+    if (data->cpu_frequency)
         info->set_cpu_frequency(data->cpu_frequency);
-    
+
+    if (data->memory)
         info->set_memory(data->memory);
-    
+
+    if (data->disk_space)
         info->set_disk_space(data->disk_space);
-    
+
+    if (data->version)
         info->set_version(data->version);
-    
+
+    if (data->release_channel)
         info->set_release_channel(data->release_channel);
-    
+
+    if (data->timezone)
         info->set_timezone(data->timezone);
-    
+
+    if (data->virtualization_type)
         info->set_virtualization_type(data->virtualization_type);
-    
+
+    if (data->container_type)
         info->set_container_type(data->container_type);
-    
+
+    if (data->custom_info)
         info->set_custom_info(data->custom_info);
-    
-        for (size_t i = 0; i < data->service_count; i++)
-            info->add_services(data->services[i]);
-    
+
+    for (size_t i = 0; i < data->service_count; i++)
+        info->add_services(data->services[i]);
+
+    if (data->machine_guid)
         info->set_machine_guid(data->machine_guid);
-    
-        map = info->mutable_host_labels();
-        label = data->host_labels_head;
-        while (label) {
-            map->insert({label->key, label->value});
-            label = label->next;
-        }
+
+    map = info->mutable_host_labels();
+    label = data->host_labels_head;
+    while (label) {
+        map->insert({label->key, label->value});
+        label = label->next;
     }
-    catch(...) {
-        return 1;
-    }
+
     return 0;
 }
 
