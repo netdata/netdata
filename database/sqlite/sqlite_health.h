f4adcfde50b39bc681419c2140ec6d366a2acea5
Emmanuel Vasilakis <mrzammler@mm.st>
2021-08-26 11:22:15 +0300

    removed web api endopoint to get alert config

--- database/sqlite/sqlite_health.h
+++ database/sqlite/sqlite_health.h
@@ -12,6 +12,5 @@ extern void sql_health_alarm_log_update(RRDHOST *host, ALARM_ENTRY *ae);
 extern void sql_health_alarm_log_insert(RRDHOST *host, ALARM_ENTRY *ae);
 extern void sql_health_alarm_log_save(RRDHOST *host, ALARM_ENTRY *ae);
 extern void sql_health_alarm_log_cleanup(RRDHOST *host);
-extern void sql_select_alert_config(char *hash_str, BUFFER *wb);
 extern int alert_hash_and_store_config(uuid_t hash_id, struct alert_config *cfg);
 #endif //NETDATA_SQLITE_HEALTH_H
