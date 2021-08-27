651189517cf5c924cb1365a01b9cd89e14250019
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2021-08-27 21:47:02 +0300

    Expose prepare_statement

--- database/sqlite/sqlite_functions.h
+++ database/sqlite/sqlite_functions.h
@@ -63,6 +63,7 @@ extern int find_uuid_type(uuid_t *uuid);
 extern void sql_rrdset2json(RRDHOST *host, BUFFER *wb);
 
 extern RRDHOST *sql_create_host_by_uuid(char *guid);
+extern int prepare_statement(sqlite3 *database, char *query, sqlite3_stmt **statement);
 extern void db_execute(const char *cmd);
 extern int file_is_migrated(char *path);
 extern void add_migrated_file(char *path, uint64_t file_size);
