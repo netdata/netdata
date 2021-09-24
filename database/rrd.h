b3f0fcd9f2d1c986d083c1eb84551726f9c31fec
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2021-09-24 21:53:02 +0300

    Define liveness count and proper include files

--- database/rrd.h
+++ database/rrd.h
@@ -50,6 +50,7 @@ struct context_param {
     uint8_t flags;
 };
 
+#define RRDSET_MINIMUM_LIVE_COUNT 3
 #define META_CHART_UPDATED 1
 #define META_PLUGIN_UPDATED 2
 #define META_MODULE_UPDATED 4
@@ -1359,4 +1360,8 @@ extern void set_host_properties(
 #endif
 #include "sqlite/sqlite_functions.h"
 #include "sqlite/sqlite_aclk.h"
+#include "sqlite/sqlite_aclk_chart.h"
+#include "sqlite/sqlite_aclk_alert.h"
+#include "sqlite/sqlite_aclk_node.h"
+#include "sqlite/sqlite_health.h"
 #endif /* NETDATA_RRD_H */
