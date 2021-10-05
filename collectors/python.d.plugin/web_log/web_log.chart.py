02237e6ffe2d2a361b2bbb64f97ab738bb1bd048
Soren Dossing <netcom@sauber.net>
2021-10-05 12:36:13 +0200

    Use filter when checking
    Module supports filter: options. These filter options are used for including or excluding lines during each update cycle, but it not used at startup when checking for a valid file to generate charts.
    
    This change respects filter options during check by reading lines from end for file one by one passing through filter until a line is accepted by filter.

--- collectors/python.d.plugin/web_log/web_log.chart.py
+++ collectors/python.d.plugin/web_log/web_log.chart.py
@@ -433,7 +433,9 @@ class Web:
         return getattr(self.service, item)
 
     def check(self):
-        last_line = read_last_line(self.log_path)
+        raw_data = read_last_lines(self.log_path)
+        last_lines = filter_data(raw_data=raw_data, pre_filter=self.pre_filter)
+        last_line = next(last_lines)
         if not last_line:
             return False
         # Custom_log_format or predefined log format.
@@ -939,7 +941,9 @@ class Squid:
         return getattr(self.service, item)
 
     def check(self):
-        last_line = read_last_line(self.log_path)
+        raw_data = read_last_lines(self.log_path)
+        last_lines = filter_data(raw_data=raw_data, pre_filter=self.pre_filter)
+        last_line = next(last_lines)
         if not last_line:
             return False
         self.storage['unique_all_time'] = list()
@@ -1192,3 +1196,36 @@ def filter_data(raw_data, pre_filter):
         elif elem.description == 'filter_exclude':
             filtered = filterfalse(elem.func, filtered)
     return filtered
+
+# Snipped from https://stackoverflow.com/questions/2301789/how-to-read-a-file-in-reverse-order
+def read_last_lines(filename, buf_size=8192):
+    #"""A generator that returns the lines of a file in reverse order"""
+    with open(filename) as fh:
+        segment = None
+        offset = 0
+        fh.seek(0, os.SEEK_END)
+        file_size = remaining_size = fh.tell()
+        while remaining_size > 0:
+            offset = min(file_size, offset + buf_size)
+            fh.seek(file_size - offset)
+            buffer = fh.read(min(remaining_size, buf_size))
+            remaining_size -= buf_size
+            lines = buffer.split('\n')
+            # The first line of the buffer is probably not a complete line so
+            # we'll save it and append it to the last line of the next buffer
+            # we read
+            if segment is not None:
+                # If the previous chunk starts right from the beginning of line
+                # do not concat the segment to the last line of new chunk.
+                # Instead, yield the segment first
+                if buffer[-1] != '\n':
+                    lines[-1] += segment
+                else:
+                    yield segment
+            segment = lines[0]
+            for index in range(len(lines) - 1, 0, -1):
+                if lines[index]:
+                    yield lines[index]
+        # Don't yield None if the file was empty
+        if segment is not None:
+            yield segment
