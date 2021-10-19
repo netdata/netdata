1f3150c0a6eecf1e6cecbe5ee96cebd3301ff89b
Austin S. Hemmelgarn <austin@netdata.cloud>
2021-10-19 07:28:26 -0400

    Optimize static build and update various dependencies. (#11660)

--- packaging/makeself/install-alpine-packages.sh
+++ packaging/makeself/install-alpine-packages.sh
@@ -34,20 +34,10 @@ apk add --no-cache -U \
   pkgconfig \
   protobuf-dev \
   snappy-dev \
+  snappy-static \
   util-linux-dev \
   wget \
   xz \
   zlib-dev \
   zlib-static ||
   exit 1
-
-# snappy doesn't have static version in alpine, let's compile it
-export SNAPPY_VER="1.1.7"
-wget -O /snappy.tar.gz https://github.com/google/snappy/archive/${SNAPPY_VER}.tar.gz
-tar -C / -xf /snappy.tar.gz
-rm /snappy.tar.gz
-cd /snappy-${SNAPPY_VER} || exit 1
-mkdir build
-cd build || exit 1
-cmake -DCMAKE_BUILD_SHARED_LIBS=true -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_INSTALL_LIBDIR=lib ../
-make && make install
