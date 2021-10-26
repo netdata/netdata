c832a540cb394703a4d53ae8bd8f4782280047da
Thiago Marques <thiagoftsm@gmail.com>
2021-10-26 22:57:35 +0000

    ebpf_kickstart: add a missing , to avoid error no such file or directory

--- packaging/makeself/build-static.sh
+++ packaging/makeself/build-static.sh
@@ -39,7 +39,7 @@ if ! docker inspect "${DOCKER_CONTAINER_NAME}" > /dev/null 2>&1; then
   # inside the container and runs the script install-alpine-packages.sh
   # (also inside the container)
   #
-  if docker inspect alpine:3.14 > dev/null 2>&1; then
+  if docker inspect alpine:3.14 > /dev/null 2>&1; then
     run docker image remove alpine:3.14
     run docker pull --platform=${platform}  alpine:3.14
   fi
