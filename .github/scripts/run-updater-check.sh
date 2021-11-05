ca7117990024f10e21e0bea0b19344b9f90dee1c
Austin S. Hemmelgarn <austin@netdata.cloud>
2021-11-05 07:44:07 -0400

    Better delineate phases of updater check in logs.

--- .github/scripts/run-updater-check.sh
+++ .github/scripts/run-updater-check.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo ">>> Installing Netdata..."
 /netdata/packaging/installer/kickstart.sh --dont-wait --disable-telemetry || exit 1
+echo ">>> Updating Netdata..."
 /netdata/packaging/installer/netdata-updater.sh --not-running-from-cron --no-updater-self-update || exit 1
+echo ">>> Checking if update was successful..."
 /netdata/.github/scripts/check-updater.sh || exit 1
