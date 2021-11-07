1ff2f45f7e6804870cc3fdad2d491f1572eca279
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2021-11-07 20:47:47 +0200

    Process dimension deletion when retention updates are sent

--- database/sqlite/sqlite_aclk.c
+++ database/sqlite/sqlite_aclk.c
@@ -455,10 +455,12 @@ void aclk_database_worker(void *arg)
 #ifdef ENABLE_NEW_CLOUD_PROTOCOL
                 case ACLK_DATABASE_DIM_DELETION:
                     debug(D_ACLK_SYNC,"Sending dimension deletion information %s", wc->uuid_str);
+                    aclk_process_dimension_deletion(wc, cmd);
                     break;
                 case ACLK_DATABASE_UPD_RETENTION:
                     debug(D_ACLK_SYNC,"Sending retention info for %s", wc->uuid_str);
                     aclk_update_retention(wc, cmd);
+                    aclk_process_dimension_deletion(wc, cmd);
                     break;
 #endif
 
