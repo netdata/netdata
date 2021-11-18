7117e3bd352cd8812f29760e6f36efb5b6c91d2a
Marius Manea <marius@netdata.com>
2021-11-18 17:06:20 +0000

    make rhel.sh work for fedora 25

--- packaging/installer/dependencies/rhel.sh
+++ packaging/installer/dependencies/rhel.sh
@@ -1,9 +1,31 @@
 #!/usr/bin/env bash
 # Package tree used for installing netdata on distribution:
-# << Fedora >>
-# supported versions: 22->35
+# << Fedora >> versions: 22->35
+# << CentOS >> versions: 6/7/8
+# << RHEL >>   versions: 6/7/8
 
-declare -a package_tree_list1=(
+os_name=$1
+
+if [[ -z $os_name ]]; then
+  echo "ERROR: os name must be passed as parameter to $0!"
+  exit 1
+fi
+
+function os_version {
+  if [[ -f /etc/os-release ]]; then
+    cat /etc/os-release | grep VERSION_ID | cut -d'=' -f2
+  fi
+}
+
+if [[ $os_name == "fedora" ]] && [[ $(os_version) -gt 24 ]]; then
+  ulogd_pkg=
+else
+  ulogd_pkg=ulogd
+fi
+
+source default.sh
+
+declare -a package_tree_rhel=(
   autoconf-archive
   zlib-devel
   libuuid-devel
@@ -14,70 +36,11 @@ declare -a package_tree_list1=(
   openssl-devel
   Judy-devel
   elfutils-libelf-devel
+  mailx
+  nmap-ncat
+  pkgconfig
+  ${ulogd_pkg}
 )
 
-declare -a package_tree_list=(
- "findutils"
- "autoconf"
- "autoconf-archive"
- "autogen"
- "automake"
- "libtool"
- "cmake"
- "json-c-devel"
- "bridge-utils"
- "chorny"
- "curl"
- "gzip"
- "tar"
- "git"
- "gcc"
- "gcc-c++"
- "gdb"
- "iotop"
- "iproute"
- "ipset"
- "jq"
- "iptables"
- "zlib-devel"
- "libuuid-devel"
- "libmnl-devel"
- "lm_sensors"
- "logwatch"
- "lxc"
- "mailutils"
- "make"
- "nmap-ncat"
- "nginx"
- "nodejs"
- "pkgconfig"
- "python"
- "MySQL-python"
- "python-psycopg2"
- "python-pip"
- "python-pymongo"
- "python3-pymongo"
- "python-requests"
- "lz4-devel"
- "libuv-devel"
- "openssl-devel"
- "Judy-devel"
- "python3"
- "screen"
- "sudo"
- "sysstat"
- "tcpdump"
- "traceroute"
- "valgrind"
- "ulogd2"
- "unzip"
- "zip"
- "elfutils-libelf-devel"
-)
-
-dnf -y install ${package_tree_list1[@]}
-
-#for x in ${package_tree_list[@]};
-#do
-#  dnf install $x -y
-#done
+dnf -y install ${package_tree_default[@]}
+dnf -y install ${package_tree_rhel[@]}
