65a0ecfe8c1dd88a0c44841df34df972d36159eb
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2021-11-19 11:20:27 +0200

    Use sqlite3_exec, if it fails it will be retried on the next health log entries rotation

--- database/sqlite/sqlite_aclk_alert.c
+++ database/sqlite/sqlite_aclk_alert.c
@@ -870,9 +870,14 @@ void sql_aclk_alert_clean_dead_entries(RRDHOST *host)
 
     buffer_sprintf(sql,"delete from aclk_alert_%s where alert_unique_id not in "
                    " (select unique_id from health_log_%s); ", uuid_str, uuid_str);
-
-    db_execute(buffer_tostring(sql));
-
+    
+    char *err_msg = NULL;
+    int rc = sqlite3_exec(db_meta, buffer_tostring(sql), NULL, NULL, &err_msg);
+    if (rc != SQLITE_OK) {
+        error_report("Failed when trying to clean stale ACLK alert entries from aclk_alert_%s, error message \"%s""",
+                     uuid_str, err_msg);
+        sqlite3_free(err_msg);
+    }
     buffer_free(sql);
 #else
     UNUSED(host);
