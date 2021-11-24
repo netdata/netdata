aa903c4255f5c4f75d11de38889ba7adc4c11817
Marius Manea <locomotion.itservices@gmail.com>
2021-11-24 16:40:49 +0200

    add ubuntu dockerfile and update dependencies

---
+++ packaging/installer/dependencies/ubuntu.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+# Package tree used for installing netdata on distribution:
+# << Ubuntu >>
+# supported versions: 20.10
+
+set -e
+
+package_tree="
+  git
+  gcc
+  g++
+  make
+  automake
+  cmake
+  autoconf
+  autoconf-archive
+  autogen
+  libtool
+  pkg-config
+  tar
+  curl
+  gzip
+  netcat
+  zlib1g-dev
+  uuid-dev
+  libmnl-dev
+  libjson-c-dev
+  libuv1-dev
+  liblz4-dev
+  libssl-dev
+  libjudy-dev
+  libelf-dev
+  python3
+  "
+
+packages_to_install=
+
+for package in $package_tree; do
+  if dpkg -s $package &> /dev/null; then
+    echo "Package '${package}' is installed"
+  else
+    echo "Package '${package}' is NOT installed"
+    packages_to_install="$packages_to_install $package"
+  fi
+done
+
+if [[ -z "$packages_to_install" ]]; then
+  echo "All required packages are already installed. Skipping .."
+else
+  echo "packages_to_install: $packages_to_install"
+  apt-get install -y $packages_to_install
+fi
