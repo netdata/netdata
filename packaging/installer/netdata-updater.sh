7c4e2a27ae8a48b8533de2ef7773d9476f2c72a6
ilyam8 <ilya@netdata.cloud>
2021-12-09 00:41:48 +0300

    redirect netdata-latest.gz.run to 3

--- packaging/installer/netdata-updater.sh
+++ packaging/installer/netdata-updater.sh
@@ -452,7 +452,7 @@ if [ "${IS_NETDATA_STATIC_BINARY}" == "yes" ]; then
 
   # Do not pass any options other than the accept, for now
   # shellcheck disable=SC2086
-  if sh "${ndtmpdir}/netdata-latest.gz.run" --accept -- ${REINSTALL_OPTIONS}; then
+  if sh "${ndtmpdir}/netdata-latest.gz.run" --accept -- ${REINSTALL_OPTIONS} >&3 2>&3; then
     rm -r "${ndtmpdir}"
   else
     info "NOTE: did not remove: ${ndtmpdir}"
