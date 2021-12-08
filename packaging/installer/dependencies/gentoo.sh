e3e191ed6c839ce520bae49a280d5ea2325f1e4c
Marius Manea <locomotion.itservices@gmail.com>
2021-12-08 17:28:26 +0200

    add gentoo script

---
+++ packaging/installer/dependencies/gentoo.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+# Package tree used for installing netdata on distribution:
+# << Gentoo >>
+# supported versions: 20.10
+
+set -e
+
+package_tree="
+  dev-vcs/git
+  sys-apps/findutils
+  sys-devel/gcc
+  sys-devel/make
+  sys-devel/autoconf
+  sys-devel/autoconf-archive
+  sys-devel/autogen
+  sys-devel/automake
+  virtual/pkgconfig
+  dev-util/cmake
+  app-arch/tar
+  net-misc/curl
+  app-arch/gzip
+  net-analyzer/netcat
+  sys-apps/util-linux
+  net-libs/libmnl
+  dev-libs/json-c
+  dev-libs/libuv
+  app-arch/lz4
+  dev-libs/openssl
+  dev-libs/judy
+  virtual/libelf
+  dev-lang/python
+  libuv
+  "
+  #libtoolize
+  #libz-dev
+
+packages_to_install=
+
+for package in $package_tree; do
+  if qlist -IRv $package &> /dev/null; then
+    echo "Package '${package}' is installed"
+  else
+    echo "Package '${package}' is NOT installed"
+    packages_to_install="$packages_to_install $package"
+  fi
+done
+
+if [[ -z "$packages_to_install" ]]; then
+  echo "All required packages are already installed. Skipping .."
+else
+  echo "packages_to_install: $packages_to_install"
+  emerge $packages_to_install
+fi
+
