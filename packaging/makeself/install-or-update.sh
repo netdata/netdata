ac05734cd8f7a930b4c6e42d9002abf38d4718e1
boxjan <i@boxjan.li>
2021-12-08 22:49:09 +0800

    fix permissions of plugins that may be built

--- packaging/makeself/install-or-update.sh
+++ packaging/makeself/install-or-update.sh
@@ -214,7 +214,7 @@ run chown -R ${NETDATA_USER}:${NETDATA_GROUP} /opt/netdata
 
 progress "fix plugin permissions"
 
-for x in apps.plugin freeipmi.plugin ioping cgroup-network ebpf.plugin; do
+for x in apps.plugin freeipmi.plugin ioping cgroup-network ebpf.plugin perf.plugin slabinfo.plugin freeipmi.plugin nfacct.plugin xenstat.plugin; do
   f="usr/libexec/netdata/plugins.d/${x}"
 
   if [ -f "${f}" ]; then
