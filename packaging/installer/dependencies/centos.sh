8657074d6bab0e346c38e710407355ce1e2215cb
Marius Manea <marius@netdata.com>
2021-12-16 19:27:55 +0200

    modify validate_tree for centos

--- packaging/installer/dependencies/centos.sh
+++ packaging/installer/dependencies/centos.sh
@@ -3,7 +3,14 @@
 # << CentOS >>
 # supported versions: 7,8
 
+source "../functions.sh"
+
 set -e
+
+NON_INTERACTIVE=0
+DONT_WAIT=0
+
+check_flags ${@}
 
 function os_version {
   if [[ -f /etc/os-release ]]; then
@@ -41,6 +48,50 @@ declare -a package_tree=(
   gzip
 )
 
+validate_tree_centos() {
+  local opts=
+  if [ "${NON_INTERACTIVE}" -eq 1 ]; then
+    echo >&2 "Running in non-interactive mode"
+    opts="-y"
+  fi
+
+  echo >&2 " > CentOS Version: $(os_version) ..."
+
+  if [[ $(os_version) =~ ^8(\..*)?$ ]]; then
+    package_manager=dnf
+    echo >&2 " > Checking for config-manager ..."
+    if ! dnf config-manager; then
+      if prompt "config-manager not found, shall I install it?"; then
+        dnf ${opts} install 'dnf-command(config-manager)'
+      fi
+    fi
+
+    echo >&2 " > Checking for PowerTools ..."
+    if ! dnf repolist | grep PowerTools; then
+      if prompt "PowerTools not found, shall I install it?"; then
+        dnf ${opts} config-manager --set-enabled powertools || enable_powertools_repo
+      fi
+    fi
+
+    echo >&2 " > Updating libarchive ..."
+    dnf ${opts} install libarchive
+
+    echo >&2 " > Installing Judy-devel directly ..."
+    dnf ${opts} install http://mirror.centos.org/centos/8/PowerTools/x86_64/os/Packages/Judy-devel-1.0.5-18.module_el8.3.0+757+d382997d.x86_64.rpm
+    dnf makecache --refresh
+
+  elif [[ $(os_version) =~ ^7(\..*)?$ ]]; then
+    package_manager=yum
+    echo >&2 " > Checking for EPEL ..."
+    if ! rpm -qa | grep epel-release > /dev/null; then
+      if prompt "EPEL not found, shall I install it?"; then
+        yum ${opts} install epel-release
+      fi
+    fi
+    yum makecache
+  fi
+}
+
 function enable_powertools_repo {
   if ! dnf repolist | grep -q powertools; then
     cat > /etc/yum.repos.d/powertools.repo <<-EOF
@@ -58,35 +109,7 @@ EOF
   fi
 }
 
-function enable_epel_release {
-  if ! rpm -qa | grep epel-release > /dev/null; then
-    echo "EPEL not found, installing it.."
-    yum install -y epel-release
-  else
-    echo "EPEL is installed"
-  fi
-}
-
-function check_plugins_core {
-  if rpm -q dnf-plugins-core; then
-    echo "Package dnf-plugins-core is INSTALLED"
-  else
-    echo "Installing 'dnf-command(config-manager)' ..."
-    dnf install -y 'dnf-command(config-manager)'
-  fi
-}
-
-if [[ $(os_version) -eq 8 ]]; then
-  package_manager=dnf
-  check_plugins_core
-  dnf config-manager --set-enabled powertools || enable_repo
-  dnf makecache --refresh
-else
- package_manager=yum
- enable_epel_release
- yum makecache
-
-fi
+validate_tree_centos
 
 packages_to_install=
 
@@ -103,6 +126,11 @@ if [[ -z $packages_to_install ]]; then
   echo "All required packages are already installed. Skipping .."
 else
   echo "packages_to_install: ${packages_to_install[@]}"
-  ${package_manager} install -y ${packages_to_install[@]}
+  opts=
+  if [ "${NON_INTERACTIVE}" -eq 1 ]; then
+    echo >&2 "Running in non-interactive mode"
+    opts="-y"
+  fi
+  ${package_manager} install ${opts} ${packages_to_install[@]}
 fi
 
