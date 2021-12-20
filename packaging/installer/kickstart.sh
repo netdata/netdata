5b711294a07868ad44a29a81d3d4d2383c7280c2
ilyam8 <ilya@netdata.cloud>
2021-12-20 17:17:38 +0300

    fix printing arguments in run_failed

--- packaging/installer/kickstart.sh
+++ packaging/installer/kickstart.sh
@@ -176,7 +176,7 @@ run_ok() {
 }
 
 run_failed() {
-  printf >&2 "${TPUT_BGRED}${TPUT_WHITE}${TPUT_BOLD} FAILED ${TPUT_RESET} \n\n"
+  printf >&2 "${TPUT_BGRED}${TPUT_WHITE}${TPUT_BOLD} FAILED ${TPUT_RESET} ${1} \n\n"
 }
 
 ESCAPED_PRINT_METHOD=
