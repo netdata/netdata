30458ace671a6da5ccd27ceb89fb4ca0707eae35
Erdem Ergen <erdem@netdata.com>
2022-01-20 14:58:25 +0300

    [Library] queue - initial queue library commit

---
+++ libnetdata/queue/README.md
@@ -0,0 +1,14 @@
+<!--
+title: "Queue"
+custom_edit_url: https://github.com/netdata/netdata/edit/master/libnetdata/queue/README.md
+-->
+
+# Queue
+
+Queue is a thread-safe library to hanle queue processes with independent objects.
+
+Library includes functions below:
+`Queue* initqueue(int max)`
+`void freequeue(Queue* q)`
+`void enqueue(void *q, void* item)`	
+`void* dequeue(void *q)`	
---
+++ libnetdata/queue/queue.h
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#ifndef NETDATA_QUEUE_H
+#define NETDATA_QUEUE_H 
+
+#include "../libnetdata.h"
+		
+typedef struct nodes				
+{
+	void *item;
+	struct nodes *next;			
+} node;
+
+typedef struct Queues			
+{		
+    pthread_cond_t conde;
+    pthread_cond_t condf;
+    pthread_mutex_t lock;
+	node *front;			
+	node *rear;
+	int max;
+	int count;
+} Queue;
+
+Queue* initqueue(int max);
+void freequeue(Queue* q);
+void enqueue(void *q, void* item);	
+void* dequeue(void *q);				
+
+#endif //NETDATA_QUEUE_H
