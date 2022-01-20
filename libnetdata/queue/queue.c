751eec41bd778f3ad9307a5a669b1c35afbaee5d
Erdem Ergen <erdem@netdata.com>
2022-01-20 19:01:44 +0300

    [Library] queue - structs are converted to opaque pointers

--- libnetdata/queue/queue.c
+++ libnetdata/queue/queue.c
@@ -2,67 +2,82 @@
 
 #include "../libnetdata.h"
 
-extern void enqueue(void *qp, void *i)	
+struct nodes{
+	void *item;
+	struct nodes *next;			
+};
+
+struct queues{		
+	pthread_cond_t conde;
+	pthread_cond_t condf;
+	pthread_mutex_t lock;
+	node front;			
+	node rear;
+	int max;
+	int count;
+};
+
+void enqueue(queue q, void* i)	
 {		
-    Queue *q = qp;								
-    node *temp;
-    temp = (node *) malloc(sizeof(node));
-
-    pthread_mutex_lock(&q->lock);
-    while(q->count == q->max){
-        pthread_cond_wait(&q->condf, &q->lock);
-    }
-    temp->item = i;
-    temp->next = NULL;
-    if(q->count != 0)		
-    {		
-        q->rear->next = (struct nodes*)temp;
-    }
-    else{
-        q->front = temp;
-    }
-    q->rear = temp;
-    q->count++; 
-    pthread_cond_signal(&q->conde);
-    pthread_mutex_unlock(&q->lock);
+	//queue q = qp;								
+	node temp;
+	temp = (node) malloc(sizeof(struct nodes));
+	pthread_mutex_lock(&q->lock);
+	while(q->count == q->max){
+		pthread_cond_wait(&q->condf, &q->lock);
+	}
+	temp->item = i;
+	temp->next = NULL;
+	if(q->count != 0)		
+	{		
+		q->rear->next = (node)temp;
+	}
+	else{
+		q->front = temp;
+	}
+	q->rear = temp;
+    	q->count++; 
+	pthread_cond_signal(&q->conde);
+	pthread_mutex_unlock(&q->lock);
 }
 
-extern void* dequeue(void *qp)			
+void* dequeue(queue q)			
 {			
-    Queue *q = qp;							
-    int *temp = NULL;					
-    node *temp2;	
-    pthread_mutex_lock(&q->lock);
-    while(q->count == 0)
-    {
-        pthread_cond_wait(&q->conde, &q->lock);
-    }
-    temp = q->front->item;			
-    temp2 = q->front;				
-    q->front = q->front->next;		
-    free(temp2);					
-    if(q->front == NULL){ 
-        q->rear=NULL;
-    } 
-    q->count--;
-    pthread_cond_signal(&q->condf);
-    pthread_mutex_unlock(&q->lock);
-    return temp;						
+	//queue q = qp;							
+	void* temp = NULL;					
+	node temp2;	
+	pthread_mutex_lock(&q->lock);
+	while(q->count == 0)
+	{
+		pthread_cond_wait(&q->conde, &q->lock);
+	}
+	
+	temp = q->front->item;			
+	temp2 = q->front;				
+	q->front = q->front->next;		
+	free(temp2);					
+	if(q->front==NULL){
+		q->rear=NULL;
+	} 
+	q->count--;
+	pthread_cond_signal(&q->condf);
+	pthread_mutex_unlock(&q->lock);
+	return temp;						
 }
 
-extern Queue* initqueue(int max){
-    Queue *q;
-    q = (Queue *) malloc(sizeof(Queue));	
-    q->front = NULL;			
-    q->rear = NULL;
-    q->max = max;
-    q->count = 0;
-    q->conde = (pthread_cond_t)PTHREAD_COND_INITIALIZER;
-    q->condf = (pthread_cond_t)PTHREAD_COND_INITIALIZER;
-    q->lock = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
-    return q;
+queue initqueue(int max){
+	queue q;
+	q = (queue) malloc(sizeof(struct queues));	
+	q->front = NULL;			
+	q->rear = NULL;
+	q->max = max;
+	q->count = 0;
+	q->conde = (pthread_cond_t)PTHREAD_COND_INITIALIZER;
+	q->condf = (pthread_cond_t)PTHREAD_COND_INITIALIZER;
+	q->lock = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
+	return q;
 }
 
-extern void freequeue(Queue* q){
-    free(q);
+void freequeue(queue q){
+	free(q);
 }
--- libnetdata/queue/queue.h
+++ libnetdata/queue/queue.h
@@ -5,26 +5,12 @@
 
 #include "../libnetdata.h"
 		
-typedef struct nodes				
-{
-    void *item;
-    struct nodes *next;			
-} node;
+typedef struct nodes *node;
+typedef struct queues *queue;
 
-typedef struct Queues			
-{		
-    pthread_cond_t conde;
-    pthread_cond_t condf;
-    pthread_mutex_t lock;
-    node *front;			
-    node *rear;
-    int max;
-    int count;
-} Queue;
-
-Queue* initqueue(int max);
-void freequeue(Queue* q);
-void enqueue(void *q, void* item);	
-void* dequeue(void *q);				
+queue initqueue(int max);
+void freequeue(queue q);
+void enqueue(queue, void*);	
+void* dequeue(queue);				
 
 #endif //NETDATA_QUEUE_H
