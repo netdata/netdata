2d9735ef91e58e90039df42dd2a3babfb087eb8d
Timotej Šiškovič <timotej@netdata.cloud>
2022-02-11 10:50:21 +0100

    OCD commit - center  URL

--- aclk/aclk.c
+++ aclk/aclk.c
@@ -821,7 +821,7 @@ void *aclk_main(void *ptr)
         error_report("#      no protobuf protocol support (uses legacy JSON protocol)      #");
         error_report("#  Legacy protocol will be deprecated soon (planned 1st March 2022)  #");
         error_report("#  Visit following link for more info and instructions how to solve  #");
-        error_report("#  https://www.netdata.cloud/blog/netdata-clouds-new-architecture/   #");
+        error_report("#   https://www.netdata.cloud/blog/netdata-clouds-new-architecture   #");
         error_report("######################################################################");
 #endif
 
