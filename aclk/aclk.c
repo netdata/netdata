67a2c6827763c82d0c3e6fc95be5642d1bb75e45
Timotej Šiškovič <timotej@netdata.cloud>
2022-02-11 10:45:16 +0100

    update with date and URL

--- aclk/aclk.c
+++ aclk/aclk.c
@@ -816,12 +816,13 @@ void *aclk_main(void *ptr)
             goto exit_full;
 
 #if defined(ENABLE_ACLK) && !defined(ENABLE_NEW_CLOUD_PROTOCOL)
-        error_report("#########################  WARNING  #########################");
-        error_report("#   Your agent is configured to connect to cloud but has    #");
-        error_report("# no protobuf protocol support (uses legacy JSON protocol). #");
-        error_report("#         Legacy protocol will be deprecated soon.          #");
-        error_report("#  Please update your agent to avoid service interruption.  #");
-        error_report("#############################################################");
+        error_report("############################  WARNING  ###############################");
+        error_report("#       Your agent is configured to connect to cloud but has         #");
+        error_report("#      no protobuf protocol support (uses legacy JSON protocol)      #");
+        error_report("#  Legacy protocol will be deprecated soon (planned 1st March 2022)  #");
+        error_report("#  Visit following link for more info and instructions how to solve  #");
+        error_report("#  https://www.netdata.cloud/blog/netdata-clouds-new-architecture/   #");
+        error_report("######################################################################");
 #endif
 
         // warning this assumes the popcorning is relative short (3s)
