4e896c624db4f20a754a156d7fee7b4afa58d8cd
ilyam8 <ilya@netdata.cloud>
2022-03-12 22:41:10 +0200

    fix exiting on error

--- packaging/docker/run.sh
+++ packaging/docker/run.sh
@@ -21,8 +21,8 @@ if [ ! "${DISABLE_TELEMETRY:-0}" -eq 0 ] ||
   touch /etc/netdata/.opt-out-from-anonymous-statistics
 fi
 
-BALENA_PGID=$(stat -c %g /var/run/balena.sock 2>/dev/null)
-DOCKER_PGID=$(stat -c %g /var/run/docker.sock 2>/dev/null)
+BALENA_PGID=$(stat -c %g /var/run/balena.sock 2>/dev/null | awk '{print $1}')
+DOCKER_PGID=$(stat -c %g /var/run/docker.sock 2>/dev/null | awk '{print $1}')
 
 re='^[0-9]+$'
 if [[ $BALENA_PGID =~ $re ]]; then
