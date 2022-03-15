cc09af87fe16d09e580acd83c2ce0b7c7f557e37
Marius Manea <locomotion.itservices@gmail.com>
2022-03-15 10:48:07 +0200

    add --reinstall-clean flag for kickstart and fix uninstaller

--- packaging/installer/netdata-uninstaller.sh
+++ packaging/installer/netdata-uninstaller.sh
@@ -83,7 +83,7 @@ user_input() {
   fi
 }
 
-if [ -x "$(command -v apt-get)" ]; then
+if [ -x "$(command -v apt-get)" ] && echo "${INSTALL_TYPE}" | grep -q "binpkg-*"; then
   if dpkg -s netdata > /dev/null; then
     echo "Found netdata native installation"
     if user_input "Do you want to remove netdata? "; then
@@ -101,7 +101,7 @@ if [ -x "$(command -v apt-get)" ]; then
     fi
     exit 0
   fi
-elif [ -x "$(command -v dnf)" ]; then
+elif [ -x "$(command -v dnf)" ] && echo "${INSTALL_TYPE}" | grep -q "binpkg-*"; then
   if rpm -q netdata > /dev/null; then
     echo "Found netdata native installation."
     if user_input "Do you want to remove netdata? "; then
@@ -119,7 +119,7 @@ elif [ -x "$(command -v dnf)" ]; then
     fi
     exit 0
   fi
-elif [ -x "$(command -v yum)" ]; then
+elif [ -x "$(command -v yum)" ] && echo "${INSTALL_TYPE}" | grep -q "binpkg-*"; then
   if rpm -q netdata > /dev/null; then
     echo "Found netdata native installation."
     if user_input "Do you want to remove netdata? "; then
@@ -137,7 +137,7 @@ elif [ -x "$(command -v yum)" ]; then
     fi
     exit 0
   fi
-elif [ -x "$(command -v zypper)" ]; then
+elif [ -x "$(command -v zypper)" ] && echo "${INSTALL_TYPE}" | grep -q "binpkg-*"; then
   if [ "${FLAG}" = "-y" ]; then
     FLAG=-n
   fi
