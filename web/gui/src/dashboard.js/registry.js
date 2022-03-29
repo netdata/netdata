b4c6cd64e57e8532dc292a3b22d4743344ac7014
Wesley <xrgman@gmail.com>
2022-03-29 07:18:37 +0000

    Fix wrapping issue registry.js

--- web/gui/src/dashboard.js/registry.js
+++ web/gui/src/dashboard.js/registry.js
@@ -15,7 +15,7 @@ NETDATA.registry = {
     MASKED_DATA: "***",
 
     isUsingGlobalRegistry: function() {
-        return NETDATA.registry.server == "https://registry.my-netdata.io";
+        return NETDATA.registry.server === "https://registry.my-netdata.io";
     },
 
     isRegistryEnabled: function() {
@@ -87,11 +87,14 @@ NETDATA.registry = {
                 if (!NETDATA.registry.anonymous_statistics_checked) {
                     NETDATA.registry.anonymous_statistics_checked=true;
                     if (data.anonymous_statistics) {
-                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=false;j.src=
-                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-N6CBMJD');
+                        (function(w,d,s,l,i){
+                            w[l]=w[l]||[];
+                            w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                            var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!=='dataLayer'?'&l='+l:'';
+                            j.async=false;
+                            j.src= 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                            f.parentNode.insertBefore(j,f);
+                        }(window, document, 'script', 'dataLayer', 'GTM-N6CBMJD'));
                         dataLayer.push({"anonymous_statistics" : "true", "machine_guid" : data.machine_guid});
                     }
                 }
