40b70b038bd1ff5a160ac42f8e081064bf7e99d4
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2022-03-30 20:39:50 +0300

    Add the ability to execute queries from the cloud for archived hosts by node id

--- aclk/aclk_query.c
+++ aclk/aclk_query.c
@@ -111,6 +111,7 @@ static int http_api_v2(struct aclk_query_thread *query_thr, aclk_query_t query)
     w->tv_in = query->created_tv;
     now_realtime_timeval(&w->tv_ready);
 
+    RRDHOST *temp_host = NULL;
     if (!strncmp(query->data.http_api_v2.query, NODE_ID_QUERY, strlen(NODE_ID_QUERY))) {
         char *node_uuid = query->data.http_api_v2.query + strlen(NODE_ID_QUERY);
         char nodeid[UUID_STR_LEN];
@@ -125,11 +126,14 @@ static int http_api_v2(struct aclk_query_thread *query_thr, aclk_query_t query)
 
         query_host = node_id_2_rrdhost(nodeid);
         if (!query_host) {
-            error_report("Host with node_id \"%s\" not found! Returning 404 to Cloud!", nodeid);
-            retval = 1;
-            w->response.code = 404;
-            aclk_http_msg_v2_err(query_thr->client, query->callback_topic, query->msg_id, w->response.code, CLOUD_EC_NODE_NOT_FOUND, CLOUD_EMSG_NODE_NOT_FOUND, NULL, 0);
-            goto cleanup;
+            temp_host = sql_create_host_by_uuid(nodeid);
+            if (!temp_host) {
+                error_report("Host with node_id \"%s\" not found! Returning 404 to Cloud!", nodeid);
+                retval = 1;
+                w->response.code = 404;
+                aclk_http_msg_v2_err(query_thr->client, query->callback_topic, query->msg_id, w->response.code, CLOUD_EC_NODE_NOT_FOUND, CLOUD_EMSG_NODE_NOT_FOUND, NULL, 0);
+                goto cleanup;
+            }
         }
     }
 
@@ -150,7 +154,18 @@ static int http_api_v2(struct aclk_query_thread *query_thr, aclk_query_t query)
     }
 
     // execute the query
-    t = aclk_web_api_v1_request(query_host, w, mysep ? mysep + 1 : "noop");
+    t = aclk_web_api_v1_request(query_host ? query_host : temp_host, w, mysep ? mysep + 1 : "noop");
+    if (unlikely(temp_host)) {
+        freez(temp_host->hostname);
+        freez((char *)temp_host->os);
+        freez((char *)temp_host->tags);
+        freez((char *)temp_host->timezone);
+        freez(temp_host->program_name);
+        freez(temp_host->program_version);
+        freez(temp_host->registry_hostname);
+        freez(temp_host->system_info);
+        freez(temp_host);
+    }
     size = (w->mode == WEB_CLIENT_MODE_FILECOPY) ? w->response.rlen : w->response.data->len;
     sent = size;
 
