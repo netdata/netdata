f740da36b16d6660ad42d4f5523a86765ec32baf
Emmanuel Vasilakis <mrzammler@mm.st>
2022-04-18 11:28:07 +0300

    reduce timeout to 2 min

--- streaming/receiver.c
+++ streaming/receiver.c
@@ -610,7 +610,7 @@ static int rrdpush_receive(struct receiver_state *rpt)
         error("STREAM %s [receive from [%s]:%s]: cannot remove the non-blocking flag from socket %d", rpt->host->hostname, rpt->client_ip, rpt->client_port, rpt->fd);
 
     struct timeval timeout;
-    timeout.tv_sec = 600;
+    timeout.tv_sec = 120;
     timeout.tv_usec = 0;
     setsockopt (rpt->fd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof timeout);
 
