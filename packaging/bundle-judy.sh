netdata/netdata

commit 3f97fe0355dd809939fb5a73fe1e76b86306908f

Disable automake dependency tracking in our various one-time builds. (#12701)
* Disable automake dependency tracking in our various one-time builds.

* Also disable dependency tracking code in package builds.

.github/workflows/checks.yml
@@ -27,7 +27,7 @@ jobs:
           apk del openssl openssl-dev;
           apk add libressl libressl-dev;
           autoreconf -ivf;
-          ./configure;
+          ./configure --disable-dependency-trackiong;
           make;'
   clang-checks:
     name: Clang
@@ -51,7 +51,7 @@ jobs:
       - name: Prepare environment
         run: ./packaging/installer/install-required-packages.sh --dont-wait --non-interactive netdata
       - name: Build netdata
-        run: ./netdata-installer.sh --dont-start-it --disable-telemetry --dont-wait --install /tmp/install
+        run: ./netdata-installer.sh --dont-start-it --disable-telemetry --dont-wait --install /tmp/install --one-time-build
       - name: Check that repo is clean
         run: |
           git status --porcelain=v1 > /tmp/porcelain

packaging/bundle-judy.sh
@@ -14,7 +14,7 @@ aclocal || exit 1
 autoheader || exit 1
 automake --add-missing --force --copy --include-deps || exit 1
 autoconf || exit 1
-./configure || exit 1
+./configure --disable-dependncy-tracking || exit 1
 make -C src || exit 1
 ar -r src/libJudy.a src/Judy*/*.o || exit 1
 cd "${OLDPWD}" || exit 1

.github/scripts/build-artifacts.sh
@@ -33,6 +33,7 @@ build_dist() {
       --with-zlib \
       --with-math \
       --with-user=netdata \
+      --disable-dependency-tracking \
       CFLAGS=-O2
     make dist
     mv "${BASENAME}.tar.gz" artifacts/

.github/scripts/build-dist.sh
@@ -32,6 +32,7 @@ build_dist() {
       --with-zlib \
       --with-math \
       --with-user=netdata \
+      --disable-dependency-tracking \
       CFLAGS=-O2
     make dist
     mv "${BASENAME}.tar.gz" artifacts/

.github/scripts/run_install_with_dist_file.sh
@@ -33,7 +33,7 @@ docker run \
   -v "${PWD}:/netdata" \
   -w /netdata \
   "ubuntu:latest" \
-  /bin/bash -c "./install-required-packages.sh --dont-wait --non-interactive netdata && apt install wget && ./netdata-installer.sh --dont-wait --require-cloud --disable-telemetry --install /tmp && echo \"Validating netdata instance is running\" && wget -O - 'http://127.0.0.1:19999/api/v1/info' | grep version"
+  /bin/bash -c "./install-required-packages.sh --dont-wait --non-interactive netdata && apt install wget && ./netdata-installer.sh --dont-wait --require-cloud --disable-telemetry --install /tmp --one-time-build && echo \"Validating netdata instance is running\" && wget -O - 'http://127.0.0.1:19999/api/v1/info' | grep version"
 popd || exit 1
 
 echo "All Done!"

.github/workflows/build.yml
@@ -321,23 +321,23 @@ jobs:
         id: build-basic
         run: |
           docker run --security-opt seccomp=unconfined -w /netdata sha256:${{ steps.load.outputs.image }} \
-              /bin/sh -c 'autoreconf -ivf && ./configure && make -j2'
+              /bin/sh -c 'autoreconf -ivf && ./configure --disable-dependency-tracking && make -j2'
       - name: netdata-installer on ${{ matrix.distro }}, disable cloud
         id: build-no-cloud
         run: |
           docker run --security-opt seccomp=unconfined -w /netdata sha256:${{ steps.load.outputs.image }} \
-              /bin/sh -c './netdata-installer.sh --dont-wait --dont-start-it --disable-cloud'
+              /bin/sh -c './netdata-installer.sh --dont-wait --dont-start-it --disable-cloud --one-time-build'
       - name: netdata-installer on ${{ matrix.distro }}, require cloud
         id: build-cloud
         run: |
           docker run --security-opt seccomp=unconfined -w /netdata sha256:${{ steps.load.outputs.image }} \
-              /bin/sh -c './netdata-installer.sh --dont-wait --dont-start-it --require-cloud'
+              /bin/sh -c './netdata-installer.sh --dont-wait --dont-start-it --require-cloud --one-time-build'
       - name: netdata-installer on ${{ matrix.distro }}, require cloud, no JSON-C
         id: build-no-jsonc
         if: matrix.jsonc_removal != ''
         run: |
           docker run --security-opt seccomp=unconfined -w /netdata sha256:${{ steps.load.outputs.image }} \
-              /bin/sh -c '/rmjsonc.sh && ./netdata-installer.sh --dont-wait --dont-start-it --require-cloud'
+              /bin/sh -c '/rmjsonc.sh && ./netdata-installer.sh --dont-wait --dont-start-it --require-cloud --one-time-build'
       - name: Failure Notification
         uses: rtCamp/action-slack-notify@v2
         env:

.github/workflows/tests.yml
@@ -57,7 +57,7 @@ jobs:
       - name: Configure
         run: |
           autoreconf -ivf
-          ./configure --disable-ml
+          ./configure --disable-ml --disable-dependency-tracking
       # XXX: Work-around for bug with libbson-1.0 in Ubuntu 18.04
       # See: https://bugs.launchpad.net/ubuntu/+source/libmongoc/+bug/1790771
       #      https://jira.mongodb.org/browse/CDRIVER-2818

contrib/debian/rules
@@ -41,7 +41,7 @@ override_dh_auto_configure:
 	autoreconf -ivf
 	dh_auto_configure -- --prefix=/usr --sysconfdir=/etc --localstatedir=/var --libdir=/usr/lib \
 	--libexecdir=/usr/libexec --with-user=netdata --with-math --with-zlib --with-webdir=/var/lib/netdata/www \
-	$(EBPF_CONFIG)
+	--disable-dependency-tracking $(EBPF_CONFIG)
 
 override_dh_install:
 	cp -v $(BASE_CONFIG) debian/netdata.conf

commit aa68b0a5a90a5d00ff8a6219740be94642891a5a

Fix typos introduced by #12701.

.github/workflows/checks.yml
@@ -27,7 +27,7 @@ jobs:
           apk del openssl openssl-dev;
           apk add libressl libressl-dev;
           autoreconf -ivf;
-          ./configure --disable-dependency-trackiong;
+          ./configure --disable-dependency-tracking;
           make;'
   clang-checks:
     name: Clang

packaging/bundle-judy.sh
@@ -14,7 +14,7 @@ aclocal || exit 1
 autoheader || exit 1
 automake --add-missing --force --copy --include-deps || exit 1
 autoconf || exit 1
-./configure --disable-dependncy-tracking || exit 1
+./configure --disable-dependency-tracking || exit 1
 make -C src || exit 1
 ar -r src/libJudy.a src/Judy*/*.o || exit 1
 cd "${OLDPWD}" || exit 1