944137f850eaf2a3ba7026a78da1649922c49515
Austin S. Hemmelgarn <austin@netdata.cloud>
2022-05-03 09:19:26 -0400

    Add support to `download` function for `file://` URLs.
    This will be used to reference local files instead of having to set up a
    web server or complicate the code for fetching files.

--- packaging/installer/kickstart.sh
+++ packaging/installer/kickstart.sh
@@ -495,7 +495,10 @@ check_for_remote_file() {
 download() {
   url="${1}"
   dest="${2}"
-  if command -v curl > /dev/null 2>&1; then
+
+  if echo "${url}" | grep -Eq "^file:///"; then
+    run cp "${url%file://}" "${dest}" || return 1
+  elif command -v curl > /dev/null 2>&1; then
     run curl --fail -q -sSL --connect-timeout 10 --retry 3 --output "${dest}" "${url}" || return 1
   elif command -v wget > /dev/null 2>&1; then
     run wget -T 15 -O "${dest}" "${url}" || return 1
