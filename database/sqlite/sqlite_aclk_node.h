ecdfdf7f64cd9a0e7db63a6e4bd6279306a5d841
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2022-05-19 17:37:52 +0300

    Move aclk_update_retention to the proper header file

--- database/sqlite/sqlite_aclk_node.h
+++ database/sqlite/sqlite_aclk_node.h
@@ -4,5 +4,4 @@
 #define NETDATA_SQLITE_ACLK_NODE_H
 
 void sql_build_node_info(struct aclk_database_worker_config *wc, struct aclk_database_cmd cmd);
-void aclk_update_retention(struct aclk_database_worker_config *wc, struct aclk_database_cmd cmd);
 #endif //NETDATA_SQLITE_ACLK_NODE_H
