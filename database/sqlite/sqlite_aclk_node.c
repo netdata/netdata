013980c75d4b6115354d3f556562f1311eb07c22
Emmanuel Vasilakis <mrzammler@mm.st>
2022-05-26 13:39:32 +0300

    send collectors to cloud via node_info

--- database/sqlite/sqlite_aclk_node.c
+++ database/sqlite/sqlite_aclk_node.c
@@ -7,6 +7,66 @@
 #include "../../aclk/aclk_charts_api.h"
 #endif
 
+struct collector {
+    char *plugin;
+    char *module;
+};
+
+struct array_printer {
+    int c;
+    char **collectors;
+};
+
+int store_collectors(void *entry, void *data) {
+    char name[500];
+    struct collector *col=(struct collector *) entry;
+    struct array_printer *ap = (struct array_printer *)data;
+    ap->collectors = reallocz(ap->collectors, sizeof(ap->collectors) * (ap->c + 1));
+    snprintfz(name, 499, "%s:%s", col->plugin ? col->plugin : "", col->module ? col->module : "");
+    ap->collectors[ap->c] = strdupz(name);
+    (ap->c)++;
+    return 0;
+}
+
+char **collectors_from_charts(RRDHOST *host, size_t *count) {
+    DICTIONARY *dict = dictionary_create(DICTIONARY_FLAG_SINGLE_THREADED);
+    RRDSET *st;
+    char name[500];
+
+    time_t now = now_realtime_sec();
+    rrdhost_rdlock(host);
+    rrdset_foreach_read(st, host) {
+        if (rrdset_is_available_for_viewers(st)) {
+            struct collector col = {
+                    .plugin = st->plugin_name ? st->plugin_name : "",
+                    .module = st->module_name ? st->module_name : ""
+            };
+            snprintfz(name, 499, "%s:%s", col.plugin, col.module);
+            dictionary_set(dict, name, &col, sizeof(struct collector));
+            st->last_accessed_time = now;
+        }
+    }
+    rrdhost_unlock(host);
+
+    struct array_printer ap;
+    ap.c = 0;
+    ap.collectors = NULL;
+
+    dictionary_get_all(dict, store_collectors, &ap);
+    dictionary_destroy(dict);
+
+    *count = ap.c;
+    return ap.collectors;
+}
+
+void free_collectors_from_charts(char **collectors, size_t count)
+{
+    for (size_t i=0;i<count;i++)
+        freez(collectors[i]);
+
+    freez(collectors);
+}
+
 void sql_build_node_info(struct aclk_database_worker_config *wc, struct aclk_database_cmd cmd)
 {
     UNUSED(cmd);
@@ -60,8 +120,7 @@ void sql_build_node_info(struct aclk_database_worker_config *wc, struct aclk_dat
     node_info.data.virtualization_type = host->system_info->virtualization ? host->system_info->virtualization : "unknown";
     node_info.data.container_type = host->system_info->container ? host->system_info->container : "unknown";
     node_info.data.custom_info = config_get(CONFIG_SECTION_WEB, "custom dashboard_info.js", "");
-    node_info.data.collectors = NULL;   // char **
-    node_info.data.collector_count = 0;
+    node_info.data.collectors = collectors_from_charts(host, &node_info.data.collector_count);
     node_info.data.machine_guid = wc->host_guid;
 
     struct capability node_caps[] = {
@@ -84,6 +143,7 @@ void sql_build_node_info(struct aclk_database_worker_config *wc, struct aclk_dat
     rrd_unlock();
     freez(node_info.claim_id);
     freez(host_version);
+    free_collectors_from_charts(node_info.data.collectors, node_info.data.collector_count);
 #else
     UNUSED(wc);
 #endif
