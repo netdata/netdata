42601d75a304721c4556924a8d4cb95376a11bc5
Vladimir Kobal <vlad@prokk.net>
2022-06-07 19:49:26 +0200

    Fix exporting unit tests

--- exporting/opentsdb/opentsdb.c
+++ exporting/opentsdb/opentsdb.c
@@ -154,7 +154,7 @@ int format_host_labels_opentsdb_telnet(struct instance *instance, RRDHOST *host)
         return 0;
 
     buffer_strcat(instance->labels_buffer, " ");
-    rrdlabels_to_buffer(host->host_labels, instance->labels_buffer, "", "=", "\"", " ",
+    rrdlabels_to_buffer(host->host_labels, instance->labels_buffer, "", "=", "", " ",
                         exporting_labels_filter_callback, instance,
                         NULL, sanitize_opentsdb_label_value);
     return 0;
--- exporting/tests/test_exporting_engine.c
+++ exporting/tests/test_exporting_engine.c
@@ -381,7 +381,7 @@ static void test_prepare_buffers(void **state)
     expect_value(__mock_end_batch_formatting, instance, instance);
     will_return(__mock_end_batch_formatting, 0);
 
-    assert_int_equal(__real_prepare_buffers(engine), 0);
+    __real_prepare_buffers(engine);
 
     assert_int_equal(instance->stats.buffered_metrics, 1);
 
@@ -393,7 +393,7 @@ static void test_prepare_buffers(void **state)
     instance->end_chart_formatting = NULL;
     instance->end_host_formatting = NULL;
     instance->end_batch_formatting = NULL;
-    assert_int_equal(__real_prepare_buffers(engine), 0);
+    __real_prepare_buffers(engine);
 
     assert_int_equal(instance->scheduled, 0);
     assert_int_equal(instance->after, 2);
