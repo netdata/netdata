b925387c0f3064196e3c556a892966aba79ec1b0
Timo <timotej@netdata.cloud>
2022-06-09 18:46:50 +0200

    remove aclk_use_new_cloud_arch from web_api_v1

--- web/api/web_api_v1.c
+++ web/api/web_api_v1.c
@@ -3,7 +3,6 @@
 #include "web_api_v1.h"
 
 char *api_secret;
-extern int aclk_use_new_cloud_arch;
 
 static struct {
     const char *name;
@@ -1081,12 +1080,7 @@ inline int web_client_api_request_v1_info_fill_buffer(RRDHOST *host, BUFFER *wb)
 #ifdef ENABLE_ACLK
     if (aclk_connected) {
         buffer_strcat(wb, "\t\"aclk-available\": true,\n");
-#ifdef ENABLE_NEW_CLOUD_PROTOCOL
-        if (aclk_use_new_cloud_arch)
-            buffer_strcat(wb, "\t\"aclk-available-protocol\": \"New\",\n");
-        else
-#endif
-            buffer_strcat(wb, "\t\"aclk-available-protocol\": \"Legacy\",\n");
+        buffer_strcat(wb, "\t\"aclk-available-protocol\": \"New\",\n");
     }
     else
 #endif
