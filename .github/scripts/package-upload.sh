eed3d387aefdd6649eaadb6cf887ecc26b00e6e2
Austin S. Hemmelgarn <austin@netdata.cloud>
2022-06-28 02:11:37 -0400

    Add package uploads for RPM packages on new infrastructure.

---
+++ .github/scripts/package-upload.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+host="packages.netdata.cloud"
+user="netdatabot"
+
+distro="${1}"
+arch="${2}"
+format="${3}"
+repo="${4}"
+
+staging="/tmp/package-staging/"
+prefix="/var/www/html/repos/${repo}/"
+
+packages="$(find artifacts -name "*.${format}")"
+
+mkdir -p "${staging}"
+
+case "${format}" in
+    deb)
+        exit 0
+        ;;
+    rpm)
+        src="${staging}/${distro}/${arch}/"
+
+        for pkg in ${packages}; do
+            cp "${pkg}" "${src}"
+        done
+        ;;
+    *)
+        echo "Unrecognized package format ${format}."
+        exit 1
+        ;;
+esac
+
+rsync -vrlp --omit-dir-times --omit-link-times --inplace "${src}" "${user}@${host}:${prefix}"
