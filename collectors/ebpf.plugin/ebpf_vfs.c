129d4c284b0dfa685d9dabac258e69361f9fe8a5
Thiago Marques <thiagoftsm@gmail.com>
2022-07-11 01:26:29 +0000

    fix_interpolation: Avoid chart recreation when cgroup is not from systemd

--- collectors/ebpf.plugin/ebpf_vfs.c
+++ collectors/ebpf.plugin/ebpf_vfs.c
@@ -987,7 +987,7 @@ static int ebpf_send_systemd_vfs_charts(ebpf_module_t *em)
     for (ect = ebpf_cgroup_pids; ect ; ect = ect->next) {
         if (unlikely(ect->systemd) && unlikely(ect->updated)) {
             write_chart_dimension(ect->name, ect->publish_systemd_vfs.unlink_call);
-        } else
+        } else if (unlikely(ect->systemd))
             ret = 0;
     }
     write_end_chart();
