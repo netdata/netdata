11c7ee3455057e31bc65c14158379023e72ff070
Thiago Marques <thiagoftsm@gmail.com>
2022-07-12 19:23:14 +0000

    fix_interpolation: Remove pthread_cond_wait and convert mutex to normal mutex used when it is necessary to work with apps and cgroup (swap)

--- collectors/ebpf.plugin/ebpf_swap.c
+++ collectors/ebpf.plugin/ebpf_swap.c
@@ -686,12 +686,15 @@ static void swap_collector(ebpf_module_t *em)
     int cgroup = em->cgroup_charts;
     int update_every = em->update_every;
     int counter = update_every - 1;
+    heartbeat_t hb;
+    heartbeat_init(&hb);
+    usec_t step = em->update_every * USEC_PER_SEC;
     while (!close_ebpf_plugin) {
-        pthread_mutex_lock(&collect_data_mutex);
-        pthread_cond_wait(&collect_data_cond_var, &collect_data_mutex);
+        (void)heartbeat_next(&hb, step);
 
         if (++counter == update_every) {
             counter = 0;
+            pthread_mutex_lock(&collect_data_mutex);
             if (apps)
                 read_apps_table();
 
@@ -709,8 +712,8 @@ static void swap_collector(ebpf_module_t *em)
                 ebpf_swap_send_cgroup_data(update_every);
 
             pthread_mutex_unlock(&lock);
+            pthread_mutex_unlock(&collect_data_mutex);
         }
-        pthread_mutex_unlock(&collect_data_mutex);
     }
 }
 
