64bdcfaec8474f7a68c313c1512f1fdb9164bf1c
Thiago Marques <thiagoftsm@gmail.com>
2022-07-14 18:32:10 +0000

    fix_interpolation: Remove unecessary variable (SWAP)

--- collectors/ebpf.plugin/ebpf_swap.c
+++ collectors/ebpf.plugin/ebpf_swap.c
@@ -685,35 +685,31 @@ static void swap_collector(ebpf_module_t *em)
     int apps = em->apps_charts;
     int cgroup = em->cgroup_charts;
     int update_every = em->update_every;
-    int counter = update_every - 1;
     heartbeat_t hb;
     heartbeat_init(&hb);
-    usec_t step = em->update_every * USEC_PER_SEC;
+    usec_t step = update_every * USEC_PER_SEC;
     while (!close_ebpf_plugin) {
         (void)heartbeat_next(&hb, step);
 
-        if (++counter == update_every) {
-            counter = 0;
-            pthread_mutex_lock(&collect_data_mutex);
-            if (apps)
-                read_apps_table();
+        pthread_mutex_lock(&collect_data_mutex);
+        if (apps)
+            read_apps_table();
 
-            if (cgroup)
-                ebpf_update_swap_cgroup();
+        if (cgroup)
+            ebpf_update_swap_cgroup();
 
-            pthread_mutex_lock(&lock);
+        pthread_mutex_lock(&lock);
 
-            swap_send_global();
+        swap_send_global();
 
-            if (apps)
-                ebpf_swap_send_apps_data(apps_groups_root_target);
+        if (apps)
+            ebpf_swap_send_apps_data(apps_groups_root_target);
 
-            if (cgroup)
-                ebpf_swap_send_cgroup_data(update_every);
+        if (cgroup)
+            ebpf_swap_send_cgroup_data(update_every);
 
-            pthread_mutex_unlock(&lock);
-            pthread_mutex_unlock(&collect_data_mutex);
-        }
+        pthread_mutex_unlock(&lock);
+        pthread_mutex_unlock(&collect_data_mutex);
     }
 }
 
