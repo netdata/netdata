4b4b974ff55343f44dc9b0e3baa9cef2986f2821
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2022-07-18 11:27:47 +0300

    Fix to handle host constrains better

--- database/sqlite/sqlite_functions.c
+++ database/sqlite/sqlite_functions.c
@@ -7,11 +7,13 @@
 
 const char *database_config[] = {
     "CREATE TABLE IF NOT EXISTS host(host_id BLOB PRIMARY KEY, hostname TEXT NOT NULL, "
-    "registry_hostname TEXT NOT NULL, update_every INT NOT NULL, "
-    "os TEXT NOT NULL, timezone TEXT NOT NULL, tags TEXT NOT NULL, hops INT NOT NULL,"
-    "memory_mode INT, abbrev_timezone TEXT, utc_offset INT NOT NULL, "
-    "program_name TEXT NOT NULL, program_version TEXT NOT NULL, entries INT,"
-    "health_enabled INT NOT NULL);",
+    "registry_hostname TEXT NOT NULL default 'unknown', update_every INT NOT NULL default 1, "
+    "os TEXT NOT NULL default 'unknown', timezone TEXT NOT NULL default 'unknown', tags TEXT NOT NULL default '',"
+    "hops INT NOT NULL DEFAULT 0,"
+    "memory_mode INT DEFAULT 0, abbrev_timezone TEXT DEFAULT '', utc_offset INT NOT NULL DEFAULT 0,"
+    "program_name TEXT NOT NULL DEFAULT 'unknown', program_version TEXT NOT NULL DEFAULT 'unknown', "
+    "entries INT NOT NULL DEFAULT 0,"
+    "health_enabled INT NOT NULL DEFAULT 0);",
 
     "CREATE TABLE IF NOT EXISTS chart(chart_id blob PRIMARY KEY, host_id blob, type text, id text, name text, "
     "family text, context text, title text, unit text, plugin text, module text, priority int, update_every int, "
