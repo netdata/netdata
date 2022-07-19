c0a5962b3939cadcd4fae531da8c0190c1bbcf3b
Thiago Marques <thiagoftsm@gmail.com>
2022-07-19 22:19:31 +0000

    ebpf_fix_mem_issues: Add missing call to socket_bpf__destroy

--- collectors/ebpf.plugin/ebpf_socket.c
+++ collectors/ebpf.plugin/ebpf_socket.c
@@ -2927,6 +2927,10 @@ static void ebpf_socket_cleanup(void *ptr)
         if (objects)
             bpf_object__close(objects);
     }
+#ifdef LIBBPF_MAJOR_VERSION
+    else if (bpf_obj)
+        socket_bpf__destroy(bpf_obj);
+#endif
     finalized_threads = 1;
 }
 
