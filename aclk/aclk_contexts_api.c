89ddf8c2eb92a1b8c71ad5fcdac34a103e348461
Timo <timotej@netdata.cloud>
2022-09-08 14:44:17 +0200

    return aclk_update_node_collectors and aclk_update_node_info

--- aclk/aclk_contexts_api.c
+++ aclk/aclk_contexts_api.c
@@ -21,3 +21,21 @@ void aclk_send_contexts_updated(contexts_updated_t data)
     query->data.bin_payload.msg_name = "ContextsUpdated";
     QUEUE_IF_PAYLOAD_PRESENT(query);
 }
+
+void aclk_update_node_collectors(struct update_node_collectors *collectors)
+{
+    aclk_query_t query = aclk_query_new(UPDATE_NODE_COLLECTORS);
+    query->data.bin_payload.topic = ACLK_TOPICID_NODE_COLLECTORS;
+    query->data.bin_payload.payload = generate_update_node_collectors_message(&query->data.bin_payload.size, collectors);
+    query->data.bin_payload.msg_name = "UpdateNodeCollectors";
+    QUEUE_IF_PAYLOAD_PRESENT(query);
+}
+
+void aclk_update_node_info(struct update_node_info *info)
+{
+    aclk_query_t query = aclk_query_new(UPDATE_NODE_INFO);
+    query->data.bin_payload.topic = ACLK_TOPICID_NODE_INFO;
+    query->data.bin_payload.payload = generate_update_node_info_message(&query->data.bin_payload.size, info);
+    query->data.bin_payload.msg_name = "UpdateNodeInfo";
+    QUEUE_IF_PAYLOAD_PRESENT(query);
+}
