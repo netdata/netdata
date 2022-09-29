d21407719747ed69e575cf626c0ce56f212aa547
Costa Tsaousis <costa@netdata.cloud>
2022-09-29 18:32:57 +0300

    better logging of capabilities

--- streaming/rrdpush.c
+++ streaming/rrdpush.c
@@ -981,7 +981,7 @@ void log_receiver_capabilities(struct receiver_state *rpt) {
     BUFFER *wb = buffer_create(100);
     stream_capabilities_to_string(wb, rpt->capabilities);
 
-    info("STREAM %s [receive from [%s]:%s]: Netdata is using stream capabilities: %s",
+    info("STREAM %s [receive from [%s]:%s]: established link with agreed capabilities: %s",
          rrdhost_hostname(rpt->host), rpt->client_ip, rpt->client_port, buffer_tostring(wb));
 
     buffer_free(wb);
@@ -991,7 +991,7 @@ void log_sender_capabilities(struct sender_state *s) {
     BUFFER *wb = buffer_create(100);
     stream_capabilities_to_string(wb, s->capabilities);
 
-    info("STREAM %s [send to %s]: established communication with a parent using stream capabilities: %s",
+    info("STREAM %s [send to %s]: established link with agreed capabilities: %s",
          rrdhost_hostname(s->host), s->connected_to, buffer_tostring(wb));
 
     buffer_free(wb);
