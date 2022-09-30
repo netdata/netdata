e97b06215782f03d8bbea107927c2c409a90008f
Costa Tsaousis <costa@netdata.cloud>
2022-09-30 08:32:08 +0300

    fix for popen() not returning the right value

--- libnetdata/popen/popen.c
+++ libnetdata/popen/popen.c
@@ -282,42 +282,43 @@ cleanup_and_return:
     if (posix_spawn_file_actions_destroy(&fa))
         error("POPEN: posix_spawn_file_actions_destroy() failed");
 
-    if (ret == -1) {
-        if (fp_child_stdout)
-            fclose(fp_child_stdout);
-
-        if (fp_child_stdin)
-            fclose(fp_child_stdin);
-
+    if(ret == -1 || !fpp_child_stdin) {
         if (pipefd_stdin[PIPE_READ] != -1)
             close(pipefd_stdin[PIPE_READ]);
 
         if (pipefd_stdin[PIPE_WRITE] != -1)
             close(pipefd_stdin[PIPE_WRITE]);
 
+        if (fp_child_stdin)
+            fclose(fp_child_stdin);
+    }
+    else {
+        if(pipefd_stdin[PIPE_READ] != -1)
+            close(pipefd_stdin[PIPE_READ]);
+
+        if(fpp_child_stdin)
+            *fpp_child_stdin  = fp_child_stdin;
+    }
+
+    if (ret == -1 || !fpp_child_stdout) {
         if (pipefd_stdout[PIPE_READ] != -1)
             close(pipefd_stdout[PIPE_READ]);
 
         if (pipefd_stdout[PIPE_WRITE] != -1)
             close(pipefd_stdout[PIPE_WRITE]);
+
+        if (fp_child_stdout)
+            fclose(fp_child_stdout);
     }
     else {
-        // we are at the parent
-        // so close the child ends of the pipes
-
-        if(pipefd_stdin[PIPE_READ] != -1)
-            close(pipefd_stdin[PIPE_READ]);
-
         if(pipefd_stdout[PIPE_WRITE] != -1)
             close(pipefd_stdout[PIPE_WRITE]);
 
-        // and these are the file pointer that can be used
-        // to communicate with the child
-        if(fpp_child_stdin)  *fpp_child_stdin  = fp_child_stdin;
-        if(fpp_child_stdout) *fpp_child_stdout = fp_child_stdout;
+        if(fpp_child_stdout)
+            *fpp_child_stdout = fp_child_stdout;
     }
 
-    return -1;
+    return ret;
 }
 
 int netdata_popene_variadic_internal_dont_use_directly(volatile pid_t *pidptr, char **env, uint8_t flags, FILE **fpp_child_input, FILE **fpp_child_output, const char *command, ...) {
