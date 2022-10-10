0aed2dac4045e1ece4e3afb2d589d39667f799ba
Dim-P <dimitris1703@gmail.com>
2022-10-10 17:00:59 +0100

    Add paired and blocked device status metrics

--- collectors/python.d.plugin/bluetooth/bluetooth.chart.py
+++ collectors/python.d.plugin/bluetooth/bluetooth.chart.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# Description: example netdata python.d module
-# Author: Put your name here (your github login)
+# Description: bluetooth netdata python.d module
+# Author: Dim-P
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 try:
@@ -13,14 +13,16 @@ except ImportError:
 from bases.FrameworkServices.SimpleService import SimpleService
 
 ORDER = [
-    'bluetooth',
+    'status',
 ]
 
 CHARTS = {
-    'bluetooth': {
-        'options': [None, 'Connected Devices', 'number', 'connected', 'bluetooth.con', 'line'],
+    'status': {
+        'options': [None, 'Device Status', 'devices', 'Device Status', 'bluetooth.device_status', 'line'],
         'lines': [
-            ['connected', 'Connected', 'absolute', 1, 1]
+            ['connected', 'connected', 'absolute', 1, 1],
+            ['paired', 'paired', 'absolute', 1, 1],
+            ['blocked', 'blocked', 'absolute', 1, 1],
         ]
     }
 }
@@ -30,21 +32,33 @@ class Service(SimpleService):
     def __init__(self, configuration=None, name=None, bus=None, manager=None):
         SimpleService.__init__(self, configuration=configuration, name=name)
         self.order = ORDER
-        self.definitions = CHARTS
-        self.bus=dbus.SystemBus()
-        self.manager=dbus.Interface(self.bus.get_object('org.bluez','/'),'org.freedesktop.DBus.ObjectManager')
-    
-    def get_connected(self):
-        objs=self.manager.GetManagedObjects()
-        return [str(props['org.bluez.Device1']['Address']) for path,props in objs.items() if 'org.bluez.Device1' in props and props['org.bluez.Device1']['Connected']]
+        self.definitions = CHARTS        
 
     def check(self):
         if not HAS_DBUS:
-            self.error("Could not find dbus library")
+            self.error("Could not find dbus-python")
             return False
+        
+        self.bus=dbus.SystemBus()
+        self.manager=dbus.Interface(self.bus.get_object('org.bluez','/'),'org.freedesktop.DBus.ObjectManager')
 
         return True
 
     def get_data(self):
-        return {'connected': len(self.get_connected())}
+        connected=0
+        paired=0
+        blocked=0
+        objs=self.manager.GetManagedObjects()
+        for path,props in objs.items():
+            if 'org.bluez.Device1' in props:
+                if props['org.bluez.Device1']['Connected']:
+                    connected += 1
+                if props['org.bluez.Device1']['Paired']:
+                    paired += 1
+                if props['org.bluez.Device1']['Blocked']:
+                    blocked += 1
+                # print(props)
+        return {'connected':connected, 
+                'paired':   paired,
+                'blocked':  blocked}
 
