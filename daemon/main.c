4de2ce54d59a4128425f8dde5924eed4fc6dad97
vkalintiris <vasilis@netdata.cloud>
2022-11-29 17:26:35 +0200

    Sanitize command arguments. (#14064)
    * Sanitize bash arguments.
    
    Remove leading dashes and escape single quotes in command arguments.
    
    * Quote expanded variable in test

--- daemon/main.c
+++ daemon/main.c
@@ -1024,6 +1024,9 @@ int main(int argc, char **argv) {
                             fprintf(stderr, "\n\nALL TESTS PASSED\n\n");
                             return 0;
                         }
+                        else if(strcmp(optarg, "escapetest") == 0) {
+                            return command_argument_sanitization_tests();
+                        }
 #ifdef ENABLE_ML_TESTS
                         else if(strcmp(optarg, "mltest") == 0) {
                             return test_ml(argc, argv);
