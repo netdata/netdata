689dc6b7fbbf495ce3e020dcff0d014a8d338c52
vkalintiris <vasilis@netdata.cloud>
2022-12-21 15:03:05 +0200

    Refactor ML code and add support for multiple KMeans models. (#14065)
    * Add profile.plugin
    
    Creates the specified number of charts/dimensions, and supports
    backfilling with pseudo-historical data.
    
    * Bump
    
    * Remove wrongly merged line.
    
    * Use the number of models specified from the config section.
    
    * Add option to consult all ML models.
    
    * Remove profiling option consuming all models.
    
    * Add underscore after chart name prefix.
    
    * prediction -> dimensions chart
    
    * reorder funcs
    
    * Split charts across types with correct priority
    
    * Ignore training request when chart is under replication.
    
    * Track global number of models consulted.
    
    * Cleanup config.
    
    * initial readme updates
    
    * fix readme
    
    * readme
    
    * Fix function definition when ML is disabled.
    
    * Add dummy ml_chart_update_{begin,end}
    
    * Remove profile_plugin
    
    * Define chart priorities under collectors/all.h
    
    * s/curr_t/current_time/
    
    Co-authored-by: Andrew Maguire <andrewm4894@gmail.com>

--- ml/ADCharts.h
+++ ml/ADCharts.h
@@ -3,20 +3,18 @@
 #ifndef ML_ADCHARTS_H
 #define ML_ADCHARTS_H
 
+#include "Stats.h"
 #include "ml-private.h"
 
 namespace ml {
 
-void updateDimensionsChart(RRDHOST *RH,
-                           collected_number NumTrainedDimensions,
-                           collected_number NumNormalDimensions,
-                           collected_number NumAnomalousDimensions);
+void updateDimensionsChart(RRDHOST *RH, const MachineLearningStats &MLS);
 
 void updateHostAndDetectionRateCharts(RRDHOST *RH, collected_number AnomalyRate);
 
-void updateDetectionChart(RRDHOST *RH);
+void updateResourceUsageCharts(RRDHOST *RH, const struct rusage &PredictionRU, const struct rusage &TrainingRU);
 
-void updateTrainingChart(RRDHOST *RH, struct rusage *TRU);
+void updateTrainingStatisticsChart(RRDHOST *RH, const TrainingStats &TS);
 
 } // namespace ml
 
