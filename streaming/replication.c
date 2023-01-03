ee4b08351edf9e49bf6fa713dbe3a5638b624e11
Costa Tsaousis <costa@netdata.cloud>
2023-01-03 22:04:31 +0200

    be more sensitive to enable streaming after replication

--- streaming/replication.c
+++ streaming/replication.c
@@ -472,7 +472,11 @@ bool replication_response_execute_and_finalize(struct replication_query *q) {
     // get again the world clock time
     time_t world_clock_time = now_realtime_sec();
     if(enable_streaming) {
-        if(query_time < world_clock_time) {
+        if(query_time < world_clock_time ||
+           query_time < st->last_updated.tv_sec ||
+           query_time < st->last_collected_time.tv_sec ||
+           before < st->last_updated.tv_sec ||
+           before < st->last_collected_time.tv_sec) {
             // we needed time to execute this request
             // so, the parent will need to replicate more data
             enable_streaming = false;
