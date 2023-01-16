63fd4bb837b9b59a72f17bc34fc6914faa34d3e6
Austin S. Hemmelgarn <austin@netdata.cloud>
2023-01-16 08:30:52 -0500

    Set an explicit timeout in updater checks.
    If it takes more than an hour to run the updater, something has gone
    horribly wrong, so just kill it instead of letting it keep running.

--- .github/scripts/run-updater-check.sh
+++ .github/scripts/run-updater-check.sh
@@ -12,7 +12,13 @@ netdata -W buildinfo
 echo "::endgroup::"
 echo ">>> Updating Netdata..."
 export NETDATA_BASE_URL="http://localhost:8080/artifacts/" # Pull the tarball from the local web server.
-/netdata/packaging/installer/netdata-updater.sh --not-running-from-cron --no-updater-self-update || exit 1
+timeout 3600 /netdata/packaging/installer/netdata-updater.sh --not-running-from-cron --no-updater-self-update
+
+case "$?" in
+    124) echo "!!! Updater timed out." ; exit 1 ;;
+    0) ;;
+    *) echo "!!! Updater failed." ; exit 1 ;;
+esac
 echo "::group::>>> Post-Update Environment File Contents"
 cat /etc/netdata/.environment
 echo "::endgroup::"
