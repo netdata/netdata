52c7261812976519b437c7d63e669ba4a2885694
ilyam8 <ilya@netdata.cloud>
2023-01-17 16:42:14 +0200

    upd

--- .github/scripts/run-updater-check.sh
+++ .github/scripts/run-updater-check.sh
@@ -11,13 +11,23 @@ echo "::group::>>> Pre-Update Netdata Build Info"
 netdata -W buildinfo
 echo "::endgroup::"
 echo ">>> Updating Netdata..."
+cp /dev/null /var/log/netdata/error.log 2>/dev/null
 export NETDATA_BASE_URL="http://localhost:8080/artifacts/" # Pull the tarball from the local web server.
-timeout 1200 /netdata/packaging/installer/netdata-updater.sh --not-running-from-cron --no-updater-self-update
+timeout 900 /netdata/packaging/installer/netdata-updater.sh --not-running-from-cron --no-updater-self-update
 
 case "$?" in
-    124) echo "!!! Updater timed out." ; exit 1 ;;
-    0) ;;
-    *) echo "!!! Updater failed." ; exit 1 ;;
+124)
+    echo "!!! Updater timed out."
+    echo "::group::>>> Netdata error.log"
+    cat /var/log/netdata/error.log
+    echo "::endgroup::"
+    exit 1
+    ;;
+0) ;;
+*)
+    echo "!!! Updater failed."
+    exit 1
+    ;;
 esac
 echo "::group::>>> Post-Update Environment File Contents"
 cat /etc/netdata/.environment
