1f9a8e03f4c182f77faa4333ec1302d89fa384cb
Dim-P <dimitris1703@gmail.com>
2023-01-24 18:55:30 +0000

    Fallback to 'cat /etc/groups' if no getent exists

--- packaging/installer/functions.sh
+++ packaging/installer/functions.sh
@@ -396,6 +396,14 @@ get_os_key() {
   fi
 }
 
+group_exists(){
+  if command -v getent > /dev/null 2>&1; then
+    getent group "${1:-""}"
+  else
+    cat /etc/group | grep "^${1}:" 
+  fi
+}
+
 issystemd() {
   pids=''
   p=''
@@ -933,7 +941,7 @@ portable_add_group() {
   groupname="${1}"
 
   # Check if group exist
-  if getent group | grep -q "^${groupname}:"; then
+  if group_exists "${groupname}" > /dev/null 2>&1; then
     echo >&2 "Group '${groupname}' already exists."
     return 0
   fi
@@ -969,14 +977,14 @@ portable_add_user_to_group() {
   username="${2}"
 
   # Check if group exist
-  if ! getent group | grep -q "^${groupname}:"; then
+  if ! group_exists "${groupname}" > /dev/null 2>&1; then
     echo >&2 "Group '${groupname}' does not exist."
     # Don’t treat this as a failure, if the group does not exist we should not be trying to add the user to it.
     return 0
   fi
 
   # Check if user is in group
-  if getent group | grep "^${groupname}:" | cut -d ':' -f 4 | grep -q "${username}"; then
+  if group_exists "${groupname}" | cut -d ':' -f 4 | grep -wq "${username}"; then
     # username is already there
     echo >&2 "User '${username}' is already in group '${groupname}'."
     return 0
