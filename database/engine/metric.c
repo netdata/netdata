665a4bf88d3754d1a69c57bbc2da7a366be794b3
Costa Tsaousis <costa@netdata.cloud>
2023-01-30 19:33:14 +0200

    metrics registry aral max size to 512 elements per page

--- database/engine/metric.c
+++ database/engine/metric.c
@@ -319,7 +319,7 @@ MRG *mrg_create(void) {
         mrg->aral[i] = aral_create("mrg",
                                 sizeof(METRIC),
                                 0,
-                                4096,
+                                512,
                                 NULL, NULL, false,
                                 false);
     }
