0a2a25c561c7582278d394c64b069d7dd1bc19fd
Andrew Maguire <andrewm4894@gmail.com>
2023-02-09 15:19:36 +0000

    Update scrape-health-confs.py

--- .github/scripts/scrape-health-confs.py
+++ .github/scripts/scrape-health-confs.py
@@ -7,7 +7,7 @@ rootdir = './health/health.d'
 
 #%%
 
-
+configs = []
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         if file.endswith('.conf'):
@@ -15,7 +15,17 @@ for subdir, dirs, files in os.walk(rootdir):
                 data = file.read()
                 blocks = [block.split('\n') for block in data.split('\n\n')]
                 blocks = [block for block in blocks if len(block)>=3]
-                print(blocks)
-                break
+                for block in blocks:
+                    conf_dict = {'file': file}
+                    for line in block:
+                        if ':' in line and not line.startswith('#'):
+                            print(line)    
+                            line_parts = line.strip().split(':')
+                            key = line_parts[0].strip()
+                            value = line_parts[1].strip()
+                            conf_dict[key] = value
+                    configs.append(conf_dict)
+
+print(configs)
 
 #%%
