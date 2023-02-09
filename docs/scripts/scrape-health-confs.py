ac9d903dd74c735a8423d10fd978ff5f39e28ca7
Andrew Maguire <andrewm4894@gmail.com>
2023-02-09 15:38:40 +0000

    dev

--- docs/scripts/scrape-health-confs.py
+++ docs/scripts/scrape-health-confs.py
@@ -3,29 +3,39 @@
 #%%
 
 import os
-rootdir = './health/health.d'
+import pandas as pd
+
+root_dir = './health/health.d'
+out_dir = './docs/'
+
 
 #%%
 
 configs = []
-for subdir, dirs, files in os.walk(rootdir):
+for sub_dir, dirs, files in os.walk(root_dir):
     for file in files:
         if file.endswith('.conf'):
-            with open(os.path.join(subdir, file), 'r') as file:
+            with open(os.path.join(sub_dir, file), 'r') as file:
                 data = file.read()
                 blocks = [block.split('\n') for block in data.split('\n\n')]
                 blocks = [block for block in blocks if len(block)>=3]
                 for block in blocks:
-                    conf_dict = {'file': file}
+                    conf_dict = {'file': file.name}
                     for line in block:
                         if ':' in line and not line.startswith('#'):
-                            print(line)    
                             line_parts = line.strip().split(':')
                             key = line_parts[0].strip()
                             value = line_parts[1].strip()
                             conf_dict[key] = value
                     configs.append(conf_dict)
 
-print(configs)
+#%%
+
+df_configs = pd.DataFrame(configs)
+print(df_configs)
+
+df_configs.to_csv(f'{out_dir}/health_confs.csv', index=False)
+
+
 
 #%%
