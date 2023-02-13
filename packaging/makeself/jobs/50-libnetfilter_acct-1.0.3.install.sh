1190dec2e1a635ad068401bf20370960db5b3b3d
Tasos Katsoulas <tasos@netdata.cloud>
2023-02-13 14:52:10 +0200

    retry
    Signed-off-by: Tasos Katsoulas <tasos@netdata.cloud>

--- packaging/makeself/jobs/50-libnetfilter_acct-1.0.3.install.sh
+++ packaging/makeself/jobs/50-libnetfilter_acct-1.0.3.install.sh
@@ -15,7 +15,7 @@ version="1.0.3"
 export CFLAGS="-I/usr/include/libmnl -pipe"
 export LDFLAGS="-static -L/usr/lib -lmnl"
 export PKG_CONFIG="pkg-config --static"
-export PKG_CONFIG_PATH="/usr/lib/pkgconfig/libmnl.pc"
+export PKG_CONFIG_PATH="/usr/lib/pkgconfig"
 
 fetch "libnetfilter_acct-${version}" "https://www.netfilter.org/projects/libnetfilter_acct/files/libnetfilter_acct-${version}.tar.bz2" \
     4250ceef3efe2034f4ac05906c3ee427db31b9b0a2df41b2744f4bf79a959a1a libnetfilter_acct
