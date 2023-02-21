2096cbc32b69c1616d971abe426ac9ca5609d5e6
ghanapunq <vnphnqng@gmail.com>
2023-02-21 16:28:39 +0000

    WIP add rx bandwidth

--- collectors/python.d.plugin/ethtool/ethtool.chart.py
+++ collectors/python.d.plugin/ethtool/ethtool.chart.py
@@ -74,13 +74,28 @@ def device_charts(devs):
         )
 
     for d in devs:
-        order.append('dev_{0}_bytes_lost'.format(d.id))
+        order.append('dev_{0}_rx_bw'.format(d.id))
         charts.update(
             {
-                'dev_{0}_bytes_lost'.format(d.id): {
-                    'options': [None, 'Bytes lost', 'bytes lost', 'network_device', 'ethtool.dev_bytes_lost', 'line'],
+                'dev_{0}_rx_bw'.format(d.id): {
+                    'options': [None, 'Rx Bandwidth Bytes/sec', 'absolute', 'network_device',
+                                'ethtool.dev_rx_bandwidth', 'line'],
                     'lines': [
-                        ['dev_{0}_bytes_lost'.format(d.id), 'adapter {0}'.format(d.id)],
+                        ['dev_{0}_rx_bw'.format(d.id), 'device {0}'.format(d.id)],
+                    ]
+                }
+            }
+        )
+
+
+    for d in devs:
+        order.append('dev_{0}_packets_lost'.format(d.id))
+        charts.update(
+            {
+                'dev_{0}_packets_lost'.format(d.id): {
+                    'options': [None, 'Lost packets', 'Lost packets', 'network_device', 'ethtool.packets_lost', 'line'],
+                    'lines': [
+                        ['dev_{0}_packets_lost'.format(d.id), 'adapter {0}'.format(d.id)],
                     ]
                 }
             }
@@ -90,53 +105,75 @@ def device_charts(devs):
 
 
 class Metrics:
-    def __init__(self, device_id, bytes_received, rx_discards_phy):
+    def __init__(self, device_id, bytes_received, rx_bw, rx_discards_phy):
         self.id = device_id
         self.bytes_received = bytes_received
         self.rx_discards_phy = rx_discards_phy
+        self.rx_bw = rx_bw
 
     def data(self):
         return {
             'dev_{0}_bytes_received'.format(self.id): self.bytes_received,
-            'dev_{0}_bytes_lost'.format(self.id): self.rx_discards_phy,
+            'dev_{0}_rx_bw'.format(self.id): self.rx_bw,
+            'dev_{0}_packets_lost'.format(self.id): self.rx_discards_phy,
         }
-
-def format_data(raw_data, name):
-    rx_bytes_received = 0
-    rx_discards_phy = 0
-    for line in raw_data :
-        if 'rx_bytes_phy' in line.split(':')[0] :
-            rx_bytes_received = line.split(':')[1]
-        if 'rx_discards_phy' in line.split(':')[0] :
-            rx_discards_phy = line.split(':')[1]
-    return Metrics(name, rx_bytes_received, rx_discards_phy)
 
 
 class Service(ExecutableService):
     def __init__(self, configuration=None, name=None):
         ExecutableService.__init__(self, configuration=configuration, name=name)
+        self.dev_filter = self.configuration.get('dev_filter','')
         self.order = list()
         self.definitions = dict()
         self.s = find_binary('sudo')
         self.ethtool = find_binary('ethtool')
         self.ethtool_info =  [self.ethtool, '-S']
         self.devices = self.find_devices()
+        self.last_update = 0
+        self.start_rx_bytes_received = 0
+        self.last_rx_bytes_received = 0
         
+
+        
+    def format_data(self, raw_data, name, last_update):
+        rx_bytes_received = 0
+        rx_discards_phy = 0
+        for line in raw_data :
+            if 'rx_bytes_phy' in line.split(':')[0] :
+                rx_bytes_received = int(line.split(':')[1])
+            if 'rx_discards_phy' in line.split(':')[0] :
+                rx_discards_phy = int(line.split(':')[1])
+
+        if self.last_update == 0 :
+            rx_bw = 0
+            self.start_rx_bytes_received = rx_bytes_received
+            self.debug('init rx bw and last update start rx bytes received {}'.format(self.start_rx_bytes_received))
+        else:
+            nb_sec = (last_update - self.last_update).total_seconds()
+            self.debug(' rx_bytes_received {0} last_rx_bytes_received {1} nb seconds {2}'.format(rx_bytes_received, self.last_rx_bytes_received, nb_sec))
+            rx_bw = int((rx_bytes_received - self.last_rx_bytes_received) / nb_sec)
+        self.last_rx_bytes_received = rx_bytes_received
+        self.last_update = last_update
+        toto = Metrics(name, self.last_rx_bytes_received, rx_bw, rx_discards_phy)
+        self.debug('last_rx_bytes_received {}'.format(self.last_rx_bytes_received))
+        self.debug('toto {}'.format(toto.data()))
+        return toto
+
     def find_devices(self):
-        #TODO implement method to find all devices
-        # for the moment hardcoded
-        ifconfig = find_binary('ifconfig')
-        command = [ifconfig, '-s']
+        ls = find_binary('ls')
+        command = [ls, '/sys/class/net']
         raw_data = self._get_raw_data(command=command)
-        device_names = []
-        for line in raw_data:
-            name = line.split()[0]
-            if name == 'Iface':
-                continue
-            elif name == "ens1f0":
-            # else :
-                device_names.append(name)
-        return device_names
+        device_names = [s.rstrip() for s in raw_data ]
+        devices_filtered = []
+        # Filter devices according to the parameter set in the conf file
+        if self.dev_filter:
+            for device in device_names:
+                if self.dev_filter in device :
+                    devices_filtered.append(device)
+        else:
+            devices_filtered = device_names
+
+        return devices_filtered
 
     def check(self):
         self.debug('------ check my ethtool service ')
@@ -148,9 +185,10 @@ class Service(ExecutableService):
         for d in self.devices:
             command = self.ethtool_info.copy()
             command.append(d)
+            current_time = datetime.datetime.now()
             raw_data = self._get_raw_data(command=command)
-            data.append(format_data(raw_data, d))
-            break # WIP
+            data.append(self.format_data(raw_data, d, current_time))
+            
 
         o, c = device_charts(data)
         self.order.extend(o)
@@ -158,16 +196,15 @@ class Service(ExecutableService):
         return True
 
     def get_ethtool_data(self):
+        self.debug('------ get_ethtool data my ethtool service ')
         data = dict()
 
         for d in self.devices:
             command = self.ethtool_info.copy()
             command.append(d)
-            start = datetime.datetime.now()
+            current_time = datetime.datetime.now()
             raw_data = self._get_raw_data(command=command)
-            end = datetime.datetime.now()
-            self.debug('------ Get raw data at {0} finish at {1} elapsed {2}'.format(start, end, end - start))
-            data.update(format_data(raw_data, d).data())
+            data.update(self.format_data(raw_data, d, current_time).data())
 
         return data
 
