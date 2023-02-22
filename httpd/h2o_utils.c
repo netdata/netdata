f672f512723e5f0ef2930a870dab5cf9dd96593f
Timo <timotej@netdata.cloud>
2023-02-22 12:45:20 +0700

    fix for libh2o 2.2.6 vs latest master (used in my dev branch)

--- httpd/h2o_utils.c
+++ httpd/h2o_utils.c
@@ -21,7 +21,7 @@ h2o_iovec_pair_vector_t *parse_URL_params(h2o_mem_pool_t *pool, h2o_iovec_t para
     memset(params_vec, 0, sizeof(h2o_iovec_pair_vector_t));
 
     h2o_iovec_pair_t param;
-    while ((param.name.base = (char*)h2o_next_token(&params_string, '&', '&', &param.name.len, &param.value)) != NULL) {
+    while ((param.name.base = (char*)h2o_next_token(&params_string, '&', &param.name.len, &param.value)) != NULL) {
         if (params_vec->capacity == params_vec->size)
             h2o_vector_reserve(pool, params_vec, params_vec->capacity + KEY_VAL_BUFFER_GROWTH_STEP);
 
