ce6c9c6279504e783160a30872116999730885b4
Austin S. Hemmelgarn <austin@netdata.cloud>
2023-02-24 10:11:32 -0500

    Collapse cases with one-line bodies to single lines.

--- packaging/installer/kickstart.sh
+++ packaging/installer/kickstart.sh
@@ -333,9 +333,7 @@ trap_handler() {
   printf >&2 "%s\n\n" "${TPUT_BGRED}${TPUT_WHITE}${TPUT_BOLD} ERROR ${TPUT_RESET} Installer exited unexpectedly (${code}-${lineno})"
 
   case "${code}" in
-    0)
-      printf >&2 "%s\n" "This is almost certainly the result of a bug. If you have time, please report it at ${AGENT_BUG_REPORT_URL}."
-      ;;
+    0) printf >&2 "%s\n" "This is almost certainly the result of a bug. If you have time, please report it at ${AGENT_BUG_REPORT_URL}." ;;
     *)
       printf >&2 "%s\n" "This is probably a result of a transient issue on your system. Things should work correctly if you try again."
       printf >&2 "%s\n" "If you continue to experience this issue, you can reacn out to us for support on:"
@@ -673,24 +671,19 @@ get_system_info() {
           DISTRO_COMPAT_NAME="${DISTRO}"
       else
           case "${DISTRO}" in
-          opensuse-leap)
-              DISTRO_COMPAT_NAME="opensuse"
-              ;;
-          cloudlinux|almalinux|rocky|rhel)
-              DISTRO_COMPAT_NAME="centos"
-              ;;
-          artix|manjaro|obarun)
-              DISTRO_COMPAT_NAME="arch"
-              ;;
-          *)
-              DISTRO_COMPAT_NAME="unknown"
-              ;;
+          opensuse-leap) DISTRO_COMPAT_NAME="opensuse" ;;
+          cloudlinux|almalinux|rocky|rhel) DISTRO_COMPAT_NAME="centos" ;;
+          artix|manjaro|obarun) DISTRO_COMPAT_NAME="arch" ;;
+          *) DISTRO_COMPAT_NAME="unknown" ;;
           esac
       fi
 
       case "${DISTRO_COMPAT_NAME}" in
-        centos|ol)
-          SYSVERSION=$(echo "$SYSVERSION" | cut -d'.' -f1)
+        centos|ol) SYSVERSION=$(echo "$SYSVERSION" | cut -d'.' -f1) ;;
+        ubuntu|debian)
+          if [ -z "${SYSCODENAME}" ]; then
+            fatal "Could not determine ${DISTRO} release code name. This is required information on these systems." F0511
+          fi
           ;;
       esac
       ;;
@@ -702,9 +695,7 @@ get_system_info() {
       SYSTYPE="FreeBSD"
       SYSVERSION="$(uname -K)"
       ;;
-    *)
-      fatal "Unsupported system type detected. Netdata cannot be installed on this system using this script." F0200
-      ;;
+    *) fatal "Unsupported system type detected. Netdata cannot be installed on this system using this script." F0200 ;;
   esac
 }
 
@@ -1168,52 +1159,24 @@ claim() {
       progress "Successfully claimed node"
       return 0
       ;;
-    1)
-      warning "Unable to claim node due to invalid claiming options. If you are seeing this message, you’ve probably found a bug and should open a bug report at ${AGENT_BUG_REPORT_URL}"
-      ;;
-    2)
-      warning "Unable to claim node due to issues creating the claiming directory or preparing the local claiming key. Make sure you have a working openssl command and that ${INSTALL_PREFIX}/var/lib/netdata/cloud.d exists, then try again."
-      ;;
-    3)
-      warning "Unable to claim node due to missing dependencies. Usually this means that the Netdata Agent was built without support for Netdata Cloud. If you built the agent from source, please install all needed dependencies for Cloud support. If you used the regular installation script and see this error, please file a bug report at ${AGENT_BUG_REPORT_URL}."
-      ;;
-    4)
-      warning "Failed to claim node due to inability to connect to ${NETDATA_CLAIM_URL}. Usually this either means that the specified claiming URL is wrong, or that you are having networking problems."
-      ;;
+    1) warning "Unable to claim node due to invalid claiming options. If you are seeing this message, you’ve probably found a bug and should open a bug report at ${AGENT_BUG_REPORT_URL}" ;;
+    2) warning "Unable to claim node due to issues creating the claiming directory or preparing the local claiming key. Make sure you have a working openssl command and that ${INSTALL_PREFIX}/var/lib/netdata/cloud.d exists, then try again." ;;
+    3) warning "Unable to claim node due to missing dependencies. Usually this means that the Netdata Agent was built without support for Netdata Cloud. If you built the agent from source, please install all needed dependencies for Cloud support. If you used the regular installation script and see this error, please file a bug report at ${AGENT_BUG_REPORT_URL}." ;;
+    4) warning "Failed to claim node due to inability to connect to ${NETDATA_CLAIM_URL}. Usually this either means that the specified claiming URL is wrong, or that you are having networking problems." ;;
     5)
       progress "Successfully claimed node, but was not able to notify the Netdata Agent. You will need to restart the Netdata service on this node before it will show up in the Cloud."
       return 0
       ;;
-    8)
-      warning "Failed to claim node due to an invalid agent ID. You can usually resolve this by removing ${INSTALL_PREFIX}/var/lib/netdata/registry/netdata.public.unique.id and restarting the agent. Then try to claim it again using the same options."
-      ;;
-    9)
-      warning "Failed to claim node due to an invalid node name. This probably means you tried to specify a custom name for this node (for example, using the --claim-hostname option), but the hostname itself was either empty or consisted solely of whitespace. You can resolve this by specifying a valid host name and trying again."
-      ;;
-    10)
-      warning "Failed to claim node due to an invalid room ID. This issue is most likely caused by a typo.  Please check if the room(s) you are trying to add appear on the list of rooms provided to the --claim-rooms option ('${NETDATA_CLAIM_ROOMS}'). Then verify if the rooms are visible in Netdata Cloud and try again."
-      ;;
-    11)
-      warning "Failed to claim node due to an issue with the generated RSA key pair. You can usually resolve this by removing all files in ${INSTALL_PREFIX}/var/lib/netdata/cloud.d and then trying again."
-      ;;
-    12)
-      warning "Failed to claim node due to an invalid or expired claiming token. Please check that the token specified with the --claim-token option ('${NETDATA_CLAIM_TOKEN}') matches what you see in the Cloud and try again."
-      ;;
-    13)
-      warning "Failed to claim node because the Cloud thinks it is already claimed. If this node was created by cloning a VM or as a container from a template, please remove the file ${INSTALL_PREFIX}/var/lib/netdata/registry/netdata.public.unique.id and restart the agent. Then try to claim it again with the same options. Otherwise, if you are certain this node has never been claimed before, you can use the --claim-id option to specify a new node ID to use for claiming, for example by using the uuidgen command like so: --claim-id \"\$(uuidgen)\""
-      ;;
-    14)
-      warning "Failed to claim node because the node is already in the process of being claimed. You should not need to do anything to resolve this, the node should show up properly in the Cloud soon. If it does not, please report a bug at ${AGENT_BUG_REPORT_URL}."
-      ;;
-    15|16|17)
-      warning "Failed to claim node due to an internal server error in the Cloud. Please retry claiming this node later, and if you still see this message file a bug report at ${CLOUD_BUG_REPORT_URL}."
-      ;;
-    18)
-      warning "Unable to claim node because this Netdata installation does not have a unique ID yet. Make sure the agent is running and started up correctly, and then try again."
-      ;;
-    *)
-      warning "Failed to claim node for an unknown reason. This usually means either networking problems or a bug. Please retry claiming later, and if you still see this message file a bug report at ${AGENT_BUG_REPORT_URL}"
-      ;;
+    8) warning "Failed to claim node due to an invalid agent ID. You can usually resolve this by removing ${INSTALL_PREFIX}/var/lib/netdata/registry/netdata.public.unique.id and restarting the agent. Then try to claim it again using the same options." ;;
+    9) warning "Failed to claim node due to an invalid node name. This probably means you tried to specify a custom name for this node (for example, using the --claim-hostname option), but the hostname itself was either empty or consisted solely of whitespace. You can resolve this by specifying a valid host name and trying again." ;;
+    10) warning "Failed to claim node due to an invalid room ID. This issue is most likely caused by a typo.  Please check if the room(s) you are trying to add appear on the list of rooms provided to the --claim-rooms option ('${NETDATA_CLAIM_ROOMS}'). Then verify if the rooms are visible in Netdata Cloud and try again." ;;
+    11) warning "Failed to claim node due to an issue with the generated RSA key pair. You can usually resolve this by removing all files in ${INSTALL_PREFIX}/var/lib/netdata/cloud.d and then trying again." ;;
+    12) warning "Failed to claim node due to an invalid or expired claiming token. Please check that the token specified with the --claim-token option ('${NETDATA_CLAIM_TOKEN}') matches what you see in the Cloud and try again." ;;
+    13) warning "Failed to claim node because the Cloud thinks it is already claimed. If this node was created by cloning a VM or as a container from a template, please remove the file ${INSTALL_PREFIX}/var/lib/netdata/registry/netdata.public.unique.id and restart the agent. Then try to claim it again with the same options. Otherwise, if you are certain this node has never been claimed before, you can use the --claim-id option to specify a new node ID to use for claiming, for example by using the uuidgen command like so: --claim-id \"\$(uuidgen)\"" ;;
+    14) warning "Failed to claim node because the node is already in the process of being claimed. You should not need to do anything to resolve this, the node should show up properly in the Cloud soon. If it does not, please report a bug at ${AGENT_BUG_REPORT_URL}." ;;
+    15|16|17) warning "Failed to claim node due to an internal server error in the Cloud. Please retry claiming this node later, and if you still see this message file a bug report at ${CLOUD_BUG_REPORT_URL}." ;;
+    18) warning "Unable to claim node because this Netdata installation does not have a unique ID yet. Make sure the agent is running and started up correctly, and then try again." ;;
+    *) warning "Failed to claim node for an unknown reason. This usually means either networking problems or a bug. Please retry claiming later, and if you still see this message file a bug report at ${AGENT_BUG_REPORT_URL}" ;;
   esac
 
   if [ -z "${NETDATA_NEW_INSTALL}" ]; then
@@ -1281,9 +1244,7 @@ pkg_installed() {
           pacman -Qi "${1}" > /dev/null 2>&1
           return $?
           ;;
-        *)
-          return 1
-          ;;
+        *) return 1 ;;
       esac
       ;;
     Darwin)
@@ -1322,9 +1283,7 @@ netdata_avail_check() {
       zypper packages -r "$(zypper repos | grep -E 'netdata |netdata-edge ' | cut -f 1 -d '|' | tr -d ' ')" | grep -E 'netdata '
       return $?
       ;;
-    *)
-      return 1
-      ;;
+    *) return 1 ;;
   esac
 }
 
@@ -1777,9 +1736,7 @@ build_and_install() {
         fatal "netdata-installer.sh failed to run: Encountered an unhandled error in the installer code." I0000
       fi
       ;;
-    2)
-      fatal "Insufficient RAM to install netdata." F0008
-      ;;
+    2) fatal "Insufficient RAM to install netdata." F0008 ;;
   esac
 }
 
@@ -1945,9 +1902,7 @@ install_on_linux() {
         NETDATA_INSTALL_SUCCESSFUL=1
         INSTALL_PREFIX="/"
         ;;
-      1)
-        fatal "Unable to install on this system." F0300
-        ;;
+      1) fatal "Unable to install on this system." F0300 ;;
       2)
         if [ "${NETDATA_ONLY_NATIVE}" -eq 1 ]; then
           fatal "Could not install native binary packages." F0301
@@ -1968,9 +1923,7 @@ install_on_linux() {
         NETDATA_INSTALL_SUCCESSFUL=1
         INSTALL_PREFIX="/opt/netdata"
         ;;
-      1)
-        fatal "Unable to install on this system." F0302
-        ;;
+      1) fatal "Unable to install on this system." F0302 ;;
       2)
         if [ "${NETDATA_ONLY_STATIC}" -eq 1 ]; then
           fatal "Could not install static build." F0303
@@ -1987,12 +1940,8 @@ install_on_linux() {
     try_build_install
 
     case "$?" in
-      0)
-        NETDATA_INSTALL_SUCCESSFUL=1
-        ;;
-      *)
-        fatal "Unable to install on this system." F0304
-        ;;
+      0) NETDATA_INSTALL_SUCCESSFUL=1 ;;
+      *) fatal "Unable to install on this system." F0304 ;;
     esac
   fi
 }
@@ -2008,12 +1957,8 @@ install_on_macos() {
     try_build_install
 
     case "$?" in
-      0)
-        NETDATA_INSTALL_SUCCESSFUL=1
-        ;;
-      *)
-        fatal "Unable to install on this system." F0307
-        ;;
+      0) NETDATA_INSTALL_SUCCESSFUL=1 ;;
+      *) fatal "Unable to install on this system." F0307 ;;
     esac
   fi
 }
@@ -2029,12 +1974,8 @@ install_on_freebsd() {
     try_build_install
 
     case "$?" in
-      0)
-        NETDATA_INSTALL_SUCCESSFUL=1
-        ;;
-      *)
-        fatal "Unable to install on this system." F030A
-        ;;
+      0) NETDATA_INSTALL_SUCCESSFUL=1 ;;
+      *) fatal "Unable to install on this system." F030A ;;
     esac
   fi
 }
@@ -2133,9 +2074,7 @@ parse_args() {
       "--release-channel")
         RELEASE_CHANNEL="$(echo "${2}" | tr '[:upper:]' '[:lower:]')"
         case "${RELEASE_CHANNEL}" in
-          nightly|stable|default)
-            shift 1
-            ;;
+          nightly|stable|default) shift 1 ;;
           *)
             echo "Unrecognized value for --release-channel. Valid release channels are: stable, nightly, default"
             exit 1
@@ -2154,9 +2093,7 @@ parse_args() {
       "--auto-update-method")
         NETDATA_AUTO_UPDATE_TYPE="$(echo "${2}" | tr '[:upper:]' '[:lower:]')"
         case "${NETDATA_AUTO_UPDATE_TYPE}" in
-          systemd|interval|crontab)
-            shift 1
-            ;;
+          systemd|interval|crontab) shift 1 ;;
           *)
             echo "Unrecognized value for --auto-update-type. Valid values are: systemd, interval, crontab"
             exit 1
@@ -2252,12 +2189,8 @@ parse_args() {
             NETDATA_CLAIM_EXTRA="${NETDATA_CLAIM_EXTRA} -${optname}=${2}"
             shift 1
             ;;
-          verbose|insecure|noproxy|noreload|daemon-not-running)
-            NETDATA_CLAIM_EXTRA="${NETDATA_CLAIM_EXTRA} -${optname}"
-            ;;
-          *)
-            warning "Ignoring unrecognized claiming option ${optname}"
-            ;;
+          verbose|insecure|noproxy|noreload|daemon-not-running) NETDATA_CLAIM_EXTRA="${NETDATA_CLAIM_EXTRA} -${optname}" ;;
+          *) warning "Ignoring unrecognized claiming option ${optname}" ;;
         esac
         ;;
       "--local-build-options")
