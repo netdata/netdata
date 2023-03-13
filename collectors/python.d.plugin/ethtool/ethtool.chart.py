6a634748bd81697cd73bd34729e1b5c4b93c6b44
ghanapunq <vnphnqng@gmail.com>
2023-03-13 14:59:28 +0000

    fix flake8 errors

--- collectors/python.d.plugin/ethtool/ethtool.chart.py
+++ collectors/python.d.plugin/ethtool/ethtool.chart.py
@@ -5,7 +5,6 @@
 # User Memory Stat Author: Guido Scatena (scatenag)
 
 import datetime
-import re
 
 from bases.FrameworkServices.ExecutableService import ExecutableService
 from bases.collection import find_binary
@@ -21,6 +20,7 @@ ORDER = [
     RX_BW,
     TX_BW,
 ]
+
 
 def device_charts(devs):
     order = list()
@@ -74,7 +74,8 @@ def device_charts(devs):
 
 
 class Metrics:
-    def __init__(self, device_id, rx_bytes, rx_bw, rx_bw_util, rx_discards_phy, tx_bytes, tx_bw, tx_bw_util, tx_discards_phy, update_time):
+    def __init__(self, device_id, rx_bytes, rx_bw, rx_bw_util, rx_discards_phy,
+                 tx_bytes, tx_bw, tx_bw_util, tx_discards_phy, update_time):
         self.id = device_id
         self.rx_bytes = rx_bytes
         self.rx_discards_phy = rx_discards_phy
@@ -100,28 +101,27 @@ class Metrics:
 class Service(ExecutableService):
     def __init__(self, configuration=None, name=None):
         ExecutableService.__init__(self, configuration=configuration, name=name)
-        self.dev_filter = self.configuration.get('dev_filter','')
+        self.dev_filter = self.configuration.get('dev_filter', '')
         self.order = list()
         self.definitions = dict()
         self.s = find_binary('sudo')
         self.ethtool = find_binary('ethtool')
-        self.ethtool_info =  [self.ethtool, '-S']
+        self.ethtool_info = [self.ethtool, '-S']
         self.devices = self.find_devices()
         self.metrics = dict()
         self.max_bw_per_device = self.get_max_bw_per_device()
-        
-        
+
     def format_data(self, raw_data, name, last_update):
         rx_bytes = 0
         rx_discards_phy = 0
-        for line in raw_data :
-            if 'rx_bytes_phy' in line.split(':')[0] :
+        for line in raw_data:
+            if 'rx_bytes_phy' in line.split(':')[0]:
                 rx_bytes = int(line.split(':')[1])
-            if 'rx_discards_phy' in line.split(':')[0] :
+            if 'rx_discards_phy' in line.split(':')[0]:
                 rx_discards_phy = int(line.split(':')[1])
-            if 'tx_bytes_phy' in line.split(':')[0] :
+            if 'tx_bytes_phy' in line.split(':')[0]:
                 tx_bytes = int(line.split(':')[1])
-            if 'tx_discards_phy' in line.split(':')[0] :
+            if 'tx_discards_phy' in line.split(':')[0]:
                 tx_discards_phy = int(line.split(':')[1])
 
         if name not in self.metrics.keys() :
@@ -133,13 +133,15 @@ class Service(ExecutableService):
             tx_bw_util = 0
         else:
             nb_sec = (last_update - self.metrics[name].update_time).total_seconds()
-            self.debug(' rx_bytes {0} last_rx_bytes {1} nb seconds {2}'.format(rx_bytes, self.metrics[name].rx_bytes, nb_sec))
+            # self.debug(' rx_bytes {0} last_rx_bytes {1} nb seconds {2}'.format(rx_bytes, self.metrics[name].rx_bytes,
+            #  nb_sec))
             # Calculation of the bandwidth in Gb/s
             rx_bw = int((rx_bytes - self.metrics[name].rx_bytes) / nb_sec * 8 / (1000*1000*1000))
             rx_bw_util = rx_bw / self.max_bw_per_device[name] * 100
             tx_bw = int((tx_bytes - self.metrics[name].tx_bytes) / nb_sec * 8 / (1000*1000*1000))
             tx_bw_util = int(tx_bw / self.max_bw_per_device[name] * 100)
-        data = Metrics(name, rx_bytes, rx_bw, rx_bw_util, rx_discards_phy, tx_bytes, tx_bw, tx_bw_util, tx_discards_phy, last_update)
+        data = Metrics(name, rx_bytes, rx_bw, rx_bw_util, rx_discards_phy,
+                       tx_bytes, tx_bw, tx_bw_util, tx_discards_phy, last_update)
         self.debug('toto {}'.format(data.data()))
         self.metrics[name] = data
         return data
@@ -148,12 +150,12 @@ class Service(ExecutableService):
         ls = find_binary('ls')
         command = [ls, '/sys/class/net']
         raw_data = self._get_raw_data(command=command)
-        device_names = [s.rstrip() for s in raw_data ]
+        device_names = [s.rstrip() for s in raw_data]
         devices_filtered = []
         # Filter devices according to the parameter set in the conf file
         if self.dev_filter:
             for device in device_names:
-                if self.dev_filter in device :
+                if self.dev_filter in device:
                     devices_filtered.append(device)
         else:
             devices_filtered = device_names
@@ -168,12 +170,11 @@ class Service(ExecutableService):
             for l in raw_data:
                 s = l.split(':')
                 if 'Speed' in s[0]:
-                    bw = int(s[1].split('M')[0]) / 1000 # in Gb/s
+                    bw = int(s[1].split('M')[0]) / 1000  # in Gb/s
                     self.debug('------- Max speed of device {0} is {1} Gb/s'.format(d, bw))
                     max_bw_per_device[d] = bw
-        
+
         return max_bw_per_device
-                    
 
     def check(self):
         # check when it is used
@@ -189,8 +190,6 @@ class Service(ExecutableService):
             current_time = datetime.datetime.now()
             raw_data = self._get_raw_data(command=command)
             data.append(self.format_data(raw_data, d, current_time))
-            
-
         o, c = device_charts(data)
         self.order.extend(o)
         self.definitions.update(c)
@@ -213,4 +212,3 @@ class Service(ExecutableService):
         data = dict()
         data.update(self.get_ethtool_data())
         return data or None
-
