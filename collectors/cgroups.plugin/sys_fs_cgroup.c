449747bcfa2d9598229c140f9a4c886dca1ff12b
Thiago Marques <thiagoftsm@gmail.com>
2023-03-16 20:56:55 +0000

    log_level: Modify cgroup to inform cgroup_network what is the current severity_level

--- collectors/cgroups.plugin/sys_fs_cgroup.c
+++ collectors/cgroups.plugin/sys_fs_cgroup.c
@@ -1656,9 +1656,11 @@ static inline void read_cgroup_network_interfaces(struct cgroup *cg) {
 
     netdata_log_debug(D_CGROUP, "executing cgroup_identifier %s --cgroup '%s' for cgroup '%s'", cgroups_network_interface_script, cgroup_identifier, cg->id);
     FILE *fp_child_input, *fp_child_output;
-    (void)netdata_popen_raw_default_flags_and_environment(&cgroup_pid, &fp_child_input, &fp_child_output, cgroups_network_interface_script, "--cgroup", cgroup_identifier);
+    (void)netdata_popen_raw_default_flags_and_environment(&cgroup_pid, &fp_child_input, &fp_child_output, cgroups_network_interface_script, "--cgroup", cgroup_identifier, "--severity-level", log_severity_level_to_severity_string(use_severity_level));
     if(!fp_child_output) {
-        collector_error("CGROUP: cannot popen(%s --cgroup \"%s\", \"r\").", cgroups_network_interface_script, cgroup_identifier);
+        collector_error("CGROUP: cannot popen(%s --cgroup \"%s\", --severity-level \"%s\" \"r\").",
+                        cgroups_network_interface_script, cgroup_identifier,
+                        log_severity_level_to_severity_string(use_severity_level));
         return;
     }
 
