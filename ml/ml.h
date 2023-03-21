5046e034212c008557dd014196b6f6204eda24b2
vkalintiris <vasilis@netdata.cloud>
2023-03-21 11:24:41 +0200

    Use static thread-pool for training. (#14702)
    * Use static thread-pool for training.
    
    * Add missing function definition
    
    * disable training stats chart
    
    * Add config option to explicitly enable ML stats charts.
    
    ---------
    
    Co-authored-by: Costa Tsaousis <costa@netdata.cloud>

--- ml/ml.h
+++ ml/ml.h
@@ -13,7 +13,9 @@ extern "C" {
 bool ml_capable();
 bool ml_enabled(RRDHOST *rh);
 bool ml_streaming_enabled();
+
 void ml_init(void);
+void ml_fini(void);
 
 void ml_host_new(RRDHOST *rh);
 void ml_host_delete(RRDHOST *rh);
@@ -21,10 +23,6 @@ void ml_host_delete(RRDHOST *rh);
 void ml_host_get_info(RRDHOST *RH, BUFFER *wb);
 void ml_host_get_detection_info(RRDHOST *RH, BUFFER *wb);
 void ml_host_get_models(RRDHOST *RH, BUFFER *wb);
-
-void ml_host_start_training_thread(RRDHOST *rh);
-void ml_host_cancel_training_thread(RRDHOST *rh);
-void ml_host_stop_training_thread(RRDHOST *rh);
 
 void ml_chart_new(RRDSET *rs);
 void ml_chart_delete(RRDSET *rs);
@@ -34,6 +32,8 @@ void ml_chart_update_end(RRDSET *rs);
 void ml_dimension_new(RRDDIM *rd);
 void ml_dimension_delete(RRDDIM *rd);
 bool ml_dimension_is_anomalous(RRDDIM *rd, time_t curr_time, double value, bool exists);
+
+void ml_update_global_statistics_charts(uint64_t models_consulted);
 
 #ifdef __cplusplus
 };
