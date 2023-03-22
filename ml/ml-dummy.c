58ea9a18af8a39403ff0873deef42828c2d8daeb
vkalintiris <vasilis@netdata.cloud>
2023-03-22 13:16:12 +0200

    Revert "Revert "Use static thread-pool for training. (#14702)" (#14782)"
    This reverts commit 5321ca8d1ef8d974a6a2b2128ca8804de6acb693.

--- ml/ml-dummy.c
+++ ml/ml-dummy.c
@@ -19,6 +19,8 @@ bool ml_streaming_enabled() {
 
 void ml_init(void) {}
 
+void ml_fini(void) {}
+
 void ml_host_new(RRDHOST *rh) {
     UNUSED(rh);
 }
@@ -84,6 +86,10 @@ bool ml_dimension_is_anomalous(RRDDIM *rd, time_t curr_time, double value, bool 
     UNUSED(value);
     UNUSED(exists);
     return false;
+}
+
+void ml_update_global_statistics_charts(uint64_t models_consulted) {
+    UNUSED(models_consulted);
 }
 
 #endif
