be887c58f82fc76930b4c36848962c259ac0999e
Thiago Marques <thiagoftsm@gmail.com>
2023-03-26 18:09:28 +0000

    wolfssl: Modify rrdengine.h to use specific header only when linked with OpenSSL

--- database/engine/rrdengine.h
+++ database/engine/rrdengine.h
@@ -9,8 +9,10 @@
 #include <fcntl.h>
 #include <lz4.h>
 #include <Judy.h>
+#if defined(OPENSSL_VERSION_NUMBER)
 #include <openssl/sha.h>
 #include <openssl/evp.h>
+#endif
 #include "daemon/common.h"
 #include "../rrd.h"
 #include "rrddiskprotocol.h"
