4fb9aa5cbc5c2a06d0c6f0c2729738c829f40e9a
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2023-03-29 20:24:22 +0300

    Check error code

--- database/sqlite/sqlite_aclk.c
+++ database/sqlite/sqlite_aclk.c
@@ -849,6 +849,8 @@ void sql_create_aclk_table(RRDHOST *host __maybe_unused, uuid_t *host_uuid __may
         return;
     snprintfz(sql, ACLK_SYNC_QUERY_SIZE-1, INDEX_ACLK_ALERT, uuid_str, uuid_str);
     rc = db_execute(db_aclk, sql);
+    if (unlikely(rc))
+        error_report("Failed to create ACLK alert table index for host %s", host ? string2str(host->hostname) : host_guid);
 
     if (unlikely(!host))
         return;
