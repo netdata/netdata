e910c86d9fd107f7dfd8633a31ed2225ec101575
Thiago Marques <thiagoftsm@gmail.com>
2023-04-24 23:03:21 +0000

    fix_log: Remove lock when netdata is opening a log file.

--- libnetdata/log/log.c
+++ libnetdata/log/log.c
@@ -606,9 +606,7 @@ void open_all_log_files() {
     open_log_file(STDOUT_FILENO, stdout, stdout_filename, &output_log_syslog, 0, NULL);
     open_log_file(STDERR_FILENO, stderr, stdcollector_filename, &collector_log_syslog, 0, NULL);
 
-    log_lock();
     stderror = open_log_file(stdcollector_fd, NULL, stderr_filename, &error_log_syslog, 1, &stdcollector_fd);
-    log_unlock();
 
 #ifdef ENABLE_ACLK
     if(aclklog_enabled)
