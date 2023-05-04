8b55e065c285ebaadd5bccf6a77af23e0853f0c3
Thiago Marques <thiagoftsm@gmail.com>
2023-05-04 22:08:53 +0000

    debugfs_collector: Add initial files for new collector

---
+++ collectors/debugfs.plugin/README.md
@@ -0,0 +1,8 @@
+<!--
+title: "DebugFS monitoring (debugfs.plugin)"
+sidebar_label: "DebugFS monitoring "
+custom_edit_url: "https://github.com/netdata/netdata/edit/master/collectors/debugfs.plugin/README.md"
+learn_status: "Not Published"
+learn_topic_type: "References"
+learn_rel_path: "Integrations/Monitor/System metrics"
+-->
---
+++ collectors/debugfs.plugin/debugfs_plugin.c
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "collectors/all.h"
+#include "libnetdata/libnetdata.h"
+#include "libnetdata/required_dummies.h"
+
+int main(int argc, char **argv)
+{
+    // debug_flags = D_PROCFILE;
+    stderror = stderr;
+
+    // set the name for logging
+    program_name = "debugfs.plugin";
+
+    // disable syslog for debugfs.plugin
+    error_log_syslog = 0;
+
+    netdata_configured_host_prefix = getenv("NETDATA_HOST_PREFIX");
+    if(verify_netdata_host_prefix() == -1) exit(1);
+
+    user_config_dir = getenv("NETDATA_USER_CONFIG_DIR");
+    if(user_config_dir == NULL) {
+        user_config_dir = CONFIG_DIR;
+    }
+
+    stock_config_dir = getenv("NETDATA_STOCK_CONFIG_DIR");
+    if(stock_config_dir == NULL) {
+        // info("NETDATA_CONFIG_DIR is not passed from netdata");
+        stock_config_dir = LIBCONFIG_DIR;
+    }
+
+    return 0;
+}
