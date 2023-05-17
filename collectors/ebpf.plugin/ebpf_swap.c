7fe9f09233afb0007cc4a23145697c6abc6f5bda
Thiago Marques <thiagoftsm@gmail.com>
2023-05-17 22:34:32 +0000

    adjust_co_re: When it is not possible to load eBPF using trampoline, plugin tries to use kprobe (swap)

--- collectors/ebpf.plugin/ebpf_swap.c
+++ collectors/ebpf.plugin/ebpf_swap.c
@@ -953,8 +953,19 @@ static int ebpf_swap_load_bpf(ebpf_module_t *em)
         bpf_obj = swap_bpf__open();
         if (!bpf_obj)
             ret = -1;
-        else
+        else {
             ret = ebpf_swap_load_and_attach(bpf_obj, em);
+            if (ret && em->targets[NETDATA_DC_TARGET_LOOKUP_FAST].mode == EBPF_LOAD_TRAMPOLINE) {
+                swap_bpf__destroy(bpf_obj);
+                bpf_obj = swap_bpf__open();
+                if (bpf_obj)
+                    ret = -1;
+                else {
+                    ebpf_update_target_value(em, EBPF_LOAD_PROBE);
+                    ret = ebpf_swap_load_and_attach(bpf_obj, em);
+                }
+            }
+        }
     }
 #endif
 
