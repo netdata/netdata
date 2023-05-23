f80d0ae854b0a0da9a63d407b72aed31495ec785
Timo <timotej@netdata.cloud>
2023-05-23 18:08:17 +0200

    fix child

--- streaming/sender.c
+++ streaming/sender.c
@@ -617,8 +617,8 @@ static int rrdpush_http_upgrade_prelude(RRDHOST *host, struct sender_state *s) {
         0,
         1000);
 
-    if (!bytes) {
-        error_report("No response from remote");
+    if (bytes <= 0) {
+        error_report("Error reading from remote");
         return 1;
     }
 
