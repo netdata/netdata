f4a91941835164c3f57d325c0b23c657047881c7
Costa Tsaousis <costa@netdata.cloud>
2023-06-08 21:42:10 +0300

    add missing conditions

--- streaming/rrdpush.c
+++ streaming/rrdpush.c
@@ -524,6 +524,9 @@ void rrdpush_send_global_functions(RRDHOST *host) {
     if(!stream_has_capability(host->sender, STREAM_CAP_FUNCTIONS))
         return;
 
+    if(unlikely(!rrdhost_can_send_definitions_to_parent(host)))
+        return;
+
     BUFFER *wb = sender_start(host->sender);
 
     rrd_functions_expose_global_rrdpush(host, wb);
