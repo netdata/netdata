35eeffd8e123465aca00c3464b8e1031b3f450e6
vkalintiris <vasilis@netdata.cloud>
2023-06-08 07:41:52 +0300

    Add library to encode/decode Gorilla compressed buffers. (#15128)
    * Add library to encode/decode Gorilla compressed buffers.
    
    * Code cleanup + fix high-level API for 64 bits.
    
    * Add scripts to build benchmarks and fuzzer.
    
    * Fix CMake builds
    
    * Add license note.
    
    * Return 0 instead of false literal.

---
+++ libnetdata/gorilla/fuzzer.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+set -exu -o pipefail
+
+clang++ \
+    -std=c++11 -Wall -Wextra \
+    -DENABLE_FUZZER -O2 -g \
+    -fsanitize=fuzzer \
+    -o gorilla_fuzzer gorilla.cc
+
+./gorilla_fuzzer -workers=8 -jobs=8
