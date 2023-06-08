2586dbf3f893c37f8210c62ba31849f8212cb212
Timo <timotej@netdata.cloud>
2023-06-08 12:48:18 +0200

    rebase fix

--- streaming/sender.c
+++ streaming/sender.c
@@ -485,6 +485,13 @@ static inline bool rrdpush_sender_validate_response(RRDHOST *host, struct sender
     return false;
 }
 
+unsigned char alpn_proto_list[] = {
+    18, 'n', 'e', 't', 'd', 'a', 't', 'a', '_', 's', 't', 'r', 'e', 'a', 'm', '/', '2', '.', '0',
+    8, 'h', 't', 't', 'p', '/', '1', '.', '1'
+};
+
+#define CONN_UPGRADE_VAL "upgrade"
+
 static bool rrdpush_sender_connect_ssl(struct sender_state *s) {
 #ifdef ENABLE_HTTPS
     RRDHOST *host = s->host;
@@ -495,7 +502,7 @@ static bool rrdpush_sender_connect_ssl(struct sender_state *s) {
     if(!ssl_required)
         return true;
 
-    if (netdata_ssl_open(&host->sender->ssl, netdata_ssl_streaming_sender_ctx, s->rrdpush_sender_socket)) {
+    if (netdata_ssl_open_ext(&host->sender->ssl, netdata_ssl_streaming_sender_ctx, s->rrdpush_sender_socket, alpn_proto_list, sizeof(alpn_proto_list))) {
         if(!netdata_ssl_connect(&host->sender->ssl)) {
             // couldn't connect
 
@@ -531,13 +538,6 @@ static bool rrdpush_sender_connect_ssl(struct sender_state *s) {
     return true;
 #endif
 }
-
-unsigned char alpn_proto_list[] = {
-    18, 'n', 'e', 't', 'd', 'a', 't', 'a', '_', 's', 't', 'r', 'e', 'a', 'm', '/', '2', '.', '0',
-    8, 'h', 't', 't', 'p', '/', '1', '.', '1'
-};
-
-#define CONN_UPGRADE_VAL "upgrade"
 
 static int rrdpush_http_upgrade_prelude(RRDHOST *host, struct sender_state *s) {
 
@@ -663,36 +663,6 @@ static bool rrdpush_sender_thread_connect_to_parent(RRDHOST *host, int default_p
     }
 
     // info("STREAM %s [send to %s]: initializing communication...", rrdhost_hostname(host), s->connected_to);
-
-#ifdef ENABLE_HTTPS
-    if(netdata_ssl_client_ctx){
-        host->sender->ssl.flags = NETDATA_SSL_START;
-        if (!host->sender->ssl.conn){
-            host->sender->ssl.conn = SSL_new(netdata_ssl_client_ctx);
-            if(!host->sender->ssl.conn){
-                error("Failed to allocate SSL structure.");
-                host->sender->ssl.flags = NETDATA_SSL_NO_HANDSHAKE;
-            }
-            SSL_set_alpn_protos(host->sender->ssl.conn, alpn_proto_list, sizeof(alpn_proto_list));
-        }
-        else{
-            SSL_clear(host->sender->ssl.conn);
-        }
-
-        if (host->sender->ssl.conn)
-        {
-            if (SSL_set_fd(host->sender->ssl.conn, s->rrdpush_sender_socket) != 1) {
-                error("Failed to set the socket to the SSL on socket fd %d.", s->rrdpush_sender_socket);
-                host->sender->ssl.flags = NETDATA_SSL_NO_HANDSHAKE;
-            } else{
-                host->sender->ssl.flags = NETDATA_SSL_HANDSHAKE_COMPLETE;
-            }
-        }
-    }
-    else {
-        host->sender->ssl.flags = NETDATA_SSL_NO_HANDSHAKE;
-    }
-#endif
 
     // reset our capabilities to default
     s->capabilities = stream_our_capabilities();
