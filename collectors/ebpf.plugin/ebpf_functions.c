4de6a8a6c82824b9f043c6ca300309e824046ee7
thiagoftsm <thiagoftsm@gmail.com>
2023-06-14 22:49:58 +0000

    ebpf_functions: Add new thread that will be used for internal functions

---
+++ collectors/ebpf.plugin/ebpf_functions.c
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "ebpf.h"
+#include "ebpf_functions.h"
+
+/*****************************************************************
+ *  EBPF FUNCTION THREAD
+ *****************************************************************/
+
+/**
+ * FUNCTION thread.
+ *
+ * @param ptr a `ebpf_module_t *`.
+ * @return always NULL.
+ */
+void *ebpf_function_thread(void *ptr)
+{
+    return NULL;
+}
---
+++ collectors/ebpf.plugin/ebpf_functions.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef NETDATA_EBPF_FUNCTIONS_H
+#define NETDATA_EBPF_FUNCTIONS_H 1
+
+// configuration file
+#define NETDATA_DIRECTORY_FUNCTIONS_CONFIG_FILE "functions.conf"
+
+void *ebpf_function_thread(void *ptr);
+
+#endif
