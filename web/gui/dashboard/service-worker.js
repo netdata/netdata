fdf574ea5bc36653b39e73bafd1025467632198f
netdatabot <netdatabot@users.noreply.github.com>
2023-06-15 12:27:27 +0000

    Update dashboard to version v2.30.2.

--- web/gui/dashboard/service-worker.js
+++ web/gui/dashboard/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.21dcd7c609bff6504512face054c360f.js"
+  "./precache-manifest.6c2eed2426c491ea76dc7b3b70e39e2e.js"
 );
 
 self.addEventListener('message', (event) => {
