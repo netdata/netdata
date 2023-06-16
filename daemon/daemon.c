808c2aade5b4daec9b839e2fa7e9d3dda18498c8
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2023-06-16 14:12:25 +0300

    Fix recursion properly (part 4)

--- daemon/daemon.c
+++ daemon/daemon.c
@@ -54,12 +54,12 @@ static void fix_directory_file_permissions(const char *dirname, uid_t uid, gid_t
     struct dirent *de = NULL;
 
     while ((de = readdir(dir))) {
-        if (de->d_type == DT_DIR && (!strcmp(de->d_name,".") || !strcmp(de->d_name,"..") || !strcmp(de->d_name,"www")))
-                continue;
+        if (de->d_type == DT_DIR && (!strcmp(de->d_name, ".") || !strcmp(de->d_name, "..")))
+            continue;
 
         (void) snprintfz(filename, FILENAME_MAX, "%s/%s", dirname, de->d_name);
-        if (chown(filename, uid, gid) == -1)
-            error("Cannot chown directory '%s' to %u:%u", filename, (unsigned int)uid, (unsigned int)gid);
+        if ((de->d_type == DT_REG || recursive) && chown(filename, uid, gid) == -1)
+            error("Cannot chown %s '%s' to %u:%u", de->d_type == DT_DIR ? "directory" : "file", filename, (unsigned int)uid, (unsigned int)gid);
 
         if (de->d_type == DT_DIR && recursive)
             fix_directory_file_permissions(filename, uid, gid, recursive);
