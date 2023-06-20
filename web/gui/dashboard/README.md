314dab16c6361d4c55e768abf188e0cd4cbe2934
Austin S. Hemmelgarn <austin@netdata.cloud>
2023-06-20 10:22:37 -0400

    Update dashboard bundling script and remove old copy of single-node dashboard.

--- web/gui/dashboard/README.md
+++
@@ -1,1 +0,0 @@
-../.dashboard-notice.md
--- web/gui/dashboard/static/js/4.2dbcd906.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@netdata/dashboard"]=this["webpackJsonp@netdata/dashboard"]||[]).push([[4],{586:function(a,s,t){}}]);
-//# sourceMappingURL=4.2dbcd906.chunk.js.map
