175cff05ce9af341f674910bcb88145712a599ed
thiagoftsm <thiagoftsm@gmail.com>
2023-07-05 17:54:33 +0000

    ebpf_functions: Move function update for a position where it can workas as expected

--- collectors/ebpf.plugin/ebpf_cachestat.c
+++ collectors/ebpf.plugin/ebpf_cachestat.c
@@ -564,7 +564,6 @@ static void ebpf_cachestat_exit(void *ptr)
 
     pthread_mutex_lock(&ebpf_exit_cleanup);
     em->enabled = NETDATA_THREAD_EBPF_STOPPED;
-    ebpf_update_kernel_memory_with_vector(&plugin_statistics, em->maps, EBPF_ACTION_STAT_REMOVE);
     ebpf_update_stats(&plugin_statistics, em);
     pthread_mutex_unlock(&ebpf_exit_cleanup);
 }
