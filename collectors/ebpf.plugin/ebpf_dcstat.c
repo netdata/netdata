1f62db6a2200cf83d792beb00a7a7e111b53977f
thiagoftsm <thiagoftsm@gmail.com>
2023-07-05 02:02:31 +0000

    ebpf_functions: Update memory usage chart when thread goes out

--- collectors/ebpf.plugin/ebpf_dcstat.c
+++ collectors/ebpf.plugin/ebpf_dcstat.c
@@ -493,6 +493,7 @@ static void ebpf_dcstat_exit(void *ptr)
 
     pthread_mutex_lock(&ebpf_exit_cleanup);
     em->enabled = NETDATA_THREAD_EBPF_STOPPED;
+    ebpf_update_kernel_memory_with_vector(&plugin_statistics, em->maps, EBPF_ACTION_STAT_REMOVE);
     ebpf_update_stats(&plugin_statistics, em);
     pthread_mutex_unlock(&ebpf_exit_cleanup);
 }
