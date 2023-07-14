654bf674b66305dc72e730b9c0d598fe152d9471
vkalintiris <vasilis@netdata.cloud>
2023-07-14 22:07:21 +0300

    Rm unused header include

--- database/rrd.h
+++ database/rrd.h
@@ -1264,7 +1264,6 @@ static inline void rrdhost_retention(RRDHOST *host, time_t now, bool online, tim
 #ifdef ENABLE_DBENGINE
 #include "database/engine/rrdengineapi.h"
 #endif
-#include "sqlite/sqlite_functions.h"
 #include "sqlite/sqlite_context.h"
 #include "sqlite/sqlite_metadata.h"
 #include "sqlite/sqlite_aclk.h"
