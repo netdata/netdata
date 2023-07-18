59ff2246a212377ea621795f44faddaf724d7e4c
Vasilis Kalintiris <vasilis@netdata.cloud>
2023-07-18 10:21:57 +0300

    Fix macos build

--- collectors/macos.plugin/plugin_macos.h
+++ collectors/macos.plugin/plugin_macos.h
@@ -4,6 +4,7 @@
 #define NETDATA_PLUGIN_MACOS_H 1
 
 #include "daemon/common.h"
+#include "collectors/utils.h"
 
 int do_macos_sysctl(int update_every, usec_t dt);
 int do_macos_mach_smi(int update_every, usec_t dt);
