44680e06588c2041a013ac165257816324221b85
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2023-08-17 13:00:30 +0300

    Check if thread is available (it may not be if we are failing during init)

--- ml/ml.cc
+++ ml/ml.cc
@@ -1726,6 +1726,9 @@ void ml_stop_threads()
     Cfg.detection_stop = true;
     Cfg.training_stop = true;
 
+    if (!Cfg.detection_thread)
+        return;
+
     netdata_thread_cancel(Cfg.detection_thread);
     netdata_thread_join(Cfg.detection_thread, NULL);
 
