2f92e1e2f94c57e9531acb35506879a5136548eb
Costa Tsaousis <costa@netdata.cloud>
2023-08-18 20:01:27 +0300

    enable threads in openssl

--- packaging/makeself/jobs/20-openssl.install.sh
+++ packaging/makeself/jobs/20-openssl.install.sh
@@ -36,7 +36,7 @@ fi
 cd "${NETDATA_MAKESELF_PATH}/tmp/openssl" || exit 1
 
 if [ "${CACHE_HIT:-0}" -eq 0 ]; then
-    run ./config -static no-tests --prefix=/openssl-static --openssldir=/opt/netdata/etc/ssl
+    run ./config -static threads no-tests --prefix=/openssl-static --openssldir=/opt/netdata/etc/ssl
     run make -j "$(nproc)"
 fi
 
