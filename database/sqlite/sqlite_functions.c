66dc02960ab6e42b46ba5a22c3df94b92488f720
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2023-08-22 17:19:36 +0300

    Drop the env for setting the fail reason. Always pass netdata_fail_reason

--- database/sqlite/sqlite_functions.c
+++ database/sqlite/sqlite_functions.c
@@ -323,7 +323,7 @@ int init_database_batch(sqlite3 *database, int rebuild, int init_type, const cha
         if (rc != SQLITE_OK) {
             error_report("SQLite error during database %s, rc = %d (%s)", init_type ? "cleanup" : "setup", rc, err_msg);
             error_report("SQLite failed statement %s", batch[i]);
-            setenv("NETDATA_FAIL_REASON", err_msg, 1);
+            analytics_set_data_str(&analytics_data.netdata_fail_reason, err_msg);
             sqlite3_free(err_msg);
             if (SQLITE_CORRUPT == rc) {
                 if (!rebuild)
@@ -398,7 +398,7 @@ int sql_init_database(db_check_action_type_t rebuild, int memory)
     rc = sqlite3_open(sqlite_database, &db_meta);
     if (rc != SQLITE_OK) {
         error_report("Failed to initialize database at %s, due to \"%s\"", sqlite_database, sqlite3_errstr(rc));
-        setenv("NETDATA_FAIL_REASON", sqlite3_errstr(rc), 1);
+        analytics_set_data_str(&analytics_data.netdata_fail_reason, sqlite3_errstr(rc));
         sqlite3_close(db_meta);
         db_meta = NULL;
         return 1;
