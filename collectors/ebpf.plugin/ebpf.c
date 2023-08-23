e0cccf19aa40375aff00616905e5bd270a9bd421
thiagoftsm <thiagoftsm@gmail.com>
2023-08-23 00:39:46 +0000

    ebpf_socket_function: Remove codacy warning

--- collectors/ebpf.plugin/ebpf.c
+++ collectors/ebpf.plugin/ebpf.c
@@ -1531,10 +1531,8 @@ static void get_ipv6_first_addr(union netdata_ip_t *out, union netdata_ip_t *in,
         ret[1] = 0ULL;
 
         tmp = be64toh(ret[0]);
-        if (prefix > 0) {
-            mask = 0xFFFFFFFFFFFFFFFFULL << (64 - prefix);
-            tmp &= mask;
-        }
+        mask = 0xFFFFFFFFFFFFFFFFULL << (64 - prefix);
+        tmp &= mask;
         ret[0] = htobe64(tmp);
     } else {
         mask = 0xFFFFFFFFFFFFFFFFULL << (128 - prefix);
