81ee45c6cf681c14fdb2bfc5520caede5e352ce0
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2023-08-25 15:52:06 +0300

    Fix rebase issues

--- database/rrd.h
+++ database/rrd.h
@@ -1105,7 +1105,7 @@ struct rrdhost_system_info {
     int mc_version;
 };
 
-struct rrdhost_system_info *rrdhost_labels_to_system_info(DICTIONARY *labels);
+struct rrdhost_system_info *rrdhost_labels_to_system_info(RRDLABELS *labels);
 
 struct rrdhost {
     char machine_guid[GUID_LEN + 1];                // the unique ID of this host
