700d9ae7f54715fa62d3e892b2baf6e24aaa2ad5
Fotis Voutsas <fotis@netdata.cloud>
2023-08-29 18:02:37 +0300

    initial commit for generating integrations docs from integrations.js

---
+++ integrations/gen_docs_integrations.py
@@ -0,0 +1,199 @@
+import json
+
+path_dict = {}
+
+path_dict["collector"] = "collectors/"
+path_dict["exporter"] = "exporting/"
+path_dict["notification"] = "health/notifications/"
+
+
+def generate_category_from_name(category_fragment, category_array):
+    """
+    Takes a category ID in splitted form ("." as delimiter) and the array of the categories, and returns the proper category name that Learn expects.
+    """
+
+    category_name = ""
+    i = 0
+    dummy_id = category_fragment[0]
+
+    while i < len(category_fragment):
+        for category in category_array:
+
+            if dummy_id == category['id']:
+                category_name = category_name + "/" + category["name"]
+                try:
+                    # print("equals")
+                    # print(fragment, category_fragment[i+1])
+                    dummy_id = dummy_id + "." + category_fragment[i+1]
+                    # print(dummy_id)
+                except:
+                    return category_name.split("/", 1)[1]
+                category_array = category['children']
+                break
+        i += 1
+
+
+with open('integrations/integrations.js') as dataFile:
+    data = dataFile.read()
+    categories_str = data.split("export const categories = ")[1].split("export const integrations = ")[0]
+    integrations_str = data.split("export const categories = ")[1].split("export const integrations = ")[1]
+    # jsonObj = json.loads("{" + f"{categories_obj}" + "}")
+    categories = json.loads(categories_str)
+
+    integrations = json.loads(integrations_str)
+
+# print(integrations[0].keys())
+
+# print(integrations[0]["integration_type"])
+
+# COLLECTORS
+for integration in integrations:
+    # integration = integrations[0]
+    if integration['integration_type'] == "collectorfail":
+
+        try:
+            custom_edit_url = integration['edit_link'].replace("blob", "edit")
+
+            sidebar_label = integration['meta']['monitored_instance']['name']
+
+            learn_rel_path = generate_category_from_name(
+                integration['meta']['monitored_instance']['categories'][0].split("."), categories)
+
+            md = f"""
+---
+custom_edit_url: "{custom_edit_url}"
+sidebar_label: "{sidebar_label}"
+learn_status: "Published"
+learn_rel_path "{learn_rel_path}"
+---
+
+{integration['overview']}
+"""
+
+            if integration['metrics']:
+                md += f"""
+{integration['metrics']}
+"""
+
+            if integration['alerts']:
+                md += f"""
+{integration['alerts']}
+"""
+
+            if integration['setup']:
+                md += f"""
+{integration['setup']}
+"""
+
+            if integration['troubleshooting']:
+                md += f"""
+{integration['troubleshooting']}
+"""
+
+            path = custom_edit_url.replace("https://github.com/netdata/", "").split("/",
+                                                                                    1)[1].replace("edit/master/", "").replace("/metadata.yaml", "")
+
+            # print(path)
+
+            try:
+                with open(f'{path}/README_new.md', 'w') as txt:
+                    txt.write(md)
+            except:
+                pass
+        except Exception as e:
+            print(e, "\n", integration)
+            quit()
+    elif integration['integration_type'] == "exporter":
+        try:
+            # print(integration.keys())
+            custom_edit_url = integration['edit_link'].replace("blob", "edit")
+
+            sidebar_label = integration['meta']['name']
+
+            learn_rel_path = generate_category_from_name(integration['meta']['categories'][0].split("."), categories)
+
+            md = f"""
+---
+custom_edit_url: "{custom_edit_url}"
+sidebar_label: "{sidebar_label}"
+learn_status: "Published"
+learn_rel_path "{learn_rel_path}"
+---
+
+{integration['overview']}
+"""
+
+            if integration['setup']:
+                md += f"""
+{integration['setup']}
+"""
+
+            if integration['troubleshooting']:
+                md += f"""
+{integration['troubleshooting']}
+"""
+
+            path = custom_edit_url.replace("https://github.com/netdata/", "").split("/",
+                                                                                    1)[1].replace("edit/master/", "").replace("/metadata.yaml", "")
+
+            try:
+                with open(f'{path}/test.md', 'w') as txt:
+                    txt.write(md)
+            except:
+                pass
+        except Exception as e:
+            print(e, "\n", integration)
+            quit()
+    elif integration['integration_type'] == "notification":
+        try:
+            print(integration.keys())
+            custom_edit_url = integration['edit_link'].replace("blob", "edit")
+
+            sidebar_label = integration['meta']['name']
+
+            learn_rel_path = generate_category_from_name(integration['meta']['categories'][0].split("."), categories)
+
+            md = f"""
+---
+custom_edit_url: "{custom_edit_url}"
+sidebar_label: "{sidebar_label}"
+learn_status: "Published"
+learn_rel_path "{learn_rel_path}"
+---
+
+{integration['overview']}
+"""
+
+            if integration['setup']:
+                md += f"""
+{integration['setup']}
+"""
+
+            if integration['troubleshooting']:
+                md += f"""
+{integration['troubleshooting']}
+"""
+
+            path = custom_edit_url.replace("https://github.com/netdata/", "").split("/",
+                                                                                    1)[1].replace("edit/master/", "").replace("/metadata.yaml", "")
+
+            if "cloud-notifications" in path:
+                name = integration['meta']['name'].lower()
+                try:
+                    with open(f'{path}/{name}.md', 'w') as txt:
+                        txt.write(md)
+                except:
+                    print("EXCEPTION", integration)
+                    quit()
+            else:
+
+                try:
+                    with open(f'{path}/test.md', 'w') as txt:
+                        txt.write(md)
+                except:
+                    print("EXCEPTION", integration)
+                    quit()
+
+        except Exception as e:
+            print(e, "\n", integration)
+            quit()
