3658b7f45c4ac34418e9cbe30c9943d5c6a00e05
Austin S. Hemmelgarn <austin@netdata.cloud>
2023-09-14 07:32:48 -0400

    Skip trying to preserve file owners when bundling external code.
    This fixes an issue that shows up when building packages for some
    distros (most notably CentOS 7) with a rootless container runtime (such
    as using Podman for the build as a regular user).

--- packaging/bundle-ebpf.sh
+++ packaging/bundle-ebpf.sh
@@ -11,7 +11,7 @@ if [ -x "${PLUGINDIR}/ebpf.plugin" ] || [ "${FORCE}" = "force" ]; then
     mkdir -p "${SRCDIR}/tmp/ebpf"
     curl -sSL --connect-timeout 10 --retry 3 "https://github.com/netdata/kernel-collector/releases/download/${EBPF_VERSION}/${EBPF_TARBALL}" > "${EBPF_TARBALL}" || exit 1
     grep "${EBPF_TARBALL}" "${SRCDIR}/packaging/ebpf.checksums" | sha256sum -c - || exit 1
-    tar -xvaf "${EBPF_TARBALL}" -C "${SRCDIR}/tmp/ebpf" || exit 1
+    tar -xva --no-same-owner -f "${EBPF_TARBALL}" -C "${SRCDIR}/tmp/ebpf" || exit 1
     if [ ! -d "${PLUGINDIR}/ebpf.d" ];then
         mkdir "${PLUGINDIR}/ebpf.d"
     fi
