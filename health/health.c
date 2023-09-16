7b0395b9c493e37f5b250bf44dcfafd6459d7354
Emmanuel Vasilakis <mrzammler@mm.st>
2023-09-16 19:39:18 +0300

    re-store rrdvars on late dimensions

--- health/health.c
+++ health/health.c
@@ -990,7 +990,7 @@ static void health_execute_delayed_initializations(RRDHOST *host) {
             }
             foreach_rrdcalctemplate_done(rt);
 
-            if (health_variable_check(health_rrdvars, st, rd))
+            if (health_variable_check(health_rrdvars, st, rd) || rrdset_flag_check(st, RRDSET_FLAG_HAS_RRDCALC_LINKED))
                 rrdvar_store_for_chart(host, st);
         }
         rrddim_foreach_done(rd);
