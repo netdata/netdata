c000af37285d363fb8240fb4ecff260912a7c344
Timo <timotej@netdata.cloud>
2023-11-02 14:39:21 +0100

    temp

--- web/server/h2o/h2o_utils.c
+++ web/server/h2o/h2o_utils.c
@@ -20,13 +20,13 @@ h2o_iovec_pair_vector_t *parse_URL_params(h2o_mem_pool_t *pool, h2o_iovec_t para
     h2o_iovec_pair_vector_t *params_vec = h2o_mem_alloc_shared(pool, sizeof(h2o_iovec_pair_vector_t), NULL);
     memset(params_vec, 0, sizeof(h2o_iovec_pair_vector_t));
 
-    h2o_iovec_pair_t param;
+/*    h2o_iovec_pair_t param;
     while ((param.name.base = (char*)h2o_next_token(&params_string, '&', &param.name.len, &param.value)) != NULL) {
         if (params_vec->capacity == params_vec->size)
             h2o_vector_reserve(pool, params_vec, params_vec->capacity + KEY_VAL_BUFFER_GROWTH_STEP);
 
             params_vec->entries[params_vec->size++] = param;
-    }
+    } */
 
     return params_vec;
 }
