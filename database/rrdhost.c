3f978e0c8cc87d3ca429aa76f07fa7b0ddf53ec8
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2023-11-13 16:16:18 +0200

    Node empheral removal timeout 86400 seconds (1 day)

--- database/rrdhost.c
+++ database/rrdhost.c
@@ -31,7 +31,7 @@ netdata_rwlock_t rrd_rwlock = NETDATA_RWLOCK_INITIALIZER;
 
 time_t rrdset_free_obsolete_time_s = 3600;
 time_t rrdhost_free_orphan_time_s = 3600;
-time_t rrdhost_free_ephemeral_time_s = 3600;
+time_t rrdhost_free_ephemeral_time_s = 86400;
 
 bool is_storage_engine_shared(STORAGE_INSTANCE *engine __maybe_unused) {
 #ifdef ENABLE_DBENGINE
