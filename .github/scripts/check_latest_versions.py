2d5a545e2ce7008d47fdaf82c39a6dc1a01eed61
Tasos Katsoulas <tasos@netdata.cloud>
2023-11-13 21:27:55 +0200

    minor formating plus workflow fix
    Signed-off-by: Tasos Katsoulas <tasos@netdata.cloud>

--- .github/scripts/check_latest_versions.py
+++ .github/scripts/check_latest_versions.py
@@ -1,26 +1,30 @@
 import sys
 import os
 import modules.version_manipulation as ndvm
+import modules.github_actions as cigh
+
 
 def main(command_line_args):
-    """
-    Inputs: Single version or multiple versions
-    """
-    versions = [str(arg) for arg in command_line_args]
-    # Create a temp output folder for the release that need update
-    staging = os.path.join(os.environ.get('TMPDIR', '/tmp'), 'staging-new-releases')
-    os.makedirs(staging, exist_ok=True)
-    
-    for version in versions:
-        temp_value = ndvm.compare_version_with_remote(version)
-        if temp_value == version:
-            path, filename = ndvm.get_release_path_and_filename(version)
-            release_path = os.path.join(staging, path)
-            os.makedirs(release_path, exist_ok=True)
-            file_release_path = os.path.join(release_path, filename)
-            with open(file_release_path, "w") as file:
-                print("Creating local copy of the release version update at: ", file_release_path)
-                file.write(version)
+	"""
+	Inputs: Single version or multiple versions
+	"""
+	versions = [str(arg) for arg in command_line_args]
+	# Create a temp output folder for the release that need update
+	staging = os.path.join(os.environ.get('TMPDIR', '/tmp'), 'staging-new-releases')
+	os.makedirs(staging, exist_ok=True)
+	for version in versions:
+		temp_value = ndvm.compare_version_with_remote(version)
+		if temp_value == version:
+			path, filename = ndvm.get_release_path_and_filename(version)
+			release_path = os.path.join(staging, path)
+			os.makedirs(release_path, exist_ok=True)
+			file_release_path = os.path.join(release_path, filename)
+			with open(file_release_path, "w") as file:
+				print("Creating local copy of the release version update at: ", file_release_path)
+				file.write(version)
+				if cigh.run_as_github_action():
+					cigh.update_github_output("versions_needs_update", "true")
+
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+	main(sys.argv[1:])
