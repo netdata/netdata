f6b20df37810ee77ad5fbdac828c79199078aab1
ilyam8 <ilya@netdata.cloud>
2023-11-30 22:01:53 +0200

    setenv

--- daemon/main.c
+++ daemon/main.c
@@ -1346,6 +1346,8 @@ int get_system_info(struct rrdhost_system_info *system_info) {
 
                 if(unlikely(rrdhost_set_system_info_variable(system_info, line, value))) {
                     netdata_log_error("Unexpected environment variable %s=%s", line, value);
+                } else {
+                    setenv(line, value, 1);
                 }
             }
         }
