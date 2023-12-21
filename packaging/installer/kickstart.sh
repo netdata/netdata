95d8a5ffd9dee9a6288550913c8f04b3d591a683
Austin S. Hemmelgarn <austin@netdata.cloud>
2023-12-21 07:11:11 -0500

    Remove Netdata packages from APT cache when attempting to install. (#16566)
    * Remove Netdata packages from APT cache when attempting to install.
    
    This should help resolve some issues we have with installing when dealing
    with unclean caches after dist-upgrades.
    
    * Run cached package removal as root.
    
    * Update packaging/installer/kickstart.sh
    
    Co-authored-by: Ilya Mashchenko <ilya@netdata.cloud>
    
    ---------
    
    Co-authored-by: Ilya Mashchenko <ilya@netdata.cloud>

--- packaging/installer/kickstart.sh
+++ packaging/installer/kickstart.sh
@@ -1357,6 +1357,14 @@ check_special_native_deps() {
   fi
 }
 
+cleanup_apt_cache() {
+    cache_dir="/var/cache/apt/archives"
+
+    if [ -d "${cache_dir}" ]; then
+        run_as_root find "${cache_dir}" -type f -name 'netdata*.deb' -delete
+    fi
+}
+
 common_rpm_opts() {
   pkg_type="rpm"
   pkg_suffix=".noarch"
@@ -1423,6 +1431,7 @@ try_package_install() {
         install_subcmd="install"
       fi
       needs_early_refresh=1
+      needs_apt_cache_cleanup=1
       pm_cmd="apt-get"
       repo_subcmd="update"
       pkg_type="deb"
@@ -1520,6 +1529,10 @@ try_package_install() {
       if ! run_as_root env ${env} ${pm_cmd} ${repo_subcmd} ${repo_update_opts}; then
         warning "${failed_refresh_msg}"
         return 2
+      fi
+
+      if [ -n "${needs_apt_cache_cleanup}" ]; then
+        cleanup_apt_cache
       fi
     fi
 
