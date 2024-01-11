1eff208e0e225fb379b770b41f1d8f911e68699e
Costa Tsaousis <costa@netdata.cloud>
2024-01-11 20:43:25 +0200

    fix cmake _GNU_SOURCE warnings

--- mqtt_websockets/src/mqtt_wss_client.c
+++ mqtt_websockets/src/mqtt_wss_client.c
@@ -10,7 +10,9 @@
 //
 // You should have received a copy of the GNU General Public License along with this program.
 // If not, see <https://www.gnu.org/licenses/>.
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
 
 #include "mqtt_wss_client.h"
 #include "mqtt_ng.h"
