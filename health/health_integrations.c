0dc18d93a6059ce033f26e3152cfb66b9aa885d7
Costa Tsaousis <costa@netdata.cloud>
2024-01-29 11:11:06 +0200

    removed duplicate code

--- health/health_integrations.c
+++ health/health_integrations.c
@@ -4,42 +4,6 @@
 
 // --------------------------------------------------------------------------------------------------------------------
 
-#define ENUM_STR_MAP_DEFINE(type)   \
-static struct  {                    \
-    type id;                        \
-    const char *name;               \
-} type ## _names[]
-
-#define ENUM_STR_DEFINE_FUNCTIONS_EXTERN(type)                                                                         \
-    type type ## _2id(const char *str);                                                                                \
-    const char *type##_2str(type id);
-
-#define ENUM_STR_DEFINE_FUNCTIONS(type, def, def_str)                                                                  \
-    type type##_2id(const char *str)                                                                                   \
-    {                                                                                                                  \
-        if (!str || !*str)                                                                                             \
-            return def;                                                                                                \
-                                                                                                                       \
-        for (size_t i = 0; type ## _names[i].name; i++) {                                                              \
-            if (strcmp(type ## _names[i].name, str) == 0)                                                              \
-                return type ## _names[i].id;                                                                           \
-        }                                                                                                              \
-                                                                                                                       \
-        return def;                                                                                                    \
-    }                                                                                                                  \
-                                                                                                                       \
-    const char *type##_2str(type id)                                                                                   \
-    {                                                                                                                  \
-        for (size_t i = 0; type ## _names[i].name; i++) {                                                              \
-            if (id == type ## _names[i].id)                                                                            \
-                return type ## _names[i].name;                                                                         \
-        }                                                                                                              \
-                                                                                                                       \
-        return def_str;                                                                                                \
-    }
-
-// --------------------------------------------------------------------------------------------------------------------
-
 typedef enum __attribute__((packed)) {
     NOTIF_CURL_NONE = 0,
     NOTIF_CURL_GET,
