03e31834ef4bf231cae7d13137a6264f562c0e34
vkalintiris <vasilis@netdata.cloud>
2024-02-14 17:16:47 +0200

    Add license header

--- src/daemon/watcher.c
+++ src/daemon/watcher.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "daemon/common.h"
 #include "watcher.h"
 
--- src/daemon/watcher.h
+++ src/daemon/watcher.h
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #ifndef DAEMON_WATCHER_H
 #define DAEMON_WATCHER_H
 
