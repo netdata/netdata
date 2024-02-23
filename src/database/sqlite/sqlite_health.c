6a9c09e2f37f98e5d31162015707a168ab0bfce8
Costa Tsaousis <costa@netdata.cloud>
2024-02-23 02:07:08 +0200

    remaining

--- src/database/sqlite/sqlite_health.c
+++ src/database/sqlite/sqlite_health.c
@@ -966,10 +966,6 @@ int sql_alert_store_config(RRD_ALERT_PROTOTYPE *ap __maybe_unused)
     if (unlikely(rc != SQLITE_OK))
         goto bind_fail;
 
-    rc = SQLITE3_BIND_STRING_OR_NULL(res, ap->match.host, ++param);
-    if (unlikely(rc != SQLITE_OK))
-        goto bind_fail;
-
     rc = SQLITE3_BIND_STRING_OR_NULL(res, ap->config.lookup, ++param);
     if (unlikely(rc != SQLITE_OK))
         goto bind_fail;
