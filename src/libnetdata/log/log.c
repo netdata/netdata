9967b2900bb641d86cc7cef7da795f8e3eb62833
vkalintiris <vasilis@netdata.cloud>
2024-02-26 12:00:26 +0200

    Fix typo

--- src/libnetdata/log/log.c
+++ src/libnetdata/log/log.c
@@ -14,7 +14,7 @@
 #include <machine/endian.h>
 #endif
 
-#if !defined(ENABLE_SERNTRY) && defined(HAVE_BACKTRACE)
+#if !defined(ENABLE_SENTRY) && defined(HAVE_BACKTRACE)
 #include <execinfo.h>
 #endif
 
@@ -2334,7 +2334,7 @@ void netdata_logger_fatal( const char *file, const char *function, const unsigne
     char action_result[60+1];
     snprintfz(action_result, 60, "%s:%s", program_name, tag_to_send);
 
-#if !defined(ENABLE_SERNTRY) && defined(HAVE_BACKTRACE)
+#if !defined(ENABLE_SENTRY) && defined(HAVE_BACKTRACE)
     int fd = nd_log.sources[NDLS_DAEMON].fd;
     if(fd == -1)
         fd = STDERR_FILENO;
