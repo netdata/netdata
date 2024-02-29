f1f5b8adc94eb193741f04bbd244a6399f5377d4
thiagoftsm <thiagoftsm@gmail.com>
2024-02-29 01:51:11 +0000

    nv_ebpf: Add function to read data from eBPF table

--- src/libnetdata/maps/local-sockets.h
+++ src/libnetdata/maps/local-sockets.h
@@ -248,6 +248,58 @@ static inline void local_sockets_ebpf_selector(LS_STATE *ls) {
     if (ls->ebpf_module->maps && ls->ebpf_module->maps[NETWORK_VIEWER_EBPF_NV_SOCKET].map_fd != -1)
         ls->use_ebpf = true;
 }
+
+static inline bool local_sockets_ebpf_get_sockets(LS_STATE *ls) {
+    ebpf_nv_idx_t key =  { };
+    ebpf_nv_idx_t next_key = { };
+    ebpf_nv_data_t stored = {};
+    int fd = ls->ebpf_module->maps[NETWORK_VIEWER_EBPF_NV_SOCKET].map_fd;
+    while (!bpf_map_get_next_key(fd, &key, &next_key)) {
+        if (bpf_map_lookup_elem(fd, &key, &stored)) {
+            goto end_socket_read_loop;
+        }
+
+        LOCAL_SOCKET n = {
+            .inode = stored.ts,
+            .direction = SOCKET_DIRECTION_NONE,
+            .state = stored.state,
+            .local = {
+                .family = stored.family,
+                .protocol = stored.protocol,
+                .port = key.sport,
+             },
+            .remote = {
+                .family = stored.family,
+                .protocol = stored.protocol,
+                .port = key.dport,
+            },
+            .timer = stored.timer,
+            .retransmits = stored.retransmits,
+            .expires = stored.expires,
+            .rqueue = stored.rqueue,
+            .wqueue = stored.wqueue,
+            .uid = stored.uid,
+           };
+
+        if (stored.family == AF_INET) {
+            memcpy(&n.local.ip.ipv4, &key.saddr.ipv4, sizeof(n.local.ip.ipv4));
+            memcpy(&n.remote.ip.ipv4, &key.daddr.ipv4, sizeof(n.remote.ip.ipv4));
+        }
+        else if (stored.family == AF_INET6) {
+            memcpy(&n.local.ip.ipv6, &key.saddr.ipv6, sizeof(n.local.ip.ipv6));
+            memcpy(&n.remote.ip.ipv6, &key.daddr.ipv6, sizeof(n.remote.ip.ipv6));
+        }
+
+        strncpyz(n.comm, stored.name, sizeof(n.comm) - 1);
+
+end_socket_read_loop:
+        key = next_key;
+        // cleanup avoiding garbage from previous socket
+        memset(&stored, 0, sizeof(stored));
+    }
+    return true;
+}
+
 #endif // defined(ENABLE_PLUGIN_EBPF) && !defined(__cplusplus)
 
 // --------------------------------------------------------------------------------------------------------------------
@@ -1033,6 +1085,16 @@ static inline void local_sockets_read_sockets_from_proc(LS_STATE *ls) {
         snprintfz(path, sizeof(path), "%s/proc/self/ns/net", ls->config.host_prefix);
         local_sockets_read_proc_inode_link(ls, path, &ls->proc_self_net_ns_inode, "net");
     }
+
+#if defined(ENABLE_PLUGIN_EBPF) && !defined(__cplusplus)
+    if (ls->use_ebpf) {
+        ls->use_ebpf =  local_sockets_ebpf_get_sockets(ls);
+        /* Keeping commented while the rest of the algorithm is inserted to compare with other collections
+        if (ls->use_ebpf)
+            return;
+        */
+    }
+#endif
 
     if(ls->config.cmdline || ls->config.comm || ls->config.pid || ls->config.namespaces) {
         snprintfz(path, sizeof(path), "%s/proc", ls->config.host_prefix);
