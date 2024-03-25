288d311afdf3b9f7e4c0fcc5eb82d4031a4b609c
Ilya Mashchenko <ilya@netdata.cloud>
2024-03-25 21:34:10 +0200

    go.d: sd local-listeners: discover /proc/net/tcp6 only apps (#17252)

--- src/go/collectors/go.d.plugin/agent/discovery/sd/discoverer/netlisteners/netlisteners_test.go
+++ src/go/collectors/go.d.plugin/agent/discovery/sd/discoverer/netlisteners/netlisteners_test.go
@@ -21,6 +21,8 @@ func TestDiscoverer_Discover(t *testing.T) {
 				cli.addListener("TCP|0.0.0.0|8125|/opt/netdata/usr/sbin/netdata -P /run/netdata/netdata.pid -D")
 				cli.addListener("TCP|192.0.2.1|8125|/opt/netdata/usr/sbin/netdata -P /run/netdata/netdata.pid -D")
 				cli.addListener("UDP|127.0.0.1|53768|/opt/netdata/usr/libexec/netdata/plugins.d/go.d.plugin 1")
+				cli.addListener("TCP6|::|80|/usr/sbin/apache2 -k start")
+				cli.addListener("TCP|0.0.0.0|80|/usr/sbin/apache2 -k start")
 				time.Sleep(interval * 2)
 			},
 			wantGroups: []model.TargetGroup{&targetGroup{
@@ -28,20 +30,12 @@ func TestDiscoverer_Discover(t *testing.T) {
 				source:   "discoverer=net_listeners,host=localhost",
 				targets: []model.Target{
 					withHash(&target{
-						Protocol:  "UDP6",
-						IPAddress: "::1",
-						Port:      "8125",
-						Address:   "[::1]:8125",
-						Comm:      "netdata",
-						Cmdline:   "/opt/netdata/usr/sbin/netdata -P /run/netdata/netdata.pid -D",
-					}),
-					withHash(&target{
-						Protocol:  "TCP6",
-						IPAddress: "::1",
-						Port:      "8125",
-						Address:   "[::1]:8125",
-						Comm:      "netdata",
-						Cmdline:   "/opt/netdata/usr/sbin/netdata -P /run/netdata/netdata.pid -D",
+						Protocol:  "TCP",
+						IPAddress: "127.0.0.1",
+						Port:      "80",
+						Address:   "127.0.0.1:80",
+						Comm:      "apache2",
+						Cmdline:   "/usr/sbin/apache2 -k start",
 					}),
 					withHash(&target{
 						Protocol:  "TCP",
@@ -58,6 +52,14 @@ func TestDiscoverer_Discover(t *testing.T) {
 						Address:   "127.0.0.1:53768",
 						Comm:      "go.d.plugin",
 						Cmdline:   "/opt/netdata/usr/libexec/netdata/plugins.d/go.d.plugin 1",
+					}),
+					withHash(&target{
+						Protocol:  "UDP6",
+						IPAddress: "::1",
+						Port:      "8125",
+						Address:   "[::1]:8125",
+						Comm:      "netdata",
+						Cmdline:   "/opt/netdata/usr/sbin/netdata -P /run/netdata/netdata.pid -D",
 					}),
 				},
 			}},
@@ -79,14 +81,6 @@ func TestDiscoverer_Discover(t *testing.T) {
 				targets: []model.Target{
 					withHash(&target{
 						Protocol:  "UDP6",
-						IPAddress: "::1",
-						Port:      "8125",
-						Address:   "[::1]:8125",
-						Comm:      "netdata",
-						Cmdline:   "/opt/netdata/usr/sbin/netdata -P /run/netdata/netdata.pid -D",
-					}),
-					withHash(&target{
-						Protocol:  "TCP6",
 						IPAddress: "::1",
 						Port:      "8125",
 						Address:   "[::1]:8125",
@@ -127,14 +121,6 @@ func TestDiscoverer_Discover(t *testing.T) {
 				provider: "sd:net_listeners",
 				source:   "discoverer=net_listeners,host=localhost",
 				targets: []model.Target{
-					withHash(&target{
-						Protocol:  "TCP6",
-						IPAddress: "::1",
-						Port:      "8125",
-						Address:   "[::1]:8125",
-						Comm:      "netdata",
-						Cmdline:   "/opt/netdata/usr/sbin/netdata -P /run/netdata/netdata.pid -D",
-					}),
 					withHash(&target{
 						Protocol:  "TCP",
 						IPAddress: "127.0.0.1",
