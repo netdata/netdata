3f1accde00919c3c3e1382aba3c9f347b1dddccf
Costa Tsaousis <costa@netdata.cloud>
2024-04-02 11:52:40 +0300

    dyncfg function on parents should not require any access rights

--- src/daemon/config/dyncfg.c
+++ src/daemon/config/dyncfg.c
@@ -438,7 +438,7 @@ void dyncfg_add_streaming(BUFFER *wb) {
                    , 120
                    , "Dynamic configuration"
                    , "config"
-                   , (HTTP_ACCESS_FORMAT_CAST)(HTTP_ACCESS_SIGNED_ID | HTTP_ACCESS_SAME_SPACE | HTTP_ACCESS_SENSITIVE_DATA)
+                   , HTTP_ACCESS_ANONYMOUS_DATA
                    , 1000
     );
 }
