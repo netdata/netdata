d28ed6ccad6c2794690ad7dc37a47e99a8645cec
vkalintiris <vasilis@netdata.cloud>
2024-04-18 15:18:48 +0300

    timex is not supported on windows.

--- src/daemon/static_threads_macos.c
+++ src/daemon/static_threads_macos.c
@@ -3,8 +3,18 @@
 #include "common.h"
 
 extern void *macos_main(void *ptr);
+extern void *timex_main(void *ptr);
 
 const struct netdata_static_thread static_threads_macos[] = {
+    {
+        .name = "P[timex]",
+        .config_section = CONFIG_SECTION_PLUGINS,
+        .config_name = "timex",
+        .enabled = 1,
+        .thread = NULL,
+        .init_routine = NULL,
+        .start_routine = timex_main
+    },
     {
         .name = "P[macos]",
         .config_section = CONFIG_SECTION_PLUGINS,
