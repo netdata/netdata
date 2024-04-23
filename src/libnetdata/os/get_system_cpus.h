2663d243b10c0b17242835ab59ed6afe4c83e96e
Costa Tsaousis <costa@netdata.cloud>
2024-04-23 22:57:33 +0300

    fix missing functions

--- src/libnetdata/os/get_system_cpus.h
+++ src/libnetdata/os/get_system_cpus.h
@@ -3,7 +3,8 @@
 #ifndef NETDATA_GET_SYSTEM_CPUS_H
 #define NETDATA_GET_SYSTEM_CPUS_H
 
-#include <stdbool.h>
+#include "../libnetdata.h"
+
 long os_get_system_cpus_cached(bool cache, bool for_netdata);
 
 #endif //NETDATA_GET_SYSTEM_CPUS_H
