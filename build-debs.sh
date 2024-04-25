d33bc16c17002ff2d662490aad2dca4497dd9564
vkalintiris <vasilis@netdata.cloud>
2024-04-25 12:52:11 +0300

    Update extraction logic

--- build-debs.sh
+++ build-debs.sh
@@ -18,33 +18,9 @@ popd
 #
 pushd build/packages
 
-declare -A packages
-
-packages=(
-    [netdata]=netdata_6.7.8_amd64.deb
-    [apps]=netdata-plugin-apps_6.7.8_amd64.deb
-    [chartsd]=netdata-plugin-chartsd_6.7.8_all.deb
-    [cups]=netdata-plugin-cups_6.7.8_amd64.deb
-    [debugfs]=netdata-plugin-debugfs_6.7.8_amd64.deb
-    [ebpf_code_legacy]=netdata-ebpf-code-legacy_6.7.8_amd64.deb
-    [ebpf]=netdata-plugin-ebpf_6.7.8_amd64.deb
-    [freeipmi]=netdata-plugin-freeipmi_6.7.8_amd64.deb
-    [god]=netdata-plugin-go_6.7.8_amd64.deb
-    [logs_management]=netdata-plugin-logs-management_6.7.8_amd64.deb
-    [network_viewer]=netdata-plugin-network-viewer_6.7.8_amd64.deb
-    [nfacct]=netdata-plugin-nfacct_6.7.8_amd64.deb
-    [perf]=netdata-plugin-perf_6.7.8_amd64.deb
-    [pythond]=netdata-plugin-pythond_6.7.8_all.deb
-    [slabinfo]=netdata-plugin-slabinfo_6.7.8_amd64.deb
-    [systemd_journal]=netdata-plugin-systemd-journal_6.7.8_amd64.deb
-    [xenstat]=netdata-plugin-xenstat_6.7.8_amd64.deb
-)
-
-for key in "${!packages[@]}"; do
-    package=${packages[$key]}
-    if [ -e "${package}" ]; then
-        dpkg-deb -x "$package" rfs
-    fi
+for package in *.deb; do
+    dpkg-deb -R "$package" "${package%.*}"
+    dpkg-deb -x "$package" rfs
 done
 
 popd
