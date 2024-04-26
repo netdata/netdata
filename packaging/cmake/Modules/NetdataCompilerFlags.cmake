1ade55a681f7fa291bc13837c995c6cc2a1bd576
Austin S. Hemmelgarn <austin@netdata.cloud>
2024-04-26 12:17:14 +0300

    Switch compiler flag handling to (mostly) use properties.
    This makes it easier to override individual flags on a per-target
    basis and also results in slightly simpler CMake code.

--- packaging/cmake/Modules/NetdataCompilerFlags.cmake
+++ packaging/cmake/Modules/NetdataCompilerFlags.cmake
@@ -20,30 +20,29 @@ endfunction()
 #
 # If the language flags already match the `match` argument, skip this flag.
 # Otherwise, check for support for `flag` and if support is found, add it to
-# the language-specific `target` flag group.
-function(add_simple_extra_compiler_flag match flag target)
+# the compiler flags for the run.
+function(add_simple_extra_compiler_flag match flag)
         set(CMAKE_REQUIRED_FLAGS "-Werror")
 
         make_cpp_safe_name("${flag}" flag_name)
 
         if(NOT ${CMAKE_C_FLAGS} MATCHES ${match})
                 check_c_compiler_flag("${flag}" HAVE_C_${flag_name})
-                if(HAVE_C_${flag_name})
-                        set(${target}_C_FLAGS "${${target}_C_FLAGS} ${flag}" PARENT_SCOPE)
-                endif()
         endif()
 
         if(NOT ${CMAKE_CXX_FLAGS} MATCHES ${match})
                 check_cxx_compiler_flag("${flag}" HAVE_CXX_${flag_name})
-                if(HAVE_CXX_${flag_name})
-                        set(${target}_CXX_FLAGS "${${target}_CXX_FLAGS} ${flag}" PARENT_SCOPE)
-                endif()
+        endif()
+
+        if(${HAVE_C_${flag_name}} AND ${HAVE_CXX_${flag_name}})
+            add_compile_options("${flag}")
+            add_link_options("${flag}")
         endif()
 endfunction()
 
 # Same as add_simple_extra_compiler_flag, but check for a second flag if the
 # first one is unsupported.
-function(add_double_extra_compiler_flag match flag1 flag2 target)
+function(add_double_extra_compiler_flag match flag1 flag2)
         set(CMAKE_REQUIRED_FLAGS "-Werror")
 
         make_cpp_safe_name("${flag1}" flag1_name)
@@ -51,26 +50,24 @@ function(add_double_extra_compiler_flag match flag1 flag2 target)
 
         if(NOT ${CMAKE_C_FLAGS} MATCHES ${match})
                 check_c_compiler_flag("${flag1}" HAVE_C_${flag1_name})
-                if(HAVE_C_${flag1_name})
-                        set(${target}_C_FLAGS "${${target}_C_FLAGS} ${flag1}" PARENT_SCOPE)
-                else()
+                if(NOT HAVE_C_${flag1_name})
                         check_c_compiler_flag("${flag2}" HAVE_C_${flag2_name})
-                        if(HAVE_C_${flag2_name})
-                                set(${target}_C_FLAGS "${${target}_C_FLAGS} ${flag2}" PARENT_SCOPE)
-                        endif()
                 endif()
         endif()
 
         if(NOT ${CMAKE_CXX_FLAGS} MATCHES ${match})
                 check_cxx_compiler_flag("${flag1}" HAVE_CXX_${flag1_name})
-                if(HAVE_CXX_${flag1_name})
-                        set(${target}_CXX_FLAGS "${${target}_CXX_FLAGS} ${flag1}" PARENT_SCOPE)
-                else()
+                if(NOT HAVE_CXX_${flag1_name})
                         check_cxx_compiler_flag("${flag2}" HAVE_CXX_${flag2_name})
-                        if(HAVE_CXX_${flag2_name})
-                                set(${target}_CXX_FLAGS "${${target}_CXX_FLAGS} ${flag2}" PARENT_SCOPE)
-                        endif()
                 endif()
+        endif()
+
+        if(${HAVE_C_${flag1_name}} AND ${HAVE_CXX_${flag1_name}})
+            add_compile_options("${flag1}")
+            add_link_options("${flag1}")
+        elseif(${HAVE_C_${flag2_name}} AND ${HAVE_CXX${flag2_name}})
+            add_compile_options("${flag2}")
+            add_link_options("${flag2}")
         endif()
 endfunction()
 
@@ -87,31 +84,19 @@ if(ENABLE_ADDRESS_SANITIZER)
         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=address")
 endif()
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fexceptions")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_C_FLAGS}")
 
-set(EXTRA_HARDENING_C_FLAGS "")
-set(EXTRA_HARDENING_CXX_FLAGS "")
-
-set(EXTRA_OPT_C_FLAGS "")
-set(EXTRA_OPT_CXX_FLAGS "")
-
 if(NOT ${DISABLE_HARDENING})
-        add_double_extra_compiler_flag("stack-protector" "-fstack-protector-strong" "-fstack-protector" EXTRA_HARDENING)
-        add_double_extra_compiler_flag("_FORTIFY_SOURCE" "-D_FORTIFY_SOURCE=3" "-D_FORTIFY_SOURCE=2" EXTRA_HARDENING)
-        add_simple_extra_compiler_flag("stack-clash-protection" "-fstack-clash-protection" EXTRA_HARDENING)
-        add_simple_extra_compiler_flag("-fcf-protection" "-fcf-protection=full" EXTRA_HARDENING)
-        add_simple_extra_compiler_flag("branch-protection" "-mbranch-protection=standard" EXTRA_HARDENING)
+        add_double_extra_compiler_flag("stack-protector" "-fstack-protector-strong" "-fstack-protector")
+        add_double_extra_compiler_flag("_FORTIFY_SOURCE" "-D_FORTIFY_SOURCE=3" "-D_FORTIFY_SOURCE=2")
+        add_simple_extra_compiler_flag("stack-clash-protection" "-fstack-clash-protection")
+        add_simple_extra_compiler_flag("-fcf-protection" "-fcf-protection=full")
+        add_simple_extra_compiler_flag("branch-protection" "-mbranch-protection=standard")
 endif()
 
 foreach(FLAG function-sections data-sections)
-        add_simple_extra_compiler_flag("${FLAG}" "-f${FLAG}" EXTRA_OPT)
+        add_simple_extra_compiler_flag("${FLAG}" "-f${FLAG}")
 endforeach()
 
-add_simple_extra_compiler_flag("-Wbuiltin-macro-redefined" "-Wno-builtin-macro-redefined" EXTRA_OPT)
-
-foreach(RELTYP RELEASE DEBUG RELWITHDEBINFO MINSIZEREL)
-        foreach(L C CXX)
-                set(CMAKE_${L}_FLAGS_${RELTYP} "${CMAKE_${L}_FLAGS_${RELTYP}} ${EXTRA_HARDENING_C_FLAGS} ${EXTRA_OPT_C_FLAGS}")
-        endforeach()
-endforeach()
+add_simple_extra_compiler_flag("-Wbuiltin-macro-redefined" "-Wno-builtin-macro-redefined")
+add_simple_extra_compiler_flag("-fexecptions" "-fexceptions")
