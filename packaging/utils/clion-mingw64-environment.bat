ae44bfef1ea49d9f55fba947d1f8b58118c17757
Costa Tsaousis <costa@netdata.cloud>
2024-05-07 02:42:40 +0300

    enable compilation on windows clion

---
+++ packaging/utils/clion-mingw64-environment.bat
@@ -0,0 +1,7 @@
+@set MSYSTEM=MINGW64
+@set PATH="C:\msys64\mingw64\bin;C:\msys64\bin;C:\msys64\usr\bin;%PATH%"
+::@set PATH="C:\msys64\usr\local\bin;C:\msys64\usr\bin;C:\msys64\bin;%PATH%"
+@set PKG_CONFIG_EXECUTABLE=C:\msys64\mingw64\bin\pkg-config.exe
+@set CMAKE_C_COMPILER=C:\msys64\mingw64\bin\gcc.exe
+@set CMAKE_CC_COMPILER=C:\msys64\mingw64\bin\g++.exe
+@set PROTOBUF_PROTOC_EXECUTABLE=C:\msys64\mingw64\bin\protoc.exe
