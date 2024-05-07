5ea0866f04aab2110ee0c8134208406ece5a87dd
Costa Tsaousis <costa@netdata.cloud>
2024-05-07 17:37:59 +0300

    cleanup and docs

--- packaging/utils/bash_execute.sh
+++ packaging/utils/bash_execute.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/bash
 
-#export MSYSTEM=MSYS
-#export PATH="/usr/bin:${PATH}"
-
-source /etc/profile
-
 convert_path() {
     local ARG="$1"
     ARG="${ARG//C:\\//c/}"
--- packaging/utils/protoc.bat
+++ packaging/utils/protoc.bat
@@ -1,7 +1,9 @@
 @echo off
-set MSYSTEM=MSYS
-set PATH=c:\msys64\usr\bin;%PATH%"
-
+::
+:: The problem with /usr/bin/protoc is that it accepts colon separated (:) paths at its parameters.
+:: This makes C:/ being parsed as 2 paths: C and /, which of course both fail.
+:: To overcome this problem, we use bash_execute.sh, which replaces all occurences of C: with /c.
+::
 set "batch_dir=%~dp0"
 set "batch_dir=%batch_dir:\=/%"
-bash %batch_dir%/bash_execute.sh protoc %*
+C:\msys64\usr\bin\bash.exe %batch_dir%/bash_execute.sh protoc %*
