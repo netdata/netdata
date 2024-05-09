ee1969a4a80641fa084d4a2c30bc2c8f8680d6df
Ilya Mashchenko <ilya@netdata.cloud>
2024-05-09 17:11:22 +0300

    go.d isc_dhcpd create a chart for each pool (#17629)

--- src/go/collectors/go.d.plugin/modules/isc_dhcpd/charts.go
+++ src/go/collectors/go.d.plugin/modules/isc_dhcpd/charts.go
@@ -6,30 +6,52 @@ import (
 	"github.com/netdata/netdata/go/go.d.plugin/agent/module"
 )
 
+const (
+	prioLeasesTotal = module.Priority + iota
+
+	prioDHCPPoolUtilization
+	prioDHCPPoolActiveLeases
+)
+
 var activeLeasesTotalChart = module.Chart{
-	ID:    "active_leases_total",
-	Title: "Active Leases Total",
-	Units: "leases",
-	Fam:   "summary",
-	Ctx:   "isc_dhcpd.active_leases_total",
+	ID:       "active_leases_total",
+	Title:    "Active Leases Total",
+	Units:    "leases",
+	Fam:      "summary",
+	Ctx:      "isc_dhcpd.active_leases_total",
+	Priority: prioLeasesTotal,
 	Dims: module.Dims{
 		{ID: "active_leases_total", Name: "active"},
 	},
 }
 
+var dhcpPoolChartsTmpl = module.Charts{
+	dhcpPoolActiveLeasesChartTmpl.Copy(),
+	dhcpPoolUtilizationChartTmpl.Copy(),
+}
+
 var (
-	poolActiveLeasesChart = module.Chart{
-		ID:    "pool_active_leases",
-		Title: "Pool Active Leases",
-		Units: "leases",
-		Fam:   "pools",
-		Ctx:   "isc_dhcpd.pool_active_leases",
+	dhcpPoolUtilizationChartTmpl = module.Chart{
+		ID:       "dhcp_pool_%s_utilization",
+		Title:    "DHCP Pool Utilization",
+		Units:    "percent",
+		Fam:      "pools",
+		Ctx:      "isc_dhcpd.dhcp_pool_utilization",
+		Priority: prioDHCPPoolUtilization,
+		Type:     module.Area,
+		Dims: module.Dims{
+			{ID: "dhcp_pool_%s_utilization", Name: "utilization"},
+		},
 	}
-	poolUtilizationChart = module.Chart{
-		ID:    "pool_utilization",
-		Title: "Pool Utilization",
-		Units: "percentage",
-		Fam:   "pools",
-		Ctx:   "isc_dhcpd.pool_utilization",
+	dhcpPoolActiveLeasesChartTmpl = module.Chart{
+		ID:       "dhcp_pool_%s_active_leases",
+		Title:    "DHCP Pool Active Leases",
+		Units:    "leases",
+		Fam:      "pools",
+		Ctx:      "isc_dhcpd.dhcp_pool_active_leases",
+		Priority: prioDHCPPoolActiveLeases,
+		Dims: module.Dims{
+			{ID: "dhcp_pool_%s_active_leases", Name: "active"},
+		},
 	}
 )
--- src/go/collectors/go.d.plugin/modules/isc_dhcpd/init.go
+++ src/go/collectors/go.d.plugin/modules/isc_dhcpd/init.go
@@ -5,6 +5,7 @@ package isc_dhcpd
 import (
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/netdata/netdata/go/go.d.plugin/agent/module"
 	"github.com/netdata/netdata/go/go.d.plugin/pkg/iprange"
@@ -35,18 +36,20 @@ func (d *DHCPd) validateConfig() error {
 
 func (d *DHCPd) initPools() ([]ipPool, error) {
 	var pools []ipPool
+
 	for i, cfg := range d.Pools {
-		rs, err := iprange.ParseRanges(cfg.Networks)
+		ipRange, err := iprange.ParseRanges(cfg.Networks)
 		if err != nil {
 			return nil, fmt.Errorf("parse pools[%d]->pool.networks '%s' ('%s'): %v", i+1, cfg.Name, cfg.Networks, err)
 		}
-		if len(rs) != 0 {
-			pools = append(pools, ipPool{
-				name:      cfg.Name,
-				addresses: rs,
-			})
+		if len(ipRange) == 0 {
+			continue
 		}
+
+		pool := ipPool{name: cfg.Name, addresses: ipRange}
+		pools = append(pools, pool)
 	}
+
 	return pools, nil
 }
 
@@ -57,34 +60,29 @@ func (d *DHCPd) initCharts(pools []ipPool) (*module.Charts, error) {
 		return nil, err
 	}
 
-	chart := poolActiveLeasesChart.Copy()
-	if err := charts.Add(chart); err != nil {
-		return nil, err
-	}
 	for _, pool := range pools {
-		dim := &module.Dim{
-			ID:   "pool_" + pool.name + "_active_leases",
-			Name: pool.name,
-		}
-		if err := chart.AddDim(dim); err != nil {
-			return nil, err
-		}
-	}
+		poolCharts := dhcpPoolChartsTmpl.Copy()
 
-	chart = poolUtilizationChart.Copy()
-	if err := charts.Add(chart); err != nil {
-		return nil, err
-	}
-	for _, pool := range pools {
-		dim := &module.Dim{
-			ID:   "pool_" + pool.name + "_utilization",
-			Name: pool.name,
-			Div:  precision,
+		for _, chart := range *poolCharts {
+			chart.ID = fmt.Sprintf(chart.ID, cleanPoolNameForChart(pool.name))
+			chart.Labels = []module.Label{
+				{Key: "dhcp_pool_name", Value: pool.name},
+			}
+			for _, dim := range chart.Dims {
+				dim.ID = fmt.Sprintf(dim.ID, pool.name)
+			}
 		}
-		if err := chart.AddDim(dim); err != nil {
+
+		if err := charts.Add(*poolCharts...); err != nil {
 			return nil, err
 		}
 	}
 
 	return charts, nil
+}
+
+func cleanPoolNameForChart(name string) string {
+	name = strings.ReplaceAll(name, " ", "_")
+	name = strings.ReplaceAll(name, ".", "_")
+	return name
 }
