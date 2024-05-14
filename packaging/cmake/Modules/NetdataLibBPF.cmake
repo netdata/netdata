3e798ebbf846928259a51b1de99dce6efa5e7be6
thiagoftsm <thiagoftsm@gmail.com>
2024-05-14 01:59:55 +0000

    update_libbpf: Update Libbpf

--- packaging/cmake/Modules/NetdataLibBPF.cmake
+++ packaging/cmake/Modules/NetdataLibBPF.cmake
@@ -31,7 +31,7 @@ function(netdata_bundle_libbpf)
     if(USE_LEGACY_LIBBPF)
         set(_libbpf_tag 673424c56127bb556e64095f41fd60c26f9083ec) # v0.0.9_netdata-1
     else()
-        set(_libbpf_tag b39b7f426f276dcfa5761f62c58fab88d7edb8e2) # v1.4.1p_netdata
+        set(_libbpf_tag 89aecd2188fa525718cb88d2777ba0e08defd286) # v1.4.2p_netdata
     endif()
 
     if(DEFINED BUILD_SHARED_LIBS)
