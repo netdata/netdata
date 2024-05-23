223f335f99acebd2c4ed71034d2f0b63b7516d58
thiagoftsm <thiagoftsm@gmail.com>
2024-05-23 22:48:18 +0000

    ebpf_oomkill: Fix charts

--- src/collectors/common-contexts/system.processes.h
+++ src/collectors/common-contexts/system.processes.h
@@ -38,17 +38,30 @@ static inline void common_system_processes(uint64_t running, int update_every) {
 
 // EBPF COUNTER PART
 static inline void common_system_threads(uint64_t threads, int update_every) {
-    static RRDSET *st_processes = NULL;
+    static RRDSET *st_threads = NULL;
     static RRDDIM *rd_threads = NULL;
 
-    if(unlikely(!st_processes)) {
-        st_processes = _system_process_chart();
+    if(unlikely(!st_threads)) {
+        st_threads = rrdset_create_localhost(
+            "system"
+            , "threads"
+            , NULL
+            , "processes"
+            , NULL
+            , "Threads"
+            , "threads"
+            , _COMMON_PLUGIN_NAME
+            , _COMMON_PLUGIN_MODULE_NAME
+            , NETDATA_CHART_PRIO_WINDOWS_THREADS
+            , update_every
+            , RRDSET_TYPE_LINE
+            );
 
         rd_threads = rrddim_add(st_processes, "threads", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
     }
 
-    rrddim_set_by_pointer(st_processes, rd_threads, threads);
-    rrdset_done(st_processes);
+    rrddim_set_by_pointer(st_threads, rd_threads, threads);
+    rrdset_done(st_threads);
 }
 #endif
 
@@ -59,20 +72,7 @@ static inline void common_system_processes(uint64_t running, uint64_t blocked, i
     static RRDDIM *rd_blocked = NULL;
 
     if(unlikely(!st_processes)) {
-        st_processes = rrdset_create_localhost(
-          "system"
-        , "processes"
-        , NULL
-        , "processes"
-        , NULL
-        , "System Processes"
-        , "processes"
-        , _COMMON_PLUGIN_NAME
-        , _COMMON_PLUGIN_MODULE_NAME
-        , NETDATA_CHART_PRIO_SYSTEM_PROCESSES
-        , update_every
-        , RRDSET_TYPE_LINE
-        );
+        st_processes = _system_process_chart();
 
         rd_running = rrddim_add(st_processes, "running", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
         rd_blocked = rrddim_add(st_processes, "blocked", NULL, -1, 1, RRD_ALGORITHM_ABSOLUTE);
