0ef074fcad627e890f74f1ee83f1da9ec4ac84b8
thiagoftsm <thiagoftsm@gmail.com>
2024-05-26 00:35:21 +0000

    ebpf_oomkill: Improve and reorganize algorithm

--- src/collectors/ebpf.plugin/ebpf_oomkill.c
+++ src/collectors/ebpf.plugin/ebpf_oomkill.c
@@ -164,8 +164,10 @@ static void oomkill_write_data(int32_t *keys, uint32_t total)
 {
     // for each app, see if it was OOM killed. record as 1 if so otherwise 0.
     struct ebpf_target *w;
+    uint32_t used_pid = 0;
+    pthread_mutex_lock(&collect_data_mutex);
     for (w = apps_groups_root_target; w != NULL; w = w->next) {
-        if (unlikely(!(w->charts_created & (1<<EBPF_MODULE_OOMKILL_IDX))))
+        if (unlikely(!(w->charts_created & (1 << EBPF_MODULE_OOMKILL_IDX))))
             continue;
 
         bool was_oomkilled = false;
@@ -175,6 +177,7 @@ static void oomkill_write_data(int32_t *keys, uint32_t total)
                 uint32_t j;
                 for (j = 0; j < total; j++) {
                     if (pids->pid == keys[j]) {
+                        used_pid++;
                         was_oomkilled = true;
                         // set to 0 so we consider it "done".
                         keys[j] = 0;
@@ -190,22 +193,26 @@ write_dim:
         ebpf_write_end_chart();
     }
 
-    // for any remaining keys for which we couldn't find a group, this could be
-    // for various reasons, but the primary one is that the PID has not yet
-    // been picked up by the process thread when parsing the proc filesystem.
-    // since it's been OOM killed, it will never be parsed in the future, so
-    // we have no choice but to dump it into `other`.
-    uint32_t j;
-    uint32_t rem_count = 0;
-    for (j = 0; j < total; j++) {
-        int32_t key = keys[j];
-        if (key != 0) {
-            rem_count += 1;
+    if (total != used_pid) {
+        // for any remaining keys for which we couldn't find a group, this could be
+        // for various reasons, but the primary one is that the PID has not yet
+        // been picked up by the process thread when parsing the proc filesystem.
+        // since it's been OOM killed, it will never be parsed in the future, so
+        // we have no choice but to dump it into `other`.
+        uint32_t j;
+        uint32_t rem_count = 0;
+        for (j = 0; j < total; j++) {
+            int32_t key = keys[j];
+            if (key != 0) {
+                rem_count += 1;
+            }
+        }
+        if (rem_count > 0) {
+            write_chart_dimension("other", rem_count);
         }
     }
-    if (rem_count > 0) {
-        write_chart_dimension("other", rem_count);
-    }
+
+    pthread_mutex_unlock(&collect_data_mutex);
 }
 
 /**
@@ -369,6 +376,7 @@ static uint32_t oomkill_read_data(int32_t *keys)
     uint32_t curr_key = 0;
     uint32_t key = 0;
     int mapfd = oomkill_maps[OOMKILL_MAP_KILLCNT].map_fd;
+    int limit = NETDATA_OOMKILL_MAX_ENTRIES -1;
     while (bpf_map_get_next_key(mapfd, &curr_key, &key) == 0) {
         curr_key = key;
 
@@ -383,6 +391,8 @@ static uint32_t oomkill_read_data(int32_t *keys)
             // impossible to get this condition.
             netdata_log_error("key unexpectedly not available for deletion.");
         }
+        if (i > limit)
+            break;
     }
 
     return i;
@@ -399,6 +409,7 @@ static uint32_t oomkill_read_data(int32_t *keys)
 static void ebpf_update_oomkill_cgroup(int32_t *keys, uint32_t total)
 {
     ebpf_cgroup_target_t *ect;
+    pthread_mutex_lock(&mutex_cgroup_shm);
     for (ect = ebpf_cgroup_pids; ect; ect = ect->next) {
         ect->oomkill = 0;
         struct pid_on_target2 *pids;
@@ -413,6 +424,7 @@ static void ebpf_update_oomkill_cgroup(int32_t *keys, uint32_t total)
             }
         }
     }
+    pthread_mutex_unlock(&mutex_cgroup_shm);
 }
 
 /**
@@ -470,18 +482,18 @@ static void oomkill_collector(ebpf_module_t *em)
         stats[NETDATA_CONTROLLER_PID_TABLE_ADD] += (uint64_t) count;
         stats[NETDATA_CONTROLLER_PID_TABLE_DEL] += (uint64_t) count;
 
-        pthread_mutex_lock(&lock);
-        if (cgroups && shm_ebpf_cgroup.header) {
+        if (cgroups && shm_ebpf_cgroup.header)
             ebpf_update_oomkill_cgroup(keys, count);
-            // write everything from the ebpf map.
-            ebpf_oomkill_send_cgroup_data(update_every);
-        }
 
-        if (em->apps_charts & NETDATA_EBPF_APPS_FLAG_CHART_CREATED) {
-            pthread_mutex_lock(&collect_data_mutex);
+        netdata_apps_integration_flags_t apps = em->apps_charts;
+        pthread_mutex_lock(&lock);
+        if (apps & NETDATA_EBPF_APPS_FLAG_CHART_CREATED)
             oomkill_write_data(keys, count);
-            pthread_mutex_unlock(&collect_data_mutex);
-        }
+
+        // write everything from the ebpf map.
+        if (cgroups && shm_ebpf_cgroup.header)
+            ebpf_oomkill_send_cgroup_data(update_every);
+
         pthread_mutex_unlock(&lock);
 
         running_time = ebpf_update_oomkill_period(running_time, em);
@@ -517,7 +529,9 @@ void ebpf_oomkill_create_apps_charts(struct ebpf_module *em, void *ptr)
                              NETDATA_EBPF_MODULE_NAME_OOMKILL);
         ebpf_create_chart_labels("app_group", w->name, RRDLABEL_SRC_AUTO);
         ebpf_commit_label();
-        fprintf(stdout, "DIMENSION kills '' %s 1 1\n", ebpf_algorithms[NETDATA_EBPF_ABSOLUTE_IDX]);
+        fprintf(stdout, "DIMENSION '%s' '' %s 1 1\n",
+                EBPF_COMMON_DIMENSION_KILLS,
+                ebpf_algorithms[NETDATA_EBPF_ABSOLUTE_IDX]);
 
         w->charts_created |= 1<<EBPF_MODULE_OOMKILL_IDX;
     }
