3f97b15afa187fd96aede776ea13f93083317bc7
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2024-06-09 22:21:26 +0300

    Do not query disk space for non dbengine tier

--- src/database/contexts/api_v2.c
+++ src/database/contexts/api_v2.c
@@ -1183,7 +1183,7 @@ void buffer_json_agents_v2(BUFFER *wb, struct query_timings *timings, time_t now
             group_seconds *= storage_tiers_grouping_iterations[tier];
             uint64_t max = storage_engine_disk_space_max(eng->seb, localhost->db[tier].si);
 #ifdef ENABLE_DBENGINE
-            if (!max)
+            if (!max && eng->seb == STORAGE_ENGINE_BACKEND_DBENGINE)
                 max = get_directory_free_bytes_space(multidb_ctx[tier]);
 #endif
             uint64_t used = storage_engine_disk_space_used(eng->seb, localhost->db[tier].si);
