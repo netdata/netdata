741c1596da1bde8a4965899e22a694ac0ac3c330
Austin S. Hemmelgarn <austin@netdata.cloud>
2024-06-17 10:20:17 -0400

    Fix CMake handling for old RPM distros.

--- packaging/repoconfig/build-rpm.sh
+++ packaging/repoconfig/build-rpm.sh
@@ -11,7 +11,20 @@ if command -v dnf > /dev/null ; then
     dnf install -y --nodocs --setopt=install_weak_deps=False rpm-build cmake make || exit 1
 elif command -v yum > /dev/null ; then
     yum distro-sync -y || exit 1
-    yum install -y rpm-build cmake make || exit 1
+    yum install -y rpm-build make || exit 1
+    curl --fail -sSL --connect-timeout 20 --retry 3 --output "cmake-linux-$(uname -m).sh" "https://github.com/Kitware/CMake/releases/download/v3.27.6/cmake-3.27.6-linux-$(uname -m).sh" && \
+    if [ "$(uname -m)" = "x86_64" ]; then \
+        echo '8c449dabb2b2563ec4e6d5e0fb0ae09e729680efab71527b59015131cea4a042  cmake-linux-x86_64.sh' | sha256sum -c - ; \
+    elif [ "$(uname -m)" = "aarch64" ]; then \
+        echo 'a83e01ed1cdf44c2e33e0726513b9a35a8c09e3b5a126fd720b3c8a9d5552368  cmake-linux-aarch64.sh' | sha256sum -c - ; \
+    else \
+        echo "ARCH NOT SUPPORTED BY CMAKE" ; \
+        exit 1 ; \
+    fi && \
+    chmod +x "./cmake-linux-$(uname -m).sh" && \
+    mkdir -p /cmake && \
+    "./cmake-linux-$(uname -m).sh" --skip-license --prefix=/cmake
+    PATH="/cmake/bin:${PATH}"
 elif command -v zypper > /dev/null ; then
     zypper update -y || exit 1
     zypper install -y rpm-build cmake make || exit 1
@@ -19,16 +32,8 @@ fi
 echo "::endgroup::"
 
 echo "::group::Building Packages"
-if [ "$(rpm --eval '%{centos}' | cut -f 1 -d '.')" -lt 8 ]; then
-    mkdir -p "${BUILD_DIR}"
-    cp -rv "${SRC_DIR}" "${BUILD_DIR}"
-    cd "${BUILD_DIR}"
-    cmake .
-    cmake --build .
-else
-    cmake -S "${SRC_DIR}" -B "${BUILD_DIR}"
-    cmake --build "${BUILD_DIR}"
-fi
+cmake -S "${SRC_DIR}" -B "${BUILD_DIR}"
+cmake --build "${BUILD_DIR}"
 
 cd "${BUILD_DIR}"
 cpack -G RPM
