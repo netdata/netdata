07dad925c2718f214411734e1bde5e8bc90fac8f
Vasileios Kalintiris <vasilis@netdata.cloud>
2024-06-20 15:47:52 +0300

    Print tag when we fail to join a thread.

--- src/libnetdata/threads/threads.c
+++ src/libnetdata/threads/threads.c
@@ -423,8 +423,9 @@ int nd_thread_join(ND_THREAD *nti) {
         return ESRCH;
 
     int ret = pthread_join(nti->thread, NULL);
-    if(ret != 0)
-        nd_log(NDLS_DAEMON, NDLP_WARNING, "cannot join thread. pthread_join() failed with code %d.", ret);
+    if(ret != 0) {
+        nd_log(NDLS_DAEMON, NDLP_WARNING, "cannot join thread. pthread_join() failed with code %d. (tag=%s)", ret, nti->tag);
+    }
     else {
         nd_thread_status_set(nti, NETDATA_THREAD_STATUS_JOINED);
 
