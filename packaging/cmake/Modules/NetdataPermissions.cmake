d0524322eb9201caeda4a6a7b400c11f6cac2329
Austin S. Hemmelgarn <austin@netdata.cloud>
2024-06-27 08:50:48 -0400

    Integrate support for static builds.

--- packaging/cmake/Modules/NetdataPermissions.cmake
+++ packaging/cmake/Modules/NetdataPermissions.cmake
@@ -8,32 +8,6 @@ set(_nd_perms_list_file "${CMAKE_BINARY_DIR}/extra-perms-list")
 set(_nd_perms_hooks_dir "${CMAKE_BINARY_DIR}/extra-perms-hooks/")
 file(REMOVE "${_nd_perms_list_file}")
 
-# Extract the path from a supplementary permissions entry.
-function(_nd_extract_path var entry)
-  string(REGEX MATCH "^.+::" entry_path "${entry}")
-  string(REGEX REPLACE "::$" "" entry_path "${entry_path}")
-  string(REGEX MATCH "^.+::" entry_path "${entry_path}")
-  string(REPLACE "::" "" entry_path "${entry_path}")
-  set(${var} "${entry_path}" PARENT_SCOPE)
-endfunction()
-
-# Extract the component from a supplementary permissions entry.
-function(_nd_extract_component var entry)
-  string(REGEX MATCH "::.+::" entry_component "${entry}")
-  string(REPLACE "::" "" entry_component "${entry_component}")
-  set(${var} "${entry_component}" PARENT_SCOPE)
-endfunction()
-
-# Extract the permissions list from a supplementary permissions entry.
-function(_nd_extract_permissions var entry)
-  string(REGEX MATCH "::.+$" entry_perms "${entry}")
-  string(REGEX REPLACE "^::" "" entry_perms "${entry_perms}")
-  string(REGEX MATCH "::.+$" entry_perms "${entry_perms}")
-  string(REPLACE "::" "" entry_perms "${entry_perms}")
-  string(REPLACE "," ";" entry_perms "${entry_perms}")
-  set(${var} "${entry_perms}" PARENT_SCOPE)
-endfunction()
-
 # Add the requested additional permissions to the specified path in the
 # specified component.
 #
@@ -72,6 +46,80 @@ function(netdata_add_permissions)
   file(APPEND "${_nd_perms_list_file}" "${nd_perms_PATH}::${nd_perms_COMPONENT}::${nd_perms_PERMISSIONS_ITEMS}\n")
 endfunction()
 
+# Extract the path from a supplementary permissions entry.
+function(_nd_extract_path var entry)
+  string(REGEX MATCH "^.+::" entry_path "${entry}")
+  string(REGEX REPLACE "::$" "" entry_path "${entry_path}")
+  string(REGEX MATCH "^.+::" entry_path "${entry_path}")
+  string(REPLACE "::" "" entry_path "${entry_path}")
+  set(${var} "${entry_path}" PARENT_SCOPE)
+endfunction()
+
+# Extract the component from a supplementary permissions entry.
+function(_nd_extract_component var entry)
+  string(REGEX MATCH "::.+::" entry_component "${entry}")
+  string(REPLACE "::" "" entry_component "${entry_component}")
+  set(${var} "${entry_component}" PARENT_SCOPE)
+endfunction()
+
+# Extract the permissions list from a supplementary permissions entry.
+function(_nd_extract_permissions var entry)
+  string(REGEX MATCH "::.+$" entry_perms "${entry}")
+  string(REGEX REPLACE "^::" "" entry_perms "${entry_perms}")
+  string(REGEX MATCH "::.+$" entry_perms "${entry_perms}")
+  string(REPLACE "::" "" entry_perms "${entry_perms}")
+  string(REPLACE "," ";" entry_perms "${entry_perms}")
+  set(${var} "${entry_perms}" PARENT_SCOPE)
+endfunction()
+
+# Add shell script to the specified variable to handle marking the
+# specified path SUID.
+function(_nd_perms_mark_path_suid var path)
+  set(tmp_var "${var}chown -f 'root:${NETDATA_USER}' '${path}'\n")
+  set(tmp_var "${tmp_var}chmod -f 4750 '${path}'\n")
+  set(${var} "${tmp_var}" PARENT_SCOPE)
+endfunction()
+
+# Add shell script to the specified variable to handle marking the
+# specified path with the specified filecaps.
+function(_nd_perms_mark_path_filecaps var path capset)
+  set(tmp_var "${var}chown -f 'root:${NETDATA_USER}' '${path}'\n")
+  set(tmp_var "${tmpvar}chmod -f 0750 '${path}'\n")
+  set(tmp_var "${tmp_var}if ! capset '${capset}' '${path}' 2>/dev/null; then\n")
+
+  if(capset MATCHES "perfmon")
+    string(REPLACE "perfmon" "sys_admin" capset2 "${capset}")
+    set(tmp_var "${tmp_var}    if ! capset '${capset2}' '${entry_path}' 2>/dev/null; then\n")
+    set(tmp_var "${tmp_var}        chmod -f 4750 '${entry_path}'\n")
+    set(tmp_var "${tmp_var}    fi\n")
+    set(tmp_var "${tmp_var}fi\n")
+  else()
+    set(tmp_var "${tmp_var}    chmod -f 4750 '${entry_path}'\n")
+    set(tmp_var "${tmp_var}fi\n")
+  endif()
+
+  set(${var} "${tmp_var}" PARENT_SCOPE)
+endfunction()
+
+# Add shell script for the specified permissions entry to the specified
+# variable.
+function(_nd_perms_generate_entry_script var entry)
+  _nd_extract_path(entry_path "${item}")
+  _nd_extract_permissions(entry_perms "${item}")
+
+  list(TRANSFORM entry_perms PREPEND "cap_")
+  list(JOIN entry_perms "," capset)
+  set(capset "${capset}+eip")
+
+  if("${entry_perms}" STREQUAL "suid" OR NOT USE_FILE_CAPABILITIES)
+    _nd_perms_mark_path_suid(result "${entry_path}")
+  else()
+    _nd_perms_mark_path_filecaps(result "${entry_path}" "${capset}")
+  endif()
+
+  set(${var} "${var}${result}" PARENT_SCOPE)
+endfunction()
+
 # Prepare an install hook for the specified path in the specified component
 # with the specified permissions.
 function(nd_perms_generate_cmake_install_hook path component perms)
@@ -101,35 +149,38 @@ function(nd_perms_prepare_deb_postinst_scripts entries)
     list(FILTER component_entries INCLUDE REGEX "^.+::${component}::.+$")
 
     foreach(item IN LISTS component_entries)
-      _nd_extract_path(entry_path "${item}")
-      _nd_extract_permissions(entry_perms "${item}")
-
-      set(ND_APPLY_PERMISSIONS "${ND_APPLY_PERMISSIONS}chown -f 'root:${NETDATA_USER}' '${entry_path}'\n")
-
-      list(TRANSFORM entry_perms PREPEND "cap_")
-      list(JOIN entry_perms "," capset)
-      set(capset "${capset}+eip")
-
-      if("${entry_perms}" STREQUAL "suid" OR NOT USE_FILE_CAPABILITIES)
-        set(ND_APPLY_PERMISSIONS "${ND_APPLY_PERMISSIONS}chmod -f 4750 '${entry_path}'\n")
-      elseif("perfmon" IN_LIST entry_perms)
-        string(REPLACE "perfmon" "sys_admin" capset2 "${capset}")
-        set(ND_APPLY_PERMISSIONS "${ND_APPLY_PERMISSIONS}chmod -f 0750 '${entry_path}'\n")
-        set(ND_APPLY_PERMISSIONS "${ND_APPLY_PERMISSIONS}if ! capset '${capset}' '${entry_path}' 2>/dev/null; then\n")
-        set(ND_APPLY_PERMISSIONS "${ND_APPLY_PERMISSIONS}    if ! capset '${capset2}' '${entry_path}' 2>/dev/null; then\n")
-        set(ND_APPLY_PERMISSIONS "${ND_APPLY_PERMISSIONS}        chmod -f 4750 '${entry_path}'\n")
-        set(ND_APPLY_PERMISSIONS "${ND_APPLY_PERMISSIONS}    fi\n")
-        set(ND_APPLY_PERMISSIONS "${ND_APPLY_PERMISSIONS}fi\n")
-      else()
-        set(ND_APPLY_PERMISSIONS "${ND_APPLY_PERMISSIONS}chmod -f 0750 '${entry_path}'\n")
-        set(ND_APPLY_PERMISSIONS "${ND_APPLY_PERMISSIONS}if ! capset '${capset}' '${entry_path}' 2>/dev/null; then\n")
-        set(ND_APPLY_PERMISSIONS "${ND_APPLY_PERMISSIONS}    chmod -f 4750 '${entry_path}'\n")
-        set(ND_APPLY_PERMISSIONS "${ND_APPLY_PERMISSIONS}fi\n")
-      endif()
+      _nd_perms_generate_entry_script(ND_APPLY_PERMISSIONS "${item}")
     endforeach()
 
     configure_file("${postinst_src}" "${postinst}" @ONLY)
   endforeach()
+endfunction()
+
+# Generate a post-install hook script for our static builds.
+function(nd_perms_prepare_static_postinstall_hook entries)
+  file(MAKE_DIRECTORY "${_nd_perms_hooks_dir}/static")
+  set(hook_path "${_nd_perms_hooks_dir}/static/apply-filecaps.sh")
+  # This next variable is needed to work around CMake’s inability to
+  # escape `$` in arguments.
+  #
+  # Syntax highlighting for this line does not work correctly in at
+  # least some editors.
+  set(euid_check [=[[ "${EUID}" -ne 0 ] || exit 0]=])
+
+  message(STATUS "Generating ${hook_path}")
+
+  set(hook_script "#!/bin/bash\n")
+  set(hook_script "${hook_script}set -e\n")
+  set(hook_script "${hook_script}${euid_check}\n")
+
+  foreach(entry IN LISTS entries)
+    _nd_perms_generate_entry_script(hook_script "${entry}")
+  endforeach()
+
+  file(WRITE "${hook_path}" "${hook_script}")
+  install(PROGRAMS "${hook_path}"
+          DESTINATION system
+          COMPONENT netdata)
 endfunction()
 
 # Handle generation of supplementary permissions hooks.
@@ -163,5 +214,7 @@ function(netdata_install_extra_permissions)
 
   if(PACKAGE_TYPE STREQUAL "deb")
     nd_perms_prepare_deb_postinst_scripts("${extra_perms_entries}")
+  elseif(PACKAGE_TYPE STREQUAL "static")
+    nd_perms_prepare_static_postinstall_hook("${extra_perms_entries}")
   endif()
 endfunction()
--- packaging/makeself/jobs/70-netdata-git.install.sh
+++ packaging/makeself/jobs/70-netdata-git.install.sh
@@ -22,10 +22,7 @@ export IS_NETDATA_STATIC_BINARY="yes"
 export EBPF_LIBC="static"
 export PKG_CONFIG="pkg-config --static"
 export PKG_CONFIG_PATH="/openssl-static/lib64/pkgconfig:/libnetfilter-acct-static/lib/pkgconfig:/usr/lib/pkgconfig:/curl-local/lib/pkgconfig"
-
-# Set correct CMake flags for building against non-System OpenSSL
-# See: https://github.com/warmcat/libwebsockets/blob/master/READMEs/README.build.md
-export CMAKE_FLAGS="-DOPENSSL_ROOT_DIR=/openssl-static -DOPENSSL_LIBRARIES=/openssl-static/lib64 -DCMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE=/openssl-static -DLWS_OPENSSL_INCLUDE_DIRS=/openssl-static/include -DLWS_OPENSSL_LIBRARIES=/openssl-static/lib64/libssl.a;/openssl-static/lib64/libcrypto.a"
+export NETDATA_CMAKE_OPTIONS="-DBUILD_FOR_PACKAGING=On -DPACKAGE_TYPE=static"
 
 run ./netdata-installer.sh \
   --install-prefix "${NETDATA_INSTALL_PARENT}" \
