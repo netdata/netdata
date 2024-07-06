b8cf678618612462b931f03dceb6237ce2144550
ilyam8 <ilya@netdata.cloud>
2024-07-06 22:02:32 +0300

    dont collect swapio if no swap

--- src/collectors/proc.plugin/proc_vmstat.c
+++ src/collectors/proc.plugin/proc_vmstat.c
@@ -10,20 +10,53 @@
 #define _COMMON_PLUGIN_MODULE_NAME PLUGIN_PROC_MODULE_VMSTAT_NAME
 #include "../common-contexts/common-contexts.h"
 
+static bool is_ksm_enabled() {
+    unsigned long long ksm_run = 0;
+
+    char filename[FILENAME_MAX + 1];
+    snprintfz(filename, FILENAME_MAX, "%s/sys/kernel/mm/ksm/run", netdata_configured_host_prefix);
+
+    return !read_single_number_file(filename, &ksm_run) && ksm_run == 1;
+}
+
+static bool is_swap_enabled() {
+    char filename[FILENAME_MAX + 1];
+    snprintfz(filename, FILENAME_MAX, "%s/proc/meminfo", netdata_configured_host_prefix);
+
+    procfile *ff = procfile_open(filename, " \t:", PROCFILE_FLAG_DEFAULT);
+    if (!ff) {
+        return false;
+    }
+
+    ff = procfile_readall(ff);
+    if (!ff) {
+        procfile_close(ff);
+        return false;
+    }
+
+    unsigned long long swap_total = 0;
+
+    size_t lines = procfile_lines(ff), l;
+
+    for (l = 0; l < lines; l++) {
+        size_t words = procfile_linewords(ff, l);
+        if (words < 2)
+            continue;
+
+        const char *key = procfile_lineword(ff, l, 0);
+        if (strcmp(key, "SwapTotal") == 0) {
+            swap_total = str2ull(procfile_lineword(ff, l, 1), NULL);
+            break;
+        }
+    }
+
+    procfile_close(ff);
+
+    return swap_total > 0;
+}
+
 int do_proc_vmstat(int update_every, usec_t dt) {
     (void)dt;
-
-    static int ksm_enabled = -1;
-
-    if (ksm_enabled == -1) {
-        unsigned long long ksm_run = 0;
-        char filename[FILENAME_MAX + 1];
-        snprintfz(filename, FILENAME_MAX, "%s/sys/kernel/mm/ksm/run", netdata_configured_host_prefix);
-
-        read_single_number_file(filename, &ksm_run);
-
-        ksm_enabled = ksm_run == 1 ? 1 : 0;
-    }
 
     static procfile *ff = NULL;
     static int do_swapio = -1, do_io = -1, do_pgfaults = -1, do_oom_kill = -1, do_numa = -1, do_thp = -1, do_zswapio = -1, do_balloon = -1, do_ksm = -1;
@@ -166,9 +199,9 @@ int do_proc_vmstat(int update_every, usec_t dt) {
         do_zswapio = config_get_boolean_ondemand("plugin:proc:/proc/vmstat", "zswap i/o", CONFIG_BOOLEAN_AUTO);
         do_balloon = config_get_boolean_ondemand("plugin:proc:/proc/vmstat", "memory ballooning", CONFIG_BOOLEAN_AUTO);
         do_ksm = config_get_boolean_ondemand("plugin:proc:/proc/vmstat", "kernel same memory", CONFIG_BOOLEAN_AUTO);
-        if (!ksm_enabled) {
-            do_ksm = CONFIG_BOOLEAN_NO;
-        }
+
+        do_ksm = is_ksm_enabled() ? do_ksm : CONFIG_BOOLEAN_NO;
+        do_swapio = is_swap_enabled() ? do_swapio : CONFIG_BOOLEAN_NO;
 
         arl_base = arl_create("vmstat", NULL, 60);
         arl_expect(arl_base, "pgfault", &pgfault);
