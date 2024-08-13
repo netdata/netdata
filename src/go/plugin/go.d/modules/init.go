e88e204aab697f7e1d2bdcb7b81264f92edc9cd0
ilyam8 <ilya@netdata.cloud>
2024-08-13 19:44:37 +0300

    add go.d/uwsgi

--- src/go/plugin/go.d/modules/init.go
+++ src/go/plugin/go.d/modules/init.go
@@ -99,6 +99,7 @@ import (
 	_ "github.com/netdata/netdata/go/plugins/plugin/go.d/modules/traefik"
 	_ "github.com/netdata/netdata/go/plugins/plugin/go.d/modules/unbound"
 	_ "github.com/netdata/netdata/go/plugins/plugin/go.d/modules/upsd"
+	_ "github.com/netdata/netdata/go/plugins/plugin/go.d/modules/uwsgi"
 	_ "github.com/netdata/netdata/go/plugins/plugin/go.d/modules/vcsa"
 	_ "github.com/netdata/netdata/go/plugins/plugin/go.d/modules/vernemq"
 	_ "github.com/netdata/netdata/go/plugins/plugin/go.d/modules/vsphere"
