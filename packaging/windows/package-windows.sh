0a842a400138e0a482381038cbe817b422239eae
thiagoftsm <thiagoftsm@gmail.com>
2024-08-19 22:36:35 +0000

    installer_new_pages: Add commands to do download of gpl-3

--- packaging/windows/package-windows.sh
+++ packaging/windows/package-windows.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-REPO_ROOT="$(dirname "$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd -P)")")"
+repo_root="$(dirname "$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd -P)")")"
 
-# shellcheck source=./win-build-dir.sh
-. "${REPO_ROOT}/packaging/windows/win-build-dir.sh"
+if [ -n "${BUILD_DIR}" ]; then
+    build="${BUILD_DIR}"
+elif [ -n "${OSTYPE}" ]; then
+    if [ -n "${MSYSTEM}" ]; then
+        build="${repo_root}/build-${OSTYPE}-${MSYSTEM}"
+    else
+        build="${repo_root}/build-${OSTYPE}"
+    fi
+elif [ "$USER" = "vk" ]; then
+    build="${repo_root}/build"
+else
+    build="${repo_root}/build"
+fi
 
 set -exu -o pipefail
 
@@ -13,7 +24,7 @@ ${GITHUB_ACTIONS+echo "::endgroup::"}
 
 if [ ! -f "/msys2-installer.exe" ]; then
     ${GITHUB_ACTIONS+echo "::group::Fetching MSYS2 installer"}
-    "${REPO_ROOT}/packaging/windows/fetch-msys2-installer.py" /msys2-installer.exe
+    "${repo_root}/packaging/windows/fetch-msys2-installer.py" /msys2-installer.exe
     ${GITHUB_ACTIONS+echo "::endgroup::"}
 fi
 
@@ -22,5 +33,11 @@ NDVERSION=$"$(grep 'CMAKE_PROJECT_VERSION:STATIC' "${build}/CMakeCache.txt"| cut
 NDMAJORVERSION=$"$(grep 'CMAKE_PROJECT_VERSION_MAJOR:STATIC' "${build}/CMakeCache.txt"| cut -d= -f2)"
 NDMINORVERSION=$"$(grep 'CMAKE_PROJECT_VERSION_MINOR:STATIC' "${build}/CMakeCache.txt"| cut -d= -f2)"
 
-/mingw64/bin/makensis.exe -DCURRVERSION="${NDVERSION}" -DMAJORVERSION="${NDMAJORVERSION}" -DMINORVERSION="${NDMINORVERSION}" "${REPO_ROOT}/packaging/windows/installer.nsi"
+if [ -f "/gpl-3.0.txt" ]; then
+    ${GITHUB_ACTIONS+echo "::group::Fetching GPL3 License"}
+    curl -o /gpl-3.0.txt "https://www.gnu.org/licenses/gpl-3.0.txt"
+    ${GITHUB_ACTIONS+echo "::endgroup::"}
+fi
+
+/mingw64/bin/makensis.exe -DCURRVERSION="${NDVERSION}" -DMAJORVERSION="${NDMAJORVERSION}" -DMINORVERSION="${NDMINORVERSION}" "${repo_root}/packaging/windows/installer.nsi"
 ${GITHUB_ACTIONS+echo "::endgroup::"}
