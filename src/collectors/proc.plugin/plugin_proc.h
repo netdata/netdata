a37a3de85a0dcb4da7a97d2907544dbca0816f45
ilyam8 <ilya@netdata.cloud>
2024-08-21 19:44:57 +0300

    remove proc zfspools

--- src/collectors/proc.plugin/plugin_proc.h
+++ src/collectors/proc.plugin/plugin_proc.h
@@ -37,7 +37,6 @@ int do_proc_sys_devices_system_edac_mc(int update_every, usec_t dt);
 int do_proc_sys_devices_pci_aer(int update_every, usec_t dt);
 int do_proc_sys_devices_system_node(int update_every, usec_t dt);
 int do_proc_spl_kstat_zfs_arcstats(int update_every, usec_t dt);
-int do_proc_spl_kstat_zfs_pool_state(int update_every, usec_t dt);
 int do_sys_fs_btrfs(int update_every, usec_t dt);
 int do_proc_net_sockstat(int update_every, usec_t dt);
 int do_proc_net_sockstat6(int update_every, usec_t dt);
