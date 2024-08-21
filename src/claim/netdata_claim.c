d82a88ec4c245fcf9eafc9543b4a4b6650ada9be
thiagoftsm <thiagoftsm@gmail.com>
2024-08-21 23:46:04 +0000

    nd_claim_software: Add initial structure to have a binary to claim on Microsoft

---
+++ src/claim/netdata_claim.c
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    return 0;
+}
+
