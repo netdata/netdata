8f41508773801e6f10513294121fd41ceb385106
thiagoftsm <thiagoftsm@gmail.com>
2024-09-02 23:43:59 +0000

    netdata_cli_ui: Add basis to new software

--- src/cli/ui.c
+++ src/cli/ui.c
@@ -1,13 +1,49 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#ifndef UNICODE
 #define UNICODE
+#endif
+
+#ifndef _UNICODE
 #define _UNICODE
+#endif
+
 #include <windows.h>
 #include <shellapi.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <signal.h>
+
+#include "tchar.h"
+
+static LPCTSTR szWindowClass = _T("DesktopApp");
+
+HWND hNetdataWND = NULL;
+
+static LRESULT CALLBACK NetdataCliProc(HWND hNetdatawnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message) {
+        case WM_CREATE: {
+            break;
+        }
+        case WM_COMMAND: {
+            break;
+        }
+        case WM_DRAWITEM: {
+            break;
+        }
+        case WM_DESTROY: {
+            PostQuitMessage(0);
+            break;
+        }
+        default: {
+            return DefWindowProc(hNetdatawnd, message, wParam, lParam);
+            break;
+        }
+    }
+    return 0;
+}
 
 static void netdata_claim_exit_callback(int signal)
 {
@@ -20,5 +56,51 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     signal(SIGINT, netdata_claim_exit_callback);
     signal(SIGTERM, netdata_claim_exit_callback);
 
-    return 0;
+    WNDCLASSEX wcex;
+
+    wcex.cbSize         = sizeof(WNDCLASSEX);
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = NetdataCliProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(wcex.hInstance, MAKEINTRESOURCEW(11));
+    wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = NULL;
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wcex)) {
+        MessageBoxW(NULL, L"Call to RegisterClassEx failed!", L"Error", 0);
+        return (int)GetLastError();
+    }
+
+    hNetdataWND = CreateWindowExW(WS_EX_OVERLAPPEDWINDOW,
+                                  szWindowClass,
+                                  L"Netdata Client",
+                                  WS_OVERLAPPEDWINDOW,
+                                  CW_USEDEFAULT, CW_USEDEFAULT,
+                                  600, 400,
+                                  HWND_DESKTOP,
+                                  NULL,
+                                  hInstance,
+                                  NULL
+                                  );
+
+    if (!hNetdataWND) {
+        MessageBoxW(NULL, L"Call to CreateWindow failed!", L"Error", 0);
+        return (int)GetLastError();
+    }
+
+    ShowWindow(hNetdataWND, nCmdShow);
+    UpdateWindow(hNetdataWND);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int) msg.wParam;
 }
