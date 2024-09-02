73b9f44b14f4775a305fb21d18cb116de17dfbb3
thiagoftsm <thiagoftsm@gmail.com>
2024-09-02 00:06:20 +0000

    clean_rename: Remove msys script before to update avoiding errors

--- packaging/windows/package-windows.sh
+++ packaging/windows/package-windows.sh
@@ -23,6 +23,11 @@ if [ -d /opt/netdata/etc/pki/ ]; then
     rm -rf /opt/netdata/etc/pki/
 fi
 
+# Remove previous installation of msys2 script
+if [ -f /opt/netdata/usr/bin/bashbug ]; then
+    rm -rf /opt/netdata/usr/bin/bashbug
+fi
+
 ${GITHUB_ACTIONS+echo "::group::Installing"}
 cmake --install "${build}"
 ${GITHUB_ACTIONS+echo "::endgroup::"}
