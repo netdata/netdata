b97782a73799f9af3584af7508b03769cd487968
thiagoftsm <thiagoftsm@gmail.com>
2024-09-03 02:03:22 +0000

    netdata_cli_ui: Draw components on screen

---
+++ src/cli/ui.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef NETDATA_CLI_UI_H_
+# define NETDATA_CLI_UI_H_ 1
+
+#define IDC_RELOAD_HEALTH 101
+#define IDC_RELOAD_LABELS 102
+#define IDC_SAVE_DATABASE 103
+#define IDC_REOPEN_LOGS   104
+#define IDC_STOP_SERVICE  105
+#define IDC_OPEN_MSYS     106
+
+#endif
