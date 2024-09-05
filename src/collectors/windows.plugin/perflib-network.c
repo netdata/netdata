1ca93ec8755f121fef940fe0973349e39b062726
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2024-09-05 15:33:54 +0300

    Add charts for TCPv4/TCPV6/ICMP errors

--- src/collectors/windows.plugin/perflib-network.c
+++ src/collectors/windows.plugin/perflib-network.c
@@ -3,6 +3,28 @@
 #include "windows_plugin.h"
 #include "windows-internals.h"
 
+
+#define ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, counter)                                                \
+    do {                                                                                                               \
+        if ((p)->packets.counter.key) {                                                                                \
+            packets += perflibGetObjectCounter((pDataBlock), (pObjectType), &(p)->packets.counter) ? 1 : 0;            \
+        }                                                                                                              \
+    } while (0)
+
+#define SET_DIM_IF_KEY_AND_UPDATED(p, field)                                                                           \
+    do {                                                                                                               \
+        if ((p)->packets.field.key && (p)->packets.field.updated) {                                                    \
+            rrddim_set_by_pointer(                                                                                     \
+                (p)->packets.st, (p)->packets.rd_##field, (collected_number)(p)->packets.field.current.Data);          \
+        }                                                                                                              \
+    } while (0)
+
+#define ADD_RRD_DIM_IF_KEY(packet_field, id, name, multiplier, algorithm)                                              \
+    do {                                                                                                               \
+        if (p->packets.packet_field.key)                                                                               \
+            p->packets.rd_##packet_field = rrddim_add(st, id, name, multiplier, 1, algorithm);                         \
+    } while (0)
+
 // --------------------------------------------------------------------------------------------------------------------
 // network protocols
 
@@ -14,11 +36,75 @@ struct network_protocol {
         COUNTER_DATA sent;
         COUNTER_DATA delivered;
         COUNTER_DATA forwarded;
+
+        COUNTER_DATA InDiscards;
+        COUNTER_DATA OutDiscards;
+        COUNTER_DATA InHdrErrors;
+        COUNTER_DATA InAddrErrors;
+        COUNTER_DATA InUnknownProtos;
+        COUNTER_DATA InTooBigErrors;
+        COUNTER_DATA InTruncatedPkts;
+        COUNTER_DATA InNoRoutes;
+        COUNTER_DATA OutNoRoutes;
+
+        COUNTER_DATA InEchoReps;
+        COUNTER_DATA OutEchoReps;
+        COUNTER_DATA InDestUnreachs;
+        COUNTER_DATA OutDestUnreachs;
+        COUNTER_DATA InRedirects;
+        COUNTER_DATA OutRedirects;
+        COUNTER_DATA InEchos;
+        COUNTER_DATA OutEchos;
+        COUNTER_DATA InRouterAdvert;
+        COUNTER_DATA OutRouterAdvert;
+        COUNTER_DATA InRouterSelect;
+        COUNTER_DATA OutRouterSelect;
+        COUNTER_DATA InTimeExcds;
+        COUNTER_DATA OutTimeExcds;
+        COUNTER_DATA InParmProbs;
+        COUNTER_DATA OutParmProbs;
+        COUNTER_DATA InTimestamps;
+        COUNTER_DATA OutTimestamps;
+        COUNTER_DATA InTimestampReps;
+        COUNTER_DATA OutTimestampReps;
+
         RRDSET *st;
         RRDDIM *rd_received;
         RRDDIM *rd_sent;
         RRDDIM *rd_forwarded;
         RRDDIM *rd_delivered;
+
+        RRDDIM *rd_InDiscards;
+        RRDDIM *rd_OutDiscards;
+        RRDDIM *rd_InHdrErrors;
+        RRDDIM *rd_InAddrErrors;
+        RRDDIM *rd_InUnknownProtos;
+        RRDDIM *rd_InTooBigErrors;
+        RRDDIM *rd_InTruncatedPkts;
+        RRDDIM *rd_InNoRoutes;
+        RRDDIM *rd_OutNoRoutes;
+
+        RRDDIM *rd_InEchoReps;
+        RRDDIM *rd_OutEchoReps;
+        RRDDIM *rd_InDestUnreachs;
+        RRDDIM *rd_OutDestUnreachs;
+        RRDDIM *rd_InRedirects;
+        RRDDIM *rd_OutRedirects;
+        RRDDIM *rd_InEchos;
+        RRDDIM *rd_OutEchos;
+        RRDDIM *rd_InRouterAdvert;
+        RRDDIM *rd_OutRouterAdvert;
+        RRDDIM *rd_InRouterSelect;
+        RRDDIM *rd_OutRouterSelect;
+        RRDDIM *rd_InTimeExcds;
+        RRDDIM *rd_OutTimeExcds;
+        RRDDIM *rd_InParmProbs;
+        RRDDIM *rd_OutParmProbs;
+        RRDDIM *rd_InTimestamps;
+        RRDDIM *rd_OutTimestamps;
+        RRDDIM *rd_InTimestampReps;
+        RRDDIM *rd_OutTimestampReps;
+
         const char *type;
         const char *id;
         const char *family;
@@ -137,6 +223,107 @@ struct network_protocol {
         },
     },
 
+    {
+        .protocol = "IPv4",
+        .packets = {
+            .InDiscards = { .key = "Datagrams Received Discarded" },
+            .OutDiscards = { .key = "Datagrams Outbound Discarded" },
+            .OutNoRoutes = { .key = "Datagrams Outbound No Route" },
+            .InAddrErrors = { .key = "Datagrams Received Address Errors" },
+            .InHdrErrors = { .key = "Datagrams Received Header Errors" },
+            .InUnknownProtos = { .key = "Datagrams Received Unknown Protocol" },
+            .type = "ipv4",
+            .id = "errors",
+            .family = "errors",
+            .context = "ipv4.errors",
+            .title = "IPv4 TCP errors",
+            .priority = NETDATA_CHART_PRIO_IPV4_ERRORS,
+        },
+    },
+    {
+        .protocol = "IPv6",
+        .packets = {
+            .InDiscards = { .key = "Datagrams Received Discarded" },
+            .OutDiscards = { .key = "Datagrams Outbound Discarded" },
+            .OutNoRoutes = { .key = "Datagrams Outbound No Route" },
+            .InAddrErrors = { .key = "Datagrams Received Address Errors" },
+            .InHdrErrors = { .key = "Datagrams Received Header Errors" },
+            .InUnknownProtos = { .key = "Datagrams Received Unknown Protocol" },
+            .type = "ipv6",
+            .id = "errors",
+            .family = "errors",
+            .context = "ipv6.errors",
+            .title = "IPv6 TCP errors",
+            .priority = NETDATA_CHART_PRIO_IPV6_ERRORS,
+        },
+    },
+    {
+        .protocol = "ICMP",
+        .packets =
+            {
+                .InEchoReps = {.key = "Received Echo Reply/sec"},
+                .OutEchoReps = {.key = "Received Echo Reply/sec"},
+                .InDestUnreachs = {.key = "Received Dest. Unreachable"},
+                .OutDestUnreachs = {.key = "Sent Destination Unreachable"},
+                .InRedirects = {.key = "Received Redirect/sec"},
+                .OutRedirects = {.key = "Sent Redirect/sec"},
+                .InEchos = {.key = "Received Echo/sec"},
+                .OutEchos = {.key = "Sent Echo/sec"},
+                .InRouterAdvert = {.key = NULL},
+                .OutRouterAdvert = {.key = NULL},
+                .InRouterSelect = {.key = NULL},
+                .OutRouterSelect = {.key = NULL},
+                .InTimeExcds = {.key = "Received Time Exceeded"},
+                .OutTimeExcds = {.key = "Sent Time Exceeded"},
+                .InParmProbs = {.key = "Received Parameter Problem"},
+                .OutParmProbs = {.key = "Sent Parameter Problem"},
+                .InTimestamps = {.key = "Received Timestamp/sec"},
+                .OutTimestamps = {.key = "Sent Timestamp/sec"},
+                .InTimestampReps = {.key = "Received Timestamp Reply/sec"},
+                .OutTimestampReps = {.key = "Sent Timestamp Reply/sec"},
+
+                .type = "ipv4",
+                .id = "icmpmsg",
+                .family = "icmp",
+                .context = "ipv4.icmpmsg",
+                .title = "IPv4 ICMP Packets",
+                .priority = NETDATA_CHART_PRIO_IPV4_ICMP_MESSAGES,
+            },
+    },
+    {
+        .protocol = "ICMPv6",
+        .packets =
+            {
+                .InEchoReps = {.key = "Received Echo Reply/sec"},
+                .OutEchoReps = {.key = "Received Echo Reply/sec"},
+                .InDestUnreachs = {.key = "Received Dest. Unreachable"},
+                .OutDestUnreachs = {.key = "Sent Destination Unreachable"},
+                .InRedirects = {.key = "Received Redirect/sec"},
+                .OutRedirects = {.key = "Sent Redirect/sec"},
+                .InEchos = {.key = "Received Echo/sec"},
+                .OutEchos = {.key = "Sent Echo/sec"},
+                .InRouterAdvert = {.key = NULL},
+                .OutRouterAdvert = {.key = NULL},
+                .InRouterSelect = {.key = NULL},
+                .OutRouterSelect = {.key = NULL},
+                .InTimeExcds = {.key = "Received Time Exceeded"},
+                .OutTimeExcds = {.key = "Sent Time Exceeded"},
+                .InParmProbs = {.key = "Received Parameter Problem"},
+                .OutParmProbs = {.key = "Sent Parameter Problem"},
+                .InTimestamps = {.key = "Received Timestamp/sec"},
+                .OutTimestamps = {.key = "Sent Timestamp/sec"},
+                .InTimestampReps = {.key = "Received Timestamp Reply/sec"},
+                .OutTimestampReps = {.key = "Sent Timestamp Reply/sec"},
+
+                .type = "ipv6",
+                .id = "icmpmsg",
+                .family = "icmp",
+                .context = "ipv6.icmpmsg",
+                .title = "IPv6 ICMP Packets",
+                .priority = NETDATA_CHART_PRIO_IPV6_ICMP_MESSAGES,
+            },
+    },
+
     // terminator
     {
         .protocol = NULL,
@@ -171,27 +358,78 @@ static void protocol_packets_chart_update(struct network_protocol *p, int update
             , RRDSET_TYPE_AREA
         );
 
-        p->packets.rd_received  = rrddim_add(p->packets.st, "received", NULL,  1, 1, RRD_ALGORITHM_INCREMENTAL);
-        p->packets.rd_sent      = rrddim_add(p->packets.st, "sent",     NULL, -1, 1, RRD_ALGORITHM_INCREMENTAL);
+        RRDSET *st = p->packets.st;
+        
+        ADD_RRD_DIM_IF_KEY(received, "received", NULL, 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(sent, "sent", NULL, -1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(forwarded, "forwarded", NULL, -1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(delivered, "delivered", NULL, 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InDiscards, "InDiscards", NULL, 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(OutDiscards, "OutDiscards", NULL, -1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InHdrErrors, "InHdrErrors", NULL, 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InAddrErrors, "InAddrErrors", NULL, 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InUnknownProtos, "InUnknownProtos", NULL, 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InTooBigErrors, "InTooBigErrors", NULL, 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InTruncatedPkts, "InTruncatedPkts", NULL, 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InNoRoutes, "InNoRoutes", NULL, 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(OutNoRoutes, "OutNoRoutes", NULL, -1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InEchoReps, "InType0", "InEchoReps", 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(OutEchoReps, "OutType0", "OutEchoReps", -1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InDestUnreachs, "InType3", "InDestUnreachs", 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(OutDestUnreachs, "OutType3", "OutDestUnreachs", -1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InRedirects, "InType5", "InRedirects", 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(OutRedirects, "OutType5", "OutRedirects", -1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InEchos, "InType8", "InEchos", 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(OutEchos, "OutType8", "OutEchos", -1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InRouterAdvert, "InType9", "InRouterAdvert", 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(OutRouterAdvert, "OutType9", "OutRouterAdvert", -1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InRouterSelect, "InType10", "InRouterSelect", 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(OutRouterSelect, "OutType10", "OutRouterSelect", -1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InTimeExcds, "InType11", "InTimeExcds", 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(OutTimeExcds, "OutType11", "OutTimeExcds", -1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InParmProbs, "InType12", "InParmProbs", 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(OutParmProbs, "OutType12", "OutParmProbs", -1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InTimestamps, "InType13", "InTimestamps", 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(OutTimestamps, "OutType13", "OutTimestamps", -1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(InTimestampReps, "InType14", "InTimestampReps", 1, RRD_ALGORITHM_INCREMENTAL);
+        ADD_RRD_DIM_IF_KEY(OutTimestampReps, "OutType14", "OutTimestampReps", -1, RRD_ALGORITHM_INCREMENTAL);
 
-        if(p->packets.forwarded.key)
-            p->packets.rd_forwarded = rrddim_add(p->packets.st, "forwarded", NULL, -1, 1, RRD_ALGORITHM_INCREMENTAL);
-
-        if(p->packets.delivered.key)
-            p->packets.rd_delivered = rrddim_add(p->packets.st, "delivered", NULL,  1, 1, RRD_ALGORITHM_INCREMENTAL);
     }
 
-    if(p->packets.received.updated)
-        rrddim_set_by_pointer(p->packets.st, p->packets.rd_received,  (collected_number)p->packets.received.current.Data);
+    SET_DIM_IF_KEY_AND_UPDATED(p, received);
+    SET_DIM_IF_KEY_AND_UPDATED(p, sent);
 
-    if(p->packets.sent.updated)
-        rrddim_set_by_pointer(p->packets.st, p->packets.rd_sent,      (collected_number)p->packets.sent.current.Data);
-
-    if(p->packets.forwarded.key && p->packets.forwarded.updated)
-        rrddim_set_by_pointer(p->packets.st, p->packets.rd_forwarded, (collected_number)p->packets.forwarded.current.Data);
-
-    if(p->packets.delivered.key && p->packets.delivered.updated)
-        rrddim_set_by_pointer(p->packets.st, p->packets.rd_delivered, (collected_number)p->packets.delivered.current.Data);
+    SET_DIM_IF_KEY_AND_UPDATED(p, forwarded);
+    SET_DIM_IF_KEY_AND_UPDATED(p, delivered);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InDiscards);
+    SET_DIM_IF_KEY_AND_UPDATED(p, OutDiscards);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InHdrErrors);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InAddrErrors);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InUnknownProtos);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InTooBigErrors);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InTruncatedPkts);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InNoRoutes);
+    SET_DIM_IF_KEY_AND_UPDATED(p, OutNoRoutes);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InEchoReps);
+    SET_DIM_IF_KEY_AND_UPDATED(p, OutEchoReps);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InDestUnreachs);
+    SET_DIM_IF_KEY_AND_UPDATED(p, OutDestUnreachs);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InRedirects);
+    SET_DIM_IF_KEY_AND_UPDATED(p, OutRedirects);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InEchos);
+    SET_DIM_IF_KEY_AND_UPDATED(p, OutEchos);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InRouterAdvert);
+    SET_DIM_IF_KEY_AND_UPDATED(p, OutRouterAdvert);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InRouterSelect);
+    SET_DIM_IF_KEY_AND_UPDATED(p, OutRouterSelect);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InTimeExcds);
+    SET_DIM_IF_KEY_AND_UPDATED(p, OutTimeExcds);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InParmProbs);
+    SET_DIM_IF_KEY_AND_UPDATED(p, OutParmProbs);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InTimestamps);
+    SET_DIM_IF_KEY_AND_UPDATED(p, OutTimestamps);
+    SET_DIM_IF_KEY_AND_UPDATED(p, InTimestampReps);
+    SET_DIM_IF_KEY_AND_UPDATED(p, OutTimestampReps);
 
     rrdset_done(p->packets.st);
 }
@@ -203,17 +441,41 @@ static bool do_network_protocol(PERF_DATA_BLOCK *pDataBlock, int update_every, s
     if(!pObjectType) return false;
 
     size_t packets = 0;
-    if(p->packets.received.key)
-        packets += perflibGetObjectCounter(pDataBlock, pObjectType, &p->packets.received) ? 1 : 0;
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, received);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, sent);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, delivered);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, forwarded);
 
-    if(p->packets.sent.key)
-        packets += perflibGetObjectCounter(pDataBlock, pObjectType, &p->packets.sent) ? 1 : 0;
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InDiscards);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, OutDiscards);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InHdrErrors);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InAddrErrors);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InUnknownProtos);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InTooBigErrors);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InTruncatedPkts);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InNoRoutes);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, OutNoRoutes);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InEchoReps);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, OutEchoReps);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InDestUnreachs);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, OutDestUnreachs);
 
-    if(p->packets.delivered.key)
-        packets += perflibGetObjectCounter(pDataBlock, pObjectType, &p->packets.delivered) ? 1 :0;
-
-    if(p->packets.forwarded.key)
-        packets += perflibGetObjectCounter(pDataBlock, pObjectType, &p->packets.forwarded) ? 1 : 0;
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InRedirects);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, OutRedirects);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InEchos);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, OutEchos);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InRouterAdvert);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, OutRouterAdvert);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InRouterSelect);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, OutRouterSelect);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InTimeExcds);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, OutTimeExcds);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InParmProbs);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, OutParmProbs);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InTimestamps);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, OutTimestamps);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, InTimestampReps);
+    ADD_PACKET_IF_KEY(p, packets, pDataBlock, pObjectType, OutTimestampReps);
 
     if(packets)
         protocol_packets_chart_update(p, update_every);
@@ -448,6 +710,5 @@ int do_PerflibNetwork(int update_every, usec_t dt __maybe_unused) {
         tcp46.packets.sent.current.Data += tcp6->packets.sent.current.Data;
         protocol_packets_chart_update(&tcp46, update_every);
     }
-
     return 0;
 }
