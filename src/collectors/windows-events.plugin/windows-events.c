9407f5842ad3266e1c9c6fbc446e459e7710d25d
Costa Tsaousis <costa@netdata.cloud>
2024-09-05 17:54:29 +0300

    progress on windows events sources

--- src/collectors/windows-events.plugin/windows-events.c
+++ src/collectors/windows-events.plugin/windows-events.c
@@ -11,6 +11,33 @@
 // Link with Wevtapi.lib
 #pragma comment(lib, "Wevtapi.lib")
 
+typedef enum {
+    WEVTS_NONE               = 0,
+    WEVTS_ALL                = (1 << 0),
+} WEVT_SOURCE_TYPE;
+
+#define WEVT_FUNCTION_DESCRIPTION    "View, search and analyze Microsoft Windows events."
+#define WEVT_FUNCTION_NAME           "windows-events"
+
+// functions needed by LQS
+static WEVT_SOURCE_TYPE wevts_internal_source_type(const char *value);
+static void wevts_sources_to_json_array(BUFFER *wb);
+
+// structures needed by LQS
+struct lqs_extension {};
+
+// prepare LQS
+#define LQS_FUNCTION_NAME           WEVT_FUNCTION_NAME
+#define LQS_FUNCTION_DESCRIPTION    WEVT_FUNCTION_DESCRIPTION
+#define LQS_DEFAULT_ITEMS_PER_QUERY 200
+#define LQS_DEFAULT_ITEMS_SAMPLING  1000000
+#define LQS_SOURCE_TYPE             WEVT_SOURCE_TYPE
+#define LQS_SOURCE_TYPE_ALL         WEVTS_ALL
+#define LQS_SOURCE_TYPE_NONE        WEVTS_NONE
+#define LQS_FUNCTION_GET_INTERNAL_SOURCE_TYPE(value) wevts_internal_source_type(value)
+#define LQS_FUNCTION_SOURCE_TO_JSON_ARRAY(wb) wevts_sources_to_json_array(wb)
+#include "libnetdata/facets/logs_query_status.h"
+
 // Function to convert a wide string (UTF-16) to a multibyte string (UTF-8)
 static char *channel2utf8(LPWSTR wideStr) {
     static __thread char buffer[1024];
@@ -25,8 +52,14 @@ static char *channel2utf8(LPWSTR wideStr) {
     return buffer;
 }
 
-// Function to enumerate and list all event channels
-void ListEventChannels() {
+static WEVT_SOURCE_TYPE wevts_internal_source_type(const char *value) {
+    if(strcmp(value, "all") == 0)
+        return WEVTS_ALL;
+
+    return WEVTS_NONE;
+}
+
+static void wevts_sources_to_json_array(BUFFER *wb) {
     EVT_HANDLE hChannelEnum = NULL;
     LPWSTR pChannelPath = NULL;
     DWORD dwChannelBufferSize = 0;
@@ -36,11 +69,11 @@ void ListEventChannels() {
     // Open a handle to enumerate the event channels
     hChannelEnum = EvtOpenChannelEnum(NULL, 0);
     if (NULL == hChannelEnum) {
-        printf("WINDOWS EVENTS: EvtOpenChannelEnum() failed with %"PRIu64"\n", (uint64_t)GetLastError());
-        return;
+        nd_log(NDLS_COLLECTORS, NDLP_ERR, "WINDOWS EVENTS: EvtOpenChannelEnum() failed with %"PRIu64"\n",
+               (uint64_t)GetLastError());
+        goto cleanup;
     }
 
-    // Enumerate all channels
     while (true) {
         if (!EvtNextChannelPath(hChannelEnum, dwChannelBufferSize, pChannelPath, &dwChannelBufferUsed)) {
             status = GetLastError();
@@ -52,22 +85,29 @@ void ListEventChannels() {
                 continue;
             }
             else {
-                printf("WINDOWS EVENTS: EvtNextChannelPath() failed with %lu\n", status);
-                goto Cleanup;
+                nd_log(NDLS_COLLECTORS, NDLP_ERR,
+                       "WINDOWS EVENTS: EvtNextChannelPath() failed with %"PRIu64"\n",
+                       (uint64_t)status);
+                goto cleanup;
             }
         }
 
-        // Convert channel path to UTF-8 and print it
-        printf("Channel: %s\n", channel2utf8(pChannelPath));
+        const char *name = channel2utf8(pChannelPath);
+        buffer_json_add_array_item_object(wb);
+        {
+            buffer_json_member_add_string(wb, "id", name);
+            buffer_json_member_add_string(wb, "name", name);
+            buffer_json_member_add_string(wb, "pill", "size");
+            buffer_json_member_add_string(wb, "info", "");
+        }
+        buffer_json_object_close(wb); // options object
     }
 
-Cleanup:
-    // Free allocated resources
+cleanup:
     freez(pChannelPath);
     EvtClose(hChannelEnum);
 }
 
 int main(void) {
-    ListEventChannels();
     return 0;
 }
