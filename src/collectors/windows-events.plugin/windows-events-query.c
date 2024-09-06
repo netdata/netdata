8f871697c3448e959df5c16c21f41f56f659ec9b
Costa Tsaousis <costa@netdata.cloud>
2024-09-06 17:00:58 +0300

    extract basic fields from windows events

--- src/collectors/windows-events.plugin/windows-events-query.c
+++ src/collectors/windows-events.plugin/windows-events-query.c
@@ -4,20 +4,95 @@
 
 static uint64_t wevt_log_file_size(const wchar_t *channel);
 
-#define	VAR_PROVIDER_NAME(p)			    ((p)[0].StringVal)
-#define	VAR_SOURCE_NAME(p)			        ((p)[1].StringVal)
-#define	VAR_RECORD_NUMBER(p)			    ((p)[2].UInt64Val)
-#define	VAR_EVENT_ID(p)				        ((p)[3].UInt16Val)
-#define	VAR_LEVEL(p)				        ((p)[4].ByteVal)
-#define	VAR_KEYWORDS(p)				        ((p)[5].UInt64Val)
-#define	VAR_TIME_CREATED(p)			        ((p)[6].FileTimeVal)
-#define	VAR_EVENT_DATA_STRING(p)		    ((p)[7].StringVal)
-#define	VAR_EVENT_DATA_STRING_ARRAY(p, i)	((p)[7].StringArr[i])
-#define	VAR_EVENT_DATA_TYPE(p)			    ((p)[7].Type)
-#define	VAR_EVENT_DATA_COUNT(p)			    ((p)[7].Count)
+#define VAR_PROVIDER_NAME(p)                ((p)[0].StringVal)
+#define VAR_SOURCE_NAME(p)                  ((p)[1].StringVal)
+#define VAR_RECORD_NUMBER(p)                ((p)[2].UInt64Val)
+#define VAR_EVENT_ID(p)                     ((p)[3].UInt16Val)
+#define VAR_LEVEL(p)                        ((p)[4].ByteVal)
+#define VAR_KEYWORDS(p)                     ((p)[5].UInt64Val)
+#define VAR_TIME_CREATED(p)                 ((p)[6].FileTimeVal)
+#define VAR_COMPUTER_NAME(p)                ((p)[7].StringVal)
+#define VAR_USER_ID(p)                      ((p)[8].StringVal)
+#define VAR_CORRELATION_ACTIVITY_ID(p)      ((p)[9].GuidVal)
+#define	VAR_EVENT_DATA_STRING(p)		    ((p)[10].StringVal)
+#define	VAR_EVENT_DATA_STRING_ARRAY(p, i)	((p)[10].StringArr[i])
+#define	VAR_EVENT_DATA_TYPE(p)			    ((p)[10].Type)
+#define	VAR_EVENT_DATA_COUNT(p)			    ((p)[10].Count)
+
+bool wevt_str_wchar_to_utf8(TXT_UTF8 *utf8, const wchar_t *src, int src_len) {
+    if(!src || !src_len) goto cleanup;
+
+    // Try to convert using the existing buffer (if it exists, otherwise get the required buffer size)
+    int size = WideCharToMultiByte(CP_UTF8, 0, src, src_len, utf8->data, (int)utf8->size, NULL, NULL);
+    if(size <= 0 || !utf8->data) {
+        // we have to set a buffer, or increase it
+
+        if(utf8->data) {
+            // we need to increase it the buffer size
+
+            if(GetLastError() != ERROR_INSUFFICIENT_BUFFER) {
+                nd_log(NDLS_COLLECTORS, NDLP_ERR, "WideCharToMultiByte() failed.");
+                goto cleanup;
+            }
+
+            // we have to find the required buffer size
+            size = WideCharToMultiByte(CP_UTF8, 0, src, src_len, NULL, 0, NULL, NULL);
+            if(size <= 0) goto cleanup;
+        }
+
+        // Retry conversion with the new buffer
+        txt_utf8_resize(utf8, size);
+        size = WideCharToMultiByte(CP_UTF8, 0, src, src_len, utf8->data, (int)utf8->size, NULL, NULL);
+        if (size <= 0) {
+            nd_log(NDLS_COLLECTORS, NDLP_ERR, "WideCharToMultiByte() failed after resizing.");
+            goto cleanup;
+        }
+    }
+
+    utf8->len = (size_t)size;
+    return true;
+
+    cleanup:
+    txt_utf8_resize(utf8, 128);
+    utf8->len = snprintfz(utf8->data, utf8->size, "[failed to convert UNICODE message to UTF8]") + 1;
+    return false;
+}
+
+bool wevt_str_unicode_to_utf8(TXT_UTF8 *utf8, TXT_UNICODE *unicode) {
+    assert(utf8 && ((utf8->data && utf8->size) || (!utf8->data && !utf8->size)));
+    assert(unicode && ((unicode->data && unicode->size) || (!unicode->data && !unicode->size)));
+    return wevt_str_wchar_to_utf8(utf8, unicode->data, (int)unicode->len);
+}
+
+bool wevt_get_message_utf8(WEVT_LOG *log, EVT_HANDLE event_handle) {
+    DWORD size = 0;
+
+    // First, try to get the message using the existing buffer
+    if (!EvtFormatMessage(NULL, event_handle, 0, 0, NULL, EvtFormatMessageEvent, log->ops.unicode.size, log->ops.unicode.data, &size)) {
+        if (GetLastError() != ERROR_INSUFFICIENT_BUFFER) {
+            // nd_log(NDLS_COLLECTORS, NDLP_ERR, "EvtFormatMessage() failed.");
+            goto cleanup;
+        }
+
+        // Try again with the resized buffer
+        txt_unicode_resize(&log->ops.unicode, size);
+        if (!EvtFormatMessage(NULL, event_handle, 0, 0, NULL, EvtFormatMessageEvent, log->ops.unicode.size, log->ops.unicode.data, &size)) {
+            // nd_log(NDLS_COLLECTORS, NDLP_ERR, "EvtFormatMessage() failed after resizing buffer.");
+            goto cleanup;
+        }
+    }
+
+    log->ops.unicode.len = size;
+    return wevt_str_unicode_to_utf8(&log->ops.message, &log->ops.unicode);
+
+cleanup:
+    txt_utf8_resize(&log->ops.message, 128);
+    log->ops.message.len = snprintfz(log->ops.message.data, log->ops.message.size, "[failed to get UNICODE message for this event]") + 1;
+    return false;
+}
 
 static bool wevt_get_next_event(WEVT_LOG *log, WEVT_EVENT *ev) {
-    DWORD		size_required_next = 0, size_required = 0, size = 0, bookmarkedCount = 0;
+    DWORD		size_required_next = 0, size = 0, bookmarkedCount = 0;
     EVT_HANDLE	tmp_event_bookmark = NULL;
     bool ret = false;
 
@@ -27,25 +102,30 @@ static bool wevt_get_next_event(WEVT_LOG *log, WEVT_EVENT *ev) {
         goto cleanup; // no data available, return failure
 
     // obtain the information from selected events
-    if (!EvtRender(log->render_context, tmp_event_bookmark, EvtRenderEventValues, size, log->ops.renderedContent, &size_required, &bookmarkedCount)) {
+    if (!EvtRender(log->render_context, tmp_event_bookmark, EvtRenderEventValues, log->ops.content.size, log->ops.content.data, &size, &bookmarkedCount)) {
         // information exceeds the allocated space
-        if (ERROR_INSUFFICIENT_BUFFER != GetLastError()) {
+        if (GetLastError() != ERROR_INSUFFICIENT_BUFFER) {
             nd_log(NDLS_COLLECTORS, NDLP_ERR, "EvtRender() failed");
             goto cleanup;
         }
 
-        size = size_required;
-        freez(log->ops.renderedContent);
-        log->ops.renderedContent = (EVT_VARIANT *)mallocz(size);
+        freez(log->ops.content.data);
+        log->ops.content.size = size;
+        log->ops.content.data = (EVT_VARIANT *)mallocz(log->ops.content.size);
 
-        if (!EvtRender(log->render_context, tmp_event_bookmark, EvtRenderEventValues, size, log->ops.renderedContent, &size_required, &bookmarkedCount)) {
+        if (!EvtRender(log->render_context, tmp_event_bookmark, EvtRenderEventValues, log->ops.content.size, log->ops.content.data, &size, &bookmarkedCount)) {
             nd_log(NDLS_COLLECTORS, NDLP_ERR, "EvtRender() failed, after size increase");
             goto cleanup;
         }
     }
 
-    ev->id = VAR_RECORD_NUMBER(log->ops.renderedContent);
-    ev->created_ns = os_windows_ulonglong_to_unix_epoch_ns(VAR_TIME_CREATED(log->ops.renderedContent));
+    ev->id = VAR_RECORD_NUMBER(log->ops.content.data);
+    ev->created_ns = os_windows_ulonglong_to_unix_epoch_ns(VAR_TIME_CREATED(log->ops.content.data));
+    wevt_get_message_utf8(log, tmp_event_bookmark);
+    wevt_str_wchar_to_utf8(&log->ops.provider, VAR_PROVIDER_NAME(log->ops.content.data), -1);
+    wevt_str_wchar_to_utf8(&log->ops.source, VAR_SOURCE_NAME(log->ops.content.data), -1);
+    wevt_str_wchar_to_utf8(&log->ops.computer, VAR_COMPUTER_NAME(log->ops.content.data), -1);
+    wevt_str_wchar_to_utf8(&log->ops.user, VAR_USER_ID(log->ops.content.data), -1);
     ret = true;
 
 cleanup:
@@ -62,9 +142,13 @@ void wevt_closelog6(WEVT_LOG *log) {
     if (log->render_context)
         EvtClose(log->render_context);
 
-    if (log->ops.renderedContent)
-        freez(log->ops.renderedContent);
-
+    freez(log->ops.content.data);
+    txt_unicode_cleanup(&log->ops.unicode);
+    txt_utf8_cleanup(&log->ops.message);
+    txt_utf8_cleanup(&log->ops.provider);
+    txt_utf8_cleanup(&log->ops.source);
+    txt_utf8_cleanup(&log->ops.computer);
+    txt_utf8_cleanup(&log->ops.user);
     freez(log);
 }
 
@@ -77,6 +161,9 @@ WEVT_LOG *wevt_openlog6(const wchar_t *channel, bool file_size) {
             L"/Event/System/Level",
             L"/Event/System/Keywords",
             L"/Event/System/TimeCreated/@SystemTime",
+            L"/Event/System/Computer",
+            L"/Event/System/Security/@UserID",
+            L"/Event/System/Correlation/@ActivityID",
             L"/Event/EventData/Data"
     };
 
--- src/collectors/windows-events.plugin/windows-events-query.h
+++ src/collectors/windows-events.plugin/windows-events-query.h
@@ -13,6 +13,18 @@ typedef struct {
 #define WEVT_EVENT_EMPTY (WEVT_EVENT){ .id = 0, .created_ns = 0, }
 
 typedef struct {
+    char *data;
+    size_t size; // the allocated size of data buffer
+    size_t len;  // the used size of the data buffer (including null terminators, if any)
+} TXT_UTF8;
+
+typedef struct {
+    wchar_t *data;
+    size_t size; // the allocated size of data buffer
+    size_t len;  // the used size of the data buffer (including null terminators, if any)
+} TXT_UNICODE;
+
+typedef struct {
     EVT_HANDLE event_query;
     EVT_HANDLE render_context;
 
@@ -26,12 +38,67 @@ typedef struct {
     } retention;
 
     struct {
-        EVT_VARIANT	*renderedContent;
+        // temp buffer used for rendering event log messages
+        // never use directly
+        struct {
+            EVT_VARIANT	*data;
+            size_t size;
+        } content;
+
+        // temp buffer used for fetching and converting UNICODE and UTF-8
+        // every string operation overwrites it, multiple times per event log entry
+        // it can be used within any function, for its own purposes,
+        // but never share between functions
+        TXT_UNICODE unicode;
+
+        // string attributes of the current event log entry
+        // valid until another event if fetched
+        TXT_UTF8 message;
+        TXT_UTF8 provider;
+        TXT_UTF8 source;
+        TXT_UTF8 computer;
+        TXT_UTF8 user;
     } ops;
 
 } WEVT_LOG;
 
 void wevt_closelog6(WEVT_LOG *log);
 WEVT_LOG *wevt_openlog6(const wchar_t *channel, bool file_size);
+
+static inline size_t compute_new_size(size_t old_size, size_t required_size) {
+    size_t size = (required_size % 4096 == 0) ? required_size : required_size + 4096;
+    size = (size / 4096) * 4096;
+
+    if(size < old_size * 2)
+        size = old_size * 2;
+
+    return size;
+}
+
+static inline void txt_utf8_cleanup(TXT_UTF8 *utf8) {
+    freez(utf8->data);
+}
+
+static inline void txt_utf8_resize(TXT_UTF8 *utf8, size_t required_size) {
+    if(required_size < utf8->size)
+        return;
+
+    txt_utf8_cleanup(utf8);
+    utf8->size = compute_new_size(utf8->size, required_size);
+    utf8->data = mallocz(utf8->size);
+}
+
+static inline void txt_unicode_cleanup(TXT_UNICODE *unicode) {
+    freez(unicode->data);
+}
+
+static inline void txt_unicode_resize(TXT_UNICODE *unicode, size_t required_size) {
+    if(required_size < unicode->size)
+        return;
+
+    txt_unicode_cleanup(unicode);
+    unicode->size = compute_new_size(unicode->size, required_size);
+    unicode->data = mallocz(unicode->size);
+}
 
 #endif //NETDATA_WINDOWS_EVENTS_QUERY_H
