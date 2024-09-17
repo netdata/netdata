15878f929dd839757dd2db257b67f9bc8ef716ee
Ilya Mashchenko <ilya@netdata.cloud>
2024-09-18 00:02:11 +0300

    go.d/portcheck: add UDP support (#18569)

--- src/go/plugin/go.d/modules/portcheck/init.go
+++ src/go/plugin/go.d/modules/portcheck/init.go
@@ -6,44 +6,27 @@ import (
 	"errors"
 	"net"
 	"time"
-
-	"github.com/netdata/netdata/go/plugins/plugin/go.d/agent/module"
 )
 
-type dialFunc func(network, address string, timeout time.Duration) (net.Conn, error)
-
-type port struct {
-	number  int
-	state   checkState
-	inState int
-	latency int
-}
+type dialTCPFunc func(network, address string, timeout time.Duration) (net.Conn, error)
 
 func (pc *PortCheck) validateConfig() error {
 	if pc.Host == "" {
-		return errors.New("'host' parameter not set")
+		return errors.New("missing required parameter: 'host' must be specified")
 	}
-	if len(pc.Ports) == 0 {
-		return errors.New("'ports' parameter not set")
+	if len(pc.Ports) == 0 && len(pc.UDPPorts) == 0 {
+		return errors.New("missing required parameters: at least one of 'ports' (TCP) or 'udp_ports' (UDP) must be specified")
 	}
 	return nil
 }
 
-func (pc *PortCheck) initCharts() (*module.Charts, error) {
-	charts := module.Charts{}
-
-	for _, port := range pc.Ports {
-		if err := charts.Add(*newPortCharts(pc.Host, port)...); err != nil {
-			return nil, err
-		}
-	}
-
-	return &charts, nil
-}
-
-func (pc *PortCheck) initPorts() (ports []*port) {
+func (pc *PortCheck) initPorts() (tcpPorts []*tcpPort, udpPorts []*udpPort) {
 	for _, p := range pc.Ports {
-		ports = append(ports, &port{number: p})
+		tcpPorts = append(tcpPorts, &tcpPort{number: p})
 	}
-	return ports
+	for _, p := range pc.UDPPorts {
+		udpPorts = append(udpPorts, &udpPort{number: p})
+	}
+
+	return tcpPorts, udpPorts
 }
