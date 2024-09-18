63b9d7bcdefbb4f1f27eccdc25b693081f312572
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2024-09-18 17:31:10 +0300

    Reduce connection timeout and fallback to IPV4 for ACLK connections (#18568)
    (cherry picked from commit 1bb3047ee60a46cf210b16c563b15be4fafc1045)

--- src/aclk/https_client.h
+++ src/aclk/https_client.h
@@ -78,7 +78,7 @@ void https_req_response_free(https_req_response_t *res);
         .proxy_port = 8080                          \
     }
 
-int https_request(https_req_t *request, https_req_response_t *response);
+int https_request(https_req_t *request, https_req_response_t *response, bool *fallback_ipv4);
 
 // we expose previously internal parser as this is usefull also from
 // other parts of the code
