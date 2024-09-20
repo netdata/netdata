a2f6b820ac4c899bc6abb9d9ed14d10584b3015b
thiagoftsm <thiagoftsm@gmail.com>
2024-09-20 00:53:08 +0000

    mssql_metrics: Add first metrics (General Stats)

--- src/collectors/windows.plugin/perflib-mssql.c
+++ src/collectors/windows.plugin/perflib-mssql.c
@@ -7,7 +7,20 @@
 #define NETDATA_MAX_INSTANCE_NAME 32
 #define NETDATA_MAX_INSTANCE_OBJECT 128
 
+enum netdata_mssql_metrics {
+    NETDATA_MSSQL_GENERAL_STATS,
+    NETDATA_MSSQL_SQL_ERRORS,
+    NETDATA_MSSQL_DATABASE,
+    NETDATA_MSSQL_LOCKS,
+    NETDATA_MSSQL_MEMORY,
+    NETDATA_MSSQL_BUFFER_MANAGEMENT,
+    NETDATA_MSSQL_SQL_STATS,
+    NETDATA_MSSQL_TRANSACTIONS
+};
+
 struct mssql_instance {
+    char *instanceID;
+
     char *genStats;
     char *sqlErrors;
     char *databases;
@@ -16,6 +29,12 @@ struct mssql_instance {
     char *bufMan;
     char *memmgr;
     char *locks;
+
+    RRDSET *st_user_connections;
+    RRDDIM *rd_user_connections;
+
+    RRDSET *st_process_blocked;
+    RRDDIM *rd_process_blocked;
 
     COUNTER_DATA MSSQLAccessMethodPageSplits;
     COUNTER_DATA MSSQLBufferCacheHits;
@@ -83,6 +102,9 @@ static inline void initialize_mssql_objects(struct mssql_instance *p, const char
 
     strncpyz(&name[length], "Locks", sizeof(name) - length);
     p->locks = strdup(name);
+
+    p->instanceID = strdup(instance);
+    netdata_fix_chart_name(p->instanceID);
 }
 
 static inline void initialize_mssql_keys(struct mssql_instance *p) {
@@ -186,8 +208,142 @@ static int initialize(void) {
     return 0;
 }
 
-static bool do_MSSQL(PERF_DATA_BLOCK *pDataBlock, int update_every) {
+static inline void do_MSSQL_GENERAL_STATS(PERF_DATA_BLOCK *pDataBlock, struct mssql_instance *p, int update_every)
+{
+    char id[RRD_ID_LENGTH_MAX + 1];
+    PERF_OBJECT_TYPE *pObjectType = perflibFindObjectTypeByName(pDataBlock, p->genStats);
+    if (!pObjectType)
+        return;
+
+    if (perflibGetObjectCounter(pDataBlock, pObjectType, &p->MSSQLUserConnections)) {
+        snprintfz(id, RRD_ID_LENGTH_MAX, "instance_%s_user_connection", p->instanceID);
+        if (!p->st_user_connections) {
+            p->st_user_connections = rrdset_create_localhost("mssql"
+                                                             , id, NULL
+                                                             , "connections"
+                                                             , "mssql.instance_user_connection"
+                                                             , "User connections"
+                                                             , "connections"
+                                                             , PLUGIN_WINDOWS_NAME
+                                                             , "MSSQL"
+                                                             , PRIO_MSSQL_USER_CONNECTIONS
+                                                             , update_every
+                                                             , RRDSET_TYPE_LINE
+                                                             );
+
+            snprintfz(id, RRD_ID_LENGTH_MAX, "mssql_instance_%s_genstats_user_connections", p->instanceID);
+            p->rd_user_connections  = rrddim_add(p->st_user_connections,
+                                                id,
+                                                "user",
+                                                1,
+                                                1,
+                                                RRD_ALGORITHM_ABSOLUTE);
+
+            rrdlabels_add(p->st_file_transfer->rrdlabels, "mssql_instance", p->instanceID, RRDLABEL_SRC_AUTO);
+        }
+
+        rrddim_set_by_pointer(p->st_user_connections,
+                              p->rd_user_connections,
+                              (collected_number)p->MSSQLUserConnections.current.Data);
+        rrdset_done(p->st_user_connections);
+    }
+
+    if (perflibGetObjectCounter(pDataBlock, pObjectType, &p->MSSQLBlockedProcesses)) {
+        snprintfz(id, RRD_ID_LENGTH_MAX, "instance_%s_blocked_process", p->instanceID);
+        if (!p->st_process_blocked) {
+            p->st_process_blocked = rrdset_create_localhost("mssql"
+                                                            , id, NULL
+                                                            , "processes"
+                                                            , "mssql.instance_blocked_processes"
+                                                            , "Blocked processes"
+                                                            , "process"
+                                                            , PLUGIN_WINDOWS_NAME
+                                                            , "MSSQL"
+                                                            , PRIO_MSSQL_BLOCKED_PROCESSES
+                                                            , update_every
+                                                            , RRDSET_TYPE_LINE
+                                                            );
+
+            snprintfz(id, RRD_ID_LENGTH_MAX, "mssql_instance_%s_genstats_blocked_processes", p->instanceID);
+            p->rd_process_blocked  = rrddim_add(p->st_process_blocked,
+                                                 id,
+                                                 "blocked",
+                                                 1,
+                                                 1,
+                                                 RRD_ALGORITHM_ABSOLUTE);
+
+            rrdlabels_add(p->st_file_transfer->rrdlabels, "mssql_instance", p->instanceID, RRDLABEL_SRC_AUTO);
+        }
+
+        rrddim_set_by_pointer(p->st_process_blocked,
+                              p->rd_process_blocked,
+                              (collected_number)p->MSSQLBlockedProcesses.current.Data);
+        rrdset_done(p->st_process_blocked);
+    }
+}
+
+static bool do_MSSQL(PERF_DATA_BLOCK *pDataBlock, struct mssql_instance *instance, int update_every, int caller_idx)
+{
+    switch(caller_idx) {
+        case NETDATA_MSSQL_GENERAL_STATS: {
+            do_MSSQL_GENERAL_STATS(pDataBlock, instance, update_every);
+            break;
+        }
+        case NETDATA_MSSQL_SQL_ERRORS: {
+            break;
+        }
+        case NETDATA_MSSQL_DATABASE: {
+            break;
+        }
+        case NETDATA_MSSQL_LOCKS: {
+            break;
+        }
+        case NETDATA_MSSQL_MEMORY: {
+            break;
+        }
+        case NETDATA_MSSQL_BUFFER_MANAGEMENT: {
+            break;
+        }
+        case NETDATA_MSSQL_SQL_STATS: {
+            break;
+        }
+        case NETDATA_MSSQL_TRANSACTIONS: {
+            break;
+        }
+    }
+
     return true;
+}
+
+int dict_mssql_charts_cb(const DICTIONARY_ITEM *item __maybe_unused, void *value, void *data __maybe_unused) {
+    struct mssql_instance *p = value;
+    int *update_every = data;
+
+    char *loop_through_data[] = {
+        p->genStats,
+        p->sqlErrors,
+        p->databases,
+        p->locks,
+        p->memmgr,
+        p->bufMan,
+        p->sqlStatistics,
+        p->transactions,
+        NULL
+    };
+
+    DWORD i;
+    for (i = 0; loop_through_data[i] ; i++) {
+        DWORD id = RegistryFindIDByName(loop_through_data[i]);
+        if(id == PERFLIB_REGISTRY_NAME_NOT_FOUND)
+            return -1;
+
+        PERF_DATA_BLOCK *pDataBlock = perflibGetPerformanceData(id);
+        if(!pDataBlock) return -1;
+
+        do_MSSQL(pDataBlock, p, *update_every, i);
+    }
+
+    return 1;
 }
 
 int do_PerflibMSSQL(int update_every, usec_t dt __maybe_unused)
@@ -201,7 +357,7 @@ int do_PerflibMSSQL(int update_every, usec_t dt __maybe_unused)
         initialized = true;
     }
 
-    do_MSSQL(NULL, update_every);
+    dictionary_sorted_walkthrough_read(mssql_instances, dict_mssql_charts_cb, &update_every);
 
     return 0;
 }
