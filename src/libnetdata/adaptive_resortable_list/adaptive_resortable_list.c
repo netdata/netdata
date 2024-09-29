9576bb3b797ca3502682e5608f53ca2e10f30719
vkalintiris <vasilis@netdata.cloud>
2024-09-29 10:56:40 +0300

    Reformat

--- src/libnetdata/adaptive_resortable_list/adaptive_resortable_list.c
+++ src/libnetdata/adaptive_resortable_list/adaptive_resortable_list.c
@@ -4,7 +4,8 @@
 
 // the default processor() of the ARL
 // can be overwritten at arl_create()
-inline void arl_callback_str2ull(const char *name, uint32_t hash, const char *value, void *dst) {
+inline void arl_callback_str2ull(const char *name, uint32_t hash, const char *value, void *dst)
+{
     (void)name;
     (void)hash;
 
@@ -13,7 +14,8 @@ inline void arl_callback_str2ull(const char *name, uint32_t hash, const char *va
     // fprintf(stderr, "name '%s' with hash %u and value '%s' is %llu\n", name, hash, value, *d);
 }
 
-inline void arl_callback_str2kernel_uint_t(const char *name, uint32_t hash, const char *value, void *dst) {
+inline void arl_callback_str2kernel_uint_t(const char *name, uint32_t hash, const char *value, void *dst)
+{
     (void)name;
     (void)hash;
 
@@ -22,7 +24,8 @@ inline void arl_callback_str2kernel_uint_t(const char *name, uint32_t hash, cons
     // fprintf(stderr, "name '%s' with hash %u and value '%s' is %llu\n", name, hash, value, (unsigned long long)*d);
 }
 
-inline void arl_callback_ssize_t(const char *name, uint32_t hash, const char *value, void *dst) {
+inline void arl_callback_ssize_t(const char *name, uint32_t hash, const char *value, void *dst)
+{
     (void)name;
     (void)hash;
 
@@ -32,12 +35,13 @@ inline void arl_callback_ssize_t(const char *name, uint32_t hash, const char *va
 }
 
 // create a new ARL
-ARL_BASE *arl_create(const char *name, void (*processor)(const char *, uint32_t, const char *, void *), size_t rechecks) {
+ARL_BASE *arl_create(const char *name, void (*processor)(const char *, uint32_t, const char *, void *), size_t rechecks)
+{
     ARL_BASE *base = callocz(1, sizeof(ARL_BASE));
 
     base->name = strdupz(name);
 
-    if(!processor)
+    if (!processor)
         base->processor = arl_callback_str2ull;
     else
         base->processor = processor;
@@ -47,11 +51,12 @@ ARL_BASE *arl_create(const char *name, void (*processor)(const char *, uint32_t,
     return base;
 }
 
-void arl_free(ARL_BASE *arl_base) {
-    if(unlikely(!arl_base))
+void arl_free(ARL_BASE *arl_base)
+{
+    if (unlikely(!arl_base))
         return;
 
-    while(arl_base->head) {
+    while (arl_base->head) {
         ARL_ENTRY *e = arl_base->head;
         arl_base->head = e->next;
 
@@ -71,19 +76,26 @@ void arl_free(ARL_BASE *arl_base) {
     freez(arl_base);
 }
 
-void arl_begin(ARL_BASE *base) {
-
+void arl_begin(ARL_BASE *base)
+{
 #ifdef NETDATA_INTERNAL_CHECKS
-    if(likely(base->iteration > 10)) {
+    if (likely(base->iteration > 10)) {
         // do these checks after the ARL has been sorted
 
-        if(unlikely(base->relinkings > (base->expected + base->allocated)))
-            netdata_log_info("ARL '%s' has %zu relinkings with %zu expected and %zu allocated entries. Is the source changing so fast?"
-                 , base->name, base->relinkings, base->expected, base->allocated);
+        if (unlikely(base->relinkings > (base->expected + base->allocated)))
+            netdata_log_info(
+                "ARL '%s' has %zu relinkings with %zu expected and %zu allocated entries. Is the source changing so fast?",
+                base->name,
+                base->relinkings,
+                base->expected,
+                base->allocated);
 
-        if(unlikely(base->slow > base->fast))
-            netdata_log_info("ARL '%s' has %zu fast searches and %zu slow searches. Is the source really changing so fast?"
-                 , base->name, base->fast, base->slow);
+        if (unlikely(base->slow > base->fast))
+            netdata_log_info(
+                "ARL '%s' has %zu fast searches and %zu slow searches. Is the source really changing so fast?",
+                base->name,
+                base->fast,
+                base->slow);
 
         /*
         if(unlikely(base->iteration % 60 == 0)) {
@@ -107,27 +119,25 @@ void arl_begin(ARL_BASE *base) {
     }
 #endif
 
-    if(unlikely(base->iteration > 0 && (base->added || (base->iteration % base->rechecks) == 0))) {
+    if (unlikely(base->iteration > 0 && (base->added || (base->iteration % base->rechecks) == 0))) {
         int wanted_equals_expected = ((base->iteration % base->rechecks) == 0);
 
         // fprintf(stderr, "\n\narl_begin() rechecking, added %zu, iteration %zu, rechecks %zu, wanted_equals_expected %d\n\n\n", base->added, base->iteration, base->rechecks, wanted_equals_expected);
 
         base->added = 0;
-        base->wanted = (wanted_equals_expected)?base->expected:0;
+        base->wanted = (wanted_equals_expected) ? base->expected : 0;
 
         ARL_ENTRY *e = base->head;
-        while(e) {
-            if(e->flags & ARL_ENTRY_FLAG_FOUND) {
-
+        while (e) {
+            if (e->flags & ARL_ENTRY_FLAG_FOUND) {
                 // remove the found flag
                 e->flags &= ~ARL_ENTRY_FLAG_FOUND;
 
                 // count it in wanted
-                if(!wanted_equals_expected && e->flags & ARL_ENTRY_FLAG_EXPECTED)
+                if (!wanted_equals_expected && e->flags & ARL_ENTRY_FLAG_EXPECTED)
                     base->wanted++;
 
-            }
-            else if(e->flags & ARL_ENTRY_FLAG_DYNAMIC && !(base->head == e && !e->next)) { // not last entry
+            } else if (e->flags & ARL_ENTRY_FLAG_DYNAMIC && !(base->head == e && !e->next)) { // not last entry
                 // we can remove this entry
                 // it is not found, and it was created because
                 // it was found in the source file
@@ -136,9 +146,12 @@ void arl_begin(ARL_BASE *base) {
                 ARL_ENTRY *t = e->next;
 
                 // remove it from the list
-                if(e->next) e->next->prev = e->prev;
-                if(e->prev) e->prev->next = e->next;
-                if(base->head == e) base->head = e->next;
+                if (e->next)
+                    e->next->prev = e->prev;
+                if (e->prev)
+                    e->prev->next = e->next;
+                if (base->head == e)
+                    base->head = e->next;
 
                 // free it
                 freez(e->name);
@@ -156,7 +169,7 @@ void arl_begin(ARL_BASE *base) {
         }
     }
 
-    if(unlikely(!base->head)) {
+    if (unlikely(!base->head)) {
         // hm... no nodes at all in the list #1700
         // add a fake one to prevent a crash
         // this is better than checking for the existence of nodes all the time
@@ -166,23 +179,29 @@ void arl_begin(ARL_BASE *base) {
     base->iteration++;
     base->next_keyword = base->head;
     base->found = 0;
-
 }
 
 // register an expected keyword to the ARL
 // together with its destination ( i.e. the output of the processor() )
-ARL_ENTRY *arl_expect_custom(ARL_BASE *base, const char *keyword, void (*processor)(const char *name, uint32_t hash, const char *value, void *dst), void *dst) {
+ARL_ENTRY *arl_expect_custom(
+    ARL_BASE *base,
+    const char *keyword,
+    void (*processor)(const char *name, uint32_t hash, const char *value, void *dst),
+    void *dst)
+{
     ARL_ENTRY *e = callocz(1, sizeof(ARL_ENTRY));
     e->name = strdupz(keyword);
     e->hash = simple_hash(e->name);
-    e->processor = (processor)?processor:base->processor;
+    e->processor = (processor) ? processor : base->processor;
     e->dst = dst;
     e->flags = ARL_ENTRY_FLAG_EXPECTED;
     e->prev = NULL;
     e->next = base->head;
 
-    if(base->head) base->head->prev = e;
-    else base->next_keyword = e;
+    if (base->head)
+        base->head->prev = e;
+    else
+        base->next_keyword = e;
 
     base->head = e;
     base->expected++;
@@ -193,41 +212,43 @@ ARL_ENTRY *arl_expect_custom(ARL_BASE *base, const char *keyword, void (*process
     return e;
 }
 
-int arl_find_or_create_and_relink(ARL_BASE *base, const char *s, const char *value) {
+int arl_find_or_create_and_relink(ARL_BASE *base, const char *s, const char *value)
+{
     ARL_ENTRY *e;
 
     uint32_t hash = simple_hash(s);
 
     // find if it already exists in the data
-    for(e = base->head; e ; e = e->next)
-        if(e->hash == hash && !strcmp(e->name, s))
+    for (e = base->head; e; e = e->next)
+        if (e->hash == hash && !strcmp(e->name, s))
             break;
 
 #ifdef NETDATA_INTERNAL_CHECKS
-    if(unlikely(base->next_keyword && e == base->next_keyword))
+    if (unlikely(base->next_keyword && e == base->next_keyword))
         fatal("Internal Error: e == base->last");
 #endif
 
-    if(e) {
+    if (e) {
         // found it in the keywords
 
         base->relinkings++;
 
         // run the processor for it
-        if(unlikely(e->dst)) {
+        if (unlikely(e->dst)) {
             e->processor(e->name, hash, value, e->dst);
             base->found++;
         }
 
         // unlink it - we will relink it below
-        if(e->next) e->next->prev = e->prev;
-        if(e->prev) e->prev->next = e->next;
+        if (e->next)
+            e->next->prev = e->prev;
+        if (e->prev)
+            e->prev->next = e->next;
 
         // make sure the head is properly linked
-        if(base->head == e)
+        if (base->head == e)
             base->head = e->next;
-    }
-    else {
+    } else {
         // not found
 
         // create it
@@ -241,7 +262,7 @@ int arl_find_or_create_and_relink(ARL_BASE *base, const char *s, const char *val
     }
 
 #ifdef NETDATA_INTERNAL_CHECKS
-    if(unlikely(base->iteration % 60 == 0 && e->flags & ARL_ENTRY_FLAG_FOUND))
+    if (unlikely(base->iteration % 60 == 0 && e->flags & ARL_ENTRY_FLAG_FOUND))
         netdata_log_info("ARL '%s': entry '%s' is already found. Did you forget to call arl_begin()?", base->name, s);
 #endif
 
@@ -249,29 +270,28 @@ int arl_find_or_create_and_relink(ARL_BASE *base, const char *s, const char *val
 
     // link it here
     e->next = base->next_keyword;
-    if(base->next_keyword) {
+    if (base->next_keyword) {
         e->prev = base->next_keyword->prev;
         base->next_keyword->prev = e;
 
-        if(e->prev)
+        if (e->prev)
             e->prev->next = e;
 
-        if(base->head == base->next_keyword)
+        if (base->head == base->next_keyword)
             base->head = e;
-    }
-    else {
+    } else {
         e->prev = NULL;
 
-        if(!base->head)
+        if (!base->head)
             base->head = e;
     }
 
     // prepare the next iteration
     base->next_keyword = e->next;
-    if(unlikely(!base->next_keyword))
+    if (unlikely(!base->next_keyword))
         base->next_keyword = base->head;
 
-    if(unlikely(base->found == base->wanted)) {
+    if (unlikely(base->found == base->wanted)) {
         // fprintf(stderr, "FOUND ALL WANTED 1: found = %zu, wanted = %zu, expected %zu\n", base->found, base->wanted, base->expected);
         return 1;
     }
