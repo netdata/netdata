e42733343f778dc66fa5b00932adb710f7f1b59f
Costa Tsaousis <costa@netdata.cloud>
2024-10-07 22:55:33 +0300

    added utf16_to_utf8()

--- src/libnetdata/string/utf8.c
+++ src/libnetdata/string/utf8.c
@@ -13,11 +13,15 @@
  *   3. Always return the number of wide characters written, including the null terminator
  */
 
-size_t any_to_utf16(uint32_t CodePage, wchar_t *utf16, size_t utf16_count, const char *src, int src_len) {
-    if(!src || src_len == 0)
-        return 0; // invalid input
+size_t any_to_utf16(uint32_t CodePage, wchar_t *dst, size_t dst_size, const char *src, int src_len) {
+    if(!src || src_len == 0) {
+        // invalid input
+        if(dst && dst_size)
+            *dst = L'\0';
+        return 0;
+    }
 
-    if(!utf16 || !utf16_count) {
+    if(!dst || !dst_size) {
         // the caller wants to know the buffer to allocate for the conversion
         int required = MultiByteToWideChar(CodePage, 0, src, src_len, NULL, 0);
         if(required <= 0) return 0; // error in the conversion
@@ -28,7 +32,7 @@ size_t any_to_utf16(uint32_t CodePage, wchar_t *utf16, size_t utf16_count, const
     }
 
     // do the conversion directly to the destination buffer
-    int rc = MultiByteToWideChar(CodePage, 0, src, src_len, utf16, (int)utf16_count);
+    int rc = MultiByteToWideChar(CodePage, 0, src, src_len, dst, (int)dst_size);
     if(rc <= 0) {
         // conversion failed, let's see why...
         DWORD status = GetLastError();
@@ -50,7 +54,7 @@ size_t any_to_utf16(uint32_t CodePage, wchar_t *utf16, size_t utf16_count, const
             rc = MultiByteToWideChar(CodePage, 0, src, src_len, tmp, required_size);
             if (rc <= 0) {
                 // it failed!
-                utf16[0] = L'\0';
+                *dst = L'\0';
                 freez(tmp);
                 return 0;
             }
@@ -58,39 +62,131 @@ size_t any_to_utf16(uint32_t CodePage, wchar_t *utf16, size_t utf16_count, const
             size_t len = rc;
 
             // copy as much as we can
-            memcpy(utf16, tmp, MIN(len, (utf16_count - 1)) * sizeof(wchar_t));
+            memcpy(dst, tmp, MIN(len, (dst_size - 1)) * sizeof(wchar_t));
 
             // null terminate it
-            utf16[MIN(len, (utf16_count - 1))] = L'\0';
+            dst[MIN(len, (dst_size - 1))] = L'\0';
 
             // free the temporary buffer
             freez(tmp);
 
             // return the actual bytes written
-            return MIN(len, utf16_count);
+            return MIN(len, dst_size);
         }
 
         // empty the destination
-        utf16[0] = L'\0';
+        *dst = L'\0';
         return 0;
     }
 
     size_t len = rc;
 
-    if(len >= utf16_count) {
+    if(len >= dst_size) {
         // truncate it to fit the null
-        utf16[utf16_count - 1] = L'\0';
-        return utf16_count;
+        dst[dst_size - 1] = L'\0';
+        return dst_size;
     }
 
-    if(utf16[len - 1] != L'\0') {
+    if(dst[len - 1] != L'\0') {
         // the result is not null terminated
         // append the null
-        utf16[len] = L'\0';
+        dst[len] = L'\0';
         return len + 1;
     }
 
     // the result is already null terminated
+    return len;
+}
+
+/*
+ * Convert UTF16 (wide-character string) to UTF8
+ * Goals:
+ *   1. Destination is always NULL terminated
+ *   2. If the destination buffer is not enough, return as much as possible data (truncate)
+ *   3. Always return the number of bytes written, including the null terminator
+ */
+
+size_t utf16_to_utf8(char *dst, size_t dst_size, const wchar_t *src, int src_len) {
+    if (!src || src_len == 0) {
+        // invalid input
+        if(dst && dst_size)
+            *dst = L'\0';
+        return 0;
+    }
+
+    if (!dst || dst_size == 0) {
+        // The caller wants to know the buffer size required for the conversion
+        int required = WideCharToMultiByte(CP_UTF8, 0, src, src_len, NULL, 0, NULL, NULL);
+        if (required <= 0) return 0; // error in the conversion
+
+        // Add 1 for null terminator only if src_len is not -1
+        return (size_t)required + ((src_len != -1) ? 1 : 0);
+    }
+
+    // Perform the conversion directly into the destination buffer
+    int rc = WideCharToMultiByte(CP_UTF8, 0, src, src_len, dst, (int)dst_size, NULL, NULL);
+    if (rc <= 0) {
+        // Conversion failed, let's see why...
+        DWORD status = GetLastError();
+        if (status == ERROR_INSUFFICIENT_BUFFER) {
+            // It cannot fit entirely, let's allocate a new buffer to convert it
+            // and then truncate it to the destination buffer
+
+            // Clear errno and LastError to clear the error of the
+            // WideCharToMultiByte() that failed
+            errno_clear();
+
+            // Get the required size
+            int required_size = WideCharToMultiByte(CP_UTF8, 0, src, src_len, NULL, 0, NULL, NULL);
+
+            // mallocz() never fails (exits the program on NULL)
+            char *tmp = mallocz(required_size * sizeof(char));
+
+            // Convert it, now it should fit
+            rc = WideCharToMultiByte(CP_UTF8, 0, src, src_len, tmp, required_size, NULL, NULL);
+            if (rc <= 0) {
+                // Conversion failed
+                *dst = '\0';
+                freez(tmp);
+                return 0;
+            }
+
+            size_t len = rc;
+
+            // Copy as much as we can
+            memcpy(dst, tmp, MIN(len, (dst_size - 1)) * sizeof(char));
+
+            // Null-terminate it
+            dst[MIN(len, (dst_size - 1))] = '\0';
+
+            // Free the temporary buffer
+            freez(tmp);
+
+            // Return the actual bytes written
+            return MIN(len, dst_size);
+        }
+
+        // Empty the destination
+        *dst = '\0';
+        return 0;
+    }
+
+    size_t len = rc;
+
+    if (len >= dst_size) {
+        // Truncate it to fit the null terminator
+        dst[dst_size - 1] = '\0';
+        return dst_size;
+    }
+
+    if (dst[len - 1] != '\0') {
+        // The result is not null-terminated
+        // Append the null terminator
+        dst[len] = '\0';
+        return len + 1;
+    }
+
+    // The result is already null-terminated
     return len;
 }
 #endif
--- src/libnetdata/string/utf8.h
+++ src/libnetdata/string/utf8.h
@@ -15,11 +15,16 @@
 // return an always null terminated wide string, truncate to given size if destination is not big enough,
 // src_len can be -1 use all of it.
 // returns zero on errors, > 0 otherwise (including the null, even if src is not null terminated).
-size_t any_to_utf16(uint32_t CodePage, wchar_t *utf16, size_t utf16_count, const char *src, int src_len);
+size_t any_to_utf16(uint32_t CodePage, wchar_t *dst, size_t dst_size, const char *src, int src_len);
 
-// always null terminated, truncated if it does not fit, src_len can be -1 use all of it.
+// always null terminated, truncated if it does not fit, src_len can be -1 to use all of it.
 // returns zero on errors, > 0 otherwise (including the null, even if src is not null terminated).
 #define utf8_to_utf16(utf16, utf16_count, src, src_len) any_to_utf16(CP_UTF8, utf16, utf16_count, src, src_len)
+
+// always null terminated, truncated if it does not fit, src_len can be -1 to use all of it.
+// returns zero on errors, > 0 otherwise (including the null, even if src is not null terminated).
+size_t utf16_to_utf8(char *dst, size_t dst_size, const wchar_t *src, int src_len);
+
 #endif
 
 #endif /* NETDATA_STRING_UTF8_H */
