6508312e996e33e439149e748c8ce84194def4d3
thiagoftsm <thiagoftsm@gmail.com>
2024-10-10 21:46:29 +0000

    dotnet_part1: Add JIT metrics

--- src/collectors/windows.plugin/perflib-netframework.c
+++ src/collectors/windows.plugin/perflib-netframework.c
@@ -34,6 +34,18 @@ struct net_framework_instances {
     RRDSET *st_clrinterop_interop_stubs_created;
     RRDDIM *rd_clrinterop_interop_stubs_created;
 
+    RRDSET *st_clrjit_methods;
+    RRDDIM *rd_clrjit_methods;
+
+    RRDSET *st_clrjit_time;
+    RRDDIM *rd_clrjit_time;
+
+    RRDSET *st_clrjit_standard_failures;
+    RRDDIM *rd_clrjit_standard_failures;
+
+    RRDSET *st_clrjit_il_bytes;
+    RRDDIM *rd_clrjit_il_bytes;
+
     COUNTER_DATA NETFrameworkCLRExceptionThrown;
     COUNTER_DATA NETFrameworkCLRExceptionFilters;
     COUNTER_DATA NETFrameworkCLRExceptionFinallys;
@@ -42,6 +54,12 @@ struct net_framework_instances {
     COUNTER_DATA NETFrameworkCLRInteropCOMCallableWrappers;
     COUNTER_DATA NETFrameworkCLRInteropMarshalling;
     COUNTER_DATA NETFrameworkCLRInteropStubsCreated;
+
+    COUNTER_DATA NETFrameworkCLRJITMethods;
+    COUNTER_DATA NETFrameworkCLRJITPercentTime;
+    COUNTER_DATA NETFrameworkCLRJITFrequencyTime;
+    COUNTER_DATA NETFrameworkCLRJITStandardFailures;
+    COUNTER_DATA NETFrameworkCLRJITIlBytes;
 };
 
 static inline void initialize_net_framework_processes_keys(struct net_framework_instances *p) {
@@ -53,6 +71,12 @@ static inline void initialize_net_framework_processes_keys(struct net_framework_
     p->NETFrameworkCLRInteropCOMCallableWrappers.key = "# of CCWs";
     p->NETFrameworkCLRInteropMarshalling.key = "# of Stubs";
     p->NETFrameworkCLRInteropStubsCreated.key = "# of marshalling";
+
+    p->NETFrameworkCLRJITMethods.key = "# of Methods Jitted";
+    p->NETFrameworkCLRJITPercentTime.key = "% Time in Jit";
+    p->NETFrameworkCLRJITFrequencyTime.key = "IL Bytes Jitted / sec";
+    p->NETFrameworkCLRJITStandardFailures.key = "Standard Jit Failures";
+    p->NETFrameworkCLRJITIlBytes.key = "# of IL Bytes Jitted";
 }
 
 void dict_net_framework_processes_insert_cb(const DICTIONARY_ITEM *item __maybe_unused, void *value, void *data __maybe_unused) {
@@ -84,7 +108,7 @@ static void netdata_framework_clr_exceptions(PERF_DATA_BLOCK *pDataBlock,
         if(!getInstanceName(pDataBlock, pObjectType, pi, windows_shared_buffer, sizeof(windows_shared_buffer)))
             strncpyz(windows_shared_buffer, "[unknown]", sizeof(windows_shared_buffer) - 1);
 
-        if(strcasecmp(windows_shared_buffer, "_Global") == 0)
+        if(strcasecmp(windows_shared_buffer, "_Global_") == 0)
             continue;
 
         netdata_fix_chart_name(windows_shared_buffer);
@@ -251,7 +275,7 @@ static void netdata_framework_clr_interop(PERF_DATA_BLOCK *pDataBlock,
         if(!getInstanceName(pDataBlock, pObjectType, pi, windows_shared_buffer, sizeof(windows_shared_buffer)))
             strncpyz(windows_shared_buffer, "[unknown]", sizeof(windows_shared_buffer) - 1);
 
-        if(strcasecmp(windows_shared_buffer, "_Global") == 0)
+        if(strcasecmp(windows_shared_buffer, "_Global_") == 0)
             continue;
 
         netdata_fix_chart_name(windows_shared_buffer);
@@ -373,6 +397,7 @@ static void netdata_framework_clr_jit(PERF_DATA_BLOCK *pDataBlock,
                                       int update_every)
 {
     PERF_INSTANCE_DEFINITION *pi = NULL;
+    char id[RRD_ID_LENGTH_MAX + 1];
     for(LONG i = 0; i < pObjectType->NumInstances ; i++) {
         pi = perflibForEachInstance(pDataBlock, pObjectType, pi);
         if (!pi)
@@ -381,8 +406,159 @@ static void netdata_framework_clr_jit(PERF_DATA_BLOCK *pDataBlock,
         if(!getInstanceName(pDataBlock, pObjectType, pi, windows_shared_buffer, sizeof(windows_shared_buffer)))
             strncpyz(windows_shared_buffer, "[unknown]", sizeof(windows_shared_buffer) - 1);
 
-        if(strcasecmp(windows_shared_buffer, "_Global") == 0)
+        if(strcasecmp(windows_shared_buffer, "_Global_") == 0)
             continue;
+
+        netdata_fix_chart_name(windows_shared_buffer);
+        struct net_framework_instances *p = dictionary_set(processes, windows_shared_buffer, NULL, sizeof(*p));
+
+        if (perflibGetObjectCounter(pDataBlock, pObjectType, &p->NETFrameworkCLRJITMethods)) {
+            snprintfz(id, RRD_ID_LENGTH_MAX, "%s_clrjit_methods", windows_shared_buffer);
+            if (!p->st_clrjit_methods) {
+                p->st_clrjit_methods = rrdset_create_localhost("netframework"
+                                                                                 , windows_shared_buffer, NULL
+                                                                                 , "jit"
+                                                                                 , "netframework.clrjit_methods"
+                                                                                 , "JIT-compiled methods"
+                                                                                 , "methods/s"
+                                                                                 , PLUGIN_WINDOWS_NAME
+                                                                                 , "PerflibNetFramework"
+                                                                                 , PRIO_NETFRAMEWORK_CLR_JIT_METHODS
+                                                                                 , update_every
+                                                                                 , RRDSET_TYPE_LINE
+                                                                                 );
+
+                snprintfz(id, RRD_ID_LENGTH_MAX, "netframework_%s_clrjit_methods_total", windows_shared_buffer);
+                p->rd_clrjit_methods  = rrddim_add(p->st_clrjit_methods,
+                                                  id,
+                                                  "jit-compiled",
+                                                  1,
+                                                  1,
+                                                  RRD_ALGORITHM_INCREMENTAL);
+
+                rrdlabels_add(p->st_clrjit_methods->rrdlabels,
+                              "process",
+                              windows_shared_buffer,
+                              RRDLABEL_SRC_AUTO);
+            }
+
+            rrddim_set_by_pointer(p->st_clrjit_methods,
+                                  p->rd_clrjit_methods,
+                                  (collected_number)p->NETFrameworkCLRJITMethods.current.Data);
+            rrdset_done(p->st_clrjit_methods);
+        }
+
+        if (perflibGetObjectCounter(pDataBlock, pObjectType, &p->NETFrameworkCLRJITFrequencyTime) &&
+            perflibGetObjectCounter(pDataBlock, pObjectType, &p->NETFrameworkCLRJITPercentTime)) {
+            snprintfz(id, RRD_ID_LENGTH_MAX, "%s_clrjit_time", windows_shared_buffer);
+            if (!p->st_clrjit_time) {
+                p->st_clrjit_time = rrdset_create_localhost("netframework"
+                                                            , windows_shared_buffer, NULL
+                                                            , "jit"
+                                                            , "netframework.clrjit_time"
+                                                            , "Time spent in JIT compilation"
+                                                            , "percentage"
+                                                            , PLUGIN_WINDOWS_NAME
+                                                            , "PerflibNetFramework"
+                                                            , PRIO_NETFRAMEWORK_CLR_JIT_TIME
+                                                            , update_every
+                                                            , RRDSET_TYPE_LINE
+                                                            );
+
+                snprintfz(id, RRD_ID_LENGTH_MAX, "netframework_%s_clrjit_time_percent", windows_shared_buffer);
+                p->rd_clrjit_time  = rrddim_add(p->st_clrjit_time,
+                                                   id,
+                                                   "time",
+                                                   1,
+                                                   100,
+                                                   RRD_ALGORITHM_ABSOLUTE);
+
+                rrdlabels_add(p->st_clrjit_time->rrdlabels,
+                              "process",
+                              windows_shared_buffer,
+                              RRDLABEL_SRC_AUTO);
+            }
+
+            double percTime = (double) p->NETFrameworkCLRJITPercentTime.current.Data;
+            percTime /= (double) p->NETFrameworkCLRJITFrequencyTime.current.Data;
+            percTime *= 100;
+            rrddim_set_by_pointer(p->st_clrjit_time,
+                                  p->rd_clrjit_time,
+                                  (collected_number) percTime);
+            rrdset_done(p->st_clrjit_time);
+        }
+
+        if (perflibGetObjectCounter(pDataBlock, pObjectType, &p->NETFrameworkCLRJITStandardFailures)) {
+            snprintfz(id, RRD_ID_LENGTH_MAX, "%s_clrjit_standard_failures", windows_shared_buffer);
+            if (!p->st_clrjit_standard_failures) {
+                p->st_clrjit_standard_failures = rrdset_create_localhost("netframework"
+                                                                         , windows_shared_buffer, NULL
+                                                                         , "jit"
+                                                                         , "netframework.clrjit_standard_failures"
+                                                                         , "JIT compiler failures"
+                                                                         , "failures/s"
+                                                                         , PLUGIN_WINDOWS_NAME
+                                                                         , "PerflibNetFramework"
+                                                                         , PRIO_NETFRAMEWORK_CLR_JIT_STANDARD_FAILURES
+                                                                         , update_every
+                                                                         , RRDSET_TYPE_LINE
+                                                                         );
+
+                snprintfz(id, RRD_ID_LENGTH_MAX, "netframework_%s_clrjit_standard_failures_total", windows_shared_buffer);
+                p->rd_clrjit_standard_failures  = rrddim_add(p->st_clrjit_standard_failures,
+                                                            id,
+                                                            "failures",
+                                                            1,
+                                                            1,
+                                                            RRD_ALGORITHM_INCREMENTAL);
+
+                rrdlabels_add(p->st_clrjit_standard_failures->rrdlabels,
+                              "process",
+                              windows_shared_buffer,
+                              RRDLABEL_SRC_AUTO);
+            }
+
+            rrddim_set_by_pointer(p->st_clrjit_standard_failures,
+                                  p->rd_clrjit_standard_failures,
+                                  (collected_number)p->NETFrameworkCLRJITStandardFailures.current.Data);
+            rrdset_done(p->st_clrjit_standard_failures);
+        }
+
+        if (perflibGetObjectCounter(pDataBlock, pObjectType, &p->NETFrameworkCLRJITIlBytes)) {
+            snprintfz(id, RRD_ID_LENGTH_MAX, "%s_clrjit_il_bytes", windows_shared_buffer);
+            if (!p->st_clrjit_il_bytes) {
+                p->st_clrjit_il_bytes = rrdset_create_localhost("netframework"
+                                                                , windows_shared_buffer, NULL
+                                                                , "jit"
+                                                                , "netframework.clrjit_il_bytes"
+                                                                , "Compiled Microsoft intermediate language (MSIL) bytes"
+                                                                , "bytes/s"
+                                                                , PLUGIN_WINDOWS_NAME
+                                                                , "PerflibNetFramework"
+                                                                , PRIO_NETFRAMEWORK_CLR_JIT_IL_BYTES
+                                                                , update_every
+                                                                , RRDSET_TYPE_LINE
+                                                                );
+
+                snprintfz(id, RRD_ID_LENGTH_MAX, "netframework_%s_clrjit_il_bytes_total", windows_shared_buffer);
+                p->rd_clrjit_il_bytes  = rrddim_add(p->st_clrjit_il_bytes,
+                                                   id,
+                                                   "compiled_msil",
+                                                   1,
+                                                   1,
+                                                   RRD_ALGORITHM_INCREMENTAL);
+
+                rrdlabels_add(p->st_clrjit_il_bytes->rrdlabels,
+                              "process",
+                              windows_shared_buffer,
+                              RRDLABEL_SRC_AUTO);
+            }
+
+            rrddim_set_by_pointer(p->st_clrjit_il_bytes,
+                                  p->rd_clrjit_il_bytes,
+                                  (collected_number)p->NETFrameworkCLRJITIlBytes.current.Data);
+            rrdset_done(p->st_clrjit_il_bytes);
+        }
     }
 }
 
@@ -400,7 +576,7 @@ static void netdata_framework_clr_loading(PERF_DATA_BLOCK *pDataBlock,
         if(!getInstanceName(pDataBlock, pObjectType, pi, windows_shared_buffer, sizeof(windows_shared_buffer)))
             strncpyz(windows_shared_buffer, "[unknown]", sizeof(windows_shared_buffer) - 1);
 
-        if(strcasecmp(windows_shared_buffer, "_Global") == 0)
+        if(strcasecmp(windows_shared_buffer, "_Global_") == 0)
             continue;
     }
 }
