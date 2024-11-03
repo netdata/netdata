d5cea5453536387a3d1f0494c073a46ea9a0c208
Costa Tsaousis <costa@netdata.cloud>
2024-11-03 21:50:25 +0200

    fix random for all operating systems

--- src/libnetdata/os/random.c
+++ src/libnetdata/os/random.c
@@ -3,32 +3,35 @@
 #include "libnetdata/libnetdata.h"
 
 void os_random_init(OS_RANDOM *jt, unsigned int seed) {
-    if(!seed)
+    if (!seed)
         seed = (unsigned int)(os_gettid() + now_monotonic_usec());
 
+    jt->seed = seed;
+
+#ifdef RANDOM_USE_RANDOM_R
     memset(&jt->rand_state, 0, sizeof(jt->rand_state));
     initstate_r(seed, jt->rand_state_buf, sizeof(jt->rand_state_buf), &jt->rand_state);
+#endif
 }
 
 uint64_t os_random(OS_RANDOM *jt, uint64_t max) {
-    uint64_t rand_val = 0;
-    int32_t temp;
+    if (max == 0) return 0;
 
-    // Determine the number of random_r() calls based on the value of max
+    uint64_t rand_val = 0;
+
+#if defined(RANDOM_USE_RANDOM_R)
     if (max <= (1ULL << 31)) {
-        // Case 1: max fits in 31 bits, 1 call
+        int32_t temp;
         random_r(&jt->rand_state, &temp);
         rand_val = (uint64_t)temp & 0x7FFFFFFF;
-    }
-    else if (max <= (1ULL << 62)) {
-        // Case 2: max fits in 62 bits, 2 calls
+    } else if (max <= (1ULL << 62)) {
+        int32_t temp;
         random_r(&jt->rand_state, &temp);
         rand_val = (uint64_t)temp & 0x7FFFFFFF;
         random_r(&jt->rand_state, &temp);
         rand_val |= ((uint64_t)temp & 0x7FFFFFFF) << 31;
-    }
-    else {
-        // Case 3: max requires up to 64 bits, 3 calls
+    } else {
+        int32_t temp;
         random_r(&jt->rand_state, &temp);
         rand_val = (uint64_t)temp & 0x7FFFFFFF;
         random_r(&jt->rand_state, &temp);
@@ -36,6 +39,28 @@ uint64_t os_random(OS_RANDOM *jt, uint64_t max) {
         random_r(&jt->rand_state, &temp);
         rand_val |= ((uint64_t)temp & 0x3) << 62;  // Only take 2 bits from the third call
     }
+
+#elif defined(RANDOM_USE_ARC4RANDOM)
+    if (max <= UINT32_MAX)
+        rand_val = arc4random_uniform((uint32_t)max);
+    else {
+        rand_val  = (uint64_t)arc4random_uniform(UINT32_MAX);
+        rand_val |= ((uint64_t)arc4random_uniform(UINT32_MAX) << 32);
+    }
+
+#elif defined(RANDOM_USE_RAND_S)
+    if (max <= UINT32_MAX) {
+        unsigned int rand_num;
+        rand_s(&rand_num);
+        rand_val = (uint64_t)rand_num;
+    } else {
+        unsigned int rand_num;
+        rand_s(&rand_num);
+        rand_val = (uint64_t)rand_num;
+        rand_s(&rand_num);
+        rand_val |= ((uint64_t)rand_num << 32);
+    }
+#endif
 
     return rand_val % max;
 }
--- src/libnetdata/os/random.h
+++ src/libnetdata/os/random.h
@@ -5,9 +5,22 @@
 
 #include "libnetdata/common.h"
 
+#if defined(OS_LINUX) || defined(OS_FREEBSD)
+#define RANDOM_USE_RANDOM_R
+#elif defined(OS_MACOS)
+#define RANDOM_USE_ARC4RANDOM
+#elif defined(OS_WINDOWS)
+#define RANDOM_USE_RAND_S
+#else
+#error "Can't use any random number generator"
+#endif
+
 typedef struct {
-    struct random_data rand_state;       // Embedding random_data directly
-    char rand_state_buf[64];             // Buffer for random state
+    unsigned int seed;
+#ifdef RANDOM_USE_RANDOM_R
+    struct random_data rand_state;
+    char rand_state_buf[64];
+#endif
 } OS_RANDOM;
 
 void os_random_init(OS_RANDOM *jt, unsigned int seed);
