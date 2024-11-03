c48ca1ab0d5a80eea8b777e445ede44b5f1bcb29
Costa Tsaousis <costa@netdata.cloud>
2024-11-04 00:20:27 +0200

    use the buffer versions of the random generators; fallback to using random()

--- src/libnetdata/os/random.c
+++ src/libnetdata/os/random.c
@@ -6,61 +6,75 @@
 #include <sys/random.h>
 #endif
 
+#if !defined(HAVE_ARC4RANDOM_BUF) && !defined(HAVE_GETRANDOM) && !defined(HAVE_RAND_S)
+static __attribute__((constructor)) void seed_random() {
+    // Use current time and process ID to create a high-entropy seed
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    uint32_t seed = (uint32_t)(tv.tv_sec ^ tv.tv_usec ^ getpid());
+
+    // Seed the random number generator
+    srandom(seed);
+}
+#endif
+
 // return a random number 0 to max - 1
 uint64_t os_random(uint64_t max) {
     if (max <= 1) return 0;
 
-    union {
-        uint64_t u64;
-        struct {
-            uint32_t hi;
-            uint32_t lo;
-        } u32;
-    } value;
+    uint64_t value;
 
-#if defined(HAVE_ARC4RANDOM_UNIFORM)
-    if (max <= UINT32_MAX) {
-        value.u64 = arc4random_uniform((uint32_t)max);
-    } else {
-        value.u32.lo = arc4random_uniform(UINT32_MAX);
-        value.u32.hi = arc4random_uniform(UINT32_MAX);
-    }
-
-#elif defined(HAVE_RAND_S)
-    if (max <= UINT32_MAX) {
-        unsigned int temp;
-        rand_s(&temp);
-        value.u64 = temp;
-    } else {
-        unsigned int temp_lo, temp_hi;
-        rand_s(&temp_lo);
-        rand_s(&temp_hi);
-        value.u32.lo = temp_lo;
-        value.u32.hi = temp_hi;
-    }
+#if defined(HAVE_ARC4RANDOM_BUF)
+    if (max <= UINT8_MAX) {
+        uint8_t v;
+        arc4random_buf(&v, sizeof(v));
+        value = v;
+    } else if(max <= UINT16_MAX) {
+        uint16_t v;
+        arc4random_buf(&v, sizeof(v));
+        value = v;
+    } else if (max <= UINT32_MAX) {
+        uint32_t v;
+        arc4random_buf(&v, sizeof(v));
+        value = v;
+    } else
+        arc4random_buf(&value, sizeof(value));
 
 #elif defined(HAVE_GETRANDOM)
     if (max <= UINT8_MAX) {
         uint8_t v;
         getrandom(&v, sizeof(v), 0);
-        value.u64 = v;
+        value = v;
     } else if(max <= UINT16_MAX) {
         uint16_t v;
         getrandom(&v, sizeof(v), 0);
-        value.u64 = v;
+        value = v;
     } else if (max <= UINT32_MAX) {
         uint32_t v;
         getrandom(&v, sizeof(v), 0);
-        value.u64 = v;
+        value = v;
+    } else
+        getrandom(&value, sizeof(value), 0);
+
+#elif defined(HAVE_RAND_S)
+    if (max <= UINT_MAX) {
+        unsigned int temp;
+        rand_s(&temp);
+        value = temp;
     } else {
-        uint64_t v;
-        getrandom(&v, sizeof(v), 0);
-        value.u64 = v;
+        unsigned int temp_lo, temp_hi;
+        rand_s(&temp_lo);
+        rand_s(&temp_hi);
+        value = ((uint64_t)temp_hi << 32) + (uint64_t)temp_lo;
     }
 
 #else
-#error "Can't use any random number generator"
+    if(max <= INT32_MAX)
+        value = random();
+    else
+        value = ((uint64_t) random() << 33) | ((uint64_t) random() << 2) | (random() & 0x3);
 #endif
 
-    return value.u64 % max;
+    return value % max;
 }
