09eeadedae8c9865df91fc52b3adea0992080450
Austin S. Hemmelgarn <austin@netdata.cloud>
2024-11-08 07:35:12 -0500

    Properly include curl for updater check.

--- .github/scripts/ci-support-pkgs.sh
+++ .github/scripts/ci-support-pkgs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # This script installs supporting packages needed for CI, which provide following:
-# cron, pidof
+# curl, cron, pidof
 
 set -e
 
@@ -9,10 +9,13 @@ set -e
 
 case "${ID}" in
     amzn|almalinux|centos|fedora)
-        dnf install -y procps-ng cronie cronie-anacron || \
-        yum install -y procps-ng cronie cronie-anacron
+        dnf install -y procps-ng cronie cronie-anacron curl || \
+        yum install -y procps-ng cronie cronie-anacron curl
         ;;
     arch)
-        pacman -S --noconfirm cronie
+        pacman -S --noconfirm cronie curl
+        ;;
+    debian|ubuntu)
+        apt update && apt install -y procps-ng cronie anacron curl
         ;;
 esac
