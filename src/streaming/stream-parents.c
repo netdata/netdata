c74cd3207a2d6703a992ebb71e503708dd29d759
Costa Tsaousis <costa@netdata.cloud>
2024-11-10 23:40:45 +0200

    fix for corruption

--- src/streaming/stream-parents.c
+++ src/streaming/stream-parents.c
@@ -469,8 +469,6 @@ bool stream_parent_connect_to_one(
                     similar++;
                 else
                     break;
-
-                similar++;
             }
 
             // if we have only 1 similar, move on
@@ -492,7 +490,7 @@ bool stream_parent_connect_to_one(
                 rrdhost_stream_path_check_corruption(host, "stream_parent_connect_to_one - before randomizer");
                 while (similar > 1) {
                     size_t chosen = base;
-                    for(size_t i = base + 1 ; i < base + similar && i < count ;i++) {
+                    for(size_t i = base + 1 ; i < base + similar ;i++) {
                         uint32_t i_nonce = array[i]->remote.nonce | os_random32();
                         uint32_t chosen_nonce = array[chosen]->remote.nonce | os_random32();
                         if(i_nonce > chosen_nonce) chosen = i;
