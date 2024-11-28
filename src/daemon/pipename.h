caa2e2670ba9d900b53c08498be2ded08248036a
vkalintiris <vasilis@netdata.cloud>
2024-11-28 08:19:38 +0200

    Use CLI11 to provide proper argument/option parsing to netdatacli

--- src/daemon/pipename.h
+++ src/daemon/pipename.h
@@ -3,6 +3,14 @@
 #ifndef DAEMON_PIPENAME_H
 #define DAEMON_PIPENAME_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 const char *daemon_pipename(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* DAEMON_PIPENAME_H */
