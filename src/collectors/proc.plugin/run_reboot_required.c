bc294b81c62c7b2f7e2ee0edc44f8ad75e1e8cf7
ilyam8 <ilya@netdata.cloud>
2024-11-29 22:51:33 +0200

    disable if not mounted in container

--- src/collectors/proc.plugin/run_reboot_required.c
+++ src/collectors/proc.plugin/run_reboot_required.c
@@ -1,19 +1,43 @@
 #include "plugin_proc.h"
 
+static int is_dir_mounted(const char *path) {
+    struct stat stat_buf, parent_stat_buf;
+    char parent_path[PATH_MAX];
+
+    if (stat(path, &stat_buf) == -1) {
+        return -1;
+    }
+
+    snprintfz(parent_path, sizeof(parent_path), "%s/..", path);
+    if (stat(parent_path, &parent_stat_buf) == -1) {
+        return -1;
+    }
+
+    return stat_buf.st_dev != parent_stat_buf.st_dev;
+}
+
 int do_run_reboot_required(int update_every, usec_t dt) {
     (void)dt;
 
     static char *signal_file_path = NULL;
+    static RRDSET *st = NULL;
+    static RRDDIM *rd_required = NULL;
+    static RRDDIM *rd_not_required = NULL;
 
     if (!signal_file_path) {
         char filename[FILENAME_MAX + 1];
+        snprintfz(filename, FILENAME_MAX, "%s%s", netdata_configured_host_prefix, "/var/run");
+
+        // Disable monitoring if running inside a container and the directory is not mounted from the host
+        if (getenv("NETDATA_LISTENER_PORT") != NULL) {
+            if (!netdata_configured_host_prefix || !*netdata_configured_host_prefix || is_dir_mounted(filename) != 1) {
+                return 1;
+            }
+        }
+
         snprintfz(filename, FILENAME_MAX, "%s%s", netdata_configured_host_prefix, "/var/run/reboot-required");
         signal_file_path = strdupz(filename);
     }
-
-    static RRDSET *st = NULL;
-    static RRDDIM *rd_yes = NULL;
-    static RRDDIM *rd_no = NULL;
 
     if (unlikely(!st)) {
         st = rrdset_create_localhost(
@@ -30,15 +54,15 @@ int do_run_reboot_required(int update_every, usec_t dt) {
             update_every,
             RRDSET_TYPE_LINE);
 
-        rd_yes = rrddim_add(st, "required", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
-        rd_no = rrddim_add(st, "not_required", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
+        rd_required = rrddim_add(st, "required", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
+        rd_not_required = rrddim_add(st, "not_required", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
     }
 
     struct stat buf;
     bool exists = (stat(signal_file_path, &buf) == 0);
 
-    rrddim_set_by_pointer(st, rd_yes, exists ? 1 : 0);
-    rrddim_set_by_pointer(st, rd_no, exists ? 0 : 1);
+    rrddim_set_by_pointer(st, rd_required, exists ? 1 : 0);
+    rrddim_set_by_pointer(st, rd_not_required, exists ? 0 : 1);
     rrdset_done(st);
 
     return 0;
