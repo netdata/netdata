fdc8870c07f7b339dfae2a6b75ddee72ebe8fc4e
Costa Tsaousis <costa@netdata.cloud>
2024-12-04 09:48:01 +0200

    Revert "Add fuzzer for pluginsd line splitter."
    This reverts commit 454cbcf6e107811a5d7b0265888dfd74abac052c.

--- src/libnetdata/line_splitter/re2c_line_splitter.re.c
+++ src/libnetdata/line_splitter/re2c_line_splitter.re.c
@@ -5,16 +5,15 @@
 size_t quoted_strings_splitter_pluginsd_re2c(char *start, char **words, size_t max_words)
 {
     size_t count = 0;
-    char *YYMARKER = NULL;
 
-    char *YYCURSOR = start;
+    const char *YYCURSOR = start;
     for (;;) {
     /*!re2c
         re2c:define:YYCTYPE = char;
         re2c:yyfill:enable = 0;
 
-        single_quotes_word = ["] [^"\x00]* ["];
-        double_quotes_word = ['] [^'\x00]* ['];
+        single_quotes_word = ["] [^"]* ["];
+        double_quotes_word = ['] [^']* ['];
         unquoted_word = [^= "'\t\n\v\f\r\x00]+;
         whitespace = [= \t\n\v\f\r]+;
 
@@ -42,11 +41,9 @@ size_t quoted_strings_splitter_pluginsd_re2c(char *start, char **words, size_t m
             if (count == max_words)
                 return count;
 
-            if (*YYCURSOR != '\0')
-                *YYCURSOR++ = '\0';
-
+            start[YYCURSOR - start] = '\0';
             words[count++] = start;
-            start = YYCURSOR;
+            start = (char *) ++YYCURSOR;
             continue;
         }
         whitespace {
