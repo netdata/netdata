52e88ab2d3721bd77574bcd30d9cf4f202120c13
Costa Tsaousis <costa@netdata.cloud>
2024-12-04 09:48:01 +0200

    Revert "Use re2c for the line splitting pluginsd."
    This reverts commit 2b2f9d38877780f3058727c343e67b902c5b615d.

--- src/libnetdata/line_splitter/README.md
+++ src/libnetdata/line_splitter/README.md
@@ -1,8 +1,5 @@
-# Line splitter
+# Log
 
-Common code that handles line splitting into words.
+The netdata log library supports debug, info, error and fatal error logging. 
+By default we have an access log, an error log and a collectors log. 
 
-The pluginsd line splitter is generated with re2c because it leads to 3x the
-performance of the hand-written line splitter:
-
-`re2c re2c_line_splitter.re.c -o re2c_line_splitter.c`
--- src/libnetdata/line_splitter/line_splitter.h
+++ src/libnetdata/line_splitter/line_splitter.h
@@ -108,8 +108,6 @@ static inline size_t quoted_strings_splitter(char *str, char **words, size_t max
     return i;
 }
 
-size_t quoted_strings_splitter_pluginsd_re2c(char *start, char **words, size_t max_words);
-
 #define quoted_strings_splitter_whitespace(str, words, max_words) \
         quoted_strings_splitter(str, words, max_words, isspace_map_whitespace)
 
@@ -120,7 +118,7 @@ size_t quoted_strings_splitter_pluginsd_re2c(char *start, char **words, size_t m
         quoted_strings_splitter(str, words, max_words, isspace_map_config)
 
 #define quoted_strings_splitter_pluginsd(str, words, max_words) \
-        quoted_strings_splitter_pluginsd_re2c(str, words, max_words)
+        quoted_strings_splitter(str, words, max_words, isspace_map_pluginsd)
 
 #define quoted_strings_splitter_dyncfg_id(str, words, max_words) \
         quoted_strings_splitter(str, words, max_words, isspace_dyncfg_id_map)
