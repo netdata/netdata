0d83213018b1d2aed7690597d2866c278683b4ad
vkalintiris <vasilis@netdata.cloud>
2024-12-05 14:25:14 +0200

    Optimize ML queuing operations.
    - Allocate 25% of cores for ML.
    - Split queues by request type.
    - Accurate stats for queue operations by type.

--- src/ml/ml_queue.cc
+++ src/ml/ml_queue.cc
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include "ml/ml_queue.h"
 #include "ml_private.h"
 
-ml_queue_t * ml_queue_init()
+ml_queue_t *ml_queue_init()
 {
     ml_queue_t *q = new ml_queue_t();
 
@@ -22,7 +23,23 @@ void ml_queue_destroy(ml_queue_t *q)
 void ml_queue_push(ml_queue_t *q, const ml_queue_item_t req)
 {
     netdata_mutex_lock(&q->mutex);
-    q->internal.push(req);
+
+    switch (req.type) {
+        case ML_QUEUE_ITEM_TYPE_CREATE_NEW_MODEL:
+            q->create_model_queue.push(req.create_new_model);
+            q->stats.total_create_new_model_requests_pushed += 1;
+            break;
+
+        case ML_QUEUE_ITEM_TYPE_ADD_EXISTING_MODEL:
+            q->add_model_queue.push(req.add_existing_model);
+            q->stats.total_add_existing_model_requests_pushed += 1;
+            break;
+
+        case ML_QUEUE_ITEM_STOP_REQUEST:
+            // Stop requests don't need to be queued
+            break;
+    }
+
     pthread_cond_signal(&q->cond_var);
     netdata_mutex_unlock(&q->mutex);
 }
@@ -34,7 +51,7 @@ ml_queue_item_t ml_queue_pop(ml_queue_t *q)
     ml_queue_item_t req;
     req.type = ML_QUEUE_ITEM_STOP_REQUEST;
 
-    while (q->internal.empty()) {
+    while (q->create_model_queue.empty() && q->add_model_queue.empty()) {
         pthread_cond_wait(&q->cond_var, &q->mutex);
 
         if (q->exit) {
@@ -43,19 +60,33 @@ ml_queue_item_t ml_queue_pop(ml_queue_t *q)
         }
     }
 
-    req = q->internal.front();
-    q->internal.pop();
+    // Prioritize adding model requests
+    if (!q->add_model_queue.empty()) {
+        req.type = ML_QUEUE_ITEM_TYPE_ADD_EXISTING_MODEL;
+        req.add_existing_model = q->add_model_queue.front();
+        q->add_model_queue.pop();
+        q->stats.total_add_existing_model_requests_popped += 1;
+    } else if (!q->create_model_queue.empty()) {
+        req.type = ML_QUEUE_ITEM_TYPE_CREATE_NEW_MODEL;
+        req.create_new_model = q->create_model_queue.front();
+        q->create_model_queue.pop();
+        q->stats.total_create_new_model_requests_popped += 1;
+    }
 
     netdata_mutex_unlock(&q->mutex);
     return req;
 }
 
-size_t ml_queue_size(ml_queue_t *q)
+ml_queue_size_t ml_queue_size(ml_queue_t *q)
 {
     netdata_mutex_lock(&q->mutex);
-    size_t size = q->internal.size();
+    ml_queue_size_t qs = ml_queue_size_t {
+        q->create_model_queue.size(),
+        q->add_model_queue.size(),
+    };
     netdata_mutex_unlock(&q->mutex);
-    return size;
+
+    return qs;
 }
 
 void ml_queue_signal(ml_queue_t *q)
@@ -64,4 +95,13 @@ void ml_queue_signal(ml_queue_t *q)
     q->exit = true;
     pthread_cond_signal(&q->cond_var);
     netdata_mutex_unlock(&q->mutex);
+}
+
+ml_queue_stats_t ml_queue_stats(ml_queue_t *q)
+{
+    netdata_mutex_lock(&q->mutex);
+    ml_queue_stats_t stats = q->stats;
+    netdata_mutex_unlock(&q->mutex);
+
+    return stats;
 }
