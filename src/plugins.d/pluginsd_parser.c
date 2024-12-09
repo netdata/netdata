378f788c25996336141004f98977656e6c0726e3
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2024-12-09 12:08:09 +0200

    Update vnode host info metadata to cloud

--- src/plugins.d/pluginsd_parser.c
+++ src/plugins.d/pluginsd_parser.c
@@ -219,7 +219,7 @@ static inline PARSER_RC pluginsd_host_define_end(char **words __maybe_unused, si
 
     rrdhost_flag_clear(host, RRDHOST_FLAG_ORPHAN);
     rrdcontext_host_child_connected(host);
-    schedule_node_state_update(host, 100);
+    aclk_queue_node_info(host, true);
 
     return PARSER_RC_OK;
 }
