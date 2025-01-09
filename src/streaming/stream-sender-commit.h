340310a5eff9bf3b9eb73b8ba475a91048661d70
Costa Tsaousis <costa@netdata.cloud>
2025-01-09 11:04:26 +0200

    fix comments

--- src/streaming/stream-sender-commit.h
+++ src/streaming/stream-sender-commit.h
@@ -24,18 +24,20 @@ void sender_buffer_destroy(struct sender_buffer *commit);
 // thread buffer for sending data upstream (to a parent)
 
 void sender_thread_buffer_free(void);
+
+// get the thread buffer
+// this is the preferred buffer for dedicated workers sending a lot of messages (like replication)
+// these threads need to maintain enough allocation for repeated use of the buffer
 BUFFER *sender_thread_buffer_with_trace(struct sender_state *s, const char *func);
 #define sender_thread_buffer(s) sender_thread_buffer_with_trace(s, __FUNCTION__)
 
-// commit the global host buffer
+// get the global host buffer
 // this is the preferred buffer for stream threads (unified receiver / sender threads)
 // these threads require a buffer that can remain intact while switching hosts
 BUFFER *sender_host_buffer_with_trace(struct rrdhost *host, const char *func);
 #define sender_host_buffer(host) sender_host_buffer_with_trace(host, __FUNCTION__)
 
-// commit a buffer acquired with sender_thread_buffer()
-// this is the preferred buffer for dedicated workers sending a lot of messages (like replication)
-// these threads need to maintain enough allocation for repeated use of the buffer
+// commit a buffer acquired with sender_thread_buffer() or sender_host_buffer()
 void sender_thread_commit_with_trace(struct sender_state *s, BUFFER *wb, STREAM_TRAFFIC_TYPE type, const char *func);
 #define sender_commit(s, wb, type) sender_thread_commit_with_trace(s, wb, type, __FUNCTION__)
 
