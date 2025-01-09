472476dbc97800af9b8c217b520d1a8d1e64ef81
Costa Tsaousis <costa@netdata.cloud>
2025-01-09 22:08:04 +0200

    avoid checking replication status all the time

--- src/streaming/stream-receiver-internals.h
+++ src/streaming/stream-receiver-internals.h
@@ -81,6 +81,7 @@ struct receiver_state {
         uint32_t last_counter_in;   // copy from the host, to detect progress
         uint32_t last_counter_out;  // copy from the host, to detect progress
         usec_t last_progress_ut;    // last time we found some progress (monotonic)
+        usec_t last_checked_ut;     // last time we checked for stalled progress (monotonic)
 
         time_t first_time_s;
     } replication;
