6ab9f23551404f0c38f26a9bfa386086c5119f3a
Costa Tsaousis <costa@netdata.cloud>
2025-01-14 15:43:36 +0200

    added windows version of get_system_pagesize()

--- src/libnetdata/os/get_system_pagesize.c
+++ src/libnetdata/os/get_system_pagesize.c
@@ -2,6 +2,25 @@
 
 #include "get_system_pagesize.h"
 
+#if defined(OS_WINDOWS)
+#include <windows.h>
+
+size_t os_get_system_page_size(void) {
+    static size_t page_size = 0;
+
+    if (!page_size) {
+        SYSTEM_INFO sys_info;
+        GetSystemInfo(&sys_info);
+
+        // Set the page size, default to 4096 if for some reason it's invalid
+        page_size = (sys_info.dwPageSize >= 4096) ? sys_info.dwPageSize : 4096;
+    }
+
+    return page_size;
+}
+
+#else
+
 size_t os_get_system_page_size(void) {
     static long int page_size = 0;
 
@@ -13,3 +32,5 @@ size_t os_get_system_page_size(void) {
 
     return page_size;
 }
+
+#endif
