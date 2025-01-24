67d226a91196a9a501f612a686772cdbdf846059
Austin S. Hemmelgarn <austin@netdata.cloud>
2025-01-24 08:50:51 -0500

    Fix libnetfilter_acct build (Debian doesn’t ship a static libmnl).

--- packaging/makeself/jobs/30-libnetfilter_acct.install.sh
+++ packaging/makeself/jobs/30-libnetfilter_acct.install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-# Install the libnetfilter_acct and it's dependency libmnl
-
+# Install the libnetfilter_acct
 
 # shellcheck source=packaging/makeself/functions.sh
 . "$(dirname "${0}")/../functions.sh" "${@}" || exit 1
@@ -12,15 +11,14 @@
 # shellcheck disable=SC2015
 [ "${GITHUB_ACTIONS}" = "true" ] && echo "::group::building libnetfilter_acct" || true
 
-export CFLAGS="${TUNING_FLAGS} -static -I/usr/include/libmnl -pipe"
+export CFLAGS="${TUNING_FLAGS} -static -pipe"
 export CXXFLAGS="${CFLAGS}"
-export LDFLAGS="-static -L/usr/lib -lmnl"
+export LDFLAGS="-static"
 export PKG_CONFIG="pkg-config --static"
-export PKG_CONFIG_PATH="/usr/lib/pkgconfig"
+export PKG_CONFIG_PATH="/libmnl-static/lib/pkgconfig"
 
 fetch "libnetfilter_acct-${LIBNETFILTER_ACT_VERSION}" "${LIBNETFILTER_ACT_SOURCE}/libnetfilter_acct-${LIBNETFILTER_ACT_VERSION}.tar.bz2" \
     "${LIBNETFILTER_ACT_ARTIFACT_SHA256}" libnetfilter_acct
-
 
 if [ "${CACHE_HIT:-0}" -eq 0 ]; then
     run ./configure \
@@ -34,7 +32,6 @@ fi
 run make install
 
 store_cache libnetfilter_acct "${NETDATA_MAKESELF_PATH}/tmp/libnetfilter_acct-${LIBNETFILTER_ACT_VERSION}"
-
 
 # shellcheck disable=SC2015
 [ "${GITHUB_ACTIONS}" = "true" ] && echo "::endgroup::" || true
