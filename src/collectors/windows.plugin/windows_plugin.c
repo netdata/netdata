13236b69a26f36d6e4767a345120b3d3aa0b7c5e
thiagoftsm <thiagoftsm@gmail.com>
2025-01-31 14:10:18 +0000

    Reformat (Part II)

--- src/collectors/windows.plugin/windows_plugin.c
+++ src/collectors/windows.plugin/windows_plugin.c
@@ -13,43 +13,53 @@ static struct proc_module {
 } win_modules[] = {
 
     // system metrics
-    {.name = "GetSystemUptime",     .dim = "GetSystemUptime",      .enabled = CONFIG_BOOLEAN_YES, .func = do_GetSystemUptime},
-    {.name = "GetSystemRAM",        .dim = "GetSystemRAM",         .enabled = CONFIG_BOOLEAN_YES, .func = do_GetSystemRAM},
+    {.name = "GetSystemUptime", .dim = "GetSystemUptime", .enabled = CONFIG_BOOLEAN_YES, .func = do_GetSystemUptime},
+    {.name = "GetSystemRAM", .dim = "GetSystemRAM", .enabled = CONFIG_BOOLEAN_YES, .func = do_GetSystemRAM},
 
     // the same is provided by PerflibProcessor, with more detailed analysis
     //{.name = "GetSystemCPU",        .dim = "GetSystemCPU",       .enabled = CONFIG_BOOLEAN_YES, .func = do_GetSystemCPU},
 
-    {.name = "PerflibProcesses",    .dim = "PerflibProcesses",     .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibProcesses},
-    {.name = "PerflibProcessor",    .dim = "PerflibProcessor",     .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibProcessor},
-    {.name = "PerflibMemory",       .dim = "PerflibMemory",        .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibMemory},
-    {.name = "PerflibStorage",      .dim = "PerflibStorage",       .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibStorage},
-    {.name = "PerflibNetwork",      .dim = "PerflibNetwork",       .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibNetwork},
-    {.name = "PerflibObjects",      .dim = "PerflibObjects",       .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibObjects},
-    {.name = "PerflibHyperV",       .dim = "PerflibHyperV",        .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibHyperV},
+    {.name = "PerflibProcesses", .dim = "PerflibProcesses", .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibProcesses},
+    {.name = "PerflibProcessor", .dim = "PerflibProcessor", .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibProcessor},
+    {.name = "PerflibMemory", .dim = "PerflibMemory", .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibMemory},
+    {.name = "PerflibStorage", .dim = "PerflibStorage", .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibStorage},
+    {.name = "PerflibNetwork", .dim = "PerflibNetwork", .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibNetwork},
+    {.name = "PerflibObjects", .dim = "PerflibObjects", .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibObjects},
+    {.name = "PerflibHyperV", .dim = "PerflibHyperV", .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibHyperV},
 
-    {.name = "PerflibThermalZone",  .dim = "PerflibThermalZone",   .enabled = CONFIG_BOOLEAN_NO, .func = do_PerflibThermalZone},
+    {.name = "PerflibThermalZone",
+     .dim = "PerflibThermalZone",
+     .enabled = CONFIG_BOOLEAN_NO,
+     .func = do_PerflibThermalZone},
 
-    {.name = "PerflibWebService",  .dim = "PerflibWebService",     .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibWebService},
-    {.name = "PerflibMSSQL",  .dim = "PerflibMSSQL",               .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibMSSQL},
+    {.name = "PerflibWebService",
+     .dim = "PerflibWebService",
+     .enabled = CONFIG_BOOLEAN_YES,
+     .func = do_PerflibWebService},
+    {.name = "PerflibMSSQL", .dim = "PerflibMSSQL", .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibMSSQL},
 
-    {.name = "PerflibNetFramework",  .dim = "PerflibNetFramework", .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibNetFramework},
-    {.name = "PerflibAD",            .dim = "PerflibAD",           .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibAD},
+    {.name = "PerflibNetFramework",
+     .dim = "PerflibNetFramework",
+     .enabled = CONFIG_BOOLEAN_YES,
+     .func = do_PerflibNetFramework},
+    {.name = "PerflibAD", .dim = "PerflibAD", .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibAD},
 
-    {.name = "PerflibADCS",         .dim = "PerflibADCS",          .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibADCS},
+    {.name = "PerflibADCS", .dim = "PerflibADCS", .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibADCS},
 
-    {.name = "PerflibADCS",         .dim = "PerflibADCS",          .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibADCS},
+    {.name = "PerflibADCS", .dim = "PerflibADCS", .enabled = CONFIG_BOOLEAN_YES, .func = do_PerflibADCS},
 
     // the terminator of this array
-    {.name = NULL, .dim = NULL, .func = NULL}
-};
+    {.name = NULL, .dim = NULL, .func = NULL}};
 
 #if WORKER_UTILIZATION_MAX_JOB_TYPES < 36
 #error WORKER_UTILIZATION_MAX_JOB_TYPES has to be at least 36
 #endif
 
-static void windows_main_cleanup(void *pptr) {
+static void windows_main_cleanup(void *pptr)
+{
     struct netdata_static_thread *static_thread = CLEANUP_FUNCTION_GET_PTR(pptr);
-    if(!static_thread) return;
+    if (!static_thread)
+        return;
 
     static_thread->enabled = NETDATA_MAIN_THREAD_EXITING;
 
@@ -58,13 +68,15 @@ static void windows_main_cleanup(void *pptr) {
     worker_unregister();
 }
 
-static bool log_windows_module(BUFFER *wb, void *data) {
+static bool log_windows_module(BUFFER *wb, void *data)
+{
     struct proc_module *pm = data;
     buffer_sprintf(wb, PLUGIN_WINDOWS_NAME "[%s]", pm->name);
     return true;
 }
 
-void *win_plugin_main(void *ptr) {
+void *win_plugin_main(void *ptr)
+{
     worker_register("WIN");
 
     rrd_collector_started();
@@ -74,7 +86,7 @@ void *win_plugin_main(void *ptr) {
 
     // check the enabled status for each module
     int i;
-    for(i = 0; win_modules[i].name; i++) {
+    for (i = 0; win_modules[i].name; i++) {
         struct proc_module *pm = &win_modules[i];
 
         pm->enabled = config_get_boolean("plugin:windows", pm->name, pm->enabled);
@@ -94,21 +106,21 @@ void *win_plugin_main(void *ptr) {
     };
     ND_LOG_STACK_PUSH(lgs);
 
-    while(service_running(SERVICE_COLLECTORS)) {
+    while (service_running(SERVICE_COLLECTORS)) {
         worker_is_idle();
         usec_t hb_dt = heartbeat_next(&hb);
 
-        if(unlikely(!service_running(SERVICE_COLLECTORS)))
+        if (unlikely(!service_running(SERVICE_COLLECTORS)))
             break;
 
         PerflibNamesRegistryUpdate();
 
-        for(i = 0; win_modules[i].name; i++) {
-            if(unlikely(!service_running(SERVICE_COLLECTORS)))
+        for (i = 0; win_modules[i].name; i++) {
+            if (unlikely(!service_running(SERVICE_COLLECTORS)))
                 break;
 
             struct proc_module *pm = &win_modules[i];
-            if(unlikely(!pm->enabled))
+            if (unlikely(!pm->enabled))
                 continue;
 
             worker_is_busy(i);
