bbf7bdfba3f61a944791d240bf32333f23ec8861
thiagoftsm <thiagoftsm@gmail.com>
2025-02-04 20:38:06 +0000

    pipe_plugin_socket: Fix compilation

---
+++ src/collectors/ebpf.plugin/ebpf_socket_ipc.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef NETDATA_EBPF_SOCKET_IPC_H
+# define NETDATA_EBPF_SOCKET_IPC_H 1
+
+void *ebpf_socket_thread_ipc(void *ptr);
+
+#endif // NETDATA_EBPF_SOCKET_IPC_H_
