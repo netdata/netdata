df1b12dd2f6a3a91ea4af9f2c3ee75a7f23a8de8
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2025-02-09 22:25:43 +0200

    Close database if there is an error to prevent further operations on it during shutdown

--- src/database/sqlite/sqlite_metadata.c
+++ src/database/sqlite/sqlite_metadata.c
@@ -830,17 +830,22 @@ int sql_init_meta_database(db_check_action_type_t rebuild, int memory)
         target_version = perform_database_migration(db_meta, DB_METADATA_VERSION);
 
     if (configure_sqlite_database(db_meta, target_version, "meta_config"))
-        return 1;
+        goto close_database;
 
     if (init_database_batch(db_meta, &database_config[0], "meta_init"))
-        return 1;
+        goto close_database;
 
     if (init_database_batch(db_meta, &database_cleanup[0], "meta_cleanup"))
-        return 1;
+        goto close_database;
 
     netdata_log_info("SQLite database initialization completed");
 
     return 0;
+
+close_database:
+    sqlite3_close(db_meta);
+    db_meta = NULL;
+    return 1;
 }
 
 // Metadata functions
