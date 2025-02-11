9b018f297497a52674657ffff3d76d16fd7147da
Costa Tsaousis <costa@netdata.cloud>
2025-02-12 00:22:54 +0200

    detect the system ca bundle at runtime

--- src/daemon/environment.c
+++ src/daemon/environment.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "common.h"
+#include <curl/curl.h>
 
 static const char *verify_required_directory(const char *dir)
 {
@@ -31,6 +32,37 @@ static const char *verify_or_create_required_private_directory(const char *dir) 
         fatal("Cannot create required directory '%s'", dir);
 
     return verify_required_directory(dir);
+}
+
+const char *detect_libcurl_default_ca() {
+    curl_version_info_data *info = curl_version_info(CURLVERSION_NOW);
+    if (info) {
+
+        // Check built-in CA bundle
+        if (info->cainfo && access(info->cainfo, R_OK) == 0)
+            return info->cainfo;
+    }
+
+    return NULL;
+}
+
+static const char *detect_ca_path(void) {
+    static const char *paths[] = {
+        "/etc/ssl/certs/ca-certificates.crt",               // Debian, Ubuntu, Arch
+        "/etc/pki/tls/certs/ca-bundle.crt",                 // RHEL, CentOS, Fedora
+        "/etc/ssl/ca-bundle.pem",                           // OpenSUSE
+        "/etc/ssl/cert.pem",                                // Alpine
+        "/opt/netdata/etc/ssl/certs/ca-certificates.crt",   // Netdata static build
+        "/opt/netdata/share/ssl/certs/ca-certificates.crt", // Netdata static build - fallback
+        NULL
+    };
+
+    for (int i = 0; paths[i] != NULL; i++) {
+        if (access(paths[i], R_OK) == 0)
+            return paths[i];
+    }
+
+    return "";
 }
 
 void set_environment_for_plugins_and_scripts(void) {
@@ -81,7 +113,8 @@ void set_environment_for_plugins_and_scripts(void) {
         freez((char *)default_port);
 
     // set the path we need
-    char path[4096], *p = getenv("PATH");
+    char path[4096];
+    const char *p = getenv("PATH");
     if (!p) p = "/bin:/usr/bin";
     snprintfz(path, sizeof(path), "%s:%s", p, "/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin");
     setenv("PATH", inicfg_get(&netdata_config, CONFIG_SECTION_ENV_VARS, "PATH", path), 1);
@@ -93,6 +126,11 @@ void set_environment_for_plugins_and_scripts(void) {
 
     // disable buffering for python plugins
     setenv("PYTHONUNBUFFERED", "1", 1);
+
+    p = getenv("CURL_CA_BUNDLE");
+    if(!p || !*p) p = detect_libcurl_default_ca();
+    if(!p || !*p) p = detect_ca_path();
+    setenv("CURL_CA_BUNDLE", inicfg_get(&netdata_config, CONFIG_SECTION_ENV_VARS, "CURL_CA_BUNDLE", p), 1);
 
     // switch to standard locale for plugins
     setenv("LC_ALL", "C", 1);
