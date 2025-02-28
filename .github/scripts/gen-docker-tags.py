0b2a411ff9e9cc6b93c15bfbcc4a16eda0097d4a
Austin S. Hemmelgarn <austin@netdata.cloud>
2025-02-28 08:00:22 -0500

    Add Docker tags for the last few nightly builds.
    This adds new Docker tags for our nightly builds to track prior nightly
    builds relative to the current one.
    
    The new tags take the form `edge-x` where the `x` is a number that
    represents how many builds previous to the current build the tag tracks.
    For example, `edge-1` is the build just prior to the current nightly
    version, while `edge-3` is three builds prior to the current nightly
    build.
    
    These tags are rotated every time a nightly build is published. This
    approach was chosen to avoid the significant complexity that would be
    required to properly clean up old nightly build tags if we were tagging
    by actual nightly version instead of tagging relative to the current
    build.
    
    The current configuration will track the 4 most recent nightly builds.
    
    The special tag `edge-0` is a new alias for the current nightly build.
    This is only needed to allow the tag update logic to work correctly, as
    the workflow is intentionally configured to publish the new nightly
    images before rotating the tags for the old ones (so that if rotation
    fails it does not prevent us from publishing the current nightly).

--- .github/scripts/gen-docker-tags.py
+++ .github/scripts/gen-docker-tags.py
@@ -1,36 +1,67 @@
 #!/usr/bin/env python3
 
+from __future__ import annotations
+
+import os
 import sys
 
-github_event = sys.argv[1]
-version = sys.argv[2]
+from typing import Final
 
-REPO = 'netdata/netdata'
+github_event: Final = sys.argv[1]
+version: Final = sys.argv[2]
 
-REPOS = (
-    REPO,
-    f'quay.io/{REPO}',
-    f'ghcr.io/{REPO}',
-)
+REPO: Final = 'netdata/netdata'
+REPOS: Final = {
+    'docker': REPO,
+    'quay': f'quay.io/{REPO}',
+    'ghcr': f'ghcr.io/{REPO}',
+}
+
+REPO: Final = 'netdata/netdata'
+QUAY_REPO: Final = f'quay.io/{REPO}'
+GHCR_REPO: Final = f'ghcr.io/{REPO}'
+NIGHTLY_TAG: Final = 'edge'
+
+tags = {
+    k: [] for k in REPOS.keys()
+}
+
+nightly = False
 
 match version:
     case '':
-        tags = (f'{REPO}:test',)
+        for h in REPOS.keys():
+            tags[h] = [f'{REPOS[h]}:test']
     case 'nightly':
-        tags = tuple([
-            f'{r}:{t}' for r in REPOS for t in ('edge', 'latest')
-        ])
+        for h in REPOS.keys():
+            tags[h] = [f'{REPOS[h]}:{t}' for t in (NIGHTLY_TAG, 'latest')]
+
+        nightly = True
     case _:
         v = f'v{version}'.split('.')
 
-        tags = tuple([
-            f'{r}:{t}' for r in REPOS for t in (
-                v[0],
-                '.'.join(v[0:2]),
-                '.'.join(v[0:3]),
-            )
-        ])
+        versions: Final = (
+            v[0],
+            '.'.join(v[0:2]),
+            '.'.join(v[0:3]),
+            'stable',
+        )
 
-        tags = tags + tuple([f'{r}:stable' for r in REPOS])
+        for h in REPOS.keys():
+            tags[h] = [f'{REPOS[h]}:{t}' for t in versions]
 
-print(','.join(tags))
+all_tags = [x for y in tags.values() for x in y]
+
+
+def write_output(name: str, value: str) -> None:
+    with open(os.getenv('GITHUB_OUTPUT'), 'a') as f:
+        f.write(f'{name}={value}')
+
+
+write_output('tags', ','.join(all_tags))
+write_output('nightly', '1' if nightly else '0')
+write_output('nightly_tag', NIGHTLY_TAG)
+
+for h in REPOS.keys():
+    write_output(f'{h}_tags', ','.join(tags[h]))
+    write_output(f'{h}_repo', REPOS[h])
