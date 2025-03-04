bd1a049f2edb6f9c7c9013414933197efdacfc07
Costa Tsaousis <costa@netdata.cloud>
2025-03-04 12:21:15 +0200

    change the way the buffer is created to find the bad field

--- src/daemon/analytics.c
+++ src/daemon/analytics.c
@@ -873,64 +873,72 @@ bool analytics_check_enabled(void) {
     return netdata_anonymous_statistics_enabled;
 }
 
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+#define CMD_APPEND_QUOTED(cmd, str) do {    \
+        daemon_status_file_startup_step("startup(analytics-line-" STR(__LINE__) ")"); \
+        buffer_putc(cmd, ' ');              \
+        buffer_putc(cmd, '\'');             \
+        buffer_strcat(cmd, str ? str : ""); \
+        buffer_putc(cmd, '\'');             \
+} while(0)
+
 void analytics_statistic_send(const analytics_statistic_t *statistic) {
-    if (!statistic || !statistic->action || !*statistic->action|| !analytics_check_enabled() || !analytics_script_exists())
+    if (!statistic || !statistic->action || !*statistic->action || !analytics_check_enabled() || !analytics_script_exists())
         return;
 
     const char *action_result = statistic->result;
     const char *action_data = statistic->data;
 
     CLEAN_BUFFER *cmd = buffer_create(16384, NULL);
-    buffer_sprintf(
-        cmd,
-        "%s/anonymous-statistics.sh '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' ",
-        netdata_configured_primary_plugins_dir,
-        statistic->action,
-        action_result ? action_result : "",
-        action_data ? action_data : "",
-        analytics_data.netdata_config_stream_enabled ? analytics_data.netdata_config_stream_enabled : "",
-        analytics_data.netdata_config_memory_mode ? analytics_data.netdata_config_memory_mode : "",
-        analytics_data.netdata_config_exporting_enabled ? analytics_data.netdata_config_exporting_enabled : "",
-        analytics_data.netdata_exporting_connectors ? analytics_data.netdata_exporting_connectors : "",
-        analytics_data.netdata_allmetrics_prometheus_used ? analytics_data.netdata_allmetrics_prometheus_used : "",
-        analytics_data.netdata_allmetrics_shell_used ? analytics_data.netdata_allmetrics_shell_used : "",
-        analytics_data.netdata_allmetrics_json_used ? analytics_data.netdata_allmetrics_json_used : "",
-        analytics_data.netdata_dashboard_used ? analytics_data.netdata_dashboard_used : "",
-        analytics_data.netdata_collectors ? analytics_data.netdata_collectors : "",
-        analytics_data.netdata_collectors_count ? analytics_data.netdata_collectors_count : "",
-        analytics_data.netdata_buildinfo ? analytics_data.netdata_buildinfo : "",
-        analytics_data.netdata_config_page_cache_size ? analytics_data.netdata_config_page_cache_size : "",
-        analytics_data.netdata_config_multidb_disk_quota ? analytics_data.netdata_config_multidb_disk_quota : "",
-        analytics_data.netdata_config_https_enabled ? analytics_data.netdata_config_https_enabled : "",
-        analytics_data.netdata_config_web_enabled ? analytics_data.netdata_config_web_enabled : "",
-        analytics_data.netdata_config_release_channel ? analytics_data.netdata_config_release_channel : "",
-        analytics_data.netdata_mirrored_host_count ? analytics_data.netdata_mirrored_host_count : "",
-        analytics_data.netdata_mirrored_hosts_reachable ? analytics_data.netdata_mirrored_hosts_reachable : "",
-        analytics_data.netdata_mirrored_hosts_unreachable ? analytics_data.netdata_mirrored_hosts_unreachable : "",
-        analytics_data.netdata_notification_methods ? analytics_data.netdata_notification_methods : "",
-        analytics_data.netdata_alarms_normal ? analytics_data.netdata_alarms_normal : "",
-        analytics_data.netdata_alarms_warning ? analytics_data.netdata_alarms_warning : "",
-        analytics_data.netdata_alarms_critical ? analytics_data.netdata_alarms_critical : "",
-        analytics_data.netdata_charts_count ? analytics_data.netdata_charts_count : "",
-        analytics_data.netdata_metrics_count ? analytics_data.netdata_metrics_count : "",
-        analytics_data.netdata_config_is_parent ? analytics_data.netdata_config_is_parent : "",
-        analytics_data.netdata_config_hosts_available ? analytics_data.netdata_config_hosts_available : "",
-        analytics_data.netdata_host_cloud_available ? analytics_data.netdata_host_cloud_available : "",
-        analytics_data.netdata_host_aclk_available ? analytics_data.netdata_host_aclk_available : "",
-        analytics_data.netdata_host_aclk_protocol ? analytics_data.netdata_host_aclk_protocol : "",
-        analytics_data.netdata_host_aclk_implementation ? analytics_data.netdata_host_aclk_implementation : "",
-        analytics_data.netdata_host_agent_claimed ? analytics_data.netdata_host_agent_claimed : "",
-        analytics_data.netdata_host_cloud_enabled ? analytics_data.netdata_host_cloud_enabled : "",
-        analytics_data.netdata_config_https_available ? analytics_data.netdata_config_https_available : "",
-        analytics_data.netdata_install_type ? analytics_data.netdata_install_type : "",
-        analytics_data.netdata_config_is_private_registry ? analytics_data.netdata_config_is_private_registry : "",
-        analytics_data.netdata_config_use_private_registry ? analytics_data.netdata_config_use_private_registry : "",
-        analytics_data.netdata_config_oom_score ? analytics_data.netdata_config_oom_score : "",
-        analytics_data.netdata_prebuilt_distro ? analytics_data.netdata_prebuilt_distro : "",
-        analytics_data.netdata_fail_reason ? analytics_data.netdata_fail_reason : ""
-        );
 
-    return;
+    buffer_strcat(cmd, netdata_configured_primary_plugins_dir);
+    buffer_putc(cmd, '/');
+    buffer_strcat(cmd, "anonymous-statistics.sh");
+
+    CMD_APPEND_QUOTED(cmd, statistic->action);
+    CMD_APPEND_QUOTED(cmd, action_result);
+    CMD_APPEND_QUOTED(cmd, action_data);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_config_stream_enabled);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_config_memory_mode);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_config_exporting_enabled);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_exporting_connectors);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_allmetrics_prometheus_used);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_allmetrics_shell_used);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_allmetrics_json_used);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_dashboard_used);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_collectors);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_collectors_count);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_buildinfo);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_config_page_cache_size);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_config_multidb_disk_quota);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_config_https_enabled);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_config_web_enabled);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_config_release_channel);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_mirrored_host_count);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_mirrored_hosts_reachable);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_mirrored_hosts_unreachable);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_notification_methods);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_alarms_normal);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_alarms_warning);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_alarms_critical);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_charts_count);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_metrics_count);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_config_is_parent);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_config_hosts_available);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_host_cloud_available);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_host_aclk_available);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_host_aclk_protocol);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_host_aclk_implementation);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_host_agent_claimed);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_host_cloud_enabled);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_config_https_available);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_install_type);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_config_is_private_registry);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_config_use_private_registry);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_config_oom_score);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_prebuilt_distro);
+    CMD_APPEND_QUOTED(cmd, analytics_data.netdata_fail_reason);
 
     nd_log(NDLS_DAEMON, NDLP_DEBUG,
            "%s/anonymous-statistics.sh '%s' '%s' '%s'",
