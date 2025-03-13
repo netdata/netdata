5db5d2759fd2aa6c7fdb261d62663ced7f112548
thiagoftsm <thiagoftsm@gmail.com>
2025-03-13 05:48:25 +0000

    ebpf_rework_mem: Add Judy Array to shared memory

--- src/collectors/collectors-ipc/ebpf-ipc.c
+++ src/collectors/collectors-ipc/ebpf-ipc.c
@@ -6,20 +6,72 @@ netdata_ebpf_pid_stats_t *integration_shm = NULL;
 int shm_fd_ebpf_integration = -1;
 sem_t *shm_mutex_ebpf_integration = SEM_FAILED;
 
+static Pvoid_t ebpf_ipc_JudyL = NULL;
+static uint32_t last_idx = 0;
+static uint32_t max_idx = 0;
+
 bool using_vector = false;
+
+static uint32_t *ebpf_shm_find_index_unsafe(uint32_t pid)
+{
+    Pvoid_t *Pvalue = JudyLGet(ebpf_ipc_JudyL, (Word_t)pid, PJE0);
+    return (uint32_t *)*Pvalue;
+}
+
+static bool ebpf_find_pid_shm_del_unsafe(uint32_t pid, enum ebpf_pids_index idx)
+{
+    uint32_t *lpid = ebpf_shm_find_index_unsafe(pid);
+    if (!lpid)
+        return false;
+
+    netdata_ebpf_pid_stats_t *ptr = &integration_shm[*lpid];
+    ptr->threads &= ~(idx << 1);
+    if (ptr->threads) {
+        return true;
+    }
+
+    (void)JudyLDel(&ebpf_ipc_JudyL, (Word_t)pid, PJE0);
+
+    last_idx--;
+    if (!last_idx)
+        return false;
+
+    netdata_ebpf_pid_stats_t *newValue = &integration_shm[last_idx];
+    uint32_t *move = ebpf_shm_find_index_unsafe(newValue->pid);
+    *move = *lpid;
+
+    memcpy(ptr, newValue, sizeof(*ptr));
+    return false;
+}
+
+static uint32_t *ebpf_find_or_create_index_pid(uint32_t pid)
+{
+    uint32_t *idx = ebpf_shm_find_index_unsafe(pid);
+    if (!idx) {
+        Pvoid_t *Pvalue = JudyLIns(&ebpf_ipc_JudyL, (Word_t)pid, PJE0);
+        internal_fatal(!Pvalue || Pvalue == PJERR, "EBPF: pid judy index");
+        if (likely(!*Pvalue))
+            *Pvalue = idx = callocz(1, sizeof(*idx));
+        else
+            idx = *Pvalue;
+    }
+    return idx;
+}
 
 bool netdata_ebpf_reset_shm_pointer_unsafe(int fd, uint32_t pid, enum ebpf_pids_index idx)
 {
-    if (using_vector && integration_shm) {
-        if (idx != NETDATA_EBPF_PIDS_SOCKET_IDX)
-            bpf_map_delete_elem(fd, &pid);
+    if (idx != NETDATA_EBPF_PIDS_SOCKET_IDX)
+        bpf_map_delete_elem(fd, &pid);
 
+    if (using_vector && integration_shm) {
         netdata_ebpf_pid_stats_t *ptr = &integration_shm[pid];
         ptr->threads &= ~(idx << 1);
         if (!ptr->threads) {
             memset(ptr, 0, sizeof(*ptr));
             return false;
         }
+    } else {
+        return ebpf_find_pid_shm_del_unsafe(pid, idx);
     }
 
     return true;
@@ -27,16 +79,21 @@ bool netdata_ebpf_reset_shm_pointer_unsafe(int fd, uint32_t pid, enum ebpf_pids_
 
 netdata_ebpf_pid_stats_t *netdata_ebpf_get_shm_pointer_unsafe(uint32_t pid, enum ebpf_pids_index idx)
 {
-    if (!integration_shm)
+    if (!integration_shm || (last_idx + 1) == max_idx)
         return NULL;
 
-    if (using_vector) {
-        netdata_ebpf_pid_stats_t *ptr = &integration_shm[pid];
-        ptr->threads |= idx << 1;
-        return ptr;
+    if (!using_vector) {
+        uint32_t *pidx = ebpf_find_or_create_index_pid(pid);
+        if (*pidx == 0) {
+            *pidx = last_idx++;
+        }
+        pid = *pidx;
     }
+    netdata_ebpf_pid_stats_t *ptr = &integration_shm[pid];
+    ptr->pid = pid;
+    ptr->threads |= idx << 1;
 
-    return NULL;
+    return ptr;
 }
 
 void netdata_integration_cleanup_shm()
