f7abdb40a69fb94ad63528369ff59625be6bd963
Costa Tsaousis <costa@netdata.cloud>
2025-03-21 16:01:10 +0200

    polishing and cleanup

--- src/daemon/machine-guid.c
+++ src/daemon/machine-guid.c
@@ -3,19 +3,19 @@
 #include "common.h"
 #include "machine-guid.h"
 
-static char *blacklisted[] = {
-    "8a795b0c-2311-11e6-8563-000c295076a6",
-    "4aed1458-1c3e-11e6-a53f-000c290fc8f5",
-};
-
 static bool machine_guid_check_blacklisted(const char *guid) {
     // these are machine GUIDs that have been included in distribution packages.
     // we blacklist them here, so that the next version of netdata will generate
     // new ones.
 
+    static char *blacklisted[] = {
+        "8a795b0c-2311-11e6-8563-000c295076a6",
+        "4aed1458-1c3e-11e6-a53f-000c290fc8f5",
+    };
+
     for(size_t i = 0; i < _countof(blacklisted); i++) {
         if (!strcmp(guid, blacklisted[i])) {
-            netdata_log_error("Blacklisted machine GUID '%s' found.", guid);
+            nd_log(NDLS_DAEMON, NDLP_INFO, "MACHINE_GUID: blacklisted machine GUID '%s' found, generating new one.", guid);
             return true;
         }
     }
@@ -24,55 +24,56 @@ static bool machine_guid_check_blacklisted(const char *guid) {
 }
 
 static bool machine_guid_read_from_file(const char *filename, ND_MACHINE_GUID *host_id) {
-    if(!filename || !*filename)
+    if (!filename || !*filename)
         return false;
 
     ND_MACHINE_GUID h;
     memset(&h, 0, sizeof(h));
 
     int fd = open(filename, O_RDONLY | O_CLOEXEC);
-    if(fd == -1) {
-        nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot open file '%s' for reading", filename);
+    if (fd == -1) {
+        nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot open GUID file '%s' for reading", filename);
         return false;
     }
 
-    if(read(fd, h.txt, sizeof(h.txt) - 1) != sizeof(h.txt) - 1) {
-        nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot read file '%s'", filename);
+    if (read(fd, h.txt, sizeof(h.txt) - 1) != sizeof(h.txt) - 1) {
+        nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot read GUID file '%s'", filename);
         close(fd);
         return false;
     }
+    h.txt[sizeof(h.txt) - 1] = '\0';
 
-    if(uuid_parse(h.txt, h.uuid.uuid) != 0) {
+    if (uuid_parse(h.txt, h.uuid.uuid) != 0) {
         nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot parse GUID from file '%s'", filename);
         close(fd);
         return false;
     }
 
-    if(UUIDiszero(h.uuid)) {
+    if (UUIDiszero(h.uuid)) {
         nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: GUID read from file '%s' is zero", filename);
         close(fd);
         return false;
     }
 
     struct stat st;
-    if(fstat(fd, &st) != 0) {
-        nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot stat file '%s'", filename);
+    if (fstat(fd, &st) != 0) {
+        nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot stat the GUID file '%s'", filename);
         close(fd);
         return false;
     }
     close(fd);
 
-    // recreate the text version of it, to always be lowercase
+    // Recreate the text version of it, ensuring lowercase format.
     uuid_unparse_lower(h.uuid.uuid, h.txt);
 
-    if(machine_guid_check_blacklisted(h.txt))
+    if (machine_guid_check_blacklisted(h.txt))
         return false;
 
-    // update its last modified timestamp
+    // Update last modified timestamp.
     h.last_modified_ut = st.st_mtim.tv_sec * USEC_PER_SEC + st.st_mtim.tv_nsec / 1000;
     *host_id = h;
 
-    nd_log(NDLS_DAEMON, NDLP_INFO, "MACHINE_GUID: read from file '%s'", filename);
+    nd_log(NDLS_DAEMON, NDLP_INFO, "MACHINE_GUID: GUID read from file '%s'", filename);
 
     return true;
 }
@@ -85,25 +86,34 @@ static struct timespec usec_to_timespec(usec_t usec) {
 }
 
 static bool machine_guid_write_to_file(const char *filename, ND_MACHINE_GUID *host_id) {
-    if(!filename || !*filename)
+    static size_t save_id = 0;
+
+    if (!filename || !*filename)
         return false;
 
     ND_MACHINE_GUID h;
     memset(&h, 0, sizeof(h));
 
-    if(host_id) {
+    if (host_id) {
         h.uuid = host_id->uuid;
         h.last_modified_ut = host_id->last_modified_ut;
     }
 
-    int fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 444);
-    if(fd == -1) {
-        nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot open file '%s' for writing", filename);
+    // Create the text representation before writing.
+    uuid_unparse_lower(h.uuid.uuid, h.txt);
+
+    // Use a temporary filename for atomic writes.
+    char tmp_filename[FILENAME_MAX];
+    snprintf(tmp_filename, sizeof(tmp_filename), "%s.%zu", filename, __atomic_add_fetch(&save_id, 1, __ATOMIC_RELAXED));
+
+    int fd = open(tmp_filename, O_WRONLY | O_CREAT | O_TRUNC, 0444);
+    if (fd == -1) {
+        nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot create the temporary GUID file '%s'", tmp_filename);
         return false;
     }
 
-    if(write(fd, h.txt, sizeof(h.txt) - 1) != sizeof(h.txt) - 1) {
-        nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot save file '%s'", filename);
+    if (write(fd, h.txt, sizeof(h.txt) - 1) != sizeof(h.txt) - 1) {
+        nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot write GUID to the temporary GUID file '%s'", tmp_filename);
         close(fd);
         return false;
     }
@@ -115,11 +125,18 @@ static bool machine_guid_write_to_file(const char *filename, ND_MACHINE_GUID *ho
         usec_to_timespec(h.last_modified_ut), // modification time
     };
 
-    // update its timestamps to reflect the last modified time
-    if (utimensat(AT_FDCWD, filename, times, 0) < 0)
-        nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot update the timestamps of '%s'", filename);
+    // Update file timestamps.
+    if (utimensat(AT_FDCWD, tmp_filename, times, 0) < 0)
+        nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot update the timestamps of the temporary GUID file '%s'", tmp_filename);
 
-    nd_log(NDLS_DAEMON, NDLP_INFO, "MACHINE_GUID: saved to file '%s'", filename);
+    // Rename the temporary file to the target filename atomically.
+    if (rename(tmp_filename, filename) != 0) {
+        nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot rename temporary GUID file '%s' to '%s'", tmp_filename, filename);
+        unlink(tmp_filename);
+        return false;
+    }
+
+    nd_log(NDLS_DAEMON, NDLP_INFO, "MACHINE_GUID: GUID saved to file '%s'", filename);
 
     return true;
 }
@@ -135,41 +152,47 @@ static ND_MACHINE_GUID machine_guid_get_or_create(void) {
     ND_MACHINE_GUID h;
     memset(&h, 0, sizeof(h));
 
+    // Build the file path.
     snprintfz(pathname, sizeof(pathname), "%s/registry", netdata_configured_varlib_dir);
     snprintfz(filename, sizeof(filename), "%s/%s", pathname, "netdata.public.unique.id");
-    if(machine_guid_read_from_file(filename, &h))
+
+    // Attempt to read the GUID from the file.
+    if (machine_guid_read_from_file(filename, &h))
         return h;
 
-    // could not read the file
-    nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: failed to read file '%s'", filename);
+    // Log failure to read the file.
+    nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: failed to read GUID from file '%s'", filename);
 
-    // get the last one saved in the daemon status file
+    // Attempt to retrieve GUID from daemon status file.
     h.uuid = daemon_status_file_get_host_id();
-    if(UUIDiszero(h.uuid)) {
-        // daemon status file does not have a machine guid
-        // generate a new one
-        nd_log(NDLS_DAEMON, NDLP_INFO, "MACHINE_GUID: generating a new one");
+    if (UUIDiszero(h.uuid)) {
+        // If the status file does not contain a GUID, generate a new one.
+        nd_log(NDLS_DAEMON, NDLP_INFO, "MACHINE_GUID: generating a new GUID");
         uuid_generate_time(h.uuid.uuid);
     }
     else
-        nd_log(NDLS_DAEMON, NDLP_INFO, "MACHINE_GUID: got previous one from daemon status file");
+        nd_log(NDLS_DAEMON, NDLP_INFO, "MACHINE_GUID: got previous GUID from daemon status file");
 
+    // Ensure the text representation is updated.
     uuid_unparse_lower(h.uuid.uuid, h.txt);
     h.last_modified_ut = now_realtime_usec();
     nd_machine_guid = h;
 
-    if(access(pathname, W_OK) != 0) {
-        nd_log(NDLS_DAEMON, NDLP_DEBUG, "MACHINE_GUID: cannot access directory '%s'. Will try to create it.", pathname);
+    // Ensure the registry directory exists.
+    if (access(pathname, W_OK) != 0) {
+        nd_log(NDLS_DAEMON, NDLP_DEBUG, "MACHINE_GUID: cannot access directory '%s'. Attempting to create it.", pathname);
 
         errno_clear();
-        if(mkdir(pathname, 0775) != 0 &&  errno != EEXIST) {
+        if (mkdir(pathname, 0775) != 0 && errno != EEXIST) {
             nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot create directory '%s'", pathname);
+            // Even if directory creation fails, continue with in-memory GUID.
             return h;
         }
     }
 
+    errno_clear();
     if (!machine_guid_write_to_file(filename, &h))
-        nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot save to file '%s'", filename);
+        nd_log(NDLS_DAEMON, NDLP_ERR, "MACHINE_GUID: cannot save GUID to file '%s'", filename);
 
     return h;
 }
