07a0620f0654c43f8b21c04970ffd00b1a7d7d2a
thiagoftsm <thiagoftsm@gmail.com>
2025-03-26 03:16:13 +0000

    fix-msi: Add option to do download and move PS file

--- packaging/windows/copy_files.ps1
+++ packaging/windows/copy_files.ps1
@@ -6,16 +6,32 @@ Function NetdataCopyConfig {
     if (-Not (Test-Path $testDST)) {
         if (Test-Path $testSRC) {
             robocopy /xc /xn /xo $src $dst $file
-            exit 0;
-        } else {
-            exit 1;
         }
     }
 }
 
-$streamPath = "C:\Program Files\Netdata\etc\netdata";
+Function NetdataDownloadNetdataConfig {
+    param ($path)
+
+    $netdataConfPATH = "$path\netdata.conf"
+    $netdataConfURL = "http://localhost:19999/netdata.conf"
+    if (Test-Path $netdataConfPATH) {
+        exit 0
+    }
+
+    Invoke-WebRequest $netdataConfURL -OutFile $netdataConfPATH
+    if (Test-Path $netdataConfPATH) {
+        exit 0
+    }
+
+    New-Item -Path "$netdataConfPATH" -ItemType File
+}
+
+$confPath = "C:\Program Files\Netdata\etc\netdata";
 $stockStreamPath = "C:\Program Files\Netdata\usr\lib\netdata\conf.d";
 
-NetdataCopyConfig $streamPath $stockStreamPath "stream.conf"
+NetdataCopyConfig $confPath $stockStreamPath "stream.conf"
+
+NetdataDownloadNetdataConfig $confPath
 
 exit 0;
