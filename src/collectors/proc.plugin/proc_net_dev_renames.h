ffc66c7a9e10a213da7e1fb0670cbb9f38f74c11
Costa Tsaousis <costa@netdata.cloud>
2025-04-03 18:56:14 +0300

    fix cgroup netdev renames when multiple renames exist for the same device

--- src/collectors/proc.plugin/proc_net_dev_renames.h
+++ src/collectors/proc.plugin/proc_net_dev_renames.h
@@ -13,6 +13,7 @@
 extern DICTIONARY *netdev_renames;
 
 struct rename_task {
+    SPINLOCK spinlock;
     const char *container_device;
     const char *container_name;
     const char *ctx_prefix;
@@ -20,6 +21,11 @@ struct rename_task {
     const DICTIONARY_ITEM *cgroup_netdev_link;
     XXH64_hash_t checksum;
 };
+
+#define freez_and_set_to_null(p) do { \
+    freez((void *)p); \
+    p = NULL; \
+} while(0)
 
 void netdev_renames_init(void);
 
