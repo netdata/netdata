3c1279fd0311567c0f51297e47d2dea34dc5e466
Costa Tsaousis <costa@netdata.cloud>
2025-04-03 12:04:28 +0300

    trace crashes No 4 (#20043)
    * detect issue at uv_worker job 13
    
    * assertion at journalfile_migrate_to_v2_callback()
    
    (cherry picked from commit 934c4ccbcdd84479708222700455901eb728a6c6)

--- src/database/engine/journalfile.c
+++ src/database/engine/journalfile.c
@@ -1351,6 +1351,7 @@ bool journalfile_migrate_to_v2_callback(Word_t section, unsigned datafile_fileno
         return false;
     }
 
+    fatal_assert(extent_offset <= total_file_size);
     memset(data_start, 0, extent_offset);
 
     // Write header
@@ -1402,11 +1403,14 @@ bool journalfile_migrate_to_v2_callback(Word_t section, unsigned datafile_fileno
     while ((PValue = JudyLFirstThenNext(JudyL_metrics, &Index, &first_then_next))) {
         metric_info = *PValue;
 
+        fatal_assert(metric_info != NULL);
         fatal_assert(count < number_of_metrics);
         uuid_list[count++].metric_info = metric_info;
         min_time_s = MIN(min_time_s, metric_info->first_time_s);
         max_time_s = MAX(max_time_s, metric_info->last_time_s);
     }
+
+    fatal_assert(count == number_of_metrics);
 
     // Check if not properly set in the loop above to prevent overflow
     if (min_time_s == LONG_MAX)
