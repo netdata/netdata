e7528f83e240417e66060ae585b5c3b0ac4df3bb
thiagoftsm <thiagoftsm@gmail.com>
2025-04-04 02:28:24 +0000

    win_services: Add collection data

--- src/collectors/windows.plugin/perflib-services.c
+++ src/collectors/windows.plugin/perflib-services.c
@@ -5,6 +5,9 @@
 
 // Service data
 struct win_service {
+    char *service_name;
+    char *display_name;
+
     RRDSET *st_service_state;
     RRDDIM *rd_service_state_running;
     RRDDIM *rd_service_state_stopped;
@@ -35,12 +38,15 @@ struct win_service {
 
 static DICTIONARY *win_services = NULL;
 
-void dict_win_service_insert_cb(
-    const DICTIONARY_ITEM *item __maybe_unused,
-    void *value __maybe_unused,
-    void *data __maybe_unused)
+void dict_win_service_insert_cb(const DICTIONARY_ITEM *item, void *value, void *data __maybe_unused)
 {
-    ;
+    struct win_service *ptr = value;
+    const char *name = dictionary_acquired_item_name((DICTIONARY_ITEM *)item);
+    LPENUM_SERVICE_STATUS_PROCESS service = data;
+
+    ptr->service_name = strdupz(name);
+    netdata_fix_chart_name(ptr->service_name);
+    ptr->display_name = strdupz(service->lpDisplayName);
 }
 
 static void initialize(void)
@@ -51,6 +57,60 @@ static void initialize(void)
     dictionary_register_insert_callback(win_services, dict_win_service_insert_cb, NULL);
 }
 
+static BOOL fill_dictionary_with_content()
+{
+    static PVOID buffer = NULL;
+    static ULONG initial_length = 0x8000;
+
+    LPENUM_SERVICE_STATUS_PROCESS service, services;
+    DWORD bytes_needed = 0, total_services = 0;
+    SC_HANDLE ndSCMH = OpenSCManager(NULL, NULL, SC_MANAGER_ENUMERATE_SERVICE | SC_MANAGER_CONNECT);
+    if (!ndSCMH) {
+        return -1;
+    }
+
+    // Query to obtain overall information
+    BOOL ret = EnumServicesStatusEx(
+        ndSCMH,
+        SC_ENUM_PROCESS_INFO,
+        SERVICE_WIN32,
+        SERVICE_STATE_ALL,
+        NULL,
+        0,
+        (LPDWORD)&bytes_needed,
+        (LPDWORD)&total_services,
+        NULL,
+        NULL);
+
+    if (GetLastError() == ERROR_MORE_DATA) {
+        if (!buffer)
+            buffer = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, bytes_needed);
+        else
+            buffer = HeapReAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, buffer, bytes_needed);
+    }
+
+    if (!buffer) {
+        ret = FALSE;
+        goto endServiceCollection;
+    }
+
+    services = (LPENUM_SERVICE_STATUS_PROCESS)buffer;
+
+    for (ULONG i = 0; i < total_services; i++) {
+        service = &services[i];
+        struct win_service *p = dictionary_set(win_services, service->lpServiceName, service, sizeof(*p));
+        if (!p)
+            continue;
+
+        p->ServiceState.current.Data = service->ServiceStatusProcess.dwCurrentState;
+    }
+
+endServiceCollection:
+
+    CloseServiceHandle(ndSCMH);
+    return ret;
+}
+
 int do_PerflibServices(int update_every, usec_t dt __maybe_unused)
 {
     static bool initialized = false;
@@ -59,6 +119,9 @@ int do_PerflibServices(int update_every, usec_t dt __maybe_unused)
         initialize();
         initialized = true;
     }
+
+    if (!fill_dictionary_with_content())
+        return 0;
 
     return 0;
 }
