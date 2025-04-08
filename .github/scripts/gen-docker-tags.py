7f4754b9b21c97d4f222d137c9d06ede096f7dcb
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2025-04-08 09:49:10 -0400

    Revert "Add Docker tags for the last few nightly builds. (#19734)"
    This reverts commit 4627b4f163f676fff7221c939212907d61412271.

--- .github/scripts/gen-docker-tags.py
+++ .github/scripts/gen-docker-tags.py
@@ -1,67 +1,36 @@
 #!/usr/bin/env python3
 
-from __future__ import annotations
-
-import os
 import sys
 
-from typing import Final
+github_event = sys.argv[1]
+version = sys.argv[2]
 
-github_event: Final = sys.argv[1]
-version: Final = sys.argv[2]
+REPO = 'netdata/netdata'
 
-REPO: Final = 'netdata/netdata'
-REPOS: Final = {
-    'docker': REPO,
-    'quay': f'quay.io/{REPO}',
-    'ghcr': f'ghcr.io/{REPO}',
-}
-
-REPO: Final = 'netdata/netdata'
-QUAY_REPO: Final = f'quay.io/{REPO}'
-GHCR_REPO: Final = f'ghcr.io/{REPO}'
-NIGHTLY_TAG: Final = 'edge'
-
-tags = {
-    k: [] for k in REPOS.keys()
-}
-
-nightly = False
+REPOS = (
+    REPO,
+    f'quay.io/{REPO}',
+    f'ghcr.io/{REPO}',
+)
 
 match version:
     case '':
-        for h in REPOS.keys():
-            tags[h] = [f'{REPOS[h]}:test']
+        tags = (f'{REPO}:test',)
     case 'nightly':
-        for h in REPOS.keys():
-            tags[h] = [f'{REPOS[h]}:{t}' for t in (NIGHTLY_TAG, 'latest')]
-
-        nightly = True
+        tags = tuple([
+            f'{r}:{t}' for r in REPOS for t in ('edge', 'latest')
+        ])
     case _:
         v = f'v{version}'.split('.')
 
-        versions: Final = (
-            v[0],
-            '.'.join(v[0:2]),
-            '.'.join(v[0:3]),
-            'stable',
-        )
+        tags = tuple([
+            f'{r}:{t}' for r in REPOS for t in (
+                v[0],
+                '.'.join(v[0:2]),
+                '.'.join(v[0:3]),
+            )
+        ])
 
-        for h in REPOS.keys():
-            tags[h] = [f'{REPOS[h]}:{t}' for t in versions]
+        tags = tags + tuple([f'{r}:stable' for r in REPOS])
 
-all_tags = [x for y in tags.values() for x in y]
-
-
-def write_output(name: str, value: str) -> None:
-    with open(os.getenv('GITHUB_OUTPUT'), 'a') as f:
-        f.write(f'{name}={value}')
-
-
-write_output('tags', ','.join(all_tags))
-write_output('nightly', '1' if nightly else '0')
-write_output('nightly_tag', NIGHTLY_TAG)
-
-for h in REPOS.keys():
-    write_output(f'{h}_tags', ','.join(tags[h]))
-    write_output(f'{h}_repo', REPOS[h])
+print(','.join(tags))
