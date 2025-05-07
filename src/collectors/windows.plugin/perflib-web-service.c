f2b38dcf631ec798383decc10793aabd7665b0c4
thiagoftsm <thiagoftsm@gmail.com>
2025-05-07 00:07:51 +0000

    iis_w3svc: Add File Flushed Total chart

--- src/collectors/windows.plugin/perflib-web-service.c
+++ src/collectors/windows.plugin/perflib-web-service.c
@@ -109,6 +109,9 @@ struct ws3svc_w3wp_data {
     RRDSET *st_wescv_w3wp_file_cache_total;
     RRDDIM *rd_wescv_w3wp_file_cache_total;
 
+    RRDSET *st_wescv_w3wp_file_flushed_total;
+    RRDDIM *rd_wescv_w3wp_file_flushed_total;
+
     COUNTER_DATA WESCVW3WPActiveThreads;
 
     COUNTER_DATA WESCVW3WPRequestTotal;
@@ -1565,6 +1568,38 @@ static inline void w3svc_w3wp_files_flushed_total(
     PERF_INSTANCE_DEFINITION *pi,
     int update_every)
 {
+    if (perflibGetInstanceCounter(pDataBlock, pObjectType, pi, &p->WESCVW3WPFilesFlushedTotal)) {
+        if (!p->st_wescv_w3wp_file_flushed_total) {
+            char id[RRD_ID_LENGTH_MAX + 1];
+            snprintfz(id, RRD_ID_LENGTH_MAX, "w3scv_w3wp_%s_file_flushed_total", windows_shared_buffer);
+            netdata_fix_chart_name(id);
+            p->st_wescv_w3wp_file_flushed_total = rrdset_create_localhost(
+                "iis",
+                id,
+                NULL,
+                "w3scv w3wp",
+                "iis.w3scv_w3wp_file_flushed_total",
+                "Total number of file handles that have been removed from the cache.",
+                "files/s",
+                PLUGIN_WINDOWS_NAME,
+                "PerflibWebService",
+                PRIO_W3SVC_W3WP_FILE_FLUSHED_TOTAL,
+                update_every,
+                RRDSET_TYPE_LINE);
+
+            p->rd_wescv_w3wp_file_flushed_total =
+                rrddim_add(p->st_wescv_w3wp_file_flushed_total, "files", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
+            rrdlabels_add(
+                p->st_wescv_w3wp_file_flushed_total->rrdlabels, "app", windows_shared_buffer, RRDLABEL_SRC_AUTO);
+        }
+
+        rrddim_set_by_pointer(
+            p->st_wescv_w3wp_file_flushed_total,
+            p->rd_wescv_w3wp_file_flushed_total,
+            (collected_number)p->WESCVW3WPFilesFlushedTotal.current.Data);
+
+        rrdset_done(p->st_wescv_w3wp_file_flushed_total);
+    }
 }
 
 static inline void w3svc_w3wp_uri_cached_flushed(
