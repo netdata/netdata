d1ec73238f1d7b52d5592abdb1e573c18329914c
Costa Tsaousis <costa@netdata.cloud>
2025-05-31 16:15:07 +0300

    updated documentation for apps.plugin; matches are now case insensitive; windows processes names are now part of the matching

--- src/collectors/apps.plugin/apps_pid_match.c
+++ src/collectors/apps.plugin/apps_pid_match.c
@@ -4,46 +4,90 @@
 
 bool pid_match_check(struct pid_stat *p, APPS_MATCH *match) {
     if(!match->starts_with && !match->ends_with) {
+        // Exact match
         if(match->pattern) {
             if(simple_pattern_matches_string(match->pattern, p->comm))
                 return true;
+#if (PROCESSES_HAVE_COMM_AND_NAME == 1)
+            if(p->name && simple_pattern_matches_string(match->pattern, p->name))
+                return true;
+#endif
         }
         else {
-            if(match->compare == p->comm || match->compare == p->comm_orig)
+            if(string_equals_string_nocase(match->compare, p->comm) || 
+               string_equals_string_nocase(match->compare, p->comm_orig))
                 return true;
+#if (PROCESSES_HAVE_COMM_AND_NAME == 1)
+            if(p->name && string_equals_string_nocase(match->compare, p->name))
+                return true;
+#endif
         }
     }
     else if(match->starts_with && !match->ends_with) {
+        // Prefix match
         if(match->pattern) {
             if(simple_pattern_matches_string(match->pattern, p->comm))
                 return true;
+#if (PROCESSES_HAVE_COMM_AND_NAME == 1)
+            if(p->name && simple_pattern_matches_string(match->pattern, p->name))
+                return true;
+#endif
         }
         else {
-            if(string_starts_with_string(p->comm, match->compare) ||
-               (p->comm != p->comm_orig && string_starts_with_string(p->comm, match->compare)))
+            if(string_starts_with_string_nocase(p->comm, match->compare) ||
+               (p->comm != p->comm_orig && string_starts_with_string_nocase(p->comm_orig, match->compare)))
                 return true;
+#if (PROCESSES_HAVE_COMM_AND_NAME == 1)
+            if(p->name && string_starts_with_string_nocase(p->name, match->compare))
+                return true;
+#endif
         }
     }
     else if(!match->starts_with && match->ends_with) {
+        // Suffix match
         if(match->pattern) {
             if(simple_pattern_matches_string(match->pattern, p->comm))
                 return true;
+#if (PROCESSES_HAVE_COMM_AND_NAME == 1)
+            if(p->name && simple_pattern_matches_string(match->pattern, p->name))
+                return true;
+#endif
         }
         else {
-            if(string_ends_with_string(p->comm, match->compare) ||
-               (p->comm != p->comm_orig && string_ends_with_string(p->comm, match->compare)))
+            if(string_ends_with_string_nocase(p->comm, match->compare) ||
+               (p->comm != p->comm_orig && string_ends_with_string_nocase(p->comm_orig, match->compare)))
                 return true;
+#if (PROCESSES_HAVE_COMM_AND_NAME == 1)
+            if(p->name && string_ends_with_string_nocase(p->name, match->compare))
+                return true;
+#endif
         }
     }
-    else if(match->starts_with && match->ends_with && p->cmdline) {
-        if(match->pattern) {
-            if(simple_pattern_matches_string(match->pattern, p->cmdline))
-                return true;
+    else if(match->starts_with && match->ends_with) {
+        // Substring match - search in cmdline and on Windows also in name
+        if(p->cmdline) {
+            if(match->pattern) {
+                if(simple_pattern_matches_string(match->pattern, p->cmdline))
+                    return true;
+            }
+            else {
+                if(strcasestr(string2str(p->cmdline), string2str(match->compare)))
+                    return true;
+            }
         }
-        else {
-            if(strstr(string2str(p->cmdline), string2str(match->compare)))
-                return true;
+#if (PROCESSES_HAVE_COMM_AND_NAME == 1)
+        // On Windows, also search in the name field for substring patterns
+        if(p->name) {
+            if(match->pattern) {
+                if(simple_pattern_matches_string(match->pattern, p->name))
+                    return true;
+            }
+            else {
+                if(strcasestr(string2str(p->name), string2str(match->compare)))
+                    return true;
+            }
         }
+#endif
     }
 
     return false;
@@ -78,7 +122,7 @@ APPS_MATCH pid_match_create(const char *comm) {
     m.compare = string_strdupz(nid);
 
     if(strchr(nid, '*'))
-        m.pattern = simple_pattern_create(comm, SIMPLE_PATTERN_NO_SEPARATORS, SIMPLE_PATTERN_EXACT, true);
+        m.pattern = simple_pattern_create(comm, SIMPLE_PATTERN_NO_SEPARATORS, SIMPLE_PATTERN_EXACT, false);
 
     return m;
 }
