7a02b64167e1955232d8ee5acea994612515e0ae
Austin S. Hemmelgarn <austin@netdata.cloud>
2025-06-04 07:05:17 -0400

    Fix build architecture checking.

--- packaging/makeself/jobs/82-cpu-arch-check.sh
+++ packaging/makeself/jobs/82-cpu-arch-check.sh
@@ -7,11 +7,7 @@
 # shellcheck disable=SC2015
 [ "${GITHUB_ACTIONS}" = "true" ] && echo "::group::Checking Go plugin CPU architecture" || true
 
-failed=0
-check_files="
-${NETDATA_INSTALL_PATH}/bin/netdata
-${NETDATA_INSTALL_PATH}/usr/libexec/netdata/plugins.d/go.d.plugin
-"
+check_files="${NETDATA_INSTALL_PATH}/bin/netdata ${NETDATA_INSTALL_PATH}/usr/libexec/netdata/plugins.d/go.d.plugin"
 
 case "${BUILDARCH}" in
     aarch64) ELF_MACHINE="AArch64" ;;
@@ -27,11 +23,9 @@ esac
 for f in ${check_files}; do
     if [ "$(readelf -h "${f}" | grep 'Machine' | rev | awk '{print $1}' | rev)" != "${ELF_MACHINE}" ]; then
         echo "${f} was built for the wrong architecture"
-        failed=$((failed + 1))
+        exit 1
     fi
 done
-
-[ "${failed}" -gt 0 ] && exit 1
 
 # shellcheck disable=SC2015
 [ "${GITHUB_ACTIONS}" = "true" ] && echo "::endgroup::" || true
