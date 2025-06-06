0c23951654ab82a83477a9742de84aec6faecac8
thiagoftsm <thiagoftsm@gmail.com>
2025-06-06 00:46:50 +0000

    exchange: Add exchange auto discover request chart

--- src/collectors/windows.plugin/perflib-exchange.c
+++ src/collectors/windows.plugin/perflib-exchange.c
@@ -5,7 +5,54 @@
 
 static void initialize(void)
 {
+    ;
 }
+
+static
+void netdata_exchange_auto_discover(PERF_DATA_BLOCK *pDataBlock, PERF_OBJECT_TYPE *pObjectType, int update_every) {
+    static COUNTER_DATA exchangeAutoDiscoverRequestsTotal = {.key = "Requests/sec" };
+
+    static RRDSET *st_exchange_auto_discover_request_total = NULL;
+    static RRDDIM *rd_exchange_auto_discover_request_total = NULL;
+
+    if (!perflibGetObjectCounter(pDataBlock, pObjectType, &exchangeAutoDiscoverRequestsTotal)) {
+        return;
+    }
+
+    if (unlikely(!st_exchange_auto_discover_request_total)) {
+        st_exchange_auto_discover_request_total = rrdset_create_localhost(
+            "exchange",
+            "exchange_autodiscover_requests",
+            NULL,
+            "requests",
+            "exchange.autodiscover_requests",
+            "Autodiscover service requests processed.",
+            "requests/s",
+            PLUGIN_WINDOWS_NAME,
+            "PerflibExchange",
+            PRIO_EXCHANGE_AUTO_DISCOVER_REQUESTS,
+            update_every,
+            RRDSET_TYPE_LINE);
+
+        rd_exchange_auto_discover_request_total =
+            rrddim_add(st_exchange_auto_discover_request_total, "processed", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
+    }
+
+    rrddim_set_by_pointer(
+        st_exchange_auto_discover_request_total,
+        rd_exchange_auto_discover_request_total,
+        (collected_number)exchangeAutoDiscoverRequestsTotal.current.Data);
+    rrdset_done(st_exchange_auto_discover_request_total);
+}
+
+struct netdata_exchange_objects {
+    void (*fnct)(PERF_DATA_BLOCK *, PERF_OBJECT_TYPE *, int);
+    char *object;
+} exchange_obj[] = {
+    {.fcnt = netdata_exchange_auto_discover, .object = "MSExchangeAutodiscover"},
+
+    // This is the end of the loop
+    {.fcnt = NULL, .object = NULL}};
 
 int do_PerflibExchange(int update_every, usec_t dt __maybe_unused)
 {
@@ -14,6 +61,22 @@ int do_PerflibExchange(int update_every, usec_t dt __maybe_unused)
     if (unlikely(!initialized)) {
         initialize();
         initialized = true;
+    }
+
+    for (int i = 0; exchange_obj[i]; i++) {
+        DWORD id = RegistryFindIDByName(exchange_obj[i].object);
+        if (id == PERFLIB_REGISTRY_NAME_NOT_FOUND)
+            continue;
+
+        PERF_DATA_BLOCK *pDataBlock = perflibGetPerformanceData(id);
+        if (!pDataBlock)
+            continue;
+
+        PERF_OBJECT_TYPE *pObjectType = perflibFindObjectTypeByName(pDataBlock, exchange_obj[i].object);
+        if (!pObjectType)
+            continue;
+
+        exchange_obj[i].fnct(pDataBlock, pObjectType, update_every);
     }
 
     return 0;
