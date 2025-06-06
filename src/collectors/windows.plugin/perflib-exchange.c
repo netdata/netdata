1c887a5547ac2dcd56d7072747274beda3f3896e
thiagoftsm <thiagoftsm@gmail.com>
2025-06-06 17:17:29 +0000

    exchange: Add Sync charts

--- src/collectors/windows.plugin/perflib-exchange.c
+++ src/collectors/windows.plugin/perflib-exchange.c
@@ -82,6 +82,113 @@ void netdata_exchange_owa(PERF_DATA_BLOCK *pDataBlock, PERF_OBJECT_TYPE *pObject
     netdata_exchange_owa_request_total(&exchangeOWARequestsTotal, update_every);
 }
 
+static void netdata_exchange_active_ping_cmd(COUNTER_DATA *value, int update_every) {
+    static RRDSET *st_exchange_active_ping_cmds = NULL;
+    static RRDDIM *rd_exchange_active_ping_cmds = NULL;
+
+    if (unlikely(!st_exchange_active_ping_cmds)) {
+        st_exchange_active_ping_cmds = rrdset_create_localhost(
+            "exchange",
+            "activesync_ping_cmds_pending",
+            NULL,
+            "sync",
+            "exchange.activesync_ping_cmds_pending",
+            "Ping commands pending in queue.",
+            "commands",
+            PLUGIN_WINDOWS_NAME,
+            "PerflibExchange",
+            PRIO_EXCHANGE_ACTIVE_SYNC_PING_CMDS_PENDING,
+            update_every,
+            RRDSET_TYPE_LINE);
+
+        rd_exchange_active_ping_cmds =
+            rrddim_add(st_exchange_active_ping_cmds, "ping", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
+    }
+
+    rrddim_set_by_pointer(
+        st_exchange_active_ping_cmds,
+        rd_exchange_active_ping_cmds,
+        (collected_number)value->current.Data);
+    rrdset_done(st_exchange_active_ping_cmds);
+}
+
+static void netdata_exchange_active_requests(COUNTER_DATA *value, int update_every) {
+    static RRDSET *st_exchange_received_requests = NULL;
+    static RRDDIM *rd_exchange_received_requests = NULL;
+
+    if (unlikely(!st_exchange_received_requests)) {
+        st_exchange_received_requests = rrdset_create_localhost(
+            "exchange",
+            "activesync_requests",
+            NULL,
+            "sync",
+            "exchange.activesync_requests",
+            "HTTP requests received from ASP.NET.",
+            "requests/s",
+            PLUGIN_WINDOWS_NAME,
+            "PerflibExchange",
+            PRIO_EXCHANGE_ACTIVE_SYNC_REQUESTS,
+            update_every,
+            RRDSET_TYPE_LINE);
+
+        rd_exchange_received_requests =
+            rrddim_add(st_exchange_received_requests, "requests", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
+    }
+
+    rrddim_set_by_pointer(
+        st_exchange_received_requests,
+        rd_exchange_received_requests,
+        (collected_number)value->current.Data);
+    rrdset_done(st_exchange_received_requests);
+}
+
+static void netdata_exchange_sync_cmds(COUNTER_DATA *value, int update_every) {
+    static RRDSET *st_exchange_sync_cmds = NULL;
+    static RRDDIM *rd_exchange_sync_cmds = NULL;
+
+    if (unlikely(!st_exchange_sync_cmds)) {
+        st_exchange_sync_cmds = rrdset_create_localhost(
+            "exchange",
+            "activesync_sync_cmds",
+            NULL,
+            "sync",
+            "exchange.activesync_sync_cmds",
+            "Sync commands processed.",
+            "commands/s",
+            PLUGIN_WINDOWS_NAME,
+            "PerflibExchange",
+            PRIO_EXCHANGE_ACTIVE_SYNC_CMDS,
+            update_every,
+            RRDSET_TYPE_LINE);
+
+        rd_exchange_sync_cmds =
+            rrddim_add(st_exchange_sync_cmds, "sync", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
+    }
+
+    rrddim_set_by_pointer(
+        st_exchange_sync_cmds,
+        rd_exchange_sync_cmds,
+        (collected_number)value->current.Data);
+    rrdset_done(st_exchange_sync_cmds);
+}
+
+static
+void netdata_exchange_active_sync(PERF_DATA_BLOCK *pDataBlock, PERF_OBJECT_TYPE *pObjectType, int update_every) {
+    static COUNTER_DATA exchangePingCommands = {.key = "Ping Commands Pending" };
+    static COUNTER_DATA exchangeSyncCommands = {.key = "Sync Commands/sec" };
+    static COUNTER_DATA exchangeActiveRequests = {.key = "Requests/sec" };
+
+    if (!perflibGetObjectCounter(pDataBlock, pObjectType, &exchangePingCommands) ||
+        !perflibGetObjectCounter(pDataBlock, pObjectType, &exchangeSyncCommands) ||
+        !perflibGetObjectCounter(pDataBlock, pObjectType, &exchangeActiveRequests)) {
+        return;
+    }
+
+    netdata_exchange_active_ping_cmd(&exchangePingCommands, update_every);
+    netdata_exchange_active_requests(&exchangeActiveRequests, update_every);
+    netdata_exchange_sync_cmds(&exchangeSyncCommands, update_every);
+}
+
 static
 void netdata_exchange_auto_discover(PERF_DATA_BLOCK *pDataBlock, PERF_OBJECT_TYPE *pObjectType, int update_every) {
     static COUNTER_DATA exchangeAutoDiscoverRequestsTotal = {.key = "Requests/sec" };
@@ -161,6 +268,7 @@ struct netdata_exchange_objects {
     char *object;
 } exchange_obj[] = {
     {.fnct = netdata_exchange_owa, .object = "MSExchange OWA"},
+    {.fnct = netdata_exchange_active_sync, .object = "MSExchange ActiveSync"},
     {.fnct = netdata_exchange_auto_discover, .object = "MSExchangeAutodiscover"},
     {.fnct = netdata_exchange_availability_service, .object = "MSExchange Availability Service"},
 
