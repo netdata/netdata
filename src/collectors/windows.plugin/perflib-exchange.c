9c6c91e1bf59ef0944285aa23230142d9b8336f9
thiagoftsm <thiagoftsm@gmail.com>
2025-06-07 02:42:09 +0000

    exchange: Add Exchange RPC

--- src/collectors/windows.plugin/perflib-exchange.c
+++ src/collectors/windows.plugin/perflib-exchange.c
@@ -28,7 +28,7 @@ static void netdata_exchange_owa_current_unique_users(COUNTER_DATA *value, int u
             RRDSET_TYPE_LINE);
 
         rd_exchange_owa_unique_users =
-            rrddim_add(st_exchange_owa_unique_users, "users", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
+            rrddim_add(st_exchange_owa_unique_users, "users", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
     }
 
     rrddim_set_by_pointer(
@@ -263,6 +263,214 @@ void netdata_exchange_availability_service(PERF_DATA_BLOCK *pDataBlock, PERF_OBJ
     rrdset_done(st_exchange_avail_service_requests);
 }
 
+static void netdata_exchange_rpc_avg_latency(COUNTER_DATA *value, int update_every) {
+    static RRDSET *st_exchange_rpc_avg_latency = NULL;
+    static RRDDIM *rd_exchange_rpc_avg_latency = NULL;
+
+    if (unlikely(!st_exchange_rpc_avg_latency)) {
+        st_exchange_rpc_avg_latency = rrdset_create_localhost(
+            "exchange",
+            "rpc_avg_latency",
+            NULL,
+            "rpc",
+            "exchange.rpc_avg_latency",
+            "Average latency.",
+            "seconds",
+            PLUGIN_WINDOWS_NAME,
+            "PerflibExchange",
+            PRIO_EXCHANGE_RPC_AVG_LATENCY,
+            update_every,
+            RRDSET_TYPE_LINE);
+
+        rd_exchange_rpc_avg_latency =
+            rrddim_add(st_exchange_rpc_avg_latency, "latency", NULL, 1, 1000, RRD_ALGORITHM_ABSOLUTE);
+    }
+
+    rrddim_set_by_pointer(
+        st_exchange_rpc_avg_latency,
+        rd_exchange_rpc_avg_latency,
+        (collected_number)value->current.Data);
+    rrdset_done(st_exchange_rpc_avg_latency);
+}
+
+static void netdata_exchange_rpc_requests(COUNTER_DATA *value, int update_every) {
+    static RRDSET *st_exchange_rpc_requests = NULL;
+    static RRDDIM *rd_exchange_rpc_requests = NULL;
+
+    if (unlikely(!st_exchange_rpc_requests)) {
+        st_exchange_rpc_requests = rrdset_create_localhost(
+            "exchange",
+            "rpc_requests_total",
+            NULL,
+            "rpc",
+            "exchange.rpc_requests_total",
+            "Clients requests currently being processed.",
+            "requests",
+            PLUGIN_WINDOWS_NAME,
+            "PerflibExchange",
+            PRIO_EXCHANGE_RPC_REQUESTS,
+            update_every,
+            RRDSET_TYPE_LINE);
+
+        rd_exchange_rpc_requests =
+            rrddim_add(st_exchange_rpc_requests, "requests", NULL, 1, 1000, RRD_ALGORITHM_ABSOLUTE);
+    }
+
+    rrddim_set_by_pointer(
+        st_exchange_rpc_requests,
+        rd_exchange_rpc_requests,
+        (collected_number)value->current.Data);
+    rrdset_done(st_exchange_rpc_requests);
+}
+
+static void netdata_exchange_rpc_active_user_count(COUNTER_DATA *value, int update_every) {
+    static RRDSET *st_exchange_active_user_account = NULL;
+    static RRDDIM *rd_exchange_active_user_account = NULL;
+
+    if (unlikely(!st_exchange_active_user_account)) {
+        st_exchange_active_user_account = rrdset_create_localhost(
+            "exchange",
+            "rpc_active_user",
+            NULL,
+            "rpc",
+            "exchange.rpc_active_user",
+            "Active unique users in the last 2 minutes.",
+            "users",
+            PLUGIN_WINDOWS_NAME,
+            "PerflibExchange",
+            PRIO_EXCHANGE_RPC_ACTIVE_USERS_COUNT,
+            update_every,
+            RRDSET_TYPE_LINE);
+
+        rd_exchange_active_user_account =
+            rrddim_add(st_exchange_active_user_account, "users", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
+    }
+
+    rrddim_set_by_pointer(
+        st_exchange_active_user_account,
+        rd_exchange_active_user_account,
+        (collected_number)value->current.Data);
+    rrdset_done(st_exchange_active_user_account);
+}
+
+static void netdata_exchange_rpc_connection_count(COUNTER_DATA *value, int update_every) {
+    static RRDSET *st_exchange_rpc_connection_count = NULL;
+    static RRDDIM *rd_exchange_rpc_connection_count = NULL;
+
+    if (unlikely(!st_exchange_rpc_connection_count)) {
+        st_exchange_rpc_connection_count = rrdset_create_localhost(
+            "exchange",
+            "rpc_connection",
+            NULL,
+            "rpc",
+            "exchange.rpc_connection",
+            "Client connections.",
+            "connections",
+            PLUGIN_WINDOWS_NAME,
+            "PerflibExchange",
+            PRIO_EXCHANGE_RPC_CONNECTION_COUNT,
+            update_every,
+            RRDSET_TYPE_LINE);
+
+        rd_exchange_rpc_connection_count =
+            rrddim_add(st_exchange_rpc_connection_count, "connections", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
+    }
+
+    rrddim_set_by_pointer(
+        st_exchange_rpc_connection_count,
+        rd_exchange_rpc_connection_count,
+        (collected_number)value->current.Data);
+    rrdset_done(st_exchange_rpc_connection_count);
+}
+
+static void netdata_exchange_rpc_op_per_sec(COUNTER_DATA *value, int update_every) {
+    static RRDSET *st_exchange_rpc_opc_per_sec = NULL;
+    static RRDDIM *rd_exchange_rpc_opc_per_sec = NULL;
+
+    if (unlikely(!st_exchange_rpc_opc_per_sec)) {
+        st_exchange_rpc_opc_per_sec = rrdset_create_localhost(
+            "exchange",
+            "rpc_operations",
+            NULL,
+            "rpc",
+            "exchange.rpc_operations",
+            "RPC operations.",
+            "operations/s",
+            PLUGIN_WINDOWS_NAME,
+            "PerflibExchange",
+            PRIO_EXCHANGE_RPC_OPERATIONS_TOTAL,
+            update_every,
+            RRDSET_TYPE_LINE);
+
+        rd_exchange_rpc_opc_per_sec =
+            rrddim_add(st_exchange_rpc_opc_per_sec, "operations", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
+    }
+
+    rrddim_set_by_pointer(
+        st_exchange_rpc_opc_per_sec,
+        rd_exchange_rpc_opc_per_sec,
+        (collected_number)value->current.Data);
+    rrdset_done(st_exchange_rpc_opc_per_sec);
+}
+
+static void netdata_exchange_rpc_user_count(COUNTER_DATA *value, int update_every) {
+    static RRDSET *st_exchange_rpc_user_count = NULL;
+    static RRDDIM *rd_exchange_rpc_user_count = NULL;
+
+    if (unlikely(!st_exchange_rpc_user_count)) {
+        st_exchange_rpc_user_count = rrdset_create_localhost(
+            "exchange",
+            "rpc_user",
+            NULL,
+            "rpc",
+            "exchange.rpc_user",
+            "RPC users.",
+            "users",
+            PLUGIN_WINDOWS_NAME,
+            "PerflibExchange",
+            PRIO_EXCHANGE_RPC_USER_COUNT,
+            update_every,
+            RRDSET_TYPE_LINE);
+
+        rd_exchange_rpc_user_count =
+            rrddim_add(st_exchange_rpc_user_count, "users", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
+    }
+
+    rrddim_set_by_pointer(
+        st_exchange_rpc_user_count,
+        rd_exchange_rpc_user_count,
+        (collected_number)value->current.Data);
+    rrdset_done(st_exchange_rpc_user_count);
+}
+
+static
+void netdata_exchange_rpc(PERF_DATA_BLOCK *pDataBlock, PERF_OBJECT_TYPE *pObjectType, int update_every) {
+    static COUNTER_DATA exchangeRPCAveragedLatency = {.key = "RPC Averaged Latency" };
+    static COUNTER_DATA exchangeRPCRequest = {.key = "RPC Requests" };
+    static COUNTER_DATA exchangeRPCActiveUserCount = {.key = "Active User Count" };
+    static COUNTER_DATA exchangeRPCConnectionCount = {.key = "Connection Count" };
+    static COUNTER_DATA exchangeRPCOperationPerSec = {.key = "RPC Operations/sec" };
+    static COUNTER_DATA exchangeRPCUserCount = {.key = "User Count" };
+
+    if (perflibGetObjectCounter(pDataBlock, pObjectType, &exchangeRPCAveragedLatency))
+        netdata_exchange_rpc_avg_latency(&exchangeRPCAveragedLatency, update_every);
+
+    if (perflibGetObjectCounter(pDataBlock, pObjectType, &exchangeRPCRequest))
+        netdata_exchange_rpc_requests(&exchangeRPCRequest, update_every);
+
+    if (perflibGetObjectCounter(pDataBlock, pObjectType, &exchangeRPCActiveUserCount))
+        netdata_exchange_rpc_active_user_count(&exchangeRPCRequest, update_every);
+
+    if (perflibGetObjectCounter(pDataBlock, pObjectType, &exchangeRPCConnectionCount))
+        netdata_exchange_rpc_connection_count(&exchangeRPCConnectionCount, update_every);
+
+    if (perflibGetObjectCounter(pDataBlock, pObjectType, &exchangeRPCOperationPerSec))
+        netdata_exchange_rpc_op_per_sec(&exchangeRPCOperationPerSec, update_every);
+
+    if (perflibGetObjectCounter(pDataBlock, pObjectType, &exchangeRPCUserCount))
+        netdata_exchange_rpc_user_count(&exchangeRPCUserCount, update_every);
+}
+
 struct netdata_exchange_objects {
     void (*fnct)(PERF_DATA_BLOCK *, PERF_OBJECT_TYPE *, int);
     char *object;
@@ -271,6 +479,7 @@ struct netdata_exchange_objects {
     {.fnct = netdata_exchange_active_sync, .object = "MSExchange ActiveSync"},
     {.fnct = netdata_exchange_auto_discover, .object = "MSExchangeAutodiscover"},
     {.fnct = netdata_exchange_availability_service, .object = "MSExchange Availability Service"},
+    {.fnct = netdata_exchange_rpc, .object = "MSExchange RpcClientAccess"},
 
     // This is the end of the loop
     {.fnct = NULL, .object = NULL}};
