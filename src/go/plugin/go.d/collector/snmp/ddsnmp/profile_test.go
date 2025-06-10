c55d08d49f2f2ac31f77b1a0508f5001622c6ec5
Ilya Mashchenko <ilya@netdata.cloud>
2025-06-10 11:56:39 +0300

    improvement(go.d/ddsnmp): dedup metrics when merging profiles (#20456)

--- src/go/plugin/go.d/collector/snmp/ddsnmp/profile_test.go
+++ src/go/plugin/go.d/collector/snmp/ddsnmp/profile_test.go
@@ -5,8 +5,11 @@ package ddsnmp
 import (
 	"os"
 	"path/filepath"
+	"slices"
+	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -49,5 +52,23 @@ func Test_FindProfiles(t *testing.T) {
 
 			require.Len(t, profiles, test.wanProfiles)
 		})
+	}
+}
+
+func Test_Profile_merge(t *testing.T) {
+	profiles := FindProfiles("1.3.6.1.4.1.9.1.1216") // cisco-nexus
+
+	i := slices.IndexFunc(profiles, func(p *Profile) bool {
+		return strings.HasSuffix(p.SourceFile, "cisco-nexus.yaml")
+	})
+
+	require.GreaterOrEqual(t, 1, 0)
+
+	for _, m := range profiles[i].Definition.Metrics {
+		if m.IsColumn() && m.Table.Name == "ciscoMemoryPoolTable" {
+			for _, s := range m.Symbols {
+				assert.NotEqual(t, "memory.used", s.Name)
+			}
+		}
 	}
 }
