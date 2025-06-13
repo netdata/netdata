e11d5451df6f9f1368cfbd2f5a6f488284080510
Austin S. Hemmelgarn <austin@netdata.cloud>
2025-06-13 11:09:33 -0400

    Correctly ignore patches that are already applied. (#20480)
    (cherry picked from commit ade0e5be3c2eb010a1e07d1dea9db65f700fe69b)

--- packaging/cmake/patches/apply-patches.sh
+++ packaging/cmake/patches/apply-patches.sh
@@ -3,10 +3,16 @@
 SRC_DIR="${1}"
 PATCH_DIR="${2}"
 
-set -e
-
-cd "${SRC_DIR}"
+cd "${SRC_DIR}" || exit 1
 
 for patch_file in "${PATCH_DIR}"/*; do
-    patch -p1 -i "${patch_file}"
+    patch -p1 --forward -i "${patch_file}"
+    ret="${?}"
+
+    # We want to ignore patches that are already applied instead of
+    # failing the patching process, and patch returns 1 for a patch that
+    # is already applied, and higher numbers for other errors.
+    if [ "${ret}" -gt 1 ] ; then
+        exit 1
+    fi
 done
