1efba82980dc56dffc201a99ce45644bf9c5091c
Ilya Mashchenko <ilya@netdata.cloud>
2025-08-11 17:52:35 +0300

    fix pathvalidate non unix (#20801)

--- src/go/plugin/go.d/pkg/pathvalidate/validate_stub.go
+++ src/go/plugin/go.d/pkg/pathvalidate/validate_stub.go
@@ -6,6 +6,6 @@ package pathvalidate
 
 // ValidateBinaryPath checks if a binary path is secure for execution.
 // This is a stub implementation for non-Unix platforms.
-func ValidateBinaryPath(path string) error {
-	return nil
+func ValidateBinaryPath(path string) (string, error) {
+	return path, nil
 }
