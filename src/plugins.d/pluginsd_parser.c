b4f14a3712695275ac01ff7f8c439d17d846b679
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2025-08-11 07:55:13 +0300

    Virtual node version adjustment (#20777)
    Handle virtual host version announcement to the cloud
    
    (cherry picked from commit 5e7eb79121d8df56409a23f344ff5d665b271401)

--- src/plugins.d/pluginsd_parser.c
+++ src/plugins.d/pluginsd_parser.c
@@ -200,7 +200,7 @@ static inline PARSER_RC pluginsd_host_define_end(char **words __maybe_unused, si
         string2str(parser->user.host_define.hostname),
         string2str(parser->user.host_define.hostname),
         parser->user.host_define.machine_guid_str,
-        "Netdata Virtual Host 1.0",
+        NETDATA_VIRTUAL_HOST,
         netdata_configured_timezone,
         netdata_configured_abbrev_timezone,
         netdata_configured_utc_offset,
