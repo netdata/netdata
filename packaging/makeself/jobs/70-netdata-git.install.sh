da52c1878326fc78d9973faa03c1db5d195abbb3
Austin S. Hemmelgarn <austin@netdata.cloud>
2025-08-14 07:50:13 -0400

    Consolidate compiler flag handling as much as possible.
    Instead of relying on compiler flags being set correctly in our build
    environments by the build scripts, set them appropriately in our CMake
    code.
    
    Also, move as much of our compiler flag handling within the CMake code
    as possible into the NetdataCompilerFlags module.

--- packaging/makeself/jobs/70-netdata-git.install.sh
+++ packaging/makeself/jobs/70-netdata-git.install.sh
@@ -7,37 +7,35 @@
 cd "${NETDATA_SOURCE_PATH}" || exit 1
 
 if [ "${NETDATA_BUILD_WITH_DEBUG}" -eq 0 ]; then
-  export CFLAGS="${TUNING_FLAGS} -ffunction-sections -fdata-sections -static -O2 -funroll-loops -DNETDATA_STATIC_BUILD=1 -I/openssl-static/include -I/libnetfilter-acct-static/include/libnetfilter_acct -I/curl-local/include/curl -I/usr/include/libmnl -pipe"
+  export CFLAGS="${TUNING_FLAGS} -O2 -pipe -funroll-loops -I/openssl-static/include -I/libnetfilter-acct-static/include/libnetfilter_acct -I/curl-local/include/curl -I/usr/include/libmnl"
 else
-  export CFLAGS="${TUNING_FLAGS} -static -O1 -pipe -ggdb -Wall -Wextra -Wformat-signedness -DNETDATA_STATIC_BUILD=1 -DNETDATA_INTERNAL_CHECKS=1 -I/openssl-static/include -I/libnetfilter-acct-static/include/libnetfilter_acct -I/curl-local/include/curl -I/usr/include/libmnl"
+  export CFLAGS="${TUNING_FLAGS} -O1 -pipe -ggdb -Wall -Wextra -Wformat-signedness -DNETDATA_INTERNAL_CHECKS=1 -I/openssl-static/include -I/libnetfilter-acct-static/include/libnetfilter_acct -I/curl-local/include/curl -I/usr/include/libmnl"
 fi
 
-export LDFLAGS="-Wl,--gc-sections -static -L/openssl-static/lib64 -L/libnetfilter-acct-static/lib -lnetfilter_acct -L/usr/lib -lmnl -L/usr/lib -lzstd -L/curl-local/lib"
+export LDFLAGS="-Wl,--gc-sections -L/openssl-static/lib64 -L/libnetfilter-acct-static/lib -lnetfilter_acct -L/usr/lib -lmnl -L/usr/lib -lzstd -L/curl-local/lib"
+export PKG_CONFIG_PATH="/openssl-static/lib64/pkgconfig:/libnetfilter-acct-static/lib/pkgconfig:/usr/lib/pkgconfig:/curl-local/lib/pkgconfig"
 
 # We export this to 'yes', installer sets this to .environment.
 # The updater consumes this one, so that it can tell whether it should update a static install or a non-static one
 export IS_NETDATA_STATIC_BINARY="yes"
 
-# Set eBPF LIBC to "static" to bundle the `-static` variant of the kernel-collector
-export EBPF_LIBC="static"
-export PKG_CONFIG="pkg-config --static"
-export PKG_CONFIG_PATH="/openssl-static/lib64/pkgconfig:/libnetfilter-acct-static/lib/pkgconfig:/usr/lib/pkgconfig:/curl-local/lib/pkgconfig"
-
 NETDATA_BUILD_DIR="$(build_path netdata)"
 export NETDATA_BUILD_DIR
 
+# Needed to make Rust play nice with our static builds
+# Once Cargo’s profile-rustflags feature is a bit more widespread, we should switch to using that to specify this.
+export RUSTFLAGS="-C target-feature=+crt-static"
+
 case "${BUILDARCH}" in
     armv6l)
-        export NETDATA_CMAKE_OPTIONS="-DENABLE_LIBBACKTRACE=On"
+        export NETDATA_CMAKE_OPTIONS="-DBUILD_SHARED_LIBS=Off -DENABLE_LIBBACKTRACE=On"
         export INSTALLER_ARGS="--disable-plugin-systemd-journal --disable-plugin-otel"
         ;;
     *)
-        export NETDATA_CMAKE_OPTIONS="-DENABLE_LIBBACKTRACE=On"
+        export NETDATA_CMAKE_OPTIONS="-DBUILD_SHARED_LIBS=Off -DENABLE_LIBBACKTRACE=On"
         export INSTALLER_ARGS="--enable-plugin-systemd-journal --internal-systemd-journal --enable-plugin-otel"
         ;;
 esac
-
-export RUSTFLAGS="-C target-feature=+crt-static"
 
 run ./netdata-installer.sh \
   --install-prefix "${NETDATA_INSTALL_PARENT}" \
