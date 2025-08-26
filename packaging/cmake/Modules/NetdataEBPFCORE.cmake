cd2c3878e3aa372a8f4af12d360f5ec82a025d47
thiagoftsm <thiagoftsm@gmail.com>
2025-08-26 16:36:11 +0000

    fix_process_tp: Update CO-RE

--- packaging/cmake/Modules/NetdataEBPFCORE.cmake
+++ packaging/cmake/Modules/NetdataEBPFCORE.cmake
@@ -9,8 +9,8 @@ set(ebpf-co-re_SOURCE_DIR "${CMAKE_BINARY_DIR}/ebpf-co-re")
 function(netdata_fetch_ebpf_co_re)
     ExternalProject_Add(
         ebpf-co-re
-        URL https://github.com/netdata/ebpf-co-re/releases/download/v1.6.2/netdata-ebpf-co-re-glibc-v1.6.2.tar.xz
-        URL_HASH SHA256=144637fd0d2cbe19ec5819a396aa502bcacd04ca11b664a776cb13aef5c8d768
+        URL https://github.com/netdata/ebpf-co-re/releases/download/v1.6.2.1/netdata-ebpf-co-re-glibc-v1.6.2.1.tar.xz
+        URL_HASH SHA256=3c8bf9599d348ae5f8b97da48893378bf62f97f688424033758caa3e6994148c
         SOURCE_DIR "${ebpf-co-re_SOURCE_DIR}"
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
