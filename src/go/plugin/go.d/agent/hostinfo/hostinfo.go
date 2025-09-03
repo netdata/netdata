20e4fabe46759e4cecbfd679a3f2910d8f3be8bd
Ilya Mashchenko <ilya@netdata.cloud>
2025-09-03 16:29:24 +0300

    chore(go.d): remove unused resolve hostname functionality (#20922)

--- src/go/plugin/go.d/agent/hostinfo/hostinfo.go
+++ src/go/plugin/go.d/agent/hostinfo/hostinfo.go
@@ -3,31 +3,8 @@
 package hostinfo
 
 import (
-	"bytes"
-	"context"
 	"os"
-	"os/exec"
-	"time"
 )
-
-var Hostname = getHostname()
-
-func getHostname() string {
-	path, err := exec.LookPath("hostname")
-	if err != nil {
-		return ""
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
-	defer cancel()
-
-	bs, err := exec.CommandContext(ctx, path).Output()
-	if err != nil {
-		return ""
-	}
-
-	return string(bytes.TrimSpace(bs))
-}
 
 var (
 	envKubeHost = os.Getenv("KUBERNETES_SERVICE_HOST")
