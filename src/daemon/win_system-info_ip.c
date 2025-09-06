08a4b80dd593d27b7e55541c3e9aa3d968934bad
thiagoftsm <thiagoftsm@gmail.com>
2025-09-06 00:27:01 +0000

    win_ip_labels: Fill variables properly

--- src/daemon/win_system-info_ip.c
+++ src/daemon/win_system-info_ip.c
@@ -23,7 +23,50 @@ static unsigned int netdata_fill_default_ip()
 
     default_ip.initialized = true;
 
-    return 0;
+    MIB_IPFORWARDROW route;
+    DWORD dest = 0;
+    if (GetBestRoute(dest, 0, &route) != NO_ERROR) {
+        return -1;
+    }
+
+    DWORD ifIndex = route.dwForwardIfIndex;
+
+    ULONG bufLen = 15000;
+    PIP_ADAPTER_ADDRESSES adapters = (PIP_ADAPTER_ADDRESSES)malloc(bufLen);
+    if (!adapters) {
+        return 1;
+    }
+
+    int ret = GetAdaptersAddresses(AF_INET, GAA_FLAG_INCLUDE_PREFIX, NULL, adapters, &bufLen);
+    if (ret != NO_ERROR) {
+        goto end_ip_detection;
+    }
+
+    PIP_ADAPTER_ADDRESSES aa = adapters;
+    while (aa) {
+        if (aa->IfIndex == ifIndex) {
+            default_ip.interface = strdup(aa->FriendlyName);
+
+            PIP_ADAPTER_UNICAST_ADDRESS ua = aa->FirstUnicastAddress;
+            while (ua) {
+                if (ua->Address.lpSockaddr->sa_family == AF_INET) {
+                    char ipstr[INET_ADDRSTRLEN];
+                    struct sockaddr_in *sa_in = (struct sockaddr_in *)ua->Address.lpSockaddr;
+                    inet_ntop(AF_INET, &(sa_in->sin_addr), ipstr, sizeof(ipstr));
+                    default_ip.ipaddr = strdup(ipstr);
+                    goto end_ip_detection;
+                }
+                ua = ua->Next;
+            }
+            break;
+        }
+        aa = aa->Next;
+    }
+
+    ret = NO_ERROR;
+end_ip_detection:
+    free(adapters);
+    return ret;
 }
 
 char *netdata_win_local_interface()
