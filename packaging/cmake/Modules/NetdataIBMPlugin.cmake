72280eb29405d0dea3fa07a873d9a33a182d05fb
Austin S. Hemmelgarn <austin@netdata.cloud>
2025-09-30 08:20:52 -0400

    Fix platform detection and MQ library retrieval.

--- packaging/cmake/Modules/NetdataIBMPlugin.cmake
+++ packaging/cmake/Modules/NetdataIBMPlugin.cmake
@@ -12,14 +12,15 @@ macro(add_ibm_plugin_target)
 
   FetchContent_Declare(
     ibm_mq
-    URL https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/redist/9.4.1.0-IBM-MQC-Redist-LinuxX64.tar.gz
-    URL_HASH SHA256=9df98ab0ab69954fa080dc7765c9a3eb7cd6c683e5d596ae10c4fbb32eec2ad8
     SOURCE_DIR "${IBM_MQ_DIR}"
-    CONFIGURE_COMMAND "${CMAKE_COMMAND}" -E true
-    BUILD_COMMAND "${CMAKE_COMMAND}" -E true
-    INSTALL_COMMAND "${CMAKE_COMMAND}" -E true
+    CONFIGURE_COMMAND "${CMAKE_COMMAND} -E true"
+    BUILD_COMMAND "${CMAKE_COMMAND} -E true"
+    INSTALL_COMMAND "${CMAKE_COMMAND} -E true"
+    URL https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/redist/9.4.1.0-IBM-MQC-Redist-LinuxX64.tar.gz
+    URL_HASH SHA256=f0759f621c3ccbbad2764204fc3d9ccef6409fd8293e23b351cceb3c6c6a0478
   )
 
+  message(STATUS "Fetching IBM MQ client libraries for build")
   FetchContent_MakeAvailable(ibm_mq)
 
   # Find all Go source files - resolve symlinks for consistent dependency tracking
@@ -39,7 +40,6 @@ macro(add_ibm_plugin_target)
 
   # Add MQ support if MQ client libraries are available
   if(EXISTS "${IBM_MQ_DIR}/inc/cmqc.h")
-    message(STATUS "IBM MQ client libraries found - enabling MQ PCF collector support")
     set(IBM_CGO_CFLAGS "-I${IBM_MQ_DIR}/inc")
     set(IBM_CGO_LDFLAGS "-L${IBM_MQ_DIR}/lib64 -lmqic_r")
     set(IBM_RPATH_FLAGS "-Wl,-rpath,${IBM_MQ_DIR}/lib64")
