a1ce8dbb4c79d5f54db61cf23a88b1770a010c62
Austin S. Hemmelgarn <austin@netdata.cloud>
2025-10-13 11:32:09 -0400

    Switch to using CMakePushCheckState to manage check state.
    This helps ensure that additional configuration required for specific
    individual checks doesn’t spill over to other checks.

--- packaging/cmake/Modules/NetdataDetectSystemd.cmake
+++ packaging/cmake/Modules/NetdataDetectSystemd.cmake
@@ -1,40 +1,41 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # CMake Module to handle all the systemd-related checks for Netdata.
 
+include(CMakePushCheckState)
+
 macro(detect_systemd)
-   find_library(SYSTEMD_LIBRARY NAMES systemd)
+  find_library(SYSTEMD_LIBRARY NAMES systemd)
 
-   set(ENABLE_DSYSTEMD_DBUS NO)
-   pkg_check_modules(SYSTEMD libsystemd)
+  set(ENABLE_DSYSTEMD_DBUS NO)
+  pkg_check_modules(SYSTEMD libsystemd)
 
-   if(SYSTEMD_FOUND)
-       set(CMAKE_REQUIRED_LIBRARIES_BEFORE_SYSTEMD "${CMAKE_REQUIRED_LIBRARIES}")
-       set(CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES};${SYSTEMD_LIBRARIES}")
+  if(SYSTEMD_FOUND)
+    cmake_push_check_state()
+    set(CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES};${SYSTEMD_LIBRARIES}")
 
-       check_c_source_compiles("
-       #include <systemd/sd-journal.h>
+    check_c_source_compiles("
+    #include <systemd/sd-journal.h>
 
-       int main() {
-           int x = SD_JOURNAL_OS_ROOT;
-           return 0;
-       }" HAVE_SD_JOURNAL_OS_ROOT)
+    int main() {
+      int x = SD_JOURNAL_OS_ROOT;
+      return 0;
+    }" HAVE_SD_JOURNAL_OS_ROOT)
 
-       check_symbol_exists(SD_JOURNAL_OS_ROOT "systemd/sd-journal.h" HAVE_SD_JOURNAL_OS_ROOT)
-       check_symbol_exists(sd_journal_open_files_fd "systemd/sd-journal.h" HAVE_SD_JOURNAL_OPEN_FILES_FD)
-       check_symbol_exists(sd_journal_restart_fields "systemd/sd-journal.h" HAVE_SD_JOURNAL_RESTART_FIELDS)
-       check_symbol_exists(sd_journal_get_seqnum "systemd/sd-journal.h" HAVE_SD_JOURNAL_GET_SEQNUM)
+    check_symbol_exists(SD_JOURNAL_OS_ROOT "systemd/sd-journal.h" HAVE_SD_JOURNAL_OS_ROOT)
+    check_symbol_exists(sd_journal_open_files_fd "systemd/sd-journal.h" HAVE_SD_JOURNAL_OPEN_FILES_FD)
+    check_symbol_exists(sd_journal_restart_fields "systemd/sd-journal.h" HAVE_SD_JOURNAL_RESTART_FIELDS)
+    check_symbol_exists(sd_journal_get_seqnum "systemd/sd-journal.h" HAVE_SD_JOURNAL_GET_SEQNUM)
 
-       check_symbol_exists(sd_bus_default_system "systemd/sd-bus.h" HAVE_SD_BUS_DEFAULT_SYSTEM)
-       check_symbol_exists(sd_bus_call_method "systemd/sd-bus.h" HAVE_SD_BUS_CALL_METHOD)
-       check_symbol_exists(sd_bus_message_enter_container "systemd/sd-bus.h" HAVE_SD_BUS_MESSAGE_ENTER_CONTAINER)
-       check_symbol_exists(sd_bus_message_read "systemd/sd-bus.h" HAVE_SD_BUS_MESSAGE_READ)
-       check_symbol_exists(sd_bus_message_exit_container "systemd/sd-bus.h" HAVE_SD_BUS_MESSAGE_EXIT_CONTAINER)
+    check_symbol_exists(sd_bus_default_system "systemd/sd-bus.h" HAVE_SD_BUS_DEFAULT_SYSTEM)
+    check_symbol_exists(sd_bus_call_method "systemd/sd-bus.h" HAVE_SD_BUS_CALL_METHOD)
+    check_symbol_exists(sd_bus_message_enter_container "systemd/sd-bus.h" HAVE_SD_BUS_MESSAGE_ENTER_CONTAINER)
+    check_symbol_exists(sd_bus_message_read "systemd/sd-bus.h" HAVE_SD_BUS_MESSAGE_READ)
+    check_symbol_exists(sd_bus_message_exit_container "systemd/sd-bus.h" HAVE_SD_BUS_MESSAGE_EXIT_CONTAINER)
 
-       set(CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES_BEFORE_SYSTEMD}")
-
-       set(HAVE_SYSTEMD True)
-       if(HAVE_SD_BUS_DEFAULT_SYSTEM AND HAVE_SD_BUS_CALL_METHOD AND HAVE_SD_BUS_MESSAGE_ENTER_CONTAINER AND HAVE_SD_BUS_MESSAGE_READ AND HAVE_SD_BUS_MESSAGE_EXIT_CONTAINER)
-           set(ENABLE_SYSTEMD_DBUS YES)
-       endif()
-   endif()
+    cmake_pop_check_state()
+    set(HAVE_SYSTEMD True)
+    if(HAVE_SD_BUS_DEFAULT_SYSTEM AND HAVE_SD_BUS_CALL_METHOD AND HAVE_SD_BUS_MESSAGE_ENTER_CONTAINER AND HAVE_SD_BUS_MESSAGE_READ AND HAVE_SD_BUS_MESSAGE_EXIT_CONTAINER)
+      set(ENABLE_SYSTEMD_DBUS YES)
+    endif()
+  endif()
 endmacro()
