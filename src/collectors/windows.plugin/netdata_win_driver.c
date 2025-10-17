1c6c468c1231ec96ef23ec928bede485b40590bb
thiagoftsm <thiagoftsm@gmail.com>
2025-10-17 04:04:24 +0000

    cpu_temp_win: Address CPU temperature error

--- src/collectors/windows.plugin/netdata_win_driver.c
+++ src/collectors/windows.plugin/netdata_win_driver.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include <ddk/ntddk.h>
+#include <intrin.h>
 
 #define MSR_IA32_THERM_STATUS 0x19C
 #define MSR_DEVICE_NAME L"\\Device\\NDDrv"
@@ -8,7 +9,6 @@
 
 #define IOCTL_MSR_READ CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
 
-// Payloads
 typedef struct _MSR_REQUEST {
     ULONG msr;
     ULONG cpu;
@@ -20,21 +20,22 @@ DRIVER_UNLOAD     NetdataMsrUnload;
 DRIVER_DISPATCH   NetdataMsrCreateClose;
 DRIVER_DISPATCH   NetdataMsrDeviceControl;
 
-extern ULONGLONG __readmsr(ULONG reg);
-
 static UNICODE_STRING g_DeviceName  = RTL_CONSTANT_STRING(MSR_DEVICE_NAME);
 static UNICODE_STRING g_DosLinkName = RTL_CONSTANT_STRING(MSR_DOSLINK_NAME);
 
 VOID NetdataMsrUnload(_In_ PDRIVER_OBJECT DriverObject)
 {
-    UNREFERENCED_PARAMETER(DriverObject);
     IoDeleteSymbolicLink(&g_DosLinkName);
-    IoDeleteDevice(DriverObject->DeviceObject);
+
+    if (DriverObject->DeviceObject) {
+        IoDeleteDevice(DriverObject->DeviceObject);
+    }
 }
 
 NTSTATUS NetdataMsrCreateClose(_In_ PDEVICE_OBJECT DeviceObject, _Inout_ PIRP Irp)
 {
     UNREFERENCED_PARAMETER(DeviceObject);
+
     Irp->IoStatus.Status = STATUS_SUCCESS;
     Irp->IoStatus.Information = 0;
     IoCompleteRequest(Irp, IO_NO_INCREMENT);
@@ -44,45 +45,60 @@ NTSTATUS NetdataMsrCreateClose(_In_ PDEVICE_OBJECT DeviceObject, _Inout_ PIRP Ir
 NTSTATUS NetdataMsrDeviceControl(_In_ PDEVICE_OBJECT DeviceObject, _Inout_ PIRP Irp)
 {
     UNREFERENCED_PARAMETER(DeviceObject);
-    PIO_STACK_LOCATION irpSp = IoGetCurrentIrpStackLocation(Irp);
 
+    PIO_STACK_LOCATION irpSp = IoGetCurrentIrpStackLocation(Irp);
     NTSTATUS status = STATUS_SUCCESS;
+    ULONG information = 0;
 
     if (irpSp->Parameters.DeviceIoControl.IoControlCode == IOCTL_MSR_READ) {
-        PMSR_REQUEST req = (PMSR_REQUEST)Irp->AssociatedIrp.SystemBuffer;
+        if (irpSp->Parameters.DeviceIoControl.InputBufferLength < sizeof(MSR_REQUEST) ||
+            irpSp->Parameters.DeviceIoControl.OutputBufferLength < sizeof(MSR_REQUEST)) {
+            status = STATUS_BUFFER_TOO_SMALL;
+        } else {
+            PMSR_REQUEST req = (PMSR_REQUEST)Irp->AssociatedIrp.SystemBuffer;
 
-        KAFFINITY cpuMask = 1ull << req->cpu;
+            ULONG maxCpus = KeQueryActiveProcessorCountEx(ALL_PROCESSOR_GROUPS);
+            if (req->cpu >= maxCpus) {
+                status = STATUS_INVALID_PARAMETER;
+            } else {
+                KAFFINITY cpuMask = 1ull << req->cpu;
+                KAFFINITY oldMask = KeSetSystemAffinityThreadEx(cpuMask);
 
-        KAFFINITY oldMask = KeSetSystemAffinityThreadEx(cpuMask);
+                ULONGLONG value = __readmsr(req->msr);
+                req->low = (ULONG)(value & 0xFFFFFFFF);
+                req->high = (ULONG)(value >> 32);
+                information = sizeof(MSR_REQUEST);
 
-        ULONGLONG value = __readmsr(req->msr);
-
-        KeRevertToUserAffinityThreadEx(oldMask);
-
-        req->low = (ULONG)(value & 0xFFFFFFFF);
-        req->high = (ULONG)(value >> 32);
-
-        Irp->IoStatus.Information = sizeof(MSR_REQUEST);
+                KeRevertToUserAffinityThreadEx(oldMask);
+            }
+        }
     } else {
         status = STATUS_INVALID_DEVICE_REQUEST;
     }
+
+    Irp->IoStatus.Status = status;
+    Irp->IoStatus.Information = information;
+    IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
     return status;
 }
 
 NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 {
     UNREFERENCED_PARAMETER(RegistryPath);
-
+    NTSTATUS status;
     PDEVICE_OBJECT deviceObject = NULL;
-    NTSTATUS status = IoCreateDevice(
-        DriverObject,
-        0,                      // no device extension
-        &g_DeviceName,
-        FILE_DEVICE_UNKNOWN,
-        0,
-        FALSE,
-        &deviceObject
+
+    status = IoCreateDevice(
+            DriverObject,
+            0,
+            &g_DeviceName,
+            FILE_DEVICE_UNKNOWN,
+            FILE_DEVICE_SECURE_OPEN,
+            FALSE,
+            &deviceObject
     );
+
     if (!NT_SUCCESS(status)) {
         return status;
     }
@@ -93,12 +109,14 @@ NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING Regi
         return status;
     }
 
-    // Dispatch routines
+    deviceObject->Flags |= DO_BUFFERED_IO;
+
     DriverObject->MajorFunction[IRP_MJ_CREATE]         = NetdataMsrCreateClose;
     DriverObject->MajorFunction[IRP_MJ_CLOSE]          = NetdataMsrCreateClose;
     DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = NetdataMsrDeviceControl;
     DriverObject->DriverUnload                         = NetdataMsrUnload;
 
     deviceObject->Flags &= ~DO_DEVICE_INITIALIZING;
+
     return STATUS_SUCCESS;
 }
