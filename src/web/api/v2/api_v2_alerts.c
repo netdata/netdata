9b9ee3f286d328c03d0aec1ac326225b440adb3a
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2025-10-23 16:06:04 +0300

    Alerts api summary option (#21192)
    Summary is not the default

--- src/web/api/v2/api_v2_alerts.c
+++ src/web/api/v2/api_v2_alerts.c
@@ -3,5 +3,5 @@
 #include "api_v2_calls.h"
 
 int api_v2_alerts(RRDHOST *host __maybe_unused, struct web_client *w, char *url) {
-    return api_v2_contexts_internal(host, w, url, CONTEXTS_V2_ALERTS | CONTEXTS_V2_NODES, CONTEXTS_OPTION_SUMMARY);
+    return api_v2_contexts_internal(host, w, url, CONTEXTS_V2_ALERTS | CONTEXTS_V2_NODES, 0);
 }
