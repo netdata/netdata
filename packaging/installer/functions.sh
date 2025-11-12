c88790175a14907a6b1deac4d0cdcf40cff9b29b
Costa Tsaousis <costa@netdata.cloud>
2025-11-13 00:05:07 +0200

    added to installer

--- packaging/installer/functions.sh
+++ packaging/installer/functions.sh
@@ -379,6 +379,7 @@ prepare_cmake_options() {
   enable_feature PLUGIN_APPS "${ENABLE_APPS:-1}"
   enable_feature PLUGIN_OTEL "${ENABLE_OTEL:-0}"
   enable_feature PLUGIN_IBM "${ENABLE_IBM:-0}"
+  enable_feature PLUGIN_SCRIPTS "${ENABLE_SCRIPTS:-0}"
 
   check_for_feature EXPORTER_PROMETHEUS_REMOTE_WRITE "${EXPORTER_PROMETHEUS}" snappy
   check_for_feature EXPORTER_MONGODB "${EXPORTER_MONGODB}" libmongoc-1.0
