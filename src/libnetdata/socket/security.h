b91307e29245672f91d899f4311fbfc88755c59e
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2025-12-12 22:18:43 +0200

    Improve streaming connection loss detection (#21430)
    * Enable TCP keepalive and implement dead connection detection in stream receiver.
    
    * Add netdata_ssl_peek() and nd_sock_peek_nowait() to support non-blocking socket peeking for SSL and plain TCP
    
    * Refine TCP keepalive configuration and improve socket error handling in stream receiver.
    
    * Handle ECONNRESET in socket peek and refine error checks in stream receiver.
    
    * Refine SSL error handling and set `errno` for incomplete handshakes.

--- src/libnetdata/socket/security.h
+++ src/libnetdata/socket/security.h
@@ -48,6 +48,7 @@ void netdata_ssl_close(NETDATA_SSL *ssl);
 
 ssize_t netdata_ssl_read(NETDATA_SSL *ssl, void *buf, size_t num);
 ssize_t netdata_ssl_write(NETDATA_SSL *ssl, const void *buf, size_t num);
+ssize_t netdata_ssl_peek(NETDATA_SSL *ssl, void *buf, size_t num);
 
 ssize_t netdata_ssl_pending(NETDATA_SSL *ssl);
 bool netdata_ssl_has_pending(NETDATA_SSL *ssl);
