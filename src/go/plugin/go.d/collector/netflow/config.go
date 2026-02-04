c4d809f15395b1065a6ecc89dbc64aa6eb6dfc69
Costa Tsaousis <costa@netdata.cloud>
2026-02-04 02:40:27 +0200

    phase 2

--- src/go/plugin/go.d/collector/netflow/config.go
+++ src/go/plugin/go.d/collector/netflow/config.go
@@ -4,19 +4,20 @@ package netflow
 
 type (
 	Config struct {
-		Vnode       string           `yaml:"vnode,omitempty" json:"vnode"`
-		UpdateEvery int              `yaml:"update_every,omitempty" json:"update_every"`
-		Address     string           `yaml:"address" json:"address"`
-		Protocols   ProtocolsConfig  `yaml:"protocols,omitempty" json:"protocols"`
+		Vnode       string            `yaml:"vnode,omitempty" json:"vnode"`
+		UpdateEvery int               `yaml:"update_every,omitempty" json:"update_every"`
+		Address     string            `yaml:"address" json:"address"`
+		Protocols   ProtocolsConfig   `yaml:"protocols,omitempty" json:"protocols"`
 		Aggregation AggregationConfig `yaml:"aggregation,omitempty" json:"aggregation"`
-		Sampling    SamplingConfig   `yaml:"sampling,omitempty" json:"sampling"`
-		Exporters   []ExporterConfig `yaml:"exporters,omitempty" json:"exporters"`
+		Sampling    SamplingConfig    `yaml:"sampling,omitempty" json:"sampling"`
+		Exporters   []ExporterConfig  `yaml:"exporters,omitempty" json:"exporters"`
 	}
 
 	ProtocolsConfig struct {
 		NetFlowV5 bool `yaml:"netflow_v5" json:"netflow_v5"`
 		NetFlowV9 bool `yaml:"netflow_v9" json:"netflow_v9"`
 		IPFIX     bool `yaml:"ipfix" json:"ipfix"`
+		SFlow     bool `yaml:"sflow" json:"sflow"`
 	}
 
 	AggregationConfig struct {
