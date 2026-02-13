3ce757305f055e4b788b7ba46bc95959c88cd1a0
ilyam8 <ilya@netdata.cloud>
2026-02-13 12:36:07 +0200

    fix

--- src/go/cmd/scriptsdplugin/main.go
+++ src/go/cmd/scriptsdplugin/main.go
@@ -21,7 +21,6 @@ import (
 	"github.com/netdata/netdata/go/plugins/plugin/go.d/agent"
 	_ "github.com/netdata/netdata/go/plugins/plugin/scripts.d/modules/nagios"
 	_ "github.com/netdata/netdata/go/plugins/plugin/scripts.d/modules/scheduler"
-	_ "github.com/netdata/netdata/go/plugins/plugin/scripts.d/modules/zabbix"
 )
 
 func init() {
