15ffa4faeb78cee19bece455f9ceaf1193a74faa
Austin S. Hemmelgarn <austin@netdata.cloud>
2026-03-06 07:01:57 -0500

    Handle fetching IBM MQ libraries in CI package build jobs outside of CMake (#21862)
    * Support FETCHCONTENT_SOURCE_DIR for IBM MQ libs.
    
    This allows us to handle fetching them as a separate step in CI, letting
    us better handle the errors resulting from IBM’s questioanble
    infrastructure.
    
    * Pre-fetch IBM MQ libraries in package build CI jobs.
    
    This lets us better handle infrastructure issues when trying to fetch
    the libraries.
    
    * Fix duplicate CONFIGURE_COMMAND.
    
    * Address review comments from copilot.
    
    * Fix additional bugs.
    
    * Don’t use loop for file handling.

---
+++ .github/scripts/prep-ibm-mq-libs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -euo pipefail
+
+url="$(cat packaging/vendor/ibm_mq.url)"
+sha256="$(cat packaging/vendor/ibm_mq.sha256)"
+file="${url##*/}"
+cache_dir="artifacts/cache"
+tmp_dir="tmp"
+hash_path="${tmp_dir}/hash"
+dl_path="${tmp_dir}/${file}"
+cache_path="${cache_dir}/${file}"
+extract_path="${tmp_dir}/ibm_mq"
+need_to_fetch=1
+
+rm -rf "${extract_path}"
+mkdir -p "${tmp_dir}" "${cache_dir}" "${extract_path}"
+
+hash_valid() {
+    local path="${1}"
+    echo "${sha256}  ${path}" > "${hash_path}"
+    if sha256sum -c "${hash_path}"; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+if [ -f "${cache_path}" ]; then
+    if ! hash_valid "${cache_path}" ; then
+        rm -f "${cache_path}"
+    else
+        need_to_fetch=0
+    fi
+fi
+
+if [ "${need_to_fetch}" -eq 1 ]; then
+    rm -f "${dl_path}"
+    curl --output "${dl_path}" \
+         --fail \
+         --location \
+         --max-time 300 \
+         --connect-timeout 90 \
+         --retry 5 \
+         --retry-all-errors \
+         --retry-delay 90 \
+         --remote-time \
+         "${url}"
+
+    hash_valid "${dl_path}"
+
+    cp "${dl_path}" "${cache_path}"
+fi
+
+tar -xvzf "${cache_path}" -C "${extract_path}"
