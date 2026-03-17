963fa17459076d4d74c665698ddcd9aeb531b0f8
Austin S. Hemmelgarn <austin@netdata.cloud>
2026-03-17 07:57:11 -0400

    Split checks to their own script and also check linking.

---
+++ .github/scripts/windows-exec-test.ps1
@@ -0,0 +1,38 @@
+#Requires -Version 4.0
+
+$ErrorActionPreference = "Stop"
+
+$ndPath = "C:\Program Files\Netdata\usr\bin\netdata.exe"
+$lddPath = "C:\Program Files\Netdata\usr\bin\ldd.exe"
+
+if (Test-Path -Path $ndPath) {
+    Write-Output "$ndPath found, attempting to check linking"
+
+    if (Test-Path -Path $lddPath) {
+        & $lddPath $ndPath | Tee-Object -Variable lddResult
+
+        if ($LastExitCode -ne 0) {
+            Write-Output "Exit Code: $LastExitCode"
+            exit 1
+        }
+
+        if ($lddResult.Contains('missing')) {
+            Write-Output "Libraries missing from install"
+            exit 1
+        } else {
+            Write-Output "Linking OK"
+        }
+    } else {
+        Write-Output "$lldPath not found, skipping linking check"
+    }
+
+    & $ndPath -W buildinfo
+
+    if ($LastExitCode -ne 0) {
+        Write-Output "Exit Code: $LastExitCode"
+        exit 1
+    }
+} else {
+    Write-Output "$ndPath does not exist"
+    exit 1
+}
