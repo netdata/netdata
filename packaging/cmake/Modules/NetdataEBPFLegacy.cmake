4f54ace7d685e1d2ce6d204d1a6da6e1fc16b35f
thiagoftsm <thiagoftsm@gmail.com>
2026-03-20 00:04:11 +0000

    libbpf_1_7: Update kernel collector version

--- packaging/cmake/Modules/NetdataEBPFLegacy.cmake
+++ packaging/cmake/Modules/NetdataEBPFLegacy.cmake
@@ -18,19 +18,19 @@ function(netdata_fetch_legacy_ebpf_code)
     endif()
 
     if(need_static)
-        set(_hash 3d64ce0077ac5c074141fed6c04babf00f6769dcc7ee546b1893c930250e37eb)
+        set(_hash f06a22d1e25eb3a18a43660ccf3160c7f1c80baa0f0a4ed6c0c6f4be3d11034b)
         set(_libc "static")
     elseif(_libc STREQUAL "glibc")
-        set(_hash c49078c6af66d6dcfac3352606c8608547ba7ee4afb78cb1f68c36d9f52bcdbe)
+        set(_hash 537f73c340ad2ef2d31a9d412d1eeaa7274bbb064328a77a0b7d8c069a163077)
     elseif(_libc STREQUAL "musl")
-        set(_hash a8541037c0aafea31ccbee7c5467389811857a95b71be43cfc27f81dad2dbb74)
+        set(_hash d1da846746df0f2fe67133d97df51f0550048457df75daaf4bc9e0c5d9e7e1a5)
     else()
         message(FATAL_ERROR "Could not determine libc implementation, unable to install eBPF legacy code.")
     endif()
 
     ExternalProject_Add(
         ebpf-code-legacy
-        URL https://github.com/netdata/kernel-collector/releases/download/v1.6.2.2/netdata-kernel-collector-${_libc}-v1.6.2.2.tar.xz
+        URL https://github.com/netdata/kernel-collector/releases/download/v1.7.0/netdata-kernel-collector-${_libc}-v1.7.0.tar.xz
         URL_HASH SHA256=${_hash}
         SOURCE_DIR "${ebpf-legacy_SOURCE_DIR}"
         CONFIGURE_COMMAND ""
