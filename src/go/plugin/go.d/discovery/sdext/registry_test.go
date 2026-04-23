98977d5a7a7862219aa84bc7db040e1fffa31039
ilyam8 <ilya@netdata.cloud>
2026-04-23 16:09:49 +0300

    Add HTTP service discovery for go.d

--- src/go/plugin/go.d/discovery/sdext/registry_test.go
+++ src/go/plugin/go.d/discovery/sdext/registry_test.go
@@ -10,8 +10,10 @@ import (
 
 func TestRegistry_DockerInclusion(t *testing.T) {
 	withDocker := Registry(true)
+	assert.Contains(t, withDocker.Types(), discovererHTTP)
 	assert.Contains(t, withDocker.Types(), discovererDocker)
 
 	withoutDocker := Registry(false)
+	assert.Contains(t, withoutDocker.Types(), discovererHTTP)
 	assert.NotContains(t, withoutDocker.Types(), discovererDocker)
 }
