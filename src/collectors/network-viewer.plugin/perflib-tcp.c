8c43cb011dd1d3ae314a108b2da6b3ab4a5303e9
thiagoftsm <thiagoftsm@gmail.com>
2026-05-08 00:56:42 +0000

    nv_tcp_udp: Add missing headers (P3)

--- src/collectors/network-viewer.plugin/perflib-tcp.c
+++ src/collectors/network-viewer.plugin/perflib-tcp.c
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
 #include "../windows.plugin/windows_plugin.h"
 #include "../windows.plugin/windows-internals.h"
 
