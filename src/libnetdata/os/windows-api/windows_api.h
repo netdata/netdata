3c7d246bcf4563ee6749d61f1465660817d8750e
thiagoftsm <thiagoftsm@gmail.com>
2026-05-08 01:34:20 +0000

    nv_tcp_udp: Address headers (P5)

--- src/libnetdata/os/windows-api/windows_api.h
+++ src/libnetdata/os/windows-api/windows_api.h
@@ -3,6 +3,8 @@
 #ifndef NETDATA_WINDOWS_API_H
 #define NETDATA_WINDOWS_API_H
 
+#define NETDATA_WIN_TCP_STATE_COUNT 12
+
 #if defined(OS_WINDOWS)
 
 #include <stdbool.h>
@@ -11,10 +13,6 @@
 char *netdata_win_local_interface();
 char *netdata_win_local_ip();
 bool netdata_win_collect_tcp_state_counts(uint32_t af, uint32_t state_counts[]);
-
-enum {
-    NETDATA_WIN_TCP_STATE_COUNT = 12,
-};
 
 #endif
 
