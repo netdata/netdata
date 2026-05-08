49af0abaed343739830f5f7117b179f61811adc2
thiagoftsm <thiagoftsm@gmail.com>
2026-05-08 12:15:10 +0000

    nv_tcp_udp: Change API

--- src/collectors/network-viewer.plugin/perflib-tcp.c
+++ src/collectors/network-viewer.plugin/perflib-tcp.c
@@ -1,12 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include "../windows.plugin/windows_plugin.h"
-#include "daemon/daemon-service.h"
+#include "libnetdata/libnetdata.h"
 #include "libnetdata/required_dummies.h"
 #include "libnetdata/os/windows-api/windows_api.h"
 #include "libnetdata/os/windows-perflib/perflib.h"
 
+#include <signal.h>
+
 #define PLUGIN_NETWORK_VIEWER_NAME "network-viewer.plugin"
+
+// Priority values matching the PERFLIB_PRIO enum in windows_plugin.h
+#define PRIO_TCP_IPV4_CONNECTIONS             21069
+#define PRIO_TCP_IPV4_SEGMENTS                21070
+#define PRIO_TCP_IPV4_CONNECTIONS_STATE_COUNT 21071
+#define PRIO_TCP_IPV6_CONNECTIONS             21072
+#define PRIO_TCP_IPV6_SEGMENTS                21073
+#define PRIO_TCP_IPV6_CONNECTIONS_STATE_COUNT 21074
+
+static volatile sig_atomic_t plugin_should_exit = 0;
+static void signal_handler(int sig __maybe_unused) { plugin_should_exit = 1; }
 
 typedef struct {
     const char *af;
@@ -15,9 +27,9 @@ typedef struct {
     const char *family;
     const char *title_prefix;
     const char *context_prefix;
-    long connections_priority;
-    long segments_priority;
-    long states_priority;
+    int connections_priority;
+    int segments_priority;
+    int states_priority;
 
     COUNTER_DATA connection_failures;
     COUNTER_DATA connections_active;
@@ -29,32 +41,9 @@ typedef struct {
     COUNTER_DATA segments_retransmitted;
     COUNTER_DATA segments_sent;
 
-    RRDSET *connections_chart;
-    RRDDIM *rd_connection_failures;
-    RRDDIM *rd_connections_active;
-    RRDDIM *rd_connections_established;
-    RRDDIM *rd_connections_passive;
-    RRDDIM *rd_connections_reset;
-
-    RRDSET *segments_chart;
-    RRDDIM *rd_segments_total;
-    RRDDIM *rd_segments_received;
-    RRDDIM *rd_segments_retransmitted;
-    RRDDIM *rd_segments_sent;
-
-    RRDSET *states_chart;
-    RRDDIM *rd_state_closed;
-    RRDDIM *rd_state_listening;
-    RRDDIM *rd_state_syn_sent;
-    RRDDIM *rd_state_syn_received;
-    RRDDIM *rd_state_established;
-    RRDDIM *rd_state_fin_wait1;
-    RRDDIM *rd_state_fin_wait2;
-    RRDDIM *rd_state_close_wait;
-    RRDDIM *rd_state_closing;
-    RRDDIM *rd_state_last_ack;
-    RRDDIM *rd_state_time_wait;
-    RRDDIM *rd_state_delete_tcb;
+    bool connections_chart_created;
+    bool segments_chart_created;
+    bool states_chart_created;
 } TCP_FAMILY;
 
 static TCP_FAMILY tcp_ipv4 = {
@@ -102,124 +91,74 @@ static void initialize(void)
 
 static void tcp_create_connection_chart(TCP_FAMILY *tcp, int update_every)
 {
-    if (tcp->connections_chart)
+    if (tcp->connections_chart_created)
         return;
+    tcp->connections_chart_created = true;
 
     char context[64];
     char title[64];
-
     snprintfz(context, sizeof(context), "%s.connections", tcp->context_prefix);
     snprintfz(title, sizeof(title), "%s TCP Connections", tcp->title_prefix);
 
-    tcp->connections_chart = rrdset_create_localhost(
-        tcp->type,
-        "connections",
-        NULL,
-        tcp->family,
-        context,
-        title,
-        "connections",
-        PLUGIN_NETWORK_VIEWER_NAME,
-        "PerflibTCP",
-        tcp->connections_priority,
-        update_every,
-        RRDSET_TYPE_LINE);
-
-    tcp->rd_connection_failures = rrddim_add(tcp->connections_chart, "failures", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
-    tcp->rd_connections_active = rrddim_add(tcp->connections_chart, "active", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
-    tcp->rd_connections_established = rrddim_add(tcp->connections_chart, "established", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
-    tcp->rd_connections_passive = rrddim_add(tcp->connections_chart, "passive", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
-    tcp->rd_connections_reset = rrddim_add(tcp->connections_chart, "reset", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
+    fprintf(stdout,
+            "CHART %s.connections '' '%s' connections %s %s line %d %d '' '" PLUGIN_NETWORK_VIEWER_NAME "' PerflibTCP\n",
+            tcp->type, title, tcp->family, context, tcp->connections_priority, update_every);
+    fprintf(stdout, "DIMENSION failures '' incremental 1 1\n");
+    fprintf(stdout, "DIMENSION active '' incremental 1 1\n");
+    fprintf(stdout, "DIMENSION established '' incremental 1 1\n");
+    fprintf(stdout, "DIMENSION passive '' incremental 1 1\n");
+    fprintf(stdout, "DIMENSION reset '' incremental 1 1\n");
 }
 
 static void tcp_create_segments_chart(TCP_FAMILY *tcp, int update_every)
 {
-    if (tcp->segments_chart)
+    if (tcp->segments_chart_created)
         return;
+    tcp->segments_chart_created = true;
 
     char context[64];
     char title[64];
-
     snprintfz(context, sizeof(context), "%s.segments", tcp->context_prefix);
     snprintfz(title, sizeof(title), "%s TCP Segments", tcp->title_prefix);
 
-    tcp->segments_chart = rrdset_create_localhost(
-        tcp->type,
-        "segments",
-        NULL,
-        tcp->family,
-        context,
-        title,
-        "segments/s",
-        PLUGIN_NETWORK_VIEWER_NAME,
-        "PerflibTCP",
-        tcp->segments_priority,
-        update_every,
-        RRDSET_TYPE_LINE);
-
-    tcp->rd_segments_total = rrddim_add(tcp->segments_chart, "total", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
-    tcp->rd_segments_received = rrddim_add(tcp->segments_chart, "received", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
-    tcp->rd_segments_retransmitted =
-        rrddim_add(tcp->segments_chart, "retransmitted", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
-    tcp->rd_segments_sent = rrddim_add(tcp->segments_chart, "sent", NULL, 1, 1, RRD_ALGORITHM_INCREMENTAL);
+    fprintf(stdout,
+            "CHART %s.segments '' '%s' segments/s %s %s line %d %d '' '" PLUGIN_NETWORK_VIEWER_NAME "' PerflibTCP\n",
+            tcp->type, title, tcp->family, context, tcp->segments_priority, update_every);
+    fprintf(stdout, "DIMENSION total '' incremental 1 1\n");
+    fprintf(stdout, "DIMENSION received '' incremental 1 1\n");
+    fprintf(stdout, "DIMENSION retransmitted '' incremental 1 1\n");
+    fprintf(stdout, "DIMENSION sent '' incremental 1 1\n");
 }
 
 static void tcp_create_states_chart(TCP_FAMILY *tcp, int update_every)
 {
-    if (tcp->states_chart)
+    if (tcp->states_chart_created)
         return;
+    tcp->states_chart_created = true;
 
     char context[64];
     char title[64];
-
     snprintfz(context, sizeof(context), "%s.connections_state_count", tcp->context_prefix);
     snprintfz(title, sizeof(title), "%s TCP Connection States", tcp->title_prefix);
 
-    tcp->states_chart = rrdset_create_localhost(
-        tcp->type,
-        "connections_state_count",
-        NULL,
-        tcp->family,
-        context,
-        title,
-        "connections",
-        PLUGIN_NETWORK_VIEWER_NAME,
-        "PerflibTCP",
-        tcp->states_priority,
-        update_every,
-        RRDSET_TYPE_STACKED);
-
-    tcp->rd_state_closed = rrddim_add(tcp->states_chart, "closed", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
-    tcp->rd_state_listening = rrddim_add(tcp->states_chart, "listening", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
-    tcp->rd_state_syn_sent = rrddim_add(tcp->states_chart, "syn_sent", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
-    tcp->rd_state_syn_received = rrddim_add(tcp->states_chart, "syn_received", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
-    tcp->rd_state_established = rrddim_add(tcp->states_chart, "established", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
-    tcp->rd_state_fin_wait1 = rrddim_add(tcp->states_chart, "fin_wait1", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
-    tcp->rd_state_fin_wait2 = rrddim_add(tcp->states_chart, "fin_wait2", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
-    tcp->rd_state_close_wait = rrddim_add(tcp->states_chart, "close_wait", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
-    tcp->rd_state_closing = rrddim_add(tcp->states_chart, "closing", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
-    tcp->rd_state_last_ack = rrddim_add(tcp->states_chart, "last_ack", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
-    tcp->rd_state_time_wait = rrddim_add(tcp->states_chart, "time_wait", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
-    tcp->rd_state_delete_tcb = rrddim_add(tcp->states_chart, "delete_tcb", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
+    fprintf(stdout,
+            "CHART %s.connections_state_count '' '%s' connections %s %s stacked %d %d '' '" PLUGIN_NETWORK_VIEWER_NAME "' PerflibTCP\n",
+            tcp->type, title, tcp->family, context, tcp->states_priority, update_every);
+    fprintf(stdout, "DIMENSION closed '' absolute 1 1\n");
+    fprintf(stdout, "DIMENSION listening '' absolute 1 1\n");
+    fprintf(stdout, "DIMENSION syn_sent '' absolute 1 1\n");
+    fprintf(stdout, "DIMENSION syn_received '' absolute 1 1\n");
+    fprintf(stdout, "DIMENSION established '' absolute 1 1\n");
+    fprintf(stdout, "DIMENSION fin_wait1 '' absolute 1 1\n");
+    fprintf(stdout, "DIMENSION fin_wait2 '' absolute 1 1\n");
+    fprintf(stdout, "DIMENSION close_wait '' absolute 1 1\n");
+    fprintf(stdout, "DIMENSION closing '' absolute 1 1\n");
+    fprintf(stdout, "DIMENSION last_ack '' absolute 1 1\n");
+    fprintf(stdout, "DIMENSION time_wait '' absolute 1 1\n");
+    fprintf(stdout, "DIMENSION delete_tcb '' absolute 1 1\n");
 }
 
-static void tcp_update_state_chart(TCP_FAMILY *tcp, uint32_t state_counts[NETDATA_WIN_TCP_STATE_COUNT])
-{
-    rrddim_set_by_pointer(tcp->states_chart, tcp->rd_state_closed, (collected_number)state_counts[NETDATA_WIN_TCP_STATE_CLOSED]);
-    rrddim_set_by_pointer(tcp->states_chart, tcp->rd_state_listening, (collected_number)state_counts[NETDATA_WIN_TCP_STATE_LISTENING]);
-    rrddim_set_by_pointer(tcp->states_chart, tcp->rd_state_syn_sent, (collected_number)state_counts[NETDATA_WIN_TCP_STATE_SYN_SENT]);
-    rrddim_set_by_pointer(tcp->states_chart, tcp->rd_state_syn_received, (collected_number)state_counts[NETDATA_WIN_TCP_STATE_SYN_RECEIVED]);
-    rrddim_set_by_pointer(tcp->states_chart, tcp->rd_state_established, (collected_number)state_counts[NETDATA_WIN_TCP_STATE_ESTABLISHED]);
-    rrddim_set_by_pointer(tcp->states_chart, tcp->rd_state_fin_wait1, (collected_number)state_counts[NETDATA_WIN_TCP_STATE_FIN_WAIT1]);
-    rrddim_set_by_pointer(tcp->states_chart, tcp->rd_state_fin_wait2, (collected_number)state_counts[NETDATA_WIN_TCP_STATE_FIN_WAIT2]);
-    rrddim_set_by_pointer(tcp->states_chart, tcp->rd_state_close_wait, (collected_number)state_counts[NETDATA_WIN_TCP_STATE_CLOSE_WAIT]);
-    rrddim_set_by_pointer(tcp->states_chart, tcp->rd_state_closing, (collected_number)state_counts[NETDATA_WIN_TCP_STATE_CLOSING]);
-    rrddim_set_by_pointer(tcp->states_chart, tcp->rd_state_last_ack, (collected_number)state_counts[NETDATA_WIN_TCP_STATE_LAST_ACK]);
-    rrddim_set_by_pointer(tcp->states_chart, tcp->rd_state_time_wait, (collected_number)state_counts[NETDATA_WIN_TCP_STATE_TIME_WAIT]);
-    rrddim_set_by_pointer(tcp->states_chart, tcp->rd_state_delete_tcb, (collected_number)state_counts[NETDATA_WIN_TCP_STATE_DELETE_TCB]);
-}
-
-static bool tcp_collect_family(TCP_FAMILY *tcp, int update_every)
+static bool tcp_collect_family(TCP_FAMILY *tcp, int update_every, usec_t dt)
 {
     DWORD id = RegistryFindIDByName(tcp->object_name);
     if (id == PERFLIB_REGISTRY_NAME_NOT_FOUND)
@@ -249,30 +188,44 @@ static bool tcp_collect_family(TCP_FAMILY *tcp, int update_every)
     tcp_create_segments_chart(tcp, update_every);
     tcp_create_states_chart(tcp, update_every);
 
-    rrddim_set_by_pointer(tcp->connections_chart, tcp->rd_connection_failures, (collected_number)tcp->connection_failures.current.Data);
-    rrddim_set_by_pointer(tcp->connections_chart, tcp->rd_connections_active, (collected_number)tcp->connections_active.current.Data);
-    rrddim_set_by_pointer(tcp->connections_chart, tcp->rd_connections_established, (collected_number)tcp->connections_established.current.Data);
-    rrddim_set_by_pointer(tcp->connections_chart, tcp->rd_connections_passive, (collected_number)tcp->connections_passive.current.Data);
-    rrddim_set_by_pointer(tcp->connections_chart, tcp->rd_connections_reset, (collected_number)tcp->connections_reset.current.Data);
-    rrdset_done(tcp->connections_chart);
+    fprintf(stdout, "BEGIN %s.connections %" PRIu64 "\n", tcp->type, dt);
+    fprintf(stdout, "SET failures = %lld\n", (long long)tcp->connection_failures.current.Data);
+    fprintf(stdout, "SET active = %lld\n", (long long)tcp->connections_active.current.Data);
+    fprintf(stdout, "SET established = %lld\n", (long long)tcp->connections_established.current.Data);
+    fprintf(stdout, "SET passive = %lld\n", (long long)tcp->connections_passive.current.Data);
+    fprintf(stdout, "SET reset = %lld\n", (long long)tcp->connections_reset.current.Data);
+    fprintf(stdout, "END\n");
 
-    rrddim_set_by_pointer(tcp->segments_chart, tcp->rd_segments_total, (collected_number)tcp->segments_total.current.Data);
-    rrddim_set_by_pointer(tcp->segments_chart, tcp->rd_segments_received, (collected_number)tcp->segments_received.current.Data);
-    rrddim_set_by_pointer(tcp->segments_chart, tcp->rd_segments_retransmitted, (collected_number)tcp->segments_retransmitted.current.Data);
-    rrddim_set_by_pointer(tcp->segments_chart, tcp->rd_segments_sent, (collected_number)tcp->segments_sent.current.Data);
-    rrdset_done(tcp->segments_chart);
+    fprintf(stdout, "BEGIN %s.segments %" PRIu64 "\n", tcp->type, dt);
+    fprintf(stdout, "SET total = %lld\n", (long long)tcp->segments_total.current.Data);
+    fprintf(stdout, "SET received = %lld\n", (long long)tcp->segments_received.current.Data);
+    fprintf(stdout, "SET retransmitted = %lld\n", (long long)tcp->segments_retransmitted.current.Data);
+    fprintf(stdout, "SET sent = %lld\n", (long long)tcp->segments_sent.current.Data);
+    fprintf(stdout, "END\n");
 
     uint32_t state_counts[NETDATA_WIN_TCP_STATE_COUNT] = {0};
     if (netdata_win_collect_tcp_state_counts(strcmp(tcp->af, "ipv4") == 0 ? AF_INET : AF_INET6, state_counts)) {
-        tcp_update_state_chart(tcp, state_counts);
-        rrdset_done(tcp->states_chart);
+        fprintf(stdout, "BEGIN %s.connections_state_count %" PRIu64 "\n", tcp->type, dt);
+        fprintf(stdout, "SET closed = %lld\n",       (long long)state_counts[NETDATA_WIN_TCP_STATE_CLOSED]);
+        fprintf(stdout, "SET listening = %lld\n",    (long long)state_counts[NETDATA_WIN_TCP_STATE_LISTENING]);
+        fprintf(stdout, "SET syn_sent = %lld\n",     (long long)state_counts[NETDATA_WIN_TCP_STATE_SYN_SENT]);
+        fprintf(stdout, "SET syn_received = %lld\n", (long long)state_counts[NETDATA_WIN_TCP_STATE_SYN_RECEIVED]);
+        fprintf(stdout, "SET established = %lld\n",  (long long)state_counts[NETDATA_WIN_TCP_STATE_ESTABLISHED]);
+        fprintf(stdout, "SET fin_wait1 = %lld\n",    (long long)state_counts[NETDATA_WIN_TCP_STATE_FIN_WAIT1]);
+        fprintf(stdout, "SET fin_wait2 = %lld\n",    (long long)state_counts[NETDATA_WIN_TCP_STATE_FIN_WAIT2]);
+        fprintf(stdout, "SET close_wait = %lld\n",   (long long)state_counts[NETDATA_WIN_TCP_STATE_CLOSE_WAIT]);
+        fprintf(stdout, "SET closing = %lld\n",      (long long)state_counts[NETDATA_WIN_TCP_STATE_CLOSING]);
+        fprintf(stdout, "SET last_ack = %lld\n",     (long long)state_counts[NETDATA_WIN_TCP_STATE_LAST_ACK]);
+        fprintf(stdout, "SET time_wait = %lld\n",    (long long)state_counts[NETDATA_WIN_TCP_STATE_TIME_WAIT]);
+        fprintf(stdout, "SET delete_tcb = %lld\n",   (long long)state_counts[NETDATA_WIN_TCP_STATE_DELETE_TCB]);
+        fprintf(stdout, "END\n");
         have_any = true;
     }
 
     return have_any;
 }
 
-int do_PerflibTCP(int update_every, usec_t dt __maybe_unused)
+int do_PerflibTCP(int update_every, usec_t dt)
 {
     static bool initialized = false;
 
@@ -282,24 +235,34 @@ int do_PerflibTCP(int update_every, usec_t dt __maybe_unused)
     }
 
     bool have_any = false;
-    have_any |= tcp_collect_family(&tcp_ipv4, update_every);
-    have_any |= tcp_collect_family(&tcp_ipv6, update_every);
+    have_any |= tcp_collect_family(&tcp_ipv4, update_every, dt);
+    have_any |= tcp_collect_family(&tcp_ipv6, update_every, dt);
 
     return have_any ? 0 : -1;
 }
 
-int main(int argc __maybe_unused, char **argv __maybe_unused)
+int main(int argc, char **argv)
 {
     nd_log_initialize_for_external_plugins("network-viewer.plugin");
     netdata_threads_init_for_external_plugins(0);
 
-    int update_every = localhost->rrd_update_every;
+    signal(SIGTERM, signal_handler);
+    signal(SIGINT, signal_handler);
+
+    int update_every = 1;
+    if (argc >= 2) {
+        update_every = atoi(argv[1]);
+        if (update_every < 1)
+            update_every = 1;
+    }
+
     heartbeat_t hb;
     heartbeat_init(&hb, update_every * USEC_PER_SEC);
 
-    while(service_running(SERVICE_COLLECTORS)) {
+    while (!plugin_should_exit) {
         usec_t dt = heartbeat_next(&hb);
         do_PerflibTCP(update_every, dt);
+        fflush(stdout);
     }
 
     return 0;
