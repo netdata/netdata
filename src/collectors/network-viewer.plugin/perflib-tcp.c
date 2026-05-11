38eb3026b3d45d35371db9b1dcf2964bc3878a0e
thiagoftsm <thiagoftsm@gmail.com>
2026-05-11 19:44:32 +0000

    nv_tcp_udp: Put Windows code nearest Linux code

--- src/collectors/network-viewer.plugin/perflib-tcp.c
+++ src/collectors/network-viewer.plugin/perflib-tcp.c
@@ -16,10 +16,9 @@
 netdata_mutex_t stdout_mutex;
 static bool plugin_should_exit = false;
 
-// Defined in perflib-udp.c
-void function_udp_stats(const char *transaction, char *function, usec_t *stop_monotonic_ut,
-                        bool *cancelled, BUFFER *payload, HTTP_ACCESS access,
-                        const char *source, void *data);
+// ============================================================
+// TCP
+// ============================================================
 
 typedef struct {
     const char *af;
@@ -51,18 +50,18 @@ static TCP_FAMILY tcp_ipv6 = {
 
 static void initialize_tcp_keys(TCP_FAMILY *tcp)
 {
-    tcp->connection_failures.key    = "Connection Failures";
-    tcp->connections_active.key     = "Connections Active";
+    tcp->connection_failures.key     = "Connection Failures";
+    tcp->connections_active.key      = "Connections Active";
     tcp->connections_established.key = "Connections Established";
-    tcp->connections_passive.key    = "Connections Passive";
-    tcp->connections_reset.key      = "Connections Reset";
-    tcp->segments_total.key         = "Segments/sec";
-    tcp->segments_received.key      = "Segments Received/sec";
-    tcp->segments_retransmitted.key = "Segments Retransmitted/sec";
-    tcp->segments_sent.key          = "Segments Sent/sec";
+    tcp->connections_passive.key     = "Connections Passive";
+    tcp->connections_reset.key       = "Connections Reset";
+    tcp->segments_total.key          = "Segments/sec";
+    tcp->segments_received.key       = "Segments Received/sec";
+    tcp->segments_retransmitted.key  = "Segments Retransmitted/sec";
+    tcp->segments_sent.key           = "Segments Sent/sec";
 }
 
-static void initialize(void)
+static void tcp_initialize(void)
 {
     initialize_tcp_keys(&tcp_ipv4);
     initialize_tcp_keys(&tcp_ipv6);
@@ -143,7 +142,7 @@ void function_tcp_stats(
 {
     static bool initialized = false;
     if (unlikely(!initialized)) {
-        initialize();
+        tcp_initialize();
         initialized = true;
     }
 
@@ -294,6 +293,158 @@ void function_tcp_stats(
     netdata_mutex_unlock(&stdout_mutex);
 }
 
+// ============================================================
+// UDP
+// ============================================================
+
+typedef struct {
+    const char *af;
+    const char *object_name;
+
+    COUNTER_DATA datagrams_no_port;
+    COUNTER_DATA datagrams_received_errors;
+    COUNTER_DATA datagrams_received;
+    COUNTER_DATA datagrams_sent;
+} UDP_FAMILY;
+
+static UDP_FAMILY udp_ipv4 = {
+    .af = "IPv4",
+    .object_name = "UDPv4",
+};
+
+static UDP_FAMILY udp_ipv6 = {
+    .af = "IPv6",
+    .object_name = "UDPv6",
+};
+
+static void initialize_udp_keys(UDP_FAMILY *udp)
+{
+    udp->datagrams_no_port.key         = "Datagrams No Port/sec";
+    udp->datagrams_received_errors.key = "Datagrams Received Errors";
+    udp->datagrams_received.key        = "Datagrams Received/sec";
+    udp->datagrams_sent.key            = "Datagrams Sent/sec";
+}
+
+static void udp_initialize(void)
+{
+    initialize_udp_keys(&udp_ipv4);
+    initialize_udp_keys(&udp_ipv6);
+}
+
+static bool udp_collect_family(UDP_FAMILY *udp)
+{
+    DWORD id = RegistryFindIDByName(udp->object_name);
+    if (id == PERFLIB_REGISTRY_NAME_NOT_FOUND)
+        return false;
+
+    PERF_DATA_BLOCK *pDataBlock = perflibGetPerformanceData(id);
+    if (!pDataBlock)
+        return false;
+
+    PERF_OBJECT_TYPE *pObjectType = perflibFindObjectTypeByName(pDataBlock, udp->object_name);
+    if (!pObjectType)
+        return false;
+
+    bool have_any = false;
+    have_any |= perflibGetObjectCounter(pDataBlock, pObjectType, &udp->datagrams_no_port);
+    have_any |= perflibGetObjectCounter(pDataBlock, pObjectType, &udp->datagrams_received_errors);
+    have_any |= perflibGetObjectCounter(pDataBlock, pObjectType, &udp->datagrams_received);
+    have_any |= perflibGetObjectCounter(pDataBlock, pObjectType, &udp->datagrams_sent);
+
+    return have_any;
+}
+
+static void udp_emit_row(BUFFER *wb, const UDP_FAMILY *udp)
+{
+    buffer_json_add_array_item_array(wb);
+    {
+        buffer_json_add_array_item_string(wb, udp->af);
+        buffer_json_add_array_item_uint64(wb, (uint64_t)udp->datagrams_no_port.current.Data);
+        buffer_json_add_array_item_uint64(wb, (uint64_t)udp->datagrams_received_errors.current.Data);
+        buffer_json_add_array_item_uint64(wb, (uint64_t)udp->datagrams_received.current.Data);
+        buffer_json_add_array_item_uint64(wb, (uint64_t)udp->datagrams_sent.current.Data);
+    }
+    buffer_json_array_close(wb);
+}
+
+void function_udp_stats(
+    const char *transaction, char *function __maybe_unused,
+    usec_t *stop_monotonic_ut __maybe_unused, bool *cancelled __maybe_unused,
+    BUFFER *payload __maybe_unused, HTTP_ACCESS access __maybe_unused,
+    const char *source __maybe_unused, void *data __maybe_unused)
+{
+    static bool initialized = false;
+    if (unlikely(!initialized)) {
+        udp_initialize();
+        initialized = true;
+    }
+
+    udp_collect_family(&udp_ipv4);
+    udp_collect_family(&udp_ipv6);
+
+    time_t now_s = now_realtime_sec();
+    CLEAN_BUFFER *wb = buffer_create(0, NULL);
+    buffer_json_initialize(wb, "\"", "\"", 0, true, BUFFER_JSON_OPTIONS_MINIFY);
+
+    buffer_json_member_add_uint64(wb, "status", HTTP_RESP_OK);
+    buffer_json_member_add_string(wb, "type", "table");
+    buffer_json_member_add_time_t(wb, "update_every", NV_WIN_FUNCTION_UPDATE_EVERY);
+    buffer_json_member_add_boolean(wb, "has_history", false);
+    buffer_json_member_add_string(wb, "help", NV_WIN_FUNCTION_UDP_HELP);
+
+    buffer_json_member_add_array(wb, "data");
+    {
+        udp_emit_row(wb, &udp_ipv4);
+        udp_emit_row(wb, &udp_ipv6);
+    }
+    buffer_json_array_close(wb); // data
+
+    size_t field_id = 0;
+    buffer_json_member_add_object(wb, "columns");
+    {
+        buffer_rrdf_table_add_field(wb, field_id++, "Protocol", "IP Protocol Family",
+            RRDF_FIELD_TYPE_STRING, RRDF_FIELD_VISUAL_VALUE, RRDF_FIELD_TRANSFORM_NONE,
+            0, NULL, NAN, RRDF_FIELD_SORT_ASCENDING, NULL, RRDF_FIELD_SUMMARY_COUNT,
+            RRDF_FIELD_FILTER_MULTISELECT,
+            RRDF_FIELD_OPTS_UNIQUE_KEY | RRDF_FIELD_OPTS_VISIBLE | RRDF_FIELD_OPTS_STICKY, NULL);
+
+        buffer_rrdf_table_add_field(wb, field_id++, "DatagramsNoPort", "Datagrams with No Port",
+            RRDF_FIELD_TYPE_INTEGER, RRDF_FIELD_VISUAL_VALUE, RRDF_FIELD_TRANSFORM_NUMBER,
+            0, "datagrams", NAN, RRDF_FIELD_SORT_DESCENDING, NULL, RRDF_FIELD_SUMMARY_SUM,
+            RRDF_FIELD_FILTER_RANGE, RRDF_FIELD_OPTS_VISIBLE, NULL);
+
+        buffer_rrdf_table_add_field(wb, field_id++, "DatagramsErrors", "Datagrams Received with Errors",
+            RRDF_FIELD_TYPE_INTEGER, RRDF_FIELD_VISUAL_VALUE, RRDF_FIELD_TRANSFORM_NUMBER,
+            0, "datagrams", NAN, RRDF_FIELD_SORT_DESCENDING, NULL, RRDF_FIELD_SUMMARY_SUM,
+            RRDF_FIELD_FILTER_RANGE, RRDF_FIELD_OPTS_VISIBLE, NULL);
+
+        buffer_rrdf_table_add_field(wb, field_id++, "DatagramsReceived", "Received Datagrams",
+            RRDF_FIELD_TYPE_INTEGER, RRDF_FIELD_VISUAL_VALUE, RRDF_FIELD_TRANSFORM_NUMBER,
+            0, "datagrams", NAN, RRDF_FIELD_SORT_DESCENDING, NULL, RRDF_FIELD_SUMMARY_SUM,
+            RRDF_FIELD_FILTER_RANGE, RRDF_FIELD_OPTS_VISIBLE, NULL);
+
+        buffer_rrdf_table_add_field(wb, field_id++, "DatagramsSent", "Sent Datagrams",
+            RRDF_FIELD_TYPE_INTEGER, RRDF_FIELD_VISUAL_VALUE, RRDF_FIELD_TRANSFORM_NUMBER,
+            0, "datagrams", NAN, RRDF_FIELD_SORT_DESCENDING, NULL, RRDF_FIELD_SUMMARY_SUM,
+            RRDF_FIELD_FILTER_RANGE, RRDF_FIELD_OPTS_VISIBLE, NULL);
+    }
+    buffer_json_object_close(wb); // columns
+
+    buffer_json_member_add_time_t(wb, "expires", now_s + NV_WIN_FUNCTION_UPDATE_EVERY);
+    buffer_json_finalize(wb);
+
+    netdata_mutex_lock(&stdout_mutex);
+    wb->response_code = HTTP_RESP_OK;
+    wb->content_type = CT_APPLICATION_JSON;
+    wb->expires = now_s + NV_WIN_FUNCTION_UPDATE_EVERY;
+    pluginsd_function_result_to_stdout(transaction, wb);
+    netdata_mutex_unlock(&stdout_mutex);
+}
+
+// ============================================================
+// main
+// ============================================================
+
 int main(int argc, char **argv)
 {
     netdata_mutex_init(&stdout_mutex);
@@ -308,15 +459,7 @@ int main(int argc, char **argv)
     }
     (void)update_every;
 
-    struct functions_evloop_globals *wg =
-        functions_evloop_init(1, "NV-WIN", &stdout_mutex, &plugin_should_exit, NULL);
-
-    functions_evloop_add_function(wg, NV_WIN_FUNCTION_TCP, function_tcp_stats,
-                                  PLUGINS_FUNCTIONS_TIMEOUT_DEFAULT, NULL);
-    functions_evloop_add_function(wg, NV_WIN_FUNCTION_UDP, function_udp_stats,
-                                  PLUGINS_FUNCTIONS_TIMEOUT_DEFAULT, NULL);
-
-    netdata_mutex_lock(&stdout_mutex);
+    // Announce functions before starting the evloop (matches Linux network-viewer pattern)
     fprintf(stdout,
             PLUGINSD_KEYWORD_FUNCTION " GLOBAL \"%s\" %d \"%s\" \"top\" " HTTP_ACCESS_FORMAT " %d\n",
             NV_WIN_FUNCTION_TCP, PLUGINS_FUNCTIONS_TIMEOUT_DEFAULT, NV_WIN_FUNCTION_TCP_HELP,
@@ -328,7 +471,14 @@ int main(int argc, char **argv)
             (HTTP_ACCESS_FORMAT_CAST)(HTTP_ACCESS_SIGNED_ID | HTTP_ACCESS_SAME_SPACE),
             NV_WIN_FUNCTION_PRIORITY);
     fflush(stdout);
-    netdata_mutex_unlock(&stdout_mutex);
+
+    struct functions_evloop_globals *wg =
+        functions_evloop_init(5, "NV-WIN", &stdout_mutex, &plugin_should_exit, NULL);
+
+    functions_evloop_add_function(wg, NV_WIN_FUNCTION_TCP, function_tcp_stats,
+                                  PLUGINS_FUNCTIONS_TIMEOUT_DEFAULT, NULL);
+    functions_evloop_add_function(wg, NV_WIN_FUNCTION_UDP, function_udp_stats,
+                                  PLUGINS_FUNCTIONS_TIMEOUT_DEFAULT, NULL);
 
     usec_t send_newline_ut = 0;
     const bool tty = isatty(fileno(stdout)) == 1;
