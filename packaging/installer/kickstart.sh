346009792cbcc516186fc01de7b0a03c07af01c3
Austin S. Hemmelgarn <austin@netdata.cloud>
2026-06-03 10:32:56 -0400

    More completely address review comments.

--- packaging/installer/kickstart.sh
+++ packaging/installer/kickstart.sh
@@ -711,7 +711,7 @@ download() {
   fi
 
   if [ -n "${CURL}" ]; then
-    run sh -c "'${CURL}' --fail -q -sSL --connect-timeout 10 --retry 3 --output '${dest}' '${url}' > '${dl_log}'"
+    run sh -c '"${1}" --fail -q -sSL --connect-timeout 10 --retry 3 --output "${2}" "${3}" > "${4}"' _ "${CURL}" "${dest}" "${url}" "${dl_log}"
 
     case "$?" in
       0) return 0 ;;
@@ -817,7 +817,7 @@ get_redirect() {
   rm -f "${output}"
 
   if [ -n "${CURL}" ]; then
-    run sh -c "'${CURL}' '${url}' -s -L -I -o /dev/null -w '%{url_effective}' > '${output}'"
+    run sh -c '"${1}" "${2}" -s -L -I -o /dev/null -w "%{url_effective}" > "${3}"' _ "${CURL}" "${url}" "${output}"
 
     case "$?" in
       0)
