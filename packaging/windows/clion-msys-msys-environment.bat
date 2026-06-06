771d1189a1b56e461a0a8cbf9ef342db6a79ee5f
thiagoftsm <thiagoftsm@gmail.com>
2026-06-06 02:06:02 +0000

    compile_on_windows: Modify compilers

--- packaging/windows/clion-msys-msys-environment.bat
+++ packaging/windows/clion-msys-msys-environment.bat
@@ -1,22 +1,22 @@
 @echo off
 :: In Clion Toolchains
-:: 1. Add a MinGW profile
-:: 2. Set Toolset to C:\msys64\mingw64
+:: 1. Add an MSYS2 UCRT64 profile
+:: 2. Set Toolset to C:\msys64\ucrt64
 :: 3. Add environment and set the full path to this file, like:
-::    C:\msys64\home\costa\src\netdata-ktsaou.git\packaging\utils\clion-mingw64-environment.bat
+::    C:\msys64\home\costa\src\netdata-ktsaou.git\packaging\windows\clion-msys-msys-environment.bat
 :: 4. Let everything else to Bundled and auto-detected
 ::
 set "batch_dir=%~dp0"
 set "batch_dir=%batch_dir:\=/%"
-set MSYSTEM=MSYS
+set MSYSTEM=UCRT64
 
-:: go exists only mingw64 / ucrt64 / etc, not under msys profile
-set GOROOT=C:\msys64\mingw64
+:: go exists only under MSYS2 MinGW-family profiles, not under MSYS
+set GOROOT=C:\msys64\ucrt64
 
 set "PATH=%PATH%;C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64"
 set "PATH=%PATH%;C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.39.33519\bin\Hostx64\x64"
-set "PATH=%PATH%;C:\msys64\usr\bin;C:\msys64\bin;C:\msys64\mingw64\bin"
-::set PKG_CONFIG_EXECUTABLE=C:\msys64\mingw64\bin\pkg-config.exe
-::set CMAKE_C_COMPILER=C:\msys64\mingw64\bin\gcc.exe
-::set CMAKE_CC_COMPILER=C:\msys64\mingw64\bin\g++.exe
-set PROTOBUF_PROTOC_EXECUTABLE=%batch_dir%/protoc.bat
+set "PATH=%PATH%;C:\msys64\usr\bin;C:\msys64\bin;C:\msys64\ucrt64\bin"
+::set PKG_CONFIG_EXECUTABLE=C:\msys64\ucrt64\bin\pkg-config.exe
+::set CMAKE_C_COMPILER=C:\msys64\ucrt64\bin\gcc.exe
+::set CMAKE_CC_COMPILER=C:\msys64\ucrt64\bin\g++.exe
+set PROTOBUF_PROTOC_EXECUTABLE=C:/msys64/ucrt64/bin/protoc.exe
