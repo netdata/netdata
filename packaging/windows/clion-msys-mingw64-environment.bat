aaeb9d11e5d3837bc529e52af7e951259c61e7ba
thiagoftsm <thiagoftsm@gmail.com>
2026-06-07 19:38:50 +0000

    compile_on_windows: Fix https://github.com/netdata/netdata/pull/22650#discussion_r3369739508

--- packaging/windows/clion-msys-mingw64-environment.bat
+++ packaging/windows/clion-msys-mingw64-environment.bat
@@ -10,7 +10,7 @@ set "batch_dir=%~dp0"
 set "batch_dir=%batch_dir:\=/%"
 set MSYSTEM=UCRT64
 set GOROOT=C:\msys64\ucrt64\lib\go
-set PATH="%PATH%;C:\msys64\ucrt64\bin;C:\msys64\usr\bin;C:\msys64\bin"
+set "PATH=%PATH%;C:\msys64\ucrt64\bin;C:\msys64\usr\bin;C:\msys64\bin"
 ::set PKG_CONFIG_EXECUTABLE=C:\msys64\ucrt64\bin\pkg-config.exe
 ::set CMAKE_C_COMPILER=C:\msys64\ucrt64\bin\gcc.exe
 ::set CMAKE_CC_COMPILER=C:\msys64\ucrt64\bin\g++.exe
