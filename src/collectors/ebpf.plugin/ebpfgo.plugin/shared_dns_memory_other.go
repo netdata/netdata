72aaede2193b7cd9e86a726cacde85db00bce660
thiagoftsm <thiagoftsm@gmail.com>
2026-06-18 01:10:17 +0000

    ebpf_nv: Add missing comments

--- src/collectors/ebpf.plugin/ebpfgo.plugin/shared_dns_memory_other.go
+++ src/collectors/ebpf.plugin/ebpfgo.plugin/shared_dns_memory_other.go
@@ -11,5 +11,8 @@ func NewSharedDnsMemoryPublisher() (*SharedDnsMemoryPublisher, error) {
 }
 
 func (p *SharedDnsMemoryPublisher) Publish(_ libbpfloader.DNSSnapshot, _ []libbpfloader.DNSFlowRecord) {
+	// No shared-memory segment is available on non-Linux or non-CGo builds.
 }
-func (p *SharedDnsMemoryPublisher) Close() {}
+func (p *SharedDnsMemoryPublisher) Close() {
+	// No shared-memory segment to release on non-Linux or non-CGo builds.
+}
