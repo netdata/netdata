b1b5e35133be0f729ebe78f29acb737eff62decc
thiagoftsm <thiagoftsm@gmail.com>
2026-06-20 00:58:31 +0000

    ebpf_nv: Adjust config files

--- src/collectors/ebpf.plugin/ebpfgo.plugin/socket_config.go
+++ src/collectors/ebpf.plugin/ebpfgo.plugin/socket_config.go
@@ -1,6 +1,6 @@
 package main
 
-const socketLegacyConfigFile = "ebpf.d/network.conf"
+const socketLegacyConfigFile = "ebpf.d/socket.conf"
 
 func loadSocketConfigFiles() (pluginConfigFile, bool, error) {
 	return loadCollectorConfigFiles(socketLegacyConfigFile)
