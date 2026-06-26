cc96c750b775b7f586c72799ad8f36a9a5d6d260
thiagoftsm <thiagoftsm@gmail.com>
2026-06-26 01:36:21 +0000

    compile_on_windows: Fix constants

--- src/plugins.d/plugins_d.c
+++ src/plugins.d/plugins_d.c
@@ -23,7 +23,11 @@ inline size_t pluginsd_initialize_plugin_directories()
 
     // Get the configuration entry
     if (likely(!plugins_dir_list)) {
-        snprintfz(plugins_dirs, FILENAME_MAX * 2, "\"%s\" \"%s/custom-plugins.d\"", PLUGINS_DIR, CONFIG_DIR);
+        // Use runtime globals instead of compile-time PLUGINS_DIR / CONFIG_DIR so that
+        // Windows path overrides (set before main() by nd_windows_detect_prefix_and_override_paths)
+        // are respected.  On all other platforms these globals equal the compile-time macros.
+        snprintfz(plugins_dirs, FILENAME_MAX * 2, "\"%s\" \"%s/custom-plugins.d\"",
+                  netdata_configured_primary_plugins_dir, netdata_configured_user_config_dir);
         plugins_dir_list = strdupz(inicfg_get_quoted_path_list(&netdata_config, CONFIG_SECTION_DIRECTORIES, "plugins", plugins_dirs));
     }
 
