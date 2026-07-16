c449f92b0f1e7ca46af331d31b9bcd193babd8a6
Costa Tsaousis <costa@netdata.cloud>
2026-07-16 20:08:53 +0300

    fix cgroup-name timeout environment race

--- src/collectors/cgroups.plugin/cgroup-name-config.c
+++ src/collectors/cgroups.plugin/cgroup-name-config.c
@@ -2,8 +2,20 @@
 
 #include "cgroup-name-config.h"
 
+#include <limits.h>
+#include <stdint.h>
 #include <string.h>
 
 bool cgroup_name_is_legacy_default_helper(const char *configured, const char *legacy_default) {
     return configured && legacy_default && strcmp(configured, legacy_default) == 0;
+}
+
+int cgroup_name_timeout_ms_from_seconds(time_t timeout_s) {
+    if (timeout_s <= 0)
+        return 0;
+
+    if ((uintmax_t)timeout_s > (uintmax_t)INT_MAX / 1000)
+        return INT_MAX;
+
+    return (int)((uintmax_t)timeout_s * 1000);
 }
