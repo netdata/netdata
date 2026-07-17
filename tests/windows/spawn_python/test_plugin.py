c62a2b8c53d7fa53b92c6c175a9a8f172b966db6
thiagoftsm <thiagoftsm@gmail.com>
2026-07-17 22:00:16 +0000

    compile_on_windows: remove test after to get results

--- tests/windows/spawn_python/test_plugin.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# Minimal "plugin" used by test_spawn_python.c.
-# Prints one line identifying the interpreter then exits cleanly.
-import sys
-print("INTERP=" + sys.executable, flush=True)
