10bd8a750f7792a1cf1a5cdd03f2f4ea69a356e7
thiagoftsm <thiagoftsm@gmail.com>
2026-08-11 00:45:15 +0000

    compile_on_windows: Fix https://github.com/netdata/netdata/pull/22650#pullrequestreview-4902013191

--- src/health/health_notifications.c
+++ src/health/health_notifications.c
@@ -432,10 +432,6 @@ void health_send_notification(RRDHOST *host, ALARM_ENTRY *ae, struct health_rais
         goto done;
     }
 
-    nd_log(NDLS_DAEMON, NDLP_DEBUG,
-           "[%s]: Sending notification for alarm '%s.%s' status %s.",
-           rrdhost_hostname(host), ae_chart_id(ae), ae_name(ae), rrdcalc_status2string(ae->new_status));
-
     const char *exec      = (ae->exec)      ? ae_exec(ae)      : string2str(host->health.default_exec);
     const char *recipient = (ae->recipient) ? ae_recipient(ae) : string2str(host->health.default_recipient);
 
@@ -444,6 +440,10 @@ void health_send_notification(RRDHOST *host, ALARM_ENTRY *ae, struct health_rais
                           ae_chart_id(ae), ae_name(ae));
         goto done;
     }
+
+    nd_log(NDLS_DAEMON, NDLP_DEBUG,
+           "[%s]: Sending notification for alarm '%s.%s' status %s.",
+           rrdhost_hostname(host), ae_chart_id(ae), ae_name(ae), rrdcalc_status2string(ae->new_status));
 
     char *edit_command = ae->source ? health_edit_command_from_source(ae_source(ae)) : strdupz("UNKNOWN=0=UNKNOWN");
 
