cac7b718f0a85b68a0fd367af7170150e0a50fb6
Costa Tsaousis <costa@netdata.cloud>
2026-08-19 19:06:00 +0300

    apps.plugin: add system.processes_state chart on macOS and FreeBSD (#23296, #23535) (#23534)

--- src/collectors/apps.plugin/apps_plugin.h
+++ src/collectors/apps.plugin/apps_plugin.h
@@ -48,7 +48,7 @@ struct apps_ebpf_cachestat_totals {
 #define PROCESSES_HAVE_CMDLINE               1
 #define PROCESSES_HAVE_PID_LIMITS            0
 #define PROCESSES_HAVE_COMM_AND_NAME         0
-#define PROCESSES_HAVE_STATE                 0
+#define PROCESSES_HAVE_STATE                 1
 #define PPID_SHOULD_BE_RUNNING               1
 #define INCREMENTAL_DATA_COLLECTION          1
 #define CPU_TO_NANOSECONDCORES (1)
@@ -96,7 +96,7 @@ struct pid_info {
 #define PROCESSES_HAVE_CMDLINE               1
 #define PROCESSES_HAVE_PID_LIMITS            0
 #define PROCESSES_HAVE_COMM_AND_NAME         0
-#define PROCESSES_HAVE_STATE                 0
+#define PROCESSES_HAVE_STATE                 1
 #define PPID_SHOULD_BE_RUNNING               1
 #define INCREMENTAL_DATA_COLLECTION          1
 #define CPU_TO_NANOSECONDCORES (1) // already in nanoseconds
