53affe4e9da5ae31b5067e71b42cd182cd5e361c
ilyam8 <ilya@netdata.cloud>
2026-09-15 12:26:36 +0300

    fix(secrets): run command providers through nd-run

--- src/go/plugin/agent/secrets/resolver/cmd_process_unix.go
+++ src/go/plugin/agent/secrets/resolver/cmd_process_unix.go
@@ -5,22 +5,12 @@
 package secretresolver
 
 import (
-	"errors"
-	"os"
+	"context"
 	"os/exec"
-	"syscall"
+
+	"github.com/netdata/netdata/go/plugins/plugin/go.d/pkg/ndexec"
 )
 
-func configureCommandProcessTree(command *exec.Cmd) {
-	command.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-	command.Cancel = func() error {
-		if command.Process == nil {
-			return nil
-		}
-		err := syscall.Kill(-command.Process.Pid, syscall.SIGKILL)
-		if errors.Is(err, syscall.ESRCH) || errors.Is(err, os.ErrProcessDone) {
-			return nil
-		}
-		return err
-	}
+func secretCommandContext(ctx context.Context, binPath string, args ...string) *exec.Cmd {
+	return ndexec.UnprivilegedCommandContextWithPreservedEnv(ctx, binPath, args...)
 }
